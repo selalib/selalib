31dc0d5f718591d01230ba91c75edc2e8d8862e8
Edwin Chacon-Golcher <edwincg@gmail.com>
2011-02-24 17:00:22 +0100

    working memory allocator/deallocator with unit test

---
+++ prototype/src/memory/sll_memory.h
@@ -0,0 +1,116 @@
+#ifndef _sll_memory_h_
+#define _sll_memory_h_
+
+  ! **************************************************************************
+  ! Tentative Selalib's basic memory allocator.
+  !
+  ! This service is provided by two files:
+  ! - sll_memory.h:   the present header file that exposes the allocation,
+  !                   initialization and clearing macros, and
+  ! - sll_memory.F90: which implements the error testing function and
+  !                   other related functionalities.
+  ! 
+  ! Usage:
+  ! The directive
+  ! #include "sll_memory.h" will make available macros:
+  ! SLL_ALLOCATE
+  ! SLL_DEALLOCATE
+  ! SLL_INIT_ARRAY
+  ! SLL_CLEAR_ALLOCATE
+  ! and will provide any additional functionality included in the 
+  ! sll_memory.F90 module. This directive should be put in the 'use' section 
+  ! of the subprogram that wants to use the allocator.
+  !
+  ! Examples: 
+  !
+  ! To allocate the simple array 'a' of 1000 elements:
+  !
+  ! SLL_ALLOCATE(a(1000),err)
+  !
+  ! To allocate the 2D array 'a' with non-default indexing:
+  !
+  ! SLL_ALLOCATE(a(5:55,1:20),err),
+  !
+  ! etc. The syntax comes directly from the native Fortran allocate function.
+  !
+  ! The deallocation macro also requires the integer error variable:
+  !
+  ! SLL_DEALLOCATE(a, err)
+  !
+  ! To initialize an array:
+  !
+  ! SLL_INIT_ARRAY(a,value)
+  ! 
+  ! To allocate and initialize an array to 0:
+  !
+  ! SLL_CLEAR_ALLOCATE(a(1:1000,4:500), err)
+  !
+  ! *************************************************************************
+ 
+use sll_memory
+
+#define SLL_ALLOCATE(arry_name_and_lims, error_var)   \
+  allocate(arry_name_and_lims, stat=error_var);       \
+  call test_error_code(error_var, 'Memory allocation Failure.', __FILE__, __LINE__);
+
+#define SLL_DEALLOCATE(arry_name, error_var)  \
+  deallocate(arry_name, stat=error_var);      \
+  call test_error_code(error_var, 'Error in memory deallocation.', __FILE__, __LINE__); \
+  nullify(arry_name);
+
+#define SLL_INIT_ARRAY(arry,val) arry = merge(val, 0, .true.);
+
+#define SLL_CLEAR_ALLOCATE(arry_name_and_lims, error_var) \
+  SLL_ALLOCATE(arry_name_and_lims, error_var)             \
+  SLL_INIT_ARRAY(arry_name_and_lims, 0) 
+
+  ! **************************************************************************
+  ! IMPLEMENTATION NOTES FOR sll_memory.h:
+  !
+  ! This is an exploratory implementation of Selalib's basic memory allocator.
+  ! There are only two things that we ask of the memory allocator:
+  ! 1. To allocate the memory requested, and
+  ! 2. In case of failure, to stop the program execution with a message 
+  !    informing where was the call made.
+  ! Thus, we are leaving the initialization of the memory or any other
+  ! behavior, like more detailed logging, to other facilities built on top of
+  ! this basic allocator.
+  !
+  ! SLL_ALLOCATE has virtually the same syntax as the native Fortran 
+  ! 'allocate' function, except for the mandatory integer error variable that 
+  ! must be passed as an argument. The caller is responsible for providing 
+  ! this variable, as with the native function.
+  !
+  ! The macro is kind of a wrapper around 'allocate' with error checking.
+  ! It is written in a way that can be dealt with by the Fortran
+  ! preprocessor. The success of the macro relies on the preprocessor 
+  ! requirement that argument parentheses balance, thus, a comma within
+  ! those parentheses will not end the argument. This is why a call like
+  ! SLL_ALLOCATE(array(1:100,1:200),err) has two commas, but only the 
+  ! second acts as an argument separator. This gives a convenient
+  ! flexibility in this case.
+  !
+  ! Possible pitfall: Fortran has a limit on the length of a line of 132
+  !                   characters. The length of some of the above macros
+  !                   will exceed this limit. Any file that includes this
+  !                   header will need to be compiled with a flag to 
+  !                   allow for much longer lines. In gfortran the flag is:
+  !                   -ffree-line-length-none
+  !
+  ! In case that this way of allocating memory is found not satisfactory,
+  ! the file sll_memory.F90 has some elements of an alternative implementation,
+  ! which also uses macros to handle most of the redundancies. However, these 
+  ! macros, while still manageable by fpp, will also yield very long lines 
+  ! that may confuse a Fortran compiler if one does not use flags like:
+  ! -ffree-line-length-none
+  ! The alternative implementation consists of allocating
+  ! subroutines, each specialized for a particular type and dimension of an
+  ! array. In order to pass along the __FILE__ and __LINE__ information to
+  ! the functions conveniently, some macro call has to be made as well, unless
+  ! one goes for the truly inconvenient and ugly solution of passing those
+  ! manually.
+  !
+  ! **************************************************************************
+
+#endif
+ 
---
+++ prototype/src/memory/tester.F90
@@ -0,0 +1,73 @@
+! What we require from the memory allocators is very little:
+! To allocate the memory and in case of failure, to provide information
+! about the call that failed.
+
+program memory_tester
+!  use sll_strings
+#include "sll_memory.h"
+  implicit none
+  integer :: err
+  integer :: i, j, k
+  real, dimension(:), pointer :: a=>null()
+  real, dimension(:,:,:), pointer :: b=>null()
+  integer, dimension(:), pointer :: ai=>null()
+
+  print *, "Memory allocator testing program"
+
+  print *, "allocataing something small... "
+  SLL_CLEAR_ALLOCATE(b(1:4,1:3,1:2),err)
+  print *, 'allocation successful.'
+  print *, 'array just after allocation: '
+  print *, b(:,:,:)
+  print *, 'change the values of the elements...'
+  forall(i=1:4, j=1:3, k=1:2) b(i,j,k) = 7 ! initialize manually...
+  print *, b(:,:,:)
+  print *,'clear the array:'
+  SLL_INIT_ARRAY(b,0)
+
+! turn the following on to test the deallocator
+#if 1 
+  print *, b(:,:,:)
+  print *, 'Deallocate an unitialized pointer'
+  SLL_DEALLOCATE(a,err)
+  print *, 'deallocation successful'
+  write(*, '(a, l5)') 'Is the previous pointer associated?', associated(a)
+#endif
+  print *, 'Proceeding to break the allocator: '
+  SLL_ALLOCATE(b(1:10000,1:10000,1:10000), err)
+
+  print *, selected_int_kind(12)
+  print *, huge(0_8)
+
+!  call TEST_MACRO(a, 1,90000000000_8 )
+  SLL_ALLOCATE(a(90000000000_8), err)
+ 
+
+#if 0  
+contains
+
+subroutine sll_allocate2(a_ptr, limits, descriptor)
+  intrinsic :: modulo, reshape
+  integer, parameter :: long_int = selected_int_kind(12)
+  real, dimension(:), pointer :: a_ptr
+  integer, dimension(:), intent(in) :: limits
+  character(len=*), intent(in) :: descriptor
+  integer :: err
+  integer :: num_dims
+  integer :: i
+  integer(kind=long_int) :: sz = 1
+  num_dims = size(limits)
+  if (modulo(num_dims,2) .ne. 0) then
+     stop 'inconsistent limits passed to sll_allocate'
+  end if
+  num_dims = num_dims/2
+  do i=1,num_dims
+     sz = sz*(limits(i+1)-limits(i))
+  end do
+  call sll_allocate(a_ptr, 1, sz, descriptor)
+!  call reshape(a_ptr, limits)
+end subroutine sll_allocate2
+
+#endif
+
+end program memory_tester
