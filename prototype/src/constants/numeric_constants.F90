3495bb19143f0fb95c9defc9095adeaff20237fc
Edwin Chacon-Golcher <edwincg@gmail.com>
2011-05-30 18:13:36 +0200

    added files to store mathematical constants and physical constants

---
+++ prototype/src/constants/numeric_constants.F90
@@ -0,0 +1,8 @@
+module numeric_constants
+#include "sll_working_precision.h"
+  implicit none
+
+
+  sll_real64, parameter :: sll_pi = 3.1415926535897932384626433_f64
+
+end module numeric_constants
