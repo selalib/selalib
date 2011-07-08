819ade13c439403d2a4b12ddc3fff98dfec0dc0d
Edwin CHACON-GOLCHER <chacongolcher@math.unistra.fr>
2011-07-08 19:00:27 +0200

    added hermite BCs to the splines but there is still a bug. See unit test.

--- prototype/src/splines/unit_test.F90
+++ prototype/src/splines/unit_test.F90
@@ -11,46 +11,82 @@ program spline_tester
   sll_int32 :: err
   sll_int32 :: i
   type(sll_spline_1d), pointer :: sp1
+  type(sll_spline_1d), pointer :: sp2
   sll_real64, allocatable, dimension(:) :: data
-  sll_real64 :: accumulator
-  accumulator = 0.0_f64
+  sll_real64 :: accumulator1, accumulator2
+  accumulator1 = 0.0_f64
+  accumulator2 = 0.0_f64
 
   print *, 'Spline module unit tester'
   print *, 'allocate data array'
-  SLL_ALLOCATE(data(NC+1), err)
+  SLL_ALLOCATE(data(NC), err)
   
   print *, 'initialize data array'
-  do i=1,NC+1
-!data(i) = 1.0
+  do i=1,NC
     data(i) = sin((i-1)*sll_pi/real(NC,f64))
   end do
 !  print *, 'data: '
 !  print *, data(:)
   print *, 'proceed to allocate the spline...'
-  sp1 =>  new_spline_1D( data, NC+1, 0.0_f64, sll_pi, PERIODIC_SPLINE )
+  sp1 =>  new_spline_1D( data(1:NC), NC, 0.0_f64, sll_pi, PERIODIC_SPLINE )
+!  sp1 =>  new_spline_1D( data, NC, 0.0_f64, sll_pi, HERMITE_SPLINE )
+!  sp2 =>  new_spline_1D( data, NC, 0.0_f64, sll_pi, HERMITE_SPLINE )
+  sp2 =>  new_spline_1D( data(1:NC), NC, 0.0_f64, sll_pi, PERIODIC_SPLINE )
   
-  !  print *, 'Contents of the spline:'
-  !  print *, GET_SPLINE_DELTA(sp1)  
-  !  print *, GET_SPLINE_XMIN(sp1)
-  !  print *, GET_SPLINE_XMAX(sp1)
+    print *, 'Contents of the spline 1:'
+    print *, GET_SPLINE_DELTA(sp1)  
+    print *, GET_SPLINE_XMIN(sp1)
+    print *, GET_SPLINE_XMAX(sp1)
+    print *, 'Contents of the spline 2:'
+    print *, GET_SPLINE_DELTA(sp2)  
+    print *, GET_SPLINE_XMIN(sp2)
+    print *, GET_SPLINE_XMAX(sp2)
   print *, 'cumulative errors: '
   do i=1, NC
-     accumulator = accumulator + abs(data(i) - interpolate_value(real(i-1,f64)*sll_pi/real(NC,f64), sp1))
-     !    print *, accumulator
+     accumulator1 = accumulator1 + abs(data(i) - &
+          interpolate_value(real(i-1,f64)*sll_pi/real(NC,f64), sp1))
+     accumulator2 = accumulator2 + abs(data(i) - &
+          interpolate_value(real(i-1,f64)*sll_pi/real(NC,f64), sp2))
+     write (*,'(e20.12, e20.12)') accumulator1, accumulator2
+     !    print *, accumulator1
+     !    print *, accumulator2
   end do
+
+  print *, 'Periodic case: '
   print *, 'average error at the nodes = '
-  print *, accumulator/real(NC,f64)
+  print *, accumulator1/real(NC,f64)
   write (*,'(a,f8.5)')   'original data(0)    = ', data(1)
   write (*,'(a,f20.15)') &
        'interpolated        = ', interpolate_value( 0.0_f64,sp1)
   write (*,'(a,f20.15)')   'original data(NC/4) = ', data(NC/4+1)
   write (*,'(a,f20.15)') &
        'interpolated        = ', interpolate_value( sll_pi/4.0,sp1)
-  !  print *, 'spline coefficients: '
-  !  print *, sp1%c(:)
-  if( accumulator/real(NC,f64) < 1.0e-15 ) then 
+  print *, 'spline coefficients: '
+  print *, sp1%c(:)
+  call delete_spline_1D(sp1)
+  if( accumulator1/real(NC,f64) < 1.0e-15 ) then 
      print *, 'PASSED TEST'
   else
      print *, 'FAILED TEST'
   end if
+  print *, '**************************** '
+  print *, 'Hermite case: '
+  print *, 'average error at the nodes = '
+  print *, accumulator2/real(NC,f64)
+  write (*,'(a,f8.5)')   'original data(0)    = ', data(1)
+  write (*,'(a,f20.15)') &
+       'interpolated        = ', interpolate_value( 0.0_f64,sp2)
+  write (*,'(a,f20.15)')   'original data(NC/4) = ', data(NC/4+1)
+  write (*,'(a,f20.15)') &
+       'interpolated        = ', interpolate_value( sll_pi/4.0,sp2)
+  print *, 'spline coefficients: '
+  print *, sp2%c(:)
+  call delete_spline_1D(sp2)
+  if( accumulator2/real(NC,f64) < 1.0e-15 ) then 
+     print *, 'PASSED TEST'
+  else
+     print *, 'FAILED TEST'
+  end if
+
+
 end program spline_tester
