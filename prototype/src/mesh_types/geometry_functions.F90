ee87c78a4fff0664e8f37c317c10e6acfbcc2cc1
Eric Sonnendrucker <sonnen@math.unistra.fr>
2011-08-10 10:15:46 +0200

    implemented new geometry functions: polar and sinprod

--- prototype/src/mesh_types/geometry_functions.F90
+++ prototype/src/mesh_types/geometry_functions.F90
@@ -1,46 +1,201 @@
 module geometry_functions
 #include "sll_working_precision.h"
 
+use numeric_constants
+implicit none
 contains
-  function default_x1 ( eta1, eta2 )
-    sll_real64  :: default_x1
-    sll_real64, intent(in)   :: eta1
-    sll_real64, intent(in)   :: eta2
-    default_x1 = eta1
-  end function default_x1
+!
+! geometry functions should provide direct mapping, inverse mapping and jacobian
+! all of these should be implement following the examples using a common name to identity
+! on specific mapping
 
-  function default_x2 ( eta1, eta2 )
-    sll_real64  :: default_x2
+  ! identity function
+  !-------------------
+  ! direct mapping
+  function identity_x1 ( eta1, eta2 )
+    sll_real64  :: identity_x1
     sll_real64, intent(in)   :: eta1
     sll_real64, intent(in)   :: eta2
-    default_x2 = eta2
-  end function default_x2
+    identity_x1 = eta1
+  end function identity_x1
 
-  function default_jac11 ( eta1, eta2 )
-    sll_real64  :: default_jac11
+  function identity_x2 ( eta1, eta2 )
+    sll_real64  :: identity_x2
     sll_real64, intent(in)   :: eta1
     sll_real64, intent(in)   :: eta2
-    default_jac11 = 1.0_f64
-  end function default_jac11
+    identity_x2 = eta2
+  end function identity_x2
 
-    function default_jac12 ( eta1, eta2 )
-    sll_real64  :: default_jac12
-    sll_real64, intent(in)   :: eta1
-    sll_real64, intent(in)   :: eta2
-    default_jac12 = 0.0_f64
-  end function default_jac12
+  ! inverse mapping
+  function identity_eta1 ( x1, x2 )
+    sll_real64  :: identity_eta1
+    sll_real64, intent(in)   :: x1
+    sll_real64, intent(in)   :: x2
+    identity_eta1 = x1
+  end function identity_eta1
 
-  function default_jac21 ( eta1, eta2 )
-    sll_real64  :: default_jac21
-    sll_real64, intent(in)   :: eta1
-    sll_real64, intent(in)   :: eta2
-    default_jac21 = 0.0_f64
-  end function default_jac21
+  function identity_eta2 ( x1, x2 )
+    sll_real64  :: identity_eta2
+    sll_real64, intent(in)   :: x1
+    sll_real64, intent(in)   :: x2
+    identity_eta2 = x2
+  end function identity_eta2
 
-  function default_jac22 ( eta1, eta2 )
-    sll_real64  :: default_jac22
+  ! jacobian
+  function identity_jac11 ( eta1, eta2 )
+    sll_real64  :: identity_jac11
     sll_real64, intent(in)   :: eta1
     sll_real64, intent(in)   :: eta2
-    default_jac22 = 1.0_f64
-  end function default_jac22
+    identity_jac11 = 1.0_f64
+  end function identity_jac11
+
+    function identity_jac12 ( eta1, eta2 )
+    sll_real64  :: identity_jac12
+    sll_real64, intent(in)   :: eta1
+    sll_real64, intent(in)   :: eta2
+    identity_jac12 = 0.0_f64
+  end function identity_jac12
+
+  function identity_jac21 ( eta1, eta2 )
+    sll_real64  :: identity_jac21
+    sll_real64, intent(in)   :: eta1
+    sll_real64, intent(in)   :: eta2
+    identity_jac21 = 0.0_f64
+  end function identity_jac21
+
+  function identity_jac22 ( eta1, eta2 )
+    sll_real64  :: identity_jac22
+    sll_real64, intent(in)   :: eta1
+    sll_real64, intent(in)   :: eta2
+    identity_jac22 = 1.0_f64
+  end function identity_jac22
+
+  ! polar coordinates (r = eta1, theta = eta2)
+  !-------------------
+  ! direct mapping
+  function polar_x1 ( eta1, eta2 )
+    sll_real64  :: polar_x1
+    sll_real64, intent(in)   :: eta1
+    sll_real64, intent(in)   :: eta2
+    polar_x1 = eta1 * cos( eta2 )
+  end function polar_x1
+
+  function polar_x2 ( eta1, eta2 )
+    sll_real64  :: polar_x2
+    sll_real64, intent(in)   :: eta1
+    sll_real64, intent(in)   :: eta2
+    polar_x2 = eta1 * sin( eta2 )
+  end function polar_x2
+
+  ! inverse mapping
+  function polar_eta1 ( x1, x2 )
+    sll_real64  :: polar_eta1
+    sll_real64, intent(in)   :: x1
+    sll_real64, intent(in)   :: x2
+    polar_eta1 = sqrt( x1*x1 + x2*x2 )
+  end function polar_eta1
+
+  function polar_eta2 ( x1, x2 )
+    sll_real64  :: polar_eta2
+    sll_real64, intent(in)   :: x1
+    sll_real64, intent(in)   :: x2
+    polar_eta2 = atan( eta2 / eta1 ) 
+  end function polar_eta2
+
+  ! jacobian
+  function polar_jac11 ( eta1, eta2 )
+    sll_real64  :: polar_jac11
+    sll_real64, intent(in)   :: eta1
+    sll_real64, intent(in)   :: eta2
+    polar_jac11 = cos ( eta2) 
+  end function polar_jac11
+
+    function polar_jac12 ( eta1, eta2 )
+    sll_real64  :: polar_jac12
+    sll_real64, intent(in)   :: eta1
+    sll_real64, intent(in)   :: eta2
+    polar_jac12 = - eta1 * sin(theta)
+  end function polar_jac12
+
+  function polar_jac21 ( eta1, eta2 )
+    sll_real64  :: polar_jac21
+    sll_real64, intent(in)   :: eta1
+    sll_real64, intent(in)   :: eta2
+    polar_jac21 = sin ( eta2 )
+  end function polar_jac21
+
+  function polar_jac22 ( eta1, eta2 )
+    sll_real64  :: polar_jac22
+    sll_real64, intent(in)   :: eta1
+    sll_real64, intent(in)   :: eta2
+    polar_jac22 = eta1 * cos ( eta2 )
+  end function polar_jac22
+
+  ! sinusoidal product (see P. Colella et al. JCP 230 (2011) formula (102) p 2968)
+  ! x1 = eta1 + 0.1 * sin(2*pi*eta1) * sin(2*pi*eta2)
+  ! x2 = eta2 + 0.1 * sin(2*pi*eta1) * sin(2*pi*eta2)
+  !-------------------
+  ! direct mapping
+  function sinprod_x1 ( eta1, eta2 )
+    sll_real64  :: sinprod_x1
+    sll_real64, intent(in)   :: eta1
+    sll_real64, intent(in)   :: eta2
+    sinprod_x1 = eta1 + 0.1_f64 * sin(2*sll_pi*eta1) * sin(2*pi*eta2)
+  end function sinprod_x1
+
+  function sinprod_x2 ( eta1, eta2 )
+    sll_real64  :: sinprod_x2
+    sll_real64, intent(in)   :: eta1
+    sll_real64, intent(in)   :: eta2
+    sinprod_x2 = eta2 + 0.1_f64 * sin(2*sll_pi*eta1) * sin(2*pi*eta2)
+  end function sinprod_x2
+
+  ! inverse mapping 
+  ! cannot be computed analytically in this case. Use fixed point iterations.
+  function sinprod_eta1 ( x1, x2 )
+    sll_real64  :: sinprod_eta1
+    sll_real64, intent(in)   :: x1
+    sll_real64, intent(in)   :: x2
+    ! NEEDS TO BE IMPLEMENTED
+    STOP 'function not implemented'
+    sinprod_eta1 = x1
+  end function sinprod_eta1
+
+  function sinprod_eta2 ( x1, x2 )
+    sll_real64  :: sinprod_eta2
+    sll_real64, intent(in)   :: x1
+    sll_real64, intent(in)   :: x2
+    ! NEEDS TO BE IMPLEMENTED
+    STOP 'function not implemented'
+    sinprod_eta2 = x2
+  end function sinprod_eta2
+
+  ! jacobian
+  function sinprod_jac11 ( eta1, eta2 )
+    sll_real64  :: sinprod_jac11
+    sll_real64, intent(in)   :: eta1
+    sll_real64, intent(in)   :: eta2
+    sinprod_jac11 = 1.0_f64 + 0.1_f64 * 2*sll_pi * cos (eta1) * sin (eta2)
+  end function sinprod_jac11
+
+    function sinprod_jac12 ( eta1, eta2 )
+    sll_real64  :: sinprod_jac12
+    sll_real64, intent(in)   :: eta1
+    sll_real64, intent(in)   :: eta2
+    sinprod_jac12 = 0.1_f64 * 2*sll_pi * sin (eta1) * cos (eta2)
+  end function sinprod_jac12
+
+  function sinprod_jac21 ( eta1, eta2 )
+    sll_real64  :: sinprod_jac21
+    sll_real64, intent(in)   :: eta1
+    sll_real64, intent(in)   :: eta2
+    sinprod_jac21 = 0.1_f64 * 2*sll_pi * cos (eta1) * sin (eta2)
+  end function sinprod_jac21
+
+  function sinprod_jac22 ( eta1, eta2 )
+    sll_real64  :: sinprod_jac22
+    sll_real64, intent(in)   :: eta1
+    sll_real64, intent(in)   :: eta2
+    sinprod_jac22 = 1.0_f64 + 0.1_f64 * 2*sll_pi * sin (eta1) * cos (eta2)
+  end function sinprod_jac22
 end module geometry_functions
