d51984ccee11bd46c5995e8810b017896f74aa55
Pierre Navaro <navaro@math.cnrs.fr>
2011-12-07 15:35:35 +0100

    Add Poisson solver and fix compilation problem with diagnostics

--- prototype/src/poisson_solvers/unit_test.F90
+++ prototype/src/poisson_solvers/unit_test.F90
@@ -10,40 +10,68 @@ use numeric_constants
 
 !use geometry1d_module
 use sll_poisson_1D_periodic
+use sll_poisson_2D_periodic
 
-  implicit none
+implicit none
 
-  type (mesh_descriptor_1D), pointer    :: geomx ! 1D mesh
-  type (poisson1dp)         :: poiss1dp !champ electrique
-  type(field_1D_vec1), pointer :: ex, rho, ex_exact
+type (mesh_descriptor_1D), pointer    :: geomx ! 1D mesh
+type (poisson1dp)         :: poiss1dp !champ electrique
+type(field_1D_vec1), pointer :: ex, rho, ex_exact
 
-  sll_int32      :: iflag, mode, i, ncx 
-  sll_real64     :: xmin, xmax, dx
-  !logical, parameter :: per = .true.
-  ! initialisation of 1D periodic mesh 
-  xmin = 0.0; xmax = 2*sll_pi;
-  ncx = 128
+sll_int32      :: iflag, mode, i, ncx 
+sll_real64     :: xmin, xmax, dx
+sll_real64     :: eta1_max, eta1_min, eta2_max, eta2_min
+sll_int32      :: nc_eta1, nc_eta2
+sll_real64     :: delta_eta1, delta_eta2
 
-  geomx    => new_mesh_descriptor_1D( xmin, xmax, ncx )
-  rho      => new_field_1D_vec1( geomx )
-  ex       => new_field_1D_vec1( geomx )
-  ex_exact => new_field_1D_vec1( geomx )
+type(geometry_2D), pointer :: geom
+type(mesh_descriptor_2D), pointer :: mesh
+type(field_2D_vec2), pointer :: u, u_exact
+type(field_2D_vec1), pointer :: rhs
 
-  ! intialize Poisson solver
-  call new(poiss1dp,ncx,iflag) 
+!PN!
+!PN!!Solveur de Poisson1D, commente car ne parche pas
+!PN!
+!PN!logical, parameter :: per = .true.
+!PN! initialisation of 1D periodic mesh 
+!PN!xmin = 0.0; xmax = 2*sll_pi;
+!PN!ncx = 128
+!PN!
+!PN!geomx    => new_mesh_descriptor_1D( xmin, xmax, ncx )
+!PN!rho      => new_field_1D_vec1( geomx )
+!PN!ex       => new_field_1D_vec1( geomx )
+!PN!ex_exact => new_field_1D_vec1( geomx )
+!PN!
+!PN!! intialize Poisson solver
+!PN!call new(poiss1dp,ncx,iflag) 
+!PN!
+!PN!! set rho
+!PN!mode = 7
+!PN!dx = GET_FIELD_DELTA_X1( rho )
+!PN!do i=1,ncx+1
+!PN!   FIELD_1D_AT_I(rho,i) =  mode**2*sin(mode*(i-1)*dx)
+!PN!   FIELD_1D_AT_I(ex_exact,i) = -mode*cos(mode*(i-1)*dx)
+!PN!end do
+!PN!! compute electric field
+!PN!call solve(poiss1dp,ex,rho)
+!PN!     
+!PN!! check solution
+!PN!print*,'mode=',mode,'   error=',maxval(abs(FIELD_DATA(ex)-FIELD_DATA(ex_exact)))
 
-  ! set rho
-  mode = 7
-  dx = GET_FIELD_DELTA_X1( rho )
-  do i=1,ncx+1
-     FIELD_1D_AT_I(rho,i) =  mode**2*sin(mode*(i-1)*dx)
-     FIELD_1D_AT_I(ex_exact,i) = -mode*cos(mode*(i-1)*dx)
-  end do
-  ! compute electric field
-  call solve(poiss1dp,ex,rho)
-     
-  ! check solution
-  print*,'mode=',mode,'   error=',maxval(abs(FIELD_DATA(ex)-FIELD_DATA(ex_exact)))
+
+eta1_min =  -8.0_f64; eta1_max =  8.0_f64
+eta2_min =  -8.0_f64; eta2_max =  8.0_f64 
+
+geom => new_geometry_2D ('cartesian')
+
+nc_eta1 = 100; nc_eta2 = 100
+
+mesh => new_mesh_descriptor_2D(eta1_min, eta1_max, nc_eta1, &
+        PERIODIC, eta2_min, eta2_max, nc_eta2, PERIODIC, geom)
+
+rhs     => new_field_2D_vec1(mesh)
+u       => new_field_2D_vec2(mesh)
+u_exact => new_field_2D_vec2(mesh)
 
 end program testPoisson
 
