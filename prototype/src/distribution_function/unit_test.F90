bc959761c0abede077ec6d2a312a646df0cfdaa3
Pierre Navaro <navaro@math.cnrs.fr>
2011-12-16 17:27:56 +0100

    Add some missing types in mesh_types and distribution_function in order to ru a BSL 4D case in BSL directory

--- prototype/src/distribution_function/unit_test.F90
+++ prototype/src/distribution_function/unit_test.F90
@@ -5,7 +5,10 @@ program unit_test
   use numeric_constants
   use distribution_function
   use sll_diagnostics
+
   implicit none
+
+
   
   sll_int32 :: nc_eta1, nc_eta2
   sll_real64 :: eta1_min, eta1_max, delta_eta1, delta_eta2, eta2_min, eta2_max
@@ -13,6 +16,20 @@ program unit_test
   type(mesh_descriptor_2D), pointer :: m2D_descriptor
   type(sll_distribution_function_2D_t), pointer :: dist_func
   character(32)  :: name = 'dist_func'
+  sll_int32  :: error
+  sll_int32  :: nc_x1, nc_x2, nc_v1, nc_v2
+  sll_real64 :: x1_min, x1_max, x2_min, x2_max
+  sll_real64 :: v1_min, v1_max, v2_min, v2_max
+  type(geometry_2D), pointer :: geom_x
+  type(geometry_2D), pointer :: geom_v
+  type(geometry_2D), pointer :: geom_xv
+  type(mesh_descriptor_2D), pointer :: mesh_x
+  type(mesh_descriptor_2D), pointer :: mesh_v
+  type(mesh_descriptor_2D), pointer :: mesh_xv
+  sll_real64, dimension(:,:), allocatable :: val
+  sll_int32 :: ix, iv, nnode_x1, nnode_v1
+
+  type(sll_distribution_function_4D_t), pointer :: dist_func_4D
 
   nc_eta1 = 100
   nc_eta2 = 100
@@ -34,6 +51,50 @@ program unit_test
   call write_mesh_2D(m2D_descriptor)
   call write_distribution_function (dist_func) 
 
+  x1_min =  0.0_f64; x1_max =  2.0_f64 * sll_pi
+  x2_min =  0.0_f64; x2_max =  2.0_f64 * sll_pi
+
+  v1_min = -6.0_f64; v1_max =  6.0_f64 
+  v2_min = -6.0_f64; v2_max =  6.0_f64 
+
+  nc_v1 = 31; nc_v2 = 31
+  nc_x1 = 31; nc_x2 = 31
+  
+  geom_x => new_geometry_2D('cartesian')
+  geom_v => new_geometry_2D('cartesian')
+
+  mesh_x => new_mesh_descriptor_2D(x1_min, x1_max, nc_x1, &
+          PERIODIC, x2_min, x2_max, nc_x2, PERIODIC, geom_x)
+
+  mesh_v => new_mesh_descriptor_2D(v1_min, v1_max, nc_v1, &
+          PERIODIC, v2_min, v2_max, nc_v2, PERIODIC, geom_v)
+  
+  call write_mesh_2D(mesh_x,"mesh_x")
+  call write_mesh_2D(mesh_v,"mesh_v")
+
+  dist_func_4D => sll_new_distribution_function_4D(mesh_x,mesh_v,NODE_CENTERED_DF,"df_4D")
+  call sll_init_distribution_function_4D( dist_func_4D, LANDAU)
+
+  call write_distribution_function (dist_func_4D) 
+
+  nnode_x1 = mesh_x%nc_eta1+1
+  nnode_v1 = mesh_v%nc_eta1+1
+  SLL_ALLOCATE(val(nnode_x1,nnode_v1), error)
+  do ix = 1, nnode_x1
+     do iv = 1, nnode_v1
+        val(ix,iv) = sum(dist_func_4D%field%data(ix,:,iv,:))
+     end do
+  end do
+
+  geom_xv => new_geometry_2D('cartesian')
+
+  mesh_xv => new_mesh_descriptor_2D(x1_min, x1_max, nc_x1, &
+             PERIODIC, v1_min, v1_max, nc_v1, PERIODIC, geom_xv)
+
+  call write_mesh_2D(mesh_xv,"mesh_xv")
+    
+  call write_vec1d(val,nc_x1+1,nc_v1+1,"df_on_xv_space","mesh_xv",NODE_CENTERED_DF)
+    
   print *, 'Successful, exiting program.'
   
 end program unit_test
