2509dfa10605cd1ba6c7b5a211cd66ab4894bf91
Aliou Diouf <aliou.l.diouf@inria.fr>
2012-02-10 18:01:22 +0100

    poisson_solvers modifications

--- prototype/src/poisson_solvers/sll_poisson_3d_periodic_seq.F90
+++ prototype/src/poisson_solvers/sll_poisson_3d_periodic_seq.F90
@@ -1,5 +1,5 @@
 
-!*******************************************************************
+!***************************************************************************
 !
 ! Selalib 2012     
 ! Module: sll_poisson_3d_periodic.F90
@@ -7,13 +7,13 @@
 !> @brief 
 !> Selalib 3D poisson solver
 !> Start date: Feb. 08, 2012
-!> Last modification: Feb. 09, 2012
+!> Last modification: Feb. 10, 2012
 !   
 !> @authors                    
 !> Aliou DIOUF (aliou.l.diouf@inria.fr), 
 !> Edwin CHACON-GOLCHER (chacongolcher@math.unistra.fr)
 !                                  
-!*******************************************************************
+!***************************************************************************
 
 module sll_poisson_3d_periodic_seq
 
@@ -21,38 +21,52 @@ module sll_poisson_3d_periodic_seq
 #include "sll_working_precision.h"
 #include "misc_utils.h"
 #include "sll_assert.h"
-  
+
   use sll_fft
   use numeric_constants
 
   implicit none
 
   type poisson_3d_periodic_plan
-     sll_int32 :: npx
-     sll_int32 :: npy
-     sll_int32 :: npz
+     sll_int32                   :: nx ! Number of points-1 in x-direction
+     sll_int32                   :: ny ! Number of points-1 in y-direction
+     sll_int32                   :: nz ! Number of points-1 in z-direction
+     type(sll_fft_plan), pointer :: px
+     type(sll_fft_plan), pointer :: py
+     type(sll_fft_plan), pointer :: pz
+     type(sll_fft_plan), pointer :: px_inv
+     type(sll_fft_plan), pointer :: py_inv
+     type(sll_fft_plan), pointer :: pz_inv
   end type poisson_3d_periodic_plan
 
 contains
 
 
-  function new_poisson_3d_periodic_plan()
+  function new_poisson_3d_periodic_plan(array)
 
+    sll_comp64, dimension(:,:,:) :: array
+    sll_int32                                :: nx, ny, nz
+    type(sll_fft_plan), pointer              :: px, py, pz
+    type(sll_fft_plan), pointer              :: px_inv, py_inv, pz_inv
     type (poisson_3d_periodic_plan), pointer :: new_poisson_3d_periodic_plan
 
+    nx = size(array,1)
+    ny = size(array,2)
+    nz = size(array,3)
+
+    new_poisson_3d_periodic_plan%nx = nx
+    new_poisson_3d_periodic_plan%ny = ny
+    new_poisson_3d_periodic_plan%nz = nz
+
+    new_poisson_3d_periodic_plan%px => new_plan_c2c_1d( nx, array(:,1,1), array(:,1,1), FFT_FORWARD )
+    new_poisson_3d_periodic_plan%py => new_plan_c2c_1d( ny, array(1,:,1), array(1,:,1), FFT_FORWARD )
+    new_poisson_3d_periodic_plan%pz => new_plan_c2c_1d( nz, array(1,1,:), array(1,1,:), FFT_FORWARD )
+
+    new_poisson_3d_periodic_plan%px_inv => new_plan_c2c_1d( nx, array(:,1,1), array(:,1,1), FFT_INVERSE)
+    new_poisson_3d_periodic_plan%py_inv => new_plan_c2c_1d( ny, array(1,:,1), array(1,:,1), FFT_INVERSE )
+    new_poisson_3d_periodic_plan%pz_inv => new_plan_c2c_1d( nz, array(1,1,:), array(1,1,:), FFT_INVERSE )
+
   end function new_poisson_3d_periodic_plan
-
-
-  subroutine initialize_poisson_3d_periodic_plan(npx, npy, npz, plan)
-
-    sll_int32                                :: npx, npy, npz
-    type (poisson_3d_periodic_plan), pointer :: plan
-
-    plan%npx = npx
-    plan%npy = npy
-    plan%npz = npz
-
-  end subroutine initialize_poisson_3d_periodic_plan
 
 
   subroutine solve_poisson_3d_periodic(plan, rho, phi)
@@ -60,80 +74,67 @@ contains
     type (poisson_3d_periodic_plan), pointer  :: plan
     sll_real64, dimension(:,:,:)              :: rho, phi
     sll_comp64, dimension(:,:,:), allocatable :: hat_rho, hat_phi
-    sll_int32                                 :: mx, my, mz
+    sll_int32                                 :: nx, ny, nz
     sll_int32                                 :: i, j, k, ierr
-    type(sll_fft_plan), pointer               :: p
 
-    mx = plan%npx - 1
-    my = plan%npy - 1
-    mz = plan%npz - 1
+    nx = plan%nx
+    ny = plan%ny
+    nz = plan%nz
 
-    SLL_ALLOCATE(hat_rho(mx,my,mz), ierr)
-    SLL_ALLOCATE(hat_phi(mx,my,mz), ierr)
+    SLL_ALLOCATE(hat_rho(nx,ny,nz), ierr)
+    SLL_ALLOCATE(hat_phi(nx,ny,nz), ierr)
 
     ! FFTs in x-direction
     hat_rho = cmplx(rho, 0_f64, kind=f64)
-    p => new_plan_c2c_1d( mx, hat_rho(:,1,1), hat_rho(:,1,1), FFT_FORWARD )
-    do k=1,mz
-       do j=1,my
-          call apply_fft_c2c_1d( p, hat_rho(:,j,k), hat_rho(:,j,k) )
+    do k=1,nz
+       do j=1,ny
+          call apply_fft_c2c_1d( plan%px, hat_rho(:,j,k), hat_rho(:,j,k) )
        enddo
     enddo
-    call delete(p)
 
     ! FFTs in y-direction
-    p => new_plan_c2c_1d( my, hat_rho(1,:,1), hat_rho(1,:,1), FFT_FORWARD )
-    do k=1,mz
-       do i=1,mx
-          call apply_fft_c2c_1d( p, hat_rho(i,:,k), hat_rho(i,:,k) )
+    do k=1,nz
+       do i=1,nx
+          call apply_fft_c2c_1d( plan%py, hat_rho(i,:,k), hat_rho(i,:,k) )
        enddo
     enddo
-    call delete(p)
 
     ! FFTs in z-direction
-    p => new_plan_c2c_1d( mz, hat_rho(1,1,:), hat_rho(1,1,:), FFT_FORWARD )
-    do j=1,mx
-       do i=1,mx
-          call apply_fft_c2c_1d( p, hat_rho(i,j,:), hat_rho(i,j,:) )
+    do j=1,nx
+       do i=1,nx
+          call apply_fft_c2c_1d( plan%pz, hat_rho(i,j,:), hat_rho(i,j,:) )
        enddo
     enddo
-    call delete(p)
 
-    do k=1,mz
-       do j=1,my
-          do i=1,mx
+    do k=1,nz
+       do j=1,ny
+          do i=1,nx
              hat_phi(i,j,k) = hat_rho(i,j,k) / ( 4*sll_pi * &
-             ( ((i-1)/mx)**2 + ((j-1)/my)**2 + ((k-1)/mz)**2 ) )
+                  ( ((i-1)/nx)**2 + ((j-1)/ny)**2 + ((k-1)/nz)**2 ) )
           enddo
        enddo
     enddo
 
     ! Inverse FFTs in z-direction
-    p => new_plan_c2c_1d( mz, hat_phi(1,1,:), hat_phi(1,1,:), FFT_INVERSE )
-    do j=1,mx
-       do i=1,mx
-          call apply_fft_c2c_1d( p, hat_phi(i,j,:), hat_phi(i,j,:) )
+    do j=1,nx
+       do i=1,nx
+          call apply_fft_c2c_1d( plan%px_inv, hat_phi(i,j,:), hat_phi(i,j,:) )
        enddo
     enddo
-    call delete(p)
 
     ! Inverse FFTs in y-direction
-    p => new_plan_c2c_1d( my, hat_phi(1,:,1), hat_phi(1,:,1), FFT_INVERSE )
-    do k=1,mz
-       do i=1,mx
-          call apply_fft_c2c_1d( p, hat_phi(i,:,k), hat_phi(i,:,k) )
+    do k=1,nz
+       do i=1,nx
+          call apply_fft_c2c_1d( plan%py_inv, hat_phi(i,:,k), hat_phi(i,:,k) )
        enddo
     enddo
-    call delete(p)
 
     ! Inverse FFTs in x-direction
-    p => new_plan_c2c_1d( mx, hat_phi(:,1,1), hat_phi(:,1,1), FFT_INVERSE )
-    do k=1,mz
-       do j=1,my
-          call apply_fft_c2c_1d( p, hat_phi(:,j,k), hat_phi(:,j,k) )
+    do k=1,nz
+       do j=1,ny
+          call apply_fft_c2c_1d( plan%pz_inv, hat_phi(:,j,k), hat_phi(:,j,k) )
        enddo
     enddo
-    call delete(p)
 
     phi = real(hat_phi, f64)
 
@@ -143,13 +144,21 @@ contains
   end subroutine solve_poisson_3d_periodic
 
 
-  subroutine delete_poisson_3d_periodic_plan(poisson)
+  subroutine delete_poisson_3d_periodic_plan(plan)
 
-    type (poisson_3d_periodic_plan), pointer :: poisson
+    type (poisson_3d_periodic_plan), pointer :: plan
 
     ! Fixme: some error checking, whether the poisson pointer is associated
     ! for instance
-    SLL_ASSERT( associated(poisson) )
+    SLL_ASSERT( associated(plan) )
+
+    call delete(plan%px)
+    call delete(plan%py)
+    call delete(plan%pz)
+
+    call delete(plan%px_inv)
+    call delete(plan%py_inv)
+    call delete(plan%pz_inv)
 
   end subroutine delete_poisson_3d_periodic_plan
 
