136aed44df6f9638b995e2d0fee8a1a137978821
Aliou Diouf <aliou.l.diouf@inria.fr>
2012-02-22 17:47:31 +0100

    poisson_solvers modifications

--- prototype/src/poisson_solvers/sll_poisson_3d_periodic_seq.F90
+++ prototype/src/poisson_solvers/sll_poisson_3d_periodic_seq.F90
@@ -31,6 +31,9 @@ module sll_poisson_3d_periodic_seq
      sll_int32                   :: nx ! Number of points-1 in x-direction
      sll_int32                   :: ny ! Number of points-1 in y-direction
      sll_int32                   :: nz ! Number of points-1 in z-direction
+     sll_real64                  :: Lx
+     sll_real64                  :: Ly
+     sll_real64                  :: Lz
      type(sll_fft_plan), pointer :: px
      type(sll_fft_plan), pointer :: py
      type(sll_fft_plan), pointer :: pz
@@ -42,12 +45,13 @@ module sll_poisson_3d_periodic_seq
 contains
 
 
-  function new_poisson_3d_periodic_plan(array)
+  function new_poisson_3d_periodic_plan(array, Lx, Ly, Lz)
 
     sll_comp64, dimension(:,:,:)             :: array
     sll_int32                                :: nx, ny, nz
+    sll_int32                                 :: i, j, k, ierr
+    sll_real64                                :: Lx, Ly, Lz
     type (poisson_3d_periodic_plan), pointer :: new_poisson_3d_periodic_plan
-    sll_int32                                :: ierr
 
     nx = size(array,1)
     ny = size(array,2)
@@ -58,11 +62,13 @@ contains
     new_poisson_3d_periodic_plan%nx = nx
     new_poisson_3d_periodic_plan%ny = ny
     new_poisson_3d_periodic_plan%nz = nz
+    new_poisson_3d_periodic_plan%Lx = Lx
+    new_poisson_3d_periodic_plan%Ly = Ly
+    new_poisson_3d_periodic_plan%Lz = Lz
 
     new_poisson_3d_periodic_plan%px => new_plan_c2c_1d( nx, array(:,1,1), array(:,1,1), FFT_FORWARD )
     new_poisson_3d_periodic_plan%py => new_plan_c2c_1d( ny, array(1,:,1), array(1,:,1), FFT_FORWARD )
     new_poisson_3d_periodic_plan%pz => new_plan_c2c_1d( nz, array(1,1,:), array(1,1,:), FFT_FORWARD )
-
     new_poisson_3d_periodic_plan%px_inv => new_plan_c2c_1d( nx, array(:,1,1), array(:,1,1), FFT_INVERSE)
     new_poisson_3d_periodic_plan%py_inv => new_plan_c2c_1d( ny, array(1,:,1), array(1,:,1), FFT_INVERSE )
     new_poisson_3d_periodic_plan%pz_inv => new_plan_c2c_1d( nz, array(1,1,:), array(1,1,:), FFT_INVERSE )
@@ -77,28 +83,26 @@ contains
     sll_comp64, dimension(:,:,:), allocatable :: hat_rho, hat_phi
     sll_int32                                 :: nx, ny, nz
     sll_int32                                 :: i, j, k, ierr
+    sll_real64                                :: Lx, Ly, Lz
+    sll_real64                                :: ind_x, ind_y, ind_z
 
     nx = plan%nx
     ny = plan%ny
     nz = plan%nz
+    Lx = plan%Lx
+    Ly = plan%Ly
+    Lz = plan%Lz
 
     SLL_ALLOCATE(hat_rho(nx,ny,nz), ierr)
     SLL_ALLOCATE(hat_phi(nx,ny,nz), ierr)
 
     ! FFTs in z-direction
- !call random_number(rho)
     hat_rho = cmplx(rho, 0_f64, kind=f64)
-!print*, 'test1:',hat_rho(1,1,:)
     do j=1,ny
        do i=1,nx
           call apply_fft_c2c_1d( plan%pz, hat_rho(i,j,:), hat_rho(i,j,:) )
        enddo
-    enddo
-!print*, 'test2',hat_rho(1,1,:)
-!stop
-    do k=1,nz-1,2
-       hat_rho(:,:,k) = -hat_rho(:,:,k)
-    enddo
+    enddo       
 
     ! FFTs in y-direction
     do k=1,nz
@@ -107,31 +111,38 @@ contains
        enddo
     enddo
 
-    do j=1,ny-1,2
-       hat_rho(:,j,:) = -hat_rho(:,j,:)
-    enddo
-
     ! FFTs in x-direction
     do k=1,nz
        do j=1,ny
           call apply_fft_c2c_1d( plan%px, hat_rho(:,j,k), hat_rho(:,j,k) )
        enddo
     enddo
-    do i=1,nx-1,2
-       hat_rho(i,:,:) = -hat_rho(i,:,:)
-    enddo
 
     hat_rho = hat_rho/(nx*ny*nz)
 
     ! Compute hat_phi, phi = inv_fft(hat_phi)
-    do k=-nz/2,nz/2-1
-       do j=-ny/2,ny/2-1
-          do i=-nx/2,nx/2-1
-             if ( k==0 ) then
-                hat_phi(i+nx/2+1,j+ny/2+1,k+nz/2+1) = 0.d0
+    do k=1,nz
+       do j=1,ny
+          do i=1,nx
+             if (i<=nx/2) then
+                ind_x = real(i-1,f64)
              else
-                hat_phi(i+nx/2+1,j+ny/2+1,k+nz/2+1) = hat_rho(i+nx/2+1,j+ny/2+1,k+nz/2+1) / ( &
-                  4*sll_pi**2*( (real(i,f64)/nx)**2 + (real(j,f64)/ny)**2 + (real(k,f64)/nz)**2 ) )
+                ind_x = real(nx-(i-1),f64)
+             endif
+             if (j<=ny/2) then
+                ind_y = real(j-1,f64)
+             else
+                ind_y = real(ny-(j-1),f64)
+             endif
+             if (k<=nz/2) then
+                ind_z = real(k-1,f64)
+             else
+                ind_z = real(nz-(k-1),f64)
+             endif                
+             if ( (ind_x==0) .and. (ind_y==0) .and. (ind_z==0) ) then
+                hat_phi(i,j,k) = 0.d0
+             else
+                hat_phi(i,j,k) = hat_rho(i,j,k) / (4*sll_pi**2*((ind_x/Lx)**2+(ind_y/Ly)**2+(ind_z/Lz)**2))
              endif
           enddo
        enddo
@@ -143,9 +154,6 @@ contains
           call apply_fft_c2c_1d( plan%pz_inv, hat_phi(i,j,:), hat_phi(i,j,:) )
        enddo
     enddo
-    do k=1,nz-1,2
-       hat_phi(:,:,k) = -hat_phi(:,:,k)
-    enddo
 
     ! Inverse FFTs in y-direction
     do k=1,nz
@@ -153,18 +161,12 @@ contains
           call apply_fft_c2c_1d( plan%py_inv, hat_phi(i,:,k), hat_phi(i,:,k) )
        enddo
     enddo
-    do j=1,ny-1,2
-       hat_phi(:,j,:) = -hat_phi(:,j,:)
-    enddo
 
     ! Inverse FFTs in x-direction
     do k=1,nz
        do j=1,ny
           call apply_fft_c2c_1d( plan%px_inv, hat_phi(:,j,k), hat_phi(:,j,k) )
        enddo
-    enddo
-    do i=1,nx-1,2
-       hat_phi(i,:,:) = -hat_phi(i,:,:)
     enddo
 
     phi = real(hat_phi, f64) !Inverse FFTs are not normalized
