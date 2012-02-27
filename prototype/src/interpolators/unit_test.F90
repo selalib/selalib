cbfc7008fc92d2c49573cd5d31207911b4308c30
Eric Sonnendrucker <sonnen@math.unistra.fr>
2012-02-27 09:42:25 +0100

    testing abstract interface for interpolate1d

---
+++ prototype/src/interpolators/unit_test.F90
@@ -0,0 +1,13 @@
+program unit_test
+#include "sll_working_precision.h"
+#include "sll_assert.h"
+#include "sll_memory.h"
+  use numeric_constants
+  use sll_interpolator1d_interface
+  implicit none
+
+ 
+
+  print *, 'Successful, exiting program.'
+
+end program unit_test
