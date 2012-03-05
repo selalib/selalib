a284aee6b265c4bfeddd5320727936c8c15389dd
Aliou Diouf <aliou.l.diouf@inria.fr>
2012-03-05 16:55:27 +0100

    poisson_solvers modifications in poisson_branch

--- prototype/src/poisson_solvers/unit_test.F90
+++ prototype/src/poisson_solvers/unit_test.F90
@@ -6,12 +6,12 @@
 !
 !> @brief 
 !> Selalib poisson solvers unit test
-!> Last modification: Feb. 29, 2012
+!> Last modification: March 05, 2012
 !   
 !> @authors                    
 !> Aliou DIOUF (aliou.l.diouf@inria.fr), 
 !> Edwin CHACON-GOLCHER (chacongolcher@math.unistra.fr)
-!> Pierre NAVARRO (navaro@math.unistra.fr)
+!> Pierre NAVARO (navaro@math.unistra.fr)
 !                                  
 !***************************************************************************
 
@@ -50,128 +50,129 @@ program test_poisson_solvers
   Ly = 2*sll_pi
   Lz = 2*sll_pi
 
-  !call test_sll_poisson_3d_periodic(nx, ny, nz, Lx, Ly, Lz)
-
   colsz  = sll_get_collective_size(sll_world_collective)
- ! if (colsz==1) then
+  if (colsz==1) then
      print*, ' '
      print*, 'Test poisson_2d'
      print*, ' '
      call test_poisson_2d()
-  !endif
+     print*, ' '
+  endif
+
+  call test_sll_poisson_3d_periodic(nx, ny, nz, Lx, Ly, Lz)
 
   call sll_halt_collective()
 
 contains
 
   subroutine test_poisson_2d()
-  !-------------------------------------------------------------------
-  !  test 1D Poisson solver based on FFT
-  !-------------------------------------------------------------------
-!type (mesh_descriptor_1D), pointer :: geom
-!type (poisson1dp)                  :: poisson_1d
-!type (field_1D_vec1), pointer      :: ex, rho_1d, ex_exact
-!sll_int32   :: ncx, iflag 
-!sll_real64  :: xmin, xmax
-!sll_real64  :: dx
+    !-------------------------------------------------------------------
+    !  test 1D Poisson solver based on FFT
+    !-------------------------------------------------------------------
+    !type (mesh_descriptor_1D), pointer :: geom
+    !type (poisson1dp)                  :: poisson_1d
+    !type (field_1D_vec1), pointer      :: ex, rho_1d, ex_exact
+    !sll_int32   :: ncx, iflag 
+    !sll_real64  :: xmin, xmax
+    !sll_real64  :: dx
 
-sll_real64  :: eta1_max, eta1_min, eta2_max, eta2_min
-sll_int32   :: nc_eta1, nc_eta2
-sll_int32   :: error
+    sll_real64  :: eta1_max, eta1_min, eta2_max, eta2_min
+    sll_int32   :: nc_eta1, nc_eta2
+    sll_int32   :: error
 
-type(poisson_2d_periodic), pointer :: poisson_e_fields
-type(poisson_2d_periodic), pointer :: poisson_potential
-type(geometry_2D),         pointer :: geom
-type(mesh_descriptor_2D),  pointer :: mesh
-type(field_2D_vec2),       pointer :: exy, exy_exact
-type(field_2D_vec1),       pointer :: rho, phi, phi_exact
-sll_real64                         :: x1, x2
-sll_int32                          :: mode
-sll_int32                          :: i, j
+    type(poisson_2d_periodic), pointer :: poisson_e_fields
+    type(poisson_2d_periodic), pointer :: poisson_potential
+    type(geometry_2D),         pointer :: geom
+    type(mesh_descriptor_2D),  pointer :: mesh
+    type(field_2D_vec2),       pointer :: exy, exy_exact
+    type(field_2D_vec1),       pointer :: rho, phi, phi_exact
+    sll_real64                         :: x1, x2
+    sll_int32                          :: mode
+    sll_int32                          :: i, j
 
-eta1_min = .0_f64; eta1_max = 2.0_f64*sll_pi
-eta2_min = .0_f64; eta2_max = 2.0_f64*sll_pi
+    eta1_min = .0_f64; eta1_max = 2.0_f64*sll_pi
+    eta2_min = .0_f64; eta2_max = 2.0_f64*sll_pi
 
-geom => new_geometry_2D ('cartesian')
+    geom => new_geometry_2D ('cartesian')
 
-nc_eta1 = 127; nc_eta2 = 127
+    nc_eta1 = 127; nc_eta2 = 127
 
-mesh => new_mesh_descriptor_2D(eta1_min, eta1_max, nc_eta1, &
-        PERIODIC, eta2_min, eta2_max, nc_eta2, PERIODIC, geom)
+    mesh => new_mesh_descriptor_2D(eta1_min, eta1_max, nc_eta1, &
+         PERIODIC, eta2_min, eta2_max, nc_eta2, PERIODIC, geom)
 
-call write_mesh_2D(mesh)
+    call write_mesh_2D(mesh)
 
-rho       => new_field_2D_vec1(mesh)
-phi       => new_field_2D_vec1(mesh)
-phi_exact => new_field_2D_vec1(mesh)
-exy       => new_field_2D_vec2(mesh)
-exy_exact => new_field_2D_vec2(mesh)
+    rho       => new_field_2D_vec1(mesh)
+    phi       => new_field_2D_vec1(mesh)
+    phi_exact => new_field_2D_vec1(mesh)
+    exy       => new_field_2D_vec2(mesh)
+    exy_exact => new_field_2D_vec2(mesh)
 
-poisson_e_fields  => new_poisson_2d_periodic(exy)
-poisson_potential => new_poisson_2d_periodic(phi)
+    poisson_e_fields  => new_poisson_2d_periodic(exy)
+    poisson_potential => new_poisson_2d_periodic(phi)
 
-mode = 2
-do i = 1, nc_eta1+1
-   do j = 1, nc_eta2+1
-      x1 = eta1_min+(i-1)*mesh%delta_eta1
-      x2 = eta2_min+(j-1)*mesh%delta_eta2
-      phi%data(i,j) = mode * sin(mode*x1) * cos(mode*x2)
-      rho%data(i,j) = -2_f64 * mode**3 * sin(mode*x1)*cos(mode*x2)
-      exy%data(i,j)%v1 =  mode**2*cos(mode*x1)*cos(mode*x2)
-      exy%data(i,j)%v2 = -mode**2*sin(mode*x1)*sin(mode*x2)
-   end do
-end do
+    mode = 2
+    do i = 1, nc_eta1+1
+       do j = 1, nc_eta2+1
+          x1 = eta1_min+(i-1)*mesh%delta_eta1
+          x2 = eta2_min+(j-1)*mesh%delta_eta2
+          phi%data(i,j) = mode * sin(mode*x1) * cos(mode*x2)
+          rho%data(i,j) = -2_f64 * mode**3 * sin(mode*x1)*cos(mode*x2)
+          exy%data(i,j)%v1 =  mode**2*cos(mode*x1)*cos(mode*x2)
+          exy%data(i,j)%v2 = -mode**2*sin(mode*x1)*sin(mode*x2)
+       end do
+    end do
 
-call write_vec1d(phi%data,mesh%nc_eta1+1,mesh%nc_eta2+1,"phi0","mesh",0)
-call write_vec1d(rho%data,mesh%nc_eta1+1,mesh%nc_eta2+1,"rho0","mesh",0)
-call write_vec2d(exy%data%v1,exy%data%v2,mesh%nc_eta1+1,mesh%nc_eta2+1,"exy0","mesh",0)
+    call write_vec1d(phi%data,mesh%nc_eta1+1,mesh%nc_eta2+1,"phi0","mesh",0)
+    call write_vec1d(rho%data,mesh%nc_eta1+1,mesh%nc_eta2+1,"rho0","mesh",0)
+    call write_vec2d(exy%data%v1,exy%data%v2,mesh%nc_eta1+1,mesh%nc_eta2+1,"exy0","mesh",0)
 
-FIELD_DATA(exy_exact) = FIELD_DATA(exy)
-FIELD_DATA(phi_exact) = FIELD_DATA(phi)
+    FIELD_DATA(exy_exact) = FIELD_DATA(exy)
+    FIELD_DATA(phi_exact) = FIELD_DATA(phi)
 
-call solve_poisson_2d_periodic(poisson_potential,phi%data,rho%data,error)
-call solve_poisson_2d_periodic(poisson_e_fields,exy,rho,error)
+    call solve_poisson_2d_periodic(poisson_potential,phi%data,rho%data,error)
+    call solve_poisson_2d_periodic(poisson_e_fields,exy,rho,error)
 
-call write_vec1d(phi%data,mesh%nc_eta1+1,mesh%nc_eta2+1,"phi1","mesh",0)
-call write_vec1d(rho%data,mesh%nc_eta1+1,mesh%nc_eta2+1,"rho1","mesh",0)
-call write_vec2d(exy%data%v1,exy%data%v2,mesh%nc_eta1+1,mesh%nc_eta2+1,"exy1","mesh",0)
+    call write_vec1d(phi%data,mesh%nc_eta1+1,mesh%nc_eta2+1,"phi1","mesh",0)
+    call write_vec1d(rho%data,mesh%nc_eta1+1,mesh%nc_eta2+1,"rho1","mesh",0)
+    call write_vec2d(exy%data%v1,exy%data%v2,mesh%nc_eta1+1,mesh%nc_eta2+1,"exy1","mesh",0)
 
-write(*,*) " Ex Error = " , maxval(abs(exy_exact%data%v1-exy%data%v1))
-write(*,*) " Ey Error = " , maxval(abs(exy_exact%data%v2-exy%data%v2))
-write(*,*) " Po Error = " , maxval(abs(phi_exact%data+phi%data))
+    write(*,*) " Ex Error = " , maxval(abs(exy_exact%data%v1-exy%data%v1))
+    write(*,*) " Ey Error = " , maxval(abs(exy_exact%data%v2-exy%data%v2))
+    write(*,*) " Po Error = " , maxval(abs(phi_exact%data+phi%data))
 
-call delete_poisson_2d_periodic(poisson_e_fields)
-call delete_poisson_2d_periodic(poisson_potential)
+    call delete_poisson_2d_periodic(poisson_e_fields)
+    call delete_poisson_2d_periodic(poisson_potential)
 
-call delete_field_2D_vec1( rho )
-call delete_field_2D_vec1( phi )
-call delete_field_2D_vec1( phi_exact )
-call delete_field_2D_vec2( exy )
-call delete_field_2D_vec2( exy_exact )
+    call delete_field_2D_vec1( rho )
+    call delete_field_2D_vec1( phi )
+    call delete_field_2D_vec1( phi_exact )
+    call delete_field_2D_vec2( exy )
+    call delete_field_2D_vec2( exy_exact )
 
-!Solveur de Poisson1D, commente car ne parche pas
+    !Solveur de Poisson1D, commente car ne parche pas
 
-!xmin = 0.0; xmax = 2*sll_pi;
-!ncx = 128
-!
-!geomx    => new_mesh_descriptor_1D( xmin, xmax, ncx, PERIODIC )
-!rho1d    => new_field_1D_vec1( geomx )
-!ex       => new_field_1D_vec1( geomx )
-!ex_exact => new_field_1D_vec1( geomx )
-!
-!call new(poisson_1d,ncx,iflag) 
-!
-!mode = 7
-!dx = geomx%delta_eta1
-!do i=1,ncx+1
-!   rho1d%data(i) =  mode**2*sin(mode*(i-1)*dx)
-!   ex_exact%data(i) = -mode*cos(mode*(i-1)*dx)
-!end do
-!! compute electric field
-!call solve(poisson_1d,ex,rho1d)
-!    
-! check solution
-!print*,'mode=',mode,'   error=',maxval(abs(FIELD_DATA(ex)-FIELD_DATA(ex_exact)))
+    !xmin = 0.0; xmax = 2*sll_pi;
+    !ncx = 128
+    !
+    !geomx    => new_mesh_descriptor_1D( xmin, xmax, ncx, PERIODIC )
+    !rho1d    => new_field_1D_vec1( geomx )
+    !ex       => new_field_1D_vec1( geomx )
+    !ex_exact => new_field_1D_vec1( geomx )
+    !
+    !call new(poisson_1d,ncx,iflag) 
+    !
+    !mode = 7
+    !dx = geomx%delta_eta1
+    !do i=1,ncx+1
+    !   rho1d%data(i) =  mode**2*sin(mode*(i-1)*dx)
+    !   ex_exact%data(i) = -mode*cos(mode*(i-1)*dx)
+    !end do
+    !! compute electric field
+    !call solve(poisson_1d,ex,rho1d)
+    !    
+    ! check solution
+    !print*,'mode=',mode,'   error=',maxval(abs(FIELD_DATA(ex)-FIELD_DATA(ex_exact)))
 
   end subroutine test_poisson_2d
 
@@ -211,7 +212,7 @@ call delete_field_2D_vec2( exy_exact )
              x = (i-1)*dx
              phi_an1(i,j,k) = cos(x)*sin(y)*cos(z)
              phi_an2(i,j,k) = (4/(sll_pi*sqrt(sll_pi)*Lx*Ly*Lz)) * exp(-.5*(x-Lx/2)**2) * &
-                              exp(-.5*(y-Ly/2)**2) * sin(z)
+                  exp(-.5*(y-Ly/2)**2) * sin(z)
              rho2(i,j,k) = phi_an2(i,j,k) * ( 3 - ( (x-Lx/2)**2 + (y-Ly/2)**2 ) )
           enddo
        enddo
@@ -264,7 +265,7 @@ call delete_field_2D_vec2( exy_exact )
        call flush()
        print*, 'Test poisson_3d in parallel'
     endif
-    
+
     call solve_poisson_3d_periodic_par(plan, rho1, phi_par1)
     call solve_poisson_3d_periodic_par(plan, rho2, phi_par2)
 
@@ -320,7 +321,7 @@ call delete_field_2D_vec2( exy_exact )
     endif
 
     call sll_collective_reduce(sll_world_collective, (/ ok /), 1, MPI_PROD, 0, prod4test )
-    
+
     if (myrank==0) then
        if (prod4test(1)==1.d0) then
           call flush()
@@ -334,7 +335,7 @@ call delete_field_2D_vec2( exy_exact )
     SLL_DEALLOCATE_ARRAY(phi_par1, ierr)
     SLL_DEALLOCATE_ARRAY(phi_par2, ierr)
 
-    end subroutine test_sll_poisson_3d_periodic
+  end subroutine test_sll_poisson_3d_periodic
 
-    end program test_poisson_solvers
+end program test_poisson_solvers
 
