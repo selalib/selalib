50b48462205297af23a3dd1730a2d18ac6d498fa
Pierre Navaro <navaro@math.cnrs.fr>
2012-03-13 16:46:12 +0100

    New interface in 3d Aliou s poisson solver

--- prototype/src/poisson_solvers/test_poisson_3d_par.F90
+++ prototype/src/poisson_solvers/test_poisson_3d_par.F90
@@ -45,8 +45,6 @@ program test_poisson_3d_par
   sll_int32, dimension(1:3)                 :: global
   sll_int32                                 :: gi, gj, gk
   sll_int32                                 :: myrank
-  sll_real32                                :: ok = 1.d0
-  sll_real32, dimension(1)                  :: prod4test
   type(layout_3D_t), pointer                :: layout
   sll_int64                                 :: colsz ! collective size
   sll_int32                                 :: i_test
@@ -62,6 +60,8 @@ program test_poisson_3d_par
   Lz = 2*sll_pi
 
   colsz  = sll_get_collective_size(sll_world_collective)
+  myrank = sll_get_collective_rank(sll_world_collective)
+
 
   dx = Lx/nx
   dy = Ly/ny
@@ -74,13 +74,13 @@ program test_poisson_3d_par
 
   plan => new_poisson_3d_periodic_plan_par(nx, ny, nz, Lx, Ly, Lz)
 
-  layout => plan%layout_x
-  call compute_local_sizes( layout, nx_loc, ny_loc, nz_loc )
-  SLL_ALLOCATE(rho(nx_loc,ny_loc,nz_loc), ierr)
-
   layout => plan%layout_z
   call compute_local_sizes( layout, nx_loc, ny_loc, nz_loc )
   SLL_ALLOCATE(phi(nx_loc,ny_loc,nz_loc), ierr)
+
+  layout => plan%layout_x
+  call compute_local_sizes( layout, nx_loc, ny_loc, nz_loc )
+  SLL_ALLOCATE(rho(nx_loc,ny_loc,nz_loc), ierr)
 
   do k=1,nz
      do j=1,ny
@@ -102,6 +102,8 @@ program test_poisson_3d_par
                             * exp(-.5*(y-Ly/2)**2) * sin(z)
      end if
 
+     layout => plan%layout_x
+     call compute_local_sizes( layout, nx_loc, ny_loc, nz_loc )
      do k=1,nz_loc
         do j=1,ny_loc
            do i=1,nx_loc
@@ -109,8 +111,13 @@ program test_poisson_3d_par
               gi = global(1)
               gj = global(2)
               gk = global(3)
-              rho(i,j,k) = 3*phi_an(gi,gj,gk)
-              rho(i,j,k) = phi_an(gi,gj,gk) * ( 3 - ( (x(gi,gj,gk)-Lx/2)**2 + (y(gi,gj,gk)-Ly/2)**2 ) )
+              if (i_test == 1) then
+                 rho(i,j,k) = 3*phi_an(gi,gj,gk)
+              else if(i_test == 2) then
+                 rho(i,j,k) = phi_an(gi,gj,gk)             &
+                             *(3-((x(gi,gj,gk)-Lx/2)**2    &
+                                 +(y(gi,gj,gk)-Ly/2)**2))
+              end if
            enddo
         enddo
      enddo
@@ -135,30 +142,11 @@ program test_poisson_3d_par
 
     average_err  = average_err  / (nx_loc*ny_loc*nz_loc)
 
-    call flush()
-    print*, ' myrank '
-    print*, 'local average error:', average_err
-    print*, 'dx*dy*dz =', dx*dy*dz
-
-    if ( average_err > dx*dx*dy) then
-       call flush()
-       print*, ' '
-       print*, 'Test stoppped by sll_poisson_3d_periodic_par test'
-       print*, 'myrank=', myrank
-       print*, ' '
-       stop
-    endif
-
-    call sll_collective_reduce(sll_world_collective, (/ ok /), 1, MPI_PROD, 0, prod4test )
-
-    if (myrank==0) then
-       if (prod4test(1)==1.d0) then
-          call flush()
-          print*, ' '
-          print*, 'sll_poisson_3d_periodic_par test: PASS'
-          print*, ' '
-       endif
-    endif
+    call flush(); print*, ' ------------------'
+    call flush(); print*, ' myrank ', myrank
+    call flush(); print*, 'local average error:', average_err
+    call flush(); print*, 'dx*dy*dz =', dx*dy*dz
+    call flush(); print*, ' ------------------'
 
   end do
 
