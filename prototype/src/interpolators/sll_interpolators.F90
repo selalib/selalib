960b39fb902fdfed8a2ff5528961f2879aca90dc
Eric Sonnendrücker <sonnendr@gmail.com>
2012-03-14 10:01:44 +0100

    definition of interpolator class

--- prototype/src/interpolators/sll_interpolators.F90
+++ prototype/src/interpolators/sll_interpolators.F90
@@ -6,77 +6,93 @@
 module sll_interpolator_1d
 #include "sll_working_precision.h"
 #include "sll_memory.h"
-#include "sll_assert.h"
-use sll_splines
-use sll_weno
   implicit none
 
   type :: interpolator_1d
-     type(sll_spline_1D), pointer :: spl
-     type(sll_weno_1d), pointer   :: weno
-     procedure(realarray_function_interpolator1d_int_realarray_realarray), pointer, nopass:: interpolate_1d
+   contains
+     procedure, pass :: interpolate_1d
+     procedure, pass :: reconstruct_1d
   end type interpolator_1d
 
-  abstract interface
-     function realarray_function_interpolator1d_int_realarray_realarray(interp, num_points, coordinates, data) result(res)
-       use sll_working_precision
-       import interpolator_1d
-       type(interpolator_1d), pointer, intent(in)     :: interp
-       sll_int32, intent(in)                         :: num_points    ! number of interpolation points
-       sll_real64, dimension(:), intent(in)          :: coordinates   ! points where output is desired
-       sll_real64, dimension(:), intent(in)          :: data          ! data to be interpolated 
-       sll_real64, dimension(num_points)             :: res
-     end function realarray_function_interpolator1d_int_realarray_realarray
-  end interface
+!!$  abstract interface
+!!$     function interpolate_1d_abstract(this, num_points, data, coordinates) result(res)
+!!$       use sll_working_precision
+!!$       import interpolator_1d_t
+!!$       class(interpolator_1d_t), intent(in)     :: this
+!!$       sll_int32, intent(in)                 :: num_points    ! number of interpolation points
+!!$       sll_real64, dimension(:), intent(in)  :: data          ! data to be interpolated 
+!!$       sll_real64, dimension(:), intent(in)  :: coordinates   ! points where output is desired
+!!$       sll_real64, dimension(num_points)     :: res
+!!$     end function interpolate_1d_abstract
+!!$  end interface
+
 contains
-  function new_interpolator_1d(what_interpolator, num_points, xmin, xmax, bc_type) result(interp)
-    type(interpolator_1d), pointer :: interp
-    character(len=*) :: what_interpolator
-    sll_int32 :: num_points
-    sll_real64 :: xmin, xmax
-    sll_int32 :: bc_type
-    sll_int32 :: ierr
+  ! Provides a dummy interpolate_1d function defining the interface
+  ! should never be used
+  function interpolate_1d(this, num_points, data, coordinates) result(res)
+    class(interpolator_1d), intent(in)    :: this
+    sll_int32, intent(in)                 :: num_points    ! number of interpolation points
+    sll_real64, dimension(:), intent(in)  :: data          ! data to be interpolated 
+    sll_real64, dimension(:), intent(in)  :: coordinates   ! points where output is desired
+    sll_real64, dimension(num_points)     :: res
 
-    SLL_ALLOCATE(interp,ierr)
-    select case(what_interpolator)
-    case ('spline')
-       SLL_ALLOCATE(interp%spl,ierr)
-       interp%spl => new_spline_1d( num_points, xmin, xmax, bc_type )
-       interp%interpolate_1d => interpolate_1d_spline
-    case ('weno')
-       SLL_ALLOCATE(interp%weno,ierr)
-       interp%weno => new_WENO_1D(num_points, xmin, xmax)
-       interp%interpolate_1d => interpolate_1d_weno
-    case default
-       print*, 'interpolator ', what_interpolator, ' not implemented'
-       stop
-    end select
+    res = data
+    print*, 'sll_interpolators_interface.F90: This is the dummy interpolator_1d function. It should never be called'
+    stop
+  end function interpolate_1d
+  ! Reconstruction consists in computing a flux function from cell average data for a conservative scheme
+  ! We provide here a reconstruct_1d function that computes the primitive and interpolates the primitive
+  function reconstruct_1d(this, num_points, data, coordinates) result(res)
+    class(interpolator_1d), intent(in)    :: this
+    sll_int32, intent(in)                 :: num_points    ! number of interpolation points
+    sll_real64, dimension(:), intent(in)  :: data          ! data to be interpolated 
+    sll_real64, dimension(:), intent(in)  :: coordinates   ! points where output is desired
+    sll_real64, dimension(num_points)     :: res
+    ! local variables
+    sll_real64, dimension(num_points) :: primitive
+    sll_real64  :: delta
+    sll_real64  :: eta
+    sll_real64  :: avg
+    sll_int32   :: i
+    sll_int32   :: ierr
+    sll_real64  :: coord_new, lperiod
 
-  end function new_interpolator_1d
+    ! compute the primitive on the logical mesh on [0,1] assuming that data are cell values
+    !--------------------------------------------------------------------------------------
+    delta = 1.0_f64 / (num_points-1)
+    primitive (1) = 0.0
+    do i = 2, num_points
+       primitive ( i ) = primitive ( i-1 ) + delta * data( i )
+    end do
+    ! average of dist func along the line
+    avg = primitive ( num_points )
+    ! modify primitive so that it becomes periodic
+    eta = 0.0
+    do i = 2, num_points
+       eta = eta + delta
+       primitive ( i ) = primitive ( i ) - avg * eta
+    end do
+    ! Interpolate primitive at interpolation points
+    !----------------------------------------------
+    res = this%interpolate_1d(num_points, primitive, coordinates)
+    ! Add back value that had been removed for periodicity
+    !-----------------------------------------------------
+     if (coordinates(1) > 0.5_f64) then
+        lperiod = -1.0_f64
+     else
+        lperiod = 0.0_f64
+     end if
+     coord_new = coordinates(1) +  lperiod
+     res ( 1 ) = res ( 1 ) + avg * coord_new
+     do i = 2, num_points
+        ! We need here to find the points where it has been modified by periodicity
+        if (coordinates(i) < coordinates(i-1)) then
+           lperiod = lperiod + 1.0_f64
+        end if
+        coord_new = coordinates(i) +  lperiod
+        res ( i ) = res ( i ) + avg * coord_new
+     end do
+     
+   end function reconstruct_1d
 
-  ! Define spline interpolation of values in data define on original grid at points coordinates
-  function interpolate_1d_spline(interp, num_points, coordinates, data) result(data_out)
-    type(interpolator_1d), pointer, intent(in)       :: interp
-    sll_int32,  intent(in)                 :: num_points
-    sll_real64, dimension(:), intent(in)   :: coordinates
-    sll_real64, dimension(:), intent(in)   :: data
-    sll_real64, dimension(num_points)      :: data_out
-    
-    ! compute the interpolating spline coefficients
-    call compute_spline_1D( data, interp%spl%bc_type, interp%spl )
-    call interpolate_array_values( coordinates, data_out, num_points, interp%spl )
-    
-  end function interpolate_1d_spline
-
-  function interpolate_1d_weno(interp, num_points, coordinates, data) result(data_out)
-    type(interpolator_1d), pointer, intent(in)       :: interp
-    sll_int32,  intent(in)                 :: num_points
-    sll_real64, dimension(:), intent(in)   :: coordinates
-    sll_real64, dimension(:), intent(in)   :: data
-    sll_real64, dimension(num_points)      :: data_out
-    
-    data_out = interpolate_WENO_1D(interp%weno, num_points, coordinates, data)
-
-  end function interpolate_1d_weno
-
-end module sll_interpolator_1d
+ end module sll_interpolator_1d
