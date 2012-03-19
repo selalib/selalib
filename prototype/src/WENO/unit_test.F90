16ec55d468cb77fef38a93c79715a10cdf67759f
Eric Sonnendrucker <sonnen@math.unistra.fr>
2012-03-19 18:04:15 +0100

    fixed compilation errors with new mapped meshes

--- prototype/src/WENO/unit_test.F90
+++ prototype/src/WENO/unit_test.F90
@@ -1,78 +1,72 @@
 ! input is the data and its corresponding coordinates_d with size NP
 ! output is the out at the coordinates_o with size NP
 ! default that the size of output and input data are the same = NP
+! Tests WENO interpolation and reconstruction
+! This code is made for cell centered values
 
-! to compile
-! gfortran -O3 -ffree-line-length-none sll_memory.F90 sll_assert.F90 sll_working_precision.F90 sll_WENO.F90 unit_test.F90 -o unit_test
-
-!
-! the option -O3 is for optimization
-! the option -ffree-line-length-none: 
-!    -ffree-line-length-n
-!    Set column after which characters are ignored in typical free-form lines in the source file. For free-form, 
-!    the default value is 132. n may be none, meaning that the entire line is meaningful.
-!    -ffree-line-length-0 means
-!    the same thing as -ffree-line-length-none. 
-!    -fmax-identifier-length=n 
-
-
-program SLWENO_tester
+program WENO_tester
 #include "sll_working_precision.h"
 #include "sll_assert.h"
 #include "sll_memory.h"
 
-  use sll_WENO
+  use WENO_interp
+  use WENO_recon
   use numeric_constants
   implicit none
 #define NP 30
 
   sll_int32 :: err    ! indicator for allocating data array
   sll_int32 :: i
-  type(sll_WENO_1d) :: weno
+  type(WENO_interp_1d) :: WENO_int
+  type(WENO_recon_1d)  :: WENO_rec
   sll_real64, allocatable, dimension(:) :: data       ! data at coordinates_d with size NP
   sll_real64, allocatable, dimension(:) :: coordinates_d  ! coordinates for data with size NP
   sll_real64, allocatable, dimension(:) :: out        ! data at coordinates_i with size NP
   sll_real64, allocatable, dimension(:) :: coordinates_o  ! coordinates for interpolated locations with size NP
   sll_real64, allocatable, dimension(:) :: data_interp  ! exact function value at interpolated locations
+sll_real64, allocatable, dimension(:) :: data_deriv  ! exact derivative value at initial locations
   sll_real64 :: dx
   sll_real64 :: accumulator1
 
   ! constants
   accumulator1 = 0.0_f64
 
-  print *, 'SL WENO module unit tester'
+  print *, 'WENO module unit tester'
   print *, 'allocate data array'
   SLL_ALLOCATE(data(NP), err)
   SLL_ALLOCATE(out(NP), err)
   SLL_ALLOCATE(coordinates_d(NP), err)
   SLL_ALLOCATE(coordinates_o(NP), err)
   SLL_ALLOCATE(data_interp(NP), err)
+  SLL_ALLOCATE(data_deriv(NP), err)
 
   print *, 'initialize data and coordinates array'
-  dx = sll_pi*2.0_f64/(NP-1)
+  dx = sll_pi*2.0_f64/NP
 
   do i=1,NP
-     coordinates_d(i) = (i-1)*dx !(i-0.5_f64)*dx
+     coordinates_d(i) = (i-0.5_f64)*dx
      coordinates_o(i) = modulo(coordinates_d(i) - dx/3.0_f64, 2*sll_pi)
      data(i)        = 2.0_f64*(sin(coordinates_d(i)) + 2.5_f64 + cos(coordinates_d(i)))
      data_interp(i) = 2.0_f64*(sin(coordinates_o(i)) + 2.5_f64 + cos(coordinates_o(i)))
+     data_deriv(i) = 2.0_f64*(cos(coordinates_d(i))  - sin(coordinates_d(i)))
   enddo
-  print *, 'proceed to allocate the data for WENO interpolation...'
-  weno =  new_WENO_1D(NP, coordinates_d(1), coordinates_d(NP))  
+  print *, 'Test WENO interpolation...'
+  weno_int =  new_WENO_1D(NP, coordinates_d(1), coordinates_d(NP))  
   ! NP is the number of data points, the second and the third argument is the min and max of coordinates
   ! set up the basic information for data: np, xmin, xmax, delta (mesh size), rdelta (reciprocal of delta)
-  out =  interpolate_WENO_1D( weno, NP, data, coordinates_o )
+   call interpolate_WENO_1D( weno_int, NP, data, coordinates_o, 4, out )
 
   print *, 'Contents of the weno:'
-  print *, 'weno%xmin=', weno%xmin
-  print *, 'weno%xmax=',weno%xmax
-  print *, 'dx=',weno%delta
-  print *, '1/dx=',weno%rdelta
-  print *, 'left b. =', weno%xmin !- weno%delta/2.0_f64
-  print *, 'right b= ', weno%xmax !+ weno%delta/2.0_f64 
+  print *, 'weno%xmin=', weno_int%xmin
+  print *, 'weno%xmax=',weno_int%xmax
+  print *, 'dx=',weno_int%delta
+  print *, '1/dx=',weno_int%rdelta
+  print *, 'left b. =', weno_int%xmin !- weno%delta/2.0_f64
+  print *, 'right b= ', weno_int%xmax !+ weno%delta/2.0_f64 
   print *, 'cumulative errors: '
   print *, 'periodic case, NP points: '
   print *, 'interpolating individual values from 1 to NP:'
+  
   do i=1, NP
      accumulator1 = accumulator1 + abs(data_interp(i) - out(i))
      if(i.eq.1)then
@@ -83,10 +77,41 @@ program SLWENO_tester
 
   print *, '----------------------------------------------------'
   print *, 'RESULTS: '
-  print *, 'Periodic case: '
+  print *, 'WENO interpolation. Periodic case: '
   print *, 'average error at the nodes (single values) = '
   print *, accumulator1/real(NP,f64)
 
-  call delete_WENO_1D(weno)
+  call delete_WENO_1D(weno_int)
 
-end program SLWENO_tester
+  print *, 'Test WENO reconstruction...'
+  weno_rec =  new_WENO_recon_1D(NP, 0.0_f64, sll_pi*2.0_f64)   
+  ! NP is the number of data points, the second and the third argument is the min and max of coordinates
+  ! set up the basic information for data: np, xmin, xmax, delta (mesh size), rdelta (reciprocal of delta)
+   call FD_WENO_recon_1D( weno_rec, NP, data,  4, out )
+
+  print *, 'Contents of the weno:'
+  print *, 'weno%xmin=', weno_rec%xmin
+  print *, 'weno%xmax=',weno_rec%xmax
+  print *, 'dx=',weno_rec%delta
+  print *, '1/dx=',weno_rec%rdelta
+  print *, 'left b. =', weno_rec%xmin !- weno%delta/2.0_f64
+  print *, 'right b= ', weno_rec%xmax !+ weno%delta/2.0_f64 
+  print *, 'cumulative errors: '
+  print *, 'periodic case, NP points: '
+  print *, 'reconstructing individual values from 1 to NP:'
+  accumulator1 = 0.0_f64
+  do i=1, NP
+     accumulator1 = accumulator1 + abs(data_deriv(i) - out(i))
+     if(i.eq.1)then
+        print *, abs(data_deriv(i) - out(i)), data_deriv(i) , out(i)
+     endif
+  end do
+
+
+  print *, '----------------------------------------------------'
+  print *, 'RESULTS: '
+  print *, 'WENO reconstruction. Periodic case: '
+  print *, 'average error (single values) = '
+  print *, accumulator1/real(NP,f64)
+
+end program WENO_tester
---
+++ prototype/src/interpolators/sll_interpolators_1d_base.F90
@@ -0,0 +1,35 @@
+!> \brief abstract data type for 1D interpolation and reconstruction
+!> 
+
+module sll_interpolator_1d
+#include "sll_working_precision.h"
+  implicit none
+
+  type, abstract :: interpolator_1d_base
+   contains
+     procedure(interpolate_1d_array), pass, deferred :: interpolate_array
+     procedure(reconstruct_1d_array), pass, deferred :: reconstruct_array
+  end type interpolator_1d_base
+
+  abstract interface
+     function interpolate_1d_array(this, num_points, data, coordinates) result(res)
+       use sll_working_precision
+       import interpolator_1d_base
+       class(interpolator_1d_base), intent(in)     :: this
+       sll_int32, intent(in)                 :: num_points    ! size of output array
+       sll_real64, dimension(:), intent(in)  :: data          ! data to be interpolated 
+       sll_real64, dimension(:), intent(in)  :: coordinates   ! points where output is desired
+       sll_real64, dimension(num_points)     :: res
+     end function interpolate_1d_array
+  end interface
+  abstract interface
+     function reconstruct_1d_array(this, data) result(res)
+       use sll_working_precision
+       import interpolator_1d_base
+       class(interpolator_1d_base), intent(in)     :: this
+       sll_real64, dimension(:), intent(in)  :: data          ! data to be interpolated 
+     end function reconstruct_1d_array
+  end interface
+
+
+ end module sll_interpolator_1d
