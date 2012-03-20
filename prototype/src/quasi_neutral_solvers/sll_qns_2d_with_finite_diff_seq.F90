43a18f6c26af25c38de9f92b3cfb1c96cddd6fb9
Aliou Diouf <aliou.l.diouf@inria.fr>
2012-03-20 18:02:04 +0100

    sequential qns and unit test which compile

--- prototype/src/quasi_neutral_solvers/sll_qns_2d_with_finite_diff_seq.F90
+++ prototype/src/quasi_neutral_solvers/sll_qns_2d_with_finite_diff_seq.F90
@@ -8,7 +8,7 @@
 !> Selalib 2D (r, theta) quasi-neutral solver with finite differences
 !> Some arrays are here in 3D for remap utilities
 !> Start date: March 12, 2012
-!> Last modification: March 13, 2012
+!> Last modification: March 20, 2012
 !   
 !> @authors                    
 !> Aliou DIOUF (aliou.l.diouf@inria.fr), 
@@ -16,7 +16,7 @@
 !                                  
 !***************************************************************************
 
-module sll_qns_2d_with_finite_diff
+module sll_qns_2d_with_finite_diff_seq
 
 #include "sll_memory.h"
 #include "sll_working_precision.h"
@@ -37,21 +37,22 @@ contains
   subroutine solve_qn_2d_with_finite_diff_seq(plan, phi)
 
     type(qns_2d_with_finite_diff_plan_seq), pointer :: plan
-    sll_real64                                      :: r, theta, dr, dtheta 
-    sll_int32                                       :: n, n_theta, i, j, ierr
-    sll_real64, dimension(:,:), allocatable         :: rho, phi
+    sll_real64                                      :: dr, dtheta 
+    sll_int32                                       :: n, ntheta, i, j, ierr
+    sll_real64, dimension(:,:)                      :: phi
+    sll_real64, dimension(:,:), allocatable         :: rho
     sll_comp64, dimension(:,:), allocatable         :: tild_rho, tild_phi
     sll_comp64, dimension(:),   allocatable         :: b, f, g
     sll_int32, dimension(:),    allocatable         :: ipiv
     sll_real64, dimension(:),   allocatable         :: a_resh ! 3*n
     sll_real64, dimension(:),   allocatable         :: cts  ! 7*N allocation
 
-    dr = (plan%r_max-plan%r_min)/(plan%n_r-1)
-    n_theta = plan%n_theta
-    dtheta = 2*sll_pi/n_theta
+    dr = (plan%rmax-plan%rmin)/(plan%nr-1)
+    ntheta = plan%ntheta
+    dtheta = 2*sll_pi/ntheta
 
-    SLL_ALLOCATE( f(n_theta), ierr )
-    SLL_ALLOCATE( g(n_theta), ierr )
+    SLL_ALLOCATE( f(ntheta), ierr )
+    SLL_ALLOCATE( g(ntheta), ierr )
 
     tild_rho = cmplx(rho, 0_f64, kind=f64)
     f = cmplx(plan%f, 0_f64, kind=f64)
@@ -61,9 +62,9 @@ contains
     call apply_fft_c2c_1d( plan%fft_plan, g, g )
 
     if (plan%bc=='Neumann') then
-       n = plan%n_r
+       n = plan%nr
     else ! 'Dirichlet'
-       n = plan%n_r - 2
+       n = plan%nr - 2
     endif
     
     call apply_fft_c2c_1d( plan%fft_plan, tild_rho(1,:), tild_rho(1,:) )
@@ -81,7 +82,7 @@ contains
        call apply_fft_c2c_1d( plan%fft_plan, tild_rho(i,:), tild_rho(i,:) ) 
     enddo
 
-    do j=1,n_theta
+    do j=1,ntheta
        if (plan%bc=='Neumann') then
           call neumann_matrix_resh(plan, j-1, a_resh)
        else ! 'Dirichlet'
@@ -99,7 +100,9 @@ contains
        call apply_fft_c2c_1d( plan%inv_fft_plan, tild_phi(i,:), tild_phi(i,:) ) 
     enddo
 
+    phi = real(tild_phi, f64)
+
   end subroutine solve_qn_2d_with_finite_diff_seq
 
-end module sll_qns_2d_with_finite_diff
+end module sll_qns_2d_with_finite_diff_seq
 
