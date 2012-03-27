fbf3eaadce5a184d714c33b859e499f5f99ce916
Edwin Chacon-Golcher <edwincg@gmail.com>
2012-03-27 15:29:52 +0200

    reformatted low-level IO functions to less than 80 character lines and indented. Trying to fix the build since utility functions have changed: change in mapped_meshes and ode_solvers

--- prototype/src/low_level_file_io/sll_binary_io.F90
+++ prototype/src/low_level_file_io/sll_binary_io.F90
@@ -36,70 +36,71 @@
 module sll_binary_io
 #include "sll_working_precision.h"
 #include "sll_assert.h"
-
-implicit none
-
-interface sll_binary_write_array
-module procedure sll_binary_write_array_1d
-module procedure sll_binary_write_array_2d
-module procedure sll_binary_write_array_3d
-end interface sll_binary_write_array
+  
+  implicit none
+  
+  interface sll_binary_write_array
+     module procedure sll_binary_write_array_1d
+     module procedure sll_binary_write_array_2d
+     module procedure sll_binary_write_array_3d
+  end interface
 
 
 contains
   
-!> Create binary file :
-!>    - Find a free unit file number
-!>    - Create a new file using default properties
-subroutine sll_binary_file_create(filename,file_id,error)
-character(len=*) , intent(in)  :: filename  
-sll_int32        , intent(out) :: file_id   
-sll_int32        , intent(out) :: error
-logical                        :: lopen
+  !> Create binary file :
+  !>    - Find a free unit file number
+  !>    - Create a new file using default properties
+  subroutine sll_binary_file_create(filename,file_id,error)
+    character(len=*) , intent(in)  :: filename  
+    sll_int32        , intent(out) :: file_id   
+    sll_int32        , intent(out) :: error
+    logical                        :: lopen
+    
+    error=0
 
-error=0
+    do 100 file_id=20,99
+       inquire(unit=file_id,opened=lopen)
+       if(lopen) then
+          cycle
+       else
+          open(file_id ,status='SCRATCH',err=100)
+          close(file_id,status='DELETE' ,err=100)
+          goto 200
+       end if
+       
+100    continue
+       error=1
+200    continue
+       error=0
 
-do 100 file_id=20,99
+       ! Create a new file using default properties
+       inquire(file=filename,opened=lopen)
+       SLL_ASSERT(.not. lopen)
 
-   inquire(unit=file_id,opened=lopen)
-   if(lopen) then
-      cycle
-   else
-      open(file_id ,status='SCRATCH',err=100)
-      close(file_id,status='DELETE' ,err=100)
-      goto 200
-   end if
- 
-100 continue
-error=1
-200 continue
-error=0
+       open(file_id, &
+            FILE=filename, &
+            ACCESS="STREAM", &
+            FORM='UNFORMATTED', &
+            IOSTAT=error)
 
-! Create a new file using default properties
-inquire(file=filename,opened=lopen)
-SLL_ASSERT(.not. lopen)
+     end subroutine sll_binary_file_create
 
-open(file_id,FILE=filename,ACCESS="STREAM",FORM='UNFORMATTED',IOSTAT=error)
+     !> Close the binary file :
+     subroutine sll_binary_file_close(file_id,error)
+       sll_int32, intent(in)  :: file_id
+       sll_int32, intent(out) :: error
 
-end subroutine sll_binary_file_create
-
-!> Close the binary file :
-subroutine sll_binary_file_close(file_id,error)
-sll_int32, intent(in)  :: file_id
-sll_int32, intent(out) :: error
-
-close(file_id, IOSTAT=error)
-
-end subroutine sll_binary_file_close
+       close(file_id, IOSTAT=error)
+       
+     end subroutine sll_binary_file_close
 
 #define NEW_FUNCTION(func_name, dataspace_dimension, array_name_and_dims) \
-subroutine func_name(file_id,array,error);                                \
-sll_int32 , intent(in)       :: file_id;                                  \
-sll_int32 , intent(out)      :: error;                                    \
-sll_real64, intent(in)       :: array_name_and_dims;                      \
-                                                                          \
-write(file_id,IOSTAT=error) array;                                        \
-                                                                          \
+     subroutine func_name(file_id,array,error);                           \
+       sll_int32 , intent(in)       :: file_id;                           \
+       sll_int32 , intent(out)      :: error;                             \
+       sll_real64, intent(in)       :: array_name_and_dims;               \
+       write(file_id,IOSTAT=error) array;                                 \
 end subroutine func_name
 
 !> Write a 1D array in the binary file file_id
