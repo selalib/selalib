35a153bf62cfac622b6ef7bb938dc198024f5246
Edwin Chacon-Golcher <edwincg@gmail.com>
2012-03-28 15:00:45 +0200

    miscellaneous changes to adapt to the new low-level file io

--- prototype/src/ode_solvers/sll_ode_solvers.F90
+++ prototype/src/ode_solvers/sll_ode_solvers.F90
@@ -2,7 +2,7 @@ module ode_solvers
 #include "sll_working_precision.h"
 #include "sll_memory.h"
 #include "sll_assert.h"
-!#include "sll_mesh_2d.h"
+#include "sll_scalar_field_2d.h"
   
   implicit none
   enum, bind(C)
