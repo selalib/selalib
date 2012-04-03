34931f717e2695250658850b074fbc3be9b84d3a
Edwin Chacon-Golcher <edwincg@gmail.com>
2012-04-03 21:18:06 +0200

    miscellaneous small changes in fields

--- prototype/src/distribution_function/unit_test.F90
+++ prototype/src/distribution_function/unit_test.F90
@@ -12,11 +12,12 @@ program unit_test
   sll_int32 :: nc_eta1, nc_eta2
   type(sll_mapped_mesh_2d_analytic) :: mesh2d
   type(sll_distribution_function_2D_t) :: dist_func
+  type(sll_distribution_function_2d)   :: df ! new type to test
   character(32)  :: name = 'dist_func'
+  character(32)  :: name2 = 'distribution_f'
   procedure(scalar_function_2D), pointer :: p_init_f !, px1, px2, pjac
   sll_int32  :: ierr
   sll_int32 :: ix, iv, nnode_x1, nnode_v1
-
 
   nc_eta1 = 100
   nc_eta2 = 100
@@ -42,11 +43,19 @@ program unit_test
   p_init_f => gaussian
   call sll_new_distribution_function_2D(dist_func,mesh2d,CELL_CENTERED_FIELD, &
        name, p_init_f)
- 
+
+  call initialize_distribution_function_2d( &
+       df, &
+       1.0_f64, &
+       1.0_f64, &
+       name2, &
+       mesh2d, &
+       CELL_CENTERED_FIELD, &
+       p_init_f )
   print*, 'write mesh and distribution function'
 
   call write_scalar_field_2d(dist_func,multiply_by_jacobian=.true.) 
-
+  call write_scalar_field_2d(df, multiply_by_jacobian=.true.)
 !!$  x1_min =  0.0_f64; x1_max =  2.0_f64 * sll_pi
 !!$  x2_min =  0.0_f64; x2_max =  2.0_f64 * sll_pi
 !!$
