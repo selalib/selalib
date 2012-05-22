d1282854a5016910c20a23cb6febb009a33f077f
Edwin Chacon-Golcher <edwincg@gmail.com>
2012-05-22 02:28:06 +0200

    created new initializer base class for the scalar fields and a specific landau initializer, no testing

---
+++ prototype/src/fields/landau_2d_initializer.F90
@@ -0,0 +1,56 @@
+module sll_landau_2d_initializer
+#include "working_precision.h"
+#include "sll_assert.h"
+  use numerical_constants
+  use sll_module_mapped_meshes_2d_base
+  implicit none
+
+  type, extends(scalar_field_2d_initializer_base) :: init_landau_2d
+    sll_real64 :: eps
+    sll_real64 :: kx
+  contains
+    procedure, pass(init_obj) :: initialize => initialize_landau_2d
+    procedure, pass(init_obj) :: f_of_x1x2     => f_x1x2_landau_2d
+  end type init_landau_2d
+
+contains
+
+  subroutine initialize_landau_2d( init_obj, eps_val )
+    type(init_landau_2d), intent(inout)  :: init_obj
+    sll_real64, intent(in), optional     :: eps_val
+    if( present(eps_val) ) then
+       init_obj%eps = eps_val
+    else
+       init_obj%eps = 0.01_f64 ! just some default value
+    end if
+    ! kx remains uninitialized because we need mesh information
+  end subroutine initialize_landau_2d
+
+  subroutine f_landau_2d( init_obj, mesh, data_out )
+    class(init_landau_2d)                      :: init_obj
+    class(sll_mapped_mesh_2d_base), intent(in) :: mesh
+    sll_real64, dimension(:), intent(out)      :: data_out
+    sll_int32  :: i
+    sll_int32  :: j
+    sll_int32  :: num_pts1
+    sll_int32  :: num_pts2
+    sll_real64 :: eps
+    sll_real64 :: kx
+    sll_real64 :: x
+    sll_real64 :: v
+
+    eps = init_obj%eps
+    num_pts1 = mesh%nc_eta1+1
+    num_pts2 = mesh%nc_eta2+1
+    kx = 2.0_f64*sll_pi/(mesh%x1_at_node(num_pts1,1) - mesh%x1_at_node(1,1))
+    SLL_ASSERT( size(data_out,1) .ge. num_pts1 )
+    SLL_ASSERT( size(data_out,2) .ge. num_pts2 )
+    do j=1,num_pts2
+       do i=1, num_pts1
+          v = mesh%x2_at_node(i,j)
+          x = mesh%x1_at_node(i,j)
+          data_out(i,j) = &
+               ( 1.0_f64 + eps*cos(kx*x) )/sqrt(2*sll_pi)*exp(-0.5_f64*v*v)
+  end subroutine f_landau_2d
+
+end module sll_landau_2d_initializer
---
+++ prototype/src/fields/scalar_field_initializer_base.F90
@@ -0,0 +1,30 @@
+! This module aims at providing a single interface for initializing 
+! scalar fields in a reasonably uniform way. As an example, in the 2D case, 
+! the basic interface is the subroutine f_of_x1x2, which initializes a whole
+! 2D array. The initializer object is a child of the scalar field initializer
+! class and so it can have any specific data it requires. The f_of_x1x2 
+! subroutine extracts all necessary information from a mesh object.
+
+module sll_scalar_field_initializers_base
+#include "working_precision.h"
+  use sll_module_mapped_meshes_2d_base
+  implicit none
+
+  type, abstract :: scalar_field_2d_initializer_base
+   contains
+     procedure(scalar_field_initializer), deferred, pass :: f_of_x1x2
+  end type scalar_field_2d_initializer_base
+
+contains 
+
+  abstract interface
+     subroutine scalar_field_initializer( init_obj, mesh, data_out )
+       use sll_working_precision
+       import sll_mapped_mesh_1d_base
+       class(scalar_field_2d_initializer_base), intent(inout) :: init_obj
+       class(sll_mapped_mesh_2d_base), intent(in)             :: mesh
+       sll_real64, dimension(:), intent(out)                  :: data_out
+     end subroutine scalar_field_initializer
+  end interface
+  
+end module sll_scalar_field_initializers_base
