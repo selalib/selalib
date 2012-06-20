fc0f2fd5960fb9da784f78fac7cbf4691b152fcf
Pierre Navaro <navaro@math.cnrs.fr>
2012-06-20 16:14:05 +0200

    Add a FISHPACK module

--- prototype/src/poisson_solvers/test_fishpack.F90
+++ prototype/src/poisson_solvers/test_fishpack.F90
@@ -1,4 +1,7 @@
 program test_fishpack
+
+use fishpack
+
 implicit none
 !-----------------------------------------------
 !   l o c a l   v a r i a b l e s
@@ -19,7 +22,7 @@ m = 40; n = 80
 mp1 = m + 1; np1 = n + 1
 a =  0.; b =  2.; c = -1.; d =  3.
 allocate(f(mp1,np1)); f = 0.0
-call cartesian(f,a,b,m,c,d,n)
+call test_cartesian_2d(f,a,b,m,c,d,n)
 deallocate(f)
 
 
@@ -27,12 +30,12 @@ m = 50; n = 48
 mp1 = m + 1; np1 = n + 1
 a = 0.; b = 1.; c = 0.; d = 0.5*pi
 allocate(f(mp1,np1)); f = 0.0
-call polar(f,a,b,m,c,d,n)
+call test_polar_2d(f,a,b,m,c,d,n)
 deallocate(f)
 
 contains
 
-subroutine cartesian(field, eta1_min, eta1_max, nc_eta1, &
+subroutine test_cartesian_2d(field, eta1_min, eta1_max, nc_eta1, &
                             eta2_min, eta2_max, nc_eta2)
 implicit none
 integer, intent(in)                     :: nc_eta1, nc_eta2
@@ -42,19 +45,15 @@ real(8), dimension(nc_eta2+1)           :: eta2
 real(8), intent(in)                     :: eta1_min, eta1_max
 real(8), intent(in)                     :: eta2_min, eta2_max
 
-real(8), dimension(nc_eta2+1)           :: bdb
-real(8)                                 :: bda, bdc, bdd
-integer                                 :: mbdcnd, nbdcnd
-real(8)                                 :: elmbda, pertrb
+type(poisson_fishpack) :: poisson
 
-mbdcnd = 2
-nbdcnd = 0
-elmbda = -4.
+call new_poisson_2d_fishpack(poisson, CARTESIAN_2D, &
+                             eta1_min, eta1_max, nc_eta1, &
+                             eta2_min, eta2_max, nc_eta2  )
 
-!     auxiliary quantities.
-
-
-idimf = nc_eta1+1
+poisson%mbdcnd = 2
+poisson%nbdcnd = 0
+poisson%elmbda = -4.
 
 !     generate and store grid points for the purpose of computing
 !     boundary data and the right side of the helmholtz equation.
@@ -70,7 +69,7 @@ end do
 !     generate boundary data.
 !     bda, bdc, and bdd are dummy variables.
 
-bdb(:) = 4.*cos((eta2(:)+1.)*piby2)
+poisson%bdb(:) = 4.*cos((eta2(:)+1.)*piby2)
 
 !     generate right side of equation.
 
@@ -80,9 +79,7 @@ do i = 2, nc_eta1+1
    end do
 end do
 
-call hwscrt (eta1_min, eta1_max, nc_eta1, mbdcnd, bda, bdb, &
-             eta2_min, eta2_max, nc_eta2, nbdcnd, bdc, bdd, &
-             elmbda, field, idimf, pertrb, ierror)
+call solve_poisson_fishpack(poisson, field)
 
 !     compute discretization error.  the exact solution is
 !                u(x,y) = x**2*cos((y+1)*piby2)
@@ -98,17 +95,17 @@ end do
 !     print earlier output from platforms with 32 and 64 bit floating point
 !     arithemtic followed by the output from this computer
 
-write (*, *) '    hwscrt test run *** '
+write (*, *) '    fishpack 2d cartesian test run *** '
 write (*, *) '    previous 64 bit floating point arithmetic result '
 write (*, *) '    ierror = 0,  discretization error = 5.36508-4'
 write (*, *) '    previous 32 bit floating point arithmetic result '
 write (*, *) '    ierror = 0,  discretization error = 4.9305e-4'
 write (*, *) '    the output from your computer is: '
-write (*, *) '    ierror =', ierror, ' discretization error = ', err
+write (*, *) '    ierror =', poisson%error, ' discretization error = ', err
 
-end subroutine cartesian
+end subroutine test_cartesian_2d
 
-subroutine polar(field, eta1_min, eta1_max, nc_eta1, &
+subroutine test_polar_2d(field, eta1_min, eta1_max, nc_eta1, &
                         eta2_min, eta2_max, nc_eta2)
 implicit none
 integer, intent(in)                     :: nc_eta1, nc_eta2
@@ -121,6 +118,8 @@ real(8), intent(in)                     :: eta1_min, eta1_max
 real(8), intent(in)                     :: eta2_min, eta2_max
 integer                                 :: mbdcnd, nbdcnd
 real(8)                                 :: elmbda, pertrb
+
+type(poisson_fishpack) :: poisson
 
 
 !--------------------------------------------------------------------------
@@ -147,13 +146,14 @@ real(8)                                 :: elmbda, pertrb
 !									  !
 !-------------------------------------------------------------------------!
 
+call new_poisson_2d_fishpack(poisson, POLAR_2D, &
+                             eta1_min, eta1_max, nc_eta1, &
+                             eta2_min, eta2_max, nc_eta2  )
 
 
-mbdcnd = 5
-nbdcnd = 3
-elmbda = 0.
-
-idimf = size(f,1)
+poisson%mbdcnd = 5
+poisson%nbdcnd = 3
+poisson%elmbda = 0.
 
 do i = 1, nc_eta1+1
    eta1(i) = eta1_min+(eta1_max-eta1_min)*float(i-1)/nc_eta1
@@ -163,8 +163,8 @@ do j = 1, nc_eta2+1
    eta2(j) = eta2_min+(eta2_max-eta2_min)*float(j-1)/nc_eta2
 end do
 
-bdc(:) = 0.
-bdd(:) = 0.
+poisson%bdc(:) = 0.
+poisson%bdd(:) = 0.
 
 do j = 1, nc_eta2+1
    field(nc_eta1+1,j) = 1. - cos(4.*eta2(J))
@@ -174,9 +174,7 @@ do i = 1, nc_eta1
    field(i,:nc_eta2+1) = 16.*eta1(i)**2
 end do
 
-call hwsplr (eta1_min, eta1_max, nc_eta1, mbdcnd, bda, bdb, &
-             eta2_min, eta2_max, nc_eta2, nbdcnd, bdc, bdd, &
-             elmbda, field, idimf, pertrb, ierror, w)
+call solve_poisson_fishpack (poisson, field)
 
 err = 0.
 do i = 1, nc_eta1+1
@@ -192,8 +190,8 @@ write (*, *) '    IERROR = 0,  Discretization Error = 6.19134E-4'
 write (*, *) '    Previous 32 bit floating point arithmetic result '
 write (*, *) '    IERROR = 0,  Discretization Error = 6.20723E-4'
 write (*, *) '    The output from your computer is: '
-write (*, *) '    IERROR =', IERROR, ' Discretization Error = ', ERR
+write (*, *) '    IERROR =', poisson%error, ' Discretization Error = ', ERR
 
-end subroutine polar
+end subroutine test_polar_2d
  
 end program test_fishpack
