d63daa9e5d40ff10a339171a2e57bcfa8427453a
Samuel De Santis <desantis@math.unistra.fr>
2012-06-23 20:19:03 +0200

    Fix bug for intel and xlf compiler, add new cache entries for CMake

--- prototype/src/fields/sll_scalar_field_1d.F90
+++ prototype/src/fields/sll_scalar_field_1d.F90
@@ -38,9 +38,12 @@ module sll_scalar_field_1d
   use sll_misc_utils
   implicit none
 
+#ifndef CENTERED_FIELD
+#define CENTERED_FIELD
   enum, bind(C)
      enumerator :: NODE_CENTERED_FIELD = 0, CELL_CENTERED_FIELD = 1
   end enum
+#endif
 
   type scalar_field_1d
      class(sll_mapped_mesh_1d_base), pointer :: mesh
