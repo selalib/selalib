2c07932cc84db9bae0df4f9f71714fb1a00c558c
Pierre Navaro <navaro@math.unistra.fr>
2012-06-26 11:05:40 +0200

    Remove poisson solver dependencies to field and mesh types 1d

--- prototype/src/poisson_solvers/fishpack.F90
+++ prototype/src/poisson_solvers/fishpack.F90
@@ -16,8 +16,8 @@ type, public :: fishpack_2d
    sll_real64, allocatable, dimension(:)   :: bdb
    sll_real64, allocatable, dimension(:)   :: bdc
    sll_real64, allocatable, dimension(:)   :: bdd
-   sll_int32                               :: mbdcnd
-   sll_int32                               :: nbdcnd
+   sll_int32                               :: bc_eta1
+   sll_int32                               :: bc_eta2
    sll_real64                              :: elmbda
    sll_real64                              :: pertrb
    sll_int32                               :: error
@@ -40,9 +40,9 @@ type, public :: fishpack_3d
    sll_real64, allocatable, dimension(:,:) :: bdd
    sll_real64, allocatable, dimension(:,:) :: bde
    sll_real64, allocatable, dimension(:,:) :: bdf
-   sll_int32                               :: lbdcnd
-   sll_int32                               :: mbdcnd
-   sll_int32                               :: nbdcnd
+   sll_int32                               :: bc_eta1
+   sll_int32                               :: bc_eta2
+   sll_int32                               :: bc_eta3
    sll_real64                              :: elmbda
    sll_real64                              :: pertrb
    sll_int32                               :: error
@@ -54,13 +54,15 @@ end type fishpack_3d
 
 enum, bind(C)
    enumerator :: CARTESIAN_2D = 0, POLAR_2D = 1, CARTESIAN_3D = 2
+   enumerator :: PERIODIC=0, DIRICHLET=1
+   enumerator :: NEUMANN_RIGHT=2, NEUMANN_LEFT=3, NEUMANN=4
 end enum
 
 contains
 
   subroutine new_2d(this, geometry,            &
-                    eta1_min,eta1_max,nc_eta1, &
-                    eta2_min,eta2_max,nc_eta2)
+                    eta1_min,eta1_max,nc_eta1,bc_eta1, &
+                    eta2_min,eta2_max,nc_eta2,bc_eta2)
 
     class(fishpack_2d),intent(out)     :: this
     sll_int32, intent(in)              :: nc_eta1
@@ -68,7 +70,7 @@ contains
     sll_real64, intent(in)             :: eta1_min, eta1_max
     sll_real64, intent(in)             :: eta2_min, eta2_max
     sll_int32                          :: geometry
-    !sll_int32                          :: mbdcnd, nbdcnd
+    sll_int32                          :: bc_eta1, bc_eta2
     !sll_real64                         :: elmbda
 
     this%geometry = geometry
@@ -83,22 +85,29 @@ contains
     this%eta1_max = eta1_max
     this%eta2_max = eta2_max
 
-    SLL_ALLOCATE(this%bda(nc_eta2+1),this%error)
-    SLL_ALLOCATE(this%bdb(nc_eta2+1),this%error)
-    SLL_ALLOCATE(this%bdc(nc_eta1+1),this%error)
-    SLL_ALLOCATE(this%bdd(nc_eta1+1),this%error)
+    this%bc_eta1 = bc_eta1
+    this%bc_eta2 = bc_eta2
+
+    if (bc_eta1 /= PERIODIC) then
+       SLL_ALLOCATE(this%bda(nc_eta2+1),this%error)
+       SLL_ALLOCATE(this%bdb(nc_eta2+1),this%error)
+    end if
+    if (bc_eta2 /= PERIODIC) then
+       SLL_ALLOCATE(this%bdc(nc_eta1+1),this%error)
+       SLL_ALLOCATE(this%bdd(nc_eta1+1),this%error)
+    end if
 
   end subroutine new_2d
 
   subroutine new_3d(this, geometry,            &
-                    eta1_min,eta1_max,nc_eta1, &
-                    eta2_min,eta2_max,nc_eta2, &
-                    eta3_min,eta3_max,nc_eta3  )
+                    eta1_min,eta1_max,nc_eta1,bc_eta1, &
+                    eta2_min,eta2_max,nc_eta2,bc_eta2, &
+                    eta3_min,eta3_max,nc_eta3,bc_eta3  )
 
     class(fishpack_3d),intent(out)     :: this
-    sll_int32, intent(in)              :: nc_eta1
-    sll_int32, intent(in)              :: nc_eta2
-    sll_int32, intent(in)              :: nc_eta3
+    sll_int32, intent(in)              :: nc_eta1, bc_eta1
+    sll_int32, intent(in)              :: nc_eta2, bc_eta2
+    sll_int32, intent(in)              :: nc_eta3, bc_eta3
     sll_real64, intent(in)             :: eta1_min, eta1_max
     sll_real64, intent(in)             :: eta2_min, eta2_max
     sll_real64, intent(in)             :: eta3_min, eta3_max
@@ -119,12 +128,22 @@ contains
     this%eta2_max = eta2_max
     this%eta3_max = eta3_max
 
-    SLL_ALLOCATE(this%bda(nc_eta2+1,nc_eta3+1),this%error)
-    SLL_ALLOCATE(this%bdb(nc_eta2+1,nc_eta3+1),this%error)
-    SLL_ALLOCATE(this%bdc(nc_eta1+1,nc_eta3+1),this%error)
-    SLL_ALLOCATE(this%bdd(nc_eta1+1,nc_eta3+1),this%error)
-    SLL_ALLOCATE(this%bde(nc_eta1+1,nc_eta2+1),this%error)
-    SLL_ALLOCATE(this%bdf(nc_eta1+1,nc_eta2+1),this%error)
+    this%bc_eta1 = bc_eta1
+    this%bc_eta2 = bc_eta2
+    this%bc_eta3 = bc_eta3
+
+    if (bc_eta1 /= PERIODIC) then
+       SLL_ALLOCATE(this%bda(nc_eta2+1,nc_eta3+1),this%error)
+       SLL_ALLOCATE(this%bdb(nc_eta2+1,nc_eta3+1),this%error)
+    end if
+    if (bc_eta2 /= PERIODIC) then
+       SLL_ALLOCATE(this%bdc(nc_eta1+1,nc_eta3+1),this%error)
+       SLL_ALLOCATE(this%bdd(nc_eta1+1,nc_eta3+1),this%error)
+    end if
+    if (bc_eta3 /= PERIODIC) then
+       SLL_ALLOCATE(this%bde(nc_eta1+1,nc_eta2+1),this%error)
+       SLL_ALLOCATE(this%bdf(nc_eta1+1,nc_eta2+1),this%error)
+    end if
 
   end subroutine new_3d
 
@@ -149,18 +168,18 @@ contains
      if ( this%geometry == CARTESIAN_2D ) then
   
         call hwscrt (this%eta1_min, this%eta1_max, this%nc_eta1, &
-                     this%mbdcnd, this%bda, this%bdb, &
+                     this%bc_eta1, this%bda, this%bdb, &
                      this%eta2_min, this%eta2_max, this%nc_eta2, &
-                     this%nbdcnd, this%bdc, this%bdd, &
+                     this%bc_eta2, this%bdc, this%bdd, &
                      this%elmbda, field, idimf, this%pertrb, this%error)
 
      else if ( this%geometry == POLAR_2D ) then
 
 
         call hwsplr (this%eta1_min, this%eta1_max, this%nc_eta1, &
-                     this%mbdcnd, this%bda, this%bdb, &
+                     this%bc_eta1, this%bda, this%bdb, &
                      this%eta2_min, this%eta2_max, this%nc_eta2, &
-                     this%nbdcnd, this%bdc, this%bdd, &
+                     this%bc_eta2, this%bdc, this%bdd, &
                      this%elmbda, field, idimf, this%pertrb, this%error, w)
 
      else
@@ -189,11 +208,11 @@ contains
      if ( this%geometry == CARTESIAN_3D) then
 
         call hw3crt (this%eta1_min,this%eta1_max,this%nc_eta1, &
-                     this%lbdcnd,this%bda,this%bdb, &
+                     this%bc_eta1,this%bda,this%bdb, &
                      this%eta2_min,this%eta2_max,this%nc_eta2, &
-                     this%mbdcnd,this%bdc,this%bdd, &
+                     this%bc_eta2,this%bdc,this%bdd, &
                      this%eta3_min,this%eta3_max,this%nc_eta3, &
-                     this%nbdcnd,this%bde,this%bdf,this%elmbda, &
+                     this%bc_eta3,this%bde,this%bdf,this%elmbda, &
                      ldimf,mdimf,field, this%pertrb,this%error,w)
 
      else
