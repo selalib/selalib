8e7a10299b25e6d1f63aa0c39a1994b5592bece5
Eric Sonnendrucker <sonnen@math.unistra.fr>
2012-07-11 15:29:00 +0200

    modified initializer class

--- prototype/src/fields/landau_2d_initializer.F90
+++ prototype/src/fields/landau_2d_initializer.F90
@@ -8,6 +8,7 @@ module sll_landau_2d_initializer
 
   type, extends(scalar_field_2d_initializer_base) :: init_landau_2d
     sll_real64 :: eps
+    class(sll_mapped_mesh_2d_base), pointer :: mesh
     sll_real64 :: kx
   contains
     procedure, pass(init_obj) :: initialize => initialize_landau_2d
@@ -16,21 +17,24 @@ module sll_landau_2d_initializer
 
 contains
 
-  subroutine initialize_landau_2d( init_obj, eps_val )
+  subroutine initialize_landau_2d( init_obj, mesh, eps_val )
     class(init_landau_2d), intent(inout)  :: init_obj
+    class(sll_mapped_mesh_2d_base), intent(in), target :: mesh
     sll_real64, intent(in), optional     :: eps_val
     if( present(eps_val) ) then
        init_obj%eps = eps_val
     else
        init_obj%eps = 0.01_f64 ! just some default value
     end if
+    init_obj%mesh => mesh
     ! kx remains uninitialized because we need mesh information
   end subroutine initialize_landau_2d
 
-  subroutine f_x1x2_landau_2d( init_obj, mesh, data_out )
+  subroutine f_x1x2_landau_2d( init_obj, data_out )
     class(init_landau_2d), intent(inout)       :: init_obj
-    class(sll_mapped_mesh_2d_base), intent(in) :: mesh
     sll_real64, dimension(:,:), intent(out)    :: data_out
+
+    class(sll_mapped_mesh_2d_base), pointer    :: mesh
     sll_int32  :: i
     sll_int32  :: j
     sll_int32  :: num_pts1
@@ -40,7 +44,10 @@ contains
     sll_real64 :: x
     sll_real64 :: v
 
+    
+
     eps = init_obj%eps
+    mesh => init_obj%mesh
     num_pts1 = mesh%nc_eta1+1
     num_pts2 = mesh%nc_eta2+1
     kx = 2.0_f64*sll_pi/(mesh%x1_at_node(num_pts1,1) - mesh%x1_at_node(1,1))
