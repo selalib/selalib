9a8a13025fe9a5a58dadbfdc75b06adecd703cc9
Eric Sonnendrücker <sonnendr@gmail.com>
2012-07-11 15:10:55 +0200

    fixed bug in mapped mesh

--- prototype/src/fields/scalar_field_initializer_base.F90
+++ prototype/src/fields/scalar_field_initializer_base.F90
@@ -20,7 +20,7 @@ module sll_scalar_field_initializers_base
        use sll_working_precision
        import sll_mapped_mesh_2d_base, scalar_field_2d_initializer_base
        class(scalar_field_2d_initializer_base), intent(inout) :: init_obj
-       class(sll_mapped_mesh_2d_base), intent(in)             :: mesh
+       !class(sll_mapped_mesh_2d_base), intent(in)             :: mesh
        sll_real64, dimension(:,:), intent(out)                :: data_out
      end subroutine scalar_field_initializer
   end interface
