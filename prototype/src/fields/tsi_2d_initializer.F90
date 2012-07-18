f73120eb66069c79a3ffb60d24df87327183d6ee
Eric Sonnendrücker <sonnendr@gmail.com>
2012-07-18 07:49:38 +0200

    added new Vlasov Poisson simulation on uniform cartesian grid

--- prototype/src/fields/tsi_2d_initializer.F90
+++ prototype/src/fields/tsi_2d_initializer.F90
@@ -1,4 +1,4 @@
-module sll_landau_2d_initializer
+module sll_tsi_2d_initializer
 #include "sll_working_precision.h"
 #include "sll_assert.h"
   use numeric_constants
@@ -6,22 +6,25 @@ module sll_landau_2d_initializer
   use sll_scalar_field_initializers_base
   implicit none
 
-  type, extends(scalar_field_2d_initializer_base) :: init_landau_2d
+  type, extends(scalar_field_2d_initializer_base) :: init_tsi_2d
     sll_real64 :: eps
+    sll_real64 :: xi
+    sll_real64 :: v0
     class(sll_mapped_mesh_2d_base), pointer :: mesh
-    sll_real64 :: kx
   contains
-    procedure, pass(init_obj) :: initialize => initialize_landau_2d
-    procedure, pass(init_obj) :: f_of_x1x2  => f_x1x2_landau_2d
-  end type init_landau_2d
+    procedure, pass(init_obj) :: initialize => initialize_tsi_2d
+    procedure, pass(init_obj) :: f_of_x1x2  => f_x1x2_tsi_2d
+  end type init_tsi_2d
 
 contains
 
-  subroutine initialize_landau_2d( init_obj, mesh, data_position, eps_val )
-    class(init_landau_2d), intent(inout)  :: init_obj
+  subroutine initialize_tsi_2d( init_obj, mesh, data_position, eps_val, xi_val, v0_val )
+    class(init_tsi_2d), intent(inout)  :: init_obj
     class(sll_mapped_mesh_2d_base), intent(in), target :: mesh
     sll_int32 :: data_position
     sll_real64, intent(in), optional     :: eps_val
+    sll_real64, intent(in), optional     :: xi_val
+    sll_real64, intent(in), optional     :: v0_val
 
     init_obj%data_position = data_position
     if( present(eps_val) ) then
@@ -29,12 +32,21 @@ contains
     else
        init_obj%eps = 0.01_f64 ! just some default value
     end if
+    if( present(xi_val) ) then
+       init_obj%xi = xi_val
+    else
+       init_obj%xi = 0.90_f64 ! just some default value
+    end if
+    if( present(v0_val) ) then
+       init_obj%v0 = v0_val
+    else
+       init_obj%xi = 2.4_f64 ! just some default value
+    end if
     init_obj%mesh => mesh
-    ! kx remains uninitialized because we need mesh information
-  end subroutine initialize_landau_2d
+  end subroutine initialize_tsi_2d
 
-  subroutine f_x1x2_landau_2d( init_obj, data_out )
-    class(init_landau_2d), intent(inout)       :: init_obj
+  subroutine f_x1x2_tsi_2d( init_obj, data_out )
+    class(init_tsi_2d), intent(inout)       :: init_obj
     sll_real64, dimension(:,:), intent(out)    :: data_out
 
     class(sll_mapped_mesh_2d_base), pointer    :: mesh
@@ -43,13 +55,16 @@ contains
     sll_int32  :: num_pts1
     sll_int32  :: num_pts2
     sll_real64 :: eps
+    sll_real64 :: xi
+    sll_real64 :: v0
     sll_real64 :: kx
     sll_real64 :: x
     sll_real64 :: v
-
-    
+    sll_real64 :: v2
 
     eps = init_obj%eps
+    xi = init_obj%xi
+    v0 = init_obj%v0
     mesh => init_obj%mesh
     if (init_obj%data_position ==  NODE_CENTERED_FIELD) then
        num_pts1 = mesh%nc_eta1+1
@@ -70,12 +85,15 @@ contains
              v = mesh%x2_at_cell(i,j)
              x = mesh%x1_at_cell(i,j)
           else
-             print*, 'f_x1x2_landau_2d:',  init_obj%data_position, 'not defined'
+             print*, 'f_x1x2_tsi_2d:',  init_obj%data_position, 'not defined'
           end if
+          v2 = v*v
           data_out(i,j) = &
-               ( 1.0_f64 + eps*cos(kx*x) )/sqrt(2*sll_pi)*exp(-0.5_f64*v*v)
+               (1+eps*((cos(2*kx*x)+cos(3*kx*x))/1.2_f64+cos(kx*x)))* &
+               (1/sqrt(2*sll_pi))*((2-2*xi)/(3-2*xi))*(1+.5_f64*v2/(1-xi))*exp(-.5_f64*v2)
+
        end do
     end do
-  end subroutine f_x1x2_landau_2d
+  end subroutine f_x1x2_tsi_2d
 
-end module sll_landau_2d_initializer
+end module sll_tsi_2d_initializer
