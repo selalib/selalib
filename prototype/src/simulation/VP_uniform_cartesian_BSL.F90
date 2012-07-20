769ebf3487beb3f3629fe955f96e975db6de398a
Eric Sonnendrücker <sonnendr@gmail.com>
2012-07-20 16:58:34 +0200

    corrected bug in VP_uniform_cartesian_BSL.F90

--- prototype/src/simulation/VP_uniform_cartesian_BSL.F90
+++ prototype/src/simulation/VP_uniform_cartesian_BSL.F90
@@ -27,33 +27,87 @@ program VP_1d
   sll_real64, dimension(:), pointer :: f1d
   sll_real64, dimension(:), allocatable :: v_array
   sll_int32  :: Ncx, Ncv   ! number of cells
-  sll_int32, parameter  :: th_diag = 34, ex_diag = 35
+  sll_int32, parameter  :: input_file = 33, th_diag = 34, ex_diag = 35
   sll_real64 :: kmode
-  sll_real64 :: xmin, xmax, vmin, vmax, delta_v
+  sll_real64 :: xmin, xmax, vmin, vmax
+  sll_real64 :: delta_x, delta_v
   sll_real64 :: alpha
   sll_real64 :: dt 
   sll_int32  :: nbiter
-  sll_real64 :: mass, momentum, kinetic_energy, potential_energy
-  character(len=32) :: fname, name
+  sll_real64 :: time, mass, momentum, kinetic_energy, potential_energy
+  character(len=32) :: fname, case
   sll_int32  :: istep
-  character(len=4) :: cstep
+  sll_int32  :: nbox
   sll_real64 :: eps
-  sll_int32 :: i, j
-  sll_int32 :: ierr   ! error flag 
+  sll_real64 :: v0
+  sll_int32  :: i, j
+  sll_int32  :: ierr   ! error flag 
+
+  ! namelists for data input
+  namelist / geom / xmin, Ncx, nbox, vmin, vmax, Ncv
+  namelist / time_iterations / dt, nbiter
+  namelist / landau / kmode, eps 
+  namelist / tsi / kmode, eps, v0 
+
+  ! determine what case is being run
+  call GET_COMMAND_ARGUMENT(1,case)
+  ! open and read input file
+  if (case == "landau") then
+     open(unit = input_file, file = 'landau_input.txt')
+     read(input_file, geom) 
+     read(input_file, time_iterations)
+     read(input_file,landau)
+     close(input_file)
+  else if (case == "tsi") then
+     open(unit = input_file, file = 'tsi_input.txt')
+     read(input_file, geom) 
+     read(input_file, time_iterations)
+     read(input_file,tsi)
+     close(input_file)
+  else
+     print*, 'test case ', case, ' not defined'
+     print*, '   usage: VP1D_cart test_case'
+     print*, '     where test_case is on of:'
+     print*, '        landau'
+     print*, '        tsi'
+     stop
+  endif
 
   ! define uniform cartesian mesh in x and v
-  kmode = 0.2_f64    ! mode for Landau Damping
-  Ncx = 128 
-  Ncv = 128
-  xmin = 0.0_f64
-  xmax = 2.0_f64 * sll_pi / kmode
-  vmin = -8.0_f64
-  vmax = 8.0_f64
+  xmax = nbox * 2 * sll_pi / kmode
+  delta_x = (xmax - xmin) / Ncx
   delta_v = (vmax - vmin) / Ncv
   SLL_ALLOCATE(v_array(Ncv+1),ierr)
   do j = 1, Ncv + 1
      v_array(j) = vmin + (j-1)*delta_v
   end do
+
+  ! print out run parameters
+  if (case == "landau") then
+     print*, '     --------------'
+     print*, '     | Landau run |'
+     print*, '     --------------'
+     print*, '   k=', kmode
+     print*, '   perturbation=', eps
+  else if (case == "tsi") then
+     print*, '     -----------'
+     print*, '     | TSI run |'
+     print*, '     -----------'
+     print*, '   k=', kmode
+     print*, '   perturbation=', eps
+     print*, '   v0=', v0
+  end if
+  print*, 'geometry of computational domain:'
+  print*, '   xmin=', xmin
+  print*, '   xmax=', xmax
+  print*, '   Ncx=', Ncx
+  print*, '   vmin=', vmin
+  print*, '   vmax=', vmax
+  print*, '   Ncv=', Ncv
+  print*, 'time iterations:'
+  print*, '   dt=',dt
+  print*, '   number of iterations=', nbiter
+  print*, ' '
   
   call initialize_mesh_2d_cartesian( &
     mesh2d,           &
@@ -78,13 +132,16 @@ program VP_1d
   SLL_ALLOCATE(efield(Ncx+1),ierr)
 
   ! initialization of distribution_function
-  eps = 0.05_f64  ! perturbation for Landau
   call init_landau%initialize(mesh2d_base, NODE_CENTERED_FIELD, eps, kmode)
   call init_tsi%initialize(mesh2d_base, NODE_CENTERED_FIELD, eps, kmode)
-  !p_init_f => init_landau
-  p_init_f => init_tsi
-
-  fname = "landau"
+  if (case == "landau") then
+     p_init_f => init_landau
+     fname = "landau"
+  else if (case == "tsi") then
+     p_init_f => init_tsi
+     fname = "tsi"
+  end if
+  
   call initialize_distribution_function_2d( &
        f, &
        1.0_f64, &
@@ -94,9 +151,6 @@ program VP_1d
        NODE_CENTERED_FIELD, &
        p_init_f )
   ! write mesh and initial distribution function
-  istep = 0
-  call int2string(istep,cstep)
-  f%name = trim(fname)//cstep
   call write_scalar_field_2d(f) 
 
   ! initialise Poisson
@@ -109,8 +163,6 @@ program VP_1d
   
   ! time loop
   !----------
-  nbiter = 20
-  dt = .1_f64
   ! half time step advection in v
   do istep = 1, nbiter
      do i = 1, Ncx+1
@@ -125,7 +177,7 @@ program VP_1d
         f1d = interp_x%interpolate_array_disp(Ncx+1, f1d, alpha)
      end do
      ! compute rho and electric field
-     rho = 1.0_f64 - sum(FIELD_DATA(f), DIM = 2)
+     rho = 1.0_f64 - delta_v * sum(FIELD_DATA(f), DIM = 2)
      call solve(poisson_1d, efield, rho)
      ! half time step advection in v
      do i = 1, Ncx+1
@@ -134,15 +186,15 @@ program VP_1d
         f1d = interp_v%interpolate_array_disp(Ncv+1, f1d, alpha)
      end do
      ! diagnostics
-     mass = sum(FIELD_DATA(f))
-     momentum = sum(matmul(FIELD_DATA(f),v_array))
-     kinetic_energy = 0.5_f64 * sum(matmul(FIELD_DATA(f),v_array**2))
-     potential_energy = 0.5_f64 * sum(efield**2)
-     write(th_diag,*) mass, momentum, kinetic_energy, potential_energy
+     time = istep*dt
+     mass = delta_x * delta_v * sum(FIELD_DATA(f)(1:Ncx,:))
+     momentum = delta_x * delta_v * sum(matmul(FIELD_DATA(f)(1:Ncx,:),v_array))
+     kinetic_energy = delta_x * delta_v * 0.5_f64 * &
+          sum(matmul(FIELD_DATA(f)(1:Ncx,:),v_array**2))
+     potential_energy = delta_x * 0.5_f64 * sum(efield**2)
+     write(th_diag,*) time, mass, momentum, kinetic_energy, potential_energy
      write(ex_diag,*) efield
      if (mod(istep,10)==0) then
-        call int2string(istep,cstep)
-        f%name = trim(fname)//cstep
         call write_scalar_field_2d(f) 
      end if
   end do
