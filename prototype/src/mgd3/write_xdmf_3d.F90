bae33f4058d3861572a85c118ec519f6785b95ea
Pierre Navaro <navaro@math.cnrs.fr>
2012-07-22 22:20:07 +0200

    Xdmf outputs for multiblock mesh : done!

--- prototype/src/mgd3/write_xdmf_3d.F90
+++ prototype/src/mgd3/write_xdmf_3d.F90
@@ -1,11 +1,12 @@
-subroutine write_xdmf_3d(my_id,f,sx,ex,sy,ey,sz,ez,hx,hy,hz,error)
+subroutine write_xdmf_3d(my_id,nproc,f,sx,ex,sy,ey,sz,ez,hx,hy,hz,error)
 
 use hdf5 
 use sll_misc_utils
+use mgd3
 
 implicit none
 
-integer, intent(in) :: my_id
+integer, intent(in) :: my_id, nproc
 integer, intent(in) :: sx,ex,sy,ey,sz,ez
 real(8) :: f(sx-1:ex+1,sy-1:ey+1,sz-1:ez+1)
 real    :: x(sx-1:ex+1,sy-1:ey+1,sz-1:ez+1)
@@ -15,17 +16,20 @@ real(8) :: hx, hy, hz
 integer :: nx, ny, nz
 character(len=2), dimension(3) :: coordNames
 integer, parameter :: xmf = 77
+character(len=72) :: field_label, mesh_label
 character(len=72) :: field_name, mesh_name
-character(len=4)  :: my_proc
+character(len=4)  :: my_proc, cproc
 
-integer          :: error, i, j, k
+integer          :: error, i, j, k, iproc
 integer(hid_t)   :: file_id, dataset_id, dataspace_id
 integer(hsize_t) :: data_dims(3)
 logical, save    :: mesh_writed = .false.
+integer, save    :: iplot = 0
+character(len=4) :: cplot
 
 call int2string(my_id,my_proc)
-field_name = "xdmf3d"
-mesh_name = "mesh3d"
+field_label = "xdmf3d"
+mesh_label = "mesh3d"
 
 !Write the data file.
 coordNames(1) = "/X"
@@ -38,9 +42,43 @@ ny = size(f,2)
 nz = size(f,3)
 data_dims = (/nx,ny,nz/)
 
+
+iplot = iplot+1
+call int2string(iplot,cplot)
+mesh_name  = trim(mesh_label)//my_proc//".h5"
+field_name = trim(field_label)//my_proc//"-"//cplot//".h5"
+
+!Open the file and write the XML description of the mesh..
+open(xmf,file=trim(field_label)//cplot//my_proc//".xmf")
+write(xmf,'(a)')"<?xml version=""1.0"" ?>"
+write(xmf,'(a)')"<!DOCTYPE Xdmf SYSTEM ""Xdmf.dtd"" []>"
+write(xmf,'(a)')"<Xdmf Version=""2.0"">"
+write(xmf,'(a)')"<Domain>"
+write(xmf,'(a)')"<Grid Name=""mesh3D"" GridType=""Uniform"">"
+write(xmf,'(a,3i5,a)')"<Topology TopologyType=""3DSMesh"" NumberOfElements='",nz,ny,nx,"'/>"
+write(xmf,'(a)')"<Geometry GeometryType=""X_Y_Z"">"
+write(xmf,'(a,3i5,a)')"<DataItem Dimensions='",nz,ny,nx,"' NumberType=""Float"" Precision=""4"" Format=""HDF"">"
+write(xmf,'(a)')trim(mesh_name)//":"//coordnames(1)
+write(xmf,'(a)')"</DataItem>"
+write(xmf,'(a,3i5,a)')"<DataItem Dimensions='",nz,ny,nx,"' NumberType=""Float"" Precision=""4"" Format=""HDF"">"
+write(xmf,'(a)')trim(mesh_name)//":"//coordnames(2)
+write(xmf,'(a)')"</DataItem>"
+write(xmf,'(a,3i5,a)')"<DataItem Dimensions='",nz,ny,nx,"' NumberType=""Float"" Precision=""4"" Format=""HDF"">"
+write(xmf,'(a)')trim(mesh_name)//":"//coordnames(3)
+write(xmf,'(a)')"</DataItem>"
+write(xmf,'(a)')"</Geometry>"
+write(xmf,'(a)')"<Attribute Name=""U"" AttributeType=""Scalar"" Center=""Node"">"
+write(xmf,'(a,3i5,a)')"<DataItem Dimensions='",nz,ny,nx,"' NumberType=""Float"" Precision=""8"" Format=""HDF"">"
+write(xmf,'(a)')trim(field_name)//":/F"
+write(xmf,'(a)')"</DataItem>"
+write(xmf,'(a)')"</Attribute>"
+write(xmf,'(a)')"</Grid>"
+write(xmf,'(a)')"</Domain>"
+write(xmf,'(a)')"</Xdmf>"
+close(xmf)
+
 !Initialize FORTRAN interface.
 call H5open_f (error)
-
 if (.not. mesh_writed) then
 
    print*,'Writing mesh data...'
@@ -55,8 +93,7 @@ if (.not. mesh_writed) then
    end do
 
    !Create a new file using default properties.
-   call H5Fcreate_f(trim(mesh_name)//my_proc//".h5", H5F_ACC_TRUNC_F, file_id, error);
-   
+   call H5Fcreate_f(trim(mesh_name), H5F_ACC_TRUNC_F, file_id, error);
    
    call H5Screate_simple_f(3, data_dims, dataspace_id, error)
    call H5Dcreate_f(file_id, coordnames(1), H5T_NATIVE_REAL, &
@@ -87,7 +124,7 @@ if (.not. mesh_writed) then
 end if
 
 !Create a new file using default properties.
-call H5Fcreate_f(trim(field_name)//my_proc//".h5", H5F_ACC_TRUNC_F, file_id, error);
+call H5Fcreate_f(trim(field_name), H5F_ACC_TRUNC_F, file_id, error);
  
 !Write the scalar data.
 call H5Screate_simple_f(3, data_dims, dataspace_id, error);
@@ -102,55 +139,44 @@ call H5fclose_f(file_id, error)
 !Close FORTRAN interface.
 call H5close_f(error) 
 
+if (my_id == 0) then
+   open(xmf,file="domain"//cplot//".xmf")
+   write(xmf,'(a)')"<?xml version=""1.0"" ?>"
+   write(xmf,'(a)')"<!DOCTYPE Xdmf SYSTEM ""Xdmf.dtd"" []>"
+   write(xmf,'(a)')"<Xdmf Version=""2.0"">"
+   write(xmf,'(a)')"<Domain>"
+   write(xmf,'(a)')"<Grid Name=""Domain"" GridType=""Collection"" CollectionType=""Spatial"">"
+   do iproc = 0, nproc-1
+      call int2string(iproc,cproc)
+      write(xmf,'(a)')"<Grid Name=""SubDomain"" GridType=""Uniform"">"
+      write(xmf,'(a,3i5,a)')"<Topology TopologyType=""3DSMesh"" NumberOfElements='",nz,ny,nx,"'/>"
+      write(xmf,'(a)')"<Geometry GeometryType=""X_Y_Z"">"
+      write(xmf,'(a,3i5)')"<DataItem Dimensions='",nz,ny,nx
+      write(xmf,'(a)')"' NumberType=""Float"" Precision=""4"" Format=""HDF"">"
+      write(xmf,'(a)')trim(mesh_label)//cproc//".h5:"//coordnames(1)
+      write(xmf,'(a)')"</DataItem>"
+      write(xmf,'(a,3i5)')"<DataItem Dimensions='",nz,ny,nx
+      write(xmf,'(a)')"' NumberType=""Float"" Precision=""4"" Format=""HDF"">"
+      write(xmf,'(a)')trim(mesh_label)//cproc//".h5:"//coordnames(2)
+      write(xmf,'(a)')"</DataItem>"
+      write(xmf,'(a,3i5)')"<DataItem Dimensions='",nz,ny,nx
+      write(xmf,'(a)')"' NumberType=""Float"" Precision=""4"" Format=""HDF"">"
+      write(xmf,'(a)')trim(mesh_label)//cproc//".h5:"//coordnames(3)
+      write(xmf,'(a)')"</DataItem>"
+      write(xmf,'(a)')"</Geometry>"
+      write(xmf,'(a)')"<Attribute Name=""U"" AttributeType=""Scalar"" Center=""Node"">"
+      write(xmf,'(a,3i5)')"<DataItem Dimensions='",nz,ny,nx
+      write(xmf,'(a)')"' NumberType=""Float"" Precision=""8"" Format=""HDF"">"
+      write(xmf,'(a)')trim(field_label)//cproc//"-"//cplot//".h5:/F"
+      write(xmf,'(a)')"</DataItem>"
+      write(xmf,'(a)')"</Attribute>"
+      write(xmf,'(a)')"</Grid>"
+   end do
+   write(xmf,'(a)')"</Grid>"
+   write(xmf,'(a)')"</Domain>"
+   write(xmf,'(a)')"</Xdmf>"
+   close(xmf)
+end if
 
-
-!<Grid Name="Car Wheel" GridType="Tree">
-!<Grid Name="Tire" GridType="Uniform">
-!<Topology ...
-!<Geometry ...
-!</Grid>
-!<Grid Name="Lug Nuts" GridType="Collection">
-!<Grid Name="Lug Nut 0" GridType="Uniform"
-!<Topology ....
-!<Geometry ...
-!</Grid>
-!<Grid Name="Lug Nut 1" GridType="Uniform"
-!<Topology ...
-!<Geometry ...
-!</Grid>
-!<Grid Name="Lug Nut 2" GridType="Uniform"
-!<Topology ...
-!<Geometry ...
-!</Grid>
-!</Grid>
-
-!Open the file and write the XML description of the mesh..
-open(xmf,file=trim(field_name)//my_proc//".xmf")
-write(xmf,'(a)')"<?xml version=""1.0"" ?>"
-write(xmf,'(a)')"<!DOCTYPE Xdmf SYSTEM ""Xdmf.dtd"" []>"
-write(xmf,'(a)')"<Xdmf Version=""2.0"">"
-write(xmf,'(a)')"<Domain>"
-write(xmf,'(a)')"<Grid Name=""mesh3D"" GridType=""Uniform"">"
-write(xmf,'(a,3i5,a)')"<Topology TopologyType=""3DSMesh"" NumberOfElements='",nz,ny,nx,"'/>"
-write(xmf,'(a)')"<Geometry GeometryType=""X_Y_Z"">"
-write(xmf,'(a,3i5,a)')"<DataItem Dimensions='",nz,ny,nx,"' NumberType=""Float"" Precision=""4"" Format=""HDF"">"
-write(xmf,'(a)')trim(mesh_name)//my_proc//".h5:"//coordnames(1)
-write(xmf,'(a)')"</DataItem>"
-write(xmf,'(a,3i5,a)')"<DataItem Dimensions='",nz,ny,nx,"' NumberType=""Float"" Precision=""4"" Format=""HDF"">"
-write(xmf,'(a)')trim(mesh_name)//my_proc//".h5:"//coordnames(2)
-write(xmf,'(a)')"</DataItem>"
-write(xmf,'(a,3i5,a)')"<DataItem Dimensions='",nz,ny,nx,"' NumberType=""Float"" Precision=""4"" Format=""HDF"">"
-write(xmf,'(a)')trim(mesh_name)//my_proc//".h5:"//coordnames(3)
-write(xmf,'(a)')"</DataItem>"
-write(xmf,'(a)')"</Geometry>"
-write(xmf,'(a)')"<Attribute Name=""U"" AttributeType=""Scalar"" Center=""Node"">"
-write(xmf,'(a,3i5,a)')"<DataItem Dimensions='",nz,ny,nx,"' NumberType=""Float"" Precision=""8"" Format=""HDF"">"
-write(xmf,'(a)')trim(field_name)//my_proc//".h5:/F"
-write(xmf,'(a)')"</DataItem>"
-write(xmf,'(a)')"</Attribute>"
-write(xmf,'(a)')"</Grid>"
-write(xmf,'(a)')"</Domain>"
-write(xmf,'(a)')"</Xdmf>"
-close(xmf)
 
 end subroutine write_xdmf_3d 
