474df56abc283569066a04604c72f1abaa2052e5
Aliou Diouf <aliou.l.diouf@inria.fr>
2012-08-29 18:28:46 +0200

    Updte odd splines stuffs

--- prototype/src/splines/test_sll_odd_order_splines.F90
+++ prototype/src/splines/test_sll_odd_order_splines.F90
@@ -7,7 +7,7 @@
 !> Selalib odd order splines interpolator tester
 !
 !> Start date: July 26, 2012
-!> Last modification: August 28, 2012
+!> Last modification: August 29, 2012
 !   
 !> @authors                    
 !> Aliou DIOUF (aliou.l.diouf@inria.fr)
@@ -26,82 +26,90 @@ use sll_odd_order_splines
   sll_real64                            :: xmin, xmax, h
   sll_real64                            :: x, y, mu
   sll_real64                            :: err1, err2, norm
-  sll_int32                             :: n, nmax, i, left
-  sll_int32                             :: order, ierr, ok
+  sll_int32                             :: n, nmax
+  sll_int32                             :: i, left, m
+  sll_int32                             :: order, order_max, ierr, ok
 
-  nmax = 1000
-  order = 5
+  nmax = 10
+  order_max = 19
   ok = 1
 
   print*,'Testing quintic_spline...'
 
-  do n=1,nmax
+  do order=1,order_max,2
 
-     xmin = 0.d0
-     xmax = 10.d0
-     h = (xmax-xmin)/n
-     mu = ( (xmin-order*h) + (xmin+(n+order+1)*h) ) / 2
+     m = order/2+1
 
-     SLL_ALLOCATE( f1(n+order+1), ierr)
-     SLL_ALLOCATE( f2(n+order+1), ierr)
+     do n=1,nmax
 
-     do i=order/2+1-order,n+order/2+1
+        xmin = 0.d0
+        xmax = 10.d0
+        h = (xmax-xmin)/n
+        mu = ( (xmin-order*h) + (xmin+(n+order+1)*h) ) / 2
 
-        x = xmin + i*h
-        f1(i+3) = exp( - .5*( x - mu )**2  )
+        SLL_ALLOCATE( f1(n+order+1), ierr)
+        SLL_ALLOCATE( f2(n+order+1), ierr)
+
+        do i=order/2+1-order,n+order/2+1
+
+           x = xmin + i*h
+           f1(i+m) = exp( - .5*( x - mu )**2  )
       
-        f2(i+3) = 0.d0
-        do j=i-order,i
-           if ( (j>=-order) .and. (j<=n) ) then
-              f2(i+3) = f2(i+3) + B_test(order, j, x, xmin, h)
-           endif
+           f2(i+m) = 0.d0
+           do j=i-order,i
+              if ( (j>=-order) .and. (j<=n) ) then
+                 f2(i+m) = f2(i+m) + B_test(order, j, x, xmin, h)
+              endif
+           enddo
+
         enddo
+
+        plan1 => new_odd_order_splines(n, order, xmin, xmax, f1)
+        plan2 => new_odd_order_splines(n, order, xmin, xmax, f2)
+
+        err1 = 0.d0
+        err2 = 0.d0
+        norm = 0.d0
+
+        do i=0,n
+
+           x = xmin + i*h
+           err1 = err1 + ( f1(i+m) - spline(x, plan1) )**2
+
+           call random_number(x)
+           x = xmin + x*(xmax-xmin) ! x in [xmin, xmax]
+           left = int((x-xmin)/h)!Determine the leftmost support index 'i' of x
+
+           y = 0.d0
+           do j=left-order,left
+              if ( (j>=-order) .and. (j<=n) ) then
+                 y = y + B_test(order, j, x, xmin, h)
+              endif
+           enddo
+
+           norm = norm + y*y
+           err2 = err2 + ( y - spline(x, plan2) )**2
+
+        enddo
+
+        err1 = sqrt( err1/sum( f1(m:n+m)**2 ) )
+        err2 = sqrt( err2/norm )
+        print*, 'Relative errors:', err1, err2
+
+        if ( (err1 >= 1.e-12) .or. (err2 >= 1.e-12) ) then
+           print*, 'Program stopped by iteration number', n
+           ok = 0
+           print*, 'Exiting...'
+           stop
+        endif
+    
+        SLL_DEALLOCATE_ARRAY(f1, ierr)
+        SLL_DEALLOCATE_ARRAY(f2, ierr)
+        call delete_odd_order_splines(plan1)
+        call delete_odd_order_splines(plan2)
 
      enddo
 
-     plan1 => new_odd_order_splines(n, order, xmin, xmax, f1)
-     plan2 => new_odd_order_splines(n, order, xmin, xmax, f2)
-
-     err1 = 0.d0
-     err2 = 0.d0
-     norm = 0.d0
-
-     do i=0,n
-
-        x = xmin + i*h
-        err1 = err1 + ( f1(i+3) - spline(x, plan1) )**2
-
-        call random_number(x)
-        x = xmin + x*(xmax-xmin) ! x in [xmin, xmax]
-        left = int((x-xmin)/h)!Determine the leftmost support index 'i' of x
-
-        y = 0.d0
-        do j=left-order,left
-           if ( (j>=-order) .and. (j<=n) ) then
-              y = y + B_test(order, j, x, xmin, h)
-           endif
-        enddo
-
-        norm = norm + y*y
-        err2 = err2 + ( y - spline(x, plan2) )**2
-
-     enddo
-
-     err1 = sqrt(err1/sum(f1(3:n+3)*f1(3:n+3)))
-     err2 = sqrt(err2/norm)
-     print*, 'Relative errors:', err1, err2
-
-     if ( (err1 >= 1.e-12) .or. (err2 >= 1.e-12) ) then
-        print*, 'Program stopped by iteration number', n
-        ok = 0
-        print*, 'Exiting...'
-        stop
-     endif
-
-    SLL_DEALLOCATE_ARRAY(f1, ierr)
-    SLL_DEALLOCATE_ARRAY(f2, ierr)
-    call delete_odd_order_splines(plan1)
-    call delete_odd_order_splines(plan2)
   enddo
 
   if (ok==1) then
