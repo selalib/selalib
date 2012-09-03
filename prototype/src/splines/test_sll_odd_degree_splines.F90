d61659d5a9abc9e263771d80defa3ed9ae206976
adiouf <aliou.l.diouf@inria.fr>
2012-09-03 19:26:52 +0200

    Update odd degree (former odd order) splines

--- prototype/src/splines/test_sll_odd_degree_splines.F90
+++ prototype/src/splines/test_sll_odd_degree_splines.F90
@@ -1,71 +1,71 @@
 !***************************************************************************
 !
 ! Selalib 2012     
-! Module: test_sll_odd_order_splines.F90
+! Module: test_sll_odd_degree_splines.F90
 !
 !> @brief 
-!> Selalib odd order splines interpolator tester
+!> Selalib odd degree splines interpolator tester
 !
 !> Start date: July 26, 2012
-!> Last modification: August 29, 2012
+!> Last modification: September 04, 2012
 !   
 !> @authors                    
 !> Aliou DIOUF (aliou.l.diouf@inria.fr)
 !                                  
 !***************************************************************************
 
-program test_sll_odd_order_splines
+program test_sll_odd_degree_splines
 
 #include "sll_memory.h"
 #include "sll_working_precision.h"
 use numeric_constants
-use sll_odd_order_splines
+use sll_odd_degree_splines
 
-  type(odd_order_splines_plan), pointer :: plan1, plan2
+  type(odd_degree_splines_plan), pointer :: plan1, plan2
   sll_real64, dimension(:), allocatable :: f1, f2
   sll_real64                            :: xmin, xmax, h
   sll_real64                            :: x, y, mu
   sll_real64                            :: err1, err2, norm
   sll_int32                             :: n, nmax
   sll_int32                             :: i, left, m
-  sll_int32                             :: order, order_max, ierr, ok
+  sll_int32                             :: degree, degree_max, ierr, ok
 
   nmax = 10
-  order_max = 19
+  degree_max = 19
   ok = 1
 
   print*,'Testing quintic_spline...'
 
-  do order=1,order_max,2
+  do degree=1,degree_max,2
 
-     m = order/2+1
+     m = degree/2+1
 
      do n=1,nmax
 
         xmin = 0.d0
         xmax = 10.d0
         h = (xmax-xmin)/n
-        mu = ( (xmin-order*h) + (xmin+(n+order+1)*h) ) / 2
+        mu = ( (xmin-degree*h) + (xmin+(n+degree+1)*h) ) / 2
 
-        SLL_ALLOCATE( f1(n+order+1), ierr)
-        SLL_ALLOCATE( f2(n+order+1), ierr)
+        SLL_ALLOCATE( f1(n+degree+1), ierr)
+        SLL_ALLOCATE( f2(n+degree+1), ierr)
 
-        do i=order/2+1-order,n+order/2+1
+        do i=degree/2+1-degree,n+degree/2+1
 
            x = xmin + i*h
            f1(i+m) = exp( - .5*( x - mu )**2  )
       
            f2(i+m) = 0.d0
-           do j=i-order,i
-              if ( (j>=-order) .and. (j<=n) ) then
-                 f2(i+m) = f2(i+m) + B_test(order, j, x, xmin, h)
+           do j=i-degree,i
+              if ( (j>=-degree) .and. (j<=n) ) then
+                 f2(i+m) = f2(i+m) + B_test(degree, j, x, xmin, h)
               endif
            enddo
 
         enddo
 
-        plan1 => new_odd_order_splines(n, order, xmin, xmax, f1)
-        plan2 => new_odd_order_splines(n, order, xmin, xmax, f2)
+        plan1 => new_odd_degree_splines(n, degree, xmin, xmax, f1)
+        plan2 => new_odd_degree_splines(n, degree, xmin, xmax, f2)
 
         err1 = 0.d0
         err2 = 0.d0
@@ -81,9 +81,9 @@ use sll_odd_order_splines
            left = int((x-xmin)/h)!Determine the leftmost support index 'i' of x
 
            y = 0.d0
-           do j=left-order,left
-              if ( (j>=-order) .and. (j<=n) ) then
-                 y = y + B_test(order, j, x, xmin, h)
+           do j=left-degree,left
+              if ( (j>=-degree) .and. (j<=n) ) then
+                 y = y + B_test(degree, j, x, xmin, h)
               endif
            enddo
 
@@ -105,21 +105,21 @@ use sll_odd_order_splines
     
         SLL_DEALLOCATE_ARRAY(f1, ierr)
         SLL_DEALLOCATE_ARRAY(f2, ierr)
-        call delete_odd_order_splines(plan1)
-        call delete_odd_order_splines(plan2)
+        call delete_odd_degree_splines(plan1)
+        call delete_odd_degree_splines(plan2)
 
      enddo
 
   enddo
 
   if (ok==1) then
-     print*, 'sll_odd_order_splines: PASS'
+     print*, 'sll_odd_degree_splines: PASS'
   endif
 
 
 contains
 
-  sll_real64 recursive function B_test(j, i, x, xmin, h) result(res) ! j:=order
+  sll_real64 recursive function B_test(j, i, x, xmin, h) result(res) ! j:=degree
 
     sll_real64                                    :: x, xmin, h
     sll_int32                                     :: j, i
@@ -152,4 +152,4 @@ contains
   end function B_test
 
 
-end program test_sll_odd_order_splines
+end program test_sll_odd_degree_splines
