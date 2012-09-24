4ae008a16248d11facb3e2d9801e8ac78d41c024
Pierre Navaro <navaro@math.cnrs.fr>
2012-09-24 11:56:34 +0200

    finish 3d layout

--- prototype/src/low_level_file_io/unit_test_parallel.F90
+++ prototype/src/low_level_file_io/unit_test_parallel.F90
@@ -118,8 +118,6 @@ call my_file%create(zfile, error)
 call my_file%write_array(datadims,offset,zdata,zdset,error)
 call my_file%close(error)
 
-
-
 call my_file%create('layout.h5', error)
 call my_file%write_array(datadims,offset,xdata,'x',error)
 call my_file%write_array(datadims,offset,ydata,'y',error)
@@ -157,6 +155,9 @@ if (myrank == 0) then
 
 end if
 
+
+call plot_layout3d()
+
 tcpu2 = MPI_WTIME()
 if (myrank == 0) &
    write(*,"(//10x,' Temps CPU = ', G15.3, ' sec' )") (tcpu2-tcpu1)*colsz
@@ -184,30 +185,342 @@ contains
     n2 = 2**expo2
   end subroutine factorize_in_random_2powers_2d
 
-  subroutine compute_local_sizes_2d( layout, loc_sz_i, loc_sz_j )
-    type(layout_2D), pointer :: layout
-    sll_int32, intent(out) :: loc_sz_i
-    sll_int32, intent(out) :: loc_sz_j
-    sll_int32 :: i_min
-    sll_int32 :: i_max
-    sll_int32 :: j_min
-    sll_int32 :: j_max
-    sll_int32 :: my_rank
-    if( .not. associated(layout) ) then
-       print *, 'not-associated layout passed to new_distributed_mesh_2D'
-       print *, 'Exiting...'
-       STOP
-    end if
-    my_rank = sll_get_collective_rank(get_layout_2D_collective(layout))
-    i_min = get_layout_2D_i_min( layout, my_rank )
-    i_max = get_layout_2D_i_max( layout, my_rank )
-    j_min = get_layout_2D_j_min( layout, my_rank )
-    j_max = get_layout_2D_j_max( layout, my_rank )
-    loc_sz_i = i_max - i_min + 1
-    loc_sz_j = j_max - j_min + 1
-  end subroutine compute_local_sizes_2d
+  subroutine plot_layout3d()
 
-        
+  ! Test of the 3D remapper takes a 3D array whose global size Nx*Ny*Nz,
+  ! distributed among NPi*NPj*NPk processors.
+  sll_real64, dimension(:,:,:), allocatable :: local_array1
+  sll_real64, dimension(:,:,:), allocatable :: local_array2
+  ! Take a 3D array of dimensions ni*nj*nk
+  ! ni, nj, nk: global sizes
+  integer , parameter                       :: ni = 64
+  integer , parameter                       :: nj = 64
+  integer , parameter                       :: nk = 32
+  ! Local sizes
+  integer                                   :: loc_sz_i_init
+  integer                                   :: loc_sz_j_init
+  integer                                   :: loc_sz_k_init
+  integer                                   :: loc_sz_i_final
+  integer                                   :: loc_sz_j_final
+  integer                                   :: loc_sz_k_final
+
+  ! the process mesh
+  integer                                   :: npi
+  integer                                   :: npj
+  integer                                   :: npk
+  sll_int32                                 :: gi, gj, gk
+  integer                                   :: ierr
+  integer                                   :: myrank
+  sll_int64                                 :: colsz        ! collective size
+  ! Remap stuff
+  type(layout_3D), pointer                  :: layout1
+  type(layout_3D), pointer                  :: layout2
+  type(remap_plan_3D), pointer              :: rmp3
+
+  integer                                   :: i, j, k
+  sll_int32, dimension(3)                   :: global_indices
+  type(phdf5_file)                          :: file_layout1
+  type(phdf5_file)                          :: file_layout2
+  integer                                   :: istart, jstart, kstart
+
+  INTEGER(HID_T) :: file_id       ! File identifier 
+  INTEGER(HID_T) :: dset_id       ! Dataset identifier 
+  INTEGER(HID_T) :: filespace     ! Dataspace identifier in file 
+  INTEGER(HID_T) :: plist_id      ! Property list identifier 
+
+  INTEGER(HSIZE_T), DIMENSION(3) :: dimsf = (/ni,nj,nk/) ! Dataset dimensions.
+  INTEGER(HSIZE_T), DIMENSION(3) :: dimsfi
+  INTEGER(HSIZE_T), DIMENSION(3) :: chunk_dims
+
+  INTEGER :: comm, info
+  INTEGER :: mpi_size, mpi_rank
+  INTEGER, PARAMETER :: rank = 3
+
+  INTEGER(HSIZE_T),  DIMENSION(rank) :: count  
+  INTEGER(HSSIZE_T), DIMENSION(rank) :: offset 
+  INTEGER(HSIZE_T),  DIMENSION(rank) :: stride
+  INTEGER(HSIZE_T),  DIMENSION(rank) :: block
+  INTEGER(HID_T) :: memspace 
+
+  colsz  = sll_get_collective_size(sll_world_collective)
+  myrank = sll_get_collective_rank(sll_world_collective)
+
+  layout1  => new_layout_3D( sll_world_collective )        
+  call two_power_rand_factorization(colsz, npi, npj, npk)
+
+  if( myrank .eq. 0 ) then
+     print *, '3D layout configuration: ', npi, npj, npk
+  end if
+
+  call initialize_layout_with_distributed_3D_array( &
+          ni, nj, nk, npi, npj, npk, layout1 )
+     
+  call compute_local_sizes( layout1,       &
+                            loc_sz_i_init, &
+                            loc_sz_j_init, &
+                            loc_sz_k_init )        
+
+  SLL_ALLOCATE(local_array1(loc_sz_i_init,loc_sz_j_init,loc_sz_k_init),ierr)
+ 
+  ! initialize the local data    
+  do k=1,loc_sz_k_init
+     do j=1,loc_sz_j_init 
+        do i=1,loc_sz_i_init
+           global_indices =  local_to_global_3D( layout1, (/i, j, k/) )
+           gi = global_indices(1)
+           gj = global_indices(2)
+           gk = global_indices(3)
+           local_array1(i,j,k) = myrank !gi + (gj-1)*ni + (gk-1)*ni*nj
+        enddo
+     enddo
+  enddo
+
+  chunk_dims(1) = loc_sz_i_init
+  chunk_dims(2) = loc_sz_j_init
+  chunk_dims(3) = loc_sz_k_init
+
+  comm     = sll_world_collective%comm
+  info     = MPI_INFO_NULL
+  mpi_size = sll_get_collective_size(sll_world_collective)
+  mpi_rank = sll_get_collective_rank(sll_world_collective)
+
+  CALL h5open_f(error) 
+
+  CALL h5pcreate_f(H5P_FILE_ACCESS_F, plist_id, error)
+  CALL h5pset_fapl_mpio_f(plist_id, comm, info, error)
+
+  CALL h5fcreate_f("layout1.h5",H5F_ACC_TRUNC_F,file_id,error,access_prp = plist_id)
+  CALL h5pclose_f(plist_id, error)
+  CALL h5screate_simple_f(rank, dimsf, filespace, error)
+  CALL h5screate_simple_f(rank, chunk_dims, memspace, error)
+
+  CALL h5pcreate_f(H5P_DATASET_CREATE_F, plist_id, error)
+  CALL h5pset_chunk_f(plist_id, rank, chunk_dims, error)
+  CALL h5dcreate_f(file_id, "local_array1", H5T_NATIVE_DOUBLE, filespace, &
+                   dset_id, error, plist_id)
+  CALL h5sclose_f(filespace, error)
+
+  stride = 1 
+  count =  1 
+  block = chunk_dims
+
+  istart = get_layout_3D_i_min( layout1, myrank ) - 1
+  jstart = get_layout_3D_j_min( layout1, myrank ) - 1
+  kstart = get_layout_3D_k_min( layout1, myrank ) - 1
+
+  offset = 0
+  if (istart > 0) offset(1) = istart
+  if (jstart > 0) offset(2) = jstart
+  if (kstart > 0) offset(3) = kstart
+
+  CALL h5dget_space_f(dset_id, filespace, error)
+  CALL h5sselect_hyperslab_f (filespace, H5S_SELECT_SET_F, offset, count, error, &
+                              stride, block)
+  CALL h5pcreate_f(H5P_DATASET_XFER_F, plist_id, error) 
+  CALL h5pset_dxpl_mpio_f(plist_id, H5FD_MPIO_COLLECTIVE_F, error)
+     
+  CALL h5dwrite_f(dset_id, H5T_NATIVE_DOUBLE, local_array1, dimsfi, error, &
+                  file_space_id = filespace, mem_space_id = memspace, &
+                  xfer_prp = plist_id)
+
+  CALL h5sclose_f(filespace, error)
+  CALL h5sclose_f(memspace, error)
+  CALL h5dclose_f(dset_id, error)
+  CALL h5pclose_f(plist_id, error)
+  CALL h5fclose_f(file_id, error)
+  CALL h5close_f(error)
+
+  layout2  => new_layout_3D( sll_world_collective )
+  call two_power_rand_factorization(colsz, npi, npj, npk)
+  if( myrank .eq. 0 ) then
+     print *, 'target configuration: ', npi, npj, npk
+  end if
+
+  call initialize_layout_with_distributed_3D_array( &
+       ni, nj, nk, npi, npj, npk, layout2 )
+
+  call reorganize_randomly(layout2)
+     
+  call compute_local_sizes(layout2,        &
+                           loc_sz_i_final, &
+                           loc_sz_j_final, &
+                           loc_sz_k_final )
+
+  SLL_ALLOCATE(local_array2(loc_sz_i_final, loc_sz_j_final, loc_sz_k_final), ierr )
     
+  rmp3 => NEW_REMAP_PLAN_3D( layout1, layout2, local_array1)     
 
+  call apply_remap_3D( rmp3, local_array1, local_array2 ) 
+
+  if( myrank .eq. 0 ) then
+     print *, 'Printing layout1: '
+     call sll_view_lims_3D( layout1 )
+     print *, 'Printing layout2: '
+     call sll_view_lims_3D( layout2 )
+  end if
+
+  call file_layout1%create('layout_1.h5',error)
+  call file_layout1%write_array(dimsf,offset,local_array1,'array1',error)
+  call file_layout1%close(error)
+
+  call sll_collective_barrier(sll_world_collective)
+  
+  call delete_layout_3D( layout1 )
+  call delete_layout_3D( layout2 )
+  SLL_DEALLOCATE_ARRAY(local_array1, ierr)
+  SLL_DEALLOCATE_ARRAY(local_array2, ierr)
+
+  end subroutine plot_layout3d
+
+  subroutine reorganize_randomly(layout)
+    implicit none
+    type(layout_3D), pointer   :: layout
+    integer                    :: i, colsz, proc_n, proc_p
+    real                       :: rand_real
+    colsz = sll_get_num_nodes(layout)
+    do i=0, colsz-1
+       call random_number(rand_real)
+       proc_n = int(rand_real*(colsz-1))
+       call random_number(rand_real)
+       proc_p = int(rand_real*(colsz-1))
+       call swap_box_3D(proc_n, proc_p, layout)
+    enddo    
+  end subroutine reorganize_randomly
+
+  subroutine swap_box_3D(proc_n, proc_p, layout)
+    implicit none
+    integer                  :: proc_n, proc_p
+    type(layout_3D), pointer :: layout
+    integer                  :: i_min_n, i_max_n, j_min_n, j_max_n, &
+    k_min_n, k_max_n, i_min_p, i_max_p, j_min_p, j_max_p, k_min_p, k_max_p    
+    ! Get proc_n contents from layout
+    i_min_n = get_layout_3D_i_min( layout, proc_n )
+    i_max_n = get_layout_3D_i_max( layout, proc_n )
+    j_min_n = get_layout_3D_j_min( layout, proc_n )
+    j_max_n = get_layout_3D_j_max( layout, proc_n )
+    k_min_n = get_layout_3D_k_min( layout, proc_n )
+    k_max_n = get_layout_3D_k_max( layout, proc_n )
+    ! Get proc_p contents from layout
+    i_min_p = get_layout_3D_i_min( layout, proc_p )
+    i_max_p = get_layout_3D_i_max( layout, proc_p )
+    j_min_p = get_layout_3D_j_min( layout, proc_p )
+    j_max_p = get_layout_3D_j_max( layout, proc_p )
+    k_min_p = get_layout_3D_k_min( layout, proc_p )
+    k_max_p = get_layout_3D_k_max( layout, proc_p )
+    ! Set proc_n contents in layout
+    call set_layout_3D_i_min( layout, proc_n, i_min_p )
+    call set_layout_3D_i_max( layout, proc_n, i_max_p)
+    call set_layout_3D_j_min( layout, proc_n, j_min_p )
+    call set_layout_3D_j_max( layout, proc_n, j_max_p )
+    call set_layout_3D_k_min( layout, proc_n, k_min_p )
+    call set_layout_3D_k_max( layout, proc_n, k_max_p )
+    ! Set proc_p contents in layout
+    call set_layout_3D_i_min( layout, proc_p, i_min_n )
+    call set_layout_3D_i_max( layout, proc_p, i_max_n )
+    call set_layout_3D_j_min( layout, proc_p, j_min_n )
+    call set_layout_3D_j_max( layout, proc_p, j_max_n )
+    call set_layout_3D_k_min( layout, proc_p, k_min_n )
+    call set_layout_3D_k_max( layout, proc_p, k_max_n )   
+  end subroutine swap_box_3D
+  
+  subroutine two_power_rand_factorization(n, n1, n2, n3)
+    sll_int64, intent(in) :: n
+    integer, intent(out) ::n1, n2, n3
+    integer   :: expo, expo1, expo2, expo3
+    if (.not.is_power_of_two(colsz)) then   
+       print*, 'The number of processors must be a power of 2'
+       stop
+    endif 
+    expo = int(log(real(n))/log(2.))  
+    call random_number(rand_real)
+    expo1 = int(rand_real*expo)
+    call random_number(rand_real)
+    expo2 = int(rand_real*(expo-expo1))
+    expo3 = expo - (expo1+expo2)
+    n1 = 2**expo1
+    n2 = 2**expo2
+    n3 = 2**expo3
+  end subroutine two_power_rand_factorization
+
+  subroutine factorize_in_random_2powers(n, n1, n2)
+    sll_int64, intent(in) :: n
+    integer, intent(out)  :: n1, n2
+    integer   :: expo, expo1, expo2
+    if (.not.is_power_of_two(colsz)) then   
+       print*, 'The number of processors must be a power of 2'
+       stop
+    endif
+    expo = int(log(real(n))/log(2.))  
+    call random_number(rand_real)
+    expo1 = int(rand_real*expo)
+    call random_number(rand_real)
+    expo2 = expo - expo1
+    n1 = 2**expo1
+    n2 = 2**expo2
+  end subroutine factorize_in_random_2powers
+  
+      subroutine split_interval_randomly(n, num_halvings, ans)
+        integer, intent(in) :: n
+        integer, intent(in) :: num_halvings
+        integer, dimension(:), pointer :: ans
+        integer :: load_i = 1
+        integer :: ierr
+        SLL_ALLOCATE(ans(2*(2**num_halvings)),ierr)
+        call split_interval_randomly_aux( 1, n, 0, num_halvings, load_i, ans )
+      end subroutine split_interval_randomly
+      
+      recursive subroutine split_interval_randomly_aux( lo, hi, gen, lim, loadi, ans )
+        intrinsic :: random_number
+        integer, intent(in)                :: lo
+        integer, intent(in)                :: hi
+        integer, intent(in)                :: gen ! splitting generation
+        integer, intent(in)                :: lim ! maximum number of splittings
+        integer :: mid
+        integer, parameter                 :: spread = 25
+        ! dangerous... the following should be found in the enclosing scope...
+        integer, intent(inout) :: loadi
+        integer, dimension(:), pointer :: ans
+        if( (hi-lo).eq. 1 ) then
+           ans(loadi) = lo
+           ans(loadi+1) = hi
+           loadi = loadi + 2
+        else if (gen .eq. lim) then
+           ans(loadi) = lo
+           ans(loadi+1) = hi
+           loadi = loadi + 2
+        else
+           ! call random_number(rand)
+           ! decided to use the gaussian because the uniform deviate can give
+           ! a number very close to the 0 or one and such a small interval is
+           ! hard to subdivide. In such case one may end up with less intervals
+           ! and this is a problem since the number of processors is fixed from
+           ! the beginning.
+           mid = (hi+lo)/2 + gaussian_dev()*spread
+           !       mid = int(rand*(hi-lo))+lo
+           call split_interval_randomly_aux( lo,   mid, gen+1, lim, loadi, ans )
+           call split_interval_randomly_aux( mid+1, hi, gen+1, lim, loadi, ans )
+        end if
+      end subroutine split_interval_randomly_aux
+      
+      function gaussian_dev()
+        intrinsic :: random_number
+        real :: gaussian_dev
+        real :: v1, v2
+        real :: ran1, ran2
+        real :: rsq
+        real :: fac
+        do
+           call random_number(ran1)
+           call random_number(ran2)
+           v1 = 2.0*ran1-1.0
+           v2 = 2.0*ran2-1.0
+           rsq = v1*v1 + v2*v2
+           if( (rsq .lt. 1.0) .and. (rsq .gt. 0.0) ) exit
+        end do
+        fac = sqrt(-2.0*log(rsq)/rsq)
+        gaussian_dev = v1*fac
+      end function gaussian_dev
+      
+    
+  
 end program test_io_parallel
