09afe56678e27f3f4c865aafe5fe00abf448522d
Pierre Navaro <navaro@math.cnrs.fr>
2012-10-18 21:06:37 +0200

    add df plots in vp2d (not finished)

--- slv2d/src/selalib.h
+++ slv2d/src/selalib.h
@@ -1,9 +1,11 @@
 #include "sll_assert.h"
 #include "sll_memory.h"
 #include "sll_working_precision.h"
+use hdf5
 use numeric_constants
 use sll_misc_utils
 use sll_io, only:sll_new_file_id
 use sll_collective
+use sll_hdf5_io_parallel
 
 #define MPI_MASTER 0
