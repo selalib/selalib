886e830771dba431d63c669186a1dbdbae04676f
Pierre Navaro <navaro@math.cnrs.fr>
2012-10-18 16:54:49 +0200

    Add xdmf parallel output

--- prototype/src/low_level_file_io/sll_xdmf_parallel.F90
+++ prototype/src/low_level_file_io/sll_xdmf_parallel.F90
@@ -43,6 +43,7 @@ module sll_xdmf_parallel
 #include "sll_working_precision.h"
 #include "sll_assert.h"
   
+  use sll_collective
   use sll_hdf5_io_parallel
   use sll_ascii_io
   use sll_xml_io
@@ -74,9 +75,13 @@ contains
     sll_int32, intent(out)       :: error
     sll_int32                    :: nnodes_x1
     sll_int32                    :: nnodes_x2
+    sll_int32                    :: myrank
     
-    call sll_xml_file_create(trim(file_name),file_id,error)
-    call sll_xml_grid_geometry(file_id, trim(mesh_name), nnodes_x1, nnodes_x2)
+    myrank = sll_get_collective_rank(sll_world_collective)
+    if (myrank == 0) then
+       call sll_xml_file_create(trim(file_name),file_id,error)
+       call sll_xml_grid_geometry(file_id, trim(mesh_name), nnodes_x1, nnodes_x2)
+    end if
 
   end subroutine sll_xdmf_open_2d
 
@@ -97,9 +102,14 @@ contains
     sll_int32                    :: nnodes_x1
     sll_int32                    :: nnodes_x2
     sll_int32                    :: nnodes_x3
-
-    call sll_xml_file_create(trim(file_name),file_id,error)
-    call sll_xml_grid_geometry(file_id, trim(mesh_name), nnodes_x1, nnodes_x2, nnodes_x3)
+    sll_int32                    :: myrank
+    
+    myrank = sll_get_collective_rank(sll_world_collective)
+    if (myrank == 0) then
+       call sll_xml_file_create(trim(file_name),file_id,error)
+       call sll_xml_grid_geometry(file_id, trim(mesh_name),  &
+                                  nnodes_x1, nnodes_x2, nnodes_x3)
+    end if
 
   end subroutine sll_xdmf_open_3d
   
@@ -120,9 +130,9 @@ contains
     sll_int32, intent(in), optional     :: xmffile_id
     character(len=4), optional          :: center
     sll_int32, intent(out)              :: error
+    sll_int32                           :: myrank
     
-    npoints_x1 = size(array,1)
-    npoints_x2 = size(array,2)
+    
 
     call sll_hdf5_file_create(trim(mesh_name)//"-"//trim(array_name)//".h5", &
                               file_id,error)
@@ -130,7 +140,11 @@ contains
                               array,"/"//trim(array_name),error)
     call sll_hdf5_file_close(file_id, error)
 
-    if ( present(xmffile_id) .and. present(center)) then
+    myrank = sll_get_collective_rank(sll_world_collective)
+
+    if ( present(xmffile_id) .and. present(center) .and. myrank==0) then
+       npoints_x1 = transfer(global_dims(1),1_i32)
+       npoints_x2 = transfer(global_dims(2),1_i32)
        call sll_xml_field( &
             xmffile_id, &
             trim(array_name), &
@@ -144,31 +158,36 @@ contains
   end subroutine sll_xdmf_array_2d
 
   !>Write 3d array in binary or hdf5 file and the matching line in XDMF file
-  subroutine sll_xdmf_array_3d(mesh_name,array,array_name,error,xmffile_id,center)
+  subroutine sll_xdmf_array_3d(mesh_name,global_dims,offset, &
+                               array,array_name,error,xmffile_id,center)
 
+    use hdf5
     character(len=*), intent(in)    :: mesh_name
     sll_real64, intent(in)          :: array(:,:,:)
     character(len=*), intent(in)    :: array_name
     sll_int32, intent(out)          :: error
-    sll_int32                       :: file_id
+    integer(HSSIZE_T)               :: offset(3)
+    integer(HSIZE_T)                :: global_dims(3)
+    integer(HID_T)                  :: file_id
     sll_int32, intent(in), optional :: xmffile_id
     character(len=4), optional      :: center
     sll_int32                       :: npoints_x1
     sll_int32                       :: npoints_x2
     sll_int32                       :: npoints_x3
+    sll_int32                       :: myrank
     
-    npoints_x1 = size(array,1)
-    npoints_x2 = size(array,2)
-    npoints_x3 = size(array,3)
 
-#warning sll_xdmf_array_3d is not implemented
-   !call sll_hdf5_file_create(trim(mesh_name)//"-"//trim(array_name)//".h5", &
-   !                          file_id,error)
-   !call sll_hdf5_write_array(file_id,array,"/"//trim(array_name),error)
-   !call sll_hdf5_file_close(file_id, error)
+    call sll_hdf5_file_create(trim(mesh_name)//"-"//trim(array_name)//".h5", &
+                             file_id,error)
+    call sll_hdf5_write_array(file_id,global_dims,offset,array, &
+                              "/"//trim(array_name),error)
+    call sll_hdf5_file_close(file_id, error)
 
-    if ( present(xmffile_id) .and. present(center)) then
-
+    myrank = sll_get_collective_rank(sll_world_collective)
+    if ( present(xmffile_id) .and. present(center) .and. myrank==0) then
+       npoints_x1 = transfer(global_dims(1),1_i32)
+       npoints_x2 = transfer(global_dims(2),1_i32)
+       npoints_x3 = transfer(global_dims(3),1_i32)
        call sll_xml_field(xmffile_id,trim(array_name), &
                           trim(mesh_name)//"-"//trim(array_name)//".bin", &
                           npoints_x1,npoints_x2,npoints_x3,'Binary',center)
