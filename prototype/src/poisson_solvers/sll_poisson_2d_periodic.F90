9aac3088ad69faea7a2d91b5b79a88c73c13119a
Pierre Navaro <navaro@math.cnrs.fr>
2012-11-03 19:55:57 +0100

    Add a poisson solver with fftw calls

--- prototype/src/poisson_solvers/sll_poisson_2d_periodic.F90
+++ prototype/src/poisson_solvers/sll_poisson_2d_periodic.F90
@@ -36,6 +36,9 @@ module sll_poisson_2d_periodic
 #include "sll_assert.h"
 
 use numeric_constants
+
+#ifdef _FFTPACK
+
 use fft_module
 
 implicit none
@@ -57,9 +60,38 @@ contains
    generic   :: solve => solve_e_fields, solve_potential
 end type poisson_2d_periodic
 
+#else
+
+use, intrinsic :: iso_c_binding
+implicit none
+include 'fftw3.f03'
+
+interface solve
+   module procedure solve_potential
+   module procedure solve_e_fields
+end interface
+interface free
+   module procedure free_poisson
+end interface
+
+type :: poisson_2d_periodic
+   sll_real64, dimension(:,:), pointer  :: kx, ky, k2
+   type(C_PTR)                          :: fw, bw
+   complex(C_DOUBLE_COMPLEX), dimension(:,:), pointer :: rhot
+   complex(C_DOUBLE_COMPLEX), dimension(:,:), pointer :: ext
+   complex(C_DOUBLE_COMPLEX), dimension(:,:), pointer :: eyt
+   integer(C_SIZE_T) :: sz_rhot, sz_ext, sz_eyt
+   type(C_PTR) :: p_rhot, p_ext, p_eyt
+   sll_int32 :: nc_x, nc_y
+   sll_real64  :: dx, dy
+end type poisson_2d_periodic
+
+
+#endif
 
 contains
 
+#ifdef _FFTPACK
 !> Create an object to solve Poisson equation on 2D mesh with periodic
 !> boundary conditions:
 subroutine initialize(this, x_min, x_max, nc_x, &
@@ -274,4 +306,150 @@ subroutine transpose_c2r(comp_array, real_array)
 
 end subroutine transpose_c2r
 
+#else
+
+subroutine initialize(self, x_min, x_max, nc_x, &
+                      y_min, y_max, nc_y, rho, error )
+
+   type(poisson_2d_periodic) :: self
+   sll_real64, dimension(:,:), intent(inout) :: rho
+   sll_real64, intent(in) :: x_min, x_max, y_min, y_max
+   sll_int32  :: error
+   sll_int32                                 :: nc_x, nc_y
+   sll_int32  :: ik, jk
+   sll_real64 :: kx1, kx0, ky0
+
+   self%nc_x = nc_x
+   self%nc_y = nc_y
+
+   self%dx   = (x_max-x_min) / nc_x
+   self%dy   = (y_max-y_min) / nc_y
+
+   self%sz_rhot = int((nc_x/2+1)*nc_y,C_SIZE_T)
+   self%p_rhot = fftw_alloc_complex(self%sz_rhot)
+   call c_f_pointer(self%p_rhot, self%rhot, [nc_x/2+1,nc_y])
+
+   self%sz_ext = int((nc_x/2+1)*nc_y,C_SIZE_T)
+   self%p_ext = fftw_alloc_complex(self%sz_ext)
+   call c_f_pointer(self%p_ext, self%ext, [nc_x/2+1,nc_y])
+
+   self%sz_eyt = int((nc_x/2+1)*nc_y,C_SIZE_T)
+   self%p_eyt = fftw_alloc_complex(self%sz_eyt)
+   call c_f_pointer(self%p_eyt, self%eyt, [nc_x/2+1,nc_y])
+
+   SLL_ALLOCATE(self%kx (nc_x/2+1,nc_y), error)
+   SLL_ALLOCATE(self%ky (nc_x/2+1,nc_y), error)
+   SLL_ALLOCATE(self%k2 (nc_x/2+1,nc_y), error)
+
+   !call dfftw_init_threads(error)
+   !if (error == 0) stop 'FFTW CAN''T USE THREADS'
+   !call dfftw_plan_with_nthreads(nthreads)
+
+   self%fw = fftw_plan_dft_r2c_2d(nc_y,nc_x,rho(1:nc_x,1:nc_y), &
+                                  self%ext,FFTW_ESTIMATE)
+   self%bw = fftw_plan_dft_c2r_2d(nc_y,nc_x,self%eyt,           &
+                                  rho(1:nc_x,1:nc_y),FFTW_ESTIMATE)
+
+   kx0 = 2._f64*sll_pi/(x_max-x_min)
+   ky0 = 2._f64*sll_pi/(y_max-y_min)
+   
+   do ik=1,nc_x/2+1
+      kx1 = (ik-1)*kx0
+      do jk = 1, nc_y/2
+         self%kx(ik,jk) = kx1
+         self%ky(ik,jk) = (jk-1)*ky0
+      end do
+      do jk = nc_y/2+1 , nc_y     
+         self%kx(ik,jk) = kx1
+         self%ky(ik,jk) = (jk-1-nc_y)*ky0
+      end do
+   end do
+   self%kx(1,1) = 1.0_f64
+   
+   self%k2 = self%kx*self%kx+self%ky*self%ky
+   self%kx = self%kx/self%k2
+   self%ky = self%ky/self%k2
+
+   !SLL_DEALLOCATE(self%k2, error)
+
+end subroutine initialize
+
+!> Solve Poisson equation on 2D mesh with periodic boundary conditions. 
+!> return potential.
+subroutine solve_potential(self, rho, phi)
+
+   type(poisson_2d_periodic),intent(inout)  :: self
+   sll_real64, dimension(:,:), intent(inout) :: rho
+   sll_real64, dimension(:,:), intent(out)   :: phi
+   sll_int32                                 :: nc_x, nc_y
+
+   call fftw_execute_dft_r2c(self%fw, rho, self%rhot)
+
+   self%rhot = self%rhot / self%k2
+
+   call fftw_execute_dft_c2r(self%bw, self%rhot, phi)
+
+   nc_x = self%nc_x
+   nc_y = self%nc_y
+
+   phi = phi / (nc_x*nc_y)     ! normalize
+
+end subroutine solve_potential
+
+!> Solve Poisson equation on 2D mesh with periodic boundary conditions. 
+!> return electric fields.
+subroutine solve_e_fields(self,e_x,e_y,rho,nrj)
+
+   type(poisson_2d_periodic),intent(inout)  :: self
+   sll_real64, dimension(:,:), intent(inout) :: rho
+   sll_real64, dimension(:,:), intent(out)   :: e_x
+   sll_real64, dimension(:,:), intent(out)   :: e_y
+   sll_real64, optional                      :: nrj
+   sll_int32  :: nc_x, nc_y
+   sll_real64 :: dx, dy
+
+   nc_x = self%nc_x
+   nc_y = self%nc_y
+
+   call fftw_execute_dft_r2c(self%fw, rho, self%rhot)
+
+   self%ext(1,1) = 0.0_f64
+   self%eyt(1,1) = 0.0_f64
+   self%ext = -cmplx(0.0_f64,self%kx,kind=f64)*self%rhot
+   self%eyt = -cmplx(0.0_f64,self%ky,kind=f64)*self%rhot
+
+   call fftw_execute_dft_c2r(self%bw, self%ext, e_x)
+   call fftw_execute_dft_c2r(self%bw, self%eyt, e_y)
+
+   e_x = e_x / (nc_x*nc_y)
+   e_y = e_y / (nc_x*nc_y)
+
+   if (present(nrj)) then 
+      dx = self%dx
+      dy = self%dy
+      nrj=sum(e_x*e_x+e_y*e_y)*dx*dy
+      if (nrj>1.e-30) then 
+         nrj=0.5_f64*log(nrj)
+      else
+         nrj=-10**9
+      endif
+   end if
+
+end subroutine solve_e_fields
+
+subroutine free_poisson(self)
+type(poisson_2d_periodic) :: self
+call fftw_free(self%p_rhot)
+if (c_associated(self%p_ext)) call fftw_free(self%p_ext)
+if (c_associated(self%p_eyt)) call fftw_free(self%p_eyt)
+call dfftw_destroy_plan(self%fw)
+call dfftw_destroy_plan(self%bw)
+!if (nthreads > 1) then
+   !call dfftw_cleanup_threads(error)
+!end if
+
+end subroutine
+
+
+#endif
 end module sll_poisson_2D_periodic
