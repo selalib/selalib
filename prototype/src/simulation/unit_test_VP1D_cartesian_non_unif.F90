8ebaf243573311eef5eb0b07e3ac473ff7285ae0
Michel Mehrenberger <mehrenbe@math.unistra.fr>
2012-11-05 08:09:12 +0100

    update

--- prototype/src/simulation/unit_test_VP1D_cartesian_non_unif.F90
+++ prototype/src/simulation/unit_test_VP1D_cartesian_non_unif.F90
@@ -1,20 +1,21 @@
 ! Sample computation with the following characteristics:
 ! - vlasov-poisson
-! - 4D cartesian: x, y, vx, vy (or x1, x2, x3, x4)
-! - parallel
+! - 1Dx1D cartesian: x, vx (or x1, x2)
+! - non uniform mesh
+! - sequential
+
 
 program unit_test_VP1D_cartesian_non_unif
   use simulation_VP1D_cartesian_non_unif
-  use sll_collective
   implicit none
 
   type(sll_simulation_VP1D_cartesian_non_unif) :: simulation
 
-  print *, 'Begin of VP1D_cartesian_non_unif test'
+  print *, '#Begin of VP1D_cartesian_non_unif test'
   call simulation%run( )
 
-  print *, 'reached end of VP1D_cartesian_non_unif test'
-  print *, 'PASSED'
+  print *, '#reached end of VP1D_cartesian_non_unif test'
+  print *, '#PASSED'
   !call delete_vp4d_par_cart(simulation)
 
 
