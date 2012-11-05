fb43a3357b53050f801bde92959159818d594b67
Pierre Navaro <navaro@math.unistra.fr>
2012-11-05 16:07:30 +0100

    Build slv2d with intel compiler

--- slv2d/src/CMakeModules/FortranConfig.cmake
+++ slv2d/src/CMakeModules/FortranConfig.cmake
@@ -1,9 +1,6 @@
-
 FILE(COPY ${SELALIB_DIR}/check_compiler.F90 DESTINATION ${PROJECT_SOURCE_DIR} )
-TRY_RUN( RUN_RESULT_VAR
-	 COMPILE_RESULT_VAR
-         ${CMAKE_BINARY_DIR}
-         ${CMAKE_SOURCE_DIR}/check_compiler.F90)
+TRY_RUN( RUN_RESULT_VAR COMPILE_RESULT_VAR
+         ${CMAKE_BINARY_DIR} ${CMAKE_SOURCE_DIR}/check_compiler.F90)
 
 
 set(NO_FORTRAN_2003 NO)
@@ -29,13 +26,21 @@ ELSE()
 ENDIF()
 
 IF(Fortran_COMPILER STREQUAL "GFORTRAN")
-  add_definitions(-DGFORTRAN)
-  set(CMAKE_Fortran_FLAGS "-g -Wall -cpp -pedantic -ffree-line-length-none -std=f2003 -fall-intrinsics -fbounds-check")
-  add_definitions(-DDEBUG -DMPIF90)
+
+   ADD_DEFINITIONS(-DGFORTRAN)
+   SET(CMAKE_Fortran_FLAGS "-g -Wall -cpp -pedantic -ffree-line-length-none -std=f2003 -fall-intrinsics -fbounds-check")
+   ADD_DEFINITIONS(-DDEBUG -DMPIF90)
+
 ELSEIF(Fortran_COMPILER STREQUAL "INTEL")
-	set(CMAKE_Fortran_FLAGS " ")
+
+   SET(CMAKE_Fortran_FLAGS " ")
+   SET(ENV{OMPI_FC} "ifort")
+   ADD_DEFINITIONS(-DDEBUG -DMPIF90 -DINTEL)
+
 ELSEIF(Fortran_COMPILER_NAME STREQUAL "xlf")
-  set(CMAKE_Fortran_FLAGS "-qextname=flush -qthreaded -qhalt=e -qxlf2003=polymorphic")
+
+   SET(CMAKE_Fortran_FLAGS "-qextname=flush -qthreaded -qhalt=e -qxlf2003=polymorphic")
+
 ELSE()
 	message(STATUS "NO KNOWN FORTRAN COMPILER FOUND")
 ENDIF()
@@ -44,7 +49,7 @@ ENDIF()
 # fortran95 standard was requested. 
 #STRING(FIND ${CMAKE_Fortran_FLAGS} "-std=f95" VAR)
 #IF("${VAR}" STREQUAL "-1")
-  set(STDF95 NO)
+  SET(STDF95 NO)
 #ELSE()
 #  set(STDF95 YES)
 #  set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fmax-identifier-length=63")
