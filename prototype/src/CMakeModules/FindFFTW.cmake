3b59566b4323098b1bf1abb13f863ec255243db9
Pierre Navaro <navaro@math.unistra.fr>
2012-11-13 17:22:45 +0100

    Fix bug in FFTW config

--- prototype/src/CMakeModules/FindFFTW.cmake
+++ prototype/src/CMakeModules/FindFFTW.cmake
@@ -53,19 +53,19 @@ ENDIF()
 SET(FFTW_FOUND FALSE)
 
 IF($ENV{HOSTNAME} MATCHES "hpc-f0*")
-   SET(FFTW_INCLUDE_DIR "/home/math/navaro/local/include")
+   SET(FFTW_INCLUDE_DIRS "/home/math/navaro/local/include")
    SET(FFTW_LIBRARIES "/home/math/navaro/local/lib/libfftw3.a" 
                       "/home/math/navaro/local/lib/libfftw3_threads.a" 
                       "/home/math/navaro/local/lib/libfftw3_mpi.a")
 ENDIF()
 
 IF(FFTW_INCLUDE_DIR AND FFTW_LIBRARIES)
-   MESSAGE(STATUS "FFTW_INCLUDE_DIRS=${FFTW_INCLUDE_DIR}")
+   MESSAGE(STATUS "FFTW_INCLUDE_DIRS=${FFTW_INCLUDE_DIRS}")
    MESSAGE(STATUS "FFTW_LIBRARIES=${FFTW_LIBRARIES}")
    SET(FFTW_FOUND TRUE)
 ENDIF()
 
-MARK_AS_ADVANCED( FFTW_INCLUDE_DIR
+MARK_AS_ADVANCED( FFTW_INCLUDE_DIRS
                   FFTW_MPI_INCLUDE_DIR
                   FFTW_LIBRARIES
                   FFTW_FOUND              )
