110831134d8289b68c50aa4919041ded8bcc6a77
Martin Campos Pinto <campos@ann.jussieu.fr>
2012-11-14 18:03:02 +0100

    added a directory for the linearly-transformed pic method

---
+++ prototype/src/linearly_transformed_pic/leap_frog_2nd_flow_vp_2d.F90
@@ -0,0 +1,63 @@
+module sll_leap_frog_2nd_flow_2d
+#include "sll_working_precision.h"
+#include "sll_assert.h"
+  use numeric_constants
+  use sll_flow_base
+  implicit none
+
+  type, extends(flow_base) :: leap_frog_2nd_flow_2d
+    sll_real64, dimension(:), pointer     :: electric_field
+    sll_int32 , intent(in)                :: nc_poisson_mesh ! number of cells in the poisson mesh
+    sll_real64, intent(in)                :: xmin_poisson_mesh
+    sll_real64, intent(in)                :: dx_poisson_mesh
+
+  contains
+    procedure, pass(flow_obj) :: initialize => init_lf_2nd_flow
+    procedure, pass(flow_obj) :: flow_at_xv => lf_2nd_flow_at_xv
+  end type leap_frog_2nd_flow_2d
+
+contains
+
+  subroutine init_lf_2nd_flow( flow_obj, dt, nc_poisson_mesh, xmin_poisson_mesh, xmax_poisson_mesh)
+    class(leap_frog_2nd_flow_2d), intent(inout)  :: flow_obj
+    sll_real64, intent(in)                       :: dt
+    sll_int32, intent(in)                        :: nc_poisson_mesh
+    sll_real64, intent(in)                       :: xmin_poisson_mesh
+    sll_real64, intent(in)                       :: xmax_poisson_mesh
+    sll_int32  :: ierr
+
+    flow_obj%dt = dt
+    flow_obj%xmin_poisson_mesh = xmin_poisson_mesh
+    flow_obj%nc_poisson_mesh   = nc_poisson_mesh
+    flow_obj%dx_poisson_mesh   = (xmax_poisson_mesh-xmin_poisson_mesh)/nc_poisson_mesh
+    SLL_ALLOCATE( flow_obj%electric_field(nc_poisson_mesh+1),  ierr)
+  end subroutine init_lf_2nd_flow
+
+  subroutine lf_2nd_flow_at_xv( flow_obj, x,v, f_x,f_v )
+    class(leap_frog_2nd_flow_2d), intent(inout)       :: flow_obj
+    sll_real64, intent(in)     :: x
+    sll_real64, intent(in)     :: v
+    sll_real64, intent(out)    :: f_x
+    sll_real64, intent(out)    :: f_v
+    sll_int32                  :: ix
+    sll_real64                 :: dt
+    sll_real64                 :: normalized_x
+    sll_real64                 :: theta_x
+    sll_real64                 :: elec_field_at_x
+    
+    ! here I evaluate the electric field at x with affine interpolation, but I think there should be a class for the E field,
+    ! and we should call something like electric_field%get_value(x)
+    normalized_x = (x-flow_obj%xmin_poisson_mesh)/flow_obj%dx_poisson_mesh
+    ix = 1+int( floor(normalized_x) )
+    SLL_ASSERT( ix >= 1 )
+    SLL_ASSERT( ix <= nc_poisson_mesh )
+    theta_x = ix+1-normalized_x
+    elec_field_at_x = theta_x * flow_obj%electric_field(ix) + (1-theta_x) * flow_obj%electric_field(ix+1)
+        
+    dt = flow_obj%dt 
+    f_x = x + 0.5*dt*( v + dt*elec_field_at_x )
+    f_v = v + dt*elec_field_at_x )
+
+  end subroutine lf_2nd_flow_at_xv
+
+end module sll_leap_frog_2nd_flow_2d
