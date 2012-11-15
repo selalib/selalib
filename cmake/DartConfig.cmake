0ebd2e3795fbfd370f12ffbfd713185b78f6e0fc
Pierre Navaro <navaro@math.cnrs.fr>
2012-11-15 11:26:52 +0100

    Add the bash script for nightly build

--- cmake/DartConfig.cmake
+++ cmake/DartConfig.cmake
@@ -1,11 +1,3 @@
-<<<<<<< HEAD:cmake/DartConfig.cmake
-SET(CTEST_PROJECT_NAME "Pipol")
-SET(NIGHTLY_START_TIME "20:00:00 EST")
-SET(DROP_METHOD "http")
-SET(DROP_SITE "cdash.inria.fr")
-SET(DROP_LOCATION "/CDash/submit.php?project=Pipol")
-SET(DROP_SITE_CDASH TRUE)
-=======
 SITE_NAME(HOSTNAME)
 MESSAGE(STATUS "HOSTNAME:${HOSTNAME}")
 IF(HOSTNAME MATCHES "inrialpes")
@@ -16,4 +8,3 @@ IF(HOSTNAME MATCHES "inrialpes")
    SET(DROP_LOCATION "/CDash/submit.php?project=Pipol")
    SET(DROP_SITE_CDASH TRUE)
 ENDIF()
->>>>>>> origin/phdf5-3d:cmake/DartConfig.cmake
