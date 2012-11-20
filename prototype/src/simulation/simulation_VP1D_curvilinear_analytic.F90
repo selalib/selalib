f6f2c8523a2cf17603b227441f82f3f4c8ef7502
Michel Mehrenberger <mehrenbe@math.unistra.fr>
2012-11-20 17:29:49 +0100

    change name of module: cubic_non_uniform_splines instead of cubic_nonuniform_splines

--- prototype/src/simulation/simulation_VP1D_curvilinear_analytic.F90
+++ prototype/src/simulation/simulation_VP1D_curvilinear_analytic.F90
@@ -10,7 +10,7 @@ module simulation_VP1D_curvilinear_analytic
 #include "sll_memory.h"
 
   use sll_simulation_base
-  use cubic_nonuniform_splines
+  use cubic_non_uniform_splines
   use numeric_constants
   implicit none
 
@@ -215,7 +215,7 @@ contains
   subroutine csl_advection_per(f,spl_per,Xstar,node_positions,N)
     !Xstar and node_positions are normalized to [0,1]
     use numeric_constants
-    use cubic_nonuniform_splines
+    use cubic_non_uniform_splines
     implicit none
     
     sll_real64,dimension(:),pointer::f,Xstar,node_positions
