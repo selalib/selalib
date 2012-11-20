2280ce88cae07b66fd4127ee53ac793431a91635
Michel Mehrenberger <mehrenbe@math.unistra.fr>
2012-11-20 17:32:16 +0100

    update

---
+++ prototype/src/interpolators/cubic_non_uniform_splines_interpolator_1d.F90
@@ -0,0 +1,12 @@
+module cubic_non_uniform_spline_interpolator_1d
+#include "sll_working_precision.h"
+#include "sll_memory.h"
+#include "sll_assert.h"
+#ifndef STDF95
+use sll_module_interpolators_1d_base
+#endif
+use cubic_nonuniform_splines
+use sll_splines
+  implicit none
+  
+end module cubic_non_uniform_spline_interpolator_1d  
