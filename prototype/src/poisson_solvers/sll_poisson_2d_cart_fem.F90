dac3010b5bffe6e8b4463b3fa6d8a99078147278
Pierre Navaro <navaro@math.unistra.fr>
2012-11-21 11:58:08 +0100

    STF95 problem fix ?

--- prototype/src/poisson_solvers/sll_poisson_2d_cart_fem.F90
+++ prototype/src/poisson_solvers/sll_poisson_2d_cart_fem.F90
@@ -1,11 +1,12 @@
 program sll_poisson_2d_cart_fem
 #include "sll_working_precision.h"
 #include "sll_memory.h"
+use numeric_constants
 implicit none
 
 sll_int32 :: i, j, ii, jj
-sll_int32, parameter :: nx = 64, ny = 128
-sll_real64, parameter :: dimx=1._f64, dimy=1._f64
+sll_int32, parameter :: nx = 64, ny = 64
+sll_real64 :: dimx, dimy
 
 type :: poisson_fem
 sll_real64, dimension((nx-1)*(ny-1),(nx-1)*(ny-1)) :: A, M
@@ -25,10 +26,13 @@ sll_real64, dimension(0:nx,0:ny) :: phi
 sll_real64, dimension(0:nx,0:ny) :: rho
 sll_real64, dimension(:), pointer :: x, y
 sll_real64 :: dx, dy
-sll_int32 :: error
+sll_int32 :: error, mode
 
 SLL_ALLOCATE(x(-1:nx+1),error)  
 SLL_ALLOCATE(y(-1:ny+1),error) 
+
+dimx = 2 * sll_pi
+dimy = 2 * sll_pi
 
 dx = dimx / nx
 dy = dimy / ny
@@ -44,22 +48,42 @@ do j=1,ny
 enddo
 
 
+mode = 2
+do i = 1, nx
+   do j = 1, ny
+      phi(i,j) = mode * sin(mode*x(i)) * sin(mode*y(j))
+      rho(i,j) = 2_f64 * mode**3 * sin(mode*x(i))*sin(mode*y(j))
+      write(10,*) x(i), y(j), rho(i,j)
+   end do
+   write(10,*)
+end do
+close(10)
+
 call initialize(poisson, x, y)
 call solve(poisson, ex, ey, rho)
+
+do i = 1, nx
+   do j = 1, ny
+      write(11,*) x(i), y(j), ex(i,j),  mode**2*cos(mode*x(i))*sin(mode*y(j))
+      write(12,*) x(i), y(j), ey(i,j), -mode**2*sin(mode*x(i))*cos(mode*y(j))
+   end do
+   write(11,*)
+   write(12,*)
+end do
+close(11)
+close(12)
 
 end subroutine test
 
 
 subroutine initialize( this, x, y )
-sll_real64, dimension(-1:) :: x, y
- 
 type( poisson_fem ) :: this
-sll_real64 :: aux, ddx, ddy
-sll_int32 :: i, j, indice
+sll_real64, dimension(-1:) :: x, y
+sll_int32 :: i, j
 sll_int32 :: error
 
 sll_real64, dimension(4,4) :: Axelem, Ayelem, Melem
-sll_real64 :: dum, aux1, aux2
+sll_real64 :: dum
 sll_int32 :: Iel, info
 sll_int32, dimension(4) :: Isom
 
@@ -87,25 +111,24 @@ y(ny+1) = y(ny) + this%hy(0)
 
 !** Construction des matrices elementaires
 dum = 1.d0/6.d0
-Axelem(1,1)=2*dum ; Axelem(1,2)=-2*dum ; Axelem(1,3)=-dum ; Axelem(1,4)=dum ;
-Axelem(2,1)=-2*dum ; Axelem(2,2)=2*dum ; Axelem(2,3)=dum ; Axelem(2,4)=-dum ;
-Axelem(3,1)=-dum ; Axelem(3,2)=dum ; Axelem(3,3)=2*dum ; Axelem(3,4)=-2*dum ;
-Axelem(4,1)=dum ; Axelem(4,2)=-dum ; Axelem(4,3)=-2*dum ; Axelem(4,4)=2*dum ;
+Axelem(1,1)= 2*dum ; Axelem(1,2)=-2*dum ; Axelem(1,3)= - dum ; Axelem(1,4)=   dum ;
+Axelem(2,1)=-2*dum ; Axelem(2,2)= 2*dum ; Axelem(2,3)=   dum ; Axelem(2,4)= - dum ;
+Axelem(3,1)= - dum ; Axelem(3,2)=   dum ; Axelem(3,3)= 2*dum ; Axelem(3,4)=-2*dum ;
+Axelem(4,1)=   dum ; Axelem(4,2)=-  dum ; Axelem(4,3)=-2*dum ; Axelem(4,4)= 2*dum ;
 
-Ayelem(1,1)=2*dum ; Ayelem(1,2)=dum ; Ayelem(1,3)=-dum ; Ayelem(1,4)=-2*dum ;
-Ayelem(2,1)=dum ; Ayelem(2,2)=2*dum ; Ayelem(2,3)=-2*dum ; Ayelem(2,4)=-dum ;
-Ayelem(3,1)=-dum ; Ayelem(3,2)=-2*dum ; Ayelem(3,3)=2*dum ; Ayelem(3,4)=dum ;
-Ayelem(4,1)=-2*dum ; Ayelem(4,2)=-dum ; Ayelem(4,3)=dum ; Ayelem(4,4)=2*dum ;
+Ayelem(1,1)= 2*dum ; Ayelem(1,2)=   dum ; Ayelem(1,3)= - dum ; Ayelem(1,4)=-2*dum ;
+Ayelem(2,1)=   dum ; Ayelem(2,2)= 2*dum ; Ayelem(2,3)=-2*dum ; Ayelem(2,4)= - dum ;
+Ayelem(3,1)= - dum ; Ayelem(3,2)=-2*dum ; Ayelem(3,3)= 2*dum ; Ayelem(3,4)=   dum ;
+Ayelem(4,1)=-2*dum ; Ayelem(4,2)= - dum ; Ayelem(4,3)=   dum ; Ayelem(4,4)= 2*dum ;
 
 dum = 1.d0/36.d0
-Melem(1,1)=4*dum ; Melem(1,2)=2*dum ; Melem(1,3)=dum ; Melem(1,4)=2*dum ;
-Melem(2,1)=2*dum ; Melem(2,2)=4*dum ; Melem(2,3)=2*dum ; Melem(2,4)=dum ;
-Melem(3,1)=dum ; Melem(3,2)=2*dum ; Melem(3,3)=4*dum ; Melem(3,4)=2*dum ;
-Melem(4,1)=2*dum ; Melem(4,2)=dum ; Melem(4,3)=2*dum ; Melem(4,4)=4*dum ;
+Melem(1,1)=4*dum ; Melem(1,2)=2*dum ; Melem(1,3)=  dum ; Melem(1,4)=2*dum ;
+Melem(2,1)=2*dum ; Melem(2,2)=4*dum ; Melem(2,3)=2*dum ; Melem(2,4)=  dum ;
+Melem(3,1)=  dum ; Melem(3,2)=2*dum ; Melem(3,3)=4*dum ; Melem(3,4)=2*dum ;
+Melem(4,1)=2*dum ; Melem(4,2)=  dum ; Melem(4,3)=2*dum ; Melem(4,4)=4*dum ;
 
 
 this%A = 0.d0
-
 
 !** Contribution des mailles interieures
 do i=1,nx-2
@@ -257,4 +280,3 @@ end do
 end subroutine solve
 
 end program sll_poisson_2d_cart_fem
-
