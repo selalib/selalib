38bebe88ccc7113778cff431c1b5cdd34b4d08ca
Martin Campos Pinto <campos@ann.jussieu.fr>
2012-11-21 18:24:07 +0100

    added a CMakeLists.txt file for the ltpic_2d module, and corrected some syntax errors

--- prototype/src/linearly_transformed_pic/flow_base.F90
+++ prototype/src/linearly_transformed_pic/flow_base.F90
@@ -8,6 +8,7 @@
 
 module sll_flow_base
 #include "sll_working_precision.h"
+
   implicit none
 
   ! **************************************************************************
@@ -16,15 +17,17 @@ module sll_flow_base
   !
   ! **************************************************************************
 
-  type, abstract :: flow_base_class
-     sll_real64   :: dt
+  type, abstract :: flow_base_class   ! MCP -- is that a class ?
+     sll_real64  :: dt
    contains
-     procedure(flow_evaluate), pass(flow), deferred :: flow_at_xv ! should be 'flow_at_x' (see below)
+     procedure(flow_evaluate), pass(flow), deferred :: flow_at_xv ! MCP -- for arbitrary dimensions, should be 'flow_at_x'
   end type flow_base_class
+  
 
   abstract interface
-     ! MCP -- todo: use arguments (x,f_x), with arbitrary dimensions
+     ! MCP -- here one should use arguments (x,f_x) with arbitrary dimensions (maybe later)
      subroutine flow_evaluate(flow,x,v,f_x,f_v)
+       use sll_working_precision
        import flow_base_class
        class(flow_base_class)   :: flow
        sll_real64,intent(in)    :: x
