e4736e80a6e7efde2a8c841f9f5869d120eb445c
Martin CAMPOS PINTO <campos@ann.jussieu.fr>
2012-11-21 14:44:57 +0100

    some updates for ltpic module

--- prototype/src/linearly_transformed_pic/flow_base.F90
+++ prototype/src/linearly_transformed_pic/flow_base.F90
@@ -1,5 +1,10 @@
-! Essentially, the 'flow' has a single objective which is to provide
-! a uniform interface to a flow mapping in a given number of dimensions.
+!> \file flow_base.F90
+!> \namespace sll_flow_base
+!> \authors                    
+!> Martin CAMPOS PINTO (campos@ann.jussieu.fr) 
+!> \brief  
+!> Essentially, the 'flow' has a single objective which is to provide
+!> a uniform interface to flow mappings in arbitrary dimensions.
 
 module sll_flow_base
 #include "sll_working_precision.h"
@@ -11,15 +16,22 @@ module sll_flow_base
   !
   ! **************************************************************************
 
-  type, abstract :: flow_base
+  type, abstract :: flow_base_class
      sll_real64   :: dt
    contains
-     procedure( *** MCP - don't know what to write here *** ), deferred, pass :: flow_at_xv
-     ! need to declare the initialize procedure ??
-  end type flow_base
+     procedure(flow_evaluate), pass(flow), deferred :: flow_at_xv ! should be 'flow_at_x' (see below)
+  end type flow_base_class
 
-  ! do we need an interface ?
   abstract interface
+     ! MCP -- todo: use arguments (x,f_x), with arbitrary dimensions
+     subroutine flow_evaluate(flow,x,v,f_x,f_v)
+       import flow_base_class
+       class(flow_base_class)   :: flow
+       sll_real64,intent(in)    :: x
+       sll_real64,intent(in)    :: v
+       sll_real64,intent(out)   :: f_x
+       sll_real64,intent(out)   :: f_v
+     end subroutine flow_evaluate
   end interface
 
   
