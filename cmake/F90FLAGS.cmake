a5a78fb3bb143c7dd1f18898a06446a43304d56e
Pierre Navaro <navaro@math.cnrs.fr>
2012-11-29 21:03:56 +0100

    change gysela files from f90 to F90

--- cmake/F90FLAGS.cmake
+++ cmake/F90FLAGS.cmake
@@ -2,27 +2,22 @@ IF(CMAKE_Fortran_COMPILER MATCHES "gfortran")
 
    ADD_DEFINITIONS(-DGFORTRAN)
 
-   IF(CMAKE_BUILD_TYPE MATCHES Release)
-      SET(CMAKE_Fortran_FLAGS "-fomit-frame-pointer -Wall -cpp -ffree-line-length-none -fall-intrinsics -O3")
-   ELSE()
-      SET(CMAKE_Fortran_FLAGS "-g -Wall -cpp -ffree-line-length-none -fall-intrinsics -fbounds-check -fbacktrace -ffpe-trap=zero,overflow,underflow -O0")
-   ENDIF()
+   SET(CMAKE_Fortran_FLAGS_RELEASE "-fomit-frame-pointer -Wall -cpp -ffree-line-length-none -fall-intrinsics -O3")
+   SET(CMAKE_Fortran_FLAGS_DEBUG "-g -Wall -cpp -ffree-line-length-none -fall-intrinsics -fbounds-check -fbacktrace -ffpe-trap=zero,overflow,underflow -O0")
 
 ELSEIF(CMAKE_Fortran_COMPILER MATCHES "ifort")
 
    ADD_DEFINITIONS(-DINTEL)
 
-   IF(CMAKE_BUILD_TYPE MATCHES Release)
-      SET(CMAKE_Fortran_FLAGS "-O3 -xHost -ip")
-   ELSE()
-      SET(CMAKE_Fortran_FLAGS "-O0 -check all -fpe0 -traceback -ftrapuv ")
-   ENDIF()
+      SET(CMAKE_Fortran_FLAGS_DEBUG "-O3 -xHost -ip")
+      SET(CMAKE_Fortran_FLAGS_RELEASE "-O0 -check all -fpe0 -traceback -ftrapuv ")
 
 ELSEIF(CMAKE_Fortran_COMPILER MATCHES "xlf")
 
    ADD_DEFINITIONS(-DIBM)
 
-   SET(CMAKE_Fortran_FLAGS "-qextname=flush -qthreaded -qhalt=e -qxlf2003=polymorphic")
+   SET(CMAKE_Fortran_FLAGS_DEBUG "-qextname=flush -qthreaded -qhalt=e -qxlf2003=polymorphic")
+   SET(CMAKE_Fortran_FLAGS_RELEASE "-qextname=flush -qthreaded -qhalt=e -qxlf2003=polymorphic")
 
 ELSE()
 
