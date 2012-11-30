f0850099e91f838ee30fdf70a17b4fe50cbd0e96
Pierre Navaro <navaro@math.unistra.fr>
2012-11-30 14:30:31 +0100

    Fix wrong flag for release

--- prototype/src/CMakeModules/FortranConfig.cmake
+++ prototype/src/CMakeModules/FortranConfig.cmake
@@ -61,7 +61,7 @@ MESSAGE(STATUS "Fortran_COMPILER:${Fortran_COMPILER}")
 IF(Fortran_COMPILER STREQUAL "GFORTRAN")
    ADD_DEFINITIONS(-DGFORTRAN)
 
-   SET(CMAKE_Fortran_FLAGS_RELEASE "-fomit-frame-pointer -Wall -cpp -ffree-line-length-none -fall-intrinsics -O3")
+   SET(CMAKE_Fortran_FLAGS_RELEASE "-w -ffree-line-length-none -fall-intrinsics -O3")
    SET(CMAKE_Fortran_FLAGS_DEBUG "-g -Wall -cpp -pedantic -ffree-line-length-none -std=f2003 -fall-intrinsics -fbounds-check -fbacktrace -ffpe-trap=zero,overflow,underflow -O0")
 
 ELSEIF(Fortran_COMPILER STREQUAL "INTEL")
