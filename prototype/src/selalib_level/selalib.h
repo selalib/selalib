494cf90861f2a460b2d2cb7675bc3672f0cd00a5
Pierre Navaro <navaro@math.cnrs.fr>
2012-12-05 11:02:44 +0100

    try to fix build error in pic2d

--- prototype/src/selalib_level/selalib.h
+++ prototype/src/selalib_level/selalib.h
@@ -6,12 +6,12 @@
 #include "sll_working_precision.h"
 use numeric_constants
 use sll_misc_utils
-use sll_io, only:sll_new_file_id
 use sll_collective
 use sll_ascii_io
 use sll_xml_io
 use sll_gnuplot
 use sll_xdmf
+use sll_io, only:sll_new_file_id
 
 #define MPI_MASTER 0
 
