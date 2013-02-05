64364fdc1020a3bfb88d2347aa8e94e81d6c6ff0
Blanchard <raphael.blanchard@inria.com>
2013-02-05 16:49:31 +0100

    Update lagrange interpolation

---
+++ prototype/src/lagrange_interpolation/unit_test_lagrange_interpolation.F90
@@ -0,0 +1,37 @@
+program test_lagrange
+#include "sll_working_precision.h"
+ use sll_lagrange_interpolation
+implicit none
+sll_int32 :: i,j
+sll_real64 :: x,res,li,diff
+sll_real64,dimension(1:4) ::xi,yi,coord
+
+coord(1)=1.0_f64
+coord(2)=2.5_f64
+coord(3)=3.2_f64
+coord(4)=4.5_f64
+diff=0.0_f64
+do i=1,4
+ xi(i)=i
+ yi(i)=f(xi(i))
+end do
+do i=1,4
+ res=lagrange_interpolation(coord(i),xi,yi,3)
+ print*,"interpolated value = ", res, " , Correct value = ",f(coord(i))
+ diff=diff+(f(coord(i))-res)
+end do
+
+if(diff<1e-5) then
+ print*,"Lagrange interpolation is OK"
+else
+ print*,"Error of lagrange interpolation"
+end if
+
+contains 
+
+function f(x)
+sll_real64 x,f
+f=3*x*x+2*x+1.0_f64
+end function
+
+end program
