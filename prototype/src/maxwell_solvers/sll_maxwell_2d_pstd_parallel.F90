c7cdc832deb40d917d66d195f5bdd1bf7408934a
Pierre Navaro <navaro@math.cnrs.fr>
2013-02-06 11:38:24 +0100

    improve maxwell pstd solver

--- prototype/src/maxwell_solvers/sll_maxwell_2d_pstd_parallel.F90
+++ prototype/src/maxwell_solvers/sll_maxwell_2d_pstd_parallel.F90
@@ -1,5 +1,5 @@
 !**************************************************************
-!  Copyright INRIA
+!  Copyright INRIA, CNRS
 !  Authors : 
 !     Pierre Navaro 
 !  
@@ -46,45 +46,45 @@ module sll_maxwell_2d_periodic_cartesian_par
 #include "misc_utils.h"
 #include "sll_assert.h"
 
-  use, intrinsic :: iso_c_binding
-  use remapper
-  use sll_fft
-  use numeric_constants
-  use sll_collective
-  use sll_maxwell
+use, intrinsic :: iso_c_binding
+use remapper
+use sll_fft
+use numeric_constants
+use sll_collective
+use sll_maxwell
 
-  implicit none
+implicit none
 
-  !> Maxwell solver object
-  type maxwell_2d_periodic_plan_cartesian_par
-     sll_int32                           :: ncx         !< number of cells x
-     sll_int32                           :: ncy         !< number of cells y
-     type(C_PTR)                         :: fwx         !< fftw plan forward x
-     type(C_PTR)                         :: fwy         !< fftw plan forward y
-     type(C_PTR)                         :: bwx         !< fftw plan backward x
-     type(C_PTR)                         :: bwy         !< fftw plan backward y
-     type(layout_2D),  pointer           :: layout_x    !< layout sequential in x
-     type(layout_2D),  pointer           :: layout_y    !< layout sequential in y
-     sll_real64, dimension(:), pointer   :: d_dx        !< x derivative
-     sll_real64, dimension(:), pointer   :: d_dy        !< y derivative
-     sll_comp64, dimension(:), pointer   :: fft_x_array !< fft x transform
-     sll_comp64, dimension(:), pointer   :: fft_y_array !< fft y transform
-     type(remap_plan_2D_real64), pointer :: rmp_xy      !< remap x->y pointer
-     type(remap_plan_2D_real64), pointer :: rmp_yx      !< remap y->x pointer
-     sll_real64                          :: e_0         !< electric conductibility
-     sll_real64                          :: mu_0        !< magnetic permeability
-     type(C_PTR)                         :: p_x_array   !< x pointer to memory
-     type(C_PTR)                         :: p_y_array   !< y pointer to memory
-     sll_real64, dimension(:,:), pointer :: fz_x        !< array sequential in x
-     sll_real64, dimension(:,:), pointer :: fz_y        !< array sequential in y
-     sll_real64, dimension(:), pointer   :: kx          !< x wave number
-     sll_real64, dimension(:), pointer   :: ky          !< y wave number
-  end type maxwell_2d_periodic_plan_cartesian_par
+!> Maxwell solver 2D object, PSTD scheme
+type maxwell_2d_periodic_plan_cartesian_par
+   sll_int32                           :: ncx         !< number of cells x
+   sll_int32                           :: ncy         !< number of cells y
+   type(C_PTR)                         :: fwx         !< fftw plan forward x
+   type(C_PTR)                         :: fwy         !< fftw plan forward y
+   type(C_PTR)                         :: bwx         !< fftw plan backward x
+   type(C_PTR)                         :: bwy         !< fftw plan backward y
+   type(layout_2D),  pointer           :: layout_x    !< layout sequential in x
+   type(layout_2D),  pointer           :: layout_y    !< layout sequential in y
+   sll_real64, dimension(:), pointer   :: d_dx        !< x derivative
+   sll_real64, dimension(:), pointer   :: d_dy        !< y derivative
+   sll_comp64, dimension(:), pointer   :: fft_x_array !< fft x transform
+   sll_comp64, dimension(:), pointer   :: fft_y_array !< fft y transform
+   type(remap_plan_2D_real64), pointer :: rmp_xy      !< remap x->y pointer
+   type(remap_plan_2D_real64), pointer :: rmp_yx      !< remap y->x pointer
+   sll_real64                          :: e_0         !< electric conductibility
+   sll_real64                          :: mu_0        !< magnetic permeability
+   type(C_PTR)                         :: p_x_array   !< x pointer to memory
+   type(C_PTR)                         :: p_y_array   !< y pointer to memory
+   sll_real64, dimension(:,:), pointer :: fz_x        !< array sequential in x
+   sll_real64, dimension(:,:), pointer :: fz_y        !< array sequential in y
+   sll_real64, dimension(:), pointer   :: kx          !< x wave number
+   sll_real64, dimension(:), pointer   :: ky          !< y wave number
+end type maxwell_2d_periodic_plan_cartesian_par
 
 include 'fftw3.f03'
 
-  sll_int32, private         :: i
-  sll_int32, private         :: j
+sll_int32, private :: i
+sll_int32, private :: j
 
 contains
 
