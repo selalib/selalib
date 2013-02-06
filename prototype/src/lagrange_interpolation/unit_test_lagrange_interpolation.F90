e40029fa79c2858854ca29a6f8b7da482f5fbf80
Raphael Blanchard <raphael.blanchard@inria.fr>
2013-02-06 17:19:37 +0100

    Update lagrange interpolation

--- prototype/src/lagrange_interpolation/unit_test_lagrange_interpolation.F90
+++ prototype/src/lagrange_interpolation/unit_test_lagrange_interpolation.F90
@@ -3,8 +3,8 @@ program test_lagrange
  use sll_lagrange_interpolation
 implicit none
 sll_int32 :: i,j
-sll_real64 :: x,res,li,diff
-sll_real64,dimension(1:4) ::xi,yi,coord
+sll_real64 :: res,diff
+sll_real64,dimension(1:4) ::xi,yi,coord,coef
 
 coord(1)=1.0_f64
 coord(2)=2.5_f64
@@ -27,11 +27,30 @@ else
  print*,"Error of lagrange interpolation"
 end if
 
+diff=0.0_f64
+coef=lagrange_interpolation(xi,yi,3)
+do i=1,4
+ res=calcul_newton(coord(i),xi,coef,3)
+ print*,"interpolated value = ", res, " , Correct value = ",f(coord(i))
+ diff=diff+(f(coord(i))-res)
+end do
+
+if(diff<1e-5) then
+ print*,"Newton Lagrange interpolation is OK"
+else
+ print*,"Error of Newton lagrange interpolation"
+end if
+
 contains 
 
 function f(x)
-sll_real64 x,f
+sll_real64 :: x,f
 f=3*x*x+2*x+1.0_f64
+end function
+
+function testf(x)
+sll_real64 :: x,testf
+testf=1+x*x
 end function
 
 end program
