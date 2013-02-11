78de4d8412dee7302328dfbd91afe88642f15a53
Blanchard <raphael.blanchard@inria.com>
2013-02-11 09:24:31 +0100

    Update lagrange interpolation

---
+++ prototype/src/lagrange_interpolation/lagrange_interpolation_test.F90
@@ -0,0 +1,190 @@
+module sll_lagrange_interpolation
+#include "sll_working_precision.h"
+implicit none
+
+  interface lagrange_interpolation
+    module procedure lagrange_interpolation_1d, lagrange_interpolation_2d, &
+                     neville_lagrange_interpolation, newton_lagrange_interpolation, &
+                     lagrange_interpolation_3d
+  end interface
+
+contains
+
+function lagrange_interpolation_1d(x,xi,fi,degree) result (res)
+sll_int32 ::i,j
+sll_int32, intent(in) :: degree
+sll_real64 :: li,res
+sll_real64,intent(in) :: x
+sll_real64,dimension(1:degree+1),intent(in) ::xi,fi
+
+li=1.0_f64
+res=0.0_f64
+do i=1,degree+1
+ do j=1,degree+1
+  if(j/=i)then
+   li=li*(x-xi(j))/(xi(i)-xi(j))
+  end if
+ end do
+ res=res+fi(i)*li
+ li=1.0_f64
+end do
+end function
+
+function newton_lagrange_interpolation(xi,fi,degree) result(coef)
+sll_int32 ::i,j
+sll_int32, intent(in) :: degree
+sll_real64,dimension(1:degree+1),intent(in) ::xi,fi
+sll_real64,dimension(1:degree+1) :: coef
+
+coef=fi
+do i=1,degree+1
+ do j=degree+1,i+1,-1
+  coef(j)=(coef(j)-coef(j-1))/(xi(j)-xi(j-i))
+ end do
+end do
+
+end function
+
+function compute_newton_interpolation(x,xi,coef,degree) result(res)
+sll_int32 :: i
+sll_int32,intent(in) :: degree
+sll_real64,intent(in) :: x
+sll_real64 :: res
+sll_real64,dimension(1:degree+1),intent(in) :: coef,xi
+sll_real64,dimension(1:degree+1) :: base
+
+base(1)=1.0_f64
+res=coef(1)
+do i=2,degree+1
+ base(i)=base(i-1)*(x-xi(i-1))
+ res=res+base(i)*coef(i)
+end do
+
+end function
+
+
+! function newton_lagrange_interpolation(x,xi,yi,degree) result(res)
+! sll_int32 ::i,j
+! sll_int32, intent(in) :: degree
+! sll_real64 :: res,li
+! sll_real64,intent(in) :: x
+! sll_real64,dimension(1:degree+1),intent(in) ::xi,yi
+! sll_real64,dimension(1:degree+1) :: coef,base
+! 
+! coef=yi
+! do i=1,degree+1
+!  do j=degree+1,i+1,-1
+!   coef(j)=(coef(j)-coef(j-1))/(xi(j)-xi(j-i))
+!  end do
+! end do
+! 
+! base(1)=1.0_f64
+! do i=2,degree+1
+!  base(i)=base(i-1)*(x-xi(i-1))
+! end do
+! 
+! res=0
+! do i=1,degree+1
+!  res=res+base(i)*coef(i)
+! end do
+! print*,""
+! print*,"coef",coef
+! print*,"base",base
+! 
+! end function
+
+
+function neville_lagrange_interpolation(x,xi,fi,degree,epsilon) result(res)
+sll_int32 ::i,j
+sll_int32, intent(in) :: degree
+sll_real64 :: res,li
+sll_real64,intent(in) :: x,epsilon
+sll_real64,dimension(1:degree+1),intent(in) ::xi,fi
+
+print*,"pas encore implementé"
+end function
+
+
+function lagrange_interpolation_2d(x,y,xi,yi,fi,degreex,degreey) result(res)
+sll_int32 ::i,j,k
+sll_int32, intent(in) :: degreex,degreey
+sll_real64 :: res
+sll_real64, intent(in) :: x,y
+sll_real64,dimension(1:degreex+1) :: li
+sll_real64,dimension(1:degreey+1) :: lj
+sll_real64,dimension(1:degreex+1),intent(in) ::xi
+sll_real64,dimension(1:degreey+1),intent(in) ::yi
+sll_real64,dimension(1:(degreex+1),1:(degreey+1)),intent(in) ::fi
+
+li=1.0_f64
+lj=1.0_f64
+res=0.0_f64
+do i=1,degreex+1
+ do k=1,degreex+1
+  if(k/=i)then
+   li(i)=li(i)*(x-xi(k))/(xi(i)-xi(k))
+  end if
+ end do
+end do
+do j=1,degreey+1
+ do k=1,degreey+1
+  if(k/=j)then
+   lj(j)=lj(j)*(y-yi(k))/(yi(j)-yi(k))
+  end if
+ end do
+end do
+do i=1,degreex+1
+ do j=1,degreey+1
+  res=res+fi(i,j)*li(i)*lj(j)
+ end do
+end do
+end function
+
+function lagrange_interpolation_3d(x,y,z,xi,yi,zi,fi,degreex,degreey,degreez) result(res)
+sll_int32 ::i,j,k,l
+sll_int32, intent(in) :: degreex,degreey,degreez
+sll_real64 :: res
+sll_real64, intent(in) :: x,y,z
+sll_real64,dimension(1:degreex+1) :: li
+sll_real64,dimension(1:degreey+1) :: lj
+sll_real64,dimension(1:degreez+1) :: lk
+sll_real64,dimension(1:degreex+1),intent(in) ::xi
+sll_real64,dimension(1:degreey+1),intent(in) ::yi
+sll_real64,dimension(1:degreez+1),intent(in) ::zi
+sll_real64,dimension(1:(degreex+1),1:(degreey+1),1:(degreez+1)),intent(in) ::fi
+
+li=1.0_f64
+lj=1.0_f64
+lk=1.0_f64
+res=0.0_f64
+do i=1,degreex+1
+ do l=1,degreex+1
+  if(l/=i)then
+   li(i)=li(i)*(x-xi(l))/(xi(i)-xi(l))
+  end if
+ end do
+end do
+do j=1,degreey+1
+ do l=1,degreey+1
+  if(l/=j)then
+   lj(j)=lj(j)*(y-yi(l))/(yi(j)-yi(l))
+  end if
+ end do
+end do
+do k=1,degreez+1
+ do l=1,degreez+1
+  if(l/=k)then
+   lk(k)=lk(k)*(z-zi(l))/(zi(k)-zi(l))
+  end if
+ end do
+end do
+do i=1,degreex+1
+ do j=1,degreey+1
+  do k=1,degreez+1
+   res=res+fi(i,j,k)*li(i)*lj(j)*lk(k)
+  end do
+ end do
+end do
+end function
+
+end module 
