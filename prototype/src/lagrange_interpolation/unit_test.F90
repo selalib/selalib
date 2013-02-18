98a357aa974ae9a208a71297bf99708eadc6333c
Raphael Blanchard <raphael.blanchard@inria.fr>
2013-02-18 16:19:15 +0100

    Update lagrange_interpolation

--- prototype/src/lagrange_interpolation/unit_test.F90
+++ prototype/src/lagrange_interpolation/unit_test.F90
@@ -12,24 +12,24 @@ type(sll_lagrange_interpolation_1D),pointer ::l_i
 degree=3
 allocate(xi(1:degree+1))
 allocate(fi(1:degree+1))
-allocate(coord(1:degree+1))
+allocate(coord(1:degree+2))
 !data initialization
 coord(1)=1.3_f64
 coord(2)=2.5_f64
 coord(3)=3.2_f64
 coord(4)=4.5_f64
+coord(5)=0.32_f64
 do i=1,4
  xi(i)=i
  fi(i)=f(xi(i))
 end do 
 diff=0.0_f64
-
-l_i => new_lagrange_interpolation_1D(xi,fi,degree)
-
-do i=1,degree+1
- call compute_lagrange_interpolation_1D(coord(i),l_i)
- print*,"interpolated value = ", l_i%result, " , Correct value = ",f(coord(i))
- diff=max(diff,abs(f(coord(i))-l_i%result))
+l_i => new_lagrange_interpolation_1D(xi,fi,degree,5)
+call compute_lagrange_interpolation_1D(xi,l_i)
+call interpolate_array_values(coord,l_i)
+do i=1,5
+ print*,"interpolated value = ", l_i%data_out(i), " , Correct value = ",f(coord(i))
+ diff=max(diff,abs(f(coord(i))-l_i%data_out(i)))
 end do
 
 if(diff<1e-10) then
