1b958b159c3a1dcc2bf27c40f260c5740c5691b4
Blanchard <raphael.blanchard@inria.com>
2013-03-12 11:29:09 +0100

    Update lagrange interpolation

--- prototype/src/interpolators/sll_lagrange_interpolator_1d.F90
+++ prototype/src/interpolators/sll_lagrange_interpolator_1d.F90
@@ -31,62 +31,66 @@ implicit none
  end interface
 contains  !**********************************************************
  
-subroutine initialize_li1d_interpolator(interpolator,xi,fi,degree,num_pts,alpha,bc_type)
+subroutine initialize_li1d_interpolator(interpolator,num_pts,xmin,xmax,bc_type,d)
   class(lagrange_1d_interpolator), intent(inout) :: interpolator
-    sll_int32, intent(in)                        :: degree,num_pts
-    sll_real64, intent(in)                       :: alpha
-    sll_real64,dimension(1:num_pts+1),intent(in) :: xi,fi
-    sll_int32, intent(in), optional              :: bc_type
+    sll_int32, intent(in)                        :: d,num_pts,bc_type
+    sll_real64, intent(in)                       :: xmin,xmax
 
-    if(present(bc_type))then
-        interpolator%bc_type = bc_type
-    else
-        interpolator%bc_type = 0
-    end if
     interpolator%lagrange => new_lagrange_interpolation_1D( &
-           xi, &
-           fi, &
-           degree, &
            num_pts, &
-           alpha, &
-           bc_type)
+           xmin, &
+           xmax, &
+           bc_type, &
+           d)
 end subroutine initialize_li1d_interpolator
 
-subroutine compute_interpolants_li1d(interpolator, data_array)
-  class(lagrange_1d_interpolator), intent(inout) :: interpolator
-  sll_real64, dimension(:), intent(in)           :: data_array
-
-  call compute_lagrange_interpolation_1D(data_array,interpolator%lagrange)
-end subroutine compute_interpolants_li1d
-
-function interpolate_array_li1d(this, num_points, data, coordinates) result(data_out)
+function interpolate_array_disp_li1d(this, num_points, data, alpha) result(data_out)
   class(lagrange_1d_interpolator), intent(in)     :: this
+  sll_real64, intent(in) :: alpha
   sll_int32, intent(in)  :: num_points    ! size of output array
   sll_real64, dimension(:), intent(in) :: data  ! data to be interpolated points where output is desired
-  sll_real64, dimension(:), intent(in) :: coordinates  
   sll_real64, dimension(1:num_points)    :: data_out
 
-  call compute_lagrange_interpolation_1D(coordinates,this%lagrange)
-  call interpolate_array_values(data,this%lagrange)
-  data_out=this%lagrange%data_out
-end function interpolate_array_li1d
+call compute_lagrange_interpolation_1D(data,alpha,this%lagrange)
+call interpolate_array_values(this%lagrange)
+data_out=this%lagrange%data_out
 
-
-DEFINE_NULL_INTERP_1D_ARRAY_SUB(lagrange_1d_interpolator, interpolate_array_values_li1d)
-DEFINE_NULL_INTERP_1D_ARRAY_SUB(lagrange_1d_interpolator, interpolate_array_derivatives_li1d)
-DEFINE_NULL_INTERPOLATE_1D_DISP(lagrange_1d_interpolator, interpolate_array_disp_li1d)
-DEFINE_NULL_INTERP_1D_POINTER_SUB(lagrange_1d_interpolator, interpolate_pointer_derivatives_li1d)
-DEFINE_NULL_INTERP_ONE_ARG_MSG(lagrange_1d_interpolator, interpolate_derivative_eta1_li1d)
-DEFINE_NULL_INTERP_1D_POINTER_SUB(lagrange_1d_interpolator, interpolate_pointer_values_li1d)
-DEFINE_NULL_INTERP_ONE_ARG_MSG(lagrange_1d_interpolator, interpolate_value_li1d)
-DEFINE_NULL_RECONSTRUCT_1D_ARRAY(lagrange_1d_interpolator, reconstruct_array_li1d)
-!DEFINE_NULL_INTERP_1D_ARRAY_MSG(lagrange_1d_interpolator, compute_interpolants_li1d)
-!DEFINE_NULL_INTERP_1D_ARRAY(lagrange_1d_interpolator, interpolate_array_li1d)
+end function interpolate_array_disp_li1d
 
 subroutine delete_li1d (obj)
   class(lagrange_1d_interpolator) :: obj
   call delete(obj%lagrange)
 end subroutine delete_li1d
+
+DEFINE_NULL_INTERP_1D_ARRAY_SUB(lagrange_1d_interpolator, interpolate_array_values_li1d)
+DEFINE_NULL_INTERP_1D_ARRAY_SUB(lagrange_1d_interpolator, interpolate_array_derivatives_li1d)
+DEFINE_NULL_INTERP_1D_POINTER_SUB(lagrange_1d_interpolator, interpolate_pointer_derivatives_li1d)
+DEFINE_NULL_INTERP_ONE_ARG_MSG(lagrange_1d_interpolator, interpolate_derivative_eta1_li1d)
+DEFINE_NULL_INTERP_1D_POINTER_SUB(lagrange_1d_interpolator, interpolate_pointer_values_li1d)
+DEFINE_NULL_INTERP_ONE_ARG_MSG(lagrange_1d_interpolator, interpolate_value_li1d)
+DEFINE_NULL_RECONSTRUCT_1D_ARRAY(lagrange_1d_interpolator, reconstruct_array_li1d)
+DEFINE_NULL_INTERP_1D_ARRAY(lagrange_1d_interpolator, interpolate_array_li1d)
+DEFINE_NULL_INTERP_1D_ARRAY_MSG(lagrange_1d_interpolator, compute_interpolants_li1d)
+!DEFINE_NULL_INTERPOLATE_1D_DISP(lagrange_1d_interpolator, interpolate_array_disp_li1d)
+
+! function interpolate_array_li1d(this, num_points, data, coordinates) result(data_out)
+!   class(lagrange_1d_interpolator), intent(in)     :: this
+!   sll_int32, intent(in)  :: num_points    ! size of output array
+!   sll_real64, dimension(:), intent(in) :: data  ! data to be interpolated points where output is desired
+!   sll_real64, dimension(:), intent(in) :: coordinates  
+!   sll_real64, dimension(1:num_points)    :: data_out
+! 
+!   call compute_lagrange_interpolation_1D(coordinates,this%lagrange)
+!   call interpolate_array_values(data,this%lagrange)
+!   data_out=this%lagrange%data_out
+! end function interpolate_array_li1d
+
+! subroutine compute_interpolants_li1d(interpolator, data_array)
+!   class(lagrange_1d_interpolator), intent(inout) :: interpolator
+!   sll_real64, dimension(:), intent(in)           :: data_array
+! 
+!   call compute_lagrange_interpolation_1D(data_array,interpolator%lagrange)
+! end subroutine compute_interpolants_li1di
 
 
 end module sll_lagrange_interpolator_1d
--- prototype/src/lagrange_interpolation/sll_lagrange_interpolation.F90
+++ prototype/src/lagrange_interpolation/sll_lagrange_interpolation.F90
@@ -11,12 +11,12 @@ implicit none
    sll_int32                            :: bc_type
    sll_int32                            :: indice_decalage
    sll_real64                           :: alpha
+   sll_real64                           :: xmin
+   sll_real64                           :: xmax
    sll_real64, dimension(:,:), pointer  :: wj
    sll_real64, dimension(:), pointer    :: xiadd 
    sll_real64, dimension(:), pointer    :: fiadd 
    sll_real64, dimension(:), pointer    :: xi 
-   sll_real64, dimension(:), pointer    :: x
-   sll_real64, dimension(:), pointer    :: fi ! fi(i)=function(xi(i))
    sll_real64, dimension(:), pointer    :: data_out !result=p(x) where p is the polynomial of interpolation
  end type sll_lagrange_interpolation_1D
 
@@ -29,54 +29,50 @@ end interface
 contains  !*****************************************************************************
 
 
-function new_lagrange_interpolation_1D(xi,fi,d,num_points,alpha,bc_type)
+function new_lagrange_interpolation_1D(num_points,xmin,xmax,bc_type,d)
 type(sll_lagrange_interpolation_1D), pointer :: new_lagrange_interpolation_1D
 sll_int32 ::i,j,ierr
 sll_int32,intent(in) :: d, num_points
 sll_int32,intent(in),optional :: bc_type
-sll_real64 :: alpha
-sll_real64,dimension(1:num_points+1),intent(in) :: xi,fi
+sll_real64 :: xmin,xmax
 
 SLL_ALLOCATE( new_lagrange_interpolation_1D, ierr )
 
-if(present(bc_type))then
- new_lagrange_interpolation_1D%bc_type=bc_type
-else
- new_lagrange_interpolation_1D%bc_type=0
-end if
 
-SLL_ALLOCATE(new_lagrange_interpolation_1D%xi(num_points),ierr)
 SLL_ALLOCATE(new_lagrange_interpolation_1D%xiadd(num_points+2*d-1),ierr)
 SLL_ALLOCATE(new_lagrange_interpolation_1D%fiadd(num_points+2*d-1),ierr)
-SLL_ALLOCATE(new_lagrange_interpolation_1D%fi(num_points),ierr)
 SLL_ALLOCATE(new_lagrange_interpolation_1D%wj(2*d,num_points),ierr)
-SLL_ALLOCATE(new_lagrange_interpolation_1D%x(num_points),ierr)
 SLL_ALLOCATE(new_lagrange_interpolation_1D%data_out(num_points),ierr)
 new_lagrange_interpolation_1D%d=d
+new_lagrange_interpolation_1D%xmin=xmin
+new_lagrange_interpolation_1D%xmax=xmax
 new_lagrange_interpolation_1D%num_points=num_points
-new_lagrange_interpolation_1D%xi=xi
-new_lagrange_interpolation_1D%fi=fi
-new_lagrange_interpolation_1D%alpha=alpha
 end function new_lagrange_interpolation_1D
 
 !compute donne un tableau avec les wj, qui permettent de calculer le polynome
-subroutine compute_lagrange_interpolation_1D(xi,lagrange)
+subroutine compute_lagrange_interpolation_1D(fi,alpha,lagrange)
 type(sll_lagrange_interpolation_1D), pointer :: lagrange
 sll_int32 :: i,j,k,indice_decalage
 sll_real64 :: xij,xii,h
-sll_real64,dimension(1:lagrange%num_points),intent(in) :: xi
+sll_real64, intent(in) :: alpha
+sll_real64,dimension(1:lagrange%num_points) :: xi
+sll_real64,dimension(1:lagrange%num_points),intent(in) :: fi
 sll_real64,dimension(1:lagrange%d*2-1+lagrange%num_points) :: xiadd,fiadd
 sll_real64,dimension(1:2*lagrange%d,1:lagrange%num_points) :: wj
 
-!calcul de h
-h=xi(2)-xi(1)
-!calcul d'indice_decalage en fonction de alpha
-if(lagrange%alpha<0)then
-indice_decalage=lagrange%alpha/h-1
-else
-indice_decalage=lagrange%alpha/h
-end if
 
+!compute h
+h=(lagrange%xmax-lagrange%xmin)/(lagrange%num_points-1)
+!compute xi
+do i=0,lagrange%num_points-1
+xi(i+1)=lagrange%xmin+i*h
+end do
+!calcul d'indice_decalage en fonction de alpha
+if(alpha<0)then
+indice_decalage=alpha/h-1
+else
+indice_decalage=alpha/h
+end if
 
 select case(lagrange%bc_type)
 case (PERIODIC_LAGRANGE)
@@ -84,7 +80,7 @@ case (PERIODIC_LAGRANGE)
   !remplissage de xiadd et fiadd en ajoutant les noeuds fictifs à xi
   do i=1,lagrange%num_points+2*lagrange%d-1
   xiadd(i)=xi(modulo(i-lagrange%d+indice_decalage,lagrange%num_points)+1)
-  fiadd(i)=lagrange%fi(modulo(i-lagrange%d+indice_decalage,lagrange%num_points)+1)
+  fiadd(i)=fi(modulo(i-lagrange%d+indice_decalage,lagrange%num_points)+1)
   end do
 
 case (HERMITE_LAGRANGE)
@@ -103,11 +99,11 @@ case (HERMITE_LAGRANGE)
   j=1
   do i=1,lagrange%num_points+2*lagrange%d-1
    if(xiadd(i).lt.xi(1))then
-    fiadd(i)=lagrange%fi(1)
+    fiadd(i)=fi(1)
    else if(xiadd(i).gt.xi(lagrange%num_points))then
-    fiadd(i)=lagrange%fi(lagrange%num_points)
+    fiadd(i)=fi(lagrange%num_points)
    else
-    fiadd(i)=lagrange%fi(j)
+    fiadd(i)=fi(j)
     j=j+1
    end if
   end do
@@ -135,22 +131,26 @@ end do
 lagrange%wj=wj
 lagrange%xiadd=xiadd
 lagrange%fiadd=fiadd
+lagrange%alpha=alpha
 
 end subroutine compute_lagrange_interpolation_1D
 
 
-subroutine interpolate_array_values(x,lagrange)
+subroutine interpolate_array_values(lagrange)
 type(sll_lagrange_interpolation_1D), pointer :: lagrange
 sll_int32 ::j,k
-sll_real64 :: sum1,sum2
-sll_real64, dimension(1:lagrange%num_points), intent(in) :: x
+sll_real64 :: sum1,sum2,xk,h
+sll_real64, dimension(1:lagrange%num_points) :: x
+
+h=(lagrange%xmax-lagrange%xmin)/(lagrange%num_points-1)
 
 do k=1,lagrange%num_points
  sum1=0.0_f64
  sum2=0.0_f64
+ xk=lagrange%xmin+(k-1)*h+lagrange%alpha
  do j=1,lagrange%d*2
-  sum1=sum1+lagrange%fiadd(k-1+j)*lagrange%wj(j,k)/(x(k)-lagrange%xiadd(k-1+j))
-  sum2=sum2+lagrange%wj(j,k)/(x(k)-lagrange%xiadd(k-1+j))
+  sum1=sum1+lagrange%fiadd(k-1+j)*lagrange%wj(j,k)/(xk-lagrange%xiadd(k-1+j))
+  sum2=sum2+lagrange%wj(j,k)/(xk-lagrange%xiadd(k-1+j))
  end do
  lagrange%data_out(k)=sum1/sum2
 end do
@@ -162,8 +162,6 @@ subroutine delete_lagrange_interpolation_1D( lagrange_interpolation )
 type(sll_lagrange_interpolation_1D), pointer :: lagrange_interpolation
 sll_int32                    :: ierr
   SLL_ASSERT( associated(lagrange_interpolation) )
-  SLL_DEALLOCATE( lagrange_interpolation%xi, ierr )
-  SLL_DEALLOCATE( lagrange_interpolation%fi, ierr )
   SLL_DEALLOCATE( lagrange_interpolation%wj, ierr )
   SLL_DEALLOCATE( lagrange_interpolation, ierr )
 end subroutine delete_lagrange_interpolation_1D
