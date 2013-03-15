7686e83fd3d6dbffbaf4225ab008e04e338ff0ef
Edwin CHACON-GOLCHER <chacongolcher@math.unistra.fr>
2013-03-15 13:52:44 +0100

    finished changes to start simulations from an input file, added changes to existing unit tests

--- prototype/src/simulation/unit_test_4d.F90
+++ prototype/src/simulation/unit_test_4d.F90
@@ -8,18 +8,25 @@ program vlasov_poisson_4d
   use sll_collective
   implicit none
 
+  character(len=256) :: filename
+  character(len=256) :: filename_local
   type(sll_simulation_4d_vlasov_poisson_cart) :: simulation
 
   print *, 'Booting parallel environment...'
   call sll_boot_collective() ! Wrap this up somewhere else
 
-  call simulation%init_from_file("vpsim4d_input.txt")
+  ! In this test, the name of the file to open is provided as a command line
+  ! argument.
+  call getarg(1, filename)
+  filename_local = trim(filename)
+  call simulation%init_from_file(filename_local)
   call simulation%run( )
   call delete_vp4d_par_cart(simulation)
   print *, 'reached end of vp4d test'
   print *, 'PASSED'
 
   call sll_halt_collective()
+
 
 end program vlasov_poisson_4d
 
