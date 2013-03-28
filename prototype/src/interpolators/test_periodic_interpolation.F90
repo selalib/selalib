0f81c59b4a8dfc02d121c037825a9123ea3d2a51
Raphael Blanchard <raphael.blanchard@inria.fr>
2013-03-28 11:51:31 +0100

    Update lagrange interpolation

--- prototype/src/interpolators/test_periodic_interpolation.F90
+++ prototype/src/interpolators/test_periodic_interpolation.F90
@@ -7,16 +7,11 @@ program test_periodic_interp
   use numeric_constants
   use sll_periodic_interpolator_1d
   use sll_lagrange_interpolator_1d
-!!!!ajout
-  use sll_lagrange_interpolation2
   implicit none 
   
   type(per_1d_interpolator), target       :: interp_per
   type(lagrange_1d_interpolator), target       :: interp_lagrange
-!!!ajout
-  type( sll_lagrange_interpolation_1D2), pointer       :: l_i2
-  !sll_int32, parameter    :: N0 = 16
-  sll_int32, parameter    :: N0 = 4
+  sll_int32, parameter    :: N0 = 16
   sll_real64               :: u(16*N0+1), u_exact(16*N0+1), u_out(16*N0+1)
 #ifdef STDF95
   type(per_1d_interpolator), pointer     :: interp
@@ -82,8 +77,7 @@ program test_periodic_interp
 !        u_exact(i+1) = cos(mode*twopi*(i-alpha)/N)
      end do
 
-!!!modification
-     call interp_lagrange%initialize( N+1,xmin,xmax,0,6)!PERIODIC_LAGRANGE,6)
+     call interp_lagrange%initialize( N+1,xmin,xmax,PERIODIC_LAGRANGE,6)
      interp => interp_lagrange
      u_out(1:N+1)=interp%interpolate_array_disp(N+1, u(1:N+1), -alpha)
 
@@ -92,36 +86,5 @@ program test_periodic_interp
      print*, 'N=',N, 'error=', error, 'numerical order=', log(old_error/error)/log(2.0_f64) 
 
   end do
-
-
-!!!!!!!!!!!!partie test avec lagrange2
-  error = 0.0_8
-  print*,""
-  print*, 'Testing order of lagrange interpolation TESTTTT'
-  ! loop on N 
-  N = N0
-  do p=1,4
-     N= 2*N 
-     
-     ! Interpolate non trivial smooth periodic function
-     do  i=0, N
-        xi = xmin+real(i,f64)*L/real(N,f64)
-        u(i+1) = 1.0_f64 / (2.0_f64 + sin(mode*2._f64*sll_pi/L*xi))
-        u_exact(i+1) =  1.0_f64 / (2.0_f64 + sin(mode*2._f64*sll_pi/L*(xi-alpha)))
-!        u(i+1) = cos(mode*twopi*i/N)
-!        u_exact(i+1) = cos(mode*twopi*(i-alpha)/N)
-     end do
-
-	l_i2 =>new_lagrange_interpolation_1D2(N+1,xmin,xmax,0,6)
-	call compute_lagrange_interpolation_1D2(u(1:N+1),-alpha,L_i2)
-	call interpolate_array_values2(l_i2)
-	u_out=l_i2%data_out
-
-     old_error = error
-     error = maxval(abs(u_out(1:N+1)-u_exact(1:N+1)))
-     print*, 'N=',N, 'error=', error, 'numerical order=', log(old_error/error)/log(2.0_f64) 
-
-  end do
-
 
 end program test_periodic_interp
--- prototype/src/lagrange_interpolation/unit_test.F90
+++ prototype/src/lagrange_interpolation/unit_test.F90
@@ -4,17 +4,15 @@ program lagrange_test
 #include "sll_memory.h"
  use sll_lagrange_interpolation
  use numeric_constants
- use sll_lagrange_interpolation2
 implicit none
 sll_int32  :: i,j,d,num_points
-sll_real64 :: res,diff,diff2,alpha,xmin,xmax,l
+sll_real64 :: res,diff,alpha,xmin,xmax,l
 sll_real64,dimension(:),allocatable ::xi,fi,coord
 type(sll_lagrange_interpolation_1D),pointer ::l_i
-type(sll_lagrange_interpolation_1D2),pointer ::l_i2
 
 d=2
-num_points=5
-alpha=-2.2_f64
+num_points=100
+alpha=0.2_f64
 allocate(xi(1:num_points))
 allocate(fi(1:num_points))
 allocate(coord(1:num_points))
@@ -25,33 +23,26 @@ l=xmax-xmin
 do i=1,num_points
  xi(i)=i-1
  fi(i)=f(xi(i),num_points)
- !fi(i)=f2(xi(i))
  coord(i)=xi(i)+alpha
 end do 
 diff=0.0_f64
-diff2=0.0_f64
 
-l_i => new_lagrange_interpolation_1D(num_points,xmin,xmax,1,d)!PERIODIC_LAGRANGE,d)
-l_i2 => new_lagrange_interpolation_1D2(num_points,xmin,xmax,1,d)!PERIODIC_LAGRANGE,d)
+l_i => new_lagrange_interpolation_1D(num_points,xmin,xmax,PERIODIC_LAGRANGE,d)
 call compute_lagrange_interpolation_1D(alpha,l_i)
-call compute_lagrange_interpolation_1D2(fi,alpha,l_i2)
 call interpolate_array_values(fi,l_i)
-call interpolate_array_values2(l_i2)
 do i=1,num_points
- print*,"1 interpolated value = ", l_i%data_out(i), " , Correct value = ",f(coord(i),num_points)
- print*,"2 interpolated value = ", l_i2%data_out(i), " , Correct value = ",f(coord(i),num_points)
+ !print*,"interpolated value = ", l_i%data_out(i), " , Correct value = ",f(coord(i),num_points)
  diff=max(diff,abs(f(coord(i),num_points)-l_i%data_out(i)))
- diff2=max(diff2,abs(f(coord(i),num_points)-l_i2%data_out(i)))
 end do
 
-if(diff<0.1) then
+if(diff<0.0001) then
  print *, ""
  print *, "Lagrange interpolation unit test: PASSED"
- print *, "error = ",diff,diff2
+ print *, "error = ",diff
 else
  print *, ""
  print *, "Lagrange interpolation unit test: FAILED"
- print *, "error = ",diff,diff2
+ print *, "error = ",diff
 end if
 
 deallocate(xi)
@@ -64,11 +55,6 @@ function f(x,num_points)
 sll_int32 :: num_points
 sll_real64 :: x,f
 f=cos(2*sll_pi*x/(num_points-1.0))
-end function
-
-function f2(x)
-sll_real64 :: x,f2
-f2=sll_pi
 end function
 
 end program
