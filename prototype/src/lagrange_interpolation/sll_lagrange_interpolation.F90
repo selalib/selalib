42f6c8da87ae30f66652b7e01522490e8f2c7110
Raphael Blanchard <raphael.blanchard@inria.fr>
2013-03-28 11:51:31 +0100

    Update lagrange interpolation

--- prototype/src/lagrange_interpolation/sll_lagrange_interpolation.F90
+++ prototype/src/lagrange_interpolation/sll_lagrange_interpolation.F90
@@ -9,7 +9,7 @@ implicit none
    sll_int32                            :: d !moitié du stencil
    sll_int32                            :: nb_cell 
    sll_int32                            :: bc_type
-   sll_int32                            :: indice_decalage
+   sll_int32                            :: index_gap
    sll_real64                           :: alpha
    sll_real64                           :: xmin
    sll_real64                           :: xmax
@@ -27,97 +27,90 @@ contains  !*********************************************************************
 
 
 function new_lagrange_interpolation_1D(num_points,xmin,xmax,bc_type,d)
-type(sll_lagrange_interpolation_1D), pointer :: new_lagrange_interpolation_1D
-sll_int32 ::i,j,ierr
-sll_int32,intent(in) :: d, num_points,bc_type
-sll_real64 :: xmin,xmax
-
-SLL_ALLOCATE( new_lagrange_interpolation_1D, ierr )
-
-
-SLL_ALLOCATE(new_lagrange_interpolation_1D%wj(2*d),ierr)
-SLL_ALLOCATE(new_lagrange_interpolation_1D%data_out(num_points),ierr)
-new_lagrange_interpolation_1D%d=d
-new_lagrange_interpolation_1D%xmin=xmin
-new_lagrange_interpolation_1D%xmax=xmax
-new_lagrange_interpolation_1D%nb_cell=num_points-1
-new_lagrange_interpolation_1D%bc_type=bc_type
+ type(sll_lagrange_interpolation_1D), pointer :: new_lagrange_interpolation_1D
+ sll_int32 ::i,j,ierr
+ sll_int32,intent(in) :: d, num_points,bc_type
+ sll_real64 :: xmin,xmax
+ 
+ SLL_ALLOCATE( new_lagrange_interpolation_1D, ierr )
+ 
+ 
+ SLL_ALLOCATE(new_lagrange_interpolation_1D%wj(2*d),ierr)
+ SLL_ALLOCATE(new_lagrange_interpolation_1D%data_out(num_points),ierr)
+ new_lagrange_interpolation_1D%d=d
+ new_lagrange_interpolation_1D%xmin=xmin
+ new_lagrange_interpolation_1D%xmax=xmax
+ new_lagrange_interpolation_1D%nb_cell=num_points-1
+ new_lagrange_interpolation_1D%bc_type=bc_type
 end function new_lagrange_interpolation_1D
 
-!compute_lagrange_interpolation_1D écrit les wj, qui permettent de calculer le polynome
 subroutine compute_lagrange_interpolation_1D(alpha,lagrange)
-type(sll_lagrange_interpolation_1D), pointer :: lagrange
-sll_int32 :: i,j,indice_decalage
-sll_real64 :: h
-sll_real64, intent(in) :: alpha
-sll_int32,dimension(1:4*lagrange%d-2) :: table
-sll_real64,dimension(1:2*lagrange%d) :: wj
-
-
-h=(lagrange%xmax-lagrange%xmin)/(lagrange%nb_cell)
-if(alpha<0)then
-indice_decalage=alpha/h-1
-else
-indice_decalage=alpha/h
-end if
-
-do i=i,2*lagrange%d-1
-table(i)=2*lagrange%d-1-(i-1)
-table(i+2*lagrange%d-1)=i
-end do
-
-!remplissage de wj
-wj(:)=1.0_f64
-do i=1,lagrange%d
- do j=1,2*lagrange%d-1
-  wj(i)=wj(i)*table(i+j-1)
+ type(sll_lagrange_interpolation_1D), pointer :: lagrange
+ sll_int32 :: i,j,index_gap
+ sll_real64 :: h
+ sll_real64, intent(in) :: alpha
+ sll_int32,dimension(1:4*lagrange%d-2) :: table
+ sll_real64,dimension(1:2*lagrange%d) :: wj
+ 
+ h=(lagrange%xmax-lagrange%xmin)/(lagrange%nb_cell)
+ if(alpha<0)then
+ index_gap=alpha/h-1
+ else
+ index_gap=alpha/h
+ end if
+ 
+ do i=i,2*lagrange%d-1
+ table(i)=2*lagrange%d-1-(i-1)
+ table(i+2*lagrange%d-1)=i
  end do
- wj(i)=((-1.0_f64)**(lagrange%d+i))*(h**(2*lagrange%d-1))*wj(i)
-end do
-do i=1,lagrange%d
- wj(i+lagrange%d)=-wj(lagrange%d-i+1)
-end do
-wj=1.0_f64/wj
-
-print*,"wj dans 1"
-print*,wj
-print*,""
-
-lagrange%wj=wj
-lagrange%alpha=alpha
+ 
+ wj(:)=1.0_f64
+ do i=1,lagrange%d
+  do j=1,2*lagrange%d-1
+   wj(i)=wj(i)*table(i+j-1)
+  end do
+  wj(i)=((-1.0_f64)**(lagrange%d+i))*(h**(2*lagrange%d-1))*wj(i)
+ end do
+ do i=1,lagrange%d
+  wj(i+lagrange%d)=-wj(lagrange%d-i+1)
+ end do
+ wj=1.0_f64/wj
+ 
+ 
+ lagrange%wj=wj
+ lagrange%alpha=alpha
 
 end subroutine compute_lagrange_interpolation_1D
 
 
 subroutine interpolate_array_values(fi,lagrange)
 type(sll_lagrange_interpolation_1D), pointer :: lagrange
-sll_int32 ::i,j,indice_decalage
+sll_int32 ::i,j,index_gap
 sll_real64 :: sum1,sum2,beta,h
 sll_real64,dimension(1:lagrange%nb_cell+1),intent(in) :: fi
 
+h=(lagrange%xmax-lagrange%xmin)/(lagrange%nb_cell)
+if(lagrange%alpha<0)then
+index_gap=lagrange%alpha/h-1
+beta=h+mod(lagrange%alpha,h)
+else
+index_gap=lagrange%alpha/h
+beta=mod(lagrange%alpha,h)
+end if
+
+sum2=0.0_f64
+do j=1,2*lagrange%d
+ lagrange%wj(j)=lagrange%wj(j)/(beta+real(lagrange%d-j,f64)*h)
+ sum2=sum2+lagrange%wj(j)
+end do
 
 select case(lagrange%bc_type)
 case (PERIODIC_LAGRANGE)
-
- h=(lagrange%xmax-lagrange%xmin)/(lagrange%nb_cell)
- if(lagrange%alpha<0)then
- indice_decalage=lagrange%alpha/h-1
- beta=h+mod(lagrange%alpha,h)
- else
- indice_decalage=lagrange%alpha/h
- beta=mod(lagrange%alpha,h)
- end if
- 
- do j=1,2*lagrange%d
-  lagrange%wj(j)=lagrange%wj(j)/(beta+real(lagrange%d-j,f64)*h)
- end do
  
  do i=1,lagrange%nb_cell+1
  sum1=0.0_f64
- sum2=0.0_f64
   do j=1,lagrange%d*2
-   sum1=sum1+lagrange%wj(j)*fi(modulo(indice_decalage+(i-1)+(j-1)-(lagrange%d-1),lagrange%nb_cell)+1)
-   sum2=sum2+lagrange%wj(j)
+   sum1=sum1+lagrange%wj(j)*fi(modulo(index_gap+(i-1)+(j-1)-(lagrange%d-1),lagrange%nb_cell)+1)
   end do
  lagrange%data_out(i)=sum1/sum2
  end do
@@ -126,31 +119,17 @@ case(HERMITE_LAGRANGE)
 
  do i=1,lagrange%nb_cell+1
  sum1=0.0_f64
- sum2=0.0_f64
   do j=1,lagrange%d*2
-   if(indice_decalage+(i-1)+(j-1)-(lagrange%d-1)<0)then
-    sum1=sum1+lagrange%wj(j)*fi(0)
-   else if(indice_decalage+(i-1)+(j-1)-(lagrange%d-1) > lagrange%nb_cell+1)then
+   if(index_gap+(i-1)+(j-1)-(lagrange%d-1)<0)then
+    sum1=sum1+lagrange%wj(j)*fi(1)
+   else if(index_gap+(i-1)+(j-1)-(lagrange%d-1) > lagrange%nb_cell)then
     sum1=sum1+lagrange%wj(j)*fi(lagrange%nb_cell+1)
    else
-    sum1=sum1+lagrange%wj(j)*fi(indice_decalage+(i-1)+(j-1)-(lagrange%d-1))
+    sum1=sum1+lagrange%wj(j)*fi(index_gap+(i-1)+(j-1)-(lagrange%d-1)+1)
    end if
-!   sum1=sum1+lagrange%wj(j)*fi
-   sum2=sum2+lagrange%wj(j)
-  end do
+ end do
  lagrange%data_out(i)=sum1/sum2
  end do
-
-! do k=1,lagrange%num_points
-!  sum1=0.0_f64
-!  sum2=0.0_f64
-!  xk=lagrange%xmin+(k-1)*h+lagrange%alpha
-!  do j=1,lagrange%d*2
-!   sum1=sum1+lagrange%fiadd(k-1+j)*lagrange%wj(j,k)/(xk-lagrange%xiadd(k-1+j))
-!   sum2=sum2+lagrange%wj(j,k)/(xk-lagrange%xiadd(k-1+j))
-!  end do
-!  lagrange%data_out(k)=sum1/sum2
-! end do
 
 case default
   print *, 'ERROR: compute_lagrange_interpolation_1D(): not recognized boundary condition'
@@ -160,13 +139,13 @@ end select
 end subroutine interpolate_array_values 
  
  
- subroutine delete_lagrange_interpolation_1D( lagrange_interpolation )
+subroutine delete_lagrange_interpolation_1D( lagrange_interpolation )
  type(sll_lagrange_interpolation_1D), pointer :: lagrange_interpolation
  sll_int32                    :: ierr
    SLL_ASSERT( associated(lagrange_interpolation) )
    SLL_DEALLOCATE( lagrange_interpolation%wj, ierr )
    SLL_DEALLOCATE( lagrange_interpolation, ierr )
- end subroutine delete_lagrange_interpolation_1D
+end subroutine delete_lagrange_interpolation_1D
 
 end module
 
