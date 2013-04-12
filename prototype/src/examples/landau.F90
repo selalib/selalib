dd294571c181afea6db5c1113f05acc0c5e2b284
Pierre Navaro <navaro@math.cnrs.fr>
2013-04-12 13:50:06 +0200

    Remove FFTPACK variable, build by default

--- prototype/src/examples/landau.F90
+++ prototype/src/examples/landau.F90
@@ -1,25 +1,21 @@
 program landau_1d
-#include "sll_working_precision.h"
-#include "sll_memory.h"
-#include "sll_constants.h"
-#include "sll_poisson_solvers.h"
-#include "sll_interpolators.h"
+#include "selalib.h"
 
 implicit none
   
-sll_int32  :: i_step, n_step, i, j, j_step
-sll_int32  :: nc_eta1, nc_eta2
+integer  :: i_step, n_step, i, j, j_step
+integer  :: nc_eta1, nc_eta2
 
-sll_real64 :: eta1_min, eta1_max, delta_eta1
-sll_real64 :: eta2_min, eta2_max, delta_eta2
-sll_real64 :: delta_t, time
+real(8) :: eta1_min, eta1_max, delta_eta1
+real(8) :: eta2_min, eta2_max, delta_eta2
+real(8) :: delta_t, time
 
-sll_real64, dimension(:), allocatable :: nrj
-sll_real64, dimension(:,:), allocatable :: df
+real(8), dimension(:), allocatable :: nrj
+real(8), dimension(:,:), allocatable :: df
 
-sll_int32  :: error
+integer  :: error
 
-sll_real64, dimension(:), allocatable :: eta1, eta2
+real(8), dimension(:), allocatable :: eta1, eta2
 
 class(sll_interpolator_1d_base), pointer    :: interp_x
 class(sll_interpolator_1d_base), pointer    :: interp_v
@@ -27,10 +23,10 @@ class(sll_interpolator_1d_base), pointer    :: interp_v
 type(cubic_spline_1d_interpolator), target  :: spline_x
 type(cubic_spline_1d_interpolator), target  :: spline_v
 
-sll_real64, dimension(:), allocatable :: ex
-sll_real64, dimension(:), allocatable :: rho
-type (poisson_1d_periodic)            :: poisson
-sll_real64 :: eps, kx
+real(8), dimension(:), allocatable :: ex
+real(8), dimension(:), allocatable :: rho
+type (poisson_1d_periodic)         :: poisson
+real(8) :: eps, kx
 
 interp_x => spline_x
 interp_v => spline_v
@@ -43,19 +39,19 @@ eta2_min = -6.0_f64
 eta2_max = +6.0_f64
 nc_eta2  = 256
 
-SLL_ALLOCATE(rho(nc_eta1+1),error)
-SLL_ALLOCATE(ex(nc_eta1+1),error)
-SLL_ALLOCATE(df(nc_eta1+1,nc_eta2+1),error)
+allocate(rho(nc_eta1+1))
+allocate(ex(nc_eta1+1))
+allocate(df(nc_eta1+1,nc_eta2+1))
 
 delta_eta1 = (eta1_max-eta1_min)/nc_eta1
 delta_eta2 = (eta2_max-eta2_min)/nc_eta2
 
 call new(poisson, eta1_min, eta2_max, nc_eta1, error) 
 call spline_x%initialize(nc_eta1+1, eta1_min, eta1_max, PERIODIC_SPLINE )
-call spline_v%initialize(nc_eta2+1, eta2_min, eta2_max, PERIODIC_SPLINE )
+call spline_v%initialize(nc_eta2+1, eta2_min, eta2_max, HERMITE_SPLINE )
 
-SLL_ALLOCATE(eta1(nc_eta1+1),error)
-SLL_ALLOCATE(eta2(nc_eta2+1),error)
+allocate(eta1(nc_eta1+1))
+allocate(eta2(nc_eta2+1))
 
 !Initialize distribution function
 eps = 0.05_f64
@@ -72,7 +68,7 @@ end do
 time = 0.0_f64
 n_step = 1000
 delta_t = 0.05_f64
-SLL_ALLOCATE(nrj(n_step), error)
+allocate(nrj(n_step))
 print'(a)', 'set term x11'
 
 call advection_x(0.5*delta_t)
@@ -110,14 +106,14 @@ end do
 contains
 
    subroutine advection_x(dt)
-    sll_real64, intent(in) :: dt
+    real(8), intent(in) :: dt
     do j = 1, nc_eta2+1
       df(:,j) = interp_x%interpolate_array_disp(nc_eta1+1,df(:,j),dt*eta2(j))
     end do
    end subroutine advection_x
 
    subroutine advection_v(dt)
-    sll_real64, intent(in) :: dt
+    real(8), intent(in) :: dt
     do i = 1, nc_eta1+1
       df(i,:) = interp_v%interpolate_array_disp(nc_eta2+1,df(i,:),dt*ex(i))
     end do
