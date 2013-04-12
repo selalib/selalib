d65e15e0e352cf4e3fb41789b47f6bd28a009353
Pierre Navaro <navaro@math.cnrs.fr>
2013-04-12 13:50:06 +0200

    Remove FFTPACK variable, build by default

--- prototype/src/selalib_level/selalib.h
+++ prototype/src/selalib_level/selalib.h
@@ -5,6 +5,15 @@
 #include "sll_memory.h"
 #include "sll_working_precision.h"
 
-use selalib
+use sll_memory
+use sll_working_precision
+use sll_utilities
+use sll_constants
+use sll_io
+use sll_visu_pic
+use sll_timer
+use sll_interpolators
+use sll_maxwell_solvers
+use sll_poisson_solvers
 
 #endif
