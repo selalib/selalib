2562cc4453616ba47e5232d8f9663bacf7b5e0ff
Philippe Helluy <philippe.helluy@inria.fr>
2013-05-13 11:58:40 +0200

    start mpi drift kinetic

--- prototype/src/parallel_array_initializers/sll_common_array_initializers_module.F90
+++ prototype/src/parallel_array_initializers/sll_common_array_initializers_module.F90
@@ -149,8 +149,10 @@ contains
     kx      = params(2)
     factor1 = 0.5_f64/sll_pi
 
+    !write(*,*) 'x1 v1',x1,v1
+
     sll_landau_initializer_dk_test_4d = factor1*&
-         (1.0_f64+cos(kx*x1)*exp(-0.5_f64*(v1**2)))
+         (1.0_f64+epsilon*cos(kx*x1))*exp(-0.5_f64*(v1**2))
   end function sll_landau_initializer_dk_test_4d
 
 end module sll_common_array_initializers_module
