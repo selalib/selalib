0d3a5b5335db06bf85196241d84946f6633e8e85
Aurore Back <aurore.back@gmail.com>
2013-05-23 09:20:10 +0200

    change deboor splines' '

--- prototype/src/deboor_splines/spli2d_custom.f90
+++ prototype/src/deboor_splines/spli2d_custom.f90
@@ -15,6 +15,7 @@ subroutine spli2d_custom ( ai_nx, ai_kx, apr_taux, ai_ny, ai_ky, apr_tauy, apr_g
 		real(8), dimension ( ai_nx * ai_ny ) :: lpr_work3
 		integer  :: li_i, li_j, li_iflag
 									
+									
 ! *** set up knots
 !     interpolate between knots
 		! x
@@ -22,24 +23,24 @@ subroutine spli2d_custom ( ai_nx, ai_kx, apr_taux, ai_ny, ai_ky, apr_tauy, apr_g
 		apr_tx ( ai_nx + 1 : ai_nx + ai_kx ) = apr_taux ( ai_nx )		
 		
 		do li_i = ai_kx + 1, ai_nx
-			apr_tx ( li_i ) = ( apr_taux ( li_i - ai_kx + 1 ) + apr_taux ( li_i - ai_kx + 2 ) ) / 2.0_8
+			apr_tx ( li_i ) = apr_taux ( 2 ) + (li_i-(ai_kx + 1))*( apr_taux ( ai_nx-1 ) - apr_taux ( 2 ) ) / (ai_nx-(ai_kx + 1))
+ !( apr_taux ( li_i - ai_kx + 1 ) + apr_taux ( li_i - ai_kx + 2 ) ) / 2.0_8!
 		end do
 
-		! y
-		apr_ty ( 1 : ai_ky ) = apr_tauy ( 1 )		
+		apr_ty ( 1 : ai_ky ) = apr_tauy ( 1 )
 		apr_ty ( ai_ny + 1 : ai_ny + ai_ky ) = apr_tauy ( ai_ny )		
 		
 		do li_j = ai_ky + 1, ai_ny
-			apr_ty ( li_j ) = ( apr_tauy ( li_j - ai_ky + 1 ) + apr_tauy ( li_j - ai_ky + 2 ) ) / 2.0_8
+			apr_ty ( li_j ) = apr_tauy ( 2 ) + (li_j-(ai_ky + 1))*( apr_tauy ( ai_ny -1) - apr_tauy ( 2 ) ) / (ai_ny-(ai_ky + 1))
+!( apr_tauy ( li_j - ai_ky + 1 ) + apr_tauy ( li_j - ai_ky + 2 ) ) / 2.0_8!
 		end do
-		
+
 		do li_i = 1, ai_nx
 			do li_j = 1, ai_ny
 				apr_Bcoef ( li_i, li_j ) = apr_g ( li_i, li_j )
 			end do
 		end do
-
-!
+        !print*, 'ert',apr_tx
 !  *** construct b-coefficients of interpolant
 !
 		call spli2d ( apr_taux, apr_bcoef, apr_tx, ai_nx, ai_kx, ai_ny, lpr_work2, lpr_work3, lpr_work1, li_iflag )
