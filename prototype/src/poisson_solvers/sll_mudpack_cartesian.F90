f4fdd73e36581c1feedb9fec79f7fafc343743d4
Pierre Navaro <navaro@math.cnrs.fr>
2013-05-30 19:55:20 +0200

    Working on mudpack interface

--- prototype/src/poisson_solvers/sll_mudpack_cartesian.F90
+++ prototype/src/poisson_solvers/sll_mudpack_cartesian.F90
@@ -1,39 +1,67 @@
 module sll_mudpack_cartesian
 #include "sll_working_precision.h"
 #include "sll_utilities.h"
+#include "sll_file_io.h"
+
 implicit none
 
+sll_real64, dimension(:), allocatable :: work
+sll_int32 :: mgopt(4)
 
 contains
 
-subroutine solve_mudpack_cartesian()
-!set grid sizes with parameter statements
-sll_int32, parameter :: iixp = 2 , jjyq = 3 , iiex = 6, jjey = 5 
-sll_int32, parameter :: nnx=iixp*2**(iiex-1)+1, nny=jjyq*2**(jjey-1)+1 
+subroutine initialize_mudpack_cartesian(phi, rhs,            &
+                                        eta1_min, eta1_max, nc_eta1, &
+                                        eta2_min, eta2_max, nc_eta2)
+implicit none
 
-!set minimal required work space (see tmud2sp.f)
-sll_int32, parameter :: llwork = 13264 
-sll_real64 :: phi(nnx,nny),rhs(nnx,nny),work(llwork)
+! set grid size params
+sll_real64, intent(in) :: eta1_min, eta1_max
+sll_real64, intent(in) :: eta2_min, eta2_max
+sll_int32,  intent(in) :: nc_eta1, nc_eta2
+sll_int32, parameter   :: iixp = 2 , jjyq = 2
+sll_int32 :: icall, iiex, jjey, nnx, nny, llwork
+
+sll_real64, intent(inout) :: phi(nc_eta1+1,nc_eta2+1)
+sll_real64, intent(inout) :: rhs(nc_eta1+1,nc_eta2+1)
 
 !put integer and floating point argument names in contiguous
 !storeage for labelling in vectors iprm,fprm
-sll_int32 :: iprm(16),mgopt(4)
+sll_int32  :: iprm(16)
 sll_real64 :: fprm(6)
-sll_int32 :: intl,nxa,nxb,nyc,nyd,ixp,jyq,iex,jey,nx,ny, &
-           iguess,maxcy,method,nwork,lwrkqd,itero
+sll_int32  :: intl,nxa,nxb,nyc,nyd,ixp,jyq,iex,jey,nx,ny
+sll_int32  :: iguess,maxcy,method,nwork,lwrkqd,itero
 sll_real64 :: xa,xb,yc,yd,tolmax,relmax
-sll_int32 :: i,j,ierror
+sll_int32  :: i,j,error
 sll_real64 :: dlx,dly,x,y,cxx,cyy,cx,cy,ce,pxx,pyy,px,py,pe,errmax
 sll_real64 :: cex,cey
 
 common/itmud2sp/intl,nxa,nxb,nyc,nyd,ixp,jyq,iex,jey,nx,ny, &
               iguess,maxcy,method,nwork,lwrkqd,itero
-equivalence(intl,iprm)
 common/ftmud2sp/xa,xb,yc,yd,tolmax,relmax
+
+equivalence(intl,iprm)
 equivalence(xa,fprm)
 
 !declare coefficient and boundary condition input subroutines external
 external cofx,cofy,bndsp
+
+nnx = nc_eta1+1
+nny = nc_eta2+1
+
+! set minimum required work space
+llwork=(7*(nnx+2)*(nny+2)+44*nnx*nny)/3
+      
+allocate(work(llwork))
+icall = 0
+iiex = ceiling(log((nnx-1.)/iixp)/log(2.))+1
+jjey = ceiling(log((nny-1.)/jjyq)/log(2.))+1
+
+! set multigrid arguments (w(2,1) cycling with fully weighted
+! residual restriction and cubic prolongation)
+mgopt(1) = 2
+mgopt(2) = 2
+mgopt(3) = 1
 
 !set input integer arguments
 intl = 0
@@ -49,8 +77,18 @@ ixp  = iixp
 jyq  = jjyq
 iex  = iiex
 jey  = jjey
-nx   = nnx
-ny   = nny
+
+nx = ixp*(2**(iex-1))+1
+ny = jyq*(2**(jey-1))+1
+
+if (nx /= nc_eta1+1) then
+   print*, "nx,nc_eta1+1=", nx, nc_eta1+1
+   stop ' nx different de nr dans mg_polar_poisson'
+end if
+if (ny /= nc_eta2+1) then
+   print*, "ny,nc_eta2+1=", ny, nc_eta2+1
+   stop ' ny different de nth dans mg_polar_poisson'
+end if
 
 !set multigrid arguments (w(2,1) cycling with fully weighted
 !residual restriction and cubic prolongation)
@@ -72,10 +110,10 @@ nwork = llwork
 method = 0
 
 !set end points of solution rectangle in (x,y) space
-xa = 0.0
-xb = 1.0
-yc = 0.0
-yd = 1.0
+xa = eta1_min
+xb = eta1_max
+yc = eta2_min
+yd = eta2_max
 
 !set mesh increments
 dlx = (xb-xa)/float(nx-1)
@@ -84,98 +122,17 @@ dly = (yd-yc)/float(ny-1)
 !set for no error control flag
 tolmax = 0.0
 
-!set right hand side in rhs and initialize phi to zero
-do i=1,nx
-   x = xa+float(i-1)*dlx
-   call cofx(x,cxx,cx,cex)
-   do j=1,ny
-      y = yc+float(j-1)*dly
-      call cofy(y,cyy,cy,cey)
-      ce = cex+cey
-      call exact(x,y,pxx,pyy,px,py,pe)
-      rhs(i,j) = cxx*pxx+cyy*pyy+cx*px+cy*py+ce*pe
-      phi(i,j) = 0.0
-   end do
-end do
-
-!set specified boundaries in phi
-x = xb
-do j=1,ny
-   y = yc+float(j-1)*dly
-   call exact(x,y,pxx,pyy,px,py,pe)
-   phi(nx,j) = pe
-end do
-y = yc
-do i=1,nx
-   x = xa+float(i-1)*dlx
-   call exact(x,y,pxx,pyy,px,py,pe)
-   phi(i,1) = pe
-end do
-write(*,100)
 write(*,101) (iprm(i),i=1,15)
 write(*,102) (mgopt(i),i=1,4)
-
-!intiialization call
+write(*,103) xa,xb,yc,yd,tolmax
 write(*,104) intl
-call mud2sp(iprm,fprm,work,cofx,cofy,bndsp,rhs,phi,mgopt,ierror)
+
+call mud2sp(iprm,fprm,work,cofx,cofy,bndsp,rhs,phi,mgopt,error)
 
 !print error parameter and minimum work space requirement
-write (*,200) ierror,iprm(16)
-if (ierror.gt.0) call exit(0)
+write (*,200) error,iprm(16)
+if (error > 0) call exit(0)
 
-!attempt solution
-intl = 1
-write(*,106) intl,method,iguess
-
-call mud2sp(iprm,fprm,work,cofx,cofy,bndsp,rhs,phi,mgopt,ierror)
-write(*,107) ierror
-if (ierror.gt.0) call exit(0)
-
-if (ierror .le. 0) then
-
-   !compute and print maximum norm of error
-   errmax = 0.0
-   do j=1,ny
-      y = yc+(j-1)*dly
-      do i=1,nx
-         x = xa+(i-1)*dlx
-         call exact(x,y,pxx,pyy,px,py,pe)
-         errmax = dmax1(errmax,abs((phi(i,j)-pe)))
-      end do
-   end do
-   write(*,201) errmax
-
-end if
-
-! attempt to improve approximation to fourth order
-
-call mud24sp(work,phi,ierror)
-
-call plot_field(rhs, "rhs", 0)
-call plot_field(phi, "phi", 0)
-
-write (*,108) ierror
-if (ierror.gt.0) call exit(0)
-
-if (ierror .le. 0) then
-
-   !compute and print maximum norm of error
-   errmax = 0.0
-   do j=1,ny
-      y = yc+(j-1)*dly
-      do i=1,nx
-         x = xa+(i-1)*dlx
-         call exact(x,y,pxx,pyy,px,py,pe)
-         errmax = dmax1(errmax,abs((phi(i,j)-pe)))
-      end do
-   end do
-   write(*,201) errmax
-
-end if
-
-print*,"PASSED"
-
-100 format(//' test_mudpack_2d ')
 101 format(/' integer input arguments ', &
     &/'intl = ',i2,' nxa = ',i2,' nxb = ',i2,' nyc = ',i2,' nyd = ',i2, &
     &/' ixp = ',i2,' jyq = ',i2,' iex = ',i2,' jey = ',i2 &
@@ -186,62 +143,69 @@ print*,"PASSED"
     &/' iprer = ',i2, &
     &/' ipost = ',i2, &
     &/' intpol = ',i2)
-    write(*,103) xa,xb,yc,yd,tolmax
 103 format(/' floating point input parameters ', &
     &/' xa = ',f6.3,' xb = ',f6.3,' yc = ',f6.3,' yd = ',f6.3, &
     &/' tolerance (error control) =   ',e10.3)
 104 format(/' discretization call to mud2sp', ' intl = ', i2)
+200 format(' error = ',i2, ' minimum work space = ',i7)
+     
+end subroutine initialize_mudpack_cartesian
+
+
+subroutine solve_mudpack_cartesian(phi, rhs)
+
+sll_real64 :: phi(:,:),rhs(:,:)
+
+!put integer and floating point argument names in contiguous
+!storeage for labelling in vectors iprm,fprm
+sll_int32  :: iprm(16)
+sll_real64 :: fprm(6)
+sll_int32  :: intl,nxa,nxb,nyc,nyd,ixp,jyq,iex,jey,nx,ny
+sll_int32  :: iguess,maxcy,method,nwork,lwrkqd,itero
+sll_real64 :: xa,xb,yc,yd,tolmax,relmax
+sll_int32  :: error
+
+common/itmud2sp/intl,nxa,nxb,nyc,nyd,ixp,jyq,iex,jey,nx,ny, &
+              iguess,maxcy,method,nwork,lwrkqd,itero
+common/ftmud2sp/xa,xb,yc,yd,tolmax,relmax
+
+equivalence(intl,iprm)
+equivalence(xa,fprm)
+
+!declare coefficient and boundary condition input subroutines external
+external cofx,cofy,bndsp
+
+!set input integer arguments
+intl = 1
+
+!set no initial guess forcing full multigrid cycling
+iguess = 0
+
+!attempt solution
+intl = 1
+write(*,106) intl,method,iguess
+
+call mud2sp(iprm,fprm,work,cofx,cofy,bndsp,rhs,phi,mgopt,error)
+write(*,107) error
+if (error > 0) call exit(0)
+
+! attempt to improve approximation to fourth order
+call mud24sp(work,phi,error)
+
+write (*,108) error
+if (error > 0) call exit(0)
+
 106 format(/' approximation call to mud2sp', &
     &/' intl = ',i2, ' method = ',i2,' iguess = ',i2)
-107 format(' ierror = ',i2)
-108 format(/' mud24sp test ', ' ierror = ',i2)
-200 format(' ierror = ',i2, ' minimum work space = ',i7)
-201 format(' maximum error  =  ',e10.3)
+107 format(' error = ',i2)
+108 format(/' mud24sp test ', ' error = ',i2)
      
 end subroutine solve_mudpack_cartesian
 
+end module sll_mudpack_cartesian
 
-subroutine plot_field(f, fname, iplot)
-
-   sll_int32 :: iplot, i, j
-   sll_real64 :: x, y
-   sll_real64, dimension(:,:) :: f
-   character(len=*) :: fname
-   character(len=4) :: cplot
- 
-   call int2string(iplot,cplot)
-
-   open(11, file=fname//cplot//".dat")
-   do i = 1, size(f,1)
-      do j = 1, size(f,2)
-         x = xa + (i-1)*dlx
-         y = yc + (j-1)*dly
-         write(11,*) x,y,f(i,j)
-      end do
-      write(11,*)
-   end do
-   close(11)
-   
-   open( 90, file = fname//'.gnu', position="append" )
-   if ( iplot == 1 ) then
-      rewind(90)
-      !write(90,*)"set cbrange[-1:1]"
-      !write(90,*)"set pm3d"
-      write(90,*)"set surf"
-      write(90,*)"set term x11"
-   end if
-
-   write(90,*)"set title 'step = ",iplot,"'"
-   write(90,"(a)")"splot '"//fname//cplot//".dat' u 1:2:3 w lines"
-   close(90)
-
-end subroutine plot_field
-
-
+!> input x dependent coefficients
 subroutine cofx(x,cxx,cx,cex)
-!
-!     input x dependent coefficients
-!
 implicit none
 real(8)  :: x,cxx,cx,cex
 cxx = 1.0+x*x
@@ -250,10 +214,8 @@ cex = -x
 return
 end
 
+!> input y dependent coefficients
 subroutine cofy(y,cyy,cy,cey)
-!
-!     input y dependent coefficients
-!
 implicit none
 real(8)  :: y,cyy,cy,cey
 cyy = exp(1.0-y)
@@ -262,46 +224,43 @@ cey = -y
 return
 end
 
+!> input mixed derivative b.c. to mud2sp
 subroutine bndsp(kbdy,xory,alfa,gbdy)
-!
-!     input mixed derivative b.c. to mud2sp
-!
 implicit none
 integer  :: kbdy
 real(8)  :: xory,alfa,gbdy,x,y,pe,px,py,pxx,pyy
 real(8)  :: xa,xb,yc,yd,tolmax,relmax
 common/ftmud2sp/xa,xb,yc,yd,tolmax,relmax
-if (kbdy.eq.1) then  ! x=xa boundary
-y = xory
-x = xa
-call exact(x,y,pxx,pyy,px,py,pe)
-alfa = -1.0
-gbdy = px + alfa*pe
-return
+
+if (kbdy == 1) then  ! x=xa boundary
+   y = xory
+   x = xa
+   call exact(x,y,pxx,pyy,px,py,pe)
+   alfa = -1.0
+   gbdy = px + alfa*pe
+   return
 end if
-if (kbdy.eq.4) then  ! y=yd boundary
-y = yd
-x = xory
-call exact(x,y,pxx,pyy,px,py,pe)
-alfa = 1.0
-gbdy = py + alfa*pe
-return
+
+if (kbdy == 4) then  ! y=yd boundary
+   y = yd
+   x = xory
+   call exact(x,y,pxx,pyy,px,py,pe)
+   alfa = 1.0
+   gbdy = py + alfa*pe
+   return
 end if
 end
 
+!> set an exact solution for testing mud2sp
 subroutine exact(x,y,pxx,pyy,px,py,pe)
-!
-!     set an exact solution for testing mud2sp
-!
 implicit none
 real(8)  :: x,y,pxx,pyy,px,py,pe
-pe = (x**3+y**3+1.0)/3.0
-px = x*x
-py = y*y
+pe  = (x**3+y**3+1.0)/3.0
+px  = x*x
+py  = y*y
 pxx = x+x
 pyy = y+y
 return
 end
 
-end module sll_mudpack_cartesian
 
