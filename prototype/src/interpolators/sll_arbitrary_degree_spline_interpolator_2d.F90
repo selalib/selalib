12128e3542764fb5bf6bcc9dc69f09d8c88b6a05
Edwin Chacon-Golcher <edwincg@gmail.com>
2013-06-03 18:42:19 +0200

    had missed a file while updating the BC descriptors

--- prototype/src/interpolators/sll_arbitrary_degree_spline_interpolator_2d.F90
+++ prototype/src/interpolators/sll_arbitrary_degree_spline_interpolator_2d.F90
@@ -516,7 +516,7 @@ contains
   
     case(576) !  3. periodic, dirichlet-bottom, dirichlet-top
        print *, 'sz1 = ', sz1, 'sz2 = ', sz2
-       print *, 'size(coeff_splines) = ', size(interpolator%coeff_splines)
+       print *, 'size(coeff_splines) = ', size(interpolator%coeff_splines,1),size(interpolator%coeff_splines,2)
        interpolator%size_coeffs1 = sz1+1
        interpolator%size_coeffs2 = sz2
        interpolator%size_t1 = order1 + sz1 + 1
