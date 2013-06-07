346b4ed5475a601ce182ff6cc678d0c60aa99e27
Philippe Helluy <philippe.helluy@inria.fr>
2013-06-07 11:48:01 +0200

    init vp 4d euler ok

--- prototype/src/simulation/unit_test_4d_vp_eulerian_cartesian_finite_volume.F90
+++ prototype/src/simulation/unit_test_4d_vp_eulerian_cartesian_finite_volume.F90
@@ -3,9 +3,9 @@
 ! - 4D: x1,x2,x3,v3 (or v1) with cartesian coordinate 
 ! - parallel
 
-program dk_cartesian_4d
+program vp_cartesian_4d
 #include "sll_working_precision.h"
-  use sll_simulation_4d_drift_kinetic_cartesian_finite_volume
+  use sll_simulation_4d_vp_eulerian_cartesian_finite_volume_module
   use sll_collective
   use sll_constants
   use sll_logical_meshes
@@ -14,9 +14,9 @@ program dk_cartesian_4d
 
   character(len=256) :: filename
   character(len=256) :: filename_local
-  type(sll_simulation_4d_drift_kinetic_cart_finite_volume)      :: simulation
+  type(sll_simulation_4d_vp_eulerian_cartesian_finite_volume)      :: simulation
   type(sll_logical_mesh_4d), pointer      :: mx
-  sll_real64, dimension(1:2) :: landau_params
+  sll_real64, dimension(1:5) :: landau_params
 
   print *, 'Booting parallel environment...'
   call sll_boot_collective() ! Wrap this up somewhere else
@@ -43,26 +43,42 @@ program dk_cartesian_4d
 ! hardwired, this should be consistent with whatever is read from a file
 #define NCELL1 32
 #define NCELL2 32
-#define NCELL3 4
+#define NCELL3 32
 #define NCELL4 8
+#define ETA1MIN -6.0_f64
+#define ETA1MAX 6.0_f64
+#define ETA2MIN -6.0_f64
+#define ETA2MAX 6.0_f64
+#define ETA3MIN 0.0_f64
+#define ETA3MAX 1.0_f64
+#define ETA4MIN 0.0_f64
+#define ETA4MAX 1.0_f64
 
   ! logical mesh for space coordinates
   mx => new_logical_mesh_4d( NCELL1, NCELL2,NCELL3, NCELL4 , &
-       eta1_min=-6.0_f64, eta1_max=6.0_f64,eta2_min=0.0_f64,eta2_max=1.0_f64)
+       eta1_min=ETA1MIN, eta1_max=ETA1MAX, &
+       eta2_min=ETA2MIN,eta2_max=ETA2MAX , &
+       eta3_min=ETA3MIN, eta3_max=ETA3MAX, &
+       eta4_min=ETA4MIN,eta4_max=ETA4MAX )
 
 
 
 
   ! define the values of the parameters for the landau initializer
-  landau_params(1) = 0.1
-  landau_params(2) = 2.0*sll_pi
+    landau_params(1)=ETA3MIN
+    landau_params(2)=ETA3MAX
+    landau_params(3)=ETA4MIN
+    landau_params(4)=ETA4MAX
+    landau_params(5)= 0.1
 
   ! initialize simulation object with the above parameters
-  call initialize_dk4d( &
+  call initialize_vp4d( &
        simulation, &
        mx, &
-       sll_landau_initializer_dk_test_4d, &
-       landau_params )
+       sll_landau_initializer_v1v2x1x2, &
+       landau_params, &
+       1.0_f64 )
+
 
   call simulation%run( )
   call delete(simulation)
@@ -72,6 +88,6 @@ program dk_cartesian_4d
   call sll_halt_collective()
 
 
-end program dk_cartesian_4d
+end program vp_cartesian_4d
 
 
