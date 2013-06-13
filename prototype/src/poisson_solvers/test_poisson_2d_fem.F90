f1cba8dc59325836543e9a7f661bc9854ee77adc
Pierre Navaro <navaro@math.unistra.fr>
2013-06-13 20:36:43 +0200

    Fix build error in fem poisson solver

--- prototype/src/poisson_solvers/test_poisson_2d_fem.F90
+++ prototype/src/poisson_solvers/test_poisson_2d_fem.F90
@@ -3,9 +3,6 @@ program test_poisson_2d_fem
 #include "sll_memory.h"
 #include "sll_constants.h"
 
-#define nx 64
-#define ny 64
-
 use sll_poisson_2d_fem
 implicit none
 
@@ -17,6 +14,7 @@ sll_real64, dimension(:),   pointer :: y
 sll_real64, dimension(:,:), pointer :: ex
 sll_real64, dimension(:,:), pointer :: ey
 sll_real64, dimension(:,:), pointer :: rho
+sll_real64 :: errmax
 
 call test()
 
@@ -25,13 +23,14 @@ contains
 subroutine test()
 type( poisson_fem ) :: poisson
 sll_real64 :: dx, dy
+sll_int32  :: nx, ny
 
-ncx = 64
-ncy = 64
+nx = 64
+ny = 64
 
-SLL_CLEAR_ALLOCATE(ex(1:ncx+1),error)  
-SLL_CLEAR_ALLOCATE(ey(1:ncy+1),error) 
-SLL_CLEAR_ALLOCATE(rho(1:ncx+1),error)  
+SLL_CLEAR_ALLOCATE(ex(1:nx,1:ny),error)  
+SLL_CLEAR_ALLOCATE(ey(1:nx,1:ny),error) 
+SLL_CLEAR_ALLOCATE(rho(1:nx,1:ny),error)  
 
 SLL_ALLOCATE(x(-1:nx+1),error)  
 SLL_ALLOCATE(y(-1:ny+1),error) 
@@ -39,37 +38,40 @@ SLL_ALLOCATE(y(-1:ny+1),error)
 dimx = 2 * sll_pi
 dimy = 2 * sll_pi
 
-dx = dimx / ncx
-dy = dimy / ncy
+dx = dimx / (nx-1)
+dy = dimy / (ny-1)
 
 x(0) = 0.
 y(0) = 0.
 
-do i=1,ncx+1
+do i=1,nx+1
    x(i) = (i*dx) *(i*dx+1)/(1+dimx)
 enddo
-do j=1,ncy+1
+do j=1,ny+1
    y(j) = (j*dy) *(j*dy+1)/(1+dimy)
 enddo
 
 
 mode = 2
-do j = 1, ncy+1
-   do i = 1, ncx+1
+do j = 1, ny
+   do i = 1, nx
       rho(i,j) = 2_f64 * mode**2 * sin(mode*x(i))*sin(mode*y(j))
    end do
 end do
 
-call initialize(poisson, x, y, ncx, ncy)
-call solve(poisson, ex, ey, rho, ncx, ncy)
+call initialize(poisson, x, y, nx, ny)
+call solve(poisson, ex, ey, rho, nx, ny)
 
-do j = 1, ncy+1
-   do i = 1, ncx+1
+errmax = 0.
+do j = 1, ny
+   do i = 1, nx
       write(11,*) x(i), y(j), rho(i,j),  sin(mode*x(i))*sin(mode*y(j))
+      errmax = max(errmax,abs(rho(i,j)-sin(mode*x(i))*sin(mode*y(j))))
    end do
    write(11,*)
 end do
 close(11)
+print*, 'error = ', errmax
 
 end subroutine test
 
