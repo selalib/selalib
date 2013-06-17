f2cdc57008521ae9a448e5c32cfdb73ca16b124b
Pierre Navaro <navaro@math.unistra.fr>
2013-06-17 16:45:01 +0200

    FEM Poisson solver works for compact bc

--- prototype/src/poisson_solvers/test_poisson_2d_fem.F90
+++ prototype/src/poisson_solvers/test_poisson_2d_fem.F90
@@ -8,11 +8,12 @@ implicit none
 
 sll_int32  :: i, j
 sll_real64 :: dimx, dimy
-sll_int32  :: error, mode
+sll_int32  :: error
+sll_real64 :: mode
 sll_real64, dimension(:),   pointer :: x
 sll_real64, dimension(:),   pointer :: y
-sll_real64, dimension(:,:), pointer :: e_x
-sll_real64, dimension(:,:), pointer :: e_y
+sll_real64, dimension(:,:), pointer :: ex
+sll_real64, dimension(:,:), pointer :: ey
 sll_real64, dimension(:,:), pointer :: rho
 sll_real64 :: errmax
 
@@ -23,53 +24,58 @@ contains
 subroutine test()
 type( poisson_fem ) :: poisson
 sll_real64 :: dx, dy
-sll_int32  :: nx, ny
+sll_int32  :: nc_x, nc_y
 
-nx = 64
-ny = 64
+nc_x = 64
+nc_y = 64
 
-SLL_CLEAR_ALLOCATE(e_x(1:nx+1,1:ny+1),error)  
-SLL_CLEAR_ALLOCATE(e_y(1:nx+1,1:ny+1),error) 
-SLL_CLEAR_ALLOCATE(rho(1:nx+1,1:ny+1),error)  
+SLL_CLEAR_ALLOCATE(ex(1:nc_x+1,1:nc_y+1),error)  
+SLL_CLEAR_ALLOCATE(ey(1:nc_x+1,1:nc_y+1),error) 
+SLL_CLEAR_ALLOCATE(rho(1:nc_x+1,1:nc_y+1),error)  
 
-SLL_ALLOCATE(x(1:nx+1),error)  
-SLL_ALLOCATE(y(1:ny+1),error) 
+SLL_ALLOCATE(x(1:nc_x+1),error)  
+SLL_ALLOCATE(y(1:nc_y+1),error) 
 
-dimx = 1.
-dimy = 1.
+dimx = 1.0
+dimy = 1.0
 
-dx = dimx / nx
-dy = dimy / ny
+dx = dimx / nc_x
+dy = dimy / nc_y
 
-do i=1,nx+1
-   x(i) = ((i-1)*dx)*((i-1)*dx+1)/(1+dimx)
+!Create an irregular mesh
+do i=1,nc_x+1
+   x(i) = (i-1)*dx * 0.5 * ((i-1)*dx+1)
 enddo
-do j=1,ny+1
-   y(j) = ((j-1)*dy)*((j-1)*dy+1)/(1+dimy)
+do j=1,nc_y+1
+   y(j) = (j-1)*dy * 0.5 * ((j-1)*dy+1)
 enddo
 
-mode = 2
-do j = 1, ny+1
-   do i = 1, nx+1
-      rho(i,j) = 2_f64 * mode**2 * sin(2*sll_pi*mode*x(i))*sin(2*sll_pi*mode*y(j))
-      write(10,*) x(i), y(j), rho(i,j)
+mode = 4*sll_pi
+do j = 1, nc_y
+   do i = 1, nc_x
+      rho(i,j) = 2_f64 * mode**2 * sin(mode*x(i))*sin(mode*y(j))
    end do
-   write(10,*) 
 end do
 
-call initialize(poisson, x, y, nx, ny)
-call solve(poisson, e_x, e_y, rho, nx, ny)
+call initialize(poisson, x, y, nc_x+1, nc_y+1)
+call solve(poisson, ex, ey, rho)
 
 errmax = 0.
-do j = 1, ny+1
-   do i = 1, nx+1
-      write(11,*) x(i), y(j), rho(i,j),  sin(mode*x(i))*sin(mode*y(j))
+do j = 2, nc_y
+   do i = 2, nc_x
       errmax = max(errmax,abs(rho(i,j)-sin(mode*x(i))*sin(mode*y(j))))
+   end do
+end do
+print*, 'error = ', errmax
+
+do j = 1, nc_y+1
+   do i = 1, nc_x+1
+      write(11,*) x(i), y(j), rho(i,j), sin(mode*x(i))*sin(mode*y(j))
    end do
    write(11,*)
 end do
 close(11)
-print*, 'error = ', errmax
+
 
 end subroutine test
 
