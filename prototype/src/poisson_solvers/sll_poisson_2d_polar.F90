38eb9305ba6db7cd01d6d5af3dd2a11c81ffedce
Michel Mehrenberger <mehrenbe@math.unistra.fr>
2013-07-01 11:13:39 +0200

    up

--- prototype/src/poisson_solvers/sll_poisson_2d_polar.F90
+++ prototype/src/poisson_solvers/sll_poisson_2d_polar.F90
@@ -109,6 +109,9 @@ module sll_poisson_2d_polar
      !for the tridiagonal solver
      sll_real64, dimension(:), pointer :: a,cts
      sll_int32, dimension(:), pointer :: ipiv
+     !for quasi neutral solver
+     sll_real64, dimension(:), pointer ::dlog_density,inv_Te
+     
   end type sll_plan_poisson_polar
 
   !flags for boundary conditions
@@ -138,7 +141,7 @@ contains
   !>nr and ntheta : number of space in direction r and theta
   !>bc : boundary conditions, can be combined with +
   !>bc is optionnal and default is Dirichlet condition in rmin and rmax
-  function new_plan_poisson_polar(dr,rmin,nr,ntheta,bc) result(this)
+  function new_plan_poisson_polar(dr,rmin,nr,ntheta,bc,dlog_density,inv_Te) result(this)
 
     implicit none
 
@@ -146,6 +149,7 @@ contains
     sll_int32 :: nr, ntheta
     sll_int32, optional :: bc(2)
     type(sll_plan_poisson_polar), pointer :: this
+    sll_real64,dimension(:),optional ::dlog_density,inv_Te
 
     sll_int32 :: err
     sll_real64, dimension(:), allocatable :: buf
@@ -161,7 +165,21 @@ contains
     SLL_ALLOCATE(this%a(3*(nr-1)),err)
     SLL_ALLOCATE(this%cts(7*(nr-1)),err)
     SLL_ALLOCATE(this%ipiv(nr-1),err)
-
+    
+    SLL_ALLOCATE(this%dlog_density(nr),err)
+    SLL_ALLOCATE(this%inv_Te(nr),err)
+    
+    this%dlog_density = 0._f64
+    this%inv_Te = 0._f64
+    
+    if(present(dlog_density))then
+      this%dlog_density = dlog_density
+    endif
+    if(present(inv_Te))then
+      this%inv_Te = inv_Te
+    endif
+    
+    
     this%dr=dr
     this%rmin=rmin
     this%nr=nr
@@ -294,9 +312,10 @@ contains
 
       do i=2,nr
         r = rmin + (i-1)*dr
+        plan%a(3*(i-1)  ) = -1.0_f64/dr**2-1.0_f64/(2*dr*r)-plan%dlog_density(i-1)/(2._f64*dr)
         plan%a(3*(i-1)  ) = -1.0_f64/dr**2-1.0_f64/(2*dr*r)
-        plan%a(3*(i-1)-1) =  2.0_f64/dr**2+(kval/r)**2
-        plan%a(3*(i-1)-2) = -1.0_f64/dr**2+1.0_f64/(2*dr*r)
+        plan%a(3*(i-1)-1) =  2.0_f64/dr**2+(kval/r)**2+plan%inv_Te(i-1)
+        plan%a(3*(i-1)-2) = -1.0_f64/dr**2+1.0_f64/(2*dr*r)+plan%dlog_density(i-1)/(2._f64*dr)
 
         plan%fk(i)=fft_get_mode(plan%pfwd,plan%f_fft(i,1:ntheta),k)
       enddo
@@ -548,5 +567,175 @@ contains
         ierr = ierr_sup_1em12 
     end if
   end subroutine poisson_solve_polar
+
+
+  !>subroutine poisson_solve_polar(plan,f,phi)
+  !>poisson solver for polar system : -\Delta (phi)=f
+  !>plan : sll_plan_poisson_polar, contains data for the solver
+  !>f : distribution function, size (nr+1)*(ntheta+1), input
+  !>phi : unknown field, size (nr+1)*(ntheta+1), output
+  !>initialization must be done outside the solver
+  subroutine poisson_solve_polar_dk(plan,f,phi,ierr)
+
+    implicit none
+
+    type(sll_plan_poisson_polar), pointer :: plan
+    sll_real64, dimension(plan%nr+1,plan%ntheta+1), intent(in)  :: f
+    sll_real64, dimension(plan%nr+1,plan%ntheta+1), intent(out) :: phi
+    sll_int32 ,                           optional, intent(out) :: ierr
+
+    sll_real64 :: rmin,dr
+    sll_int32  :: nr, ntheta,bc(2)
+
+    sll_real64 :: r
+    sll_int32  :: i, k, ind_k
+    sll_real64 :: kval, err
+    sll_comp64 :: err_loc
+    sll_int32  :: ierr_sup_1em12
+
+    nr     = plan%nr
+    ntheta = plan%ntheta
+    rmin   = plan%rmin
+    dr     = plan%dr
+
+    bc         = plan%bc
+    plan%f_fft = f
+
+    do i=1,nr+1
+      call fft_apply_plan(plan%pfwd,plan%f_fft(i,1:ntheta),plan%f_fft(i,1:ntheta))
+    end do
+
+    ierr_sup_1em12 = 0
+    ! poisson solver
+    do k = 0,ntheta/2
+      ind_k=k
+      kval=real(ind_k,f64)
+
+      do i=2,nr
+        r=rmin+real(i-1,f64)*dr
+        plan%a(3*(i-1))=-1.0_f64/dr**2-1.0_f64/(2.0_f64*dr*r)
+        plan%a(3*(i-1)-1)=2.0_f64/dr**2+(kval/r)**2
+        plan%a(3*(i-1)-2)=-1.0_f64/dr**2+1.0_f64/(2.0_f64*dr*r)
+
+        plan%fk(i)=fft_get_mode(plan%pfwd,plan%f_fft(i,1:ntheta),k)!ind_k)          
+      enddo
+
+      plan%phik=0.0_f64
+
+      !boundary condition at rmin
+      if(bc(1)==DIRICHLET)then !Dirichlet
+        plan%a(1)=0.0_f64
+      endif
+      if(bc(1)==NEUMANN)then
+        plan%a(2)=plan%a(2)+plan%a(1) !Neumann
+        plan%a(1)=0._f64
+      endif
+      if(bc(1)==NEUMANN_MODE0)then 
+        if(k==0)then!Neumann for mode zero
+          plan%a(2)=plan%a(2)+plan%a(1)
+          plan%a(1)=0._f64
+        else !Dirichlet for other modes
+          plan%a(1)=0._f64
+        endif
+      endif
+
+      !boundary condition at rmax
+      if(bc(2)==DIRICHLET)then !Dirichlet
+        plan%a(3*(nr-1))=0.0_f64
+      endif
+      if(bc(2)==NEUMANN)then
+        plan%a(3*(nr-1)-1)=plan%a(3*(nr-1)-1)+plan%a(3*(nr-1)) !Neumann
+        plan%a(3*(nr-1))=0.0_f64
+      endif
+      if(bc(2)==NEUMANN_MODE0)then 
+        if(k==0)then!Neumann for mode zero
+          plan%a(3*(nr-1)-1)=plan%a(3*(nr-1)-1)+plan%a(3*(nr-1))
+          plan%a(3*(nr-1))=0.0_f64
+        else !Dirichlet for other modes
+          plan%a(3*(nr-1))=0.0_f64
+        endif
+      endif
+
+      call setup_cyclic_tridiag(plan%a,nr-1,plan%cts,plan%ipiv)
+      call solve_cyclic_tridiag(plan%cts,plan%ipiv,plan%fk(2:nr),nr-1,plan%phik(2:nr))
+
+      !boundary condition at rmin
+      if(bc(1)==1)then !Dirichlet
+        plan%phik(1)=0.0_f64
+      endif
+      if(bc(1)==2)then
+        plan%phik(1)=plan%phik(2) !Neumann
+      endif
+      if(bc(1)==3)then 
+        if(k==0)then!Neumann for mode zero
+          plan%phik(1)=plan%phik(2)
+        else !Dirichlet for other modes
+          plan%phik(1)=0.0_f64
+        endif
+      endif
+
+      !boundary condition at rmax
+      if(bc(2)==1)then !Dirichlet
+        plan%phik(nr+1)=0.0_f64
+      endif
+      if(bc(2)==2)then
+        plan%phik(nr+1)=plan%phik(nr) !Neumann
+      endif
+      if(bc(2)==3)then 
+        if(k==0)then!Neumann for mode zero
+          plan%phik(nr+1)=plan%phik(nr)
+        else !Dirichlet for other modes
+          plan%phik(nr+1)=0.0_f64
+        endif
+      endif
+
+      err = 0._f64
+      do i=4,nr-4
+        r=rmin+real(i-1,f64)*dr
+        err_loc=(plan%phik(i+1)-2*plan%phik(i)+plan%phik(i-1))/dr**2
+        err_loc=err_loc+(plan%phik(i+1)-plan%phik(i-1))/(2._f64*r*dr)
+        err_loc=-err_loc+kval**2/r**2*plan%phik(i)
+        err_loc=(err_loc-plan%fk(i))
+        if(abs(err_loc)>err)then
+          err=abs(err_loc)
+        endif
+      enddo
+
+      if (err>1e-12) then
+        ierr_sup_1em12 = ierr_sup_1em12 + 1
+      endif
+
+      if(err>1e-4)then
+        do i=2,nr
+          r=rmin+real(i-1,f64)*dr
+          err_loc=(plan%phik(i+1)-2*plan%phik(i)+plan%phik(i-1))/dr**2
+          err_loc=err_loc+(plan%phik(i+1)-plan%phik(i-1))/(2._f64*r*dr)
+          err_loc=-err_loc+kval**2/r**2*plan%phik(i)
+          print *,r,real(err_loc),aimag(err_loc),real(plan%fk(i)),aimag(plan%fk(i))
+        enddo
+        stop
+      endif
+
+      do i=1,nr+1
+        call fft_set_mode(plan%pinv,phi(i,1:ntheta),plan%phik(i),k)!ind_k)
+      end do
+    end do
+
+    ! FFT INVERSE
+    do i=1,nr+1
+      call fft_apply_plan(plan%pinv,phi(i,1:ntheta),phi(i,1:ntheta))
+    end do
+
+    phi(:,ntheta+1)=phi(:,1)
+
+    if (ierr_sup_1em12.ne.0) then
+      if (present(ierr)) &
+        ierr = ierr_sup_1em12 
+    end if
+  end subroutine poisson_solve_polar_dk
+
+
+
+
 
 end module sll_poisson_2d_polar
