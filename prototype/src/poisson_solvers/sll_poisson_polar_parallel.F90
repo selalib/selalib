bc00f54902d2092fdb906d72421f8dc779403bd1
Pierre Navaro <navaro@math.cnrs.fr>
2013-07-08 09:01:17 +0200

    Improve Polar Poisson Solver

--- prototype/src/poisson_solvers/sll_poisson_polar_parallel.F90
+++ prototype/src/poisson_solvers/sll_poisson_polar_parallel.F90
@@ -31,12 +31,18 @@ module sll_poisson_polar_parallel
   !>type sll_poisson_polar
   !>type for the Poisson solver in polar coordinate
   type sll_poisson_polar
-   sll_real64                          :: dr, rmin, rmax
-   sll_int32                           :: nr, nt
+   sll_real64                          :: rmin
+   sll_real64                          :: rmax
+   sll_real64                          :: dr
+   sll_int32                           :: nr
+   sll_int32                           :: nt
    sll_int32                           :: bc(2)
-   type(sll_fft_plan), pointer         :: pfwd,pinv
-   sll_comp64, dimension(:),   pointer :: fk,phik
-   sll_real64, dimension(:),   pointer :: a,cts
+   type(sll_fft_plan), pointer         :: fw
+   type(sll_fft_plan), pointer         :: bw
+   sll_comp64, dimension(:),   pointer :: fk
+   sll_comp64, dimension(:),   pointer :: phik
+   sll_real64, dimension(:),   pointer :: a
+   sll_real64, dimension(:),   pointer :: cts
    sll_int32,  dimension(:),   pointer :: ipiv
    type(layout_2D),  pointer           :: layout_r !< layout sequential in x
    type(layout_2D),  pointer           :: layout_t !< layout sequential in y
@@ -60,13 +66,12 @@ module sll_poisson_polar_parallel
 contains
 
   !> Initialize the Poisson solver in polar coordinates
-  subroutine initialize_poisson_polar(this, &
-             layout_r, layout_t,        &
-             rmin,rmax,nr,nt,bc_rmin,bc_rmax)
+  subroutine initialize_poisson_polar(this, layout_t, &
+                                      rmin, rmax, nr, nt, &
+                                      bc_rmin,bc_rmax)
 
     implicit none
     type(sll_poisson_polar)  :: this
-    type(layout_2D), pointer :: layout_r !< sequential in r direction
     type(layout_2D), pointer :: layout_t !< sequential in theta direction
 
     sll_real64               :: rmin    !< rmin
@@ -76,6 +81,7 @@ contains
     sll_int32, optional      :: bc_rmin !< radial boundary conditions
     sll_int32, optional      :: bc_rmax !< radial boundary conditions
     sll_int32                :: error
+    sll_int32                :: psize
     sll_real64, dimension(:), allocatable :: buf
 
     SLL_ALLOCATE(this%fk(nr+1),error)
@@ -99,12 +105,18 @@ contains
     end if
 
     SLL_ALLOCATE(buf(nt),error)
-    this%pfwd => fft_new_plan(nt,buf,buf,FFT_FORWARD,FFT_NORMALIZE)
-    this%pinv => fft_new_plan(nt,buf,buf,FFT_INVERSE)
+    this%fw => fft_new_plan(nt,buf,buf,FFT_FORWARD,FFT_NORMALIZE)
+    this%bw => fft_new_plan(nt,buf,buf,FFT_INVERSE)
     SLL_DEALLOCATE_ARRAY(buf,error)
 
     ! Layout and local sizes for FFTs in r-direction
-    this%layout_r => layout_r
+    psize = sll_get_collective_size(sll_world_collective)
+    this%layout_r => new_layout_2D( sll_world_collective )
+    call initialize_layout_with_distributed_2D_array( nr+1,   &
+                                                      nt+1,   &
+                                                      1,      &
+                                                      psize,  &
+                                                      this%layout_r )
     call compute_local_sizes_2d(this%layout_r,nr_loc,nt_loc)
     SLL_CLEAR_ALLOCATE(this%f_r(1:nr_loc,1:nt_loc),error)
 
@@ -126,8 +138,8 @@ contains
     type(sll_poisson_polar), pointer :: this
     sll_int32 :: err
     if (associated(this)) then
-       call fft_delete_plan(this%pfwd)
-       call fft_delete_plan(this%pinv)
+       call fft_delete_plan(this%fw)
+       call fft_delete_plan(this%bw)
        SLL_DEALLOCATE_ARRAY(this%fk,err)
        SLL_DEALLOCATE_ARRAY(this%phik,err)
        SLL_DEALLOCATE_ARRAY(this%a,err)
@@ -138,21 +150,16 @@ contains
 
   end subroutine delete_poisson_polar
 
-!===================
-!  Poisson solver
-!===================
-
-  !>poisson solver for polar system : -\Delta (phi)=rhs
+  !>Poisson solver for polar coordinates : -\Delta (phi)=rhs
   !>initialization must be done outside the solver
   subroutine solve_poisson_polar(this,rhs,phi)
 
     implicit none
 
     type(sll_poisson_polar) :: this !< contains data for the solver
-    !>rhs field, size (nr+1)*(nt+1), input
-    sll_real64, dimension(this%nr+1,this%nt+1), intent(in)  :: rhs
-    !>unknown field, size (nr+1)*(nt+1), output
-    sll_real64, dimension(this%nr+1,this%nt+1), intent(out) :: phi
+    sll_real64, dimension(:,:), intent(in)  :: rhs
+    sll_real64, dimension(:,:), intent(inout) :: phi
+    sll_int32 :: global(2)
 
     sll_real64 :: rmin,dr
     sll_int32  :: nr, nt,bc(2)
@@ -169,12 +176,12 @@ contains
 
     call verify_argument_sizes_par(this%layout_t, rhs, phi)
 
-    this%f_t = rhs
-    
     call compute_local_sizes_2d( this%layout_t, nr_loc, nt_loc )
     
-    do i=1,nr_loc+1
-      call fft_apply_plan(this%pfwd,this%f_t(i,1:nt),this%f_t(i,1:nt))
+    this%f_t = rhs
+
+    do i=1,nr_loc
+      call fft_apply_plan(this%fw,this%f_t(i,1:nt),this%f_t(i,1:nt))
     end do
 
     call apply_remap_2D( this%rmp_tr, this%f_t, this%f_r )
@@ -182,14 +189,15 @@ contains
 
     do k = 0,nt_loc/2
 
-      kval=real(k,f64)
+      global = local_to_global_2D( this%layout_r, (/1, k/))
+      kval=real(global(2)-1,f64)
 
       do i=2,nr
         r=rmin+(i-1)*dr
         this%a(3*(i-1)  )=-1.0_f64/dr**2-1.0_f64/(2.0_f64*dr*r)
         this%a(3*(i-1)-1)= 2.0_f64/dr**2+(kval/r)**2
         this%a(3*(i-1)-2)=-1.0_f64/dr**2+1.0_f64/(2.0_f64*dr*r)
-        this%fk(i)=fft_get_mode(this%pfwd,this%f_r(i,1:nt),k)
+        this%fk(i)=fft_get_mode(this%fw,this%f_r(i,:),k)
       enddo
 
       this%phik=0.0_f64
@@ -199,7 +207,7 @@ contains
       else if(bc(1)==SLL_NEUMANN) then
         this%a(2)=this%a(2)+this%a(1) 
         this%a(1)=0._f64
-      else if(bc(1)==SLL_NEUMANN_MODE0)then 
+      else if(bc(1)==SLL_NEUMANN_MODE_0)then 
         if(k==0) then
           this%a(2)=this%a(2)+this%a(1)
           this%a(1)=0._f64
@@ -214,7 +222,7 @@ contains
       else if(bc(2)==SLL_NEUMANN)then
         this%a(3*(nr-1)-1)=this%a(3*(nr-1)-1)+this%a(3*(nr-1)) 
         this%a(3*(nr-1))=0.0_f64
-      else if(bc(2)==SLL_NEUMANN_MODE0)then 
+      else if(bc(2)==SLL_NEUMANN_MODE_0)then 
         if(k==0)then
           this%a(3*(nr-1)-1)=this%a(3*(nr-1)-1)+this%a(3*(nr-1))
           this%a(3*(nr-1))=0.0_f64
@@ -224,7 +232,8 @@ contains
       endif
 
       call setup_cyclic_tridiag(this%a,nr-1,this%cts,this%ipiv)
-      call solve_cyclic_tridiag(this%cts,this%ipiv,this%fk(2:nr),nr-1,this%phik(2:nr))
+      call solve_cyclic_tridiag(this%cts,this%ipiv,this%fk(2:nr), &
+                                nr-1,this%phik(2:nr))
 
       !boundary condition at rmin
       if(bc(1)==1)then
@@ -253,7 +262,7 @@ contains
       endif
 
       do i=1,nr+1
-        call fft_set_mode(this%pinv,phi(i,1:nt),this%phik(i),k)
+        call fft_set_mode(this%bw,this%f_r(i,:),this%phik(i),k)
       end do
 
     end do
@@ -261,12 +270,40 @@ contains
     call apply_remap_2D( this%rmp_rt, this%f_r, this%f_t )
     call compute_local_sizes_2d( this%layout_t, nr_loc, nt_loc )
     do i=1,nr_loc+1
-      call fft_apply_plan(this%pinv,phi(i,1:nt),phi(i,1:nt))
-      phi(i,nt+1)=phi(i,1)
+      call fft_apply_plan(this%bw,this%f_t(i,:),phi(i,:))
     end do
 
 
   end subroutine solve_poisson_polar
 
+  subroutine verify_argument_sizes_par(layout, rho, phi)
+    type(layout_2D), pointer       :: layout
+    sll_real64, dimension(:,:)     :: rho
+    sll_real64, dimension(:,:)     :: phi
+    sll_int32,  dimension(2)       :: n ! nx_loc, ny_loc
+    sll_int32                      :: i
+
+    ! Note that this checks for strict sizes, not an array being bigger
+    ! than a certain size, but exactly a desired size... This may be a bit
+    ! too stringent.
+    call compute_local_sizes_2d( layout, n(1), n(2) )
+
+    do i=1,2
+       if ( (n(i)/=size(rho,i)) .or. (n(i)/=size(phi,i))  ) then
+          print*, 'ERROR: solve_poisson_polar_parallel()', &
+               'size of either rhs or phi does not match expected size. '
+          if (i==1) then
+             print*, 'solve_poisson_polar_parallel(): ', &
+                  'mismatch in direction r'
+          elseif (i==2) then
+             print*, 'solve_poisson_polar_parallel(): ', &
+                  'mismatch in direction theta'
+          endif
+          print *, 'Exiting...'
+          call sll_halt_collective()
+          stop
+       endif
+    enddo
+  end subroutine verify_argument_sizes_par
 
 end module sll_poisson_polar_parallel
