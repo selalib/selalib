e3a02f227aa095d15cabedce20b8534ec01340bc
Edwin Chacon-Golcher <edwincg@gmail.com>
2013-07-10 14:30:36 +0200

    chasing bug in 4d qns simulation

--- prototype/src/simulation/unit_test_4d_qns_general.F90
+++ prototype/src/simulation/unit_test_4d_qns_general.F90
@@ -52,7 +52,8 @@ program qns_4d_general
 #define NPTS2 32
 #define NPTS3 32
 #define NPTS4 32
-
+#define SPL_DEG1 3
+#define SPL_DEG2 3
   ! logical mesh for space coordinates
   mx => new_logical_mesh_2d( NPTS1, NPTS2,       & 
        eta1_min=.0_f64, eta1_max=4.0_f64*sll_pi)
@@ -119,8 +120,8 @@ program qns_4d_general
        func_zero, &
        func_one, &
        func_zero, &
-       3, & ! spline degree
-       2, & ! spline degree2
+       SPL_DEG1, & 
+       SPL_DEG2, & 
        SLL_PERIODIC, &
        SLL_PERIODIC, &
        SLL_PERIODIC, &
