0fff609c3a4ef5f31cbbf484ff918eae65a901bf
Pierre Navaro <navaro@math.cnrs.fr>
2013-07-11 18:00:18 +0200

    Fix errors in periodic poisson solver 2d and 1d

--- prototype/src/poisson_solvers/test_poisson_1d.F90
+++ prototype/src/poisson_solvers/test_poisson_1d.F90
@@ -2,17 +2,11 @@ program test_poisson_1d
 #include "sll_working_precision.h"
 #include "sll_memory.h"
 #include "sll_assert.h"
-
-use sll_constants
-use sll_module_mapped_meshes_1d
-use sll_common_coordinate_transformations
+#include "sll_constants.h"
 use sll_cubic_splines
 use sll_poisson_1D_periodic
 
 implicit none
-
-type (sll_mapped_mesh_1d_analytic), target :: mesh_1d
-class(sll_mapped_mesh_1d_base), pointer    :: m
 
 sll_real64, dimension(:), allocatable :: ex
 sll_real64, dimension(:), allocatable :: ex_exact
@@ -21,33 +15,27 @@ sll_real64, dimension(:), allocatable :: rho
 type (poisson_1d_periodic)         :: poisson
 
 sll_int32   :: nc_eta1
-sll_real64  :: eta1_min, eta1_max
+sll_real64  :: eta1_min, eta1_max, delta_eta1, x
 sll_int32   :: error
 sll_int32   :: mode
 sll_int32   :: i
 
 nc_eta1 = 128
 
-call initialize_mesh_1d_analytic( mesh_1d, "mesh_1d",        &
-                                  nc_eta1 + 1,               &
-                                  linear_map_poisson_f,      &
-                                  linear_map_poisson_jac_f )
-
-call mesh_1d%write_to_file()
-m => mesh_1d
-
 SLL_ALLOCATE(rho(nc_eta1+1),error)
 SLL_ALLOCATE(ex(nc_eta1+1),error)
 SLL_ALLOCATE(ex_exact(nc_eta1+1),error)
 
+eta1_min = 0.0
+eta1_max = 2*sll_pi
+delta_eta1 = (eta1_max-eta1_min) / nc_eta1
 mode = 4
 do i=1,nc_eta1+1
-   rho(i)      =  mode**2*sin(mode*mesh_1d%x1_at_node(i))
-   ex_exact(i) = -mode*cos(mode*mesh_1d%x1_at_node(i))
+   x = (i-1)*delta_eta1
+   rho(i)      =  mode**2*sin(mode*x)
+   ex_exact(i) = -mode*cos(x)
 end do
 
-eta1_min = mesh_1d%x1_at_node(1)
-eta1_max = mesh_1d%x1_at_node(nc_eta1+1)
 call new(poisson, eta1_min, eta1_max, nc_eta1, error) 
 
 call solve(poisson, ex, rho)
