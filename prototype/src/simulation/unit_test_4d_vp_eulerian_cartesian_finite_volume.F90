2df0ddf0f3a4335e94d1b60f3820f19bd04a7339
Nhung PHAM <pham@math.unistra.fr>
2013-08-01 15:43:54 +0200

    have found the bug

--- prototype/src/simulation/unit_test_4d_vp_eulerian_cartesian_finite_volume.F90
+++ prototype/src/simulation/unit_test_4d_vp_eulerian_cartesian_finite_volume.F90
@@ -62,7 +62,7 @@ program vp_cartesian_4d
 #define ETA3MAX 1.0_f64
 #define ETA4MIN 0.0_f64
 #define ETA4MAX 1.0_f64
-#define TMAX 1.e-1_f64
+#define TMAX 2.e-1_f64
 !#define TMAX 0.0_f64
 #define CFL 0.5_f64
 #define EPSILON 0.05
