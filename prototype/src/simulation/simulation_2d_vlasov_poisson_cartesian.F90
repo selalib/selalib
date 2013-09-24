06ae9228180adce9ebdbefd768c6021a0413598a
Mehrenberger <mehrenbe@math.unistra.fr>
2013-09-24 21:08:11 +0200

    work on initialization

--- prototype/src/simulation/simulation_2d_vlasov_poisson_cartesian.F90
+++ prototype/src/simulation/simulation_2d_vlasov_poisson_cartesian.F90
@@ -38,25 +38,105 @@ contains
     class(sll_simulation_2d_vlasov_poisson_cart), intent(inout) :: sim
     character(len=*), intent(in)                                :: filename
     sll_int32             :: IO_stat
-    sll_real64            :: dt
-    sll_int32             :: number_iterations
-    sll_int32             :: num_cells_x1
-    sll_int32             :: num_cells_x2
     sll_int32, parameter  :: input_file = 99
+    sll_real64            :: xmin, vmin, vmax
+    sll_int32             :: Ncx, nbox, Ncv
+    sll_int32             :: interpol_x, order_x, interpol_v, order_v
+    sll_real64            :: dt
+    sll_int32             :: nbiter, freqdiag
+    sll_real64            :: kmode, eps
+    sll_int32             :: is_delta_f
+    logical               :: driven
+    sll_real64            :: t0, twL, twR, tstart, tflat, tL, tR, Edrmax, omegadr
+    logical               :: turn_drive_off
+    sll_int32, parameter  :: param_out = 37, param_out_drive = 40
+    
+    sll_real64            :: xmax
 
-    namelist /sim_params/ dt, number_iterations
-    namelist /grid_dims/ num_cells_x1, num_cells_x2
-    ! Try to add here other parameters to initialize the mesh values like
-    ! xmin, xmax and also for the distribution function initializer.
+
+      
+    ! namelists for data input
+    namelist / geom / xmin, Ncx, nbox, vmin, vmax, Ncv
+    namelist / interpolator / interpol_x, order_x, interpol_v, order_v
+    namelist / time_iterations / dt, nbiter, freqdiag
+    namelist / landau / kmode, eps, is_delta_f, driven 
+    namelist / drive / t0, twL, twR, tflat, tL, tR, turn_drive_off, Edrmax, omegadr
+
     open(unit = input_file, file=trim(filename),IOStat=IO_stat)
     if( IO_stat /= 0 ) then
        print *, '#init_vp2d_par_cart() failed to open file ', filename
        STOP
     end if
-    read(input_file, sim_params)
-    read(input_file,grid_dims)
+    read(input_file, geom) 
+    read(input_file, time_iterations)
+    read(input_file, landau)
+    if (driven) then
+        read(input_file, drive)
+        eps = 0.0  ! no initial perturbation for driven simulation
+    end if
     close(input_file)
+    
+    xmax = nbox * 2._f64 * sll_pi / kmode
+    
+    
+    if(sll_get_collective_rank(sll_world_collective)==0)then
+      print *,'##geom'
+      print *,'#xmin=',xmin
+      print *,'#Ncx=',Ncx
+      print *,'#Nbox=',Nbox
+      print *,'#vmin=',vmin
+      print *,'#vmax=',vmax
+      print *,'#Ncv=',Ncv
 
+      print *,'##interpolator'
+      print *,'#interpol_x=',interpol_x
+      print *,'#order_x=',order_x
+      print *,'#interpol_v=',interpol_v
+      print *,'#order_v=',order_v
+
+      print *,'##time_iterations'
+      print *,'#dt=',dt
+      print *,'#nbiter=',nbiter
+      print *,'#freqdiag=',freqdiag
+
+      print *,'##landau'
+      print *,'#kmode=',kmode
+      print *,'#eps=',eps
+      print *,'#is_delta_f=',is_delta_f
+      print *,'#driven=',driven
+
+
+      print *,'##drive'
+      print *,'#t0=',t0
+      print *,'#twL=',twL
+      print *,'#twR=',twR
+      print *,'#tflat=',tflat
+      print *,'#tL=',tL
+      print *,'#tR=',tR
+      print *,'#turn_drive_off=',turn_drive_off
+      print *,'#Edrmax=',Edrmax
+      print *,'#omegadr=',omegadr
+    
+
+      open(unit = param_out, file = 'param_out.dat') 
+        write(param_out,'(A6,2f10.3,I5,2f10.3,I5,f10.3,I8,I5,I2,f10.3)') &
+         "landau", xmin, xmax, ncx, vmin, vmax, ncv, &
+         dt, nbiter, freqdiag, is_delta_f, kmode
+      close(param_out)
+
+      if (driven) then
+        open(unit = param_out_drive, file = 'param_out_drive.dat') 
+        write(param_out_drive,*) t0, twL, twR, tflat, tL, tR, &
+          Edrmax, omegadr
+        !! warning we have skipped tstart  in
+        !! t0, twL, twR, tstart, tflat, tL, tR            
+        close(param_out_drive)
+      end if
+
+    endif
+
+
+    
     !sim%dt = dt
     !sim%num_iterations = number_iterations
     ! In this particular simulation, since the system is periodic, the number
