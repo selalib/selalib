d09ef36ed973cae7a249fb124518dc7f0a5eee0f
Michel Mehrenberger <mehrenbe@math.unistra.fr>
2013-09-24 18:32:54 +0200

    add skeleton for translation of VP1D_deltaf_BSL in simulation class

---
+++ prototype/src/simulation/unit_test_2d.F90
@@ -0,0 +1,33 @@
+! Sample computation with the following characteristics:
+! - vlasov-poisson
+! - 2D cartesian: x, vx (or x1, x2)
+! - parallel
+
+program vlasov_poisson_2d
+  use sll_simulation_2d_vlasov_poisson_cartesian
+  use sll_collective
+  implicit none
+
+  character(len=256) :: filename
+  character(len=256) :: filename_local
+  type(sll_simulation_2d_vlasov_poisson_cart) :: simulation
+
+  print *, '#Booting parallel environment...'
+  call sll_boot_collective() ! Wrap this up somewhere else
+
+  ! In this test, the name of the file to open is provided as a command line
+  ! argument.
+  call getarg(1, filename)
+  filename_local = trim(filename)
+  call simulation%init_from_file(filename_local)
+  call simulation%run( )
+  call delete_vp2d_par_cart(simulation)
+  print *, '#reached end of vp2d test'
+  print *, '#PASSED'
+
+  call sll_halt_collective()
+
+
+end program vlasov_poisson_2d
+
+
