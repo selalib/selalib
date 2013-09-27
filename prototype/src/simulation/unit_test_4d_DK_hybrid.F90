b064435e352982e1b218592704c21a455cce04fb
Virginie Grandgirard <virginie.grandgirard@cea.fr>
2013-09-27 17:57:03 +0200

    [DK4D] Starting point for the development of a 4D drift-kinetic simulation.

---
+++ prototype/src/simulation/unit_test_4d_DK_hybrid.F90
@@ -0,0 +1,57 @@
+! Sample computation with the following characteristics:
+! - drift kinetic
+! - 4D: x1,x2,x3,v3 (or v1) with cartesian coordinate 
+! - parallel
+
+program DK_hybrid_4d
+#include "sll_working_precision.h"
+  use sll_simulation_4d_DK_hybrid_module
+  use sll_collective
+  use sll_constants
+  use sll_logical_meshes
+  use sll_common_coordinate_transformations
+  use sll_module_coordinate_transformations_2d
+!VG!  use sll_common_array_initializers_module
+  implicit none
+
+  character(len=256) :: filename
+  character(len=256) :: filename_local
+  type(sll_simulation_4d_DK_hybrid)  :: simulation
+  type(sll_logical_mesh_4d), pointer :: logical_mesh4d
+  
+
+  print *, 'Booting parallel environment...'
+  call sll_boot_collective() ! Wrap this up somewhere else
+
+  ! In this test, the name of the file to open is provided as a command line
+  ! argument.
+  call getarg(1,filename)
+  filename_local = trim(filename)
+  call simulation%init_from_file(filename_local)
+
+  ! logical mesh for space coordinates
+  logical_mesh4D => new_logical_mesh_4d( &
+    simulation%nc_x1,simulation%nc_x2, &
+    simulation%nc_x3,simulation%nc_x4,eta1_min=0.1_f64, &
+    eta1_max=14.5_f64,eta2_min=0.0_f64,eta2_max=2.0*sll_pi, &
+    eta3_min=0._f64,eta3_max=1508._f64,eta4_min=-6._f64,eta4_max=6._f64)
+
+  ! initialize the equilibrium distribution function
+
+
+  ! initialize 4D drift-kinetic Vlasov
+  call initialize_4d_DK_hybrid(simulation,logical_mesh4D)
+
+!VG!  ! initialize generalized QN solver
+!VG!  call initialize_QN_general(simulation)
+
+  call simulation%run( )
+  call delete(simulation)
+
+  print *, 'reached end of 4d DK hybrid test'
+  print *, 'PASSED'
+!VG!
+!VG!  call sll_halt_collective()
+end program DK_hybrid_4d
+
+
