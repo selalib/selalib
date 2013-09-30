7d1f5b2093bd1083e3700cacb82887b62a780db0
Edwin Chacon-Golcher <edwincg@gmail.com>
2013-09-30 09:44:33 +0200

    point to point communications seems to be working fine now

--- prototype/src/comm/unit_test.F90
+++ prototype/src/comm/unit_test.F90
@@ -5,11 +5,11 @@ program comm_unit_test
 #include "sll_working_precision.h"
 #include "sll_utilities.h"
   implicit none
-#define PROBLEM_SIZE 8
+#define PROBLEM_SIZE 4
 
   type(sll_comm_real64), pointer :: comm
-  sll_real64, dimension(:), pointer :: array1
-  sll_real64, dimension(:), pointer :: array2
+  sll_real64, dimension(:), allocatable, target :: array1
+  sll_real64, dimension(:), allocatable, target :: array2
   sll_real64, dimension(:), pointer :: buf1
   sll_real64, dimension(:), pointer :: buf2
   sll_int32 :: count
@@ -32,28 +32,46 @@ program comm_unit_test
   SLL_ALLOCATE(array2(PROBLEM_SIZE),ierr)
 
   do i=1,PROBLEM_SIZE
-     array1(i) = rank*size+i
+     array1(i) = rank*(PROBLEM_SIZE)+i
   end do
 
   print *, 'rank: ', rank, 'array = ', array1(:)
 
   buf1 => get_buffer(comm,1)
-  buf1(1:8) = array1(1:8)
+  print *, 'buf1 is asociated: ', associated(buf1)
+
+  buf1(1:PROBLEM_SIZE) = array1(1:PROBLEM_SIZE)
 
   call comm_send_real64( comm, 1, PROBLEM_SIZE)
   print *, 'rank: ', rank, ' sent buffer 1'
-buf2  !por aqui
-  print *, 'rank: ', rank, ' reading from buffer in port 2: ', 
-  buf2 => get_buffer(comm,2)
-  call comm_receive_real64( comm, 2, count )
-  buf2 => get_buffer(comm,2)
-  print *, 'rank: ', rank, ' received buffer 2, count = ', count
+!!$  call sleep(2)
+!!$  buf2 => get_buffer(comm,1)
 
+!  print *, 'rank: ', rank, 'contents of buffer in port 2 : ', buf2(:)
+!  print *, 'rank: ', rank, ' reading from buffer in port 2: ', 
+!  buf2 => get_buffer(comm,2)
+!  call comm_receive_real64( comm, 2, count )
+  buf2 => get_buffer(comm,2)
+  buf2(1:PROBLEM_SIZE) = array1(1:PROBLEM_SIZE)
+  call comm_send_real64( comm, 2, PROBLEM_SIZE )
+  print *, 'rank: ', rank, 'sent buffer 2'
+
+  call comm_receive_real64( comm, 1, count )
+  print *, 'rank: ', rank, 'received buffer 1'
+  buf1 => get_buffer(comm,1)
+!  print *, 'rank: ', rank, ' received buffer 2, count = ', count
+  print *, 'rank: ', rank, 'received contents of buffer in port 1 : ', buf1(:)
+
+  call comm_receive_real64( comm, 2, count )
+  print *, 'rank: ', rank, 'received buffer 2'
+  buf2 => get_buffer(comm,2)
+!  print *, 'rank: ', rank, ' received buffer 2, count = ', count
+  print *, 'rank: ', rank, 'received contents of buffer in port 2 : ', buf2(:)
  
 
   print *, 'proceeding to delete comm...'
   call delete_comm_real64( comm )
-
+  call sll_halt_collective( )
   print *, 'PASSED'
 
 end program comm_unit_test
