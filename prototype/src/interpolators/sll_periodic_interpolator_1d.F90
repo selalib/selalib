bdbc1819f2372432122b473ec2e1aefb39c85a0d
Blanchard <raphael.blanchard@inria.com>
2013-10-04 16:07:39 +0200

    Modifications for Fortran95

--- prototype/src/interpolators/sll_periodic_interpolator_1d.F90
+++ prototype/src/interpolators/sll_periodic_interpolator_1d.F90
@@ -339,7 +339,11 @@ contains  ! ****************************************************************
   end subroutine delete_per1d
 
   subroutine set_coefficients_per1d( interpolator, coeffs )
-    class(per_1d_interpolator),  intent(inout) :: interpolator
+#ifdef STDF95
+    type(per_1d_interpolator), intent(inout)  :: interpolator
+#else  
+    class(per_1d_interpolator), intent(inout) :: interpolator
+#endif
     sll_real64, dimension(:), intent(in), optional :: coeffs
     print *, 'set_coefficients_per1d(): ERROR: This function has not been ', &
          'implemented yet.'
@@ -348,7 +352,11 @@ contains  ! ****************************************************************
 
 
   function get_coefficients_per1d(interpolator)
+#ifdef STDF95
+    type(per_1d_interpolator), intent(in)  :: interpolator
+#else  
     class(per_1d_interpolator), intent(in) :: interpolator
+#endif
     sll_real64, dimension(:), pointer            :: get_coefficients_per1d     
     
     print *, 'get_coefficients_per1d(): ERROR: This function has not been ', &
