745712e69fca94620a3437135cd7b974174cee96
Edwin CHACON-GOLCHER <chacongolcher@math.unistra.fr>
2013-10-10 18:21:48 +0200

    fixed errors in eulerian simulation, seems to pass now...

--- prototype/src/poisson_solvers/sll_poisson_2d_periodic_cartesian_par.F90
+++ prototype/src/poisson_solvers/sll_poisson_2d_periodic_cartesian_par.F90
@@ -112,7 +112,7 @@ contains
          loc_sz_x1, &
          loc_sz_x2 )
 
-    plan%seq_x1_local_sz_x1 = loc_sz_x1 
+    plan%seq_x1_local_sz_x1 = ncx + 1 !loc_sz_x1 
     plan%seq_x1_local_sz_x2 = loc_sz_x2
 
     SLL_ALLOCATE( plan%fft_x_array(loc_sz_x1,loc_sz_x2),ierr)
@@ -224,9 +224,9 @@ contains
     npy_loc = plan%seq_x2_local_sz_x2
 
     call apply_remap_2D( plan%rmp_xy, plan%fft_x_array, plan%fft_y_array )
-write(*,*) 'ici3'
+
     call fft_apply_plan(plan%py, plan%fft_y_array, plan%fft_y_array) 
-write(*,*) 'ici4'
+
     ! This should be inside the FFT plan...
     normalization = 1.0_f64/(ncx*ncy)
 
@@ -286,10 +286,10 @@ write(*,*) 'ici4'
           end if
        enddo
     enddo
-write(*,*) 'ici5'
+
     ! Inverse FFTs in y-direction
     call fft_apply_plan(plan%py_inv, plan%fft_y_array, plan%fft_y_array) 
-write(*,*) 'ici6'
+
     ! Force the periodicity condition in the y-direction. CAN'T USE THE FFT
     ! INTERFACE SINCE THIS POINT FALLS OUTSIDE OF THE POINTS IN THE ARRAY
     ! TOUCHED BY THE FFT. This is another reason to permit not including the
@@ -297,10 +297,10 @@ write(*,*) 'ici6'
     do i=1,npx_loc
        plan%fft_y_array(i,npy_loc) = plan%fft_y_array(i,1)
     end do
-write(*,*) 'ici7'
+
     ! Prepare to take inverse FFTs in x-direction
     call apply_remap_2D( plan%rmp_yx, plan%fft_y_array, plan%fft_x_array )
-write(*,*) 'ici8'
+
     npx_loc = plan%seq_x1_local_sz_x1 
     npy_loc = plan%seq_x1_local_sz_x2 
 
