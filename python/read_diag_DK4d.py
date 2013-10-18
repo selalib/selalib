e3dbb48e9d18ea129db60e8fe43463a30a002ef7
Aurore Back <aurore.back@gmail.com>
2013-10-18 08:21:51 +0200

    file python

--- python/read_diag_DK4d.py
+++ python/read_diag_DK4d.py
@@ -2,9 +2,9 @@ import utils_func as ut
 import matplotlib as mp
 import matplotlib.pyplot as mpp
 
-f_diag = ut.loadHDF5('DK4d_diag_d00001.h5')
+f_diag = ut.loadHDF5('/Users/back/selalib/prototype/build/DK4d_diag_d00000.h5')
 
-fig = mpp.figure(figsize=(12,12))
+fig = mpp.figure(figsize=(10,8))
 
 ax1 = fig.add_subplot(2,2,1)
 p1  = ax1.pcolormesh(f_diag.phi2d_x1x2)
