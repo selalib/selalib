da1b70883f4599035f9098781d5f31e8bd2bf05f
Michel Mehrenberger <mehrenbe@math.unistra.fr>
2013-10-23 13:54:33 +0200

    complete interface for reduction

--- prototype/src/reduction/unit_test.F90
+++ prototype/src/reduction/unit_test.F90
@@ -31,11 +31,49 @@ implicit none
 
   err=maxval(abs(data_3d-1._f64))
   
+  call compute_reduction_4d_to_3d_direction4(&
+    data_4d, &
+    data_3d, &
+    Npts(1), &
+    Npts(2), &
+    Npts(3), &
+    Npts(4), &
+    delta, &
+    my_compute_integral_trapezoid_1d)
+  print *,'#err1=',err
+
+  err=err+maxval(abs(data_3d-1._f64))
+
+
   
-  print *,'#err=',err
+  print *,'#err2=',err
 
   if(err==0._f64)then
     print *,'#PASSED'
   endif
+
+
+
+
+
+contains
+
+
+  function my_compute_integral_trapezoid_1d(data, Npts, delta, func_params) result(res)
+    sll_real64, dimension(:), intent(in)    :: data
+    sll_int32, intent(in) :: Npts
+    sll_real64,intent(in) :: delta
+    sll_real64, dimension(:), intent(in) ,optional :: func_params
+    sll_real64 :: res
+    sll_int32 :: i
+    
+    res = 0.5*(data(1)+data(Npts))
+    do i=2,Npts-1
+      res = res + data(i)
+    enddo
+    res = res*delta
+  end function my_compute_integral_trapezoid_1d
+
+
 
 end program test_reduction
