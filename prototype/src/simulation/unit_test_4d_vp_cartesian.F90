462bd9ee77257e5bb1362ee2ea3a8a6515c13f02
Mehrenberger <mehrenbe@math.unistra.fr>
2013-11-08 16:31:02 +0100

    add skeleton for 4d vp poisson sequential simu

---
+++ prototype/src/simulation/unit_test_4d_vp_cartesian.F90
@@ -0,0 +1,43 @@
+! Sample computation with the following characteristics:
+! - vlasov-poisson
+! - 4D cartesian: x,y, vx, vy (or x1, x2)
+! - parallel for vlasov
+! - sequential for poisson 
+
+program vlasov_poisson_4d
+  use sll_simulation_4d_vlasov_parallel_poisson_sequential_cartesian
+  use sll_collective
+  implicit none
+
+  character(len=256) :: filename
+  character(len=256) :: filename_local
+  !type(sll_simulation_2d_vlasov_poisson_cart) :: simulation
+  call sll_boot_collective()
+  if(sll_get_collective_rank(sll_world_collective)==0)then
+    print *, '#Booting parallel environment...'
+  endif
+
+  if(sll_get_collective_rank(sll_world_collective)==0)then
+    print *,'#not implemented for the moment'
+    stop
+  endif
+  call sll_halt_collective()
+  
+  ! In this test, the name of the file to open is provided as a command line
+  ! argument.
+
+  
+  call getarg(1, filename)
+  filename_local = trim(filename)
+  !call simulation%init_from_file(filename_local)
+  !call simulation%run( )
+  !call delete_vp2d_par_cart(simulation)
+  
+  if(sll_get_collective_rank(sll_world_collective)==0)then
+    print *, '#reached end of vp4d test'
+    print *, '#PASSED'
+  endif
+  call sll_halt_collective()
+
+
+end program vlasov_poisson_4d
