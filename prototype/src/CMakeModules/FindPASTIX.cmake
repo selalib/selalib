0fa8f7f40b1996166813acd79895e1ff590d7c8d
Pierre Navaro <navaro@math.cnrs.fr>
2013-11-13 17:34:39 +0100

    Improve pastix interface and add a 1d poisson solver using it

--- prototype/src/CMakeModules/FindPASTIX.cmake
+++ prototype/src/CMakeModules/FindPASTIX.cmake
@@ -3,18 +3,26 @@
 # Pastix requires SCOTCH or METIS (partitioning and reordering tools) as well
 
 
-find_path(PASTIX_INCLUDE_DIRS
-		NAMES pastix.h pastix_fortran.h
-		HINTS ${PASTIX_ROOT}
-		PATH_SUFFIXES include Include INCLUDE
-		DOC "PATH TO pastix.h and pastix_fortran.h")
+FIND_PATH(PASTIX_INCLUDE_DIRS
+	    NAMES pastix_fortran.h
+	    HINTS ${PASTIX_ROOT}
+	    PATH_SUFFIXES include Include INCLUDE
+	    DOC "PATH TO pastix_fortran.h")
 
-find_library(PASTIX_LIBRARIES
-		NAMES pastix matrix_driver
-		HINTS ${PASTIX_ROOT}
-		PATH_SUFFIXES lib Lib LIB
-		DOC "PATH TO libpastix.a")
+FIND_LIBRARY(PASTIX_LIBRARY NAMES pastix
+		 HINTS ${PASTIX_ROOT}
+		 PATH_SUFFIXES lib Lib LIB
+		 DOC "PATH TO libpastix.a")
 
-if (PASTIX_INCLUDE_DIRS AND PASTIX_LIBRARIES)
-  set(PASTIX_FOUND YES)
-endif(PASTIX_INCLUDE_DIRS AND PASTIX_LIBRARIES)
+FIND_LIBRARY(PASTIX_MATRIX_DRIVER_LIBRARY NAMES matrix_driver
+		 HINTS ${PASTIX_ROOT}
+		 PATH_SUFFIXES lib Lib LIB
+		 DOC "PATH TO libmatrix_driver.a")
+
+SET(PASTIX_LIBRARIES ${PASTIX_LIBRARY};${PASTIX_MATRIX_DRIVER_LIBRARY})
+
+IF (PASTIX_INCLUDE_DIRS AND PASTIX_LIBRARIES)
+
+  SET(PASTIX_FOUND YES)
+
+ENDIF(PASTIX_INCLUDE_DIRS AND PASTIX_LIBRARIES)
--- prototype/src/pastix/sll_pastix.F90
+++ prototype/src/pastix/sll_pastix.F90
@@ -1,229 +1,164 @@
-#include "pastix_fortran.h"
   
 module sll_pastix
 
+#define MPI_MASTER 0
+#include "pastix_fortran.h"
 #include "sll_working_precision.h"
 #include "sll_memory.h"
+
 use sll_collective
-use utils
+
 implicit none
 
+type, public :: pastix_solver
+pastix_data_ptr_t                     :: pastix_data ! PaStiX structure (0 for first call)
+pastix_int_t                          :: n           ! Number of columns in the matrix
+pastix_int_t   ,dimension(:), pointer :: ia          ! Index of first elt of each col in ja and avals
+pastix_int_t   ,dimension(:), pointer :: ja          ! Row of each element
+pastix_float_t ,dimension(:), pointer :: avals       ! Value of each element
+pastix_int_t   ,dimension(:), pointer :: perm        ! permutation tabular
+pastix_int_t   ,dimension(:), pointer :: invp        ! reverse permutation tabular
+pastix_float_t ,dimension(:), pointer :: rhs         ! Right hand side
+pastix_int_t                          :: nrhs        ! right hand side number (only one possible)
+pastix_int_t                          :: iparm(IPARM_SIZE) ! sll_int32 parameters
+double precision                      :: dparm(DPARM_SIZE) ! Floating poin parameters
+sll_int32                             :: nbthread    ! Number of threads in PaStiX
+sll_int32                             :: verbose     ! Verbose level
+end type pastix_solver
 
-pastix_data_ptr_t                         :: pastix_data ! Structure to keep information in PaStiX (0 for first call)
-integer                                   :: pastix_comm ! MPI communicator used by pastix
-pastix_int_t                              :: n           ! Number of columns in the matrix
-pastix_int_t   ,dimension(:), allocatable :: ia          ! Index of first element of each column in ja and avals
-pastix_int_t   ,dimension(:), allocatable :: ja          ! Row of each element
-pastix_float_t ,dimension(:), allocatable :: avals       ! Value of each element
-pastix_int_t   ,dimension(:), allocatable :: perm        ! permutation tabular
-pastix_int_t   ,dimension(:), allocatable :: invp        ! reverse permutation tabular
-pastix_float_t ,dimension(:), allocatable :: rhs         ! Right hand side
-pastix_float_t ,dimension(:), allocatable :: rhssaved    ! Copy of Right hand side 
-pastix_int_t                              :: nrhs        ! right hand side number (only one possible)
-pastix_int_t                              :: iparm(IPARM_SIZE) ! Integer parameters
-double precision                          :: dparm(DPARM_SIZE) ! Floating poin parameters
-Integer                                   :: driver_num  ! Driver number
-Character(len=64)                         :: filename    ! Path to the matrix
-Character(len=4)                          :: type        ! type of the matrix
-Character(len=4)                          :: rhstype     ! type of the right-hand-side member
-Integer                                   :: nbthread    ! Number of threads in PaStiX
-Integer                                   :: verbose     ! Verbose level
-Integer                                   :: ierr        ! Error retrun value
-Integer                                   :: required    ! MPI thread level required  
-Integer                                   :: provided    ! MPI thread level provided 
-Integer                                   :: StatInfo    ! Info returned by MPI
-Integer                                   :: NbrOfFact   ! Number of factorizations
-Integer                                   :: NbrOfSolv   ! Number of rhs for solve
-Integer                                   :: rank        ! MPI rank
-Integer                                   :: i,j
-sll_int32                                 :: psize
-pastix_int_t                              :: nnzero
-pastix_int_t                              :: mpid
-
-
+sll_int32, private                    :: comm    
+sll_int32, private                    :: prank    
+sll_int32, private                    :: psize
 
 contains
 
+subroutine initialize_pastix(this, n)
+type(pastix_solver)   :: this
+sll_int32, intent(in) :: n
+pastix_int_t          :: nnzero
+sll_int32 :: i
+sll_int32 :: j
+sll_int32 :: error
 
-subroutine initialize_pastix()
-
-NbrOfFact = 2
-NbrOfSolv = 2
-
-!
-! initiate MPI communication
-!
-call MPI_Comm_rank  (pastix_comm, rank, StatInfo);
+prank = sll_get_collective_rank( sll_world_collective )
+psize = sll_get_collective_size( sll_world_collective )
+comm  = sll_world_collective%comm
 
 ! Get options ftom command line
 
-n = 1000
-nbthread = 1
-verbose  = API_VERBOSE_YES
-driver_num = 2 ! LAPLACIAN
+this%n = n
+this%nbthread = 1
+this%verbose  = API_VERBOSE_YES
 
-!
-! reads the matrix
-!
-!call read_matrix(driver_num, filename, &
-!         n, ia, ja, avals, rhs,            &
-!         Type, rhstype, pastix_comm, ierr)
+nnzero =  3*n - 2
 
+! Allocating
+SLL_ALLOCATE( this%ia   (n+1)   , error)
+SLL_ALLOCATE( this%ja   (nnzero), error)
+SLL_ALLOCATE( this%avals(nnzero), error)
+SLL_ALLOCATE( this%rhs  (n)     , error)
+stop
 
+! Building ia, ja and avals and rhs
+j=1
+do i = 1, n
+   this%ia(i) = j
+   ! /* ONLY triangular inferior matrix */
+   this%ja(j)    = i
+   this%avals(j) = 2
+   j=j+1
+   if (i /= this%n) then
+      this%ja(j)    = i+1
+      this%avals(j) = -1.
+      j=j + 1
+   end if
+   this%rhs(i) = 0
+end do
+this%ia(n+1) = j
+this%rhs(1)  = 1
+this%rhs(n)  = 1
 
-ierr = 0
-call MPI_Comm_rank(pastix_comm,mpid, ierr)
-
-If (mpid == 0) Then
-
-!   Call genlaplacian(n, nnzero,  ia, ja, avals, rhs, type, rhstype, ierr)
-
-      nnzero = 3*n - 2
-      ! Allocating
-      allocate( ia     (n+1)   )
-      allocate( ja     (nnzero))
-      allocate( avals  (nnzero))
-      allocate( rhs    (n)     )
-
-      ! Building ia, ja and avals and rhs
-      j=1
-      do i = 1, n
-         ia(i) = j
-         ! /* ONLY triangular inferior matrix */
-         ! /*       if (i != 0) */
-         ! /* 	{ */
-         ! /* 	  (*ja)[j]    = i; */
-         ! /* 	  (*avals)[j] = -1; */
-         ! /* 	  j++; */
-         ! /* 	} */
-         ja(j)    = i
-         avals(j) = 2
-         j=j+1
-         if (i /= n) then
-            ja(j)    = i+1
-            avals(j) = -1.
-            j=j + 1
-         end if
-
-         rhs(i) = 0
-      end do
-      ia(n+1) = j
-      rhs(1)  = 1
-      rhs(n)  = 1
-
-      ! type and rhstype
-      type         = "RSA"
-      type(4:4)    = '\0'
-      rhstype(1:1) = '\0'
-
-End if
-
-call MPI_Bcast(n,1,MPI_PASTIX_INT,0,pastix_comm,ierr)
-call MPI_Bcast(nnzero,1,MPI_PASTIX_INT,0,pastix_comm,ierr)
-
-If (mpid /= 0) Then
-   allocate(ia(n+1))
-   allocate(ja(nnzero))
-   allocate(avals(nnzero))
-   allocate(rhs(n))
-End If
-
-call MPI_Bcast(ia, n+1,    MPI_PASTIX_INT,   0, pastix_comm, ierr)
-call MPI_Bcast(ja, nnzero, MPI_PASTIX_INT,   0, pastix_comm, ierr)
-call MPI_Bcast(avals,nnzero, MPI_PASTIX_FLOAT, 0, pastix_comm, ierr)
-call MPI_Bcast(rhs,n     , MPI_PASTIX_FLOAT, 0, pastix_comm, ierr)
-call MPI_Bcast(type,    3, MPI_CHARACTER,    0, pastix_comm, ierr)
-
-
-!
 ! First PaStiX call to initiate parameters
-!
-pastix_data = 0
-nrhs        = 1
-iparm(IPARM_MODIFY_PARAMETER) = API_NO
-iparm(IPARM_START_TASK)       = API_TASK_INIT
-iparm(IPARM_END_TASK)         = API_TASK_INIT
 
-call pastix_fortran(pastix_data ,pastix_comm, &
-     n,ia,ja,avals,perm,invp,rhs,nrhs,iparm,dparm)
+this%pastix_data                   = 0
+this%nrhs                          = 1
+this%iparm(IPARM_MODIFY_PARAMETER) = API_NO
+this%iparm(IPARM_START_TASK)       = API_TASK_INIT
+this%iparm(IPARM_END_TASK)         = API_TASK_INIT
 
-!
+call pastix_fortran(this%pastix_data ,comm, &
+                    this%n,this%ia,this%ja,this%avals,this%perm,this%invp,  &
+                    this%rhs,this%nrhs,this%iparm,this%dparm)
+
 ! Customize some parameters
-!
-iparm(IPARM_THREAD_NBR) = nbthread  
-iparm(IPARM_VERBOSE)    = verbose
 
-if (type(2:2) == 'S') then
+this%iparm(IPARM_THREAD_NBR) = this%nbthread  
+this%iparm(IPARM_VERBOSE)    = this%verbose
 
-   iparm(IPARM_SYM)           = API_SYM_YES
-   iparm(IPARM_FACTORIZATION) = API_FACT_LDLT
+this%iparm(IPARM_SYM)           = API_SYM_YES   ! API_SYM_NO
+this%iparm(IPARM_FACTORIZATION) = API_FACT_LDLT ! API_FACT_LU
 
-else
-   iparm(IPARM_SYM)           = API_SYM_NO
-   iparm(IPARM_FACTORIZATION) = API_FACT_LU
-End if
-iparm(IPARM_MATRIX_VERIFICATION) = API_YES
-iparm(IPARM_RHS_MAKING)          = API_RHS_1
+this%iparm(IPARM_MATRIX_VERIFICATION) = API_YES
+this%iparm(IPARM_RHS_MAKING)          = API_RHS_1
 
-allocate(perm(n))
-allocate(invp(n))
-!
-! Call PaStiX first steps (Scotch - Fax - Blend
-!
-iparm(IPARM_START_TASK)       = API_TASK_ORDERING
-iparm(IPARM_END_TASK)         = API_TASK_ANALYSE
+SLL_ALLOCATE(this%perm(n), error)
+SLL_ALLOCATE(this%invp(n), error)
 
-call pastix_fortran(pastix_data ,pastix_comm, &
-     n,ia,ja,avals,perm,invp,rhs,nrhs,iparm,dparm)
+! Call PaStiX first steps (Scotch - Fax - Blend)
+this%iparm(IPARM_START_TASK) = API_TASK_ORDERING
+this%iparm(IPARM_END_TASK)   = API_TASK_ANALYSE
 
+call pastix_fortran(this%pastix_data ,comm, &
+                    this%n,this%ia,this%ja, &
+                    this%avals,this%perm,this%invp,  &
+                    this%rhs,this%nrhs,this%iparm,this%dparm)
     
-allocate(rhssaved(n))
-rhssaved = rhs
-    
-Do i = 1, NbrOfFact
-    
-       !
-       ! Call PaStiX factorization
-       !
-       iparm(IPARM_START_TASK)       = API_TASK_NUMFACT
-       iparm(IPARM_END_TASK)         = API_TASK_NUMFACT
-       If (rank == 0) print *, "      > Factorisation number",i,"<"
-       call pastix_fortran(pastix_data ,pastix_comm, &
-            n,ia,ja,avals,perm,invp,rhs,nrhs,iparm,dparm)
-       
-       Do j = 1, NbrOfSolv
-          !
-          ! Call PaStiX updown and refinement
-          !
-          iparm(IPARM_START_TASK)       = API_TASK_SOLVE
-          iparm(IPARM_END_TASK)         = API_TASK_REFINE
-          ! rhs has been changed to solution by previous solve call
-          rhs = rhssaved
-          If (rank == 0) print *, "      >> Solve step number",i," <<"
-          call pastix_fortran(pastix_data ,pastix_comm, &
-               n,ia,ja,avals,perm,invp,rhs,nrhs,iparm,dparm)
-       End Do
-    End Do
+! Call PaStiX factorization
+this%iparm(IPARM_START_TASK) = API_TASK_NUMFACT
+this%iparm(IPARM_END_TASK)   = API_TASK_NUMFACT
+call pastix_fortran(this%pastix_data ,comm, &
+                    this%n,this%ia,this%ja, &
+                    this%avals,this%perm,this%invp,  &
+                    this%rhs,this%nrhs,this%iparm,this%dparm)
 
 end subroutine initialize_pastix
 
-subroutine delete_pastix()
+subroutine solve_pastix(this)
+type(pastix_solver)      :: this
 
-    ! Call PaStiX clean
-    iparm(IPARM_START_TASK)       = API_TASK_CLEAN
-    iparm(IPARM_END_TASK)         = API_TASK_CLEAN
+comm = sll_world_collective%comm
+
+! Call PaStiX updown and refinement
+this%iparm(IPARM_START_TASK)       = API_TASK_SOLVE
+this%iparm(IPARM_END_TASK)         = API_TASK_REFINE
+! rhs will be changed to solution 
+call pastix_fortran(this%pastix_data ,comm, &
+                    this%n,this%ia,this%ja, &
+                    this%avals,this%perm,this%invp,  &
+                    this%rhs,this%nrhs,this%iparm,this%dparm)
+
+end subroutine solve_pastix
+
+subroutine delete_pastix(this)
+type(pastix_solver) :: this
+
+comm = sll_world_collective%comm
+
+! Call PaStiX clean
+this%iparm(IPARM_START_TASK)       = API_TASK_CLEAN
+this%iparm(IPARM_END_TASK)         = API_TASK_CLEAN
     
-    call pastix_fortran(pastix_data ,pastix_comm, &
-         n,ia,ja,avals,perm,invp,rhs,nrhs,iparm,dparm)
+call pastix_fortran(this%pastix_data ,comm,         &
+                    this%n,this%ia,this%ja,         &
+                    this%avals,this%perm,this%invp, &
+                    this%rhs,this%nrhs,this%iparm,this%dparm)
     
-    deallocate(ia)
-    deallocate(ja)
-    deallocate(avals)
-    deallocate(perm)
-    deallocate(invp)
-    deallocate(rhssaved)
-    deallocate(rhs)
-    
+deallocate(this%ia)
+deallocate(this%ja)
+deallocate(this%avals)
+deallocate(this%perm)
+deallocate(this%invp)
+deallocate(this%rhs)
 
 end subroutine delete_pastix
 
