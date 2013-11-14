4d6e551406d8196e3ef42bf70811ab71fc66324e
Michel Mehrenberger <mehrenbe@math.unistra.fr>
2013-11-14 10:01:00 +0100

    add some simulation skeletons

---
+++ prototype/src/simulation/simulation_2d_guiding_center_polar.F90
@@ -0,0 +1,15 @@
+module sll_simulation_2d_guiding_center_polar
+
+!the aim is to translate CG_polar in simulation class
+!related to
+!simulation_guiding_center_2D_generalized_coords.F90
+!but here geometry and test is specifically polar
+
+#include "sll_working_precision.h"
+#include "sll_assert.h"
+#include "sll_memory.h"
+  use sll_constants
+
+
+
+end module sll_simulation_2d_guiding_center_polar
