1a6f8c7966b1994f38efb78993799d7fd7c6ac9b
Pierre Navaro <navaro@math.cnrs.fr>
2013-11-15 14:44:48 +0100

    Improve pastix interface

--- prototype/src/pastix/unit_test.F90
+++ prototype/src/pastix/unit_test.F90
@@ -1,13 +1,21 @@
-#define NPTS 100
 program test_pastix
 #include "sll_working_precision.h"
+#include "sll_memory.h"
 use sll_collective
 use sll_pastix 
 use sll_murge
 
 implicit none
 
+sll_int32                             :: npts = 100
+sll_real64, dimension(:), allocatable :: sol
+sll_real64, dimension(:), allocatable :: rhs
+sll_int32                             :: error
+
 call sll_boot_collective()
+
+SLL_CLEAR_ALLOCATE(sol(1:npts), error)
+SLL_CLEAR_ALLOCATE(rhs(1:npts), error)
 
 call test_pastix_fortran()
 
@@ -20,8 +28,8 @@ contains
 subroutine test_pastix_fortran()
 type(pastix_solver) :: solver
 
-   call initialize_pastix(solver, NPTS)
-   call solve_pastix(solver)
+   call initialize_pastix(solver, npts)
+   call solve_pastix(solver, sol, rhs)
    call delete_pastix(solver)
 
 end subroutine test_pastix_fortran
