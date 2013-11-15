d81b3ee72bdb7975f94793e4a0c146dec6f0d92a
Pierre Navaro <navaro@math.unistra.fr>
2013-11-15 17:39:00 +0100

    Improve mkl managing

--- prototype/src/CMakeModules/LapackConfig.cmake
+++ prototype/src/CMakeModules/LapackConfig.cmake
@@ -1,15 +1,23 @@
 
-MESSAGE(STATUS "LAPACK:CMAKE_Fortran_COMPILER:${CMAKE_Fortran_COMPILER}")
+GET_FILENAME_COMPONENT(Fortran_COMPILER_NAME "${CMAKE_Fortran_COMPILER}" NAME)
+MESSAGE(STATUS "LAPACK:Fortran_COMPILER_NAME:${Fortran_COMPILER_NAME}")
+
 SET(USE_MKL OFF CACHE BOOL "Using Intel Math Kernel Library")
+
+
+IF(Fortran_COMPILER_NAME MATCHES "ifort")
+   SET(USE_MKL ON)
+ENDIF()
 
 IF(USE_MKL)
 
    SET(BLA_VENDOR "Intel")
 
       INCLUDE_DIRECTORIES($ENV{MKLROOT}/include/intel64/lp64 $ENV{MKLROOT}/include)
-      SET(BLAS_LIBRARIES "-Wl,--start-group  $ENV{MKLROOT}/lib/intel64/libmkl_intel_lp64.a $ENV{MKLROOT}/lib/intel64/libmkl_sequential.a $ENV{MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group -lm")
+      SET(BLAS_LIBRARIES "-mkl")
       SET(BLAS_FOUND TRUE)
-      SET(LAPACK_LIBRARIES "-Wl,--start-group  $ENV{MKLROOT}/lib/intel64/libmkl_intel_lp64.a $ENV{MKLROOT}/lib/intel64/libmkl_sequential.a $ENV{MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group -lm")
+      SET(LAPACK_LIBRARIES "-mkl")
+      #SET(LAPACK_LIBRARIES "-Wl,--start-group  $ENV{MKLROOT}/lib/intel64/libmkl_intel_lp64.a $ENV{MKLROOT}/lib/intel64/libmkl_sequential.a $ENV{MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group -lm")
       SET(LAPACK_FOUND TRUE)
 
 ELSE()
