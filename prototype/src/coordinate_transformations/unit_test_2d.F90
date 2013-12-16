25396164f4201998c9a25b1ec1bc79e8f02435a3
Aurore Back <aurore.back@gmail.com>
2013-12-16 15:31:12 +0100

    fichier cercle

--- prototype/src/coordinate_transformations/unit_test_2d.F90
+++ prototype/src/coordinate_transformations/unit_test_2d.F90
@@ -312,8 +312,8 @@ program unit_test_2d
 
   print *, 'Test of initialization from file for a nurbs transformation:'
 
-  call t_n%read_from_file("../src/coordinate_transformations/&
-       circle_5mp_patch4.nml")
+  call t_n%read_from_file("../src/&
+       coordinate_transformations/circle_5mp_patch4.nml")
   t_n%mesh => mesh
   call t_n%write_to_file()
 
