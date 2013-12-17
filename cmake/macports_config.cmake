432669b5c3632c5cbba5c92f4d578a39492133c1
Pierre Navaro <navaro@math.cnrs.fr>
2013-12-17 11:05:26 +0100

    Improve suitesparse lib configuration

--- cmake/macports_config.cmake
+++ cmake/macports_config.cmake
@@ -1,6 +1,6 @@
 SET(CMAKE_Fortran_COMPILER   "/opt/local/bin/gfortran-mp-4.8"  CACHE FILEPATH " " FORCE)
 SET(FFTW_LIBRARY             "/opt/local/lib/libfftw3.a"       CACHE FILEPATH " " FORCE)
-SET(CMAKE_BUILD_TYPE         Release                           CACHE STRING   " " FORCE)
+#SET(CMAKE_BUILD_TYPE         Release                           CACHE STRING   " " FORCE)
 SET(CMAKE_CXX_COMPILER       "/opt/local/bin/g++-mp-4.8"       CACHE FILEPATH " " FORCE)
 SET(CMAKE_C_COMPILER         "/opt/local/bin/gcc-mp-4.8"       CACHE FILEPATH " " FORCE)
 SET(FFTW_ROOT                "/opt/local"                      CACHE PATH     " " FORCE)
@@ -16,4 +16,4 @@ SET(MPI_Fortran_INCLUDE_PATH "/opt/local/lib"                  CACHE PATH     " 
 SET(MPI_C_LIBRARIES          "/opt/local/lib/libmpi.dylib"     CACHE FILEPATH " " FORCE)
 SET(MPI_CXX_LIBRARIES        "/opt/local/lib/libmpi_cxx.dylib" CACHE FILEPATH " " FORCE)
 SET(MPI_Fortran_LIBRARIES    "/opt/local/lib/libmpi_f90.dylib" CACHE FILEPATH " " FORCE)
-
+SET(SUITESPARSE_ROOT         "/opt/local"                      CACHE PATH     " " FORCE)
