836c7534da53324bb6072c758886b1fc970df722
Pierre Navaro <navaro@math.cnrs.fr>
2013-12-18 17:22:38 +0100

    Add linear solvers interface

---
+++ prototype/src/linear_solvers/sll_linear_solvers.F90
@@ -0,0 +1,34 @@
+!**************************************************************
+!  Copyright INRIA
+!  Authors : 
+!     CALVI project team
+!  
+!  This code SeLaLib (for Semi-Lagrangian-Library) 
+!  is a parallel library for simulating the plasma turbulence 
+!  in a tokamak.
+!  
+!  This software is governed by the CeCILL-B license 
+!  under French law and abiding by the rules of distribution 
+!  of free software.  You can  use, modify and redistribute 
+!  the software under the terms of the CeCILL-B license as 
+!  circulated by CEA, CNRS and INRIA at the following URL
+!  "http://www.cecill.info". 
+!**************************************************************
+
+module sll_linear_solvers
+
+interface initialize
+end interface initialize
+
+interface factorize
+end interface factorize
+
+interface solve
+end interface solve
+
+interface delete
+end interface delete
+
+contains
+
+end module sll_linear_solvers
