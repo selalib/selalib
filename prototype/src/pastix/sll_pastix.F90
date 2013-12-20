59858f87028147774a2deea5de1eb557f4c4dc4b
Pierre Navaro <navaro@math.unistra.fr>
2013-12-20 17:09:43 +0100

    Add linear solvers interface

--- prototype/src/pastix/sll_pastix.F90
+++ prototype/src/pastix/sll_pastix.F90
@@ -13,19 +13,19 @@ implicit none
 private
 
 type, public :: pastix_solver
- pastix_data_ptr_t                     :: pastix_data !< PaStiX structure (0 for first call)
- pastix_int_t                          :: ncols       !< Number of columns in the matrix
- pastix_int_t   ,dimension(:), pointer :: colptr      !< Index of first elt of each col in ja and avals
- pastix_int_t   ,dimension(:), pointer :: row          !< Row of each element
- pastix_float_t ,dimension(:), pointer :: avals       !< Value of each element
- pastix_int_t   ,dimension(:), pointer :: perm        !< permutation tabular
- pastix_int_t   ,dimension(:), pointer :: invp        !< reverse permutation tabular
- pastix_float_t ,dimension(:), pointer :: rhs         !< Right hand side
- pastix_int_t                          :: nrhs        !< right hand side number (only one possible)
- pastix_int_t                          :: iparm(IPARM_SIZE) ! sll_int32 parameters
- double precision                      :: dparm(DPARM_SIZE) ! Floating poin parameters
- sll_int32                             :: nbthread    !< Number of threads in PaStiX
- sll_int32                             :: verbose     !< Verbose level
+ pastix_data_ptr_t        :: pastix_data !< PaStiX structure (0 for first call)
+ pastix_int_t             :: ncols       !< Number of columns in the matrix
+ pastix_int_t   , pointer :: colptr(:)   !< Index of first elt of each col in ja and avals
+ pastix_int_t   , pointer :: row(:)      !< Row of each element
+ pastix_float_t , pointer :: avals(:)    !< Value of each element
+ pastix_int_t   , pointer :: perm(:)     !< permutation tabular
+ pastix_int_t   , pointer :: invp(:)     !< reverse permutation tabular
+ pastix_float_t , pointer :: rhs(:)      !< Right hand side
+ pastix_int_t             :: nrhs        !< right hand side number (only one possible)
+ pastix_int_t             :: iparm(IPARM_SIZE) ! sll_int32 parameters
+ double precision         :: dparm(DPARM_SIZE) ! Floating poin parameters
+ sll_int32                :: nbthread    !< Number of threads in PaStiX
+ sll_int32                :: verbose     !< Verbose level
 end type pastix_solver
 
 sll_int32                             :: comm    
@@ -54,14 +54,14 @@ public :: initialize, factorize, solve, delete
 
 contains
 
-subroutine initialize_pastix(this,npts,nnzeros)
+subroutine initialize_pastix(this,n,nnzeros)
 
-   type(pastix_solver)   :: this
-   sll_int32, intent(in) :: npts
-   pastix_int_t          :: nnzeros
-   sll_int32             :: error
+   type(pastix_solver)      :: this
+   sll_int32, intent(in)    :: n
+   pastix_int_t, intent(in) :: nnzeros
+   sll_int32                :: error
 
-   this%ncols = npts
+   this%ncols = n
    prank = sll_get_collective_rank( sll_world_collective )
    psize = sll_get_collective_size( sll_world_collective )
    comm  = sll_world_collective%comm
@@ -71,10 +71,14 @@ subroutine initialize_pastix(this,npts,nnzeros)
    this%verbose  = API_VERBOSE_NO
 
    ! Allocating
-   SLL_ALLOCATE( this%colptr(1:npts+1), error)
+   SLL_ALLOCATE( this%colptr(1:n+1), error)
+   print*, associated(this%colptr)
    SLL_ALLOCATE( this%row(1:nnzeros)   , error)
+   print*, associated(this%row)
    SLL_ALLOCATE( this%avals(1:nnzeros) , error)
-   SLL_ALLOCATE( this%rhs(1:npts)     , error)
+   print*, associated(this%avals)
+   SLL_ALLOCATE( this%rhs(1:n)     , error)
+   print*, associated(this%rhs)
 
    ! First PaStiX call to initiate parameters
 
@@ -84,7 +88,10 @@ subroutine initialize_pastix(this,npts,nnzeros)
    this%iparm(IPARM_START_TASK)       = API_TASK_INIT
    this%iparm(IPARM_END_TASK)         = API_TASK_INIT
 
-   call pastix_fortran(this%pastix_data,comm,npts,this%colptr,this%row, &
+   SLL_ALLOCATE(this%perm(n), error)
+   SLL_ALLOCATE(this%invp(n), error)
+
+   call pastix_fortran(this%pastix_data,comm,n,this%colptr,this%row, &
                        this%avals,this%perm,this%invp,this%rhs,    &
                        this%nrhs,this%iparm,this%dparm)
 
@@ -98,20 +105,16 @@ subroutine initialize_pastix(this,npts,nnzeros)
 
    this%iparm(IPARM_MATRIX_VERIFICATION) = API_YES
 
-   SLL_ALLOCATE(this%perm(npts), error)
-   SLL_ALLOCATE(this%invp(npts), error)
-
-
 end subroutine initialize_pastix
 
 subroutine initialize_pastix_with_csc_matrix(this, csc_matrix)
 
    type(pastix_solver)   :: this
    type(sll_csc_matrix)  :: csc_matrix
-   pastix_int_t          :: npts
+   pastix_int_t          :: n
    sll_int32             :: error
 
-   npts  = csc_matrix%n
+   n  = csc_matrix%n
    prank = sll_get_collective_rank( sll_world_collective )
    psize = sll_get_collective_size( sll_world_collective )
    comm  = sll_world_collective%comm
@@ -124,7 +127,7 @@ subroutine initialize_pastix_with_csc_matrix(this, csc_matrix)
    this%colptr => csc_matrix%colptr
    this%row    => csc_matrix%row
    this%avals  => csc_matrix%avals
-   SLL_ALLOCATE( this%rhs(1:npts)     , error)
+   SLL_ALLOCATE( this%rhs(1:n)     , error)
 
    ! First PaStiX call to initiate parameters
 
@@ -134,7 +137,10 @@ subroutine initialize_pastix_with_csc_matrix(this, csc_matrix)
    this%iparm(IPARM_START_TASK)       = API_TASK_INIT
    this%iparm(IPARM_END_TASK)         = API_TASK_INIT
 
-   call pastix_fortran(this%pastix_data,comm,npts,this%colptr,this%row, &
+   SLL_ALLOCATE(this%perm(n), error)
+   SLL_ALLOCATE(this%invp(n), error)
+
+   call pastix_fortran(this%pastix_data,comm,n,this%colptr,this%row, &
                        this%avals,this%perm,this%invp,this%rhs,    &
                        this%nrhs,this%iparm,this%dparm)
 
@@ -147,10 +153,6 @@ subroutine initialize_pastix_with_csc_matrix(this, csc_matrix)
    this%iparm(IPARM_FACTORIZATION) = API_FACT_LDLT ! API_FACT_LU
 
    this%iparm(IPARM_MATRIX_VERIFICATION) = API_YES
-
-   SLL_ALLOCATE(this%perm(npts), error)
-   SLL_ALLOCATE(this%invp(npts), error)
-
 
 end subroutine initialize_pastix_with_csc_matrix
 
