d6fc02155d02f0283bea70141f546ccc21248eab
Pierre Navaro <navaro@math.cnrs.fr>
2013-12-20 17:09:43 +0100

    Add linear solvers interface

--- prototype/src/linear_solvers/unit_test_linear_solvers.F90
+++ prototype/src/linear_solvers/unit_test_linear_solvers.F90
@@ -18,6 +18,7 @@ type(sll_csc_matrix)                  :: csc_matrix
 type(pastix_solver)                   :: pastix
 #endif
 
+sll_int32   :: ncells
 sll_int32   :: n, nnzeros
 sll_real64  :: delta
 sll_real64  :: x
@@ -26,56 +27,58 @@ sll_int32   :: i, j
 
 call sll_boot_collective()
 
-n = 32
+ncells = 33
 
-SLL_ALLOCATE(sol(n),error)
-SLL_ALLOCATE(rhs(n),error)
-SLL_ALLOCATE(sol_exact(n),error)
+SLL_CLEAR_ALLOCATE(sol(1:ncells),error)
+SLL_CLEAR_ALLOCATE(rhs(1:ncells),error)
+SLL_CLEAR_ALLOCATE(sol_exact(1:ncells),error)
 
-
-delta = 1.0_f64 / n
-do i=1,n
+delta = 1.0_f64 / ncells
+do i=1,ncells
    x = (i-1)*delta
    rhs(i)       = 4*sll_pi**2*sin(2*sll_pi*x) * delta**2
    sol_exact(i) = sin(2*sll_pi*x)
 end do
 sol = rhs
 
-nnzeros = 3*(n-1)+2
+n = ncells-1
+nnzeros = 3*n+2
 
-call initialize(csc_matrix, n-1, nnzeros, error)
+call initialize(csc_matrix, n, nnzeros, error)
+
+call initialize(pastix, n, nnzeros)
 
 j=1
-do i = 1, n-1
-   csc_matrix%colptr(i) = j
-   csc_matrix%row(j)    = i
-   csc_matrix%avals(j)  = 2
+do i = 1, n
+   pastix%colptr(i) = j
+   pastix%row(j)    = i
+   pastix%avals(j) = 2
    j=j+1
-   if (i /= n-1) then
-      csc_matrix%row(j)   = i+1
-      csc_matrix%avals(j) = -1.
+   if (i /= n) then
+      pastix%row(j)   = i+1
+      pastix%avals(j) = -1.
       j=j+1
    end if
 end do
-csc_matrix%colptr(n) = j
+pastix%colptr(n+1) = j
 
-!call initialize(pastix, n-1, nnzeros)
+
 !pastix%colptr = csc_matrix%colptr
 !pastix%row    = csc_matrix%row
 !pastix%avals  = csc_matrix%avals
-!call factorize(pastix)
+call factorize(pastix)
 !print*, sol
 !print*, rhs
 !print*, size(pastix%rhs), size(rhs), size(sol)
 !stop
-!call solve(pastix, sol(:))
+call solve(pastix, sol(2:ncells))
 !stop
-!
-!do i = 1, n
-!   write(18,*) sngl((i-1)*delta), sngl(sol(i)), sngl(sol_exact(i))
-!end do
-!    
-!print*,'   error=',maxval(abs(sol-sol_exact))
+
+do i = 1, ncells
+   write(18,*) sngl((i-1)*delta), sngl(sol(i)), sngl(sol_exact(i))
+end do
+    
+print*,'   error=',maxval(abs(sol-sol_exact))
 
 call sll_halt_collective()
 
