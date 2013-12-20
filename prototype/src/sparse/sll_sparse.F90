071e07f7c893c4a46b27ba36b9398921ae62013d
Pierre Navaro <navaro@math.cnrs.fr>
2013-12-20 17:09:43 +0100

    Add linear solvers interface

--- prototype/src/sparse/sll_sparse.F90
+++ prototype/src/sparse/sll_sparse.F90
@@ -26,24 +26,32 @@ end type sll_csr_matrix
 
 interface initialize
    module procedure initialize_csc_matrix
+   module procedure initialize_csr_matrix
 end interface initialize
 
-public initialize
+interface todense
+   module procedure csc_todense
+   module procedure csr_todense
+end interface todense
+
+public initialize, todense
+
+sll_int32 :: i, j, k, l
 
 contains
 
-subroutine initialize_csc_matrix(this, n, nnzeros, error)
+subroutine initialize_csc_matrix(self, n, nnzeros, error)
 
-   type(sll_csc_matrix)  :: this
-   sll_int32, intent(in) :: n         !< Matrix dimension
-   sll_int32             :: nnzeros   !< Number of non-zeros
-   sll_int32             :: error
+   type(sll_csc_matrix)   :: self
+   sll_int32, intent(in)  :: n         !< Matrix dimension
+   sll_int32, intent(in)  :: nnzeros   !< Number of non-zeros
+   sll_int32, intent(out) :: error
 
-   this%n       = n
-   this%nnzeros = nnzeros
-   SLL_ALLOCATE(this%colptr(n+1), error)
-   SLL_ALLOCATE(this%row(nnzeros), error)
-   SLL_ALLOCATE(this%avals(nnzeros), error)
+   self%n       = n
+   self%nnzeros = nnzeros
+   SLL_ALLOCATE(self%colptr(n+1), error)
+   SLL_ALLOCATE(self%row(nnzeros), error)
+   SLL_CLEAR_ALLOCATE(self%avals(1:nnzeros), error)
 
 end subroutine initialize_csc_matrix
 
@@ -51,7 +59,7 @@ subroutine initialize_csr_matrix(this, n, nnzeros, error)
 
    type(sll_csr_matrix)  :: this
    sll_int32, intent(in) :: n         !< Matrix dimension
-   sll_int32             :: nnzeros   !< Number of non-zeros
+   sll_int32, intent(in) :: nnzeros   !< Number of non-zeros
    sll_int32             :: error
 
    this%n       = n
@@ -61,5 +69,37 @@ subroutine initialize_csr_matrix(this, n, nnzeros, error)
    SLL_ALLOCATE(this%avals(nnzeros), error)
 
 end subroutine initialize_csr_matrix
+
+subroutine csc_todense( this, dense_matrix)
+
+   type(sll_csc_matrix)       :: this
+   sll_real64, dimension(:,:) :: dense_matrix
+
+   l = 0
+   do j = 1, this%n 
+      do k = this%colptr(j),this%colptr(j+1)-1 
+         l = l + 1
+         i = this%row(l)
+         dense_matrix(i,j) = this%avals(l)
+      end do
+   end do
+
+end subroutine csc_todense
+
+subroutine csr_todense( this, dense_matrix)
+
+   type(sll_csr_matrix)       :: this
+   sll_real64, dimension(:,:) :: dense_matrix
+
+   l = 0
+   do i = 1, this%n 
+      do k = this%rowptr(i),this%rowptr(i+1)-1 
+         l = l + 1
+         j = this%col(l)
+         dense_matrix(i,j) = this%avals(l)
+      end do
+   end do
+
+end subroutine csr_todense
 
 end module sll_sparse
