b108aef800f18d08d39b043e1844b1903fb996d6
Aurore Back <aurore.back@gmail.com>
2013-12-27 15:07:52 +0100

    optimization general elliptic solver ok

--- prototype/src/deboor_splines/bvalue2d.f90
+++ prototype/src/deboor_splines/bvalue2d.f90
@@ -1,6 +1,6 @@
 ! Just for the record: what is ar_x, ar_y, ai_nx, etc., etc.? What is this
 ! function supposed to do? This is a deeply frustrating file.
-real(8) function bvalue2d(&
+subroutine bvalue2d(&
      ar_x,&
      ar_y,&
      ai_nx,&
@@ -9,44 +9,61 @@ real(8) function bvalue2d(&
      ai_ky,&
      apr_Bcoef,&
      apr_tx,&
-     apr_ty )
+     sz_tx,&
+     apr_ty,&
+     sz_ty,&
+     val )
   implicit none
   ! INPUT
   real(8) :: ar_x, ar_y
-  real(8) :: bvalue
-  integer  :: ai_nx, ai_kx, ai_ny, ai_ky
-  real(8), dimension ( ai_nx + ai_kx ) :: apr_tx
-  real(8), dimension ( ai_ny + ai_ky ) :: apr_ty	
-  real(8), dimension ( ai_nx , ai_ny ) :: apr_Bcoef			
+  real(8) :: bvalue,val
+  integer  :: ai_nx, ai_kx, ai_ny, ai_ky,sz_tx,sz_ty
+  real(8), dimension(:), pointer :: apr_tx !  ai_nx + ai_kx 
+  real(8), dimension(:), pointer :: apr_ty !  ai_ny + ai_ky	
+  real(8), dimension(:,:),pointer :: apr_Bcoef!( ai_nx,ai_ny)			
   ! LOCAL VARIABLES
   integer  :: li_i, li_j, li_mflag, li_lefty
-  real(8), dimension ( ai_ky			) :: lpr_coef			
+  real(8), dimension(:),pointer :: lpr_coef ! ai_ky
+  real(8), dimension(:),pointer :: tmp_tab
+  real(8), dimension(:),pointer :: tmp_ty
+  integer :: ierr
   
-  call interv ( apr_ty, ai_ny + 1, ar_y, li_lefty, li_mflag )
+  call interv ( apr_ty, sz_ty, ar_y, li_lefty, li_mflag )
   
   if ( li_mflag .NE. 0 ) then
-     bvalue2d = 0.0_8
+     val = 0.0_8
      return 
   end if
-  
+
+  ALLOCATE(lpr_coef(ai_ky))
+  ALLOCATE(tmp_tab(ai_nx))
+  ALLOCATE(tmp_ty( 2*ai_ky ))
   do li_j = 1, ai_ky
+     
+     
+     tmp_tab = apr_bcoef ( 1:ai_nx , li_lefty - ai_ky + li_j )
      
      lpr_coef ( li_j ) = bvalue(&
           apr_tx,&
-          apr_bcoef ( 1 , li_lefty - ai_ky + li_j ),&
+          tmp_tab,&
           ai_nx,&
           ai_kx,&
           ar_x,&
           0 )
      
+     
   end do
   
-  bvalue2d = bvalue(&
-       apr_ty ( li_lefty - ai_ky + 1 ),&
+  tmp_ty =  apr_ty ( li_lefty - ai_ky + 1 : li_lefty + ai_ky)
+  val = bvalue(&
+       tmp_ty,&
        lpr_coef,&
        ai_ky,&
        ai_ky,&
        ar_y,&
        0 )
   
-end function bvalue2d
+  DEALLOCATE(lpr_coef)
+  DEALLOCATE(tmp_tab)
+  DEALLOCATE(tmp_ty)
+end subroutine bvalue2d
