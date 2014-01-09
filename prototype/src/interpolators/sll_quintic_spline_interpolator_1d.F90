793fc6bb8f80f3f5821b30b02a238efa11f32b73
Michel Mehrenberger <mehrenbe@math.unistra.fr>
2014-01-09 14:31:08 +0100

    remove some added warning messages in interpolation and finish my remove-warning task

--- prototype/src/interpolators/sll_quintic_spline_interpolator_1d.F90
+++ prototype/src/interpolators/sll_quintic_spline_interpolator_1d.F90
@@ -164,10 +164,10 @@ contains  ! ****************************************************************
     sll_int32, intent(in),optional                 :: size_eta_coords
     
     if(present(eta_coords))then
-      print *,'#Warning eta_coords present but not used'
+      !print *,'#Warning eta_coords present but not used'
     endif
     if(present(size_eta_coords))then
-      print *,'#Warning size_eta_coords present but not used'
+      !print *,'#Warning size_eta_coords present but not used'
     endif
     
     call compute_quintic_coeffs_uniform( data_array, interpolator%spline )
