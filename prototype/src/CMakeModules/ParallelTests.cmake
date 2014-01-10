15ae82e20bc80f88ece6601ef737dd201635e38c
Pierre Navaro <navaro@math.cnrs.fr>
2014-01-10 10:05:28 +0100

    CHnage parallel tests list

--- prototype/src/CMakeModules/ParallelTests.cmake
+++ prototype/src/CMakeModules/ParallelTests.cmake
@@ -61,23 +61,23 @@ IF(HDF5_PARALLEL_ENABLED AND HDF5_IS_PARALLEL)
 
 ######
 #
-#      IF(FFT_DEFAULT_LIBRARY MATCHES "SLLFFT")
-#
-#      ADD_MPI_TEST(poisson_per_cart_par_2d 
-#                   test_poisson_2d_per_cart_par ${PROCS} ${ARGS})
-#      SET_TESTS_PROPERTIES(poisson_per_cart_par_2d
-#                           PROPERTIES PASS_REGULAR_EXPRESSION "PASSED")
-#   
+      IF(FFT_DEFAULT_LIBRARY MATCHES "SLLFFT")
+
+      ADD_MPI_TEST(poisson_per_cart_par_2d 
+                   test_poisson_2d_per_cart_par ${PROCS} ${ARGS})
+      SET_TESTS_PROPERTIES(poisson_per_cart_par_2d
+                           PROPERTIES PASS_REGULAR_EXPRESSION "PASSED")
+   
 #      SET(ARGS ${CMAKE_CURRENT_SOURCE_DIR}/simulation/vpsim4d_input.txt)
 #      ADD_MPI_TEST(vp4d_sim test_4d ${PROCS} ${ARGS})
 #      SET_TESTS_PROPERTIES(vp4d_sim PROPERTIES PASS_REGULAR_EXPRESSION "PASSED")
-#
-#      SET(ARGS ${CMAKE_CURRENT_SOURCE_DIR}/simulation/vpsim4d_general_input.txt)
-#      ADD_MPI_TEST(vp4d_sim_general test_4d_vp_general ${PROCS} ${ARGS})
-#      SET_TESTS_PROPERTIES(vp4d_sim_general PROPERTIES PASS_REGULAR_EXPRESSION "PASSED")
-#
-#      ENDIF()
-#
+
+      SET(ARGS ${CMAKE_CURRENT_SOURCE_DIR}/simulation/vpsim4d_general_input.txt)
+      ADD_MPI_TEST(vp4d_sim_general test_4d_vp_general ${PROCS} ${ARGS})
+      SET_TESTS_PROPERTIES(vp4d_sim_general PROPERTIES PASS_REGULAR_EXPRESSION "PASSED")
+
+      ENDIF()
+
 ######
 
       SET(ARGS ${CMAKE_CURRENT_SOURCE_DIR}/simulation/vpsim4d_cartesian_input)
