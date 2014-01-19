8df2b11a4f389698a2a47fa7be61d874e4628115
Pierre Navaro <navaro@math.unistra.fr>
2014-01-19 14:02:22 +0100

    Add cmake config for hydra with GCC compiler

---
+++ cmake/hydra-gcc.cmake
@@ -0,0 +1,4 @@
+SET(CMAKE_BUILD_TYPE "Release")
+SET(CMAKE_CC_COMPILER "/u/system/SLES11/soft/gcc/4.7.3/bin/gcc" CACHE FILEPATH " " FORCE)
+SET(CMAKE_Fortran_COMPILER "/u/system/SLES11/soft/gcc/4.7.3/bin/gfortran" CACHE FILEPATH " " FORCE)
+SET(MPI_Fortran_COMPILER "/opt/ibmhpc/pecurrent/ppe.poe/bin/mpif90" CACHE FILEPATH " " FORCE)
