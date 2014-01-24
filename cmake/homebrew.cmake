dcc565e5aae39374a2896e24691f5e6286c095fe
Christophe Steiner <christophe.steiner@math.unistra.fr>
2014-01-24 10:54:00 +0100

    Add homebrew config file

---
+++ cmake/homebrew.cmake
@@ -0,0 +1,3 @@
+SET(PASTIX_ENABLED    ON                                CACHE BOOL     " " FORCE)
+SET(HDF5_PARALLEL_ENABLED    ON                                CACHE BOOL     " " FORCE)
+SET(ZLIB_LIBRARIES    "/usr/lib/libz.dylib;/usr/local/lib/libsz.a" CACHE FILEPATH " " FORCE)
