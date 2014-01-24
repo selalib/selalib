5df9f54b8873ed2a9e26a1b77ce37423d8f0fe32
Christophe Steiner <christophe.steiner@math.unistra.fr>
2014-01-24 14:19:03 +0100

    New files for gyroaverage

---
+++ prototype/src/gyroaverage/sll_gyroaverage_2d_base.F90
@@ -0,0 +1,23 @@
+module sll_module_gyroaverage_2d_base
+#include "sll_working_precision.h"
+  implicit none
+
+  type, abstract :: sll_gyroaverage_2d_base 
+  contains
+    procedure(signature_compute_gyroaverage_2d), deferred, pass(gyroaverage) :: &
+      compute_gyroaverage
+  end type sll_gyroaverage_2d_base
+
+  abstract interface
+  !> Compute Jf = gyroaverage of f with the Larmor radius larmor_rad
+    subroutine signature_compute_gyroaverage_2d( gyroaverage, larmor_rad, f, Jf )
+      use sll_working_precision
+      import sll_gyroaverage_2d_base      
+      class(sll_gyroaverage_2d_base), target :: gyroaverage
+      sll_real64,intent(in) :: larmor_rad
+      sll_real64,dimension(:,:),intent(in) :: f
+      sll_real64,dimension(:,:),intent(out) :: Jf
+    end subroutine signature_compute_gyroaverage_2d
+  end interface
+
+end module sll_module_gyroaverage_2d_base
