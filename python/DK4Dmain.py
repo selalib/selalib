ab97006eeba9afeefc8740d29c654e423a66285f
Virginie GRANDGIRARD <virginie.grandgirard@cea.fr>
2014-01-26 00:02:18 +0100

    Adding of new python diagnostics.

---
+++ python/DK4Dmain.py
@@ -0,0 +1,37 @@
+#-------------------------------------------------
+#  file : DK4Dmain.py
+#  date : 01/02/2013
+#
+# Main program for DK4D diagnostics
+#-------------------------------------------------
+
+""" Main program for DK4D diagnostics."""
+
+import os
+import GYSutils
+GYSut = GYSutils.impall('GYSutils')
+DK4D  = GYSut.impall('DK4Dresu_class')
+DK4Dd = GYSut.impall('DK4Ddiag_main')
+
+STOP = 0
+Lresu_read = True
+
+#--> Read DK4D results
+if (Lresu_read):
+    #--> Ask DK4D result directory
+    DK4Ddir = DK4D.Ask_directory()
+    if (DK4Ddir == -1):
+        # There are NO DK4D results HDF5 files !
+        STOP = 1
+    #endif
+#endif
+
+if (STOP != 1):
+    if (Lresu_read):
+        resu = DK4D.DK4Dresu(DK4Ddir)
+        resu.read_diag()
+    #end if
+#end if
+
+#--> DK4D diagnostics
+DK4Dd.DK4Ddiag(resu)
