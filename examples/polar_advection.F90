3ad822defcfb0cb0fe299ddee5be31011f68b878
Pierre Navaro <navaro@math.cnrs.fr>
2014-01-29 14:55:39 +0100

    Add an example

--- examples/polar_advection.F90
+++ examples/polar_advection.F90
@@ -7,101 +7,104 @@
 !   in the x,y variables.
 ! - parallel
 
-program vlasov_poisson_4d_polar
-#include "selalib-mpi.h"
-use sll_vlasov4d_polar
-use sll_poisson_polar_parallel
+program polar_advection
+#include "selalib.h"
+use sll_gnuplot
 
 implicit none
 
-type(vlasov4d_polar)    :: sim
-type(sll_poisson_polar) :: poisson 
+class(sll_interpolator_2d_base), pointer    :: interp_xy
+type(cubic_spline_2d_interpolator), target  :: spline_xy
 
-type(cubic_spline_2d_interpolator), target :: spl_x1x2
-type(cubic_spline_1d_interpolator), target :: spl_x3
-type(cubic_spline_1d_interpolator), target :: spl_x4
+type(sll_logical_mesh_2d), pointer                    :: logical_mesh
+class(sll_coordinate_transformation_2d_base), pointer :: transfx
 
-sll_int32 :: i, j, k, l
-sll_int32 :: loc_sz_x1,loc_sz_x2,loc_sz_x3,loc_sz_x4
-sll_int32 :: error
+sll_int32  :: itime
+sll_int32  :: nbiter = 1000
+sll_int32  :: i
+sll_int32  :: j
 
-call sll_boot_collective() 
+sll_int32,  parameter :: nc_eta1  = 64 
+sll_int32,  parameter :: nc_eta2  = 64
+sll_real64, parameter :: eta1_min = 2.0_f64 
+sll_real64, parameter :: eta1_max = 8.0_f64 
+sll_real64, parameter :: eta2_min = -3.141592654_f64
+sll_real64, parameter :: eta2_max =  3.141592654_f64
+sll_real64, parameter :: deltat   = 0.01_f64
 
-call read_input_file(sim)
+sll_int32  :: error
+sll_real64 :: eta1, eta2, eta3, eta4
+sll_real64 :: alpha1, alpha2
+sll_real64 :: inv_j(2,2)
+sll_real64, dimension(:,:), allocatable :: f
+sll_real64, dimension(:,:), allocatable :: x1
+sll_real64, dimension(:,:), allocatable :: x2
+sll_int32 :: nc_x1, nc_x2
 
-call spl_x1x2%initialize( sim%nc_eta1+1, sim%nc_eta2+1, &
-                          sim%eta1_min, sim%eta1_max,   &
-                          sim%eta2_min, sim%eta2_max,   &
-                          SLL_PERIODIC, SLL_PERIODIC )
+logical_mesh => new_logical_mesh_2d(nc_eta1,  nc_eta2,  &
+                                    eta1_min, eta1_max, & 
+                                    eta2_min, eta2_max)
 
-call spl_x3%initialize(sim%nc_eta3+1,sim%eta3_min,sim%eta3_max,SLL_PERIODIC)
+call spline_xy%initialize( nc_eta1+1, nc_eta2+1, &
+                           eta1_min, eta1_max,   &
+                           eta2_min, eta2_max,   &
+                           SLL_PERIODIC, SLL_PERIODIC )
+!interp_xy => spline_xy
 
-call spl_x4%initialize(sim%nc_eta4+1,sim%eta4_min,sim%eta4_max,SLL_PERIODIC)
+transfx => new_coordinate_transformation_2d_analytic( &
+       "analytic_polar_transformation", &
+       logical_mesh, &
+       polar_x1, &
+       polar_x2, &
+       polar_jac11, &
+       polar_jac12, &
+       polar_jac21, &
+       polar_jac22, (/0.0_f64/) )
 
-call initialize_vp4d_polar( sim, spl_x1x2, spl_x3, spl_x4)
+nc_x1 = nc_eta1
+nc_x2 = nc_eta2
+SLL_CLEAR_ALLOCATE(x1(1:nc_x1+1,1:nc_x2+1),error)
+SLL_CLEAR_ALLOCATE(x2(1:nc_x1+1,1:nc_x2+1),error)
+SLL_CLEAR_ALLOCATE(f(1:nc_eta1+1,1:nc_eta2+1),error)
 
-!call initialize( poisson,       &
-!                 sim%layout_x1, &
-!                 sim%layout_x2, &
-!                 sim%eta1_min,  &
-!                 sim%eta1_max,  &
-!                 sim%nc_eta1,   &
-!                 sim%nc_eta2,   &
-!                 SLL_DIRICHLET, &
-!                 SLL_DIRICHLET)
-
-call compute_local_sizes_4d(sim%layout_v, &
-                            loc_sz_x1,    &
-                            loc_sz_x2,    &
-                            loc_sz_x3,    &
-                            loc_sz_x4)
-
-do l=1,loc_sz_x4 
-do k=1,loc_sz_x3
-do j=1,loc_sz_x2 
-do i=1,loc_sz_x1 
-
-   sim%ft(i,j,k,l)= exp(-0.5_f64*((sim%x1(i,j)-5.)**2+(sim%x2(i,j))**2))
-
-end do
-end do
-end do
-end do
-
-
-
-do itime = 1, sim%nbiter
-
-   if(prank == MPI_MASTER) &
-      print *, itime, ' of ', sim%nbiter, sim%dt
-
-   call plot_f(sim)
-
-   call apply_remap_4D( sim%v_to_x, sim%ft, sim%f )
-
-   call advection_x1x2(sim, sim%dt)
-
-   !call compute_charge_density(sim)
-   !call plot_rho(sim)
-   !call solve(sim%poisson,sim%rho,sim%phi_x2)
-   !call plot_phi(sim)
-   !call plot_ft(sim)
-
-   call apply_remap_4D(sim%x_to_v,sim%f,sim%ft)
-
-   !call apply_remap_2D(sim%rmp_x2x1,sim%phi_x2,sim%phi_x1)
-   !call compute_electric_fields_eta1(sim)
-   !call apply_remap_2D(sim%rmp_x1x2,sim%phi_x1,sim%phi_x2)
-   !call compute_electric_fields_eta2(sim)
-   !call advection_x3(sim,sim%dt)
-   !call advection_x4(sim,sim%dt)
-
+do j=1,nc_eta2+1
+   do i=1,nc_eta1+1
+      x1(i,j) = transfx%x1_at_node(i,j)
+      x2(i,j) = transfx%x2_at_node(i,j)
+      f(i,j)  = exp(-0.5_f64*((x1(i,j)-5.)**2+(x2(i,j))**2))
+   end do
 end do
 
-call delete( spl_x1x2 )
-call delete( spl_x3 )
-call delete( spl_x4 )
 
-call sll_halt_collective()
+do itime = 1, nbiter
 
-end program vlasov_poisson_4d_polar
+  call spline_xy%compute_interpolants(f)
+
+   do j=1,nc_eta2+1
+      do i=1,nc_eta1+1
+
+         eta1 = eta1_min + (i-1)*logical_mesh%delta_eta1
+         eta2 = eta2_min + (j-1)*logical_mesh%delta_eta2
+         eta3 =  eta1*sin(eta2)
+         eta4 = -eta1*cos(eta2)
+
+         inv_j  = transfx%inverse_jacobian_matrix(eta1,eta2)
+         alpha1 = -deltat*(inv_j(1,1)*eta3 + inv_j(1,2)*eta4)
+         alpha2 = -deltat*(inv_j(2,1)*eta3 + inv_j(2,2)*eta4)
+
+         eta1 = eta1_min + modulo(eta1 - eta1_min - alpha1, eta1_max-eta1_min)
+         eta2 = eta2_min + modulo(eta2 - eta2_min - alpha2, eta2_max-eta2_min)
+
+         f(i,j) = spline_xy%interpolate_value(eta1,eta2)
+
+      end do
+   end do
+
+   call sll_gnuplot_2d(nc_x1+1,nc_x2+1,x1,x2,f,"f_polar_advection",itime,error)
+
+end do
+
+
+end program polar_advection
+
+
