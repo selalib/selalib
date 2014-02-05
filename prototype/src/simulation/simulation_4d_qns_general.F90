7f1bd2a86b4ed9f82030c23887df4da036eb910e
Aurore Back <aurore.back@gmail.com>
2014-02-05 19:26:28 +0100

    fichier python modif to have a better read data nurbs_patch_txt_to_nml.py

--- prototype/src/simulation/simulation_4d_qns_general.F90
+++ prototype/src/simulation/simulation_4d_qns_general.F90
@@ -384,7 +384,7 @@ contains
     sll_int32, dimension(1:4)      :: gi4d   ! for storing global indices
     sll_real64 :: efield_energy_total
     ! The following could probably be abstracted for convenience
-#define BUFFER_SIZE 50
+#define BUFFER_SIZE 10
     sll_real64, dimension(BUFFER_SIZE) :: buffer
     sll_real64, dimension(BUFFER_SIZE) :: buffer_result
     sll_real64, dimension(BUFFER_SIZE) :: num_particles_local
