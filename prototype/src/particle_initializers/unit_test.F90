0a2e8bbda26e158f2cb5da44bcc795e2a57523ea
Sever Hirstoaga <hirstoaga@math.unistra.fr>
2014-02-06 16:04:50 +0100

    OK for particle initializer

--- prototype/src/particle_initializers/unit_test.F90
+++ prototype/src/particle_initializers/unit_test.F90
@@ -2,20 +2,57 @@ program initialize_tester
 #include "sll_working_precision.h"
 #include "sll_memory.h"
 #include "sll_assert.h"
-#define THERM_SPEED 1_f64
-#define 
-
 
   use sll_constants, only: sll_pi
   use sll_particle_group_2d_module
   use sll_particle_initializers
+  use sll_logical_meshes
 
+#define THERM_SPEED 1._f64
+#define NUM_PARTICLES 100000_i64
+#define GUARD_SIZE    10000_i64
+#define PARTICLE_ARRAY_SIZE 150000_i64
+#define ALPHA  0.5_f64
+#define NC_X 64_i32
+#define XMIN 0._f64
+#define KX   0.5_f64
+#define XMAX 2._f64*sll_pi/KX
+#define NC_Y 16_i32
+#define YMIN 0._f64
+#define YMAX 1._f64
+
+  
   implicit none
+  type(sll_particle_group_2d), pointer :: init_group
+  type(sll_logical_mesh_2d),   pointer :: m2d
+  sll_int64 :: j
+  
+  init_group => new_particle_2d_group( &
+       NUM_PARTICLES, &
+       PARTICLE_ARRAY_SIZE, &
+       GUARD_SIZE )
+  
+  m2d =>  new_logical_mesh_2d( NC_X, NC_Y, &
+       XMIN, XMAX, YMIN, YMAX )
 
+  call sll_initialize_some4Dfunction( THERM_SPEED, &
+       ALPHA, KX, m2d, &
+       NUM_PARTICLES, init_group )
 
-!!$  open(83,file='vitesses.dat')
-!!$  write(83,*) p_group%p_list(j)%vx, p_group%p_list(j)%vy
-!!$  close(83)
+  open(83,file='vitesses_positions.dat')
+  do j=1,NUM_PARTICLES
+     write(83,*) init_group%p_list(j)%vx, init_group%p_list(j)%vy, &
+     init_group%p_list(j)%ic, init_group%p_list(j)%dx, init_group%p_list(j)%dy, &
+     init_group%p_list(j)%dx + m2d%delta_eta1 * real( &!  reconstruction of part%X
+       mod(init_group%p_list(j)%ic-1,m2d%num_cells1), f64), &
+     init_group%p_list(j)%dy + m2d%delta_eta2  &!  reconstruction of part%Y
+     * real( int( (init_group%p_list(j)%ic-1)/m2d%num_cells1) )
+  enddo
+  close(83)
+  
+  call sll_delete( init_group )
+  print*, "PASSED"
 
-
+contains
+  
 end program initialize_tester
