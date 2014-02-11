4c2b49c06185d2e072984d276c3d96c628c1b101
Edwin Chacon-Golcher <edwincg@gmail.com>
2014-02-11 16:16:01 +0100

    still trying to get rid of the gfortran 4.6 problem

--- prototype/src/parallel_array_initializers/sll_parallel_array_initializer_module.F90
+++ prototype/src/parallel_array_initializers/sll_parallel_array_initializer_module.F90
@@ -876,6 +876,7 @@ contains
     sll_real64 :: x4
     sll_int32  :: case_selector
     sll_int32    :: sub1,sub2,sub3,sub4
+    type(sll_logical_mesh_2d), pointer :: m
     sll_int32, dimension(1:4)  :: gi ! global indices in the distributed array
 
     if( .not. associated(layout) ) then
@@ -921,7 +922,8 @@ contains
     end if
 
     if(present( transf_x1_x2 ) ) then
-       if(.not. associated(transf_x1_x2%get_logical_mesh(),mesh2d_eta1_eta2) )&
+       m => transf_x1_x2%get_logical_mesh()
+       if(.not. associated(m, mesh2d_eta1_eta2) )&
           then 
 
           print *, 'sll_4d_parallel_array_initializer warning: ', &
@@ -933,7 +935,8 @@ contains
     end if
 
     if(present( transf_x3_x4 ) ) then
-       if(.not. associated(transf_x3_x4%get_logical_mesh(),mesh2d_eta3_eta4) )&
+       m => transf_x3_x4%get_logical_mesh()
+       if(.not. associated(m,mesh2d_eta3_eta4) )&
           then 
 
           print *, 'sll_4d_parallel_array_initializer warning: ', &
