bea4c4d34dbdaedf8d2b7d02c24b1e398394f68f
Edwin Chacon-Golcher <edwincg@gmail.com>
2014-02-13 18:12:09 +0100

    working version of fields multipatch, except for hermite BCs in splines

--- prototype/src/fields_multipatch/unit_test_2d.F90
+++ prototype/src/fields_multipatch/unit_test_2d.F90
@@ -9,6 +9,7 @@ program unit_test_fields_multipatch
   type(sll_coordinate_transformation_multipatch_2d), pointer :: T
   type(sll_scalar_field_multipatch_2d), pointer              :: F
   type(sll_logical_mesh_2d), pointer                         :: m
+  type(sll_coordinate_transformation_2d_nurbs), pointer      :: transf
   sll_int32  :: ipatch
   sll_int32  :: i
   sll_int32  :: j
@@ -20,6 +21,8 @@ program unit_test_fields_multipatch
   sll_real64 :: eta2
   sll_real64 :: delta1
   sll_real64 :: delta2
+  sll_real64 :: x1
+  sll_real64 :: x2
 
   T => new_coordinate_transformation_multipatch_2d("identity_mp_info.nml")
   print *, 'initialized multipatch transformation'
@@ -32,10 +35,19 @@ program unit_test_fields_multipatch
 
   print *, 'initializing the multipatch field'
   ! loop over patches. This is necessary as in the general case we will not
-  ! have a 'global' 2d array in which we could simply loop.
+  ! have a 'global' or 'abstract' 2d array we could use to reason. This is
+  ! because the physical domain could be modelled by a collection of patches
+  ! connected in some weird way.
   num_patches = F%get_number_patches()
+
+  ! There is a lot of work here to initialize the multipatch. Could we do any
+  ! better and abstract this? This could be made into a function that takes
+  ! an analytical function initializer... but I guess that with the information
+  ! coming from CAID, we could also assume that the initial data also comes
+  ! from outside...
   do ipatch=0, num_patches-1
-     m => F%get_logical_mesh(ipatch)
+     m        => F%get_logical_mesh(ipatch)
+     transf   => F%get_transformation(ipatch)
      num_pts1 = m%num_cells1+1
      num_pts2 = m%num_cells2+1
      delta1   = m%delta_eta1
@@ -46,7 +58,9 @@ program unit_test_fields_multipatch
            ! here it is assumed that the eta_min's are = 0. This is supposed
            ! to be the case for NURBS transformations.
            eta1 = (i-1)*delta1
-           val  = test_function(eta1,eta2)
+           x1 = transf%x1(eta1,eta2)
+           x2 = transf%x2(eta1,eta2)
+           val  = test_function(x1,x2)
            call F%set_value_at_indices( i, j, ipatch, val )     
         end do
      end do
