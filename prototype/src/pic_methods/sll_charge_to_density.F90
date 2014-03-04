1a439260840415b86a56dc6eaac0c9ca2b2c11b6
Sever Hirstoaga <hirstoaga@math.unistra.fr>
2014-03-04 10:57:33 +0100

    up

--- prototype/src/pic_methods/sll_charge_to_density.F90
+++ prototype/src/pic_methods/sll_charge_to_density.F90
@@ -44,17 +44,23 @@ contains
     type(charge_accumulator_cell), dimension(1:m2d%num_cells2*m2d%num_cells1), intent(in) :: all_charge
     sll_real64, dimension(1:1+m2d%num_cells1, 1:1+m2d%num_cells2) , intent(inout) :: density
     sll_int32 :: k, i, j
+    sll_int32 :: ncx, ncy
+    sll_real64 :: delta_x, delta_y
 
-    do k = 1, m2d%num_cells2*m2d%num_cells1
-       i = mod( k, m2d%num_cells1 )
-       j = int( k/m2d%num_cells2  )
-       density(i  ,j  ) = density(i,j)    + all_charge(jj)%q_sw
-       density(i+1,j  ) = density(i+1,j)  + all_charge(jj)%q_se
-       density(i  ,j+1) = density(i,j+1)  + all_charge(jj)%q_nw
-       density(i+1,j+1) = density(i+1,j+1)+ all_charge(jj)%q_ne
-       
+    ncx = m2d%num_cells1
+    ncy = m2d%num_cells2
+    delta_x = m2d%delta_eta1
+    delta_y = m2d%delta_eta2
+
+    do k = 1, ncx * ncy
+       i = mod( k, ncx )
+       j = int( k/ ncy  )
+       density(i  ,j  ) = density(i,j)    + all_charge(k)%q_sw
+       density(i+1,j  ) = density(i+1,j)  + all_charge(k)%q_se
+       density(i  ,j+1) = density(i,j+1)  + all_charge(k)%q_nw
+       density(i+1,j+1) = density(i+1,j+1)+ all_charge(k)%q_ne
     enddo
-    density = density/(m2d%delta_eta1 * m2d%delta_eta2)
+    density = density/(delta_x*delta_y)
   end subroutine sll_charge_meshdensity
 
 
