e9a340bc96dfd322dcf6219a18a403e20b297152
Pierre Navaro <navaro@math.cnrs.fr>
2014-03-11 17:26:21 +0100

    Create new module for spectral-charge

--- slv2d/vlasov_4d/sll_vlasov4d_spectral_charge.F90
+++ slv2d/vlasov_4d/sll_vlasov4d_spectral_charge.F90
@@ -3,7 +3,7 @@ sz_array = int((array_size/2+1),C_SIZE_T);                \
 p_array = fftw_alloc_complex(sz_array);                   \
 call c_f_pointer(p_array, array, [array_size/2+1])        \
 
-module sll_vlasov4d_spectral
+module sll_vlasov4d_spectral_charge
 
 #define MPI_MASTER 0
 #include "sll_working_precision.h"
@@ -17,17 +17,14 @@ use sll_constants
 
 
  use, intrinsic :: iso_c_binding
- use used_precision
- use geometry_module
- use diagnostiques_module
  use sll_vlasov4d_base
 
  implicit none
  private
- public :: new, free, densite_courantx, densite_couranty
+ public :: initialize, free, densite_courantx, densite_couranty
  public :: advection_x1, advection_x2, advection_x3x4
 
- type, public, extends(vlasov4d_base) :: vlasov4d_spectral
+ type, public, extends(vlasov4d_base) :: vlasov4d_spectral_charge
 
    sll_real64, dimension(:,:), pointer               :: exn
    sll_real64, dimension(:,:), pointer               :: eyn
@@ -52,7 +49,7 @@ use sll_constants
    sll_real64, dimension(:,:,:,:),  pointer :: ft_star
 
 
- end type vlasov4d_spectral
+ end type vlasov4d_spectral_charge
 
  sll_int32, private :: i, j, k, l
  sll_int32, private :: loc_sz_i,loc_sz_j,loc_sz_k,loc_sz_l
@@ -61,53 +58,45 @@ use sll_constants
 
 include 'fftw3.f03'
 
- interface new
-    module procedure new_vlasov4d_spectral
- end interface new
+ interface initialize
+    module procedure initialize_vlasov4d_spectral_charge
+ end interface initialize
  interface free
-    module procedure free_vlasov4d_spectral
+    module procedure free_vlasov4d_spectral_charge
  end interface free
 
 contains
 
- subroutine new_vlasov4d_spectral(this,geomx,geomv,interp_x3x4,error)
+ subroutine initialize_vlasov4d_spectral_charge(this,interp_x3x4,error)
 
   use sll_hdf5_io
 
-  class(vlasov4d_spectral),intent(inout)   :: this
-  type(geometry),intent(in)               :: geomx
-  type(geometry),intent(in)               :: geomv
+  class(vlasov4d_spectral_charge),intent(inout)   :: this
   class(sll_interpolator_2d_base), target :: interp_x3x4
   sll_int32                               :: error
 
-  sll_int32         :: nc_x1, nc_x2, nc_x3, nc_x4
   sll_real64        :: dx, dy, kx0, ky0
   integer(C_SIZE_T) :: sz_tmp_x, sz_tmp_y
   sll_int32         :: psize, prank, comm
 
   this%interp_x3x4 => interp_x3x4
 
-  call new_vlasov4d_base(this,geomx,geomv,error)
-
-  nc_x1 = this%geomx%nx
-  nc_x2 = this%geomx%ny
-  nc_x3 = this%geomv%nx
-  nc_x4 = this%geomv%ny
+  call initialize_vlasov4d_base(this)
 
   prank = sll_get_collective_rank(sll_world_collective)
   psize = sll_get_collective_size(sll_world_collective)
   comm  = sll_world_collective%comm
 
-  this%layout_x1 => new_layout_2D( sll_world_collective )        
+ this%layout_x1 => new_layout_2D( sll_world_collective )        
   call initialize_layout_with_distributed_2D_array( &
-             geomx%nx,geomx%ny,1,int(psize,4),this%layout_x1)
+             this%nc_eta1,this%nc_eta2,1,int(psize,4),this%layout_x1)
 
 !  call compute_local_sizes_2d(this%layout_x1,loc_sz_i,loc_sz_j)        
 !  SLL_CLEAR_ALLOCATE(this%jx1(1:loc_sz_i,1:loc_sz_j),error)
 
   this%layout_x2 => new_layout_2D( sll_world_collective )
   call initialize_layout_with_distributed_2D_array( &
-              geomx%nx,geomx%ny,int(psize,4),1,this%layout_x2)
+              this%nc_eta1,this%nc_eta2,int(psize,4),1,this%layout_x2)
 
 !  call compute_local_sizes_2d(this%layout_x2,loc_sz_i,loc_sz_j)        
 !  SLL_CLEAR_ALLOCATE(this%jx2(1:loc_sz_i,1:loc_sz_j),ierr)
@@ -124,49 +113,47 @@ contains
 
   end if
 
-  SLL_CLEAR_ALLOCATE(this%ex(1:nc_x1,1:nc_x2),error)
-  SLL_CLEAR_ALLOCATE(this%ey(1:nc_x1,1:nc_x2),error)
-  SLL_CLEAR_ALLOCATE(this%exn(1:nc_x1,1:nc_x2),error)
-  SLL_CLEAR_ALLOCATE(this%eyn(1:nc_x1,1:nc_x2),error)
-  SLL_CLEAR_ALLOCATE(this%bzn(1:nc_x1,1:nc_x2),error)
+  SLL_CLEAR_ALLOCATE(this%ex(1:this%nc_eta1,1:this%nc_eta2),error)
+  SLL_CLEAR_ALLOCATE(this%ey(1:this%nc_eta1,1:this%nc_eta2),error)
+  SLL_CLEAR_ALLOCATE(this%exn(1:this%nc_eta1,1:this%nc_eta2),error)
+  SLL_CLEAR_ALLOCATE(this%eyn(1:this%nc_eta1,1:this%nc_eta2),error)
+  SLL_CLEAR_ALLOCATE(this%bzn(1:this%nc_eta1,1:this%nc_eta2),error)
 
-  SLL_CLEAR_ALLOCATE(this%bz(1:nc_x1,1:nc_x2),error)
-  SLL_CLEAR_ALLOCATE(this%rho(1:nc_x1,1:nc_x2),error)
+  SLL_CLEAR_ALLOCATE(this%bz(1:this%nc_eta1,1:this%nc_eta2),error)
+  SLL_CLEAR_ALLOCATE(this%rho(1:this%nc_eta1,1:this%nc_eta2),error)
 
-  SLL_CLEAR_ALLOCATE(this%jx(1:nc_x1,1:nc_x2),error)
-  SLL_CLEAR_ALLOCATE(this%jy(1:nc_x1,1:nc_x2),error)
+  SLL_CLEAR_ALLOCATE(this%jx(1:this%nc_eta1,1:this%nc_eta2),error)
+  SLL_CLEAR_ALLOCATE(this%jy(1:this%nc_eta1,1:this%nc_eta2),error)
 
-  SLL_CLEAR_ALLOCATE(this%jx1(1:nc_x1,1:nc_x2),error)
-  SLL_CLEAR_ALLOCATE(this%jx2(1:nc_x1,1:nc_x2),error)
-  SLL_CLEAR_ALLOCATE(this%jx3(1:nc_x1,1:nc_x2),error)
-  SLL_CLEAR_ALLOCATE(this%jy1(1:nc_x1,1:nc_x2),error)
-  SLL_CLEAR_ALLOCATE(this%jy2(1:nc_x1,1:nc_x2),error)
-  SLL_CLEAR_ALLOCATE(this%jy3(1:nc_x1,1:nc_x2),error)
+  SLL_CLEAR_ALLOCATE(this%jx1(1:this%nc_eta1,1:this%nc_eta2),error)
+  SLL_CLEAR_ALLOCATE(this%jx2(1:this%nc_eta1,1:this%nc_eta2),error)
+  SLL_CLEAR_ALLOCATE(this%jx3(1:this%nc_eta1,1:this%nc_eta2),error)
+  SLL_CLEAR_ALLOCATE(this%jy1(1:this%nc_eta1,1:this%nc_eta2),error)
+  SLL_CLEAR_ALLOCATE(this%jy2(1:this%nc_eta1,1:this%nc_eta2),error)
+  SLL_CLEAR_ALLOCATE(this%jy3(1:this%nc_eta1,1:this%nc_eta2),error)
   
-  FFTW_ALLOCATE(this%tmp_x,nc_x1/2+1,sz_tmp_x,this%p_tmp_x)
-  FFTW_ALLOCATE(this%tmp_y,nc_x2/2+1,sz_tmp_y,this%p_tmp_y)
-  SLL_CLEAR_ALLOCATE(this%d_dx(1:nc_x1),error)
-  SLL_CLEAR_ALLOCATE(this%d_dy(1:nc_x2),error)
+  FFTW_ALLOCATE(this%tmp_x,this%nc_eta1/2+1,sz_tmp_x,this%p_tmp_x)
+  FFTW_ALLOCATE(this%tmp_y,this%nc_eta2/2+1,sz_tmp_y,this%p_tmp_y)
+  SLL_CLEAR_ALLOCATE(this%d_dx(1:this%nc_eta1),error)
+  SLL_CLEAR_ALLOCATE(this%d_dy(1:this%nc_eta2),error)
 
-  this%fwx = fftw_plan_dft_r2c_1d(nc_x1,this%d_dx, this%tmp_x,FFTW_ESTIMATE)
-  this%bwx = fftw_plan_dft_c2r_1d(nc_x1,this%tmp_x,this%d_dx, FFTW_ESTIMATE)
-  this%fwy = fftw_plan_dft_r2c_1d(nc_x2,this%d_dy, this%tmp_y,FFTW_ESTIMATE)
-  this%bwy = fftw_plan_dft_c2r_1d(nc_x2,this%tmp_y,this%d_dy, FFTW_ESTIMATE)
+  this%fwx = fftw_plan_dft_r2c_1d(this%nc_eta1,this%d_dx, this%tmp_x,FFTW_ESTIMATE)
+  this%bwx = fftw_plan_dft_c2r_1d(this%nc_eta1,this%tmp_x,this%d_dx, FFTW_ESTIMATE)
+  this%fwy = fftw_plan_dft_r2c_1d(this%nc_eta2,this%d_dy, this%tmp_y,FFTW_ESTIMATE)
+  this%bwy = fftw_plan_dft_c2r_1d(this%nc_eta2,this%tmp_y,this%d_dy, FFTW_ESTIMATE)
 
-  SLL_CLEAR_ALLOCATE(this%kx(1:nc_x1/2+1), error)
-  SLL_CLEAR_ALLOCATE(this%ky(1:nc_x2/2+1), error)
+  SLL_CLEAR_ALLOCATE(this%kx(1:this%nc_eta1/2+1), error)
+  SLL_CLEAR_ALLOCATE(this%ky(1:this%nc_eta2/2+1), error)
    
-  dx = geomx%dx
-  dy = geomx%dy
+  kx0 = 2._f64*sll_pi/(this%nc_eta1*this%delta_eta1)
+  ky0 = 2._f64*sll_pi/(this%nc_eta2*this%delta_eta2)
 
-  kx0 = 2._f64*sll_pi/(nc_x1*dx)
-  ky0 = 2._f64*sll_pi/(nc_x2*dy)
 
-  do i=1,nc_x1/2+1
+  do i=1,this%nc_eta1/2+1
      this%kx(i) = (i-1)*kx0
   end do
   this%kx(1) = 1.0_f64
-  do j=1,nc_x2/2+1
+  do j=1,this%nc_eta2/2+1
      this%ky(j) = (j-1)*ky0
   end do
   this%ky(1) = 1.0_f64
@@ -179,11 +166,11 @@ contains
                               loc_sz_i,loc_sz_j,loc_sz_k,loc_sz_l)        
   SLL_CLEAR_ALLOCATE(this%ft_star(1:loc_sz_i,1:loc_sz_j,1:loc_sz_k,1:loc_sz_l),ierr)
 
- end subroutine new_vlasov4d_spectral
+ end subroutine initialize_vlasov4d_spectral_charge
 
- subroutine free_vlasov4d_spectral(this)
+ subroutine free_vlasov4d_spectral_charge(this)
 
-  class(vlasov4d_spectral) :: this
+  class(vlasov4d_spectral_charge) :: this
 
   call delete_layout_4D(this%layout_x)
   call delete_layout_4D(this%layout_v)
@@ -191,16 +178,17 @@ contains
   SLL_DEALLOCATE_ARRAY(this%ft, ierr)
   if (c_associated(this%p_tmp_x)) call fftw_free(this%p_tmp_x)
   if (c_associated(this%p_tmp_y)) call fftw_free(this%p_tmp_y)
-  call dfftw_destroy_plan(this%fwx)
-  call dfftw_destroy_plan(this%fwy)
-  call dfftw_destroy_plan(this%bwx)
-  call dfftw_destroy_plan(this%bwy)
 
- end subroutine free_vlasov4d_spectral
+  call fftw_destroy_plan(this%fwx)
+  call fftw_destroy_plan(this%fwy)
+  call fftw_destroy_plan(this%bwx)
+  call fftw_destroy_plan(this%bwy)
+
+ end subroutine free_vlasov4d_spectral_charge
 
  subroutine advection_x1(this,dt)
 
-  class(vlasov4d_spectral), intent(inout) :: this
+  class(vlasov4d_spectral_charge), intent(inout) :: this
 
   sll_real64, intent(in) :: dt
   sll_real64 :: vx, x3_min, delta_x3
@@ -208,8 +196,8 @@ contains
   ! verifier que la transposition est a jours
   SLL_ASSERT( .not. this%transposed) 
 
-  x3_min   = this%geomv%x0
-  delta_x3 = this%geomv%dx
+  x3_min   = this%eta3_min
+  delta_x3 = this%delta_eta3
 
   call compute_local_sizes_4d(this%layout_x,loc_sz_i,loc_sz_j,loc_sz_k,loc_sz_l)        
   
@@ -222,7 +210,7 @@ contains
            call fftw_execute_dft_r2c(this%fwx, this%f(:,j,k,l),this%tmp_x)
            !exact : f* = f^n exp(-i kx vx dt)
            !calcul du flux
-           do i=2,this%geomx%nx
+           do i=2,this%nc_eta1
               this%tmp_x(i) = this%tmp_x(i)*(1._f64-exp(-cmplx(0.0_f64,1,kind=f64)*vx*this%kx(i)))*cmplx(0.0_f64,-1._f64,kind=f64)/(dt*this%kx(i))
            enddo
            this%tmp_x(1)=0._f64
@@ -244,7 +232,7 @@ contains
         do j=1,loc_sz_j
            call fftw_execute_dft_r2c(this%fwx, this%f(:,j,k,l),this%tmp_x)
            !exact : f* = f^n exp(-i kx vx dt)
-           do i=2,this%geomx%nx
+           do i=2,this%nc_eta1
               this%tmp_x(i) = this%tmp_x(i)*exp(-cmplx(0.0_f64,this%kx(i),kind=f64)*vx)
            enddo
            call fftw_execute_dft_c2r(this%bwx, this%tmp_x, this%d_dx)
@@ -257,7 +245,7 @@ contains
 
  subroutine advection_x2(this,dt)
 
-  class(vlasov4d_spectral),intent(inout) :: this
+  class(vlasov4d_spectral_charge),intent(inout) :: this
 
   sll_real64, intent(in) :: dt
   sll_real64 :: x4_min, delta_x4
@@ -265,8 +253,8 @@ contains
 
   SLL_ASSERT( .not. this%transposed)
 
-  x4_min   = this%geomv%y0
-  delta_x4 = this%geomv%dy
+  x4_min   = this%eta4_min
+  delta_x4 = this%delta_eta4
   call compute_local_sizes_4d(this%layout_x,loc_sz_i,loc_sz_j,loc_sz_k,loc_sz_l)        
 
   do l=1,loc_sz_l
@@ -276,7 +264,7 @@ contains
      do k=1,loc_sz_k
         do i=1,loc_sz_i
            call fftw_execute_dft_r2c(this%fwy, this%f(i,:,k,l), this%tmp_y)
-           do j=2,this%geomx%ny
+           do j=2,this%nc_eta2
               this%tmp_y(j) = this%tmp_y(j)*(1._f64-exp(-cmplx(0.0_f64,1,kind=f64)*vy*this%ky(j)))*cmplx(0.0_f64,-1._f64,kind=f64)/(dt*this%ky(j))
            enddo
            this%tmp_y(1)=0._f64
@@ -296,7 +284,7 @@ contains
      do k=1,loc_sz_k
         do i=1,loc_sz_i
            call fftw_execute_dft_r2c(this%fwy, this%f(i,:,k,l), this%tmp_y)
-           do j=2,this%geomx%ny
+           do j=2,this%nc_eta2
               this%tmp_y(j) = this%tmp_y(j)*exp(-cmplx(0.0_f64,this%ky(j),kind=f64)*vy)
            enddo
            call fftw_execute_dft_c2r(this%bwy, this%tmp_y, this%d_dy)
@@ -309,22 +297,22 @@ end subroutine advection_x2
 
 subroutine advection_x3x4(this,dt)
 
-  class(vlasov4d_spectral),intent(inout) :: this
+  class(vlasov4d_spectral_charge),intent(inout) :: this
   sll_real64, intent(in) :: dt
-  sll_real64, dimension(this%geomv%nx,this%geomv%ny) :: alpha_x
-  sll_real64, dimension(this%geomv%nx,this%geomv%ny) :: alpha_y
-  sll_real64, dimension(this%geomv%nx,this%geomv%ny) :: alpha_x_tmp
-  sll_real64, dimension(this%geomv%nx,this%geomv%ny) :: alpha_y_tmp
+  sll_real64, dimension(this%nc_eta3,this%nc_eta4) :: alpha_x
+  sll_real64, dimension(this%nc_eta3,this%nc_eta4) :: alpha_y
+  sll_real64, dimension(this%nc_eta3,this%nc_eta4) :: alpha_x_tmp
+  sll_real64, dimension(this%nc_eta3,this%nc_eta4) :: alpha_y_tmp
   sll_real64 :: px, py, ctheta, stheta, depvx, depvy
   sll_real64 :: x3_min, x3_max, x4_min, x4_max
   sll_real64 :: delta_x3, delta_x4
 
-  x3_min   = this%geomv%x0
-  x3_max   = this%geomv%x1
-  delta_x3 = this%geomv%dx
-  x4_min   = this%geomv%y0 
-  x4_max   = this%geomv%y1
-  delta_x4 = this%geomv%dy
+  x3_min   = this%eta3_min
+  x3_max   = this%eta3_max
+  delta_x3 = this%delta_eta3
+  x4_min   = this%eta4_min
+  x4_max   = this%eta4_max
+  delta_x4 = this%delta_eta4
 
   SLL_ASSERT(this%transposed) 
   call compute_local_sizes_4d(this%layout_v,loc_sz_i,loc_sz_j,loc_sz_k,loc_sz_l)        
@@ -363,7 +351,7 @@ end subroutine advection_x3x4
 
  subroutine densite_courantx(this,star)
 
-   class(vlasov4d_spectral),intent(inout)  :: this
+   class(vlasov4d_spectral_charge),intent(inout)  :: this
    character(len=1), intent(in), optional  :: star
    sll_real64, dimension(:,:,:,:), pointer :: df
 
@@ -372,7 +360,7 @@ end subroutine advection_x3x4
    sll_int32  :: comm
    sll_real64 :: dxy
    sll_real64 :: vx 
-   sll_real64, dimension(this%geomx%nx,this%geomx%ny) :: locjx
+   sll_real64, dimension(this%nc_eta1,this%nc_eta2) :: locjx
 
    if( present(star)) then
 !      df => this%ft_star
@@ -382,7 +370,7 @@ end subroutine advection_x3x4
       df => this%f
    end if
    
-   dxy = this%geomv%dx*this%geomv%dy
+   dxy = this%delta_eta3*this%delta_eta4
    SLL_ASSERT(this%transposed)
    call compute_local_sizes_4d(this%layout_v, &
                                loc_sz_i,      &
@@ -400,7 +388,7 @@ end subroutine advection_x3x4
                gj = global_indices(2)
                gk = global_indices(3)
                gl = global_indices(4)
-               vx = this%geomv%x0+(gk-1)*this%geomv%dx
+               vx = this%eta3_min+(gk-1)*this%delta_eta3
                locjx(gi,gj) = locjx(gi,gj) + dxy*df(i,j,k,l) 
             end do
          end do
@@ -409,7 +397,7 @@ end subroutine advection_x3x4
 
    this%jx1 = 0._f64
    comm = sll_world_collective%comm
-   c    = this%geomx%nx*this%geomx%ny
+   c    = this%nc_eta1*this%nc_eta2
    
    call mpi_barrier(comm,error)
    call mpi_allreduce(locjx,this%jx1,c,MPI_REAL8,MPI_SUM,comm,error)
@@ -418,7 +406,7 @@ end subroutine advection_x3x4
 
  subroutine densite_couranty(this, star)
 
-   class(vlasov4d_spectral),intent(inout) :: this
+   class(vlasov4d_spectral_charge),intent(inout) :: this
    character(len=1), intent(in), optional  :: star
 
    sll_int32  :: error
@@ -426,7 +414,7 @@ end subroutine advection_x3x4
    sll_int32  :: c
    sll_int32  :: comm
    sll_real64 :: dxy
-   sll_real64, dimension(this%geomx%nx,this%geomx%ny) :: locjy
+   sll_real64, dimension(this%nc_eta1,this%nc_eta2) :: locjy
    sll_real64, dimension(:,:,:,:), pointer :: df
 
    if( present(star)) then
@@ -438,7 +426,7 @@ end subroutine advection_x3x4
    end if
 
 
-   dxy = this%geomv%dx*this%geomv%dy
+   dxy = this%delta_eta3*this%delta_eta4
    SLL_ASSERT(this%transposed)
    call compute_local_sizes_4d(this%layout_v, &
                                loc_sz_i,loc_sz_j,loc_sz_k,loc_sz_l)        
@@ -453,7 +441,7 @@ end subroutine advection_x3x4
       gj = global_indices(2)
       gk = global_indices(3)
       gl = global_indices(4)
-      vy = this%geomv%y0+(gl-1)*this%geomv%dy
+      vy = this%eta4_min+(gl-1)*this%delta_eta4
       locjy(gi,gj) = locjy(gi,gj) + dxy*df(i,j,k,l) 
    end do
    end do
@@ -463,9 +451,9 @@ end subroutine advection_x3x4
    this%jy1(:,:) = 0.
    comm   = sll_world_collective%comm
    call mpi_barrier(comm,error)
-   c=this%geomx%nx*this%geomx%ny
+   c=this%nc_eta1*this%nc_eta2
    call mpi_allreduce(locjy,this%jy1,c, MPI_REAL8,MPI_SUM,comm,error)
 
  end subroutine densite_couranty
 
-end module sll_vlasov4d_spectral
+end module sll_vlasov4d_spectral_charge
