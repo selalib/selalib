5c6be50c43ce5ded0423525cdad3f7ff302a4f97
Pierre Navaro <navaro@math.cnrs.fr>
2014-03-20 16:31:01 +0100

    Improve mgd solver

--- prototype/src/multigrid/test_multigrid_2d.F90
+++ prototype/src/multigrid/test_multigrid_2d.F90
@@ -2,6 +2,7 @@ program test_multigrid_2d
 
 use mpi
 use diagnostics
+use sll_multigrid_2d
 # include "compdir.inc"
 
 !-----------------------------------------------------------------------
@@ -37,6 +38,8 @@ use diagnostics
 !
 ! parameters
 !
+      type(mgd2_solver) :: mgd
+
       integer nxp2,nyp2,nx,ny,nxprocs,nyprocs,ixp,jyq,iex,jey
       integer maxcy,kcycle,iprer,ipost,iresw,nwork
       integer ngrid,nxdim,nydim,IOUT
@@ -63,9 +66,8 @@ use diagnostics
       integer status(MPI_STATUS_SIZE),nbrright,nbrbottom,nbrleft,nbrtop
       REALN p(nxdim,nydim),r(nxdim,nydim),f(nxdim,nydim),work(nwork)
       REALN xl,yl,hxi,hyi,vbc(4),phibc(4,20),wk,rro,pi
-      character(len=20) outfile
-      character(len=1) num(10)
-      data (num(i),i=1,10)/'0','1','2','3','4','5','6','7','8','9'/
+
+
       pi=4.0d0*atan(1.0d0)
 
 !-----------------------------------------------------------------------
@@ -76,116 +78,19 @@ use diagnostics
       call MPI_COMM_SIZE(MPI_COMM_WORLD,numprocs,ierr)
       realtype=MPI_DOUBLE_PRECISION
 
-!-----------------------------------------------------------------------
-! open file for output of messages and check that the number of 
-! processes is correct
-!
-      outfile='out'
-      outfile(4:5)='_'
-      m1=mod(myid,10)+1
-      m0=mod(myid/10,10)+1
-      outfile(5:6)=num(m0)
-      outfile(6:7)=num(m1)
-      !open(IOUT,file=outfile,status='unknown',form='formatted')
       if (numprocs.ne.(nxprocs*nyprocs)) then
         write(IOUT,100)
 100     format(/,'ERROR: numprocs <> (nxprocs*nyprocs)',/)
+        call mpi_finalize(ierr)
         stop
       end if
-!-----------------------------------------------------------------------
-! create Cartesian topology with periodic boundary conditions
-!
-      dims(1)=nxprocs
-      dims(2)=nyprocs
-      periods(1)=.true.
-      periods(2)=.true.
-      call MPI_CART_CREATE(MPI_COMM_WORLD,2,dims,periods,.true., &
-     &                     comm2d,ierr)
-      call MPI_COMM_RANK(comm2d,myid,ierr)
-      ibdry=0
-      jbdry=0
-      vbc(1)=0.0d0
-      vbc(2)=0.0d0
-      vbc(3)=0.0d0
-      vbc(4)=0.0d0
-      do i=1,8
-        bd(i)=0
-      end do
-!-----------------------------------------------------------------------
-! find the neighbors
-! conventions
-!
-!     6 |     7     | 8
-!       |           |
-!     -----------------
-!       |           |
-!       |           |
-!     5 |   myid    | 1 
-!       |           |
-!       |           |
-!     -----------------
-!       |           |
-!     4 |     3     | 2
-!
-      call MPI_CART_SHIFT(comm2d,0,1,nbrleft,nbrright,ierr)
-      call MPI_CART_SHIFT(comm2d,1,1,nbrbottom,nbrtop,ierr)
-      neighbor(1)=nbrright
-      neighbor(3)=nbrbottom
-      neighbor(5)=nbrleft
-      neighbor(7)=nbrtop
-      call MPI_BARRIER(comm2d,ierr)
-      call MPI_SENDRECV(neighbor(3),1,MPI_INTEGER,nbrright,0, &
-     &                  neighbor(4),1,MPI_INTEGER,nbrleft,0, &
-     &                  comm2d,status,ierr)
-      call MPI_SENDRECV(neighbor(7),1,MPI_INTEGER,nbrright,1, &
-     &                  neighbor(6),1,MPI_INTEGER,nbrleft,1, &
-     &                  comm2d,status,ierr)
-      call MPI_SENDRECV(neighbor(3),1,MPI_INTEGER,nbrleft,0, &
-     &                  neighbor(2),1,MPI_INTEGER,nbrright,0, &
-     &                  comm2d,status,ierr)
-      call MPI_SENDRECV(neighbor(7),1,MPI_INTEGER,nbrleft,1, &
-     &                  neighbor(8),1,MPI_INTEGER,nbrright,1, &
-     &                  comm2d,status,ierr)
-!-----------------------------------------------------------------------
-! find indices of subdomain and check that dimensions of arrays are
-! sufficient
-!
-      call MPI_CART_GET(comm2d,2,dims,periods,coords,ierr)
-      call MPE_DECOMP1D(nxp2-2,dims(1),coords(1),sx,ex)
-      sx=sx+1
-      ex=ex+1
-      call MPE_DECOMP1D(nyp2-2,dims(2),coords(2),sy,ey)
-      sy=sy+1
-      ey=ey+1
-      if ((ex-sx+3).gt.nxdim) then
-        write(IOUT,110) myid,nxdim,ex-sx+3
-        nerror=1
-        goto 1000
-      end if
-      if ((ey-sy+3).gt.nydim) then
-        write(IOUT,120) myid,nydim,ey-sy+3
-        nerror=1
-        goto 1000
-      end if
-      write(IOUT,*) 'sx=',sx,' ex=',ex,' sy=',sy,' ey=',ey
-      do i=1,8
-        write(IOUT,*) 'neighbor: ',neighbor(i),' bd: ',bd(i)
-      end do
-110   format(/,'ERROR: process:',i3,' nxdim=',i4,' < ex-sx+3=',i4,/, &
-     &       ' -> put the parameter formula for nxdim in main.F in ', &
-     &       'comments and',/,'    assign to nxdim the maximum ', &
-     &       'value of ex-sx+3',/)
-120   format(/,'ERROR: process:',i3,' nydim=',i4,' < ey-sy+3=',i4,/, &
-     &       ' -> put the parameter formula for nydim in main.F in ', &
-     &       'comments and'/,'     assign to nydim the maximum ', &
-     &       'value of ey-sy+3',/)
-!-----------------------------------------------------------------------
-! initialize mgd2
-!
-      call mgdinit(vbc,phibc,ixp,jyq,iex,jey,ngrid,nxp2,nyp2, &
-     &             sx,ex,sy,ey,realtype,nxprocs,nyprocs,nwork, &
-     &             ibdry,jbdry,myid,IOUT,nerror)
-      if (nerror.eq.1) goto 1000
+
+
+      call initialize(mgd, &
+                      0._f64, 1._f64, nx, nxprocs, SLL_PERIODIC, &
+                      0._f64, 1._f64, ny, nyprocs, SLL_PERIODIC)
+     
+
 !-----------------------------------------------------------------------
 ! initialize problem
 ! xl,yl are the dimensions of the domain
@@ -256,8 +161,6 @@ do j=sy,ey
   end do
 end do
 
-ierr = 0
-!
 return
 end
 
@@ -274,7 +177,7 @@ REALN p(sx-1:ex+1,sy-1:ey+1),wk,hxi,hyi,pi
 ! Called in : main
 ! Calls     : MPI_ALLREDUCE
 !-----------------------------------------------------------------------
-integer i,j
+integer i,j, ierr
 REALN errloc,err,cx,cy,exact, xi, yj
 !
 ! calculate local error
@@ -298,7 +201,6 @@ call MPI_ALLREDUCE(errloc,err,1,MPI_DOUBLE_PRECISION,MPI_SUM, &
 write(IOUT,100) errloc/float(nx*ny),err/float(nx*ny)
 100   format(/,'Local error: ',e13.6,'  total error: ',e13.6,/)
 !
-ierr = 0
 
 return
 end
@@ -308,7 +210,7 @@ subroutine gscale(sx,ex,sy,ey,a,avo,acorr,comm2d,nx,ny)
 include "mpif.h"
 integer sx,ex,sy,ey,nx,ny
 REALN a(sx-1:ex+1,sy-1:ey+1),avo,acorr
-integer comm2d
+integer comm2d, ierr
 !------------------------------------------------------------------------
 ! Rescale the field a so that its average inside the domain
 ! remains constant and equal to avo. For the density,avo should
@@ -358,7 +260,6 @@ end do
 timing(49)=timing(49)+MPI_WTIME()-tinitial
 # endif
 
-ierr = 0
 
 return
 end 
@@ -389,6 +290,5 @@ endif
 e = s + nlocal - 1
 if (e .gt. n .or. myid .eq. numprocs-1) e = n
 
-ierr = 0 
 return
 end
