91830328e376680a9ce471b16822c9211887b6d7
Pierre Navaro <navaro@math.unistra.fr>
2014-03-25 16:20:17 +0100

    improve bash script for tests

--- slv2d/vlasov_4d/tests/batch_script.sh
+++ slv2d/vlasov_4d/tests/batch_script.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
-set NPROCS = 16
+export NPROCS=8
 for CASE_NAME in "landau_cos_prod" "landau_cos_sum" "landau_x" "landau_y" "tsi"
 do
    for VA_NUMBER in 0 1 2 3
    do
       cp $CASE_NAME.nml test.nml
       echo "&algo_charge va=$VA_NUMBER /" >> test.nml
-      #mpirun -np $NPROCS ../bin/vm4d_spectral_charge ./test.nml
-      #mv thf.dat thf-landau-cos-prod-va=0.dat
-      touch thf-spectral-$CASE_NAME-va=$VA_NUMBER.dat
+      mpirun -np $NPROCS ../bin/vm4d_spectral_charge ./test.nml
+      mv thf.dat thf-spectral-$CASE_NAME-va=$VA_NUMBER.dat
    done
 done
 for VA_NUMBER in 0 2
 do
-   for METH in 1, 2, 4
+   for METH in 1 2 4
    do 
       for TEST_CASE in "landau_x" "landau_cos_prod" "tsi"
       do
          cp $CASE_NAME.nml test.nml
          echo "&algo_charge va=$VA_NUMBER meth=$METH /" >> test.nml
          echo "&solver_poisson mud_case=0/" >> test.nml
-         touch thf-csl-$CASE_NAME-va=$VA_NUMBER-meth=$METH.dat
-         #mpirun -np 256 .....
+         mpirun -np $NPROCS ../bin/vm4d_transpose ./test.nml
+         mv thf.dat thf-csl-$CASE_NAME-va=$VA_NUMBER-meth=$METH.dat
       done
    done
 done
