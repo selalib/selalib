1d460b31dfc8003c1db651e26ca9d451886e87a2
Pierre Navaro <navaro@math.cnrs.fr>
2014-04-03 17:45:08 +0200

    Back to normal in multigrid

--- prototype/src/multigrid/test_multigrid_2d.F90
+++ prototype/src/multigrid/test_multigrid_2d.F90
@@ -2,8 +2,11 @@ program test_multigrid_2d
 
 use mpi
 use diagnostics
-use sll_multigrid_2d
-# include "compdir.inc"
+!use sll_multigrid_2d
+use sll_boundary_condition_descriptors
+#include "sll_working_precision.h"
+#include "compdir.inc"
+
 
 !-----------------------------------------------------------------------
 ! Test problem for 2D multigrid parallel code mgd2. mgd2 solves the
@@ -38,16 +41,16 @@ use sll_multigrid_2d
 !
 ! parameters
 !
-   type(mgd2_solver) :: mgd
+!   type(mgd2_solver) :: mgd
 
    integer nxp2,nyp2,nx,ny,nxprocs,nyprocs,ixp,jyq,iex,jey
    integer maxcy,kcycle,iprer,ipost,iresw,nwork
    integer ngrid,nxdim,nydim,IOUT
    REALN tolmax
-   parameter( nxp2=258, nyp2=258)
+   parameter( nxp2=34, nyp2=34)
    parameter( nx=nxp2-2, ny=nyp2-2)
-   parameter( nxprocs=4, nyprocs=2)
-   parameter( ixp=4, jyq=4, iex=7, jey=7)
+   parameter( nxprocs=2, nyprocs=2)
+   parameter( ixp=4, jyq=4, iex=4, jey=4)
    parameter( maxcy=300, tolmax=1.0d-05)
    parameter( kcycle=1, iprer=2, ipost=1, iresw=1)
    parameter( nwork=(4*nx*ny*8)/(3*nxprocs*nyprocs)  &
@@ -65,7 +68,15 @@ use sll_multigrid_2d
    integer nerror,m0,m1,dims(2),coords(2),i
    integer status(MPI_STATUS_SIZE),nbrright,nbrbottom,nbrleft,nbrtop
    REALN p(nxdim,nydim),r(nxdim,nydim),f(nxdim,nydim),work(nwork)
-   REALN xl,yl,hxi,hyi,vbc(4),phibc(4,20),wk,rro,pi
+   REALN xl,yl,hxi,hyi,phibc(4,20),wk,rro,pi
+   integer :: ierr
+
+
+   sll_real64 :: vbc(4)
+   sll_real64 :: x_min = 0.0_f64
+   sll_real64 :: x_max = 1.0_f64
+   sll_real64 :: y_min = 0.0_f64
+   sll_real64 :: y_max = 1.0_f64
 
 
    pi=4.0d0*atan(1.0d0)
@@ -86,10 +97,133 @@ use sll_multigrid_2d
    end if
 
 
-   call initialize(mgd, &
-                   0._f64, 1._f64, nx, nxprocs, SLL_PERIODIC, &
-                   0._f64, 1._f64, ny, nyprocs, SLL_PERIODIC)
-  
+   call MPI_COMM_RANK(MPI_COMM_WORLD,myid,ierr)
+   call MPI_COMM_SIZE(MPI_COMM_WORLD,numprocs,ierr)
+
+  ! iex = ceiling(log((nx-1.)/ixp)/log(2.))+1
+  ! jey = ceiling(log((ny-1.)/jyq)/log(2.))+1
+
+   xl = x_max - x_min
+   yl = y_max - y_min
+
+   if (numprocs.ne.(nxprocs*nyprocs)) then
+      write(IOUT,"(/,'ERROR: numprocs <> (nxprocs*nyprocs)',/)")
+      call MPI_FINALIZE(ierr)
+   end if
+
+   ! create Cartesian topology with periodic boundary conditions
+
+   dims(1)    = nxprocs
+   dims(2)    = nyprocs
+   periods(1) = .true.
+   periods(2) = .true.
+   call MPI_CART_CREATE(MPI_COMM_WORLD,2,dims,periods,.true.,comm2d,ierr)
+
+   ibdry    = 0
+   jbdry    = 0
+   vbc(1:4) = 0.0d0
+   bd(1:8)  = 0
+
+   !-----------------------------------------------------------------------
+   ! find the neighbors
+   ! conventions
+   !
+   !     6 |     7     | 8
+   !       |           |
+   !     -----------------
+   !       |           |
+   !       |           |
+   !     5 |   myid    | 1 
+   !       |           |
+   !       |           |
+   !     -----------------
+   !       |           |
+   !     4 |     3     | 2
+   !
+
+   call MPI_CART_SHIFT(comm2d,0,1,nbrleft,nbrright,ierr)
+   call MPI_CART_SHIFT(comm2d,1,1,nbrbottom,nbrtop,ierr)
+   neighbor(1)=nbrright
+   neighbor(3)=nbrbottom
+   neighbor(5)=nbrleft
+   neighbor(7)=nbrtop
+   call MPI_BARRIER(comm2d,ierr)
+   call MPI_SENDRECV(neighbor(3),1,MPI_INTEGER,nbrright,0, &
+   &                  neighbor(4),1,MPI_INTEGER,nbrleft,0, &
+   &                  comm2d,status,ierr)
+   call MPI_SENDRECV(neighbor(7),1,MPI_INTEGER,nbrright,1, &
+   &                  neighbor(6),1,MPI_INTEGER,nbrleft,1, &
+   &                  comm2d,status,ierr)
+   call MPI_SENDRECV(neighbor(3),1,MPI_INTEGER,nbrleft,0, &
+   &                  neighbor(2),1,MPI_INTEGER,nbrright,0, &
+   &                  comm2d,status,ierr)
+   call MPI_SENDRECV(neighbor(7),1,MPI_INTEGER,nbrleft,1, &
+   &                  neighbor(8),1,MPI_INTEGER,nbrright,1, &
+   &                  comm2d,status,ierr)
+
+   !-----------------------------------------------------------------------
+   ! find indices of subdomain and check that dimensions of arrays are
+   ! sufficient
+   !
+
+   call MPI_CART_GET(comm2d,2,dims,periods,coords,ierr)
+   call MPE_DECOMP1D(nx,dims(1),coords(1),sx,ex)
+   sx=sx+1
+   ex=ex+1
+   call MPE_DECOMP1D(ny,dims(2),coords(2),sy,ey)
+   sy=sy+1
+   ey=ey+1
+   if ((ex-sx+3).gt.nxdim) then
+      write(IOUT,110) myid,nxdim,ex-sx+3
+      nerror=1
+      goto 1000
+   end if
+   if ((ey-sy+3).gt.nydim) then
+      write(IOUT,120) myid,nydim,ey-sy+3
+      nerror=1
+      goto 1000
+   end if
+   write(IOUT,*) 'sx=',sx,' ex=',ex,' sy=',sy,' ey=',ey
+
+   do i=1,8
+      write(IOUT,*) 'neighbor: ',neighbor(i),' bd: ',bd(i)
+   end do
+
+110   format(/,'ERROR: process:',i3,' nxdim=',i4,' < ex-sx+3=',i4,/, &
+     &       ' -> put the parameter formula for nxdim in main.F in ', &
+     &       'comments and',/,'    assign to nxdim the maximum ', &
+     &       'value of ex-sx+3',/)
+120   format(/,'ERROR: process:',i3,' nydim=',i4,' < ey-sy+3=',i4,/, &
+     &       ' -> put the parameter formula for nydim in main.F in ', &
+     &       'comments and'/,'     assign to nydim the maximum ', &
+     &       'value of ey-sy+3',/)
+
+   ibdry  = 0    ! Periodic bc in direction x
+   jbdry  = 0    ! Periodic bc in direction y
+
+   call mgdinit(   vbc,         &
+                   phibc,       &
+                   ixp,         &
+                   jyq,         &
+                   iex,         &
+                   jey,         &
+                   ngrid,       &
+                   nx+2,        &
+                   ny+2,        &
+                   sx,          &
+                   ex,          &
+                   sy,          &
+                   ey,          &
+                   MPI_REAL8,   &
+                   nxprocs,     &
+                   nyprocs,     &
+                   nwork,       &
+                   ibdry,       &
+                   jbdry,       &
+                   myid,        &
+                   IOUT,        &
+                   nerror)
+
 !-----------------------------------------------------------------------
 ! initialize problem
 ! xl,yl are the dimensions of the domain
