dc33a00ea759cb8c15c99f8a7205b0bc46e3ef86
Pierre Navaro <navaro@math.cnrs.fr>
2014-04-03 14:26:51 +0200

    Minor change

--- prototype/src/multigrid/test_multigrid_2d.F90
+++ prototype/src/multigrid/test_multigrid_2d.F90
@@ -33,64 +33,63 @@ use sll_multigrid_2d
 ! Calls     : MPI_INIT, MPI_COMM_RANK, MPI_COMM_SIZE, MPI_CART_CREATE,
 !             MPI_COMM_RANK, MPI_CART_SHIFT, MPI_BARRIER, MPI_SENDRECV,
 !             MPI_CART_GET, MPE_DECOMP1D, 
-!             mgdinit, ginit, mgdsolver, gerr
+!             mgdinit, mgdsolver, gerr
 !-----------------------------------------------------------------------
 !
 ! parameters
 !
-      type(mgd2_solver) :: mgd
+   type(mgd2_solver) :: mgd
 
-      integer nxp2,nyp2,nx,ny,nxprocs,nyprocs,ixp,jyq,iex,jey
-      integer maxcy,kcycle,iprer,ipost,iresw,nwork
-      integer ngrid,nxdim,nydim,IOUT
-      REALN tolmax
-      parameter( nxp2=258, nyp2=258)
-      parameter( nx=nxp2-2, ny=nyp2-2)
-      parameter( nxprocs=4, nyprocs=2)
-      parameter( ixp=4, jyq=4, iex=7, jey=7)
-      parameter( maxcy=300, tolmax=1.0d-05)
-      parameter( kcycle=1, iprer=2, ipost=1, iresw=1)
-      parameter( nwork=(4*nx*ny*8)/(3*nxprocs*nyprocs)  &
-     &                +(64*(nx+ny))/3+(32*4)/3)
-      parameter( ngrid=max(iex,jey))
-      parameter( nxdim=int(float(nxp2-2)/float(nxprocs)+0.99)+2)
-      parameter( nydim=int(float(nyp2-2)/float(nyprocs)+0.99)+2)
-      parameter( IOUT=6)
+   integer nxp2,nyp2,nx,ny,nxprocs,nyprocs,ixp,jyq,iex,jey
+   integer maxcy,kcycle,iprer,ipost,iresw,nwork
+   integer ngrid,nxdim,nydim,IOUT
+   REALN tolmax
+   parameter( nxp2=258, nyp2=258)
+   parameter( nx=nxp2-2, ny=nyp2-2)
+   parameter( nxprocs=4, nyprocs=2)
+   parameter( ixp=4, jyq=4, iex=7, jey=7)
+   parameter( maxcy=300, tolmax=1.0d-05)
+   parameter( kcycle=1, iprer=2, ipost=1, iresw=1)
+   parameter( nwork=(4*nx*ny*8)/(3*nxprocs*nyprocs)  &
+  &                +(64*(nx+ny))/3+(32*4)/3)
+   parameter( ngrid=max(iex,jey))
+   parameter( nxdim=int(float(nxp2-2)/float(nxprocs)+0.99)+2)
+   parameter( nydim=int(float(nyp2-2)/float(nyprocs)+0.99)+2)
+   parameter( IOUT=6)
 !
 ! variables
 !
-      logical periods(2)
-      integer numprocs,myid,comm2d,sx,ex,sy,ey,neighbor(8),bd(8),iter
-      integer realtype,ibdry,jbdry
-      integer nerror,m0,m1,dims(2),coords(2),i
-      integer status(MPI_STATUS_SIZE),nbrright,nbrbottom,nbrleft,nbrtop
-      REALN p(nxdim,nydim),r(nxdim,nydim),f(nxdim,nydim),work(nwork)
-      REALN xl,yl,hxi,hyi,vbc(4),phibc(4,20),wk,rro,pi
+   logical periods(2)
+   integer numprocs,myid,comm2d,sx,ex,sy,ey,neighbor(8),bd(8),iter
+   integer realtype,ibdry,jbdry
+   integer nerror,m0,m1,dims(2),coords(2),i
+   integer status(MPI_STATUS_SIZE),nbrright,nbrbottom,nbrleft,nbrtop
+   REALN p(nxdim,nydim),r(nxdim,nydim),f(nxdim,nydim),work(nwork)
+   REALN xl,yl,hxi,hyi,vbc(4),phibc(4,20),wk,rro,pi
 
 
-      pi=4.0d0*atan(1.0d0)
+   pi=4.0d0*atan(1.0d0)
 
 !-----------------------------------------------------------------------
 ! initialize MPI and create a datatype for real numbers
 !
-      call MPI_INIT(ierr)
-      call MPI_COMM_RANK(MPI_COMM_WORLD,myid,ierr)
-      call MPI_COMM_SIZE(MPI_COMM_WORLD,numprocs,ierr)
-      realtype=MPI_DOUBLE_PRECISION
+   call MPI_INIT(ierr)
+   call MPI_COMM_RANK(MPI_COMM_WORLD,myid,ierr)
+   call MPI_COMM_SIZE(MPI_COMM_WORLD,numprocs,ierr)
+   realtype=MPI_DOUBLE_PRECISION
 
-      if (numprocs.ne.(nxprocs*nyprocs)) then
-        write(IOUT,100)
+   if (numprocs.ne.(nxprocs*nyprocs)) then
+     write(IOUT,100)
 100     format(/,'ERROR: numprocs <> (nxprocs*nyprocs)',/)
-        call mpi_finalize(ierr)
-        stop
-      end if
+     call mpi_finalize(ierr)
+     stop
+   end if
 
 
-      call initialize(mgd, &
-                      0._f64, 1._f64, nx, nxprocs, SLL_PERIODIC, &
-                      0._f64, 1._f64, ny, nyprocs, SLL_PERIODIC)
-     
-
+   call initialize(mgd, &
+                   0._f64, 1._f64, nx, nxprocs, SLL_PERIODIC, &
+                   0._f64, 1._f64, ny, nyprocs, SLL_PERIODIC)
+  
 !-----------------------------------------------------------------------
 ! initialize problem
 ! xl,yl are the dimensions of the domain
@@ -98,37 +97,40 @@ use sll_multigrid_2d
 ! rro is the average density
 ! 1/hxi,1/hyi are the spatial resolutions
 !
-      xl=1.0d0
-      yl=1.0d0
-      wk=2.0d0
-      rro=1.0d0
-      hxi=float(nxp2-2)/xl
-      hyi=float(nyp2-2)/yl
-      write(IOUT,*) 'hxi=',hxi,' hyi=',hyi
-      call ginit(sx,ex,sy,ey,p,r,f,wk,hxi,hyi,pi)
+   xl=1.0d0
+   yl=1.0d0
+   wk=2.0d0
+   rro=1.0d0
+   hxi=float(nxp2-2)/xl
+   hyi=float(nyp2-2)/yl
+   write(IOUT,*) 'hxi=',hxi,' hyi=',hyi
+
+   call ginit(sx,ex,sy,ey,p,r,f,wk,hxi,hyi,pi)
+
 !-----------------------------------------------------------------------
 ! solve using mgd2
 !
-      call mgdsolver(2,sx,ex,sy,ey,p,f,r,ngrid,work, &
-     &               maxcy,tolmax,kcycle,iprer,ipost,iresw, &
-     &               xl,yl,rro,nx,ny,comm2d,myid,neighbor, &
-     &               bd,phibc,iter,.true.,IOUT,nerror)
+   call mgdsolver(2,sx,ex,sy,ey,p,f,r,ngrid,work, &
+  &               maxcy,tolmax,kcycle,iprer,ipost,iresw, &
+  &               xl,yl,rro,nx,ny,comm2d,myid,neighbor, &
+  &               bd,phibc,iter,.true.,IOUT,nerror)
 
-      if (nerror.eq.1) goto 1000
+   if (nerror.eq.1) goto 1000
 !-----------------------------------------------------------------------
 ! compare numerical and exact solutions
 !
-      call gerr(sx,ex,sy,ey,p,comm2d,wk,hxi,hyi,pi,nx,ny,IOUT)
-      call gp_plot2d(myid,numprocs,p,nxdim,nydim,hxi,hyi,sx,sy)
+   call gerr(sx,ex,sy,ey,p,comm2d,wk,hxi,hyi,pi,nx,ny,IOUT)
+   call gp_plot2d(myid,numprocs,p,nxdim,nydim,hxi,hyi,sx,sy)
 !-----------------------------------------------------------------------
-      close(8)
-      call mpi_finalize(nerror)
-      stop
+   close(8)
+   call mpi_finalize(nerror)
+   stop
 1000  write(IOUT,200)
 200   format(/,'ERROR in multigrid code',/)
-      close(8)
-      stop
-      end
+   close(8)
+   stop
+
+end
 
 subroutine ginit(sx,ex,sy,ey,p,r,f,wk,hxi,hyi,pi)
 # include "compdir.inc"
