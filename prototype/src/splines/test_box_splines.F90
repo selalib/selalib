c8493548cc9b327c6a5d0b3694b55e780fb2702f
Laura Mendoza <lasofivec@gmail.com>
2014-04-04 15:18:12 +0200

    WE-commit : Fixed some bugs. to add : correction for quasi-interpolation (cf: MM & Reich) and solve scaling problem

--- prototype/src/splines/test_box_splines.F90
+++ prototype/src/splines/test_box_splines.F90
@@ -21,6 +21,8 @@ sll_real64   :: gauss_sig
 sll_real64,dimension(:),allocatable :: x1
 sll_real64,dimension(:),allocatable :: x2
 sll_real64,dimension(:),allocatable :: f_init
+sll_real64,dimension(:),allocatable :: chi1
+sll_real64,dimension(:),allocatable :: chi2
 ! distribution at time n
 sll_real64,dimension(:),allocatable :: x1_char
 sll_real64,dimension(:),allocatable :: x2_char
@@ -37,9 +39,10 @@ sll_real64   :: t
 
 
 ! Mesh initialization 
- num_cells = 50
+ num_cells = 10
  mesh => new_hex_mesh_2d(num_cells)
  call sll_display(mesh)
+ print *,""
 
 ! Distribution initialization
  gauss_x1  = 0.0
@@ -52,7 +55,8 @@ sll_real64   :: t
  SLL_ALLOCATE(x2(mesh%num_pts_tot),error)
  SLL_ALLOCATE(x1_char(mesh%num_pts_tot),error)
  SLL_ALLOCATE(x2_char(mesh%num_pts_tot),error)
-
+ SLL_ALLOCATE(chi1(mesh%num_pts_tot),error)
+ SLL_ALLOCATE(chi2(mesh%num_pts_tot),error)
 
  do i=0, mesh%num_pts_tot-1
       x1(i+1) = from_global_x1(mesh, i)
@@ -64,8 +68,9 @@ sll_real64   :: t
 
  call write_field_hex_mesh(mesh, f_init, "init_dist.txt")
 
+
 ! Advection initialization
- advec = 0.0!5_f64
+ advec = 0.!1.5_f64
  tmax  = 0.025_f64
  dt    = 0.025_f64
  t = 0._f64
@@ -74,38 +79,61 @@ sll_real64   :: t
  x1_char(:) = x1(:) - advec*dt
  x2_char(:) = x2(:) - advec*dt
 
+
 ! Time loop
  nloops = 0
  spline => new_linear_box_spline_2d(mesh, SLL_NEUMANN, SLL_NEUMANN)
+ diff_error = 0._f64
+
+! print*,"--", from_cart_index_k1(mesh,  0.00_f64,  0.00_f64), from_cart_index_k2(mesh,  0.00_f64,  0.00_f64)
+! print*,"--", from_cart_index_k1(mesh,  0.01_f64,  0.01_f64), from_cart_index_k2(mesh,  0.01_f64,  0.01_f64)
+! print*,"--", from_cart_index_k1(mesh, -0.01_f64,  0.01_f64), from_cart_index_k2(mesh, -0.01_f64,  0.01_f64)
+! print*,"--", from_cart_index_k1(mesh, -0.01_f64, -0.01_f64), from_cart_index_k2(mesh, -0.01_f64, -0.01_f64)
+! print*,"--", from_cart_index_k1(mesh, -0.05_f64,  0.05_f64), from_cart_index_k2(mesh, -0.05_f64,  0.05_f64)
+! print*,"--", from_cart_index_k1(mesh, -0.05_f64, -0.05_f64), from_cart_index_k2(mesh, -0.05_f64, -0.05_f64)
+! print*,"--", from_cart_index_k1(mesh,  0.05_f64,  0.05_f64), from_cart_index_k2(mesh,  0.05_f64,  0.05_f64)
+! print*,"--", from_cart_index_k1(mesh, -0.09_f64,  0.09_f64), from_cart_index_k2(mesh, -0.09_f64,  0.09_f64)
+! print*,"--", from_cart_index_k1(mesh, -0.09_f64, -0.09_f64), from_cart_index_k2(mesh, -0.09_f64, -0.09_f64)
+! print*,"--", from_cart_index_k1(mesh,  0.09_f64,  0.09_f64), from_cart_index_k2(mesh,  0.09_f64,  0.09_f64)
+!
+
+ print*,""
  do while (t .lt. tmax)
       nloops = nloops + 1
-      print*, "t = ", t
       call compute_linear_box_spline_2d( f_tn, spline )
-
       t      = t + dt
       do i=1, mesh%num_pts_tot
+         ! Approximation
          f_tn(i) = hex_interpolate_value(mesh, x1_char(i), x2_char(i), spline)
+         ! Analytical value 
+         x1(i) = from_global_x1(mesh, i-1) - advec*dt*nloops
+         x2(i) = from_global_x2(mesh, i-1) - advec*dt*nloops
+         f_fin(i) = 1.5*exp(-0.5*((x1(i)-gauss_x1)**2 / gauss_sig**2 &
+                                + (x2(i)-gauss_x2)**2 / gauss_sig**2))
+         chi1(i) = chi_gen_val(spline,x1(i), x2(i),1)
+         chi2(i) = chi_gen_val(spline,x1(i), x2(i),2)
+         ! Relative error
+         if (diff_error .lt. abs(f_fin(i) - f_tn(i)) ) then
+            diff_error = abs(f_fin(i) - f_tn(i))
+         end if
+
       end do
+      print*,"   n = ", nloops, "    | error  = ", diff_error
  end do
 
+ print*,""
+ print*, "val at origin of f_app : ", f_tn(1)
+ print*, "val at origin of f_fin : ", f_fin(1)
+ print*, "val at origin of diffn : ", f_fin(1) - f_tn(1)
 
  call write_field_hex_mesh(mesh, f_tn, "final_dist.txt")
-
-! Final exact solution
- diff_error = 0._f64
- do i=0, mesh%num_pts_tot-1
-      x1(i+1) = from_global_x1(mesh, i) - advec*dt*nloops
-      x2(i+1) = from_global_x2(mesh, i) - advec*dt*nloops
-      f_fin(i+1) = 1.5*exp(-0.5*((x1(i+1)-gauss_x1)**2 / gauss_sig**2 &
-                                + (x2(i+1)-gauss_x2)**2 / gauss_sig**2))
-      if (diff_error .lt. abs(f_fin(i+1) - f_tn(i+1))) then
-         diff_error = abs(f_fin(i+1) - f_tn(i+1))/abs(f_fin(i+1))
-      end if
- end do
+ call write_field_hex_mesh(mesh, chi1, "chi1.txt")
+ call write_field_hex_mesh(mesh, chi2, "chi2.txt")
 
 
  call write_field_hex_mesh(mesh, f_fin, "an_dist.txt")
- print*," *  Final error  = ", diff_error, " *"
+ print *,""
+ print*," *    Final error  = ", diff_error, " *"
 
 
 end program test_box_splines
