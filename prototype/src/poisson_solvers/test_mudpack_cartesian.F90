ce686b1f538f50aa1504acf4c1855dd68d26e887
Pierre Navaro <navaro@math.cnrs.fr>
2014-04-09 10:00:48 +0200

    multigrid sequential changes

--- prototype/src/poisson_solvers/test_mudpack_cartesian.F90
+++ prototype/src/poisson_solvers/test_mudpack_cartesian.F90
@@ -1,15 +1,3 @@
-!
-!     a sample program/test driver for mudpack is listed below.  it
-!     can be executed as an initial test.  the output is listed for
-!     the test case described.
-!
-!     test the driver below by solving the separable elliptic pde
-!
-!     pxx + pyy = r(x,y)
-!
-!     on the square [0:2pi]x[0:2pi] with periodic boundary conditions
-!
-
 program test_mudpack_cartesian
 #include "sll_working_precision.h"
 #include "sll_utilities.h"
@@ -23,7 +11,9 @@ implicit none
 
 integer :: nc_eta1
 integer :: nc_eta2
-type(mudpack_2d) :: poisson
+type(mudpack_2d) :: periodic
+type(mudpack_2d) :: dirichlet
+real(8), allocatable :: sol(:,:)
 real(8), allocatable :: phi(:,:)
 real(8), allocatable :: rhs(:,:)
 real(8), allocatable :: eta1(:,:)
@@ -32,10 +22,7 @@ real(8), allocatable :: eta2(:,:)
 real(8) :: eta1_min, eta1_max, eta2_min, eta2_max
 real(8) :: delta_eta1, delta_eta2
 
-integer :: i, j, error, istep
-real, parameter :: mode_1 = 1.0
-real, parameter :: mode_2 = 1.0
-real(8) :: avg, dt = 0.1
+integer :: i, j, error
 
 nc_eta1 = 128
 nc_eta2 = 128
@@ -45,9 +32,9 @@ SLL_CLEAR_ALLOCATE(eta2(1:nc_eta1+1,1:nc_eta2+1),error)
 
 !set end points of solution rectangle in (x,y) space
 eta1_min = 0.0
-eta1_max = 4.*sll_pi
+eta1_max = 4.0
 eta2_min = 0.0
-eta2_max = 4.*sll_pi
+eta2_max = 4.0
 
 !set mesh increments
 delta_eta1 = (eta1_max-eta1_min)/float(nc_eta1)
@@ -61,42 +48,56 @@ do i=1,nc_eta1+1
    end do
 end do
 
-call initialize_mudpack_cartesian(poisson,                     &
-                                  eta1_min, eta1_max, nc_eta1, &
-                                  eta2_min, eta2_max, nc_eta2, &
-                                  SLL_PERIODIC, SLL_PERIODIC,  &
-                                  SLL_PERIODIC, sll_PERIODIC)
-
+SLL_CLEAR_ALLOCATE(sol( 1:nc_eta1+1,1:nc_eta2+1),error)
 SLL_CLEAR_ALLOCATE(phi( 1:nc_eta1+1,1:nc_eta2+1),error)
 SLL_CLEAR_ALLOCATE(rhs( 1:nc_eta1+1,1:nc_eta2+1),error)
 
 
-do istep = 0, 9
+!Poisson periodic
 
-   rhs = cos(mode_1*eta1)*cos(mode_2*eta2)
-   rhs  = -2*mode_1**3 * sin(mode_1*eta1)*cos(mode_2*eta2)*cos(sll_pi*istep*dt)
+call initialize_mudpack_cartesian(periodic,                    &
+                                  eta1_min, eta1_max, nc_eta1, &
+                                  eta2_min, eta2_max, nc_eta2, &
+                                  SLL_PERIODIC, SLL_PERIODIC,  &
+                                  SLL_PERIODIC, SLL_PERIODIC)
 
-   call sll_gnuplot_corect_2d(eta1_min, eta1_max, nc_eta1+1, &
-                              eta2_min, eta2_max, nc_eta2+1, &
-                              rhs, "rhs", istep+1, error)
 
-   
-   call solve_mudpack_cartesian(poisson, phi, rhs)
+sol  = sin(2*sll_pi*eta1)*sin(2*sll_pi*eta2)
+rhs  = -8*sll_pi**2 * sol + 1.
 
-   avg  = sum(phi)/((nc_eta1+1)*(nc_eta2+1))
-   rhs = rhs - phi
-   
-   call sll_gnuplot_corect_2d(eta1_min, eta1_max, nc_eta1+1, &
-                              eta2_min, eta2_max, nc_eta2+1, &
-                              phi, "phi", istep+1, error)
+call solve_mudpack_cartesian(periodic, phi, rhs)
 
-end do
+call sll_gnuplot_corect_2d(eta1_min, eta1_max, nc_eta1+1, &
+                           eta2_min, eta2_max, nc_eta2+1, &
+                           phi, "sinsin", 1, error)
 
 !compute and print maximum norm of error
-!write(*,201) maxval(abs((phi-mode*sin(mode*eta1)*cos(mode*eta2))))
+write(*,201) maxval(abs(phi-sol))
+
+print*,"PASSED"
+     
+call initialize_mudpack_cartesian(dirichlet,                    &
+                                  eta1_min, eta1_max, nc_eta1,  &
+                                  eta2_min, eta2_max, nc_eta2,  &
+                                  SLL_DIRICHLET, SLL_DIRICHLET, &
+                                  SLL_DIRICHLET, SLL_DIRICHLET)
+
+
+sol = sin(2*sll_pi*eta1)*cos(2*sll_pi*eta2)
+rhs = -8*sll_pi**2 * sol
+phi = sol
+
+call solve_mudpack_cartesian(dirichlet, phi, rhs)
+
+call sll_gnuplot_corect_2d(eta1_min, eta1_max, nc_eta1+1, &
+                           eta2_min, eta2_max, nc_eta2+1, &
+                           phi, "sincos", 2, error)
+
+!compute and print maximum norm of error
+write(*,201) maxval(abs(phi-sol))
 
 print*,"PASSED"
 
-!201 format(' maximum error  =  ',e10.3)
+201 format(' maximum error  =  ',e10.3)
      
 end program test_mudpack_cartesian
