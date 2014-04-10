92aed7102e0d36a62a3c35fe952b5951aeb4a2d5
Aurore Back <aurore.back@gmail.com>
2014-04-10 11:09:50 +0200

    add test case

--- python/read_diag_vp4d_qns.py
+++ python/read_diag_vp4d_qns.py
@@ -1,8 +1,7 @@
 import utils_func as ut
 import matplotlib as mp
 import matplotlib.pyplot as mpp
-
-filename = '/Users/back/selalib/prototype/build/vp4D_diag_d00000.h5'
+filename = '/Users/back/selalib/prototype/build/vp4D_diag_d00009.h5'
 f_diag = ut.loadHDF5(filename)
 print "HDF5 file read = " + filename
 
