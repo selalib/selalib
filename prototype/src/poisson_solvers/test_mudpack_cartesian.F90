5debc34631c2526fadf200ecb3b9a718160ed299
Pierre Navaro <navaro@math.cnrs.fr>
2014-04-29 17:45:07 +0200

    fix error in multigrid solver

--- prototype/src/poisson_solvers/test_mudpack_cartesian.F90
+++ prototype/src/poisson_solvers/test_mudpack_cartesian.F90
@@ -24,8 +24,8 @@ real(8) :: delta_eta1, delta_eta2
 
 integer :: i, j, error
 
-nc_eta1 = 128
-nc_eta2 = 128
+nc_eta1 = 64
+nc_eta2 = 64
 
 SLL_CLEAR_ALLOCATE(eta1(1:nc_eta1+1,1:nc_eta2+1),error)
 SLL_CLEAR_ALLOCATE(eta2(1:nc_eta1+1,1:nc_eta2+1),error)
@@ -71,10 +71,10 @@ write(*,201) maxval(abs(phi-sol))
 
 print*,"PASSED"
 
-eta1_min = -1.0
-eta1_max =  1.0
-eta2_min = -1.0
-eta2_max =  1.0
+eta1_min = -5.0
+eta1_max =  5.0
+eta2_min = -5.0
+eta2_max =  5.0
 
 delta_eta1 = (eta1_max-eta1_min)/float(nc_eta1)
 delta_eta2 = (eta2_max-eta2_min)/float(nc_eta2)
@@ -92,19 +92,31 @@ call initialize_mudpack_cartesian(dirichlet,                    &
                                   SLL_DIRICHLET, SLL_DIRICHLET)
 
 
-sol = exp(-(eta1*eta1+eta2*eta2)/0.04_f64)
+sol = exp(-(eta1*eta1+eta2*eta2))
+
 call sll_gnuplot_corect_2d(eta1_min, eta1_max, nc_eta1+1, &
                            eta2_min, eta2_max, nc_eta2+1, &
                            sol, "sol_dirichlet", 1, error)
 
+
 do j=2,nc_eta2
    do i=2,nc_eta1
-      rhs(i,j) = (sol(i-1,j)-2.*sol(i,j)+sol(i+1,j))/(delta_eta1*delta_eta1) + &
-                 (sol(i,j-1)-2.*sol(i,j)+sol(i,j-1))/(delta_eta2*delta_eta2)
+      rhs(i,j) = (sol(i-1,j)-2.*sol(i,j)+sol(i+1,j))/(delta_eta1*delta_eta1) &
+               + (sol(i,j-1)-2.*sol(i,j)+sol(i,j+1))/(delta_eta2*delta_eta2)
    end do
 end do
 
-phi = 0.0_f64
+!rhs = 4 * sol * (eta1*eta1 + eta2*eta2 - 1)
+
+call sll_gnuplot_corect_2d(eta1_min, eta1_max, nc_eta1+1, &
+                           eta2_min, eta2_max, nc_eta2+1, &
+                           rhs, "rhs_dirichlet", 1, error)
+
+!rhs = 4.0_f64
+phi(:,1) = sol(:,1)
+phi(:,nc_eta2+1) = sol(:,nc_eta2+1)
+phi(1,:) = sol(1,:)
+phi(nc_eta2+1,:) = sol(nc_eta2+1,:)
 
 call solve_mudpack_cartesian(dirichlet, phi, rhs)
 
