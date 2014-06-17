1c70f0561903b0655987d0c0f4409e724ff80e3b
Pierre Navaro <navaro@math.cnrs.fr>
2014-06-17 17:48:27 +0200

    Remove f77 files for BlueGene building

--- cmake/turing.cmake
+++ cmake/turing.cmake
@@ -1,3 +1,5 @@
 SET(ZLIB_LIBRARIES "/bglocal/cn/pub/zlib/1.2.5/lib/libz.a" CACHE FILEPATH " " FORCE)
 SET(BLAS_LIBRARIES "/bgsys/ibm_essl/prod/opt/ibmmath/essl/5.1/lib64/libesslbg.a" CACHE FILEPATH " " FORCE) 
 SET(LAPACK_LIBRARIES "/bglocal/cn/pub/LAPACK/3.4.2/lib/liblapacke.a;/bglocal/cn/pub/LAPACK/3.4.2/lib/liblapack.a" CACHE FILEPATH " " FORCE)
+SET(MUDPACK_ENABLED OFF CACHE BOOL " " FORCE)
+SET(FISHPACK_ENABLED OFF CACHE BOOL " " FORCE)
