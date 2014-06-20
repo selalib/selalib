66dae682b0829d97da6658d48965fc083e4653cc
Pierre Navaro <navaro@math.cnrs.fr>
2014-06-20 11:16:37 +0200

    Add config for hpc unistra

--- scripts/hpc-login.sh
+++ scripts/hpc-login.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-source ${HOME}/.bashrc
+source /home/configfiles/bashrc.default
+module purge
+module load batch/slurm
+module load compilers/intel13
+module load mpi/openmpi-1.6.i13.threaded
+module load libs/mkl13
+module load libs/hdf5-1.8
+module list
+source /etc/bash_completion.d/git
+PS1='\h:\w\e[0;36m$(__git_ps1 " (%s)")\e[m\$ '
+export GIT_PS1_SHOWDIRTYSTATE=true
+export GIT_PS1_SHOWUPSTREAM="auto"
 export HDF5_ROOT=/usr/local/hdf5-1.8.5
 export FFTW_ROOT=/usr/local/fftw-3.3.2
 source /opt/intel/composer_xe_2013.2.146/mkl/bin/mklvars.sh intel64
