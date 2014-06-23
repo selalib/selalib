5db6d717e0a5020697f73d83fa8580b49e501c91
Pierre Navaro <navaro@math.cnrs.fr>
2014-06-23 08:46:22 +0200

    Try to fix sim error on idris

--- cmake/turing-nohdf5.cmake
+++ cmake/turing-nohdf5.cmake
@@ -3,5 +3,6 @@ SET(BLAS_LIBRARIES "/bgsys/ibm_essl/prod/opt/ibmmath/essl/5.1/lib64/libesslbg.a"
 SET(LAPACK_LIBRARIES "/bglocal/cn/pub/LAPACK/3.4.2/lib/liblapacke.a;/bglocal/cn/pub/LAPACK/3.4.2/lib/liblapack.a" CACHE FILEPATH " " FORCE)
 SET(MUDPACK_ENABLED       OFF  CACHE BOOL   " " FORCE)
 SET(FISHPACK_ENABLED      OFF  CACHE BOOL   " " FORCE)
-SET(HDF5_PARALLEL_ENABLED ON   CACHE BOOL   " " FORCE)
+SET(HDF5_ENABLED          OFF  CACHE BOOL   " " FORCE)
+SET(HDF5_PARALLEL_ENABLED OFF  CACHE BOOL   " " FORCE)
 SET(Fortran_COMPILER      IBM  CACHE STRING " " FORCE)
