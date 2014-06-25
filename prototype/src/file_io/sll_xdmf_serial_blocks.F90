8932ed9e5cc4eae6f26aab5b6b9e1cee92d47e4e
Pierre Navaro <navaro@math.cnrs.fr>
2014-06-25 18:08:10 +0200

    Change HDF5 io interface
    Some minor changes to fix some errors

--- prototype/src/file_io/sll_xdmf_serial_blocks.F90
+++ prototype/src/file_io/sll_xdmf_serial_blocks.F90
@@ -27,6 +27,7 @@ module sll_xdmf_serial_blocks
 #include "sll_assert.h"
   
   use sll_collective
+  use sll_utilities
 #ifndef NOHDF5
   use hdf5
   use sll_hdf5_io_serial
@@ -43,7 +44,6 @@ module sll_xdmf_serial_blocks
   !> Write and array in an xmf file
   interface sll_xdmf_write_array
      module procedure sll_xdmf_array_2d_serial_blocks
-     module procedure sll_xdmf_array_3d_serial_blocks
   end interface
   
   !> Close the xdmf file
@@ -64,7 +64,7 @@ contains
     sll_int32                    :: prank     !< processor number id
     
     prank = sll_get_collective_rank(sll_world_collective)
-    if (rank == 0) then
+    if (prank == 0) then
        call sll_xml_file_create(trim(file_name),file_id,error)
        write(file_id,'(a)')  &
    "<Grid Name=""AllDomain"" GridType=""Collection"" CollectionType=""Spatial"">"
@@ -75,97 +75,65 @@ contains
   !>Write 2d array in parallel hdf5 file and the matching line in XDMF file
   subroutine sll_xdmf_array_2d_serial_blocks(mesh_name, &
                                array,array_name,error,&
-                               xmffile_id,center)
+                               xmf,center)
 
     character(len=*), intent(in)     :: mesh_name      !< file with mesh coordinates
     sll_real64, intent(in)           :: array(:,:)     !< data array
     character(len=*), intent(in)     :: array_name     !< name of the field
     integer(HID_T)                   :: file_id        !< data file unit number
-    sll_int32                        :: npoints_x1     !< nodes number x
-    sll_int32                        :: npoints_x2     !< nodes number y
-    sll_int32, intent(in), optional  :: xmffile_id     !< xml file unit number
+!    sll_int32                        :: npoints_x1     !< nodes number x
+!    sll_int32                        :: npoints_x2     !< nodes number y
+    sll_int32, intent(in), optional  :: xmf            !< xml file unit number
     character(len=4), optional       :: center         !< "Node" or "Cell"
     sll_int32, intent(out)           :: error          !< error code
     sll_int32                        :: prank
+    sll_int32                        :: psize
+    character(len=4)                 :: crank
+!    sll_int32                        :: iproc
+!    character(len=4)                 :: cproc
+!    character(len=4)                 :: cplot
+
+    prank = sll_get_collective_rank(sll_world_collective)
+    psize = sll_get_collective_size(sll_world_collective)
+    call int2string(prank,crank)
 
 #ifndef NOHDF5
-    call sll_hdf5_file_create(trim(mesh_name)//"-"//trim(array_name)//".h5", &
-                              file_id,error)
+    call sll_hdf5_file_create( &
+       trim(mesh_name)//"-"//trim(array_name)//"-"//crank//".h5", &
+       file_id,error)
     call sll_hdf5_write_array(file_id,array,"/"//trim(array_name),error)
     call sll_hdf5_file_close(file_id, error)
 #endif
 
-    prank = sll_get_collective_rank(sll_world_collective)
-
-    if ( present(xmffile_id) .and. present(center) .and. prank==0) then
-       npoints_x1 = size(array,1)
-       npoints_x2 = size(array,2)
-
-#ifndef NOHDF5
-       call sll_xml_field( &
-            xmffile_id, &
-            trim(array_name), &
-            trim(mesh_name)//"-"//trim(array_name)//".h5:/"//trim(array_name), &
-            npoints_x1,&
-            npoints_x2,&
-            'HDF', &
-            center)
-#else
-       call sll_xml_field(xmffile_id,trim(array_name), &
-                          trim(mesh_name)//"-"//trim(array_name)//".bin", &
-                          npoints_x1,npoints_x2,'Binary',center)
-#endif
-    end if
+!   if (prank == 0) then
+!       do iproc = 0, psize-1
+!          call int2string(iproc,cproc)
+!          write(xmf,'(a)')"<Grid Name=""SubDomain"" GridType=""Uniform"">"
+!          write(xmf,'(a,2i6,a)') &
+!          "<Topology TopologyType=""2DSMesh"" NumberOfElements='",npoints_x2,npoints_x1,"'/>"
+!          write(xmf,'(a)')"<Geometry GeometryType=""X_Y"">"
+!          write(xmf,'(a,2i6,a)')"<DataItem Dimensions='",npoints_x2,npoints_x1, &
+!          "' NumberType=""Float"" Precision=""4"" Format=""HDF"">"
+!          write(xmf,'(a)')mesh_name//cproc//".h5:"//coordnames(1)
+!          write(xmf,'(a)')"</DataItem>"
+!          write(xmf,'(a,2i6,a)')"<DataItem Dimensions='",npoints_x2,npoints_x1, &
+!          "' NumberType=""Float"" Precision=""4"" Format=""HDF"">"
+!          write(xmf,'(a)')mesh_name//cproc//".h5:"//coordnames(2)
+!          write(xmf,'(a)')"</DataItem>"
+!          write(xmf,'(a)')"</Geometry>"
+!          write(xmf,'(a)') &
+!          "<Attribute Name=""Z"" AttributeType=""Scalar"" Center=""Node"">"
+!          write(xmf,'(a,2i6,a)')"<DataItem Dimensions='",npoints_x2,npoints_x1, &
+!          "' NumberType=""Float"" Precision=""8"" Format=""HDF"">"
+!          write(xmf,'(a)')array_name//cproc//"-"//cplot//".h5:/Z"
+!          write(xmf,'(a)')"</DataItem>"
+!          write(xmf,'(a)')"</Attribute>"
+!          write(xmf,'(a)')"</Grid>"
+!       end do
+!    end if
+   
 
   end subroutine sll_xdmf_array_2d_serial_blocks
-
-  !>Write 3d array in binary or hdf5 file and the matching line in XDMF file
-  subroutine sll_xdmf_array_3d_serial_blocks(mesh_name, &
-                               array,array_name,error,xmffile_id,center)
-
-    character(len=*), intent(in)    :: mesh_name      !< file with mesh coordinates
-    sll_real64, intent(in)          :: array(:,:,:)   !< data array
-    character(len=*), intent(in)    :: array_name     !< name of the field
-    integer(HID_T)                  :: file_id        !< data file unit number
-    sll_int32                       :: npoints_x1     !< nodes number x
-    sll_int32                       :: npoints_x2     !< nodes number y
-    sll_int32                       :: npoints_x3     !< nodes number z
-    sll_int32, intent(in), optional :: xmffile_id     !< xml file unit number
-    character(len=4), optional      :: center         !< "Node" or "Cell"
-    sll_int32, intent(out)          :: error          !< error code
-    sll_int32                       :: prank
-    
-#ifndef NOHDF5
-    call sll_hdf5_file_create(trim(mesh_name)//"-"//trim(array_name)//".h5", &
-                             file_id,error)
-    call sll_hdf5_write_array(file_id,array,"/"//trim(array_name),error)
-    call sll_hdf5_file_close(file_id, error)
-#endif
-
-    prank = sll_get_collective_rank(sll_world_collective)
-    if ( present(xmffile_id) .and. present(center) .and. prank==0) then
-       npoints_x1 = size(array,1)
-       npoints_x2 = size(array,2)
-       npoints_x3 = size(array,3)
-
-#ifndef NOHDF5
-       call sll_xml_field( &
-            xmffile_id, &
-            trim(array_name), &
-            trim(mesh_name)//"-"//trim(array_name)//".h5:/"//trim(array_name), &
-            npoints_x1,&
-            npoints_x2,&
-            npoints_x3,&
-            'HDF', &
-            center)
-#else
-       call sll_xml_field(xmffile_id,trim(array_name), &
-                          trim(mesh_name)//"-"//trim(array_name)//".bin", &
-                          npoints_x1,npoints_x2,npoints_x3,'Binary',center)
-#endif
-    end if
-
-  end subroutine sll_xdmf_array_3d_serial_blocks
 
 !> Close the XML file and finish to write last lines.
   subroutine sll_xdmf_close_serial_blocks(file_id,error)
