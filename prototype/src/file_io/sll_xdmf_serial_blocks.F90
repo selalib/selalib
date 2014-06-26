766b3f474f0aac8a55bd05ec0cf0ccf60ef0d702
Pierre Navaro <navaro@math.cnrs.fr>
2014-06-26 15:22:22 +0200

    Add parallel xdmc output with serial hdf5 file
    - take a look in unit_test_io_serial_blocks
    - Perharps needs to be a function of layout object

--- prototype/src/file_io/sll_xdmf_serial_blocks.F90
+++ prototype/src/file_io/sll_xdmf_serial_blocks.F90
@@ -26,7 +26,7 @@ module sll_xdmf_serial_blocks
 #include "sll_working_precision.h"
 #include "sll_assert.h"
   
-  use sll_collective
+  use mpi
   use sll_utilities
 #ifndef NOHDF5
   use hdf5
@@ -54,84 +54,106 @@ module sll_xdmf_serial_blocks
 contains  
   
   !>Open a XDMF format file for a 2d plot
-  subroutine sll_xdmf_open_serial_blocks( file_name,  &
-                                          file_id,    &
+  subroutine sll_xdmf_open_serial_blocks( prefix,  &
+                                          x1,      &
+                                          x2,      &
+                                          xmf,     &
                                           error)
 
-    character(len=*), intent(in) :: file_name !< xmf file name 
-    sll_int32                    :: file_id   !< file unit number
-    sll_int32                    :: error     !< error code
-    sll_int32                    :: prank     !< processor number id
+    character(len=*), intent(in) :: prefix  !< xmf file name 
+    sll_real64, intent(in)       :: x1(:,:) !< x1 coordinates
+    sll_real64, intent(in)       :: x2(:,:) !< x2 coordinates
+    sll_int32, intent(out)       :: xmf     !< unit file number
+    sll_int32                    :: error   !< error code
+    sll_int32                    :: prank   !< processor number id
+    character(len=4)             :: crank
+    integer(HID_T)               :: h5file_id
     
-    prank = sll_get_collective_rank(sll_world_collective)
-    if (prank == 0) then
-       call sll_xml_file_create(trim(file_name),file_id,error)
-       write(file_id,'(a)')  &
-   "<Grid Name=""AllDomain"" GridType=""Collection"" CollectionType=""Spatial"">"
+    call MPI_COMM_RANK(MPI_COMM_WORLD,prank,error)
+    call int2string(prank,crank)
+    if(prank == 0) then
+     call sll_xml_file_create(trim(prefix)//".xmf",xmf,error)
     end if
+#ifndef NOHDF5
+    call sll_hdf5_file_create(trim(prefix)//"-mesh-"//crank//".h5",h5file_id,error)
+    call sll_hdf5_write_array(h5file_id,x1,"/x1",error)
+    call sll_hdf5_write_array(h5file_id,x2,"/x2",error)
+    call sll_hdf5_file_close(h5file_id, error)
+#endif
 
   end subroutine sll_xdmf_open_serial_blocks
 
   !>Write 2d array in parallel hdf5 file and the matching line in XDMF file
-  subroutine sll_xdmf_array_2d_serial_blocks(mesh_name, &
+  subroutine sll_xdmf_array_2d_serial_blocks(xmf, prefix, &
                                array,array_name,error,&
-                               xmf,center)
+                               center)
 
-    character(len=*), intent(in)     :: mesh_name      !< file with mesh coordinates
-    sll_real64, intent(in)           :: array(:,:)     !< data array
-    character(len=*), intent(in)     :: array_name     !< name of the field
-    integer(HID_T)                   :: file_id        !< data file unit number
-!    sll_int32                        :: npoints_x1     !< nodes number x
-!    sll_int32                        :: npoints_x2     !< nodes number y
-    sll_int32, intent(in), optional  :: xmf            !< xml file unit number
-    character(len=4), optional       :: center         !< "Node" or "Cell"
-    sll_int32, intent(out)           :: error          !< error code
-    sll_int32                        :: prank
-    sll_int32                        :: psize
-    character(len=4)                 :: crank
-!    sll_int32                        :: iproc
-!    character(len=4)                 :: cproc
-!    character(len=4)                 :: cplot
+   sll_int32, intent(in)           :: xmf        !< unit file number
+   character(len=*), intent(in)    :: prefix     !< file with mesh coordinates
+   sll_real64, intent(in)          :: array(:,:) !< data array
+   character(len=*), intent(in)    :: array_name !< name of the field
+   integer(HID_T)                  :: file_id    !< data file unit number
+   sll_int32                       :: npts_x1    !< nodes number x
+   sll_int32                       :: npts_x2    !< nodes number y
+   sll_int32, intent(out)          :: error      !< error code
+   character(len=4), optional      :: center     !< "Node" or "Cell"
+   sll_int32                       :: prank
+   sll_int32                       :: psize
+   character(len=4)                :: crank
+   sll_int32                       :: iproc
+   character(len=4)                :: cproc
+   sll_int32, parameter            :: tag=1111
+   sll_int32                       :: statut(MPI_STATUS_SIZE)
 
-    prank = sll_get_collective_rank(sll_world_collective)
-    psize = sll_get_collective_size(sll_world_collective)
-    call int2string(prank,crank)
+   call MPI_COMM_RANK(MPI_COMM_WORLD,prank,error)
+   call MPI_COMM_SIZE(MPI_COMM_WORLD,psize,error)
+   call int2string(prank,crank)
 
 #ifndef NOHDF5
-    call sll_hdf5_file_create( &
-       trim(mesh_name)//"-"//trim(array_name)//"-"//crank//".h5", &
-       file_id,error)
-    call sll_hdf5_write_array(file_id,array,"/"//trim(array_name),error)
-    call sll_hdf5_file_close(file_id, error)
+   call sll_hdf5_file_create(trim(prefix)//"-"//crank//".h5",file_id,error)
+   call sll_hdf5_write_array(file_id,array,"/"//trim(array_name),error)
+   call sll_hdf5_file_close(file_id, error)
 #endif
 
-!   if (prank == 0) then
-!       do iproc = 0, psize-1
-!          call int2string(iproc,cproc)
-!          write(xmf,'(a)')"<Grid Name=""SubDomain"" GridType=""Uniform"">"
-!          write(xmf,'(a,2i6,a)') &
-!          "<Topology TopologyType=""2DSMesh"" NumberOfElements='",npoints_x2,npoints_x1,"'/>"
-!          write(xmf,'(a)')"<Geometry GeometryType=""X_Y"">"
-!          write(xmf,'(a,2i6,a)')"<DataItem Dimensions='",npoints_x2,npoints_x1, &
-!          "' NumberType=""Float"" Precision=""4"" Format=""HDF"">"
-!          write(xmf,'(a)')mesh_name//cproc//".h5:"//coordnames(1)
-!          write(xmf,'(a)')"</DataItem>"
-!          write(xmf,'(a,2i6,a)')"<DataItem Dimensions='",npoints_x2,npoints_x1, &
-!          "' NumberType=""Float"" Precision=""4"" Format=""HDF"">"
-!          write(xmf,'(a)')mesh_name//cproc//".h5:"//coordnames(2)
-!          write(xmf,'(a)')"</DataItem>"
-!          write(xmf,'(a)')"</Geometry>"
-!          write(xmf,'(a)') &
-!          "<Attribute Name=""Z"" AttributeType=""Scalar"" Center=""Node"">"
-!          write(xmf,'(a,2i6,a)')"<DataItem Dimensions='",npoints_x2,npoints_x1, &
-!          "' NumberType=""Float"" Precision=""8"" Format=""HDF"">"
-!          write(xmf,'(a)')array_name//cproc//"-"//cplot//".h5:/Z"
-!          write(xmf,'(a)')"</DataItem>"
-!          write(xmf,'(a)')"</Attribute>"
-!          write(xmf,'(a)')"</Grid>"
-!       end do
-!    end if
-   
+   npts_x1 = size(array,1)
+   npts_x2 = size(array,2)
+
+   if (prank == 0) then
+     write(xmf,'(a)')  &
+   "<Grid Name=""AllDomain"" GridType=""Collection"" CollectionType=""Spatial"">"
+     do iproc = 0, psize-1
+       call int2string(iproc,cproc)
+       if (iproc > 0) then
+          call MPI_RECV(npts_x1,1,MPI_INTEGER,iproc,tag,MPI_COMM_WORLD,statut,error)
+          call MPI_RECV(npts_x2,1,MPI_INTEGER,iproc,tag,MPI_COMM_WORLD,statut,error)
+       end if
+
+       write(xmf,'(a)')"<Grid Name=""SubDomain"" GridType=""Uniform"">"
+       write(xmf,'(a,2i6,a)') &
+         "<Topology TopologyType=""2DSMesh"" NumberOfElements='",npts_x2,npts_x1,"'/>"
+       write(xmf,'(a)')"<Geometry GeometryType=""X_Y"">"
+       write(xmf,'(a,2i6,a)')"<DataItem Dimensions='",npts_x2,npts_x1, &
+         "' NumberType=""Float"" Precision=""4"" Format=""HDF"">"
+       write(xmf,'(a)')trim(prefix)//"-mesh-"//cproc//".h5:/x1"
+       write(xmf,'(a)')"</DataItem>"
+       write(xmf,'(a,2i6,a)')"<DataItem Dimensions='",npts_x2,npts_x1, &
+         "' NumberType=""Float"" Precision=""4"" Format=""HDF"">"
+       write(xmf,'(a)')trim(prefix)//"-mesh-"//cproc//".h5:/x2"
+       write(xmf,'(a)')"</DataItem>"
+       write(xmf,'(a)')"</Geometry>"
+       write(xmf,'(a)') &
+         "<Attribute Name='"//trim(array_name)//"' AttributeType=""Scalar"" Center=""Node"">"
+       write(xmf,'(a,2i6,a)')"<DataItem Dimensions='",npts_x2,npts_x1, &
+         "' NumberType=""Float"" Precision=""8"" Format=""HDF"">"
+       write(xmf,'(a)') trim(prefix)//"-"//cproc//".h5:/"//trim(array_name)
+       write(xmf,'(a)')"</DataItem>"
+       write(xmf,'(a)')"</Attribute>"
+       write(xmf,'(a)')"</Grid>"
+    end do
+  else
+    call MPI_SEND(npts_x1, 1, MPI_INTEGER, 0, tag, MPI_COMM_WORLD, error)
+    call MPI_SEND(npts_x2, 1, MPI_INTEGER, 0, tag, MPI_COMM_WORLD, error)
+  end if
 
   end subroutine sll_xdmf_array_2d_serial_blocks
 
@@ -140,8 +162,12 @@ contains
   sll_int32, intent(in) :: file_id !< file unit number
   sll_int32, intent(out) :: error  !< error code
   sll_int32 :: prank
-  prank = sll_get_collective_rank(sll_world_collective)
+  logical   :: i_opened, i_exist
+  character(len=255) :: i_name
+
+  call MPI_COMM_RANK(MPI_COMM_WORLD,prank,error)
   if (prank==0) then
+     INQUIRE (file_id, OPENED=I_OPENED, NAME=I_NAME, EXIST=I_EXIST) 
      call sll_xml_file_close(file_id,error)
   end if
   end subroutine sll_xdmf_close_serial_blocks
