d39288d165c2904442e55b332ded58203bfe9643
Edwin Chacon-Golcher <edwincg@gmail.com>
2014-07-03 13:11:49 +0200

    fixing some errors in mode debug

--- prototype/src/coordinate_transformations_multipatch/sll_coordinate_transformation_multipatch.F90
+++ prototype/src/coordinate_transformations_multipatch/sll_coordinate_transformation_multipatch.F90
@@ -582,16 +582,16 @@ contains
     
     
    
-    SLL_ASSERT( (patch >= 0) .and. (patch < mp%num_patches) )
-    SLL_ASSERT( (cell_i >= 1) .and. (cell_i < mp%fields(patch+1)%f%mesh%num_cells1) )
-    SLL_ASSERT( (cell_j >= 1) .and. (cell_j < mp%fields(patch+1)%f%mesh%num_cells2) )
+    SLL_ASSERT((patch >=0).and.(patch <mp%number_patches))
+    SLL_ASSERT((cell_i>=1).and.(cell_i<mp%transfs(patch+1)%t%mesh%num_cells1))
+    SLL_ASSERT((cell_j>=1).and.(cell_j<mp%transfs(patch+1)%t%mesh%num_cells2))
     
 
     lm=>mp%get_logical_mesh(patch)
     num_spline_loc_max = (mp%transfs(patch+1)%T%spline_deg1 +1)*&
                          (mp%transfs(patch+1)%T%spline_deg2 +1)
 
-    SLL_ASSERT( (splines_local >= 1) .and. (splines_local < num_spline_loc_max) )
+    SLL_ASSERT((splines_local >= 1).and.(splines_local < num_spline_loc_max))
 
     num_cell = cell_i + (cell_j-1)*lm%num_cells1
 
@@ -687,15 +687,13 @@ contains
     
     lm=>mp%get_logical_mesh(patch)
     
-    SLL_ASSERT( (patch >= 0) .and. (patch < mp%num_patches) )
+    SLL_ASSERT( (patch >= 0)  .and. (patch < mp%number_patches) )
     SLL_ASSERT( (cell_i >= 1) .and. (cell_i < lm%num_cells1) )
     SLL_ASSERT( (cell_j >= 1) .and. (cell_j < lm%num_cells2) )
     
-    
-    
     num_spline_loc_max = (mp%transfs(patch+1)%T%spline_deg1 +1)*&
          (mp%transfs(patch+1)%T%spline_deg2 +1)
-    SLL_ASSERT( (splines_local >= 1) .and. (splines_local < num_spline_loc_max) )
+    SLL_ASSERT((splines_local >= 1) .and. (splines_local < num_spline_loc_max))
     
     num_cell = cell_i + (cell_j-1)*lm%num_cells1
 
