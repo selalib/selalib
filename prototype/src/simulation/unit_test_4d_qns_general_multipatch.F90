940328a24605315a19aabd6e5f4ff10ff7201a34
Laura Mendoza <lasofivec@gmail.com>
2014-07-08 17:08:15 +0200

    First changes for multipatch on 4d qns simulation

--- prototype/src/simulation/unit_test_4d_qns_general_multipatch.F90
+++ prototype/src/simulation/unit_test_4d_qns_general_multipatch.F90
@@ -7,9 +7,10 @@
 
 program qns_4d_general_multipatch
 #include "sll_working_precision.h"
+  use sll_coordinate_transformation_multipatch_module
   use sll_simulation_4d_qns_general_multipatch_module, only: &
      sll_simulation_4d_qns_general_multipatch, &
-     initialize_4d_qns_general_multipatch, &
+     initialize_4d_qns_gen_mp, &
      run_4d_qns_general_mp
   use sll_collective
 !  use sll_constants
@@ -25,13 +26,11 @@ program qns_4d_general_multipatch
 !  use sll_module_scalar_field_2d_multipatch
   implicit none
 
+
   character(len=256) :: filename
   character(len=256) :: filename_local
   type(sll_simulation_4d_qns_general_multipatch)     :: simulation
-  type(sll_logical_mesh_2d), pointer      :: mx ! delete
   type(sll_logical_mesh_2d), pointer      :: mv ! delete
-  class(sll_coordinate_transformation_2d_base), pointer :: transformation_x
-  !class(sll_coordinate_transformation_2d_nurbs), pointer :: transformation_x
   type(sll_coordinate_transformation_multipatch_2d) :: mp
 
   sll_real64, dimension(1:8) :: landau_params
@@ -44,11 +43,11 @@ program qns_4d_general_multipatch
   sll_real64, dimension(1) :: f_epsi_params
   sll_real64, external :: electric_field_ext_1
   sll_real64, external :: electric_field_ext_2
-  
 
 
   print *, 'Booting parallel environment...'
   call sll_boot_collective() ! Wrap this up somewhere else
+
 
   ! In this test, the name of the file to open is provided as a command line
   ! argument.
@@ -77,43 +76,12 @@ program qns_4d_general_multipatch
   ! both...
 
 ! hardwired, this should be consistent with whatever is read from a file
-#define NPTS1 64
-#define NPTS2 64
 #define NPTS3 64
 #define NPTS4 64
-#define SPL_DEG_ETA1 3 
-#define SPL_DEG_ETA2 3
 #define SPL_DEG_VX 3 
 #define SPL_DEG_VY 3
 
-  ! ---------------------------------------------------------------------
-  ! logical mesh for space coordinates
-  ! NPTS1 and NPTS2 is the number of cells
-  ! ---------------------------------------------------------------------
 
-
-  ! mesh for the test case 
-  !  sll_periodic_periodic_gaussian2009_initializer_4d   
-  !  sll_periodic_periodic_gaussian2002_initializer_4d
-  ! sll_landau_initializer_4d
-
-!!$  mx => new_logical_mesh_2d( NPTS1, NPTS2,       & 
-!!$       eta1_min=0.0_f64, eta1_max= 4.0_f64*sll_pi, &
-!!$       eta2_min=0.0_f64, eta2_max= 4.0_f64*sll_pi )
-!!$  
-!!$  ! logical mesh for velocity coordinates
-!!$  mv => new_logical_mesh_2d( NPTS3, NPTS4, &
-!!$       eta1_min=-6.0_f64, eta1_max=6.0_f64, &
-!!$       eta2_min=-6.0_f64, eta2_max=6.0_f64)
-
-
-  ! mesh for the test case 
-  ! sll_gaussian_beam_initializer_4d
-!!$  
-  mx => new_logical_mesh_2d( NPTS1, NPTS2,  & 
-       eta1_min= 0.0_f64, eta1_max= 1.0_f64, &
-       eta2_min= 0.0_f64, eta2_max= 1.0_f64 )
-  
   ! logical mesh for velocity coordinates
   mv => new_logical_mesh_2d( NPTS3, NPTS4, &
        eta1_min=-9.0_f64, eta1_max=9.0_f64, &
@@ -123,52 +91,8 @@ program qns_4d_general_multipatch
   ! coordinate transformation associated with space coordinates
   ! ---------------------------------------------------------------------
 
-  ! identity transformation
+  call mp%read_from_file("identity_mp")
 
-  transformation_x => new_coordinate_transformation_2d_analytic( &
-       "analytic_identity_transformation", &
-       mx, &
-       identity_x1, &
-       identity_x2, &
-       identity_jac11, &
-       identity_jac12, &
-       identity_jac21, &
-       identity_jac22, &
-       (/ 0.0_f64 /) )
-
-  ! colella transformation
-  
-!!$  transformation_x => new_coordinate_transformation_2d_analytic( &
-!!$       "analytic_colela_transformation", &
-!!$       mx, &
-!!$       sinprod_x1, &
-!!$       sinprod_x2, &
-!!$       sinprod_jac11, &
-!!$       sinprod_jac12, &
-!!$       sinprod_jac21, &
-!!$       sinprod_jac22, &
-!!$       (/ 0.1_f64,0.1_f64,4.0_f64*sll_pi,4.0_f64*sll_pi /) )
-
-  ! colella transformation
-  
-  transformation_x => new_coordinate_transformation_2d_analytic( &
-       "analytic_colela_transformation", &
-       mx, &
-       sinprod_gen_x1, &
-       sinprod_gen_x2, &
-       sinprod_gen_jac11, &
-       sinprod_gen_jac12, &
-       sinprod_gen_jac21, &
-       sinprod_gen_jac22, &
-       (/ 0.05_f64,0.05_f64,0.0_f64,1.0_f64,-1.0_f64,1.0_f64,0.0_f64,1.0_f64,-1.0_f64,1.0_f64 /))
-
-
-! transformation_x => new_nurbs_2d_transformation_from_file("circle_n63_rayon1_patch0.nml")
-  !transformation_x => new_nurbs_2d_transformation_from_file("domain_patch0.nml")
-  !transformation_x%mesh => mx
-  mx => transformation_x%mesh !transformation_x%get_logical_mesh()
-  !print*, mx%num_cells1,mx%num_cells2,mx%eta1_min,mx%eta2_min,mx%eta1_max,mx%eta2_max
-!!$   print*, 'transformation ok'
   ! ---------------------------------------------------------------------
   ! define the values of the parameters for the landau initializer
   ! ---------------------------------------------------------------------
@@ -208,11 +132,9 @@ program qns_4d_general_multipatch
   ! ---------------------------------------------------------------------
   ! initialize simulation object with the above parameters
   ! ---------------------------------------------------------------------
-  call initialize_4d_qns_general_multipatch( &
-       simulation, &
-       mx, &
+  call simulation%initialize( &
        mv, &
-       transformation_x, &
+       mp, &
        sll_gaussian_beam_initializer_4d, &
        landau_params, &
        func_one,  &  ! a11
@@ -233,8 +155,6 @@ program qns_4d_general_multipatch
        func_zero, &   !der2 b2
        func_zero, &   ! c
        f_zero_params, &
-       SPL_DEG_ETA1, & 
-       SPL_DEG_ETA2, & 
        SPL_DEG_VX, & 
        SPL_DEG_VY, & 
        ES_GAUSS_LEGENDRE,&
