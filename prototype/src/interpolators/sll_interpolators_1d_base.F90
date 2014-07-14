f4d48f65c7431497f55544972bc6647612f3249f
Jakob Ameres <jakob.ameres@gmail.com>
2014-07-14 14:38:57 +0200

    arbitrary degree spline fix no. 1

--- prototype/src/interpolators/sll_interpolators_1d_base.F90
+++ prototype/src/interpolators/sll_interpolators_1d_base.F90
@@ -1,22 +1,22 @@
 !**************************************************************
 !  Copyright INRIA
-!  Authors :
+!  Authors : 
 !     CALVI project team
-!
-!  This code SeLaLib (for Semi-Lagrangian-Library)
-!  is a parallel library for simulating the plasma turbulence
+!  
+!  This code SeLaLib (for Semi-Lagrangian-Library) 
+!  is a parallel library for simulating the plasma turbulence 
 !  in a tokamak.
-!
-!  This software is governed by the CeCILL-B license
-!  under French law and abiding by the rules of distribution
-!  of free software.  You can  use, modify and redistribute
-!  the software under the terms of the CeCILL-B license as
+!  
+!  This software is governed by the CeCILL-B license 
+!  under French law and abiding by the rules of distribution 
+!  of free software.  You can  use, modify and redistribute 
+!  the software under the terms of the CeCILL-B license as 
 !  circulated by CEA, CNRS and INRIA at the following URL
-!  "http://www.cecill.info".
+!  "http://www.cecill.info". 
 !**************************************************************
 
 !> \brief abstract data type for 1D interpolation and reconstruction
-!>
+!> 
 
 module sll_module_interpolators_1d_base
 #include "sll_working_precision.h"
@@ -30,7 +30,7 @@ module sll_module_interpolators_1d_base
      procedure(interpolator_one_arg_msg), deferred, pass(interpolator) :: &
           interpolate_value
      procedure(interpolator_one_arg_msg), deferred, pass(interpolator) :: &
-          interpolate_derivative_eta1
+          interpolate_derivative_eta1 
      procedure(interpolate_1d_array), pass, deferred :: interpolate_array
      procedure(interpolate_1d_array_at_displacement), pass, deferred :: interpolate_array_disp
      procedure(reconstruct_1d_array), pass, deferred :: reconstruct_array
@@ -57,10 +57,10 @@ module sll_module_interpolators_1d_base
    end type sll_interpolator_1d_base
 
   sll_int32, parameter :: INTERP_PERIODIC_BC  = 0
-  sll_int32, parameter :: INTERP_DIRICHLET_BC = 1
-  sll_int32, parameter :: INTERP_NEUMANN_BC   = 2
+  sll_int32, parameter :: INTERP_DIRICHLET_BC = 1 
+  sll_int32, parameter :: INTERP_NEUMANN_BC   = 2 
 
- ! Signature of the interpolating function
+ ! Signature of the interpolating function  
   abstract interface
      function interpolator_one_arg_msg( interpolator, eta1 ) result(val)
        use sll_working_precision
@@ -125,9 +125,9 @@ module sll_module_interpolators_1d_base
        import sll_interpolator_1d_base
        class(sll_interpolator_1d_base), intent(in)     :: this
        sll_int32, intent(in)  :: num_points    ! size of output array
-       sll_real64, dimension(:), intent(in) :: data  ! data to be interpolated
+       sll_real64, dimension(:), intent(in) :: data  ! data to be interpolated 
        ! points where output is desired
-       sll_real64, dimension(:), intent(in) :: coordinates
+       sll_real64, dimension(:), intent(in) :: coordinates  
        sll_real64, dimension(num_points)    :: res
      end function interpolate_1d_array
   end interface
@@ -144,9 +144,9 @@ module sll_module_interpolators_1d_base
        import sll_interpolator_1d_base
        class(sll_interpolator_1d_base), intent(in)     :: this
        sll_int32, intent(in)  :: num_points    ! size of output array
-       sll_real64, dimension(:), intent(in) :: data  ! data to be interpolated
+       sll_real64, dimension(:), intent(in) :: data  ! data to be interpolated 
        ! points where output is desired
-       sll_real64, intent(in) :: alpha
+       sll_real64, intent(in) :: alpha  
        sll_real64, dimension(num_points)    :: res
      end function interpolate_1d_array_at_displacement
   end interface
@@ -157,7 +157,7 @@ module sll_module_interpolators_1d_base
        import sll_interpolator_1d_base
        class(sll_interpolator_1d_base), intent(in)     :: this
        sll_int32, intent(in)     :: num_points    ! size of output array
-       sll_real64, dimension(:), intent(in)  :: data ! data to be interpolated
+       sll_real64, dimension(:), intent(in)  :: data ! data to be interpolated 
        sll_real64, dimension(num_points)     :: res
      end function reconstruct_1d_array
   end interface
@@ -174,12 +174,12 @@ module sll_module_interpolators_1d_base
   end interface
 
 
-   abstract interface
+   abstract interface 
      function get_coeffs_1d(interpolator)
        use sll_working_precision
        import sll_interpolator_1d_base
        class(sll_interpolator_1d_base), intent(in) :: interpolator
-       sll_real64, dimension(:), pointer         :: get_coeffs_1d
+       sll_real64, dimension(:), pointer         :: get_coeffs_1d     
      end function get_coeffs_1d
   end interface
 
