0f769fbb5ea366faf89ce37d2127d423de42f329
Sever Hirstoaga <sever.hirstoaga@inria.fr>
2014-07-27 21:50:48 +0200

    cubic splines work

--- prototype/src/simulation/unit_test_4d_vp_pic_cartesian.F90
+++ prototype/src/simulation/unit_test_4d_vp_pic_cartesian.F90
@@ -1,20 +1,30 @@
 program pic_4d_cartesian
   use sll_pic_simulation_4d_cartesian_module
-  
+  use sll_collective 
+
   type(sll_pic_simulation_4d_cartesian) :: sim
+
+  integer :: rank, size
+
+  call sll_boot_collective()
+  size = sll_get_collective_size(sll_world_collective)
+  rank = sll_get_collective_rank(sll_world_collective)
 
   call sim%init_from_file('params_pic_4d.nml')
 ! #        OMEGA  = 1.323
 ! #        GAMMA  = -0.151
 
-  print*, sim%ions_number, 'particles',sim%m2d%num_cells1, &
+  if (rank==0) then
+     print*, sim%ions_number, 'particles',sim%m2d%num_cells1, &
        'x',sim%m2d%num_cells2,'cells'
-  print*, sim%ions_number/real(sim%m2d%num_cells1* &
-       sim%m2d%num_cells2,f64), 'particles per cell'
+     print*, sim%ions_number/real(sim%m2d%num_cells1* &
+          sim%m2d%num_cells2,f64), 'particles per cell'
+  endif
 
   call sim%run()
 
-  print*, 'PASSED'
+  if (rank==0) print*, 'PASSED'
+  call sll_halt_collective()
 
   ! call sim%init_from_file("some_name")
   ! call sim%run()
