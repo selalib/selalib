72b0538eff1c3a4f70b90f575402eb8684376069
Laura Mendoza <lasofivec@gmail.com>
2014-08-23 23:36:08 +0200

    Did some changes to the solver: nothing algorithmic, just cleaning the code and some minor optimizations. Added a function for initializing the source knots and made the file sll_knots more selalib-like. There is a still a couple of changes to be done but this is the last of it for CEMRACS

--- prototype/src/general_coordinate_elliptic_solvers/sll_knots.F90
+++ prototype/src/general_coordinate_elliptic_solvers/sll_knots.F90
@@ -1,9 +1,48 @@
 module sll_knots
+#include "sll_working_precision.h"
+
+
   implicit none
 
-  integer, parameter :: KNOTS_PERIODIC = 0, KNOTS_DIRICHLET = 1
+  sll_int32, parameter :: KNOTS_PERIODIC = 0, KNOTS_DIRICHLET = 1
 
 contains
+
+  subroutine initialize_knots_source(&
+       spline_degree, &
+       num_cells, &
+       eta_min, &
+       eta_max, &
+       knots)
+
+    sll_int32,  intent(in)   :: spline_degree 
+    sll_int32,  intent(in)   :: num_cells
+    sll_real64, intent(in)   :: eta_min
+    sll_real64, intent(in)   :: eta_max
+    sll_real64, dimension(:) :: knots
+    sll_int32                :: i
+    sll_real64               :: eta
+    sll_real64               :: delta
+
+    delta = (eta_max - eta_min)/num_cells
+
+    knots(1:spline_degree+1) = eta_min
+    knots(num_cells+2:num_cells+1+spline_degree+1) = eta_max
+    
+    if (mod(spline_degree + 1, 2) == 0) then
+       do i = spline_degree + 2, num_cells + 1
+          knots(i) = eta_min +  &
+               (i - (spline_degree +1)/2 - 1) * delta  
+       end do
+    else
+       do i = spline_degree + 2, num_cells + 1
+          knots(i) = 0.5_f64*(eta_min + (i - (spline_degree)/2 - 1) * delta + &
+               eta_min +  ( i -1 - (spline_degree)/2 -1) * delta )
+       end do
+    end if
+  end subroutine initialize_knots_source
+
+
 
   ! SUBROUTINE ASSEMBLING KNOTS ARRAY
   subroutine initialize_knots(&
@@ -15,16 +54,16 @@ contains
        bc_right, &
        knots)
     
-    integer, intent(in)   :: spline_degree 
-    integer, intent(in)   :: num_cells
-    real(8), intent(in)   :: eta_min
-    real(8), intent(in)   :: eta_max
-    integer, intent(in)   :: bc_left
-    integer, intent(in)   :: bc_right
-    real(8), dimension(:) :: knots
-    integer               :: i, k 
-    real(8)               :: eta
-    real(8)               :: delta
+    sll_int32,  intent(in)   :: spline_degree 
+    sll_int32,  intent(in)   :: num_cells
+    sll_real64, intent(in)   :: eta_min
+    sll_real64, intent(in)   :: eta_max
+    sll_int32,  intent(in)   :: bc_left
+    sll_int32,  intent(in)   :: bc_right
+    sll_real64, dimension(:) :: knots
+    sll_int32                :: i
+    sll_real64               :: eta
+    sll_real64               :: delta
 
     ! This routine needs some error-checking...
     delta = (eta_max - eta_min)/num_cells
@@ -33,8 +72,8 @@ contains
          (bc_right == KNOTS_PERIODIC) ) then 
        ! it is intentional that eta_min is not used, one intends to consider
        ! only the [0,1] interval...
-       do k = -spline_degree, spline_degree+1
-          knots(k+spline_degree+1) = delta*k 
+       do i = -spline_degree, spline_degree+1
+          knots(i+spline_degree+1) = delta*i 
        end do
     else if ( (bc_left  == KNOTS_DIRICHLET) .and. &
               (bc_right == KNOTS_DIRICHLET) ) then 
