0911167674a924ded7901e07076b02a0ae887c9e
Pierre Navaro <navaro@math.cnrs.fr>
2014-09-05 18:05:06 +0200

    Add GMRES test program

---
+++ prototype/documentation/my_library_doc.F90
@@ -0,0 +1,24 @@
+!> @namespace your_sll_library
+!> @brief 
+!> Description of the library 
+!> @details
+!> Long description of the library, you can add some references or math equations.
+!>
+!> <b> Modules available </b>
+!>  List fortran module available
+!>  - sll_mymodule_1
+!>  - sll_mymodule_2
+!>
+!> <b> How to use it </b>
+!> - Header file : \code #include "sll_mylibrary.h" \endcode
+!> - Link with   <code>-lsll_mylibrary</code> 
+!> - Add <code> use my_sll_module </code>
+!>
+!> <b> Examples </b>
+!> -Add some fortran lines to explain how ti use the library
+!> \code
+!> call initialize(my_type, arg_1, arg_2)
+!> call solve(my_type, your_result)
+!> \endcode
+!>
+
