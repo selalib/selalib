0c1741663e7766b571ffe307d4c05f475a5789ff
Pierre Navaro <navaro@math.cnrs.fr>
2014-09-08 11:07:07 +0200

    remove opencl directory

--- python/add_doc_templates.py
+++ python/add_doc_templates.py
@@ -1,6 +1,47 @@
 import os
 
-root = "../prototype/src"
-print os.listdir(root)
-#for dir, subdirs, names in os.walk( root ):
-    #print dir
+root = "../prototype/src/"
+
+def write_doc_file(library):
+     _f = open(root+library+"/%s_doc.F90"%(library), "w")
+     _f.write("! This file is read by doxygen software\n")
+     _f.write("! Change it to match with your library\n")
+     _f.write("! http://www.stack.nl/~dimitri/doxygen/manual/docblocks.html#fortranblocks\n")
+     _f.write("! Type 'make doc' in build directory\n")
+     _f.write("! To check the results, open : \n")
+     _f.write("! selalib/prototype/documentation/build/html/doxygen/html/namespaces.html \n")
+     _f.write("! The following lines will be read by doxygen to generate documentation:\n\n\n")
+     _f.write("!> @namespace %s \n"%(library))
+     _f.write("!> @brief \n")
+     _f.write("!> Description of %s library (72 characters)\n"%(library))
+     _f.write("!> @details\n")
+     _f.write("!> Long description of  %s, you can add some references or math equations.\n"%(library))
+     _f.write("!>\n")
+     _f.write("!> <b> Modules available </b>\n")
+     _f.write("!>  List fortran module available\n")
+     _f.write("!>  - sll_%s_module_1\n"%(library))
+     _f.write("!>  - sll_%s_module_2\n"%(library))
+     _f.write("!>\n")
+     _f.write("!> <b> How to use it </b>\n")
+     _f.write("!> - Header file : \code #include 'sll_%s.h'' \endcode\n"%(library))
+     _f.write("!> - Link with   <code>-lsll_%s</code>\n")
+     _f.write("!> - Add <code> use my_sll_module </code>\n")
+     _f.write("!>\n")
+     _f.write("!> <b> Examples </b>\n")
+     _f.write("!> -Add some fortran lines to explain how ti use the library\n")
+     _f.write("!> \code\n")
+     _f.write("!> call initialize(my_type, arg_1, arg_2)\n")
+     _f.write("!> call solve(my_type, your_result)\n")
+     _f.write("!> \endcode\n")
+     _f.write("!>\n")
+     _f.close()
+
+
+
+#for dir,subdir,name in os.walk(root):
+#    if os.path.join(dir, name):
+#        print dir
+
+for dir in os.walk(root).next()[1]:
+    if (dir != "CMakeModules" and dir != "package")
+    print root+dir
