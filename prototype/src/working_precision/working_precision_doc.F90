634d6281b4dd698193822a904e9e2ebee03c1c4e
Pierre Navaro <navaro@math.cnrs.fr>
2014-09-11 18:00:52 +0200

    Documentation
    - file_io
    - memory
    - working_precision
    
    http://selalib.gforge.inria.fr/doxygen/html/namespaces.html

--- prototype/src/working_precision/working_precision_doc.F90
+++ prototype/src/working_precision/working_precision_doc.F90
@@ -11,28 +11,32 @@
 
 !> @namespace sll_working_precision 
 !> @brief 
-!> Description of working_precision library (72 characters)
-!> @author Selalib team 
-!> You can add a contact, do not put your email to prevent spam.
+!> Define the kind type parameter for intern type data.
+!> @author Edwin Chacon-Golcher
 !> @details
-!> Long description of  working_precision, you can add some references or math equations.
 !>
 !> <b> Headers file available </b>
 !>  - sll_working_precision.h
 !>
-!> <b> Modules available </b>
-!>  List fortran module available
-!>  - sll_working_precision
+!> <b> How to use this module: </b> \n
 !>
-!> <b> How to use it </b>
-!> - Header file : \code #include 'sll_working_precision.h' \endcode
-!> - Link with   <code>-lsll_%s</code>
-!> - Add <code> use sll_working_precision </code>
-!>
-!> <b> Examples </b>
-!> -Add some fortran lines to explain how ti use the library
+!> If you want to select the kind parameter \a n, you need to write \n
 !> \code
-!> call initialize(my_type, arg_1, arg_2)
-!> call solve(my_type, your_result)
+!> real(kind=n) :: var1
+!> real*n       :: var2 
 !> \endcode
+!> The two entries \a var1 and \a var2 are equivalents.
+!> You can also define the constant like this \a 23.455_n.
 !>
+!> First, call the module \a sll_woring_precision like that
+!> \code #include "sll_working_precision.h" \endcode
+!> Now, you can use the types :
+!> \code
+!> _sll_int32  :: i        !integer simple precision
+!> _sll_int64  :: N        !integer double precision
+!> _sll_real32 :: theta    !real simple precision
+!> _sll_real64 :: my_pi    !real double precision
+!>
+!> my_pi = 3.1415926535897932384626433_f64
+!> theta = 2.0*real(N,f32)
+!> \endcode
