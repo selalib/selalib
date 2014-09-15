6c0d6b6d5855af01ddf255a41c6d688f4c1319f4
Charles Prouveur <prouveur@math.univ-lyon1.fr>
2014-09-15 15:49:24 +0200

    corrected a small bug

--- prototype/src/poisson_solvers/pivotbande.F90
+++ prototype/src/poisson_solvers/pivotbande.F90
@@ -5,58 +5,13 @@ module pivotbande
   implicit none
 contains
 
-  subroutine pib(a,x,b,n,l1,l2)
-    implicit none
-    sll_real64,dimension(:,:),intent(in)::a
-    sll_real64,dimension(:),  intent(in)::b
-    sll_real64,dimension(:), intent(out)::x
-    sll_real64,dimension(n,n)           ::aa
-    sll_real64,dimension(n)             ::bb
-    sll_int32,intent(in)                ::n,l1,l2
-    sll_int32                           ::i,j,k,ll1,ll2
-    sll_real64                          ::p,s
-
-    aa = a
-    bb = b
-
-    do k = 1,n-1
-       ll1 = k + l1;
-       if (ll1>n)  ll1 = n 
-       do i = k+1,ll1  
-          p = aa(i,k)/aa(k,k)             
-          ll2 = k + l2
-          if (ll2>n) ll2=n 
-          do j = k+1,ll2 
-             aa(i,j) = aa(i,j) - aa(k,j)*p 
-          enddo
-          bb(i) = bb(i) - bb(k) * p 
-       enddo
-       aa(k+1:n,k)=0._f64
-    enddo
-
-    !remontée
-
-    x(n) = bb(n)/aa(n,n)
-
-    do i=n-1,1,-1
-       s=0.
-       ll2 = i + l2
-       if (ll2>n) ll2 = n
-       do j = (i+1),ll2 
-          s = s + aa(i,j)*x(j)
-       enddo
-       x(i) = (bb(i)-s)/aa(i,i)
-    enddo
-
-  endsubroutine pib
-
-
   subroutine searchband(a,n,l1,l2)
     implicit none
     sll_real64,dimension(:,:)::a
+    sll_int32,intent(in )    ::n
     sll_int32,intent(out)    ::l1,l2
     sll_int32,dimension(n)   ::b1,b2
-    sll_int32::i,j,n,ll1,ll2!,width
+    sll_int32::i,j,ll1,ll2!,width
 
     l1=0
     l2=0
@@ -86,95 +41,7 @@ contains
 
   endsubroutine searchband
 
-
   !***********************************************
-
-
-  subroutine solvaxbameliore(a,x,b,n)
-    implicit none
-    sll_real64,dimension(:,:) ,intent(in)::a
-    sll_real64,dimension(:)   ,intent(in)::b
-    sll_real64,dimension(:),intent(inout)::x
-    sll_int32,                 intent(in)::n
-    sll_real64,dimension(n,n)            ::c
-    sll_real64,dimension(n)              ::d,e,f
-    sll_int32 ::i,j,k,l,m,p,q
-    sll_real64::s,u,v, piv
-
-    ! on résout ax=b en créant un système triangulaire équivalent
-    !méthode du pivot de gauss
-    c=a
-    d=b
-
-    do k=1,n-1
-       q=k+1
-
-       if (abs(c(k,k))<1e-12) then
-          ! trouver le cik le + grand en val abs
-          print*,"oho"
-          do i=k+1,q
-             p=i
-             do j=i,p
-                if (p==n) then
-                   e=c(k,:)
-                   f=c(i,:)
-                   u=d(k)
-                   v=d(i)
-                   ! chgt de lignes
-                   c(k,:)=f
-                   c(i,:)=e
-                   d(k)=v
-                   d(i)=u
-                else
-                   if ((c(i,k)**2)>=(c(j,k)**2)) then
-                      p=p+1
-                   else 
-                      q=q+1 
-                   endif
-                endif
-             enddo
-          enddo
-
-       else
-
-          do i=k+1,n
-
-             !if (c(i,k)/=0) then
-
-             piv = c(i,k)/c(k,k)
-             d(i)=d(i)-d(k)*piv
-             do j=n,k,-1
-                if (c(k,j)/=0) then 
-                   c(i,j)=c(i,j)-c(k,j)*piv
-                endif
-             enddo
-
-             !endif
-
-          enddo
-
-       end if
-
-    enddo
-
-
-    ! remontée
-    x(n)=d(n)/c(n,n)
-
-    do i=n-1,1,-1
-       s=0.
-       do j=(i+1),n
-          s=s+c(i,j)*x(j)
-       enddo
-       x(i)=(d(i)-s)/c(i,i)
-    enddo
-
-  endsubroutine solvaxbameliore
-
-
-
-  !***********************************************
-
 
   subroutine factolub (a,l,u,n,l1,l2)
     implicit none
@@ -183,32 +50,50 @@ contains
     sll_int32,intent(in)::n,l1,l2
     sll_int32::i,j,k,ll1,ll2
 
-    ! init
+    ! !init 
+
+    ! u = a
+    ! l = 0._f64
+    ! do i = 1,n
+    !    l(i,i) = 1._f64
+    ! enddo
+
+    ! !transformation
+
+    ! do k = 1,n-1
+    !    ll1 = k + l1
+    !    if (ll1>n) ll1=n 
+    !    do i = k + 1, ll1
+    !       l(i,k) = u(i,k)/u(k,k)             
+    !       ll2 = k+l2
+    !       if (ll2>n) ll2 = n 
+    !       do j = k,ll2
+    !          u(i,j) = u(i,j)-l(i,k)*u(k,j)
+    !       enddo
+    !    enddo
+    ! enddo
+
+    ! version bande
+
     u = a
     l = 0._f64
     do i = 1,n
-       l(i,i) = 1._f64
+       l(i,l1+1) = 1._f64
     enddo
 
-    ! transformation
+    ll1 = l1 + 1
+    ll2 = l1 + 2
 
     do k = 1,n-1
-       ll1 = k + l1
-       if (ll1>n) ll1=n 
-       do i = k + 1, ll1
-          l(i,k) = u(i,k)/u(k,k)             
-          ll2 = k+l2
-          if (ll2>n) ll2 = n 
-          do j = k,ll2
-             u(i,j) = u(i,j)-l(i,k)*u(k,j)
+       do i = 1,l1
+          if (k+ll1-i <= n) l(k,i) = u(k+ll1-i,i)/u(k,ll1)
+          do j = ll1,ll1+l2
+             if (i+k <= n)  u(i+k,j-i) = u(i+k,j-i)-l(k,ll1-i)*u(k,j)
           enddo
        enddo
     enddo
 
   end subroutine factolub
-
-
-
 
   !***********************************************
 
@@ -222,6 +107,30 @@ contains
     sll_int32                            ::i,j,ll1,ll2
     sll_real64                           ::s
 
+    ! x(1) = b(1)/l(1,1)
+
+    ! do i = 2,n
+    !    s = 0._f64
+    !    ll1 = i-l1  
+    !    if (ll1<1) ll1 = 1
+    !    do j = ll1,i-1 
+    !       s = s + l(i,j)*x(j)
+    !    enddo
+    !    x(i) = (b(i)-s)/l(i,i)
+    ! enddo
+
+    ! x(n) = x(n)/u(n,n)
+    ! do i = n-1,1,-1
+    !    s = 0._f64
+    !    ll2 = i+l2
+    !    if (ll2>n) ll2 = n
+    !    do j = (i+1),ll2 
+    !       s = s + u(i,j)*x(j)
+    !    enddo
+    !    x(i) = (x(i)-s)/u(i,i)
+    ! enddo
+
+    !version bande
     x(1) = b(1)/l(1,1)
 
     do i = 2,n
