b0c429325f695aa79172059a74037a169a1679d9
Pierre Navaro <navaro@math.cnrs.fr>
2014-09-18 11:05:37 +0200

    Add utilities documentation

--- prototype/src/utilities/tridiagonal.F90
+++ prototype/src/utilities/tridiagonal.F90
@@ -15,59 +15,12 @@
 !  "http://www.cecill.info". 
 !**************************************************************
 
-!------------------------------------------------------------------------------
-! SELALIB
-!------------------------------------------------------------------------------
-! MODULE: sll_tridiagonal
-!
-! DESCRIPTION:
-!> @file tridiagonal.F90
-!> @author Module Author Name and Affiliation
-!> @brief Tridiagonal system solver.
-!> @details To solve systems of the form Ax=b, where A is a tridiagonal matrix, Selalib 
-!! offers a native, robust tridiagonal system solver. The present implementation 
-!! contains only a serial version.
-!! The algorith is based on an LU factorisation of a given matrix,
-!! with row pivoting. The tridiagonal matrix must be given as a single array,
-!! with a memory layout shown next.
-!> \f[ A = \begin{bmatrix}
-!! a(2) & a(3) & & & & & a(1)
-!! \\ a(4) & a(5) & a(6) & & & &
-!! \\ & a(7) & a(8) & a(9) & & &
-!! \\ & & \ddots & \ddots & \ddots & &
-!! \\ & & & \ddots & \ddots & \ddots &
-!! \\ & & & & a(3n-5) & a(3n-4)&a(3n-3)
-!! \\ a(3n)& & & & & a(3n-2) & a(3n-1)
-!! \end{bmatrix} \f]
-!>
-!> Usage:
-!>
-!> To solve a tridiagonal system, first: \n
-!> -# Assemble the matrix 'a' as a single array with the layout just 
-!>    described above
-!> -# Use 'setup_cyclic_tridiag( a, n, cts, ipiv )' to factorize the system
-!>    -# In 'setup_cyclic_tridag', a is the array to be factorized, stored 
-!>        with the layout shown above. 'n' is essentially the problem size.
-!>        cts and ipiv are respectively real and integer arrays of size 7*n 
-!>        and n that are needed to return factorization information. ipiv
-!>        is the usual 'pivot' array.
-!> -# To solve the system, make a call to 
-!>         'solve_cyclic_tridiag(cts, ipiv, b, n, x)'
-!>    Here, cts and ipiv are the ones returned by setup_cyclic_tridiag. The
-!>    function returns the solution to Ax = b, storing the results in 'x'.
-!>    In case that an 'in-place' computation is desired, it is acceptable to
-!>    make the call like: 
-!>          solve_cyclic_tridiag(cts, ipiv, b, n, b)
-!>
-!
-! REVISION HISTORY:
-! DD Mmm YYYY - Initial Version
-! TODO_dd_mmm_yyyy - TODO_describe_appropriate_changes - TODO_name
-!------------------------------------------------------------------------------ 
+!> Tridiagonal system solver.
 module sll_tridiagonal
 #include "sll_working_precision.h"
 implicit none
 
+  !> Solve tridiagonal system (double or complex)
   interface solve_cyclic_tridiag
      module procedure solve_cyclic_tridiag_double, solve_cyclic_tridiag_complex
   end interface
--- prototype/src/utilities/utilities_doc.F90
+++ prototype/src/utilities/utilities_doc.F90
@@ -11,28 +11,63 @@
 
 !> @namespace sll_utilities 
 !> @brief 
-!> Description of utilities library (72 characters)
-!> @author Selalib team 
-!> You can add a contact, do not put your email to prevent spam.
+!> Some numerical low level utilities
 !> @details
-!> Long description of  utilities, you can add some references or math equations.
+!> - Cyclic tridiagonal system solver
+!> - Pentadiagonal system solver
+!> - Skyline format matrix solver
+!> - Display functions for matrix and vector
+!> - integer to string function
+!> @author Selalib team 
 !>
 !> <b> Headers file available </b>
 !>  - sll_utilities.h
 !>
 !> <b> Modules available </b>
-!>  List fortran module available
 !>  - sll_utilities
+!>  - sll_tridiagonal
+!>  - sll_toep_penta_diagonal
+!>  - skyline
 !>
 !> <b> How to use it </b>
 !> - Header file : \code #include 'sll_utilities.h' \endcode
-!> - Link with   <code>-lsll_%s</code>
+!> - Link with   <code>-lsll_utilities</code>
 !> - Add <code> use sll_utilities </code>
 !>
-!> <b> Examples </b>
-!> -Add some fortran lines to explain how ti use the library
-!> \code
-!> call initialize(my_type, arg_1, arg_2)
-!> call solve(my_type, your_result)
-!> \endcode
+!> <b> Tridiagonal system solver </b>
+!> 
+!> To solve systems of the form Ax=b, where A is a tridiagonal matrix, Selalib 
+!! offers a native, robust tridiagonal system solver. The present implementation 
+!! contains only a serial version.
+!! The algorith is based on an LU factorisation of a given matrix,
+!! with row pivoting. The tridiagonal matrix must be given as a single array,
+!! with a memory layout shown next.
+!> \f[ A = \begin{bmatrix}
+!! a(2) & a(3) & & & & & a(1)
+!! \\ a(4) & a(5) & a(6) & & & &
+!! \\ & a(7) & a(8) & a(9) & & &
+!! \\ & & \ddots & \ddots & \ddots & &
+!! \\ & & & \ddots & \ddots & \ddots &
+!! \\ & & & & a(3n-5) & a(3n-4)&a(3n-3)
+!! \\ a(3n)& & & & & a(3n-2) & a(3n-1)
+!! \end{bmatrix} \f]
+!>
+!> Usage:
+!>
+!> To solve a tridiagonal system, first: \n
+!> -# Assemble the matrix 'a' as a single array with the layout just 
+!>    described above
+!> -# Use 'setup_cyclic_tridiag( a, n, cts, ipiv )' to factorize the system
+!>    -# In 'setup_cyclic_tridag', a is the array to be factorized, stored 
+!>        with the layout shown above. 'n' is essentially the problem size.
+!>        cts and ipiv are respectively real and integer arrays of size 7*n 
+!>        and n that are needed to return factorization information. ipiv
+!>        is the usual 'pivot' array.
+!> -# To solve the system, make a call to 
+!>         'solve_cyclic_tridiag(cts, ipiv, b, n, x)'
+!>    Here, cts and ipiv are the ones returned by setup_cyclic_tridiag. The
+!>    function returns the solution to Ax = b, storing the results in 'x'.
+!>    In case that an 'in-place' computation is desired, it is acceptable to
+!>    make the call like: 
+!>          solve_cyclic_tridiag(cts, ipiv, b, n, b)
 !>
