342f478185d4570ba27abbf3456c330f66893df6
Pierre Navaro <navaro@math.cnrs.fr>
2014-09-26 17:56:49 +0200

    Add LLrun config
    not finished

---
+++ prototype/src/CMakeModules/LLRUNConfig.cmake
@@ -0,0 +1,41 @@
+FIND_PROGRAM(LLRUN_EXECUTABLE
+  NAMES llrun
+  DOC "LoadLeveler tool to launch parallel jobs.")
+
+IF (LLRUN_EXECUTABLE)
+  SET (LLRUN_FOUND "YES")
+ENDIF (LLRUN_EXECUTABLE)
+
+MARK_AS_ADVANCED( LLRUN_FOUND LLRUN_EXECUTABLE)
+
+IF(LLRUN_FOUND)
+
+   SET(MPIEXEC ${LLRUN_EXECUTABLE}) 
+   SET(MPIEXEC_PREFLAGS "-k class=clallmds job_name=selalib total_tasks=16 node=2 node_usage=not_shared wall_clock_limit=00:10:00 job_type=mpich environment=COPY_ALL; mpirun " 
+
+   SET(MPIEXEC_NUMPROC_FLAG "-procs") 
+   MACRO(ADD_MPI_TEST TEST_NAME EXEC_NAME PROCS ARGS)
+      ADD_TEST(NAME ${TEST_NAME}
+               COMMAND ${MPIEXEC} 
+                       ${MPIEXEC_PREFLAGS}
+	               ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${EXEC_NAME}
+	               ${MPIEXEC_POSTFLAGS} ${ARGS}
+                       ${MPIEXEC_NUMPROC_FLAG} ${PROCS})
+   ENDMACRO(ADD_MPI_TEST)
+
+ELSE(LLRUN_FOUND)
+
+   MACRO(ADD_MPI_TEST TEST_NAME EXEC_NAME PROCS ARGS)
+      IF(NOT APPLE)
+         STRING(REGEX REPLACE "mpiexec" "mpirun" MPIEXEC ${MPIEXEC})
+      ENDIF()
+      ADD_TEST(NAME ${TEST_NAME}
+               COMMAND ${MPIEXEC} 
+	               ${MPIEXEC_NUMPROC_FLAG} ${PROCS} ${MPIEXEC_PREFLAGS}
+	               ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${EXEC_NAME}
+	               ${MPIEXEC_POSTFLAGS} ${ARGS})
+   ENDMACRO(ADD_MPI_TEST)
+
+ENDIF(LLRUN_FOUND)
+
+
