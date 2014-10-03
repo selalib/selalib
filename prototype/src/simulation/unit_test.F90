a71d8981d6b9fc804d594a4cbfcc85f3ba237145
Pierre Navaro <navaro@math.cnrs.fr>
2014-10-03 17:33:03 +0200

    Change finite_difference interpolator names

--- prototype/src/simulation/unit_test.F90
+++ prototype/src/simulation/unit_test.F90
@@ -13,7 +13,7 @@ program unit_test
   use sll_module_interpolators_1d_base
   use sll_module_cubic_spline_interpolator_1d
   use sll_advection_field
-  use sll_finite_difference_interpolator_1d
+  use sll_module_finite_difference_interpolator_1d
   implicit none
 
   sll_int32 :: nc_eta1, nc_eta2
@@ -25,8 +25,8 @@ program unit_test
   type(init_landau_2d), target                        :: init_landau
   class(scalar_field_2d_initializer_base), pointer    :: p_init_f
   type(cubic_spline_1d_interpolator)                  :: spline_interp
-  type(finite_difference_1d_interpolator)             :: fd_interp1
-  type(finite_difference_1d_interpolator)             :: fd_interp2
+  type(sll_finite_difference_interpolator_1d)             :: fd_interp1
+  type(sll_finite_difference_interpolator_1d)             :: fd_interp2
   type(hamiltonian_advection_field_2d)                :: hamiltonian
   sll_int32  :: ierr, istep
   sll_int32  :: ieta1, ieta2, nnode_x1, nnode_v1
