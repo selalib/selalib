4c9302a6d3cb8def08375d48b618ae7ad37267f2
Pierre Navaro <navaro@math.cnrs.fr>
2014-10-05 09:44:55 +0200

    Reorganize documentation

--- prototype/src/CMakeModules/DoxygenConfig.cmake
+++ prototype/src/CMakeModules/DoxygenConfig.cmake
@@ -3,10 +3,10 @@
 find_package(Doxygen)
 if(DOXYGEN_FOUND)
    get_filename_component(DOXYGEN_OUTPUT_DIR ${CMAKE_CURRENT_SOURCE_DIR} PATH)
-   set(DOXYGEN_OUTPUT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../documentation/build/html/doxygen)
+   set(DOXYGEN_OUTPUT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../doc/build/html/doxygen)
    file(MAKE_DIRECTORY ${DOXYGEN_OUTPUT_DIR})
    message(STATUS "The documentation is in ${DOXYGEN_OUTPUT_DIR}")
-   configure_file(${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in 
+   configure_file(${CMAKE_CURRENT_SOURCE_DIR}/../doc/doxygen/Doxyfile.in 
    ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
    add_custom_target(doc 
    COMMAND ${DOXYGEN_EXECUTABLE} -u ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
