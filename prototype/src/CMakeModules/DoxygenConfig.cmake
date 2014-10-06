c9bcda8a8cf6e4b66be585e76f87abd48bf4506f
Pierre Navaro <navaro@math.cnrs.fr>
2014-10-06 12:00:31 +0200

    Major update of oxygen doc
    see result on http://selalib.gforge.inria.fr/doxygen/html/index.html

--- prototype/src/CMakeModules/DoxygenConfig.cmake
+++ prototype/src/CMakeModules/DoxygenConfig.cmake
@@ -6,7 +6,7 @@ if(DOXYGEN_FOUND)
    set(DOXYGEN_OUTPUT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../doc/build/html/doxygen)
    file(MAKE_DIRECTORY ${DOXYGEN_OUTPUT_DIR})
    message(STATUS "The documentation is in ${DOXYGEN_OUTPUT_DIR}")
-   configure_file(${CMAKE_CURRENT_SOURCE_DIR}/../doc/doxygen/Doxyfile.in 
+   configure_file(${CMAKE_CURRENT_SOURCE_DIR}/../doc/doxygen/Doxyfile
    ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
    add_custom_target(doc 
    COMMAND ${DOXYGEN_EXECUTABLE} -u ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
