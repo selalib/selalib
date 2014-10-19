5f1cb1630eadbeeec93b8957dc78fb4359331c9d
Pierre Navaro <navaro@math.cnrs.fr>
2014-10-19 14:39:34 +0200

    Improve documentation in poisson solvers
    - Add fft implementation
    - Add mudpack implementation
    - Add polar implementation

--- prototype/src/poisson_solvers/sll_poisson_1d_base.F90
+++ prototype/src/poisson_solvers/sll_poisson_1d_base.F90
@@ -1,6 +1,6 @@
 !> @ingroup poisson_solvers
 !> @brief
-!> Parent class for Poisson solvers in 1 dimension
+!> Module interface to solve Poisson equation in 1D.
 !> @details
 !> This decribes two methods
 !>   - compute_phi_from_rho
@@ -12,9 +12,11 @@ module sll_module_poisson_1d_base
   type, abstract :: sll_poisson_1d_base 
   contains
     !> Solve Poisson equation and compute electric potential from charge density, 
+    !> \f[ -\Delta \phi_i = \rho_i \f]
     procedure(signature_compute_phi_from_rho_1d), deferred, pass(poisson) :: &
       compute_phi_from_rho
     !> Solve Poisson equation and compute electric field from charge density
+    !> \f[ E_i = -\nabla \phi_i \f]  with \f[ -\Delta \phi_i = \rho_i \f].
     procedure(signature_compute_E_from_rho_1d), deferred, pass(poisson) :: &
       compute_E_from_rho
   end type sll_poisson_1d_base
@@ -23,7 +25,6 @@ module sll_module_poisson_1d_base
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 
   abstract interface
-    !> solves \f[ -\Delta phi = rho \f] in 1d 
     subroutine signature_compute_phi_from_rho_1d( poisson, phi, rho )
       use sll_working_precision
       import sll_poisson_1d_base      
@@ -34,7 +35,6 @@ module sll_module_poisson_1d_base
   end interface
 
   abstract interface    
-    !> solves E = -\nabla Phi with -\Delta phi = rho in 1d 
     subroutine signature_compute_E_from_rho_1d( poisson, E, rho )
       use sll_working_precision
       import sll_poisson_1d_base       
