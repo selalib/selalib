7f428eb007b1ece3ba2548c65eff9631c1a1f1ab
Pierre Navaro <navaro@math.cnrs.fr>
2014-12-13 20:51:37 +0100

    Try to fix the arbitrary interpolator problem
    - recall two instances are not possible

--- src/simulations/simulations_parallel/bsl_examples/advection_1d1d_arbitrary_degree_splines.F90
+++ src/simulations/simulations_parallel/bsl_examples/advection_1d1d_arbitrary_degree_splines.F90
@@ -70,10 +70,8 @@ if (.not. is_power_of_two(psize)) then
 end if
 
 call spl_eta1%initialize(nc_eta1+1, eta1_min, eta1_max, &
-                         SLL_PERIODIC, SLL_PERIODIC, 2)
+                         SLL_PERIODIC, SLL_PERIODIC, 5)
 
-call spl_eta2%initialize(nc_eta2+1, eta2_min, eta2_max, &
-                         SLL_PERIODIC, SLL_PERIODIC, 2)
 
 interp_eta1 => spl_eta1
 interp_eta2 => spl_eta2
@@ -119,27 +117,44 @@ end do
 end do
 
 call apply_remap_2D( eta2_to_eta1, f_eta2, f_eta1 )
-call advection_eta1(0.5*delta_t)
-
 offset = local_to_global(layout_eta1,(/1,1/)) 
 offset_eta1 = (offset(1)-1)*delta_eta1
 offset_eta2 = (offset(2)-1)*delta_eta2
 
 do i_step=1, n_step
 
-  call apply_remap_2D( eta1_to_eta2, f_eta1, f_eta2 )
-
-  !call advection_eta2(delta_t)
-
-  call apply_remap_2D( eta2_to_eta1, f_eta2, f_eta1 )
-
   call advection_eta1(delta_t)
 
   call sll_gnuplot_2d_parallel( offset_eta1, delta_eta1, &
-                                  offset_eta2, delta_eta2, &
-                                  size(f_eta1,1), size(f_eta1,2), &
-                                  f_eta1, 'f_parallel', &
-                                  i_step, error)
+                                offset_eta2, delta_eta2, &
+                                size(f_eta1,1), size(f_eta1,2), &
+                                f_eta1, 'f_parallel', &
+                                i_step, error)
+
+  if (prank == MPI_MASTER) &
+     write(*,"(10x,' time = ', G15.3, ' sec' )") i_step*delta_t
+
+end do
+
+call apply_remap_2D( eta1_to_eta2, f_eta1, f_eta2 )
+offset = local_to_global(layout_eta2,(/1,1/)) 
+offset_eta1 = (offset(1)-1)*delta_eta1
+offset_eta2 = (offset(2)-1)*delta_eta2
+
+call spl_eta1%delete()
+call spl_eta2%initialize(nc_eta2+1, eta2_min, eta2_max, &
+                         SLL_PERIODIC, SLL_PERIODIC, 2)
+
+do i_step=n_step+1, 2*n_step
+
+  call advection_eta2(delta_t)
+
+  call sll_gnuplot_2d_parallel( offset_eta1, delta_eta1, &
+                                offset_eta2, delta_eta2, &
+                                size(f_eta2,1), size(f_eta2,2), &
+                                f_eta2, 'f_parallel', &
+                                i_step, error)
+
   if (prank == MPI_MASTER) &
      write(*,"(10x,' time = ', G15.3, ' sec' )") i_step*delta_t
 
