e03950994945abc6028dba2caca97896accac338
Pierre Navaro <navaro@math.cnrs.fr>
2014-12-14 13:40:21 +0100

    Fix error in advection test
    - The error was not in arbitrary module but in the test :-)

--- src/simulations/simulations_parallel/bsl_examples/advection_1d1d_arbitrary_degree_splines.F90
+++ src/simulations/simulations_parallel/bsl_examples/advection_1d1d_arbitrary_degree_splines.F90
@@ -71,6 +71,8 @@ end if
 
 call spl_eta1%initialize(nc_eta1+1, eta1_min, eta1_max, &
                          SLL_PERIODIC, SLL_PERIODIC, 5)
+call spl_eta2%initialize(nc_eta2+1, eta2_min, eta2_max, &
+                         SLL_PERIODIC, SLL_PERIODIC, 5)
 
 
 interp_eta1 => spl_eta1
@@ -117,37 +119,23 @@ end do
 end do
 
 call apply_remap_2D( eta2_to_eta1, f_eta2, f_eta1 )
-offset = local_to_global(layout_eta1,(/1,1/)) 
-offset_eta1 = (offset(1)-1)*delta_eta1
-offset_eta2 = (offset(2)-1)*delta_eta2
 
-do i_step=1, n_step
-
-  call advection_eta1(delta_t)
-
-  call sll_gnuplot_2d_parallel( offset_eta1, delta_eta1, &
-                                offset_eta2, delta_eta2, &
-                                size(f_eta1,1), size(f_eta1,2), &
-                                f_eta1, 'f_parallel', &
-                                i_step, error)
-
-  if (prank == MPI_MASTER) &
-     write(*,"(10x,' time = ', G15.3, ' sec' )") i_step*delta_t
-
-end do
-
-call apply_remap_2D( eta1_to_eta2, f_eta1, f_eta2 )
+!Compute offset for gnuplot call
 offset = local_to_global(layout_eta2,(/1,1/)) 
 offset_eta1 = (offset(1)-1)*delta_eta1
 offset_eta2 = (offset(2)-1)*delta_eta2
 
-call spl_eta1%delete()
-call spl_eta2%initialize(nc_eta2+1, eta2_min, eta2_max, &
-                         SLL_PERIODIC, SLL_PERIODIC, 2)
+call advection_eta1(0.5*delta_t)
 
-do i_step=n_step+1, 2*n_step
+do i_step=1, 3*n_step
 
   call advection_eta2(delta_t)
+
+  call apply_remap_2D( eta2_to_eta1, f_eta2, f_eta1 )
+
+  call advection_eta1(delta_t)
+
+  call apply_remap_2D( eta1_to_eta2, f_eta1, f_eta2 )
 
   call sll_gnuplot_2d_parallel( offset_eta1, delta_eta1, &
                                 offset_eta2, delta_eta2, &
@@ -178,6 +166,7 @@ subroutine advection_eta1(dt)
   sll_real64, intent(in) :: dt
   sll_real64 :: alpha
 
+  print*, '--- begin advection eta1'
   call compute_local_sizes(layout_eta1,loc_sz_i,loc_sz_j)
 
   do j=1,loc_sz_j
@@ -191,6 +180,7 @@ subroutine advection_eta1(dt)
      end do
 
   end do
+  print*, '--- end advection eta1'
 
 end subroutine advection_eta1
 
@@ -199,21 +189,21 @@ subroutine advection_eta2(dt)
   sll_real64, intent(in) :: dt
   sll_real64 :: alpha
 
+  print*, '--- begin advection eta2'
   call compute_local_sizes(layout_eta2,loc_sz_i,loc_sz_j)        
 
   do i=1,loc_sz_i
 
      alpha = dt
-     call interp_eta1%compute_interpolants( f_eta2(i,:) )
+     call interp_eta2%compute_interpolants( f_eta2(i,:) )
 
      do j = 1, loc_sz_j
-        print*, i, j
-        eta2 = eta2_min + modulo((j-1)*delta_eta2 - alpha, eta2_max-eta2_min)
-        print*, eta2
+        eta2 = eta2_min + (j-1)*delta_eta2 - alpha
         f_eta2(i,j) = interp_eta2%interpolate_value(eta2)
      end do
 
   end do
+  print*, '--- end advection eta2'
 
 end subroutine advection_eta2
 
