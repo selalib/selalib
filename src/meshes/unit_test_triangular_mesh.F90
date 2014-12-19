567e61857fa3a01fdaacac11bf2b4535474388b4
Pierre Navaro <navaro@unistra.fr>
2014-12-19 17:52:28 +0100

    Add fields for FEM
    Just a clone from DG fields

--- src/meshes/unit_test_triangular_mesh.F90
+++ src/meshes/unit_test_triangular_mesh.F90
@@ -1,18 +1,17 @@
 !> @internal [example]
-program unit_test_meshes
+program unit_test_triangular_mesh
 #include "sll_working_precision.h"
   use sll_triangular_meshes
   implicit none
 
   type(sll_triangular_mesh_2d) :: mesh
-  sll_int32  :: nc_eta1=10
+  sll_int32  :: nc_eta1=4
   sll_real64 :: eta1_min=0.0_f64
   sll_real64 :: eta1_max=1.0_f64
-  sll_int32  :: nc_eta2=10
+  sll_int32  :: nc_eta2=4
   sll_real64 :: eta2_min=0.0_f64
   sll_real64 :: eta2_max=1.0_f64
 
-  
   call sll_new(mesh,     &
                nc_eta1,  &
                eta1_min, &
@@ -22,9 +21,10 @@ program unit_test_meshes
                eta2_max)
 
   call sll_display(mesh)
+  call write_triangular_mesh_mtv(mesh, "tri_mesh.mtv")
   call sll_delete(mesh)
 
   print *, 'PASSED'
 
-end program unit_test_meshes
+end program unit_test_triangular_mesh
 !> @internal [example]
