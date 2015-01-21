1bd025f9fc7f73d5aef90b0d963e7e1ffbf14fa2
mela <laura.mendoza@ipp.mpg.de>
2015-01-21 11:56:21 +0100

    added a sim for gc in hex-mesh: hermite interpolation (Charles')

--- src/simulations/simulations_sequential/sim_2d_guiding_center_hexagonal/simulation_2d_guiding_center_hexagonal_splines.F90
+++ src/simulations/simulations_sequential/sim_2d_guiding_center_hexagonal/simulation_2d_guiding_center_hexagonal_splines.F90
@@ -1,4 +1,4 @@
-program test_hex_hermite
+program sim2d_gc_hex_splines
 
 #include "sll_memory.h"
 #include "sll_working_precision.h"
@@ -617,4 +617,4 @@ contains
   end subroutine write_center
 
 
-end program
+end program sim2d_gc_hex_splines
