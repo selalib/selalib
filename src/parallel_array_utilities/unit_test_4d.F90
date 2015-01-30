e0b64ff3716cffa3c4cb6d72d8ff09ace8ddb319
Pierre Navaro <navaro@unistra.fr>
2015-01-30 11:46:59 +0100

    Move some files (parallel utilities)

--- src/parallel_array_utilities/unit_test_4d.F90
+++ src/parallel_array_utilities/unit_test_4d.F90
@@ -1,24 +1,40 @@
 program unit_test_initializers_4d
+#include "sll_memory.h"
 #include "sll_working_precision.h"
 #include "sll_utilities.h"
+#include "sll_meshes.h"
+#include "sll_coordinates_transformations.h"
+#include "sll_parallel_utilities.h"
 
-use sll_cartesian_meshes
-use sll_coordinate_transformation_2d_base_module
-use sll_module_coordinate_transformations_2d
-use sll_common_coordinate_transformations
-use sll_common_array_initializers_module
-use sll_parallel_array_initializer_module
-use sll_parallel_array_output_module
+
+#define MPI_MASTER 0
 
 implicit none
 
-! logical meshes
-type(sll_cartesian_mesh_2d), pointer :: mx
-type(sll_cartesian_mesh_2d), pointer :: mv
-type(sll_cartesian_mesh_4d), pointer :: mesh_4d
-! coordinate transformations (test transforming spatial coordinates only)
+type(layout_4d),                              pointer :: layout
+type(sll_cartesian_mesh_2d),                  pointer :: mx
+type(sll_cartesian_mesh_2d),                  pointer :: mv
+type(sll_cartesian_mesh_4d),                  pointer :: mesh_4d
 class(sll_coordinate_transformation_2d_base), pointer :: tx
-sll_real64, dimension(2) :: params_identity
+sll_real64, dimension(2)                              :: params_identity
+sll_real64, dimension(:,:,:,:), allocatable           :: f
+
+
+sll_int32  :: prank, comm
+sll_int32  :: loc_sz_i, loc_sz_j, loc_sz_k, loc_sz_l
+sll_int64  :: psize
+sll_real64 :: tcpu1, tcpu2
+sll_int32  :: i,j,k,l,error
+sll_real64 :: vx,vy,v2,x,y
+sll_real64 :: kx, ky, eps = 0.1_f64
+sll_int32  :: gi, gj, gk, gl
+sll_int32  :: global_indices(4)
+
+call sll_boot_collective()
+
+prank = sll_get_collective_rank(sll_world_collective)
+psize = sll_get_collective_size(sll_world_collective)
+comm  = sll_world_collective%comm
 
 params_identity(:) = (/0.0_f64, 0.0_f64/) ! for identity this can be whatever
 ! initialize the logical meshes
@@ -48,6 +64,65 @@ call sll_display(mesh_4d)
 
 call write_mesh_4d(mesh_4d)
 
+!Layout for plotting
+layout => new_layout_4D( sll_world_collective )
+
+call initialize_layout_with_distributed_array( mx%num_cells1,      &
+                                               mx%num_cells2,      &
+                                               mv%num_cells1,      &
+                                               mv%num_cells2,      &
+                                               1,1,1,int(psize,4), &
+                                               layout)
+
+if ( prank == MPI_MASTER ) call sll_view_lims( layout )
+call flush(6)
+
+call compute_local_sizes(layout,loc_sz_i,loc_sz_j,loc_sz_k,loc_sz_l)        
+SLL_CLEAR_ALLOCATE(f(1:loc_sz_i,1:loc_sz_j,1:loc_sz_k,1:loc_sz_l), error)
+
+
+kx  = 2_f64*sll_pi/(mx%eta1_max-mx%eta1_min)
+ky  = 2_f64*sll_pi/(mx%eta2_max-mx%eta2_min)
+    
+do l=1,loc_sz_l 
+do k=1,loc_sz_k
+do j=1,loc_sz_j
+do i=1,loc_sz_i
+
+   global_indices = local_to_global(layout,(/i,j,k,l/)) 
+   gi = global_indices(1)
+   gj = global_indices(2)
+   gk = global_indices(3)
+   gl = global_indices(4)
+
+   x  = mesh_4d%eta1_min+(gi-1)*mesh_4d%delta_eta1
+   y  = mesh_4d%eta2_min+(gj-1)*mesh_4d%delta_eta2
+   vx = mesh_4d%eta3_min+(gk-1)*mesh_4d%delta_eta3
+   vy = mesh_4d%eta4_min+(gl-1)*mesh_4D%delta_eta4
+
+   v2 = vx*vx+vy*vy
+
+   f(i,j,k,l) = landau_cos_prod(eps,kx, ky, x, y, v2)
+
+end do
+end do
+end do
+end do
+
+call write_xmf_file(mesh_4d, f, layout, iplot=1)
+
 print *, 'PASSED'
+
+contains
+
+  function landau_cos_prod(eps, kx, ky, x, y, v2)
+    sll_real64 :: landau_cos_prod
+    sll_real64, intent(in) :: x, kx
+    sll_real64, intent(in) :: y, ky
+    sll_real64, intent(in) :: eps, v2
+
+    landau_cos_prod = (1._f64+eps*cos(kx*x)*cos(ky*y))/(2*sll_pi)*exp(-0.5_f64*v2)
+
+  end function landau_cos_prod
 
 end program unit_test_initializers_4d
