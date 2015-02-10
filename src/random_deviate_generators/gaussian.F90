32f02189afc7d1d7303a10dfc96d39bdd43e30f9
Sever Hirstoaga <hirstoaga@math.unistra.fr>
2015-02-10 15:23:00 +0100

    updates

--- src/random_deviate_generators/gaussian.F90
+++ src/random_deviate_generators/gaussian.F90
@@ -41,5 +41,26 @@ contains
        g_stored=.true.
     endif
   end function gaussian_deviate
+  
+  subroutine gaussian_deviate_2D(res)! returns in 'harvest' a normally
+!   distributed deviate with zero mean and unit variance, using
+!   random as the source of uniform deviates  !!!!!
+!   It's from Numerical Recipes  !
+    implicit none
+    sll_real64,intent(out) :: res(1:2)
+    sll_real64 :: rsq, v1, v2
+    
+    do
+       call random_number(v1)
+       call random_number(v2)
+       v1=2._f64*v1 - 1._f64
+       v2=2._f64*v2 - 1._f64
+       rsq = v1**2+v2**2
+       if (rsq>0._f64.and.rsq<1._f64) exit
+    enddo
+    rsq = sqrt(-2._f64*log(rsq)/rsq)
+    res(1) = v1*rsq
+    res(2) = v2*rsq
+  end subroutine gaussian_deviate_2D
 
 end module gaussian
