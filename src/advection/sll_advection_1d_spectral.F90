024731719f3b0f4e53175e97f7c0a565b87ada1e
Pierre Navaro <navaro@unistra.fr>
2015-02-11 17:41:00 +0100

    Fix problem with Doxygen
    and try new way to compute spectral advection

--- src/advection/sll_advection_1d_spectral.F90
+++ src/advection/sll_advection_1d_spectral.F90
@@ -45,6 +45,7 @@ type,extends(sll_advection_1d_base), public :: spectral_1d_advector
   type(sll_fft_plan),       pointer :: fwx
   type(sll_fft_plan),       pointer :: bwx
   sll_comp64, dimension(:), pointer :: tmp_x
+  sll_real64, dimension(:), pointer :: work
 
 contains
 
@@ -108,11 +109,12 @@ subroutine initialize( adv, num_cells, eta_min, eta_max)
 
   adv%kx(1) = 1.0_f64
   do i=1,num_cells/2-1
-     adv%kx(i+1)       = i * kx0
+     adv%kx(i+1) = i * kx0
   end do
   adv%kx(num_cells/2+1) = sll_pi / adv%delta_eta
 
-        
+  SLL_CLEAR_ALLOCATE(adv%work(1:2*num_cells+15), error)
+
 end subroutine initialize
 
 subroutine advect_1d( adv, a, dt, input, output )
@@ -123,11 +125,11 @@ subroutine advect_1d( adv, a, dt, input, output )
   sll_real64, dimension(:), intent(in)  :: input
   sll_real64, dimension(:), intent(out) :: output      
 
-  sll_int32  :: nx
+  sll_int32  :: num_cells
     
-  nx = adv%num_cells
+  num_cells = adv%num_cells
 
-  adv%d_dx = input(1:nx)
+  adv%d_dx = input(1:num_cells)
   call fft_apply_plan(adv%fwx, adv%d_dx, adv%tmp_x)
 
   !f = f^n exp(-i kx vx dt)
@@ -135,9 +137,9 @@ subroutine advect_1d( adv, a, dt, input, output )
   adv%tmp_x = adv%tmp_x*cmplx(cos(adv%kx*a*dt),sin(adv%kx*a*dt),kind=f64)
 
   call fft_apply_plan(adv%bwx, adv%tmp_x, adv%d_dx)
-  output(1:nx)= adv%d_dx / nx
+  output(1:num_cells)= adv%d_dx / num_cells
 
-  output(nx+1) = output(1)
+  output(num_cells+1) = output(1)
 
 end subroutine advect_1d
 
@@ -149,23 +151,42 @@ subroutine advect_1d_constant( adv, a, dt, input, output )
   sll_real64, dimension(:),    intent(in)  :: input
   sll_real64, dimension(:),    intent(out) :: output      
 
-  sll_int32  :: nx
+  sll_int32  :: i, num_cells
+  sll_real64 :: x, xmin, xmax
+  sll_real64 :: tmp, ima, imb, rea, reb
     
-  nx = adv%num_cells
+  num_cells = adv%num_cells
 
-  adv%d_dx = input(1:nx)
-  call fft_apply_plan(adv%fwx, adv%d_dx, adv%tmp_x)
+  xmin      = adv%eta_min
+  xmax      = adv%eta_max
 
-  !f = f^n exp(-i kx vx dt)
+  output = input
 
-  adv%tmp_x = adv%tmp_x* exp(-cmplx(0.0_f64,adv%kx*a*dt,kind=f64))
+  x = - a*dt/(xmax-xmin)
+  x = x - floor(x)
+  x = x * 2 * sll_pi 
 
+  output(num_cells+1) = output(1)
 
+  call dfftf(num_cells, output, adv%work)
+ 
+  tmp = 1.0_f64 / num_cells
 
-  call fft_apply_plan(adv%bwx, adv%tmp_x, adv%d_dx)
-  output(1:nx)= adv%d_dx / nx
+  output(1) = output(1)*tmp
+  do i=1, (num_cells-2)/2
+    rea = output(2*i)
+    ima = output(2*i+1)
+    reb = tmp*cos(i*x)
+    imb = tmp*sin(i*x)
+    output(2*i)   = rea*reb-ima*imb
+    output(2*i+1) = rea*imb+reb*ima
+  end do
 
-  output(nx+1) = output(1)
+  if (mod(num_cells,2)==0) then
+    output(num_cells)=output(num_cells)*tmp*cos(0.5_f64*num_cells*x)
+  end if
+  
+  call dfftb(num_cells, output, adv%work)
 
 end subroutine advect_1d_constant
 
