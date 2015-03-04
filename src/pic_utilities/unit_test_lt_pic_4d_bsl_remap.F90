4e6819a6f2ab1138ec653dce4fb48d5ac34763cc
Martin CAMPOS PINTO <campos@ann.jussieu.fr>
2015-03-04 08:50:21 +0100

    minor change

--- src/pic_utilities/unit_test_lt_pic_4d_bsl_remap.F90
+++ src/pic_utilities/unit_test_lt_pic_4d_bsl_remap.F90
@@ -228,7 +228,7 @@ program unit_test_lt_pic_bsl_remap
   !MCP: for the std tensor-product hat function, take hat_shift = 1 and basis_height = 0
   hat_shift = 1.
   !aaabasis_height = 0.
-  basis_height = 0.5!aaa
+  basis_height = 0 !.5!aaa
 
   ! This initializes the particles [[file:../pic_particle_initializers/lt_pic_4d_init.F90::sll_lt_pic_4d_init_hat_f]]
 
