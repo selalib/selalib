21ccf2f180bb29b0e44c569ea32b3970ba6f35f0
Yaman Güçlü <yaman.guclu@gmail.com>
2015-03-12 12:36:26 +0100

    New definitions for SLL_WARNING and SLL_ERROR macros:
      * they require 2 arguments (caller name and message);
      * they are defined in sll_errors.h and use subroutines in sll_errors.F90;
      * the abort behavior can be defined in a single place.

--- src/utilities/test_crash.F90
+++ src/utilities/test_crash.F90
@@ -3,8 +3,9 @@ program test_crash
 #include "sll_utilities.h"
 
   implicit none
+  character( len=* ), parameter :: my_name = "test_crash"
 
-  SLL_WARNING( "Let's test a warning." )
-  SLL_ERROR  ( "Let's test an error."  )
+  SLL_WARNING( my_name, "Let's test a warning." )
+  SLL_ERROR  ( my_name, "Let's test an error."  )
 
 end program test_crash
