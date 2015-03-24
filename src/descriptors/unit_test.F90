276ee8936303b6326f9a6202e2982e2db771d3d5
Jakob Ameres <jakob.ameres@gmail.com>
2015-03-24 16:34:22 +0100

    landau damping with magnetic field

--- src/descriptors/unit_test.F90
+++ src/descriptors/unit_test.F90
@@ -10,7 +10,7 @@ type(sll_vlasovpoisson_sim) :: testcase
 
 call testcase%parse('  SLL_LANDAU_DIAG       ')
  print *, testcase%name()
- print *, SLL_LANDAU_DIAG%name()
+ print *, SLL_LANDAU_DIAG%name(), SLL_LANDAU_DIAG%id
 
 print *, "PASSED"
 
