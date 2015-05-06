ad5adefa15d37bf93995e3c2bb2be3579140a6f1
Pierre Navaro <pierre.navaro@inria.fr>
2015-05-06 13:22:50 +0200

    remove small memory leaks in GCES

--- src/interpolators/sll_arbitrary_degree_spline_interpolator_2d.F90
+++ src/interpolators/sll_arbitrary_degree_spline_interpolator_2d.F90
@@ -1144,8 +1144,8 @@ sll_int32,                  intent(in), optional :: size_eta2_coords
 sll_real64, dimension(:),   pointer :: taux
 sll_real64, dimension(:),   pointer :: tauy
 
-sll_int32, pointer :: taux_deriv(:)
-sll_int32, pointer :: tauy_deriv(:)
+sll_int32 :: taux_deriv(2)
+sll_int32 :: tauy_deriv(2)
 
 sll_int32  :: mx,my
 sll_real64 :: eta1_min, eta1_max, delta_eta1
@@ -1209,9 +1209,6 @@ ky  = interpolator%spline_degree2 + 1
 period1 = interpolator%eta1_max - interpolator%eta1_min
 period2 = interpolator%eta2_max - interpolator%eta2_min
 
-! compute the knots t1 and t2
-SLL_ALLOCATE(taux_deriv(2),ierr)
-SLL_ALLOCATE(tauy_deriv(2),ierr)
   
 select case (interpolator%bc_selector)
 
@@ -1980,7 +1977,7 @@ ky = interpolator%spline_degree2+1
 
 SLL_ALLOCATE(coef(ky),ierr)
 SLL_ALLOCATE(tab(nx),ierr)
-SLL_ALLOCATE(ty(2*ky),ierr)
+!SLL_ALLOCATE(ty(2*ky),ierr)
 
 length1 = interpolator%eta1_max-interpolator%eta1_min
 length2 = interpolator%eta2_max-interpolator%eta2_min
@@ -2012,9 +2009,12 @@ do j = 1, ky
   coef(j) = bvalue(interpolator%deboor(1), t1(1:nx+kx), tab, nx, kx, x, 0)
 end do
 
-ty = interpolator%t2(lefty-ky+1:lefty+ky)
+ty => interpolator%t2(lefty-ky+1:lefty+ky)
 
 val = bvalue(interpolator%deboor(2), ty, coef, ky, ky, y, 0)
+
+deallocate(tab)
+deallocate(coef)
 
 end function interpolate_value_ad2d
 
@@ -2107,6 +2107,10 @@ ty = t2(lefty-ky+1:lefty+ky)
 
 val = bvalue(interpolator%deboor(2), ty, coef, ky, ky, y, deriv2 )
 
+deallocate(ty)
+deallocate(tab)
+deallocate(coef)
+
 end function interpolate_derivative1_ad2d
      
 !> @brief First derivative in eta2 Interpolation on the points eta1 and eta2 
@@ -2190,6 +2194,10 @@ end do
 
 ty =  t2(lefty-ky+1:lefty+ky)
 val = bvalue(interpolator%deboor(2), ty, coef, ky, ky, y, deriv2 )
+
+deallocate(ty)
+deallocate(tab)
+deallocate(coef)
 
 end function interpolate_derivative2_ad2d
 
@@ -3174,8 +3182,8 @@ sll_int32,                           intent(in)  :: nx
 sll_int32,                           intent(in)  :: kx
 sll_int32,                           intent(in)  :: ny
 sll_int32,                           intent(in)  :: ky
-sll_real64, dimension(:),   pointer, intent(in)  :: taux
-sll_real64, dimension(:),   pointer, intent(in)  :: tauy
+sll_real64, dimension(:),            intent(in)  :: taux
+sll_real64, dimension(:),            intent(in)  :: tauy
 sll_real64, dimension(:,:), pointer, intent(in)  :: g   
 
 sll_real64, dimension(:,:), pointer, intent(out) :: bcoef
@@ -3319,7 +3327,7 @@ end subroutine spli2d_custom
 subroutine spli2d ( db, tau, gtau, t, n, k, m, work, q, bcoef, iflag )
     
 type(deboor_type)                                :: db
-sll_real64, dimension(:),   pointer, intent(in)  :: tau
+sll_real64, dimension(:),            intent(in)  :: tau
 sll_real64, dimension(:,:), pointer, intent(in)  :: gtau
 sll_real64, dimension(:),   pointer, intent(in)  :: t
 sll_int32                          , intent(in)  :: n
@@ -3480,8 +3488,8 @@ sll_int32,                           intent(in) :: mx
 sll_int32,                           intent(in) :: my
 sll_real64, dimension(:),   pointer, intent(in) :: taux 
 sll_real64, dimension(:),   pointer, intent(in) :: tauy 
-sll_int32,  dimension(:),   pointer, intent(in) :: taux_der 
-sll_int32,  dimension(:),   pointer, intent(in) :: tauy_der
+sll_int32,  dimension(:),            intent(in) :: taux_der 
+sll_int32,  dimension(:),            intent(in) :: tauy_der
 sll_real64, dimension(:,:), pointer, intent(in) :: gtau    
 sll_real64, dimension(:,:), pointer, intent(in) :: gtau_der1 
 sll_real64, dimension(:,:), pointer, intent(in) :: gtau_der2
