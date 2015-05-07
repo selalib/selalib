1fce4e89493a607b157bfe00295462e4091d3f6a
Pierre Navaro <navaro@math.cnrs.fr>
2015-05-07 19:15:17 +0200

    Try to improve SPM and upgrade jorek

--- src/sparse_matrix_manager/partition.F90
+++ src/sparse_matrix_manager/partition.F90
@@ -3,52 +3,56 @@ module qsort_partition
 contains
 
 recursive subroutine QsortC(A)
+
   integer, intent(inout), dimension(:) :: A
   integer :: iq
 
   if(size(A) > 1) then
-     call Partition(A, iq)
-     call QsortC(A(:iq-1))
-     call QsortC(A(iq:))
+    call Partition(A, iq)
+    call QsortC(A(:iq-1))
+    call QsortC(A(iq:))
   endif
+
 end subroutine QsortC
 
-subroutine Partition(A, marker)
+subroutine partition(A, marker)
 
   integer, intent(inout), dimension(:) :: A
-  integer, intent(out) :: marker
-  integer :: i, j
-  integer :: temp
-  integer :: x      ! pivot point
+  integer, intent(out)                 :: marker
+  integer                              :: i
+  integer                              :: j
+  integer                              :: temp
+  integer                              :: x
+
   x = A(1)
-  i= 0
-  j= size(A) + 1
+  i = 0
+  j = size(A) + 1
 
   do
-     j = j-1
-     do
-        if (A(j) <= x) exit
-        j = j-1
-     end do
-     i = i+1
-     do
-        if (A(i) >= x) exit
-        i = i+1
-     end do
-     if (i < j) then
-        ! exchange A(i) and A(j)
-        temp = A(i)
-        A(i) = A(j)
-        A(j) = temp
-     elseif (i == j) then
-        marker = i+1
-        return
-     else
-        marker = i
-        return
-     endif
+    j = j-1
+    do
+      if (A(j) <= x) exit
+      j = j-1
+    end do
+    i = i+1
+    do
+      if (A(i) >= x) exit
+      i = i+1
+    end do
+    if (i < j) then
+      ! exchange A(i) and A(j)
+      temp = A(i)
+      A(i) = A(j)
+      A(j) = temp
+    elseif (i == j) then
+      marker = i+1
+      return
+    else
+      marker = i
+      return
+    endif
   end do
 
-end subroutine Partition
+end subroutine partition
 
 end module qsort_partition
