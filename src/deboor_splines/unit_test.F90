53d0dcd49643522038eef22d0e8b00b81be2662d
Pierre Navaro <navaro@unistra.fr>
2015-06-18 16:52:03 +0200

    Add new test with slopes set on boundaries

--- src/deboor_splines/unit_test.F90
+++ src/deboor_splines/unit_test.F90
@@ -2,38 +2,41 @@ program unit_test
 #include "sll_memory.h"
 #include "sll_working_precision.h"
 #include "sll_constants.h"
+#include "sll_assert.h"
 use sll_module_deboor_splines_1d
 
 implicit none
 
-sll_real64, dimension(:), pointer     :: t
-sll_real64, dimension(:), allocatable :: f
-sll_real64, dimension(:), allocatable :: df
-sll_real64, dimension(:), allocatable :: tau
-sll_int32,  dimension(:), allocatable :: tau_der
+type :: sll_bsplines
+
+  sll_int32               :: n
+  sll_int32               :: k
+  sll_real64, allocatable :: tau(:)
+  sll_real64, pointer     :: t(:)
+  sll_real64, allocatable :: q(:)
+  sll_real64, pointer     :: bcoef(:)
+
+end type sll_bsplines
+
+type(sll_bsplines) :: bsplines
+
+sll_real64, dimension(:), allocatable :: x
+sll_real64, dimension(:), allocatable :: y
 sll_int32                             :: n
 sll_int32                             :: m
 sll_int32                             :: k
-sll_real64, dimension(:), allocatable :: q
-sll_real64, dimension(:),   pointer   :: bcoef
-sll_real64, dimension(:),   pointer   :: bcoef_spline 
-sll_real64, dimension(:,:), pointer   :: bcoef_der
-sll_real64, dimension(:,:), pointer   :: a
-sll_int32                             :: iflag
 sll_int32                             :: mflag
 sll_int32                             :: ierr
-sll_real64                            :: y
-sll_int32                             :: i
-sll_int32                             :: j
-sll_int32                             :: l
-sll_int32                             :: left
-sll_int32                             :: kpkm2
-sll_int32                             :: jj
-sll_real64, dimension(:), allocatable ::  gtau ! (n)
-sll_real64, dimension(:), allocatable ::  gtau_der ! (n)
+sll_real64, dimension(:), allocatable :: gtau
+sll_real64, dimension(:), allocatable :: htau
 
-sll_real64:: taui
-sll_real64:: taui_der
+sll_int32                             :: i
+sll_int32                             :: nx
+
+sll_real64 :: tau_min = 0.0_f64
+sll_real64 :: tau_max = 1.0_f64
+sll_real64 :: slope_min
+sll_real64 :: slope_max
 
 ! TAU(N),      the data point abscissas. TAU should be strictly increasing.
 ! TAU_der(M),  the node index to evaluate the derivative.
@@ -54,183 +57,271 @@ sll_real64:: taui_der
 ! Output, real ( kind = 8 ) BCOEF(N+M), the B-spline coefficients of 
 ! the interpolant.
 
+nx = 100
+SLL_ALLOCATE(x(nx),ierr)
+SLL_ALLOCATE(y(nx),ierr)
+
+do i = 1, nx
+  x(i) = (i-1)*1.0_f64/(nx-1)
+end do
 
 k = 4
 n = 10
 m = 2
 
-SLL_ALLOCATE(t(n+k+m),ierr)
-SLL_ALLOCATE(f(n),ierr)
-SLL_ALLOCATE(df(m),ierr)
-SLL_ALLOCATE(tau(n),ierr)
+call initialize_bsplines(bsplines, n, k, tau_min, tau_max)
+
+call compute_interpolants(bsplines)
+
 SLL_ALLOCATE(gtau(n),ierr)
-SLL_ALLOCATE(gtau_der(n),ierr)
-SLL_ALLOCATE(tau_der(m),ierr)
-SLL_ALLOCATE(q((2*k-1)*(n+m)),ierr)
-SLL_ALLOCATE(bcoef(n+m),ierr)
-SLL_ALLOCATE(bcoef_spline(n+m),ierr)
-SLL_ALLOCATE(bcoef_der(k,2),ierr)
-SLL_ALLOCATE(a(k,k),ierr); a = 0.0_f64
-
-!PN : Set point values and abscissae
-do i = 1 , n
- tau(i) = 0.0_f64 + (i-1)*1./(n-1)
- f(i) = cos(2*sll_pi*tau(i))
+gtau = cos(2*sll_pi*bsplines%tau)
+call interpolate_array_values( bsplines, gtau, nx, x, y)
+do i = 1, nx
+  write(12,*) x(i), y(i), cos(2*sll_pi*x(i))
 end do
 
-!PN : Initialize Knots
-t(1:k) = tau(1)
-do i = k+1, n+k-2
-  t(i) = tau(i-k+1)
-end do
-do i = n+k-1,n+k+m
-  t(i) = tau(n)
+SLL_ALLOCATE(htau(n),ierr)
+htau = sin(2*sll_pi*bsplines%tau)
+call interpolate_array_values( bsplines, htau, nx, x, y)
+do i = 1, nx
+  write(13,*) x(i), y(i), sin(2*sll_pi*x(i))
 end do
 
-tau_der(1)   = 1
-df(1) = -sin(2*sll_pi*tau(1))*2*sll_pi
-tau_der(2)   = n
-df(2) = -sin(2*sll_pi*tau(n))*2*sll_pi
+slope_min = -sin(2*sll_pi*tau_min)*2*sll_pi
+slope_max = -sin(2*sll_pi*tau_max)*2*sll_pi
 
-call splint_der(tau,f,tau_der,df,t,n,m,k,q,bcoef,iflag ) 
-
-!PN : interpolate values
-print*, " Values "
-do i = 1 , n
-  y=bvalue( t, bcoef, n+m, k, tau(i), 0)
-  print"(3(a8,f17.12))", 'approx',y,'exact', f(i), 'error', y-f(i)
-end do
-
-!PN : interpolate values of first derivative
-!print*, " Values of first derivative "
-do i = 1,n
-  y=bvalue( t, bcoef, n+m, k, tau(i), 1)
-  print"(3(a8,f17.12))",'approx',y                              &
-                       ,'exact', -sin(2*sll_pi*tau(i))*2*sll_pi &
-                       ,'error',y+sin(2*sll_pi*tau(i))*2*sll_pi
-end do
+call compute_interpolants_with_slopes(bsplines)
+call interpolate_array_values_with_slopes( bsplines, htau, nx, x, slope_min, slope_max, y)
     
-kpkm2 = 2*(k-1)
-left  = k
-q(1:(2*k-1)*(n+m)) = 0.0_f64
-bcoef_der(1:k,1:2) = 0.0_f64
+print*, 'PASSED'
 
-! we must suppose that m is <= than n 
-if (m > n) then
-  print*, 'problem m must be < = at n'
-  print*, 'value m =', m, 'value n =', n
-  stop
-end if
-l = 1 ! index for the derivative
-!
-!  Loop over I to construct the N interpolation equations.
-!
-do i = 1, n-1
-   
-  taui = tau(i)
-  
-  !
-  !  Find LEFT in the closed interval (I,I+K-1) such that
-  !
-  !    T(LEFT) <= TAU(I) < T(LEFT+1)
-  !
-  !  The matrix is singular if this is not possible.
-  !  With help of the Schoenberg-Whitney theorem 
-  !  we can prove that if the diagonal of the 
-  !  matrix B_j(x_i) is null, we have a non-inversible matrix.  
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+contains
 
-  call interv( t, n+m+k, taui, left, mflag )
+subroutine initialize_bsplines(this, n, k, tau_min, tau_max)
 
-  !  The I-th equation enforces interpolation at TAUI, hence for all J,
-  !
-  !    A(I,J) = B(J,K,T)(TAUI).
-  !
-  !Only the K entries with J = LEFT-K+1,...,LEFT actually might be nonzero.
-  !
-  !These K numbers are returned, in BCOEF (used for temporary storage here),
-  !  by the following.
-  !
-  
-  call bsplvb ( t, k, 1, taui, left, bcoef )
-  
-  !  We therefore want BCOEF(J) = B(LEFT-K+J)(TAUI) to go into
-  !  A(I,LEFT-K+J), that is, into Q(I-(LEFT+J)+2*K,(LEFT+J)-K) since
-  !  A(I+J,J) is to go into Q(I+K,J), for all I, J, if we consider Q
-  !  as a two-dimensional array, with  2*K-1 rows.  See comments in
-  !  BANFAC.
-  !
-  !  In the present program, we treat Q as an equivalent
-  !  one-dimensional array, because of fortran restrictions on
-  !  dimension statements.
-  !
-  !  We therefore want  BCOEF(J) to go into the entry of Q with index:
-  !
-  !    I -(LEFT+J)+2*K + ((LEFT+J)-K-1)*(2*K-1)
-  !    =  begin_ligne +  (begin_col -1) * number_coef_different_0
-  !   = I-LEFT+1+(LEFT -K)*(2*K-1) + (2*K-2)*J
-  !
-  jj = i - left + 1 + ( left - k ) * ( k + k - 1 ) + l - 1
-  
-  do j = 1, k
-     jj = jj + kpkm2  ! kpkm2 = 2*(k-1)
-     q(jj) = bcoef(j)
+  type(sll_bsplines), intent(out) :: this
+  sll_int32         , intent(in)  :: n
+  sll_int32         , intent(in)  :: k
+  sll_real64        , intent(in)  :: tau_min
+  sll_real64        , intent(in)  :: tau_max
+
+  sll_int32                       :: ierr
+
+  this%n = n
+  this%k = k
+
+  SLL_ALLOCATE(this%tau(n), ierr)
+  do i = 1, n
+    this%tau(i) = tau_min + (i-1) * (tau_max-tau_min) / (n-1)
   end do
 
-  bcoef_spline(i+l-1) = gtau(i)
-  if ( tau_der(l) == i ) then   
-     taui_der = taui
-     call bsplvd( t, k, taui_der, left, a, bcoef_der, 2)
-     l = l + 1
-     jj = i - left + 1 + ( left - k ) * ( k + k - 1 ) + l - 1
-     do j = 1, k
-        jj = jj + kpkm2  ! kpkm2 = 2*(k-1)
-        q(jj) = bcoef_der(j,2)
-     end do
-  bcoef_spline(i+ l-1) = gtau_der(l-1)
+  SLL_ALLOCATE(this%t(n+k), ierr)
+
+  this%t(1:k) = this%tau(1)
+  if ( mod(k,2) == 0 ) then
+    do i = k+1,n
+      this%t(i) = this%tau(i-k/2)
+    end do
+  else
+    do i = k+1, n
+      this%t(i) = 0.5*(this%tau(i-(k-1)/2)+this%tau(i-1-(k-1)/2))
+    end do
+  end if
+  do i = n+1,n+k
+    this%t(i) = this%tau(n)
+  end do
+
+  SLL_ALLOCATE(this%q(1:(2*k-1)*n),ierr)
+  SLL_ALLOCATE(this%bcoef(n),ierr)
+
+end subroutine initialize_bsplines
+
+subroutine compute_interpolants(this)
+  
+  type(sll_bsplines)     :: this 
+
+  sll_int32              :: n
+  sll_int32              :: k
+  sll_int32              :: j
+  sll_int32              :: jj
+  sll_int32              :: kpkm2
+  sll_int32              :: left
+  sll_int32              :: iflag
+  sll_real64             :: taui
+
+  n = this%n
+  k = this%k
+
+  kpkm2   = 2 * ( k - 1 )
+  left    = k
+  this%q  = 0.0D+00
+  
+  do i = 1, n !  Loop over I to construct the N interpolation equations.
+    
+    taui = this%tau(i)
+    call interv( this%t, n+k, taui, left, mflag )
+    call bsplvb ( this%t, k, 1, taui, left, this%bcoef )
+    jj = i - left + 1 + ( left - k ) * ( k + k - 1 )
+    do j = 1, k
+      jj = jj + kpkm2
+      this%q(jj) = this%bcoef(j)
+    end do
+      
+  end do
+  !
+  !  Obtain factorization of A, stored again in Q.
+  !
+  call banfac ( this%q, k+k-1, n, k-1, k-1, iflag )
+    
+  if ( iflag == 2 ) then
+    write ( *, '(a)' ) ' '
+    write ( *, '(a)' ) 'SPLINT - Fatal Error!'
+    write ( *, '(a)' ) '  The linear system is not invertible!'
+    stop
   end if
 
-end do
+end subroutine compute_interpolants
 
-taui = tau(n)
-call interv( t, n+m+k, taui, left, mflag )
-if ( tau_der(l)== n ) then   
-  taui_der = taui
-  call bsplvd( t, k, taui_der, left, a, bcoef_der, 2)
+
+subroutine interpolate_array_values( this, gtau, nx, x, y)
+
+  type(sll_bsplines)      :: this 
+  sll_real64, intent(in)  :: gtau(:)
+  sll_int32,  intent(in)  :: nx
+  sll_real64, intent(in)  :: x(nx)
+  sll_real64, intent(out) :: y(nx)
+
+  sll_int32              :: i
+  
+  SLL_ASSERT(size(gtau) == this%n)
+
+  !  Solve A * BCOEF = GTAU by back substitution.
+  this%bcoef(1:n) = gtau(1:n)
+  
+  call banslv( this%q, this%k+this%k-1, n, this%k-1, this%k-1, this%bcoef )
+
+  do i = 1, nx
+    y(i) = bvalue( this%t, this%bcoef, this%n, this%k, x(i), 0)
+  end do
+
+end subroutine interpolate_array_values
+
+subroutine compute_interpolants_with_slopes(this)
+
+  type(sll_bsplines)      :: this 
+  sll_real64              :: dbiatx(k,2)
+  sll_real64              :: a(k,k)
+  sll_real64              :: taui
+  sll_int32               :: kpkm2
+  sll_int32               :: left
+  sll_int32               :: n
+  sll_int32, parameter    :: m = 2
+  sll_int32               :: k
+  sll_int32               :: iflag
+  sll_int32               :: ierr
+  sll_int32               :: j
+  sll_int32               :: jj
+  sll_int32               :: l
+  
+
+  n = this%n
+  k = this%k
+  a = 0.0_f64
+
+
+  kpkm2     = 2*(k-1)
+  left      = k
+  this%q    = 0.0_f64
+  dbiatx    = 0.0_f64
+  
+  SLL_ASSERT(m < n) 
+
+  l = 1 ! index for the derivative
+
+  do i = 1, n-1
+      
+    taui = this%tau(i)
+    call interv( this%t, n+m+k, taui, left, mflag )
+    call bsplvb ( this%t, k, 1, taui, left, this%bcoef )
+    jj = i - left + 1 + ( left - k ) * ( k + k - 1 ) + l - 1
+    
+    do j = 1, k
+      jj = jj + kpkm2  ! kpkm2 = 2*(k-1)
+      this%q(jj) = this%bcoef(j)
+    end do
+   
+    if ( i == 1 ) then   
+      call bsplvd( this%t, k, taui, left, a, dbiatx, 2)
+      l = l + 1
+      jj = i - left + 1 + ( left - k ) * ( k + k - 1 ) + l - 1
+      do j = 1, k
+        jj = jj + kpkm2  ! kpkm2 = 2*(k-1)
+        this%q(jj) = dbiatx(j,2)
+      end do
+    end if
+   
+  end do
+  
+  taui = this%tau(n)
+  call interv( this%t, n+m+k, taui, left, mflag )
+  call bsplvd( this%t, k, taui, left, a, dbiatx, 2)
   jj = n - left + 1 + ( left - k ) * ( k + k - 1 ) + l - 1
   do j = 1, k
      jj = jj + kpkm2  ! kpkm2 = 2*(k-1)
-     q(jj) = bcoef_der(j,2)
+     this%q(jj) = dbiatx(j,2)
   end do
-  bcoef_spline(n+l-1) = gtau_der(l)
   l = l + 1
-end if
+  
+  call bsplvb ( this%t, k, 1, taui, left, this%bcoef )
+  jj = n - left + 1 + ( left - k ) * ( k + k - 1 ) + l - 1
+     
+  do j = 1, k
+    jj = jj + kpkm2  ! kpkm2 = 2*(k-1)
+    this%q(jj) = this%bcoef(j)
+  end do
+  
+  !Obtain factorization of A, stored again in Q.
 
-!  calculates the value of all possibly nonzero b-splines at taui of order
-!  k  =  max( jhigh , (j+1)*(index-1) )
- 
-call bsplvb ( t, k, 1, taui, left, bcoef )
-jj = n - left + 1 + ( left - k ) * ( k + k - 1 ) + l - 1
-   
-do j = 1, k
-  jj = jj + kpkm2  ! kpkm2 = 2*(k-1)
-  q(jj) = bcoef(j)
-end do
-bcoef_spline(n+l-1) = gtau(n)
-!
-!  Obtain factorization of A, stored again in Q.
-!
-call banfac ( q, k+k-1, n+m, k-1, k-1, iflag )
+  call banfac ( this%q, k+k-1, n+m, k-1, k-1, iflag )
 
-if ( iflag == 2 ) then
-   write ( *, '(a)' ) ' '
-   write ( *, '(a)' ) 'SPLINT - Fatal Error!'
-   write ( *, '(a)' ) '  The linear system is not invertible!'
-   stop
-end if
-!
-!  Solve A * BCOEF = GTAU by back substitution.
-!
-call banslv ( q, k+k-1, n+m, k-1, k-1, bcoef_spline )
+end subroutine compute_interpolants_with_slopes
 
-print*, 'PASSED'
+subroutine interpolate_array_values_with_slopes( this,      &
+                                                 gtau,      &
+                                                 nx,        &
+                                                 x,         &
+                                                 slope_min, &
+                                                 slope_max, &
+                                                 y)
+
+  type(sll_bsplines)      :: this 
+  sll_real64, intent(in)  :: gtau(:)
+  sll_int32,  intent(in)  :: nx
+  sll_real64, intent(in)  :: x(nx)
+  sll_real64, intent(out) :: y(nx)
+  sll_real64, intent(in)  :: slope_min
+  sll_real64, intent(in)  :: slope_max
+
+  sll_int32               :: i
+  sll_real64, allocatable :: bcoef_spline(:)
+
+  n = this%n
+  k = this%k
+
+  SLL_ALLOCATE(bcoef_spline(n+k+m), ierr)
+
+  SLL_ASSERT(size(gtau) == this%n)
+
+  bcoef_spline(2)     = slope_min
+  bcoef_spline(n+k-1) = slope_max
+
+  call banslv ( this%q, k+k-1, n+m, k-1, k-1, bcoef_spline )
+  
+  do i = 1, nx
+    y(i) = bvalue( this%t, this%bcoef, this%n, this%k, x(i), 0)
+  end do
+
+end subroutine interpolate_array_values_with_slopes
+
 end program unit_test
