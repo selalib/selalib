374ae5ae540285f579d66cd8f3686b5da8a7c45e
Antoine Le Hyaric <Antoine.Le_Hyaric@upmc.fr>
2015-06-24 18:13:43 +0200

    partial commit on DebianTesting64 (I)

---
+++ src/particle_methods/particle_methods_doc.F90
@@ -0,0 +1,22 @@
+! This file is read by doxygen software
+! Change it to match with your library
+! [[http://www.stack.nl/~dimitri/doxygen/manual/docblocks.html#fortranblocks]]
+! To see the documentation, remove the line containing your directory 
+! in [[file:~/selalib/doc/doxygen/Doxyfile::EXCLUDE tag]] if it is excluded. 
+! Type 'make doc' in build directory.
+! To check the results, open : 
+! selalib/doc/build/html/doxygen/html/defgroup___.html 
+! The following lines will be read by doxygen to generate documentation:
+
+!> @defgroup particle_methods sll_particle_methods 
+!! @brief
+!! Classes for particle methods.
+!! @details
+!! PLEASE ADD DOCUMENTATION 
+!!
+!! Methods:
+!!
+!> Classes available are:
+!>
+!> <b> How to use it </b>
+!>
