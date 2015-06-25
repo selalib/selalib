442b5ae0e3b46e435ba18c2f5ce9fc5878718ff3
Antoine Le Hyaric <Antoine.Le_Hyaric@upmc.fr>
2015-06-25 15:22:45 +0200

    Changed all paths in comments from absolute ~/selalib to environment-dependant $(SELALIB} and [[selalib:...]]

--- src/particle_methods/particle_methods_doc.F90
+++ src/particle_methods/particle_methods_doc.F90
@@ -1,16 +1,16 @@
   ! [[shell:header\alh 'particle_methods_doc.F90']] (cf [[file:~/alh/bin/headeralh]])
   ! headeralh authors=- default=0 f90 start=25/06/15
 
-  ! this documentation creates the file [[file:~/selalib/doc/build/html/doxygen/html/group__particle__methods.html]] and
-  ! is linked from the list of "Libraries" at [[file:~/selalib/doc/build/html/doxygen/html/modules.html]].
+  ! this documentation creates the file [[selalib:doc/build/html/doxygen/html/group__particle__methods.html]] and
+  ! is linked from the list of "Libraries" at [[selalib:doc/build/html/doxygen/html/modules.html]].
   !
-  ! to rebuild the documentation: [[elisp:(compile "cd ~/selalib/build && make doc")]]
+  ! to rebuild the documentation: [[elisp:(compile "cd ${SELALIB}/build && make doc")]]
 
   !> @defgroup particle_methods sll_particle_methods 
   !! @brief
   !! Centralized location for all particle methods, in directory src/particle_methods
   !! @details
-  !! 
+  !! WorkInProgressALH
   
   ! Local Variables:
   ! mode:F90
