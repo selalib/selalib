1f366f2e020e507197c8a6e5c827694f0a0754d9
Katharina Kormann <katharina.kormann@tum.de>
2015-08-05 15:33:37 +0200

    Add some documentation.

--- src/particle_methods/particle_initializers/sll_particle_initializer_2x2v.F90
+++ src/particle_methods/particle_initializers/sll_particle_initializer_2x2v.F90
@@ -1,20 +1,7 @@
-!**************************************************************
-!  Copyright INRIA
-!  Authors : 
-!     ???
-!  
-!  This code SeLaLib (for Semi-Lagrangian-Library) 
-!  is a parallel library for simulating the plasma turbulence 
-!  in a tokamak.
-!  
-!  This software is governed by the CeCILL-B license 
-!  under French law and abiding by the rules of distribution 
-!  of free software.  You can  use, modify and redistribute 
-!  the software under the terms of the CeCILL-B license as 
-!  circulated by CEA, CNRS and INRIA at the following URL
-!  "http://www.cecill.info". 
-!**************************************************************
-
+!> @ingroup particle_methods
+!> @author Katharina Kormann
+!> @brief Particle initializer class with various functions to initialize a particle.
+!> @details ...
 module sll_m_particle_initializer
 
 #include "sll_working_precision.h"
@@ -26,31 +13,32 @@ module sll_m_particle_initializer
   
   implicit none
 
-
-  type :: sll_particle_initializer_2x2v
-     
-
-   contains
-     procedure :: draw_product_density => draw_product_density_2x2v
-     
-  end type sll_particle_initializer_2x2v
+! Different ansatz. Use or remove
+!!$  type :: sll_particle_initializer_2x2v
+!!$     
+!!$
+!!$   contains
+!!$     procedure :: draw_product_density => draw_product_density_2x2v
+!!$     
+!!$  end type sll_particle_initializer_2x2v
  
 
 contains
-  
-  subroutine draw_product_density_2x2v (this, particle_group, random_seed)
-    class(sll_particle_initializer_2x2v), intent(inout)         :: this
-    class(sll_particle_group_base),  intent(inout)              :: particle_group
-    !sll_real64, intent(in)                                      :: thermal_velocity
-    sll_int32,  intent(in)                                      :: random_seed
+!!$  !---------------------------------------------------------------------------!
+!!$  
+!!$  subroutine draw_product_density_2x2v (this, particle_group, random_seed)
+!!$    class(sll_particle_initializer_2x2v), intent(inout)         :: this
+!!$    class(sll_particle_group_base),  intent(inout)              :: particle_group
+!!$    !sll_real64, intent(in)                                      :: thermal_velocity
+!!$    sll_int32,  intent(in)                                      :: random_seed
+!!$
+!!$
+!!$    !print*, this%densityx1(0.0)
+!!$    
+!!$
+!!$  end subroutine draw_product_density_2x2v
 
-
-    !print*, this%densityx1(0.0)
-    
-
-  end subroutine draw_product_density_2x2v
-
-
+  !> Initialization to initialize a 2x2v particle group. Maxwellian distribution of V, cosine perturbation along x1, equal weights.
   subroutine sll_particle_initialize_landaux1_2x2v(&
        particle_group, &
        landau_param, &
@@ -59,11 +47,11 @@ contains
        thermal_velocity, &
        rnd_seed)
     class(sll_particle_group_base),  intent(inout)              :: particle_group
-    sll_real64, intent(in)                                      :: landau_param(2)
-    sll_real64, intent(in)                                      :: xmin(2)
-    sll_real64, intent(in)                                      :: Lx(2)
-    sll_real64, intent(in)                                      :: thermal_velocity(2)
-    sll_int32,  intent(in)                                      :: rnd_seed(:)
+    sll_real64, intent(in)                                      :: landau_param(2) !< parameter defining the perturbation: landau_param(1)*cos(landau_param(2)*x1)
+    sll_real64, intent(in)                                      :: xmin(2) !< lower bound of the domain
+    sll_real64, intent(in)                                      :: Lx(2) !< length of the domain.
+    sll_real64, intent(in)                                      :: thermal_velocity(2) !< Value of the thermal velocity along each dimension.
+    sll_int32,  intent(in)                                      :: rnd_seed(:) !< Random seed.
 
     sll_real64                                                  :: x(3),v(3)
     sll_real64                                                  :: rnd_no
@@ -73,8 +61,10 @@ contains
     x = 0.0_f64
     v = 0.0_f64
 
+    ! Set random seed.
     call random_seed (put=rnd_seed)
 
+    ! Loop over particles to set coordinates and weights.
     i_part = 0
     do while( i_part < particle_group%n_particles)
        ! Rejection sampling for x(1)
@@ -94,6 +84,7 @@ contains
 
           call particle_group%set_x(i_part, x)
           call particle_group%set_v(i_part, v)
+          ! Set weights.
           call particle_group%set_weight(i_part, &
                1.0_f64/real(particle_group%n_total_particles, f64))
        end if
--- src/particle_methods/particle_pushers/sll_operator_splitting_pic_2x2v_vp.F90
+++ src/particle_methods/particle_pushers/sll_operator_splitting_pic_2x2v_vp.F90
@@ -1,3 +1,7 @@
+!> @ingroup particle_methods
+!> @author Katharina Kormann
+!> @brief Particle pusher based on operator splitting for 2x2v Vlasov-Poisson.
+!> @details MPI parallelization by domain cloning. Periodic boundaries.
 module sll_m_operator_splitting_pic_2x2v_vp
 #include "sll_working_precision.h"
 #include "sll_memory.h"
@@ -15,29 +19,30 @@ module sll_m_operator_splitting_pic_2x2v_vp
 
   implicit none
 
+  !> Operator splitting type fo
   type, extends(operator_splitting) :: sll_operator_splitting_pic_2x2v_vp
-     class(poisson_2d_fft_solver), pointer :: poisson_solver
-     class(sll_kernel_smoother_base), pointer :: kernel_smoother
-     class(sll_particle_group_base), pointer :: particle_group
+     class(poisson_2d_fft_solver), pointer    :: poisson_solver      !< Poisson solver (TODO: Use a base class here)
+     class(sll_kernel_smoother_base), pointer :: kernel_smoother  !< Kernel smoother
+     class(sll_particle_group_base), pointer  :: particle_group    !< Particle group
 
-     sll_real64, allocatable ::  rho_dofs(:)
-     sll_real64, allocatable ::  rho_dofs_local(:)
-     !sll_real64, allocatable :: j1_dofs(:)
-     !sll_real64, allocatable :: j2_dofs(:)
-     sll_real64, allocatable :: rho_2d(:,:)
-     sll_real64, allocatable :: efield1(:,:)
-     sll_real64, allocatable :: efield2(:,:)
-     sll_real64, allocatable :: efield(:,:)
+     sll_real64, allocatable :: rho_dofs(:)                      !< Degrees of freedom for kernel respresentation of the charge density. 
+     sll_real64, allocatable :: rho_dofs_local(:)                !< Processor local part of \a rho_dofs 
+     sll_real64, allocatable :: rho_2d(:,:)                      !< 2d representation of \a rho_dofs for use in Poisson solver.
+     sll_real64, allocatable :: efield1(:,:) !< 2d representation of electric field, first component for use in Poisson solver. 
+     sll_real64, allocatable :: efield2(:,:) !< 2d representation of electric field, second component for use in Poisson solver. 
+     sll_real64, allocatable :: efield(:,:) !< Electric field at particle positions.
 
    contains
-     procedure :: operatorT => advection_x_pic_2x2v_vp
-     procedure :: operatorV => advection_v_pic_2x2v_vp
+     procedure :: operatorT => advection_x_pic_2x2v_vp  !< Operator for x advection
+     procedure :: operatorV => advection_v_pic_2x2v_vp  !< Operator for v advection
 
      procedure :: initialize => initialize_operator_splitting_pic_2x2v
   end type sll_operator_splitting_pic_2x2v_vp
 
 contains
 
+  !---------------------------------------------------------------------------!
+  !> Push x 
   subroutine advection_x_pic_2x2v_vp(this, dt)
     class(sll_operator_splitting_pic_2x2v_vp), intent(inout) :: this !< time splitting object 
     sll_real64, intent(in) :: dt   !< time step
@@ -51,13 +56,14 @@ contains
        x_new = this%particle_group%get_x(i_part) + dt * this%particle_group%get_v(i_part) 
        call this%particle_group%set_x(i_part, x_new)
     end do
-    !call this%particle_group%update_x(dt * this%particle_group%get_all_v)
     
 
   end subroutine advection_x_pic_2x2v_vp
-
+  
+  !---------------------------------------------------------------------------!
+  ! Push v
   subroutine advection_v_pic_2x2v_vp(this, dt)
-    class(sll_operator_splitting_pic_2x2v_vp), intent(inout) :: this !< object 
+    class(sll_operator_splitting_pic_2x2v_vp), intent(inout) :: this !< time splitting object 
     sll_real64, intent(in) :: dt   !< time step
 
     !local variables
@@ -71,15 +77,12 @@ contains
     call this%kernel_smoother%accumulate_rho_from_klimontovich(this%particle_group, &
          this%rho_dofs_local)
 
-    !this%rho_dofs = this%rho_dofs_local
+    ! MPI to sum up contributions from each processor
     this%rho_dofs = 0.0_f64
     call sll_collective_allreduce( sll_world_collective, this%rho_dofs_local, &
          this%kernel_smoother%n_dofs, MPI_SUM, this%rho_dofs)
-    !call this%kernel_smoother%accumulate_j_from_klimontovich(this%particle_group, this%j1_dofs, 1)
-    !call this%kernel_smoother%accumulate_j_from_klimontovich(this%particle_group, this%j2_dofs, 2)
 
-    !write(30,*) this%rho_dofs
-
+    ! Bring to right shape for Poisson solver
     this%rho_2d(1:this%kernel_smoother%n_grid(1),1:this%kernel_smoother%n_grid(2)) = &
          -1.0_f64 +&
          reshape(this%rho_dofs, this%kernel_smoother%n_grid(1:2) )
@@ -87,19 +90,17 @@ contains
     this%rho_2d(:,this%kernel_smoother%n_grid(2)+1) = this%rho_2d(:,1)
     ! Solve Poisson problem
     call this%poisson_solver%compute_E_from_rho(this%efield1, this%efield2, this%rho_2d)
-    ! 
-    !print*, this%rho_dofs
-    !write(31,*) this%efield2
 
+    ! Evaluate efield1 at particle positions
     this%rho_dofs = reshape(this%efield1(1:this%kernel_smoother%n_grid(1),1:this%kernel_smoother%n_grid(2)), [this%kernel_smoother%n_dofs])
     call this%kernel_smoother%evaluate_kernel_function(this%particle_group, &
          this%rho_dofs, this%efield(:,1))
-
+    
+    ! Evaluate efield2 at particle positions
     this%rho_dofs = reshape(this%efield2(1:this%kernel_smoother%n_grid(1),1:this%kernel_smoother%n_grid(2)), [this%kernel_smoother%n_dofs])
     call this%kernel_smoother%evaluate_kernel_function(this%particle_group, &
          this%rho_dofs, this%efield(:,2))
 
-    !write(32,*) this%efield
 
     ! V_new = V_old + dt * E
     do i_part=1,this%particle_group%n_particles
@@ -107,11 +108,11 @@ contains
        v_new(1:2) = v_new(1:2) + dt * this%efield(i_part,:) 
        call this%particle_group%set_v(i_part, v_new)
     end do
-    !call this%particle_group%update_v(rhs)
     
 
   end subroutine advection_v_pic_2x2v_vp
-
+  
+  !---------------------------------------------------------------------------!
   subroutine initialize_operator_splitting_pic_2x2v(this, poisson_solver, kernel_smoother, particle_group)
     class(sll_operator_splitting_pic_2x2v_vp), intent(out) :: this !< object 
     class(poisson_2d_fft_solver),pointer, intent(in) :: poisson_solver
@@ -126,20 +127,21 @@ contains
     this%particle_group => particle_group
 
     SLL_ALLOCATE(this%rho_dofs(this%kernel_smoother%n_dofs), ierr)
-    SLL_ALLOCATE(this%rho_2d(this%kernel_smoother%n_grid(1), this%kernel_smoother%n_grid(2)), ierr)
-    SLL_ALLOCATE(this%efield1(this%kernel_smoother%n_grid(1), this%kernel_smoother%n_grid(2)), ierr)   
-    SLL_ALLOCATE(this%efield2(this%kernel_smoother%n_grid(1), this%kernel_smoother%n_grid(2)), ierr)
+    SLL_ALLOCATE(this%rho_2d(this%kernel_smoother%n_grid(1)+1, this%kernel_smoother%n_grid(2)+1), ierr)
+    SLL_ALLOCATE(this%efield1(this%kernel_smoother%n_grid(1)+1, this%kernel_smoother%n_grid(2)+1), ierr)   
+    SLL_ALLOCATE(this%efield2(this%kernel_smoother%n_grid(1)+1, this%kernel_smoother%n_grid(2)+1), ierr)
     SLL_ALLOCATE(this%efield(this%particle_group%n_particles, 2), ierr)
 
   end subroutine initialize_operator_splitting_pic_2x2v
 
 
-
+  !---------------------------------------------------------------------------!
+  !> Constructor.
   function sll_new_splitting_pic_2x2v_vp(poisson_solver, kernel_smoother, particle_group) result(this)
-    class(sll_operator_splitting_pic_2x2v_vp), pointer :: this !< object 
-    class(poisson_2d_fft_solver),pointer, intent(in) :: poisson_solver
-    class(sll_kernel_smoother_base),pointer, intent(in) :: kernel_smoother
-    class(sll_particle_group_base),pointer, intent(in) :: particle_group
+    class(sll_operator_splitting_pic_2x2v_vp), pointer :: this !< time splitting object 
+    class(poisson_2d_fft_solver),pointer, intent(in) :: poisson_solver !< Poisson solver
+    class(sll_kernel_smoother_base),pointer, intent(in) :: kernel_smoother !< Kernel smoother
+    class(sll_particle_group_base),pointer, intent(in) :: particle_group !< Particle group
 
     !local variables
     sll_int32 :: ierr
