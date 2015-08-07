70dbd766dcd367fb0ed20a603b456092581dd924
Katharina Kormann <katharina.kormann@tum.de>
2015-08-07 12:17:35 +0200

    Particle initializer for 1d2v added.

--- src/particle_methods/particle_initializers/sll_particle_initializers.F90
+++ src/particle_methods/particle_initializers/sll_particle_initializers.F90
@@ -95,4 +95,58 @@ contains
   end subroutine sll_particle_initialize_landaux1_2x2v
 
 
+!> Initialization to initialize a 1d2v particle group. Maxwellian distribution of V, cosine perturbation along x, equal weights.
+  subroutine sll_particle_initialize_landau_1d2v(&
+       particle_group, &
+       landau_param, &
+       xmin, &
+       Lx, &
+       thermal_velocity, &
+       rnd_seed)
+    class(sll_particle_group_base),  intent(inout)              :: particle_group
+    sll_real64, intent(in)                                      :: landau_param(2) !< parameter defining the perturbation: landau_param(1)*cos(landau_param(2)*x1)
+    sll_real64, intent(in)                                      :: xmin !< lower bound of the domain
+    sll_real64, intent(in)                                      :: Lx !< length of the domain.
+    sll_real64, intent(in)                                      :: thermal_velocity(2) !< Value of the thermal velocity along each dimension.
+    sll_int32,  intent(in)                                      :: rnd_seed(:) !< Random seed.
+
+    sll_real64                                                  :: x(3),v(3)
+    sll_real64                                                  :: rnd_no
+    sll_int32                                                   :: i_part
+    
+
+    x = 0.0_f64
+    v = 0.0_f64
+
+    ! Set random seed.
+    call random_seed (put=rnd_seed)
+
+    ! Loop over particles to set coordinates and weights.
+    i_part = 0
+    do while( i_part < particle_group%n_particles)
+       ! Rejection sampling for x(1)
+       call random_number(rnd_no)
+       x(1) = Lx * rnd_no + xmin
+       call random_number(rnd_no)
+       rnd_no = (1.0_f64 + landau_param(1)) * rnd_no
+       if ( (1.0_f64 + landau_param(1) * cos(landau_param(2)*x(1))) >= rnd_no ) then  
+          ! Draw x(2) from uniform distribution
+          i_part = i_part + 1
+
+          ! Draw velocities from 2D Gauss distribution
+          call gaussian_deviate_2D(v(1:2))
+          v(1:2) = v(1:2) * thermal_velocity
+
+          call particle_group%set_x(i_part, x)
+          call particle_group%set_v(i_part, v)
+          ! Set weights.
+          call particle_group%set_weight(i_part, &
+               1.0_f64/real(particle_group%n_total_particles, f64))
+       end if
+    end do
+
+    
+
+  end subroutine sll_particle_initialize_landau_1d2v
+
 end module sll_m_particle_initializer
