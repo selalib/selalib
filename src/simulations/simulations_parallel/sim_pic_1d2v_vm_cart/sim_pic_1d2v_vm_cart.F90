3d15fb555b7e928be939dc1f02352a0271029fe0
Katharina Kormann <katharina.kormann@tum.de>
2015-08-08 22:11:17 +0200

    Change 1d2v simulation to FEM with splines for Maxwell and kernel smoother. NOT FINISHED YET.

--- src/simulations/simulations_parallel/sim_pic_1d2v_vm_cart/sim_pic_1d2v_vm_cart.F90
+++ src/simulations/simulations_parallel/sim_pic_1d2v_vm_cart/sim_pic_1d2v_vm_cart.F90
@@ -1,6 +1,5 @@
-! Simulation of 1d2v Vlasov-Maxwell with simple PIC method, periodic boundary conditions, Weibel instability.
+! Simulation of 1d2v Vlasov-Maxwell with simple PIC method, periodic boundary conditions, Weibel instability. FEM with splines, degree 3 for B and 2 for E
 
-! TODO: Can be made more general
 
 module sll_m_sim_pic_1d2v_vm_cart
 
@@ -13,15 +12,18 @@ module sll_m_sim_pic_1d2v_vm_cart
   use sll_collective
   use sll_simulation_base
   use sll_cartesian_meshes
-  use sll_m_pic_maxwell_base
-  use sll_m_pic_maxwell_1d_fourier_modes
   use sll_module_pic_base
   use sll_m_particle_initializer
   use sll_m_particle_group_1d2v
   use sll_m_operator_splitting_pic_1d2v_vm
   use sll_ascii_io
+  
+  use sll_m_kernel_smoother_base
+  use sll_m_kernel_smoother_spline_1d
+  use sll_m_maxwell_1d_base
+!  use sll_maxwell_1d_fem
 
-  type, extends(sll_simulation_base_class) :: sll_sim_pic_1d2v_vm_cart
+    type, extends(sll_simulation_base_class) :: sll_sim_pic_1d2v_vm_cart
 
      ! Abstract particle group
      class(sll_particle_group_base), pointer :: particle_group
@@ -31,11 +33,16 @@ module sll_m_sim_pic_1d2v_vm_cart
      ! Cartesian mesh
      type(sll_cartesian_mesh_1d), pointer    :: mesh 
 
-     ! PIC Maxwell solver with kernel smoothing 
+     ! Maxwell solver 
      ! Abstract 
-     class(sll_pic_maxwell_base), pointer :: maxwell_pic
+     class(sll_maxwell_1d_base), pointer :: maxwell_solver
      ! Specific
-     class(sll_pic_maxwell_1d_fourier_modes), pointer :: specific_maxwell_pic
+     class(sll_maxwell_1d_fem), pointer :: specific_maxwell_solver
+
+     ! Abstract kernel smoother
+     class(sll_kernel_smoother_base), pointer :: kernel_smoother
+     ! Specific kernel smoother
+     class(sll_kernel_smoother_spline_1d), pointer :: specific_kernel_smoother
 
 
      ! Specific operator splitting
@@ -43,6 +50,7 @@ module sll_m_sim_pic_1d2v_vm_cart
      
      ! Physical parameters
      sll_real64 :: landau_param(2) ! (1+landau_param(1)*cos(landau_param(2)*x1) 
+     sll_real64 :: beta
      sll_real64 :: thermal_velocity(2) ! 
      sll_real64 :: domain(3) ! x_min, x_max, Lx
      
@@ -52,6 +60,7 @@ module sll_m_sim_pic_1d2v_vm_cart
      sll_int32  :: n_particles
      sll_int32  :: n_total_particles
      sll_int32  :: degree_smoother
+     sll_int32  :: n_gcells
 
      
      ! Parameters for MPI
@@ -77,19 +86,19 @@ contains
     character(len=*), intent(in)                                :: filename
 
     sll_int32   :: n_time_steps
-    sll_real64  :: delta_t, alpha, n_mode, thermal_v, T_r
+    sll_real64  :: delta_t, alpha, n_mode, thermal_v, T_r, beta
     sll_int32   :: ng_x
     sll_real64  :: x1_min, x1_max
-    sll_int32   :: n_particles, degree_smoother
+    sll_int32   :: n_particles!, degree_smoother
 
     sll_int32, parameter :: input_file = 99
     
 
-    namelist /sim_params/         delta_t, n_time_steps, alpha, n_mode, thermal_v, T_r
+    namelist /sim_params/         delta_t, n_time_steps, alpha, n_mode, thermal_v, T_r, beta
     
     namelist /grid_dims/          ng_x, x1_min, x1_max
 
-    namelist /pic_params/         n_particles, degree_smoother
+    namelist /pic_params/         n_particles!, degree_smoother
 
     ! Read parameters from file
     open(unit = input_file, file=trim(filename), IOStat=io_stat)
@@ -110,14 +119,16 @@ contains
     sim%n_time_steps = n_time_steps
     sim%landau_param = [alpha, n_mode*2.0_f64 * sll_pi/(x1_max - x1_min)]
     sim%thermal_velocity = [thermal_v, thermal_v* sqrt(T_r)]
+    sim%beta = beta
 
+    sim%n_gcells = ng_x1
     sim%mesh => new_cartesian_mesh_1d( ng_x1, &
          x1_min, x1_max)
     sim%domain = [x1_min, x1_max, x1_max - x1_min ]
 
     sim%n_particles = n_particles/sim%world_size
     sim%n_total_particles = sim%n_particles * sim%world_size
-    sim%degree_smoother = degree_smoother
+    sim%degree_smoother = 3!degree_smoother
     
 
   end subroutine init_pic_1d2v_vm
@@ -127,12 +138,15 @@ contains
   subroutine run_pic_1d2v_vm (sim)
     class(sll_sim_pic_1d2v_vm_cart), intent(inout) :: sim
 
-    ! Loop variables
+    ! Local variables
     sll_int32, allocatable :: rnd_seed(:)
     sll_int32 :: j, ierr
     sll_int32 :: rnd_seed_size
     sll_real64 :: eenergy
     sll_int32 :: th_diag_id
+
+     ! Kernel smoother for rho (only used for initialization
+     class(sll_kernel_smoother_spline_1d), pointer :: kernel_smoother_rho
 
 
     if (sim%rank == 0) then
@@ -143,8 +157,17 @@ contains
      sim%specific_particle_group => sll_new_particle_group_1d2v(sim%n_particles, &
          sim%n_total_particles ,1.0_f64, 1.0_f64)
     
-    !print*, 'size', size(sim%specific_particle_group%particle_array,1)
     sim%particle_group => sim%specific_particle_group
+
+    ! Initialize the field solver
+    sim%specific_maxwell_solver => sll_new_maxwell_1d_fem()
+    sim%maxwell_solver => sim%specific_maxwell_solver
+
+    ! Initialize kernel smoother    
+    sim%specific_kernel_smoother => sll_new_smoother_spline_1d(&
+         sim%domain, [sim%n_gcells], &
+         sim%n_particles, sim%degree_smoother) 
+    sim%kernel_smoother => sim%specific_kernel_smoother
     
     ! Set the seed for the random initialization
     call random_seed(size=rnd_seed_size)
@@ -153,11 +176,6 @@ contains
        rnd_seed(j) = (-1)**j*(100 + 15*j)*(2*sim%rank + 1)
     end do
 
-    ! Initialize the field solver
-    sim%specific_maxwell_pic => sll_new_pic_maxwell_1d_fourier_modes(&
-         sim%degree_smoother, sim%domain(3), sim%n_particles)
-    sim%maxwell_pic => sim%specific_maxwell_pic
-
     ! Initialize position and velocity of the particles.
     call sll_particle_initialize_landau_1d2v (sim%particle_group, sim%landau_param, &
          sim%domain(1) , &
@@ -165,10 +183,37 @@ contains
          sim%thermal_velocity, rnd_seed)
 
     ! Initialize the time-splitting propagator
-    sim%propagator => sll_new_splitting_pic_1d2v_vm(sim%maxwell_pic, &
-         sim%particle_group)
+    sim%propagator => sll_new_splitting_pic_1d2v_vm(sim%maxwell_solver, &
+         sim%kernel_smoother, sim%particle_group, &
+         sim%domain(1), sim%domain(3))
 
+    ! Set the initial fields
+    ! Efield 1 by Poisson
 
+    kernel_smoother_rho => sll_new_smoother_spline_1d(sim%domain, [sim%n_gcells], &
+         sim%n_particles, sim%degree_smoother-1) 
+    call kernel_smoother_rho%compute_shape_factors(sim%particle_group)
+    sim%propagator%j_dofs_local = 0.0_f64
+    call kernel_smoother_rho%accumulate_rho_from_klimontovich(sim%particle_group, &
+         sim%propagator%j_dofs_local(:,1))
+    ! MPI to sum up contributions from each processor
+    sim%propagator%j_dofs = 0.0_f64
+    call sll_collective_allreduce( sll_world_collective, sim%propagator%j_dofs_local(:,1), &
+         sim%n_gcells, MPI_SUM, sim%propagator%j_dofs(:,1))
+    ! Solve Poisson problem
+    call sim%maxwell_solver%compute_E_from_rho(sim%propagator%efield_dofs(:,1),&
+         sim%propagator%j_dofs(:,1))
+
+    ! Efield 2 to zero
+    sim%propagator%efield_dofs(:,2) = 0.0_f64
+    ! Bfield = beta*cos(kx): Use b = M{-1}
+    ! TODO
+    sim%propagator%bfield_dofs = 0.0_f64
+
+    ! End field initialization
+
+    ! Diagnostics
+    ! TODO : Output needs to be changes.
     write(th_diag_id,'(f12.5,2g20.12,2g20.12,2g20.12)' ) &
          0.0_f64,  sim%propagator%efield_dofs(2,:), &
          sim%propagator%bfield(2)
@@ -186,8 +231,9 @@ contains
           eenergy = sum(sim%propagator%efield_dofs(:,1)**2)*&
                sim%mesh%delta_eta
           write(th_diag_id,'(f12.5,2g20.12,2g20.12,2g20.12)' ) &
-               real(j,f64)*sim%delta_t,  sim%propagator%efield_dofs(2,:), &
-               sim%propagator%bfield(2)
+               real(j,f64)*sim%delta_t,  sim%propagator%efield_dofs(2,:)**2 +&
+               sim%propagator%efield_dofs(2+sim%degree_smoother,:)**2, &
+               sim%propagator%bfield(2)**2 + sim%propagator%bfield(2+sim%degree_smoother)**2
        end if
     end do
     
