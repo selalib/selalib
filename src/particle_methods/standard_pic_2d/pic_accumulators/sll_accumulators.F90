35587b3dd32be8cac1259ccf11eb503010959d29
Sever Hirstoaga <sever.hirstoaga@inria.fr>
2015-10-07 10:29:01 +0200

    clean and move the pic directories to particle_methods

--- src/particle_methods/standard_pic_2d/pic_accumulators/sll_accumulators.F90
+++ src/particle_methods/standard_pic_2d/pic_accumulators/sll_accumulators.F90
@@ -23,6 +23,33 @@ module sll_accumulators
   use sll_cartesian_meshes
   implicit none
   
+  ! The idea of having this data structure is to precompute the values of
+  ! the electric field, store them redundantly on a cell-based structure and
+  ! reduce the memory thrashing that would inevitably occur if we were to
+  ! compute the values of the electric field from the potential repeatedly.
+  ! These costs ought to be amortized if the number of points to be advected,
+  ! or particles to be advanced (per cell) is relatively large compared with
+  ! the cost of filling out this data structure.
+  !
+  ! The values of the electric field within the structure in 2D are named 
+  ! like this:
+  !                NW (north-west)       NE (north-east)
+  !                  +--------------------+
+  !                  |                    |
+  !                  |                    |
+  !                  |                    |
+  !                  |                    |
+  !                  |      cell(i)       |
+  !                  |                    |
+  !                  |                    |
+  !                  |                    |
+  !                  |                    |
+  !                  +--------------------+
+  !                SW (south-west)       SE (south-east)
+  !
+  ! Each corner represents a point with two electric field components defined:
+  ! ex and ey (x-component and y-component of the field respectively).
+  
   type charge_accumulator_cell_2d
      sll_real64 :: q_sw
      sll_real64 :: q_se
@@ -431,6 +458,15 @@ contains
 !!$  end subroutine sll_accumulate_charge_on_cell
 
 
+!!$  EDWIN CG
+!!$  ! The following is a pseudo-accumulator. It is written for use while some
+!!$  ! problems with the parallel loading of the previous accumulator are resolved.
+!!$  ! This one is only useful to precompute the values of the electric field in
+!!$  ! a structure that can have the same layout as the potential array.
+!!$  type :: efield_2d_point
+!!$     sll_real64 :: ex
+!!$     sll_real64 :: ey
+!!$  end type efield_2d_point
 
 
 
