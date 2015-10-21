3bcf456d4372f48bb38604ac9a60de679efde4ce
Pierre Navaro <pierre.navaro@univ-rennes1.fr>
2015-10-21 09:57:21 +0200

    Fix package build

--- package/selalib.h
+++ package/selalib.h
@@ -24,7 +24,6 @@
 #include "sll_working_precision.h"
 #include "sll_utilities.h"
 #include "sll_constants.h"
-#include "sll_file_io.h"
 #include "sll_fft.h"
 #include "sll_interpolators.h"
 #include "sll_splines.h"
@@ -32,6 +31,7 @@
 #include "sll_poisson_solvers.h"
 #include "sll_meshes.h"
 #include "sll_coordinate_transformations.h"
+use sll_m_gnuplot
 
 
 #endif
