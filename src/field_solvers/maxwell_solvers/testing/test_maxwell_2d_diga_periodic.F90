8d8bad375cfbad2d425b94e985c578214d0f0d66
Pierre Navaro <pierre.navaro@inria.fr>
2015-10-28 15:43:13 +0100

    remove some warnings in tests
    Will stop here :-)

--- src/field_solvers/maxwell_solvers/testing/test_maxwell_2d_diga_periodic.F90
+++ src/field_solvers/maxwell_solvers/testing/test_maxwell_2d_diga_periodic.F90
@@ -66,7 +66,7 @@ type(sll_dg_field_2d), pointer :: exact
 sll_real64  :: time
 sll_int32   :: istep
 sll_real64  :: dt
-sll_real64  :: cfl = 0.1
+sll_real64  :: cfl = 0.1_f64
 sll_real64  :: error
 !init functions
 sll_real64, external :: sol_bz, sol_ex, sol_ey, linear_x, linear_y
