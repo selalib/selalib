33cfd16c73b435adbbc2d9de52e9d12e6350f29e
Laura Mendoza <lasofivec@gmail.com>
2015-11-05 15:52:10 +0100

    [fekete] added separated test for fekete integration

---
+++ src/quadrature/testing/test_fekete_integration.F90
@@ -0,0 +1,78 @@
+program test_fekete_integration
+
+#include "sll_working_precision.h"
+
+  use sll_m_fekete_integration
+  implicit none
+
+  ! integer :: i,j,n
+  ! sll_int32 :: ierr
+  ! sll_int32 :: degree
+
+  ! sll_real64, dimension(10) :: x, w
+  ! sll_real64, dimension(:,:), allocatable :: d
+  ! sll_real64, dimension(:,:), allocatable :: dlag
+
+  ! character(len=18) :: string
+
+  ! For the fekete quadrature:
+  sll_real64, dimension(2, 3) :: pxy1
+  sll_real64, dimension(2, 3) :: pxy2
+  sll_real64, dimension(:,:), allocatable :: xyw
+  sll_real64 :: app_res
+  sll_int32  :: rule
+  sll_int32  :: num_cells
+  type(sll_hex_mesh_2d), pointer :: mesh
+  sll_real64, dimension(:,:), allocatable     :: knots
+  sll_int32,  dimension(:,:), allocatable     :: LM
+
+  write(*,"(/,a)") "*********************************** "
+  write(*,"(a)") "       FEKETE QUAD TEST       "
+  write(*,"(a)") "*********************************** "
+
+  !Definition of first triangle
+  pxy1(:,1) = (/ 0._f64, 0._f64 /)
+  pxy1(:,2) = (/ 1._f64, 0._f64 /)
+  pxy1(:,3) = (/ 0._f64, 1._f64 /)
+
+  !Definition of first triangle
+  pxy2(:,1) = (/ 1._f64, 0._f64 /)
+  pxy2(:,2) = (/ 1._f64, 1._f64 /)
+  pxy2(:,3) = (/ 0._f64, 1._f64 /)
+
+  rule = 2
+  call fekete_order_num ( rule, n )
+  SLL_ALLOCATE(xyw(1:3, 1:n), ierr)
+
+  write(*,"(a)") " Computing Fekete points and weights on reference triangle "
+  write(*,"(/,a)") "           x                   y                    w"
+  xyw = fekete_points_and_weights(pxy1, rule)
+
+  do j = 1, n
+     write(*, string) (xyw(i,j), i = 1, 3)
+  end do
+
+  print *, "sum weights = ", SUM(xyw(3,:))
+  write(*,"(/,a)") " --Test for a constant real function (=1) "
+  write(*,"(a)") "    on the squared domain [0,1]^2 divided on 2 triangles "
+
+  app_res = fekete_integral(one_2D, pxy1) + fekete_integral(one_2D, pxy2)
+
+  write (*,"(a, f20.12, a ,/)") " aprox = ", app_res, " (expected = 1.)"
+
+
+  ! Writing all django files....................................................
+  num_cells = 2
+  degree = 1
+  rule = 1
+  call write_all_django_files(num_cells, degree, rule)
+  print *, ""
+  print *, "*********** wrote all django files ***********"
+  print *, "   - number of cells    : ", num_cells
+  print *, "   - degree of splines  :", degree
+  print *, "   - rule of quadrature :", rule
+  print *, ""
+
+  print*, 'PASSED'
+
+end program test_fekete_integration
