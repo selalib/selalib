d410b72c761491ebabcb1c10fe79a9167e191fcc
Katharina Kormann <katharina.kormann@tum.de>
2015-11-05 15:17:23 +0100

    Update poisson_2d_base for use in with FEM.

--- src/field_solvers/poisson_solvers/sll_m_poisson_2d_base.F90
+++ src/field_solvers/poisson_solvers/sll_m_poisson_2d_base.F90
@@ -6,6 +6,7 @@
 module sll_m_poisson_2d_base
 #include "sll_working_precision.h"
 
+
   implicit none
   
   !> PLEASE ADD DOCUMENTATION
@@ -21,7 +22,25 @@ module sll_m_poisson_2d_base
     procedure(signature_compute_E_from_rho_2d), deferred, pass(poisson) :: &
       compute_E_from_rho
 
+    !> Compute the squarred L_2 for given coefficients
+    procedure(signature_norm_squarred), deferred :: &
+         l2norm_squarred
+    !> Compute the right hand side from a given function
+    procedure(signature_update_dofs_function), deferred :: &
+         compute_rhs_from_function
+
   end type sll_poisson_2d_base
+
+
+  abstract interface
+     !> nd real function
+     function sll_f_function_of_position(x)
+       use sll_m_working_precision 
+       sll_real64             :: sll_f_function_of_position
+       sll_real64, intent(in) :: x(:)
+     end function sll_f_function_of_position
+  end interface
+  
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 
@@ -56,6 +75,31 @@ module sll_m_poisson_2d_base
 
     end subroutine signature_compute_E_from_rho_2d
   end interface
+
+  !---------------------------------------------------------------------------!
+  abstract interface
+     function signature_norm_squarred(poisson, coefs_dofs) result( r )
+       use sll_m_working_precision
+       import sll_poisson_2d_base
+       class( sll_poisson_2d_base) , intent(in)                   :: poisson !< Poisson solver object.
+       sll_real64 , intent(in)                                    :: coefs_dofs(:,:) !< Values of the coefficient vectors for each DoF
+       sll_real64                                     :: r
+     end function signature_norm_squarred
+  end interface
+
+  !---------------------------------------------------------------------------!
+  abstract interface
+     subroutine signature_update_dofs_function(poisson, func, coefs_dofs)
+       use sll_m_working_precision
+       import sll_poisson_2d_base
+       import sll_f_function_of_position
+       class( sll_poisson_2d_base)                    :: poisson !< Maxwell solver object.
+       procedure(sll_f_function_of_position)          :: func !< Function to be projected.
+       sll_real64, intent(out)                        :: coefs_dofs(:) !< Coefficients of the projection.
+     end subroutine signature_update_dofs_function
+  end interface
+  
+
 
 #endif /* DOXYGEN_SHOULD_SKIP_THIS */
 
