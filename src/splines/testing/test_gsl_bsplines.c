004936c6a19f656ca2e9dfc4a21c41570f4299ce
Pierre Navaro <pierre.navaro@univ-rennes1.fr>
2015-11-20 11:24:02 +0100

    Tridiagonal matrix is set

--- src/splines/testing/test_gsl_bsplines.c
+++ src/splines/testing/test_gsl_bsplines.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <gsl/gsl_bspline.h>
 #include <gsl/gsl_spline.h>
+#include <gsl/gsl_errno.h>
 
 /* number of data points to fit */
 #define N        5
@@ -10,8 +11,12 @@
 /* number of fit coefficients */
 #define NCOEFFS  5
 
+
+/* Spline order */
+#define K  3
+
 /* nbreak = ncoeffs + 2 - k */
-#define NBREAK   (NCOEFFS - 2)
+#define NBREAK   (NCOEFFS + 2 - K)
 
 int
 main (void)
@@ -23,10 +28,22 @@ main (void)
   gsl_bspline_workspace *bw;
   gsl_vector *B;
   double xmin=0.0, xmax=2.0;
-  const size_t k = 4;
+  const size_t k = K;
   double xi;
   gsl_vector *x;
   gsl_matrix *A;
+
+  double *a;
+  double *b;
+  double *c;
+  double *r;
+  double *u;
+
+  a = (double *) malloc(n*sizeof(double));
+  b = (double *) malloc(n*sizeof(double));
+  c = (double *) malloc(n*sizeof(double));
+  r = (double *) malloc(n*sizeof(double));
+  u = (double *) malloc(n*sizeof(double));
 
   x = gsl_vector_alloc(n);
   for (i = 0; i < n; ++i)
@@ -34,7 +51,6 @@ main (void)
        
     }
   
-
   /* allocate a cubic bspline workspace */
   bw = gsl_bspline_alloc(k, nbreak);
   B  = gsl_vector_alloc(ncoeffs);
@@ -67,10 +83,10 @@ main (void)
 
       /* fill in row i of X */
       for (j = 0; j < ncoeffs; ++j)
-        {
-          double Bj = gsl_vector_get(B, j);
-          gsl_matrix_set(A, i, j, Bj);
-        }
+      {
+        double Bj = gsl_vector_get(B, j);
+        gsl_matrix_set(A, i, j, Bj);
+      }
 
     }
   for (i = 0; i < n; i++)  
@@ -113,3 +129,82 @@ main (void)
 
 } 
 
+/*
+void tridag(double a[], 
+            double b[], 
+            double c[], 
+            double r[], 
+            double u[], 
+            unsigned long n)
+{
+  unsigned long j; 
+  float bet;
+  double *gam;
+
+  gam = gsl_vector_alloc(n);
+
+  if (b[1] == 0.0) 
+    printf("Error 1 in tridag");
+
+  u[1]=r[1]/(bet=b[1]);
+
+  for (j=2;j<=n;j++) { 
+   gsl_vector_set(gam, j, c[j-1]/bet);
+   bet=b[j]-a[j]*gam[j];
+   if (bet == 0.0) 
+     printf("Error 2 in tridag");
+   u[j]=(r[j]-a[j]*u[j-1])/bet;
+
+  }
+
+  for (j=(n-1);j>=1;j--)
+    u[j] -= gsl_vector_get (gam,j+1)*u[j+1]; 
+
+  gsl_vector_free(gam);
+
+}
+
+void cyclic(double a[], 
+            double b[], 
+            double c[], 
+            double alpha, 
+            double beta, 
+            double r[], 
+            double x[], unsigned long n)
+
+{
+  unsigned long i;
+  double fact,gamma;
+  double *bb;
+  double *u;
+  double *z;
+  
+  if (n <= 2) printf("n too small in cyclic"); 
+  bb = (double *) malloc(n*sizeof(double));
+  u  = (double *) malloc(n*sizeof(double));
+  z  = (double *) malloc(n*sizeof(double));
+  
+  gamma = -b[1]; 
+  bb[1]=b[1]-gamma; 
+  bb[n]=b[n]-alpha*beta/gamma; 
+  for (i=2;i<n;i++) 
+    bb[i]=b[i]; 
+  
+  tridag(a,bb,c,r,x,n); 
+  u[1]=gamma;
+  u[n]=alpha;
+  for (i=2;i<n;i++) 
+    u[i]=0.0; 
+
+  tridag(a,bb,c,u,z,n); 
+
+  fact=(x[1]+beta*x[n]/gamma)/(1.0+z[1]+beta*z[n]/gamma);
+
+  for (i=1;i<=n;i++) 
+    x[i] -= fact*z[i]; 
+  
+  gsl_vector_free(z); 
+  gsl_vector_free(u); 
+  gsl_vector_free(bb);
+}
+*/
