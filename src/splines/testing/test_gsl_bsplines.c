a96ffd0a41fda16f168e038d3cc675a96a03bd67
Pierre Navaro <navaro@math.cnrs.fr>
2015-11-20 14:01:11 +0100

    gsl routine is finished

--- src/splines/testing/test_gsl_bsplines.c
+++ src/splines/testing/test_gsl_bsplines.c
@@ -4,12 +4,13 @@
 #include <gsl/gsl_bspline.h>
 #include <gsl/gsl_spline.h>
 #include <gsl/gsl_errno.h>
+#include <gsl/gsl_linalg.h>
 
 /* number of data points to fit */
-#define N        5
+#define N        6
 
 /* number of fit coefficients */
-#define NCOEFFS  5
+#define NCOEFFS  6
 
 
 /* Spline order */
@@ -18,118 +19,47 @@
 /* nbreak = ncoeffs + 2 - k */
 #define NBREAK   (NCOEFFS + 2 - K)
 
-int
-main (void)
+
+void gsl_solve( gsl_matrix *A,  const size_t n,
+                double xmax, double xmin )
 {
-  const size_t n = N;
-  const size_t ncoeffs = NCOEFFS;
-  const size_t nbreak = NBREAK;
-  size_t i, j;
-  gsl_bspline_workspace *bw;
-  gsl_vector *B;
-  double xmin=0.0, xmax=2.0;
-  const size_t k = K;
-  double xi;
-  gsl_vector *x;
-  gsl_matrix *A;
 
-  double *a;
-  double *b;
-  double *c;
-  double *r;
-  double *u;
+  double dx = (xmax-xmin)/(n-1);
+  double xc = (xmax+xmin)*0.5;
 
-  a = (double *) malloc(n*sizeof(double));
-  b = (double *) malloc(n*sizeof(double));
-  c = (double *) malloc(n*sizeof(double));
-  r = (double *) malloc(n*sizeof(double));
-  u = (double *) malloc(n*sizeof(double));
-
-  x = gsl_vector_alloc(n);
-  for (i = 0; i < n; ++i)
-    {
-       
-    }
-  
-  /* allocate a cubic bspline workspace */
-  bw = gsl_bspline_alloc(k, nbreak);
-  B  = gsl_vector_alloc(ncoeffs);
-  A  = gsl_matrix_alloc(n, ncoeffs);
-
-  /* gsl_bspline_knots(breakpts, bw) 
-  Compute the knots from the given breakpoints:
-   knots(1:k) = breakpts(1)
-   knots(k+1:k+l-1) = breakpts(i), i = 2 .. l
-   knots(n+1:n+k) = breakpts(l + 1) */
-
-  /* use uniform breakpoints on [xmin, xmax] */
-  gsl_bspline_knots_uniform(xmin, xmax, bw);
-  //knots(1:k) = xmin
-  //knots(k+1:k+l-1) = xmin + i*delta, i = 1 .. l - 1
-  //knots(n+1:n+k) = xmax
-
-  /* construct the fit matrix X */
-  for (i = 0; i < n; ++i)
-    {
-      double xi = ((xmax-xmin) / (N - 1)) * i;
-      double ti = gsl_vector_get(bw->knots, i);
-
-      gsl_vector_set(x, i, xi);
-
-      /* compute B_j(xi) for all j */
-      gsl_bspline_eval(xi, B, bw);
-
-      printf("%f %f\n", xi, ti);
-
-      /* fill in row i of X */
-      for (j = 0; j < ncoeffs; ++j)
-      {
-        double Bj = gsl_vector_get(B, j);
-        gsl_matrix_set(A, i, j, Bj);
-      }
-
-    }
-  for (i = 0; i < n; i++)  
+  for (int i = 0; i < n; i++)  
     { 
-      for (j = 0; j < ncoeffs; j++)
-        printf ("%6.3f", gsl_matrix_get (A, i, j));
+      for (int j = 0; j < n; j++)
+        {
+          printf ("%6.3f", gsl_matrix_get (A, i, j));
+        }
       printf("\n");
     }
 
-/*
-  gsl_permutation * p = gsl_permutation_alloc (ncoeffs);
+  gsl_vector *d; d = gsl_vector_alloc(n);
+  gsl_vector *e; e = gsl_vector_alloc(n-1);
+  gsl_vector *f; f = gsl_vector_alloc(n-1);
+  gsl_vector *b; b = gsl_vector_alloc(n);
+  gsl_vector *x; x = gsl_vector_alloc(n);
 
-  gsl_linalg_LU_decomp (A, p, &s);
+  for (int i = 0; i < n; i++)  
+    { 
+      gsl_vector_set(d, i, gsl_matrix_get(A,i,i));
+      if (i>0) 
+        gsl_vector_set(f, i-1, gsl_matrix_get(A,i,i-1));
+      if (i<n-1) 
+        gsl_vector_set(e, i, gsl_matrix_get(A,i,i+1));
+    }
 
-  gsl_linalg_LU_solve (A, p, b, x);
+  for (int i=0; i<n; i++) 
+    gsl_vector_set(b,i,fabs(i*dx-xc));
 
-  printf ("x = \n");
-  gsl_vector_fprintf (stdout, x, "%g");
+  gsl_linalg_solve_tridiag ( d, e, f, b, x);
+  for (int i=0; i<n; i++)
+    printf ("x[%d] = %f \n", i, gsl_vector_get(x,i));
+}
 
-  gsl_permutation_free (p);
-  gsl_vector_free (x);
-*/
 
-  //gsl_vector *d; d = gsl_vector_alloc(ncoeffs);
-  //gsl_vector *e; e = gsl_vector_alloc(ncoeffs);
-  //gsl_vector *f; f = gsl_vector_alloc(ncoeffs);
-  //gsl_vector *b; b = gsl_vector_alloc(ncoeffs);
-  //gsl_vector *x; x = gsl_vector_alloc(ncoeffs);
-
-  // A = ( d_0 e_0  0  f_3 )
-  //     ( f_0 d_1 e_1  0  )
-  //     (  0  f_1 d_2 e_2 )
-  //     ( e_3  0  f_2 d_3 )
- 
-  // gsl_linalg_solve_cyc_tridiag (d, e, f, b, x)
-
-  gsl_bspline_free(bw);
-  gsl_vector_free(B);
-  return 0;
-
-} 
-
-/*
 void tridag(double a[], 
             double b[], 
             double c[], 
@@ -141,29 +71,30 @@ void tridag(double a[],
   float bet;
   double *gam;
 
-  gam = gsl_vector_alloc(n);
+  gam = (double *) malloc(n*sizeof(double));
 
   if (b[1] == 0.0) 
     printf("Error 1 in tridag");
 
   u[1]=r[1]/(bet=b[1]);
 
-  for (j=2;j<=n;j++) { 
-   gsl_vector_set(gam, j, c[j-1]/bet);
-   bet=b[j]-a[j]*gam[j];
-   if (bet == 0.0) 
-     printf("Error 2 in tridag");
-   u[j]=(r[j]-a[j]*u[j-1])/bet;
-
+  for (j=2;j<=n;j++) 
+  { 
+    gam[j]= c[j-1]/bet;
+    bet=b[j]-a[j]*gam[j];
+    if (bet == 0.0) 
+      printf("Error 2 in tridag");
+    u[j]=(r[j]-a[j]*u[j-1])/bet;
   }
 
   for (j=(n-1);j>=1;j--)
-    u[j] -= gsl_vector_get (gam,j+1)*u[j+1]; 
+    u[j] -= gam[j+1]*u[j+1]; 
 
-  gsl_vector_free(gam);
+  free(gam);
 
 }
 
+/*
 void cyclic(double a[], 
             double b[], 
             double c[], 
@@ -208,3 +139,90 @@ void cyclic(double a[],
   gsl_vector_free(bb);
 }
 */
+
+int
+main (void)
+{
+  const size_t n = N;
+  const size_t ncoeffs = NCOEFFS;
+  const size_t nbreak = NBREAK;
+  size_t i, j;
+  gsl_bspline_workspace *bw;
+  gsl_vector *B;
+  double xmin=0.0, xmax=2.0;
+  const size_t k = K;
+  gsl_vector *tau;
+  gsl_matrix *A;
+
+  double dx = (xmax-xmin)/(n-1);
+  double xc = (xmax+xmin)*0.5;
+  //double *a;
+  //double *b;
+  //double *c;
+  //double *r;
+  //double *u;
+
+  //a = (double *) malloc(n*sizeof(double));
+  //b = (double *) malloc(n*sizeof(double));
+  //c = (double *) malloc(n*sizeof(double));
+  //r = (double *) malloc(n*sizeof(double));
+  //u = (double *) malloc(n*sizeof(double));
+
+  tau = gsl_vector_alloc(n);
+  
+  /* allocate a cubic bspline workspace */
+  bw = gsl_bspline_alloc(k, nbreak);
+  B  = gsl_vector_alloc(ncoeffs);
+  A  = gsl_matrix_alloc(n, ncoeffs);
+
+  /* gsl_bspline_knots(breakpts, bw) 
+  Compute the knots from the given breakpoints:
+   knots(1:k) = breakpts(1)
+   knots(k+1:k+l-1) = breakpts(i), i = 2 .. l
+   knots(n+1:n+k) = breakpts(l + 1) */
+
+  /* use uniform breakpoints on [xmin, xmax] */
+  gsl_bspline_knots_uniform(xmin, xmax, bw);
+  //knots(1:k) = xmin
+  //knots(k+1:k+l-1) = xmin + i*delta, i = 1 .. l - 1
+  //knots(n+1:n+k) = xmax
+
+  /* construct the fit matrix X */
+  for (i = 0; i < n; ++i)
+    {
+      double taui = i * dx;
+      double ti   = gsl_vector_get(bw->knots, i);
+
+      gsl_vector_set(tau, i, taui);
+
+      /* compute B_j(xi) for all j */
+      gsl_bspline_eval(taui, B, bw);
+
+      printf("%f %f\n", taui, ti);
+
+      /* fill in row i of X */
+      for (j = 0; j < ncoeffs; ++j)
+      {
+        double Bj = gsl_vector_get(B, j);
+        gsl_matrix_set(A, i, j, Bj);
+      }
+
+    }
+
+  gsl_solve( A, ncoeffs, xmax, xmin);
+
+          //b[i+1] = gsl_matrix_get (A, i, i);
+          //if (i>0) a[i+1] = gsl_matrix_get (A, i-1, i);
+          //if (i<ncoeffs-1) c[i+1] = gsl_matrix_get (A, i+1, i);
+  // A = ( d_0 e_0  0  f_3 )
+  //     ( f_0 d_1 e_1  0  )
+  //     (  0  f_1 d_2 e_2 )
+  //     ( e_3  0  f_2 d_3 )
+ 
+  // gsl_linalg_solve_cyc_tridiag (d, e, f, b, x)
+
+  gsl_bspline_free(bw);
+  gsl_vector_free(B);
+  return 0;
+
+} 
