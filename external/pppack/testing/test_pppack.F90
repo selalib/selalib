330cdfd243abcd4e68271a70f7d75903d0aaef65
Pierre Navaro <pierre.navaro@univ-rennes1.fr>
2015-11-23 09:56:14 +0100

    Change external deboor library by netlib name pppack

---
+++ external/pppack/testing/test_pppack.F90
@@ -0,0 +1,111 @@
+!   This test  produces the b-spline coeff.s  bcoef  of the spline of order
+!   k  with knots  t(i), i=1,..., n + k , which takes on the value
+!   gtau(i) at  tau(i), i=1,..., n .
+!
+!******  Given data  ******
+!  tau.....array of length  n , containing data point abscissae.
+!    a s s u m p t i o n . . .  tau  is strictly increasing
+!  gtau.....corresponding array of length  n , containing data point or-
+!        dinates
+!  t.....knot sequence, of length  n+k
+!  n.....number of data points and dimension of spline space  s(k,t)
+!  k.....order of spline
+!
+!******  Computed  ******
+!  q.....array of size  (2*k-1)*n , containing the triangular factoriz-
+!        ation of the coefficient matrix of the linear system for the b-
+!        coefficients of the spline interpolant.
+!           the b-coeffs for the interpolant of an additional data set
+!        (tau(i),htau(i)), i=1,...,n  with the same data abscissae can
+!        be obtained without going through all the calculations in this
+!        routine, simply by loading  htau  into  bcoef  and then execut-
+!        ing the    call banslv ( q, 2*k-1, n, k-1, k-1, bcoef )
+!  bcoef.....the b-coefficients of the interpolant, of length  n
+!  iflag.....an integer indicating success (= 1)  or failure (= 2)
+!        the linear system to be solved is (theoretically) invertible if
+!        and only if
+!              t(i) .lt. tau(i) .lt. t(i+k),    all i.
+!        violation of this condition is certain to lead to  iflag = 2 .
+!
+!******  m e t h o d  ******
+!     the i-th equation of the linear system  a*bcoef = b  for the b-co-
+!  effs of the interpolant enforces interpolation at  tau(i), i=1,...,n.
+!  hence,  b(i) = gtau(i), all i, and  a  is a band matrix with  2k-1
+!   bands (if it is invertible).
+!     the matrix  a  is generated row by row and stored, diagonal by di-
+!  agonal, in the  r o w s  of the array  q , with the main diagonal go-
+!  ing into row  k .  see comments in the program below.
+!     the banded system is then solved by a call to  banfac (which con-
+!  structs the triangular factorization for  a  and stores it again in
+!   q ), followed by a call to  banslv (which then obtains the solution
+!   bcoef  by substitution).
+!     banfac  does no pivoting, since the total positivity of the matrix
+!  a  makes this unnecessary.
+!
+
+program test_pppack
+
+      integer iflag,k,n,   i,ilp1mx,j,jj,km1,kpkm2,left,lenq,np1
+      real bcoef(n),gtau(n),q(1),t(1),tau(n),   taui
+c     dimension q(2*k-1,n), t(n+k)
+current fortran standard makes it impossible to specify precisely the
+c  dimension of  q  and  t  without the introduction of otherwise super-
+c  fluous additional arguments.
+      np1 = n + 1
+      km1 = k - 1
+      kpkm2 = 2*km1
+      left = k
+c                zero out all entries of q
+      lenq = n*(k+km1)
+      do 5 i=1,lenq
+    5    q(i) = 0.
+c
+c  ***   loop over i to construct the  n  interpolation equations
+      do 30 i=1,n
+         taui = tau(i)
+         ilp1mx = min0(i+k,np1)
+c        *** find  left  in the closed interval (i,i+k-1) such that
+c                t(left) .le. tau(i) .lt. t(left+1)
+c        matrix is singular if this is not possible
+         left = max0(left,i)
+         if (taui .lt. t(left))         go to 998
+   15       if (taui .lt. t(left+1))    go to 16
+            left = left + 1
+            if (left .lt. ilp1mx)       go to 15
+         left = left - 1
+         if (taui .gt. t(left+1))       go to 998
+c        *** the i-th equation enforces interpolation at taui, hence
+c        a(i,j) = b(j,k,t)(taui), all j. only the  k  entries with  j =
+c        left-k+1,...,left actually might be nonzero. these  k  numbers
+c        are returned, in  bcoef (used for temp.storage here), by the
+c        following
+   16    call bsplvb ( t, k, 1, taui, left, bcoef )
+c        we therefore want  bcoef(j) = b(left-k+j)(taui) to go into
+c        a(i,left-k+j), i.e., into  q(i-(left+j)+2*k,(left+j)-k) since
+c        a(i+j,j)  is to go into  q(i+k,j), all i,j,  if we consider  q
+c        as a two-dim. array , with  2*k-1  rows (see comments in
+c        banfac). in the present program, we treat  q  as an equivalent
+c        one-dimensional array (because of fortran restrictions on
+c        dimension statements) . we therefore want  bcoef(j) to go into
+c        entry
+c            i -(left+j) + 2*k + ((left+j) - k-1)*(2*k-1)
+c                   =  i-left+1 + (left -k)*(2*k-1) + (2*k-2)*j
+c        of  q .
+         jj = i-left+1 + (left-k)*(k+km1)
+         do 30 j=1,k
+            jj = jj+kpkm2
+   30       q(jj) = bcoef(j)
+c
+c     ***obtain factorization of  a  , stored again in  q.
+      call banfac ( q, k+km1, n, km1, km1, iflag )
+                                        go to (40,999), iflag
+c     *** solve  a*bcoef = gtau  by backsubstitution
+   40 do 41 i=1,n
+   41    bcoef(i) = gtau(i)
+      call banslv ( q, k+km1, n, km1, km1, bcoef )
+                                        return
+  998 iflag = 2
+  999 print 699
+  699 format(41h linear system in  splint  not invertible)
+                                        return
+end program test_pppack
