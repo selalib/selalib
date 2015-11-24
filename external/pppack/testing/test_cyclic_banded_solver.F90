b67fa99328e3b7f526c4c816827dd5cb34104af3
Pierre Navaro <pierre.navaro@univ-rennes1.fr>
2015-11-24 13:52:21 +0100

    Add lapack full matrix solver to compare

--- external/pppack/testing/test_cyclic_banded_solver.F90
+++ external/pppack/testing/test_cyclic_banded_solver.F90
@@ -3,8 +3,9 @@ implicit none
 
 integer, parameter :: n = 10
 integer, parameter :: k = 1
-real(8) :: f(n)
-real(8) :: gtau(n)
+real(8) :: x(n)
+real(8) :: y(n)
+real(8) :: b(n)
 real(8) :: tau(n)
 real(8) :: z(n)
 real(8) :: q(2*k+1,n)
@@ -23,18 +24,11 @@ real(8) :: fact
 real(8) :: alpha, beta, gamma
 real(8) :: pi
 
-real(8), allocatable :: df_coeffs(:) 
-
-
-allocate(df_coeffs(2*k+1))
-select case(k)
-case(1)
-  df_coeffs = [real(8) :: 1.,-2.,1.]
-case(2)
-  df_coeffs = [real(8) :: -1./12., 4./3.,-5./2.,4./3.,-1./12.]
-case default
-  stop 'wrong value of k'
-end select
+real(8), allocatable  :: df_coeffs(:) 
+real(8)               :: lu(n,n)
+integer,  allocatable :: ipiv(:)
+integer               :: info
+integer               :: nrhs
 
 pi = 4.0_8 * atan(1.0_8)
 tau_min = 0.0_8
@@ -43,18 +37,31 @@ tau_max = 2.0_8*pi
 kp1 = k+1
 do i = 1, n
   tau(i)  = tau_min + (i-1)*(tau_max-tau_min)/(n)
-  gtau(i) = sin(tau(i)) * ((tau_max-tau_min)/(n))**2
+  b(i) = 1.0_8 !sin(tau(i)) * ((tau_max-tau_min)/(n))**2
 end do
 
-a = 0.0_8
+lu = 0.0_8
+a  = 0.0_8
+call random_number(a)
 write(*,*) "a="
 do j = 1, n
   do i = -k,k
-    a(modulo(j+i-1,n)+1,j) = df_coeffs(i+k+1)
+    l=modulo(j+i-1,n)+1 
+    lu(l,j) = a(l,j)
   end do
-  write(*,"(11f7.3)") a(:,j)
+  write(*,"(11f7.3)") lu(:,j)
 end do
 write(*,*)
+
+a = lu
+
+!General Matrix Factorization
+allocate(ipiv(n)); ipiv=0
+call dgetrf(n,n,lu,n,ipiv,info)
+x = b
+nrhs = 1
+call dgetrs('n',n,nrhs,lu,n,ipiv,x,n,info)
+write(*,"(' x = ', 11f7.3)") b - matmul(a,x)
 
 q = 0.0_8
 do j = 1, n
@@ -70,7 +77,7 @@ do i = 1, 2*k+1
   write(*,"(11f7.3)") q(i,:)
 end do
 
-gamma      = - a(1,1) 
+gamma      = -a(1,1) 
 alpha      = a(1,n)
 beta       = a(n,1)
 
@@ -86,23 +93,27 @@ q(k+1,1)   = a(1,1) - u(1)
 q(k+1,n)   = a(n,n) - u(n)*v(n)
 
 call banfac ( q, k+kp1, n, k, k, iflag )
-f = gtau
-call banslv ( q, k+kp1, n, k, k, f )
+y = b
+call banslv ( q, k+kp1, n, k, k, y )
 
 z = u
 call banslv ( q, k+kp1, n, k, k, z )
 
-write(*,"(10f7.3)") z
-write(*,"(10f7.3)") v
+write(*,"('x:',10f7.3)") x
+write(*,"('z:',10f7.3)") z
+write(*,"('v:',10f7.3)") v
 
-fact = (f(1)+f(n)*v(n))/(1.0_8+v(1)*z(1)+v(n)*z(n))
-!fact = dot_product(v,f)/(1.0_8+dot_product(v,z))
-f = f - fact * z 
+write(*,"(' z.v = ', f7.3)") z(1)*v(1)+z(n)*v(n)
+write(*,"(' z.v = ', f7.3)") sum(z*v)
+write(*,"(' z.v = ', f7.3)") dot_product(z,v)
 
-write(*,"(' f = ', 11f7.3)") f
+fact = (y(1)+y(n)*v(n))/(1.0_8+v(1)*z(1)+v(n)*z(n))
+x = y - fact * z 
+
+write(*,"(' x = ', 11f7.3)") x - matmul(a,b)
 
 do i = 1, n
-   write(17,*) tau(i), f(i) - sum(f)/real(n,8)
+   write(17,*) tau(i), x(i) !- sum(x)/real(n,8)
 end do
 
 end program test_cyclic_banded_solver
