80a474bc29a4d60cd70f44e7cbcaad2de747eef6
Pierre Navaro <pierre.navaro@inria.fr>
2015-11-27 19:41:34 +0100

    Revert "create a woodbury solver"
    This reverts commit d6cf57dc334e4836fd87f9a85d592f5bacc32893.

--- external/pppack/testing/test_woodbury_solver.F90
+++ external/pppack/testing/test_woodbury_solver.F90
@@ -49,7 +49,43 @@ do i = 1, n
     m(i,l) = real(i*10+l,8)
   end do
 end do
+
 call print_matrix(m)
+!Create matrix A without corners terms
+a = 0.0_8
+do j = 1,n
+  do i = max(1,j-k), min(n,j+k)
+    a(i,j) = m(i,j)
+  end do
+end do
+call print_matrix(a)
+
+
+!set u and v vectors and modify a
+u = 0.0_8
+v = 0.0_8
+do l = 1, k
+  g(l)   = -a(l,l)  ! Arbitrary gamma is set using diagonal term of A
+  u(l,l) = g(l)
+  v(l,l) = 1.0_8 
+end do
+
+do j = 1, k
+  do i = n-k+j-1,n
+    u(i,j) = m(i,j)
+    v(i,j) = m(j,i)/g(j)
+  end do
+end do
+
+do j = 1, k
+  a(j,j) = a(j,j) - g(j) 
+end do
+
+do j = n-k+1,n
+  do i = n-k+1,n
+    a(i,j) = a(i,j) - sum(u(i,:)*v(j,:)) !This sum can be optimized
+  end do
+end do
 
 !store banded matrix A in q for banfac
 q = 0.0_8
@@ -57,12 +93,34 @@ do j = 1, n
   l = 0
   do i = -k,k
     l = l+1
-    q(l,j) = m(modulo(i+j-1,n)+1,j)
+    if (i+j >= 1 .and. i+j <= n) q(l,j) = a(i+j,j)
   end do
 end do
 call print_matrix(q)
 
-call woodbury_solver(m, q, n, k, b)
+!Factorize the matrix A
+call banfac ( q, k+kp1, n, k, k, iflag )
+
+!Solve A.y = b
+x = b
+call banslv ( q, k+kp1, n, k, k, x )
+!Solve A.z = u
+do l = 1, k
+  call banslv ( q, k+kp1, n, k, k, u(:,l) )
+end do
+
+!compute the matrix H = inverse(1+t(v).z)
+h = 0.0_8
+do i = 1, k
+  h(i,i) = 1.0_8
+end do
+h = h + matmul(transpose(v),u)
+
+call dgetrf(k,k,h,k,jpiv,info)
+call dgetri(k,h,k,jpiv,work,k*k,info)
+x = x - matmul(u,matmul(h,matmul(transpose(v),x)))
+
+write(*,"(' error = ', g15.3)") sum(b - matmul(m,x))
 
 contains
 
