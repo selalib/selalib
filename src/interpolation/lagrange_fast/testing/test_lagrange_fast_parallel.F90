3b23b7301bf3352a206ed7a24770481df832985a
Klaus Reuter <khr@rzg.mpg.de>
2015-11-27 13:43:36 +0100

    reformatted lagrange_fast, added Maple worksheet

--- src/interpolation/lagrange_fast/testing/test_lagrange_fast_parallel.F90
+++ src/interpolation/lagrange_fast/testing/test_lagrange_fast_parallel.F90
@@ -17,154 +17,154 @@
 program test_lagrange_fast_parallel
 #include "sll_working_precision.h"
 #include "sll_assert.h"
-    use sll_m_constants
-    use sll_m_lagrange_fast
-    use sll_m_collective
-    use sll_m_decomposition
+  use sll_m_constants
+  use sll_m_lagrange_fast
+  use sll_m_collective
+  use sll_m_decomposition
 
-    implicit none
+  implicit none
 
-    sll_int32, parameter :: nd = 6
-    sll_int32 :: procs_per_dimension(nd)
-    logical :: periodic(nd), check(2*nd+1)
-    type(cartesian_topology_6d), pointer :: topology
-    type(decomposition_6d), pointer :: decomposition
-    sll_int32 :: ierr
-    sll_int32 :: world_size, my_rank
-    sll_int32 :: global_grid_points_per_dimension(nd)
-    sll_int32 :: halo_width_per_dimension(nd)
-    sll_real64, dimension(:,:,:,:,:,:), pointer :: xi, fi, xp, fp
-    sll_real64 :: val
-    sll_int32 :: i, stencil
-    sll_real64 :: diff, alpha
-    character(len=128) :: filename
-    character(len=6) :: rank_str
+  sll_int32, parameter :: nd = 6
+  sll_int32 :: procs_per_dimension(nd)
+  logical :: periodic(nd), check(2*nd+1)
+  type(cartesian_topology_6d), pointer :: topology
+  type(decomposition_6d), pointer :: decomposition
+  sll_int32 :: ierr
+  sll_int32 :: world_size, my_rank
+  sll_int32 :: global_grid_points_per_dimension(nd)
+  sll_int32 :: halo_width_per_dimension(nd)
+  sll_real64, dimension(:,:,:,:,:,:), pointer :: xi, fi, xp, fp
+  sll_real64 :: val
+  sll_int32 :: i, stencil
+  sll_real64 :: diff, alpha
+  character(len=128) :: filename
+  character(len=6) :: rank_str
 
-    ! --- initialize and perform 6d domain decomposition and interpolation
+  ! --- initialize and perform 6d domain decomposition and interpolation
 
-    call sll_boot_collective()
-    world_size = sll_get_collective_size(sll_world_collective)
-    my_rank = sll_get_collective_rank(sll_world_collective)
-    write (rank_str,'(I6.6)') my_rank
+  call sll_boot_collective()
+  world_size = sll_get_collective_size(sll_world_collective)
+  my_rank = sll_get_collective_rank(sll_world_collective)
+  write (rank_str,'(I6.6)') my_rank
 
-    procs_per_dimension(1) = world_size
-    procs_per_dimension(2:6) = 1
+  procs_per_dimension(1) = world_size
+  procs_per_dimension(2:6) = 1
 
-    periodic(1) = .true.
-    periodic(2:6) = .false.
-    topology => &
-        new_cartesian_topology(sll_world_collective, procs_per_dimension, periodic)
-
-
-    global_grid_points_per_dimension(1) = 128
-    global_grid_points_per_dimension(2:6) = 1
-    halo_width_per_dimension(1) = 2
-    halo_width_per_dimension(2:6) = 0
-    decomposition => &
-        new_cartesian_domain_decomposition(topology, global_grid_points_per_dimension, halo_width_per_dimension)
-
-    !write(*,*) decomposition%local%lo, decomposition%local%hi
-
-    allocate( xi(decomposition%local%lo(1):decomposition%local%hi(1), &
-                 decomposition%local%lo(2):decomposition%local%hi(2), &
-                 decomposition%local%lo(3):decomposition%local%hi(3), &
-                 decomposition%local%lo(4):decomposition%local%hi(4), &
-                 decomposition%local%lo(5):decomposition%local%hi(5), &
-                 decomposition%local%lo(6):decomposition%local%hi(6)),&
-                 stat=ierr )
-    SLL_ASSERT( ierr == 0 )
-    allocate( fi(decomposition%local%lo(1):decomposition%local%hi(1), &
-                 decomposition%local%lo(2):decomposition%local%hi(2), &
-                 decomposition%local%lo(3):decomposition%local%hi(3), &
-                 decomposition%local%lo(4):decomposition%local%hi(4), &
-                 decomposition%local%lo(5):decomposition%local%hi(5), &
-                 decomposition%local%lo(6):decomposition%local%hi(6)),&
-                 stat=ierr )
-    SLL_ASSERT( ierr == 0 )
-    allocate( xp(decomposition%local%lo(1):decomposition%local%hi(1), &
-                 decomposition%local%lo(2):decomposition%local%hi(2), &
-                 decomposition%local%lo(3):decomposition%local%hi(3), &
-                 decomposition%local%lo(4):decomposition%local%hi(4), &
-                 decomposition%local%lo(5):decomposition%local%hi(5), &
-                 decomposition%local%lo(6):decomposition%local%hi(6)),&
-                 stat=ierr )
-    SLL_ASSERT( ierr == 0 )
-    allocate( fp(decomposition%local%lo(1):decomposition%local%hi(1), &
-                 decomposition%local%lo(2):decomposition%local%hi(2), &
-                 decomposition%local%lo(3):decomposition%local%hi(3), &
-                 decomposition%local%lo(4):decomposition%local%hi(4), &
-                 decomposition%local%lo(5):decomposition%local%hi(5), &
-                 decomposition%local%lo(6):decomposition%local%hi(6)),&
-                 stat=ierr )
-    SLL_ASSERT( ierr == 0 )
+  periodic(1) = .true.
+  periodic(2:6) = .false.
+  topology => &
+    new_cartesian_topology(sll_world_collective, procs_per_dimension, periodic)
 
 
-    ! --- set up interpolation arrays
+  global_grid_points_per_dimension(1) = 128
+  global_grid_points_per_dimension(2:6) = 1
+  halo_width_per_dimension(1) = 2
+  halo_width_per_dimension(2:6) = 0
+  decomposition => &
+    new_cartesian_domain_decomposition(topology, global_grid_points_per_dimension, halo_width_per_dimension)
 
-    xi = 0.0_f64
-    fi = 0.0_f64
-    xp = 0.0_f64
-    fp = 0.0_f64
+  !write(*,*) decomposition%local%lo, decomposition%local%hi
 
-    ! --- offset from grid points
-    alpha = 0.21
+  allocate( xi(decomposition%local%lo(1):decomposition%local%hi(1), &
+    decomposition%local%lo(2):decomposition%local%hi(2), &
+    decomposition%local%lo(3):decomposition%local%hi(3), &
+    decomposition%local%lo(4):decomposition%local%hi(4), &
+    decomposition%local%lo(5):decomposition%local%hi(5), &
+    decomposition%local%lo(6):decomposition%local%hi(6)),&
+    stat=ierr )
+  SLL_ASSERT( ierr == 0 )
+  allocate( fi(decomposition%local%lo(1):decomposition%local%hi(1), &
+    decomposition%local%lo(2):decomposition%local%hi(2), &
+    decomposition%local%lo(3):decomposition%local%hi(3), &
+    decomposition%local%lo(4):decomposition%local%hi(4), &
+    decomposition%local%lo(5):decomposition%local%hi(5), &
+    decomposition%local%lo(6):decomposition%local%hi(6)),&
+    stat=ierr )
+  SLL_ASSERT( ierr == 0 )
+  allocate( xp(decomposition%local%lo(1):decomposition%local%hi(1), &
+    decomposition%local%lo(2):decomposition%local%hi(2), &
+    decomposition%local%lo(3):decomposition%local%hi(3), &
+    decomposition%local%lo(4):decomposition%local%hi(4), &
+    decomposition%local%lo(5):decomposition%local%hi(5), &
+    decomposition%local%lo(6):decomposition%local%hi(6)),&
+    stat=ierr )
+  SLL_ASSERT( ierr == 0 )
+  allocate( fp(decomposition%local%lo(1):decomposition%local%hi(1), &
+    decomposition%local%lo(2):decomposition%local%hi(2), &
+    decomposition%local%lo(3):decomposition%local%hi(3), &
+    decomposition%local%lo(4):decomposition%local%hi(4), &
+    decomposition%local%lo(5):decomposition%local%hi(5), &
+    decomposition%local%lo(6):decomposition%local%hi(6)),&
+    stat=ierr )
+  SLL_ASSERT( ierr == 0 )
 
-    do i = decomposition%local%mn(1), decomposition%local%mx(1)
-        xi(i,1,1,1,1,1) = i - 1
-        fi(i,1,1,1,1,1) = f(xi(i,1,1,1,1,1), global_grid_points_per_dimension(1))
-        xp(i,1,1,1,1,1) = xi(i,1,1,1,1,1) + alpha
-    end do
 
-    ! --- exchange boundaries
-    call apply_halo_exchange(topology, decomposition, fi)
+  ! --- set up interpolation arrays
+
+  xi = 0.0_f64
+  fi = 0.0_f64
+  xp = 0.0_f64
+  fp = 0.0_f64
+
+  ! --- offset from grid points
+  alpha = 0.21
+
+  do i = decomposition%local%mn(1), decomposition%local%mx(1)
+    xi(i,1,1,1,1,1) = i - 1
+    fi(i,1,1,1,1,1) = f(xi(i,1,1,1,1,1), global_grid_points_per_dimension(1))
+    xp(i,1,1,1,1,1) = xi(i,1,1,1,1,1) + alpha
+  end do
+
+  ! --- exchange boundaries
+  call apply_halo_exchange(topology, decomposition, fi)
 
 
-    ! --- apply interpolation
-    stencil = 2 * halo_width_per_dimension(1) + 1
-    call lagrange_halo_cells(fi(:,1,1,1,1,1), fp(:,1,1,1,1,1), alpha, stencil)
+  ! --- apply interpolation
+  stencil = 2 * halo_width_per_dimension(1) + 1
+  call lagrange_halo_cells(fi(:,1,1,1,1,1), fp(:,1,1,1,1,1), alpha, stencil)
 
 
-    ! --- write out original values
-    filename = "grid_values_" // rank_str // ".dat"
-    call dump(xi(decomposition%local%mn(1):decomposition%local%mx(1),1,1,1,1,1), &
-              fi(decomposition%local%mn(1):decomposition%local%mx(1),1,1,1,1,1), &
-              filename)
+  ! --- write out original values
+  filename = "grid_values_" // rank_str // ".dat"
+  call dump(xi(decomposition%local%mn(1):decomposition%local%mx(1),1,1,1,1,1), &
+    fi(decomposition%local%mn(1):decomposition%local%mx(1),1,1,1,1,1), &
+    filename)
 
-    ! --- write out interpolated values
-    filename = "intp_values_" // rank_str // ".dat"
-    call dump(xp(decomposition%local%mn(1):decomposition%local%mx(1),1,1,1,1,1), &
-              fp(decomposition%local%mn(1):decomposition%local%mx(1),1,1,1,1,1), &
-              filename)
+  ! --- write out interpolated values
+  filename = "intp_values_" // rank_str // ".dat"
+  call dump(xp(decomposition%local%mn(1):decomposition%local%mx(1),1,1,1,1,1), &
+    fp(decomposition%local%mn(1):decomposition%local%mx(1),1,1,1,1,1), &
+    filename)
 
-    deallocate(xi, fi, xp, fp)
+  deallocate(xi, fi, xp, fp)
 
     ! TODO: implement real test
-	print *, "Fast Lagrange interpolation unit test: PASSED"
+  print *, "Fast Lagrange interpolation unit test: PASSED"
 
 101 call sll_halt_collective()
-    stop
+  stop
 
 contains
 
-	function f(x, num_points)
-	    sll_int32, intent(in) :: num_points
-	    sll_real64, intent(in) :: x
-	    sll_real64 :: f
-	    f = cos(2*sll_pi*x/(num_points-1.0))
-	end function
+  function f(x, num_points)
+    sll_int32, intent(in) :: num_points
+    sll_real64, intent(in) :: x
+    sll_real64 :: f
+    f = cos(2*sll_pi*x/(num_points-1.0))
+  end function
 
-    subroutine dump(x, y, filename)
-        sll_real64, intent(in) :: x(:), y(:)
-        character(len=*), intent(in) :: filename
-        integer, parameter :: fd = 42
-        integer :: n
-        n = size(x)
-        SLL_ASSERT( n == size(y) )
-		open(unit=fd, file=trim(filename), action="write", status="replace")
-		do i=1,n
-			write (fd,*) x(i), y(i)
-		enddo
-		close(fd)
-    end subroutine
+  subroutine dump(x, y, filename)
+    sll_real64, intent(in) :: x(:), y(:)
+    character(len=*), intent(in) :: filename
+    integer, parameter :: fd = 42
+    integer :: n
+    n = size(x)
+    SLL_ASSERT( n == size(y) )
+    open(unit=fd, file=trim(filename), action="write", status="replace")
+    do i=1,n
+      write (fd,*) x(i), y(i)
+    enddo
+    close(fd)
+  end subroutine
 
 end program
