95d8a983f10717ce81a73a03e036c6a50d82e779
Pierre Navaro <navaro@math.cnrs.fr>
2015-11-27 22:09:38 +0100

    woodbury module is finished

--- external/pppack/woodbury.F90
+++ external/pppack/woodbury.F90
@@ -1,15 +1,16 @@
 module woodbury
 implicit none
 
-real(8) , allocatable :: u(:,:) 
-real(8) , allocatable :: v(:,:)
-real(8) , allocatable :: h(:,:)
+type :: woodbury_solver
+  real(8) , allocatable :: u(:,:) 
+  real(8) , allocatable :: v(:,:)
+  real(8) , allocatable :: h(:,:)
+end type woodbury_solver
 
 contains
 
-subroutine woodbury_fac(n, k, q)
-implicit none
-
+subroutine woodbury_fac(s, n, k, q)
+type(woodbury_solver) :: s
 integer :: n
 integer :: k
 
@@ -28,24 +29,24 @@ integer :: jpiv(k)
 
 kp1 = k+1
 
-allocate(u(n,k))
-allocate(v(n,k))
-allocate(h(k,k))
+allocate(s%u(n,k))
+allocate(s%v(n,k))
+allocate(s%h(k,k))
 
-u = 0.0_8
-v = 0.0_8
+s%u = 0.0_8
+s%v = 0.0_8
 do l = 1, k
   g(l)   = -q(kp1,l)  ! Arbitrary gamma is set using diagonal term of A
-  u(l,l) = g(l)
-  v(l,l) = 1.0_8 
+  s%u(l,l) = g(l)
+  s%v(l,l) = 1.0_8 
 end do
 
 do j = 1, k
   l = 0
   do i = n-k+j,n
     l = l+1
-    u(i,j) = q(l,j)
-    v(i,j) = q(kp1+kp1-l,i)/g(j)
+    s%u(i,j) = q(l,j)
+    s%v(i,j) = q(kp1+kp1-l,i)/g(j)
   end do
 end do
 
@@ -56,7 +57,7 @@ do j = n-k+1,n
   l = n-j 
   do i = n-k+1,n
     l = l+1
-    q(l,j) = q(l,j) - sum(u(i,:)*v(j,:)) !This sum can be optimized
+    q(l,j) = q(l,j) - sum(s%u(i,:)*s%v(j,:)) !This sum can be optimized
   end do
 end do
 
@@ -65,35 +66,34 @@ call banfac ( q, k+kp1, n, k, k, iflag )
 
 !Solve A.z = u
 do l = 1, k
-  call banslv ( q, k+kp1, n, k, k, u(:,l) )
+  call banslv ( q, k+kp1, n, k, k, s%u(:,l) )
 end do
 
 !compute the matrix H = inverse(1+t(v).z)
-h = 0.0_8
+s%h = 0.0_8
 do i = 1, k
-  h(i,i) = 1.0_8
+  s%h(i,i) = 1.0_8
 end do
-h = h + matmul(transpose(v),u)
+s%h = s%h + matmul(transpose(s%v),s%u)
 
-call dgetrf(k,k,h,k,jpiv,info)
-call dgetri(k,h,k,jpiv,work,k*k,info)
+call dgetrf(k,k,s%h,k,jpiv,info)
+call dgetri(k,s%h,k,jpiv,work,k*k,info)
 
 
 end subroutine woodbury_fac
 
-subroutine woodbury_slv(n, k, q, b, x)
-implicit none
+subroutine woodbury_slv(s, n, k, q, x)
+type(woodbury_solver) :: s
+
 integer :: n
 integer :: k
-real(8) :: x(n), b(n)
+real(8) :: x(n)
 real(8) :: q(2*k+1,n)
 integer :: kp1
 
 kp1 = k+1
-!Solve A.y = b
-x = b
 call banslv ( q, k+kp1, n, k, k, x )
-x = x - matmul(u,matmul(h,matmul(transpose(v),x)))
+x = x - matmul(s%u,matmul(s%h,matmul(transpose(s%v),x)))
 
 end subroutine woodbury_slv
 
