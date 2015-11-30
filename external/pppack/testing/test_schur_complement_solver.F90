cb5575a5d3197773c49183e9733c4b16cf44efff
Pierre Navaro <navaro@math.cnrs.fr>
2015-11-30 17:27:34 +0100

    First version of the module schur_complement

--- external/pppack/testing/test_schur_complement_solver.F90
+++ external/pppack/testing/test_schur_complement_solver.F90
@@ -30,7 +30,10 @@
 !Resoudre H x_2 = c2 puis A x1 = b1- B x2
 
 
-program test_cyclic_banded_solver
+program test_schur_complement_solver
+
+use schur_complement
+
 implicit none
 
 integer, parameter :: n = 9
@@ -70,14 +73,6 @@ integer, allocatable :: ipiv(:)
 integer              :: jpiv(k)
 integer              :: work(k*k)
 
-do i = 1, n
-  b(i) = real(i,8)
-end do
-allocate(b1(n-k)); b1 = b(1:n-k)
-allocate(b2(k));   b2 = b(n-k+1:n)
-allocate(x1(n-k)); x1 = 0.0_8
-allocate(x2(k));   x2 = 0.0_8
-
 !Create a cyclic banded system m
 m = 0.0_8
 do i = 1, n
@@ -90,150 +85,9 @@ end do
 write(*,*) "M="
 call print_matrix(m)
 
-!General Matrix Factorization with Lapack
-allocate(ipiv(n)); ipiv=0
-p = m
-call dgetrf(n,n,p,n,ipiv,info)
-x = b
-nrhs = 1
-call dgetrs('N',n,nrhs,p,n,ipiv,x,n,info)
-write(*,"(' Lapack error = ', g15.3)") sum(abs(b-matmul(m,x)))
-call print_vector(x)
-
-allocate(aa(n-k,n-k))
-aa = 0.0_8
-do j = 1,n-k
-  do i = max(1,j-k), min(n-k,j+k)
-    aa(i,j) = m(i,j)
-  end do
-end do
-write(*,*) "A="
-call print_matrix(aa)
-
-allocate(bb(n-k,k))
-bb = 0.0_8
-do j = 1, k
-  do i = 1, n-k
-    bb(i,j) = m(i,j+n-k)
-  end do
-end do
-write(*,*) "B="
-call print_matrix(bb)
-
-allocate(cc(k,n-k))
-cc = 0.0_8
-do j = 1, n-k
-  do i = 1, k
-    cc(i,j) = m(i+n-k,j)
-  end do
-end do
-write(*,*) "C="
-call print_matrix(cc)
-
-allocate(dd(k,k))
-dd = 0.0_8
-do j = 1, k
-  do i = 1, k
-    dd(i,j) = m(i+n-k,j+n-k)
-  end do
-end do
-write(*,*) "D="
-call print_matrix(dd)
-!store banded matrix A in Q for banfac
-q = 0.0_8
-do j = 1, n-k
-  l = 0
-  do i = -k,k
-    l = l+1
-    if (i+j >=1 .and. i+j <=n-k ) q(l,j) = aa(i+j,j)
-  end do
-end do
-write(*,*) "Q ="
-call print_matrix(q)
-
-write(*,*) "Banded matrix M stored in Q:"
-
-!Factorize the matrix A
-call banfac ( q, k+kp1, n-k, k, k, info )
-
-!Solve A.Y = B
-allocate(yy(n-k,k))
-yy = bb
-do j = 1, k
-  call banslv ( q, k+kp1, n-k, k, k, yy(:,j) )
-end do
-write(*,*) "Y ="
-call print_matrix(yy)
-
-!Compute H= D - C.Y
-allocate(hh(k,k))
-hh = dd - matmul(cc,yy)
-write(*,*) "H ="
-call print_matrix(hh)
-call dgetrf(k,k,hh,k,jpiv,info)
-call dgetri(k,hh,k,jpiv,work,k*k,info)
-print*,'H^(-1) =';call print_matrix(hh)
-
-!Solve A.z2 = b1
-allocate(z2(n-k))
-z2 = b1
-call banslv ( q, k+kp1, n-k, k, k, z2 )
-write(*,*) " z2 = "
-call print_vector(z2)
-
-!compute c2 = b2 - C.z2
-allocate(c2(k))
-c2 = b2 - matmul(cc,z2)
-write(*,*) " c2 = "
-call print_vector(c2)
-!Solve H.x2 = c2
-x2 = matmul(hh,c2)
-write(*,*) " x2 = "
-call print_vector(x2)
-!Solve A.x1 = b1 - B.x2
-
-x1 = b1 - matmul(bb,x2)
-call banslv ( q, k+kp1, n-k, k, k, x1 )
-write(*,*) " x1 = "
-call print_vector(x1)
-
-x(1:n-k)   = x1
-x(n-k+1:n) = x2
-
-print*, ' x = '
-call print_vector(x)
-write(*,"(' error = ', g15.3)") sum(b - matmul(m,x))
+call schur_complement_fac(n, k, q, m)
 
 
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-contains
-
-subroutine print_vector(v)
-
-  real(8), intent(in) :: v(:)
-  integer             :: j
-  character(len=20)   :: display_format
-  
-  write(display_format, "('(''|''',i2,a,''' |'')')")size(v),'f9.3'
-  write(*,display_format) v
-  write(*,*)
-
-end subroutine print_vector
-
-subroutine print_matrix(m)
-
-  real(8), intent(in) :: m(:,:)
-  integer             :: j
-  character(len=20)   :: display_format
-  
-  write(display_format, "('(''|''',i2,a,''' |'')')")size(m,2),'f9.3'
-  do i = 1, size(m,1)
-    write(*,display_format) m(i,:)
-  end do
-  write(*,*)
-
-end subroutine print_matrix
-
-end program test_cyclic_banded_solver
+end program test_schur_complement_solver
 
 
