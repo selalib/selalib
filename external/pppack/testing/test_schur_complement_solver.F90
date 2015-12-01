37397142da70d25a1b41d9b0e0b2bb494954162d
Pierre Navaro <pierre.navaro@univ-rennes1.fr>
2015-12-01 09:50:31 +0100

    Remove m matrix as argument

--- external/pppack/testing/test_schur_complement_solver.F90
+++ external/pppack/testing/test_schur_complement_solver.F90
@@ -37,12 +37,12 @@ use schur_complement
 implicit none
 
 integer, parameter :: n = 9
-integer, parameter :: k = 2
+integer, parameter :: k = 3
 integer, parameter :: kp1 = k+1
 
 real(8) :: x(n)
 real(8) :: b(n)
-real(8) :: q(2*k+1,n-k)
+real(8) :: q(2*k+1,n)
 
 real(8), allocatable :: aa(:,:)
 real(8), allocatable :: bb(:,:)
@@ -73,19 +73,50 @@ integer, allocatable :: ipiv(:)
 integer              :: jpiv(k)
 integer              :: work(k*k)
 
+!Set the RHS 
+do i = 1, n
+  b(i) = real(i,8)
+end do
 !Create a cyclic banded system m
 m = 0.0_8
 do i = 1, n
   do j = -k,k
     l=modulo(j+i-1,n)+1 
     m(i,l) = real(i*10+l,8)
-    m(l,i) = real(i*10+l,8)
+    m(l,i) = -real(l*10+i,8)
   end do
 end do
 write(*,*) "M="
 call print_matrix(m)
+!General Matrix Factorization with Lapack
+allocate(ipiv(n)); ipiv=0
+p = m
+call dgetrf(n,n,p,n,ipiv,info)
+x = b
+nrhs = 1
+call dgetrs('N',n,nrhs,p,n,ipiv,x,n,info)
+write(*,"(' Lapack error = ', g15.3)") sum(abs(b-matmul(m,x)))
+call print_vector(x)
 
-call schur_complement_fac(n, k, q, m)
+q = 0.0_8
+do j = 1, n
+  l = 0
+  do i = -k,k
+    l = l+1
+    q(l,j) = m(modulo(i+j-1,n)+1,j)
+  end do
+end do
+write(*,*) "Q ="
+call print_matrix(q)
+write(*,*) "Banded matrix M stored in Q:"
+
+x = b
+
+call schur_complement_fac(n, k, q, x)
+
+print*, ' x = '
+call print_vector(x)
+write(*,"(' error = ', g15.3)") sum(b - matmul(m,x))
 
 
 end program test_schur_complement_solver
