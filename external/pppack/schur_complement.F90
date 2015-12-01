119fdd90ec7e46b67335ac674144279b5aa839ad
Pierre Navaro <navaro@math.cnrs.fr>
2015-12-01 09:50:31 +0100

    Remove m matrix as argument

--- external/pppack/schur_complement.F90
+++ external/pppack/schur_complement.F90
@@ -38,17 +38,17 @@ end type schur_complement_solver
 
 contains
 
-subroutine schur_complement_fac(n, k, q, m)
+subroutine schur_complement_fac(n, k, q, x)
 
 integer, intent(in) :: n
 integer, intent(in) :: k
-real(8)             :: q(2*k+1,n-k)
+real(8)             :: q(2*k+1,n)
+real(8)             :: qq(2*k+1,n-k)
 real(8)             :: m(n,n)
 
 real(8) :: x(n)
 real(8) :: b(n)
 
-real(8), allocatable :: aa(:,:)
 real(8), allocatable :: bb(:,:)
 real(8), allocatable :: cc(:,:)
 real(8), allocatable :: dd(:,:)
@@ -79,86 +79,64 @@ integer              :: work(k*k)
 integer :: kp1
 
 kp1 = k+1
+b = x
 
-do i = 1, n
-  b(i) = real(i,8)
-end do
 allocate(b1(n-k)); b1 = b(1:n-k)
 allocate(b2(k));   b2 = b(n-k+1:n)
 allocate(x1(n-k)); x1 = 0.0_8
 allocate(x2(k));   x2 = 0.0_8
 
-!General Matrix Factorization with Lapack
-allocate(ipiv(n)); ipiv=0
-p = m
-call dgetrf(n,n,p,n,ipiv,info)
-x = b
-nrhs = 1
-call dgetrs('N',n,nrhs,p,n,ipiv,x,n,info)
-write(*,"(' Lapack error = ', g15.3)") sum(abs(b-matmul(m,x)))
-call print_vector(x)
 
-allocate(aa(n-k,n-k))
-aa = 0.0_8
-do j = 1,n-k
-  do i = max(1,j-k), min(n-k,j+k)
-    aa(i,j) = m(i,j)
-  end do
-end do
-write(*,*) "A="
-call print_matrix(aa)
+allocate(bb(n-k,k  )); bb = 0.0_8
+allocate(cc(k  ,n-k)); cc = 0.0_8
+allocate(dd(k  ,k  )); dd = 0.0_8
 
-allocate(bb(n-k,k))
-bb = 0.0_8
-do j = 1, k
-  do i = 1, n-k
-    bb(i,j) = m(i,j+n-k)
-  end do
-end do
-write(*,*) "B="
-call print_matrix(bb)
-
-allocate(cc(k,n-k))
-cc = 0.0_8
-do j = 1, n-k
-  do i = 1, k
-    cc(i,j) = m(i+n-k,j)
-  end do
-end do
-write(*,*) "C="
-call print_matrix(cc)
-
-allocate(dd(k,k))
-dd = 0.0_8
-do j = 1, k
-  do i = 1, k
-    dd(i,j) = m(i+n-k,j+n-k)
-  end do
-end do
-write(*,*) "D="
-call print_matrix(dd)
-!store banded matrix A in Q for banfac
-q = 0.0_8
-do j = 1, n-k
+do i = 1, k
   l = 0
-  do i = -k,k
-    l = l+1
-    if (i+j >=1 .and. i+j <=n-k ) q(l,j) = aa(i+j,j)
+  do j = i, k
+    bb(i,j) = q(k+kp1-l,n-k+l+i)
+    l =l+1
+    bb(n-k-k+j,l) = q(i,n-k+l)
   end do
 end do
-write(*,*) "Q ="
-call print_matrix(q)
 
-write(*,*) "Banded matrix M stored in Q:"
+do j = 1, k
+  l = 0
+  do i = j, k
+    l =l+1
+    cc(i,j) = q(l,j)
+  end do
+end do
+do i = 1, k
+  l = 0
+  do j = n-k-k+i,n-k
+    cc(i,j) = q(kp1+k-l,k+i+l)
+    l=l+1
+  end do
+end do
+
+do i = 1, k
+  l = 0
+  do j = 1, k
+    l=l+1
+    dd(i,j) = q(kp1-l+i,n-k+l)
+  end do
+end do
+
+write(*,*) "B"; call print_matrix(bb)
+write(*,*) "C"; call print_matrix(cc)
+write(*,*) "D"; call print_matrix(dd)
+
+qq = q(:,1:n-k)
 
 !Factorize the matrix A
-call banfac ( q, k+kp1, n-k, k, k, info )
+call banfac ( qq, k+kp1, n-k, k, k, info )
 
 !Solve A.Y = B
 allocate(yy(n-k,k))
 yy = bb
 do j = 1, k
-  call banslv ( q, k+kp1, n-k, k, k, yy(:,j) )
+  call banslv ( qq, k+kp1, n-k, k, k, yy(:,j) )
 end do
 write(*,*) "Y ="
 call print_matrix(yy)
@@ -175,7 +153,7 @@ print*,'H^(-1) =';call print_matrix(hh)
 !Solve A.z2 = b1
 allocate(z2(n-k))
 z2 = b1
-call banslv ( q, k+kp1, n-k, k, k, z2 )
+call banslv ( qq, k+kp1, n-k, k, k, z2 )
 write(*,*) " z2 = "
 call print_vector(z2)
 
@@ -191,16 +169,13 @@ call print_vector(x2)
 !Solve A.x1 = b1 - B.x2
 
 x1 = b1 - matmul(bb,x2)
-call banslv ( q, k+kp1, n-k, k, k, x1 )
+call banslv ( qq, k+kp1, n-k, k, k, x1 )
 write(*,*) " x1 = "
 call print_vector(x1)
 
 x(1:n-k)   = x1
 x(n-k+1:n) = x2
 
-print*, ' x = '
-call print_vector(x)
-write(*,"(' error = ', g15.3)") sum(b - matmul(m,x))
 
 end subroutine schur_complement_fac
 
