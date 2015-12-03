af3986c8f83b49ba248c47a2cafba15072c90479
Yaman Güçlü <yaman.guclu@gmail.com>
2015-12-03 10:21:14 +0100

    [CC5] Add accurate extraction of interface sections from original source files.

--- python/maintenance/postprocess_interface_sections.py
+++ python/maintenance/postprocess_interface_sections.py
@@ -14,7 +14,7 @@ Modules required
 #
 # Author: Yaman Güçlü, Nov 2015 - IPP Garching
 #
-# Last revision: 30 Nov 2015
+# Last revision: 02 Dec 2015
 #
 import re
 
@@ -157,22 +157,6 @@ def find_missing_headers( f ):
             if header not in ignored_headers:
                 missing_headers.append( header )
     return missing_headers
-
-#------------------------------------------------------------------------------
-def extract_interface_section( f ):
-    # Identify the meaningful lines to be processed
-    lines = []
-    skip  = True
-    for i,line in enumerate( f ):
-        line = line.lstrip().rstrip('\n')
-        if line.startswith( 'module' ) or line.startswith( 'program' ):
-            skip = False
-
-        elif line.startswith( 'implicit' ) or line.startswith( 'contains' ):
-            break
-        if not skip:
-            lines.append( line )
-    return lines
 
 #------------------------------------------------------------------------------
 def find_ifdef_clauses( lines ):
@@ -388,39 +372,125 @@ class InterfaceHandler( LineSelector ):
         return 'stop'
 
 #==============================================================================
+
+class InterfaceExtractor( LineSelector ):
+
+    #--------------------------------------------------------------------------
+    def __init__( self ):
+        self.unit      = None
+        self.split     = False
+
+    #--------------------------------------------------------------------------
+    def status( self, line ):
+
+        if self.unit is None:
+            if self.regex['module'].match( line ):
+                self.unit = 'module'
+            elif self.regex['program'].match( line ):
+                self.unit = 'program'
+            return 'skip'
+
+        if line[0] == '#':
+            return 'extract'
+
+        if line.strip() == '':
+            self.split = False
+            return 'extract'
+
+        if line.lstrip()[0] == '!':
+            return 'extract'
+
+        if self.split:
+            self.split = self.line_is_split( line )
+            return 'extract'
+
+        self.split = self.line_is_split( line )
+        opts = ['use','use intrinsic','implicit none', \
+                'public','private','intrinsic']
+        for key in opts:
+            if self.regex[key].match( line ):
+                return 'extract'
+
+        # Line does not match any of the patterns: stop processing
+        return 'stop'
+
+#==============================================================================
+# FUNCTION: extract interface section from source file
+#==============================================================================
+
+def extract_interface_section( f ):
+
+    lines = []
+
+    # First pass: Identify the meaningful lines to be processed
+    num_start_line = -1
+    selector = InterfaceExtractor()
+    for i,line in enumerate( f ):
+        status = selector.status( line )
+        if status == 'skip':
+            continue
+        elif status == 'extract':
+            lines.append( line.rstrip('\n') )
+            if num_start_line == -1:
+                num_start_line = i
+        elif status == 'stop':
+            break
+
+    # Second pass: from top skip empty and comment lines
+    for line in list( lines ):
+        stripped = line.strip()
+        if stripped == '' or stripped.startswith('!'):
+            lines.remove( line )
+            num_start_line += 1
+        else:
+            break
+
+    # Third pass: from bottom skip empty lines, comments, #define, and #if[..]
+    for line in list( lines )[::-1]:
+        stripped = line.strip()
+        if stripped == '' or stripped.startswith('!') or \
+                line.startswith('#define') or line.startswith('#if'):
+            lines.pop()
+        else:
+            break
+
+    return lines, [num_start_line, len( lines )]
+
+#==============================================================================
 # FUNCTION: replace interface in source file
 #==============================================================================
 
 def replace_interface_section( src_fpath, int_fpath, overwrite=False ):
 
-    # Create new object that decides what to do with each line
-    selector = InterfaceHandler()
-
     # New temporary file for output
     out_fpath = src_fpath + '.new_interface'
+
+    # Read old interface file
+    src_int_fpath = src_fpath + '-interface_old.txt'
+    with open( src_int_fpath, 'r' ) as f:
+        top_line = f.readline().strip('\n')
+        num_start_line, num_lines = \
+                [int(c) for c in top_line.strip('@ ').split(',')]
 
     # Read source and interface files, and write merged result to new file
     with open( src_fpath, 'r' ) as src_f, \
             open( out_fpath, 'w' ) as out_f:
-        for src_line in src_f:
-            status = selector.status( src_line )
-            if status == 'remove':
-                continue  # skip line
-            elif status == 'keep':
+
+        for i,src_line in enumerate( src_f ):
+            # Write first part of source file
+            if i < num_start_line :
                 print( src_line, file=out_f, end='' )
-            elif status == 'stop':
-                break
-
-        # Insert interface
-        with open( int_fpath, 'r' ) as int_f:
-            for int_line in int_f:
-                print( int_line, file=out_f, end='' )
-
-        # Print remaining lines from source file
-        print( '', file=out_f )
-        print( src_line, file=out_f, end='' )
-        for src_line in src_f:
-            print( src_line, file=out_f, end='' )
+            # Insert new interface
+            elif i == num_start_line:
+                with open( int_fpath, 'r' ) as int_f:
+                    for int_line in int_f:
+                        print( int_line, file=out_f, end='' )
+            # Skip old interface
+            elif i < num_start_line + num_lines:
+                continue
+            # Print last part of source file
+            else:
+                print( src_line, file=out_f, end='' )
 
     # Overwrite original file, if requested
     if overwrite:
@@ -461,6 +531,31 @@ def remove_repeated_access_spec_stmt( src_fpath, overwrite=False ):
         rename( out_fpath, src_fpath )
 
 #==============================================================================
+# MAIN FUNCTION: extract interface sections from original source files
+#==============================================================================
+
+def extract_old_interface_sections( original_root, preproc_root ):
+
+    from maintenance_tools import recursive_file_search
+
+    original_root = original_root.rstrip('/')
+    preproc_root  =  preproc_root.rstrip('/')
+
+    for fpath in recursive_file_search(
+            preproc_root, select_file = is_interface_file ):
+
+        fpath_orig = get_original_file( fpath, original_root, preproc_root )
+        print( "Extracting interface from file: %s" % fpath_orig )
+        with open( fpath_orig, 'r' ) as f:
+            lines, [num_start_line, num_lines] = extract_interface_section( f )
+
+        fpath_orig_int = fpath_orig + '-interface_old.txt'
+        with open( fpath_orig_int, 'w' ) as f:
+            top_line = '@@ {},{} @@'.format( num_start_line, num_lines )
+            print( top_line, file=f )
+            print(   *lines, file=f, sep='\n' )
+
+#==============================================================================
 # MAIN FUNCTION: process interface
 #==============================================================================
 
@@ -475,10 +570,11 @@ def process_interface_sections( original_root, preproc_root, replace=False ):
 #    num_ifclauses  = 0
 #    num_ifmods     = 0
 
-    for fpath in recursive_file_search( 
+    for fpath in recursive_file_search(
             preproc_root, select_file = is_interface_file ):
 
         fpath_orig = get_original_file( fpath, original_root, preproc_root )
+        fpath_orig_int = fpath_orig + '-interface_old.txt'
 #        fpath_prpr = fpath.rpartition( '-interface.txt' )[0] + '.f90'
         print()
         print( "Post-processing interface: %s" % fpath )
@@ -491,16 +587,17 @@ def process_interface_sections( original_root, preproc_root, replace=False ):
             headers, lines = convert_modules_to_headers( f )
 
         # Collect all headers that should not be ignored
-        with open( fpath_orig, 'r' ) as f_orig:
-            headers += find_missing_headers( f_orig )
+        with open( fpath_orig_int, 'r' ) as f_orig_int:
+            headers += find_missing_headers( f_orig_int )
 
         # Add header lines at beginning of file
         if headers:
             lines = ['#include "%s"' % h for h in headers] + [''] + lines
 
         # Find ifdef clauses and contained use statements
-        with open( fpath_orig, 'r' ) as f_orig:
-            interface_section_orig = extract_interface_section( f_orig )
+        with open( fpath_orig_int, 'r' ) as f_orig_int:
+            interface_section_orig = \
+                    [line.rstrip('\n') for line in f_orig_int.readlines()[1:]]
         outer_if, ifclauses = find_ifdef_clauses( interface_section_orig )
         ifmods = set(m for c in ifclauses for m in find_all_used_modules( c ))
 
@@ -600,6 +697,9 @@ def main():
     args = parse_input()
     print(args)
     print('')
+
+    # Extract interface sections from original source files
+    extract_old_interface_sections( args.original_root, args.preproc_root )
 
     # Walk directory tree and post-process all interface sections
     process_interface_sections(
