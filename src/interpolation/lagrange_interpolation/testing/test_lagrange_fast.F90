7eb1fba4d35ed4950f59b213f1127aa4a5e4c1c6
Katharina Kormann <katharina.kormann@tum.de>
2015-12-07 09:41:27 +0100

    [fast lagrange] Rename public functions, continued.

--- src/interpolation/lagrange_interpolation/testing/test_lagrange_fast.F90
+++ src/interpolation/lagrange_interpolation/testing/test_lagrange_fast.F90
@@ -1,7 +1,9 @@
 program test_lagrange_fast
 #include "sll_working_precision.h"
-  use sll_m_constants
-  use sll_m_lagrange_fast
+  use sll_m_constants, only : sll_pi
+  use sll_m_lagrange_fast, only : &
+       sll_s_interpolate_array_disp_lagrange_fixed_no_bc
+  
 
   implicit none
 
@@ -25,14 +27,14 @@ program test_lagrange_fast
   xmax = num_points-1.0_f64
   l = xmax - xmin
   do i = 1, num_points
-    xi(i) = i - 1
+    xi(i) = real(i - 1, f64)
     fi(i) = f(xi(i), num_points)
     xp(i) = xi(i) + alpha
   end do
   diff = 0.0_f64
   fp(:) = 0.0_f64
 
-  call lagrange(fi, fp, alpha, s)
+  call sll_s_interpolate_array_disp_lagrange_fixed_no_bc(fi, fp, alpha, s)
 
   do i=1,num_points
     diff=max(diff, abs(f(xp(i),num_points) - fp(i)))
