9b644ad0bd01cd4507fe08a62f1366e9c403d685
Katharina Kormann <katharina.kormann@tum.de>
2015-12-07 16:21:29 +0100

    [CC5] Add use only statements for FFTPACK version.

--- src/field_solvers/poisson_solvers/sll_m_poisson_2d_periodic_solver.F90
+++ src/field_solvers/poisson_solvers/sll_m_poisson_2d_periodic_solver.F90
@@ -17,7 +17,11 @@ module sll_m_poisson_2d_periodic_solver
 
 #define poisson_2d_periodic poisson_2d_periodic_fftw
 #else
-use sll_m_poisson_2d_periodic_fftpack
+use sll_m_poisson_2d_periodic_fftpack, only: &
+    new, &
+    poisson_2d_periodic_fftpack, &
+    solve
+
 #define poisson_2d_periodic poisson_2d_periodic_fftpack
 #endif
   implicit none
