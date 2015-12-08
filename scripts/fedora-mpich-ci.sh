12679a30e23180130285a0aac4a68a2122f44baf
Pierre Navaro <navaro@math.cnrs.fr>
2015-12-08 21:42:15 +0100

    Add script to test sll_mpi

--- scripts/fedora-mpich-ci.sh
+++ scripts/fedora-mpich-ci.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 source /etc/profile.d/modules.sh
-module load mpi
+module purge
+module load mpi/mpich-x86_64
 rm -rf build
 mkdir build
 cd build; {
 cmake -DCMAKE_BUILD_TYPE=Release -DHDF5_PARALLEL_ENABLED=ON ..
+make
 make Experimental
 }; cd -
 exit 0
