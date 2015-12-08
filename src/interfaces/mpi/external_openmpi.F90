9beba91be422d6afe0a3ecfa2cf11d66500d0818
Yaman Güçlü <yaman.guclu@gmail.com>
2015-12-08 20:31:46 +0100

    Start preparing 'sll_mpi' for being fully portable.

---
+++ src/interfaces/mpi/external_openmpi.F90
@@ -0,0 +1,21 @@
+#if OMPI_V_MINOR < 10
+  external :: &
+#if OMPI_V_MINOR < 8
+    mpi_bcast,      &
+#endif
+    mpi_allgather,  &
+    mpi_allgatherv, &
+    mpi_allreduce,  &
+    mpi_alltoall,   &
+    mpi_alltoallv,  &
+    mpi_gather,     &
+    mpi_gatherv,    &
+    mpi_scatter,    &
+    mpi_scatterv,   &
+    mpi_isend,      &
+    mpi_irecv,      &
+    mpi_reduce,     &
+    mpi_recv,       &
+    mpi_send,       &
+    mpi_sendrecv
+#endif
