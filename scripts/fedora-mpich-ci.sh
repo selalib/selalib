71ad256bab9f6c8da10e8f05c9c85284f36e2cc3
Pierre Navaro <navaro@math.cnrs.fr>
2015-12-08 22:08:05 +0100

    [ci] improve the fedora mpich ci script

--- scripts/fedora-mpich-ci.sh
+++ scripts/fedora-mpich-ci.sh
@@ -5,7 +5,7 @@ module load mpi/mpich-x86_64
 rm -rf build
 mkdir build
 cd build; {
-cmake -DHDF5_INCLUDE_DIR=/usr/include/mpich-x86_64 -DCMAKE_BUILD_TYPE=Release -DHDF5_PARALLEL_ENABLED=ON ..
+cmake -DHDF5_INCLUDE_DIR=/usr/include/mpich-x86_64 -DHDF5_INCLUDE_DIR_FORTRAN=/usr/include/mpich-x86_64 -DHDF5_ROOT=/usr/lib64/mpich -DCMAKE_BUILD_TYPE=Release -DHDF5_PARALLEL_ENABLED=ON ..
 make
 make Experimental
 }; cd -
