e324705d11afc5bccf31171925da3c18327deda1
Yaman Güçlü <yaman.guclu@ipp.mpg.de>
2015-12-09 21:57:08 +0100

    Fedora-mpich: restore ctest.

--- scripts/fedora-mpich-ci.sh
+++ scripts/fedora-mpich-ci.sh
@@ -7,6 +7,6 @@ mkdir build
 cd build; {
 cmake ../ -DHDF5_PARALLEL_ENABLED=ON -DMPI_Fortran_INCLUDE_PATH=${MPI_FORTRAN_MOD_DIR} -DHDF5_INCLUDE_DIR=/usr/include/mpich-x86_64 -DHDF5_INCLUDE_DIR_FORTRAN=/usr/include/mpich-x86_64 -DHDF5_ROOT=/usr/lib64/mpich -DCMAKE_BUILD_TYPE=Release
 make 
-#make Experimental
+make Experimental
 }; cd -
 exit 0
