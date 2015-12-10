5e10fb5aed51e9b59a0eea3d7a08872ab854e0d2
Yaman Güçlü <yaman.guclu@gmail.com>
2015-12-10 16:35:35 +0100

    [sll_mpi] Better control of MPI version, plus fix for Ubuntu+MPICH.

--- src/interfaces/mpi/external_mpich.F90
+++ src/interfaces/mpi/external_mpich.F90
@@ -9,6 +9,9 @@
     mpi_cart_create, &
     mpi_cart_get,    &
     mpi_dims_create, &
+#if MPICH_VERSION < 30105
+    mpi_finalize,    &
+#endif
     mpi_gather,      &
     mpi_gatherv,     &
     mpi_iallreduce,  &
