b94f33d951505a0f258526eccdb34d661c5c56da
Katharina Kormann <katharina.kormann@tum.de>
2015-12-21 17:07:12 +0100

    [bic basic] Add symmetric Landau initializer.

--- src/particle_methods/pic_basic/pic_interface/sll_m_particle_initializer.F90
+++ src/particle_methods/pic_basic/pic_interface/sll_m_particle_initializer.F90
@@ -5,6 +5,7 @@
 module sll_m_particle_initializer
 
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#include "sll_errors.h"
 #include "sll_memory.h"
 #include "sll_working_precision.h"
 
@@ -25,6 +26,7 @@ module sll_m_particle_initializer
   public :: &
     sll_particle_initialize_random_landau_1d2v, &
     sll_particle_initialize_random_landau_2d2v, &
+    sll_particle_initialize_random_landau_symmetric_1d2v, &
     sll_particle_initialize_sobol_landau_1d2v, &
     sll_particle_initialize_sobol_landau_2d2v
 
@@ -286,6 +288,83 @@ contains
 
 
   end subroutine sll_particle_initialize_random_landau_1d2v
+
+
+!> Initialize of a 1d2v particle group with Sobol pseudorandom numbers. Maxwellian distribution of V, cosine perturbation along x, equal weights.
+  subroutine sll_particle_initialize_random_landau_symmetric_1d2v(&
+       particle_group, &
+       landau_param, &
+       xmin, &
+       Lx, &
+       thermal_velocity, &
+       rnd_seed)
+    class(sll_particle_group_base),  intent(inout)              :: particle_group
+    sll_real64, intent(in)                                      :: landau_param(2) !< parameter defining the perturbation: landau_param(1)*cos(landau_param(2)*x1)
+    sll_real64, intent(in)                                      :: xmin !< lower bound of the domain
+    sll_real64, intent(in)                                      :: Lx !< length of the domain.
+    sll_real64, intent(in)                                      :: thermal_velocity(2) !< Value of the thermal velocity along each dimension.
+    sll_int32,  intent(inout)                                   :: rnd_seed(:) !< Random seed.
+
+    sll_real64                                                  :: x(3),v(3)
+    sll_real64                                                  :: rnd_no
+    sll_int32                                                   :: i_part
+    sll_int32                                                   :: i_v
+    sll_real64                                                  :: wi
+    sll_int32                                                   :: ip
+    
+    ! Set random seed.
+    call random_seed (put=rnd_seed)
+
+    x = 0.0_f64
+    v = 0.0_f64
+
+    if (modulo(particle_group%n_particles,4) .NE. 0) then
+       SLL_WARNING('sll_particle_initialize_random_landau_symmetric_1d2v', 'particle number not multiple of 4')
+    end if
+
+
+    ! 1/Np in common weight
+    call particle_group%set_common_weight &
+         (1.0_f64/real(particle_group%n_total_particles, f64))
+
+    do i_part = 1, particle_group%n_particles
+       ! Generate random number on [0,1]
+       call random_number(rnd_no)
+       ! Transform rdn to the interval
+       x(1) = xmin + Lx * rnd_no
+       ! Landau perturbation for position 
+       wi = (1.0_f64 + landau_param(1)*cos(landau_param(2)*x(1)))*Lx
+
+       ip = modulo(i_part, 4)
+       if (ip == 1) then
+          ! Maxwellian distribution of the temperature
+          do i_v = 1,2
+             call random_number(rnd_no)
+             call normal_cdf_inv( rnd_no, 0.0_f64, 1.0_f64, &
+                  v(i_v))
+             v(i_v) = v(i_v)*(thermal_velocity(i_v))
+          end do
+       elseif (ip == 2) then
+          v(2) = -v(2)
+       elseif (ip == 3) then
+          v(1) = -v(1)
+       elseif (ip == 4) then
+          v(2) = -v(2)
+       end if
+
+       ! Copy the generated numbers to the particle
+       call particle_group%set_x(i_part, x)
+       call particle_group%set_v(i_part, v)
+       ! Set weights.
+       call particle_group%set_weights(i_part, &
+            [wi])
+
+       
+
+    end do
+
+
+  end subroutine sll_particle_initialize_random_landau_symmetric_1d2v
 
 
 
