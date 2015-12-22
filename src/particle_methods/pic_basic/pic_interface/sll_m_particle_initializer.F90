b5d9e72125081edebae3f78cdea839a259236b68
Katharina Kormann <katharina.kormann@tum.de>
2015-12-22 18:04:06 +0100

    [pic vm] Add symmetric particle load with Sobol.

--- src/particle_methods/pic_basic/pic_interface/sll_m_particle_initializer.F90
+++ src/particle_methods/pic_basic/pic_interface/sll_m_particle_initializer.F90
@@ -28,7 +28,8 @@ module sll_m_particle_initializer
     sll_s_particle_initialize_random_landau_2d2v, &
     sll_s_particle_initialize_random_landau_symmetric_1d2v, &
     sll_s_particle_initialize_sobol_landau_1d2v, &
-    sll_s_particle_initialize_sobol_landau_2d2v
+    sll_s_particle_initialize_sobol_landau_2d2v, &
+    sll_s_particle_initialize_sobol_landau_symmetric_1d2v
 
   private
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -230,6 +231,79 @@ contains
 
 
   end subroutine sll_s_particle_initialize_sobol_landau_1d2v
+
+!> Initialize of a 1d2v particle group with Sobol pseudorandom numbers. Maxwellian distribution of V, cosine perturbation along x, equal weights.
+  subroutine sll_s_particle_initialize_sobol_landau_symmetric_1d2v(&
+       particle_group, &
+       landau_param, &
+       xmin, &
+       Lx, &
+       thermal_velocity, &
+       rnd_seed)
+    class(sll_c_particle_group_base),  intent(inout)              :: particle_group
+    sll_real64, intent(in)                                      :: landau_param(2) !< parameter defining the perturbation: landau_param(1)*cos(landau_param(2)*x1)
+    sll_real64, intent(in)                                      :: xmin !< lower bound of the domain
+    sll_real64, intent(in)                                      :: Lx !< length of the domain.
+    sll_real64, intent(in)                                      :: thermal_velocity(2) !< Value of the thermal velocity along each dimension.
+    sll_int64,  intent(inout)                                   :: rnd_seed !< Random seed.
+
+    sll_real64                                                  :: x(3),v(3)
+    sll_int32                                                   :: i_part
+    sll_int32                                                   :: i_v
+    sll_real64                                                  :: rdn(3)
+    sll_real64                                                  :: wi
+    sll_int32                                                   :: ip
+    
+
+    x = 0.0_f64
+    v = 0.0_f64
+
+    !rdn(1) = 0.1_f64
+    !call sll_s_normal_cdf_inv(rdn(1), 0.0_f64, 1.0_f64, v(1))
+    !print*, 'cdf', v(1)
+    if (modulo(particle_group%n_particles,8) .NE. 0) then
+       SLL_ERROR('sll_s_particle_initialize_random_landau_symmetric_1d2v', 'particle number not multiple of 8')
+    end if
+
+    ! 1/Np in common weight
+    call particle_group%set_common_weight &
+         (1.0_f64/real(particle_group%n_total_particles, f64))
+
+    do i_part = 1, particle_group%n_particles
+       ip = modulo(i_part, 8)
+       if ( ip == 1) then
+          ! Generate Sobol numbers on [0,1]
+          call sll_s_i8_sobol( int(3,8), rnd_seed, rdn)
+
+          ! Transform rdn to the interval
+          x(1) = xmin + Lx * rdn(1)
+          ! Landau perturbation for position 
+          wi = (1.0_f64 + landau_param(1)*cos(landau_param(2)*x(1)))*Lx
+
+          ! Maxwellian distribution of the temperature
+          do i_v = 1,2
+             call sll_s_normal_cdf_inv( rdn(i_v+1), 0.0_f64, 1.0_f64, &
+                  v(i_v))
+             v(i_v) = v(i_v)*(thermal_velocity(i_v))
+          end do
+       elseif ( ip == 5) then
+          x(1) = Lx - x(1)
+       elseif ( modulo(ip,2) == 0 ) then
+          v(2) = - v(2)
+       else
+          v(1) = -v(1)
+       end if
+
+       ! Copy the generated numbers to the particle
+       call particle_group%set_x(i_part, x)
+       call particle_group%set_v(i_part, v)
+       ! Set weights.
+       call particle_group%set_weights(i_part, &
+            [wi])
+    end do
+
+
+  end subroutine sll_s_particle_initialize_sobol_landau_symmetric_1d2v
 
 !> Initialize of a 1d2v particle group with Sobol pseudorandom numbers. Maxwellian distribution of V, cosine perturbation along x, equal weights.
   subroutine sll_s_particle_initialize_random_landau_1d2v(&
