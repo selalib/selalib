ad388b74eb73a0e2a94d9971ce3103d3c3058a41
Katharina Kormann <katharina.kormann@tum.de>
2016-01-12 12:01:01 +0100

    [lagrange_fast] Remove @contact which is not a doxygen command.

--- src/interpolation/lagrange_interpolation/lagrange_interpolation_doc.F90
+++ src/interpolation/lagrange_interpolation/lagrange_interpolation_doc.F90
@@ -12,8 +12,8 @@
 !> @defgroup lagrange_interpolation sll_lagrange_interpolation 
 !> @brief 
 !> Description of sll_m_lagrange_interpolation library (72 characters)
-!> @author Raphael Blanchard, Klaus Reuter, Katharina Kormann
-!> @contact Katharina Kormann
+!> @author Raphael Blanchard, Klaus Reuter, Katharina Kormann;
+!> Contact: Katharina Kormann
 !> @details
 !> The library sll_lagrange_interpolation currently only offers two implementations of the interpolator routine \a interpolate_array_disp on uniform 1D grids. 
 !> The module sll_m_lagrange_fast is a fast implementation that also works in combination with domain decomposition. It implements a Lagrange interpolation that is always centered around the point that is displaced (odd number of points).
