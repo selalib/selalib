824174532873c8d6f25c62a71ccce47a5f22d746
Katharina Kormann <katharina.kormann@tum.de>
2016-01-14 09:09:10 +0100

    [lagrange fast] Improve implementation.

--- src/interpolation/lagrange_interpolation/lagrange_interpolation_doc.F90
+++ src/interpolation/lagrange_interpolation/lagrange_interpolation_doc.F90
@@ -18,6 +18,7 @@
 !> The library sll_lagrange_interpolation currently only offers two implementations of the interpolator routine \a interpolate_array_disp on uniform 1D grids.
 !> The module sll_m_lagrange_fast is a fast implementation that also works in combination with domain decomposition. In particular, for the MPI domain decomposition use case, it implements a Lagrange interpolation that is always centered around the point that is displaced (odd number of points).
 !> The module sll_m_lagrange_interpolation_1d, on the other hand, uses an interpolation that is centered around interval to which the point is displaced (even number of points).
+!> For the boundary closure, three possiblities are implemented: Either halo cells are provided or periodic boundary conditions can be used. A third alternative is to use a one-sided stencil at the boundary.
 !> @todo
 !> - Improve efficiency of sll_m_lagrange_interpolation_1d.F90
 !> - Implement other interpolation routines.
