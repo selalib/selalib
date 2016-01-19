421d6264f10e22cad39255b0d90cbf4008588a9b
Pierre Navaro <navaro@math.cnrs.fr>
2016-01-19 10:31:53 +0100

    [APP] Improve the script to evaluate contributions

--- scripts/count_lines.sh
+++ scripts/count_lines.sh
@@ -6,116 +6,140 @@
 #I already put some names but i can make mistake or forget something.
 #Pierre
 #
-#ECG : Edwin Chacon-Golcher
-#AB  : Aurore Back
-#PN  : Pierre Navaro
-#PH  : Philippe Helluy
-#AR  : Ahmed Ratnani
-#ES  : Eric Sonnendrucker
-#CS  : Christophe Steiner
-#YG  : Yaman Guclu
-#KK  : Katharina Kormann
-#KR  : Klaus Reuter
-#MCP : Martin Campos-Pinto
-#ALH : Antoine Le Hyaric
-#LM  : Laura Mendoza
-#MM  : Michel Mehrenberger
-#NC  : Nicolas Crouseilles
-#AH  : Adnane Hamiaz
-#SH  : Sever Hirstoaga
-#CP  : Charles Prouveur
+#EC : Edwin Chacon-Golcher
+#AB : Aurore Back
+#PN : Pierre Navaro
+#PH : Philippe Helluy
+#AR : Ahmed Ratnani
+#ES : Eric Sonnendrucker
+#CS : Christophe Steiner
+#YG : Yaman Guclu
+#KK : Katharina Kormann
+#KR : Klaus Reuter
+#MC : Martin Campos-Pinto
+#AL : Antoine Le Hyaric
+#LM : Laura Mendoza
+#MM : Michel Mehrenberger
+#NC : Nicolas Crouseilles
+#AH : Adnane Hamiaz
+#SH : Sever Hirstoaga
+#CP : Charles Prouveur
 
-AB=$(git ls-files -z ${1} add_ons/multipatch/ | xargs -0 cat | wc -l)
-echo 'AB='$((AB))
+AB=$(git ls-files ${1} add_ons/multipatch/ | grep F90| xargs cat | wc -l)
+AB=$((AB))
 
-KK1=$(git ls-files -z ${1} add_ons/sparse_grid | xargs -0 cat | wc -l)
-KK2=$(git ls-files -z ${1} interfaces/fft | xargs -0 cat | wc -l)
-KK3=$(git ls-files -z ${1} particle_methods/pic_basic | xargs -0 cat | wc -l)
-KK4=$(git ls-files -z ${1} time_integration/splitting_methods | xargs -0 cat | wc -l)
-echo 'KK='$((KK1+KK2+KK3+KK4))
+KK1=$(git ls-files add_ons/sparse_grid | grep F90| xargs cat | wc -l)
+KK2=$(git ls-files interfaces/fft | grep F90| xargs cat | wc -l)
+KK3=$(git ls-files particle_methods/pic_basic | grep F90| xargs cat | wc -l)
+KK4=$(git ls-files time_integration/splitting_methods | grep F90| xargs cat | wc -l)
 
-ECG00=$(git ls-files -z ${1} data_structures/boundary_condition_descriptors | xargs -0 cat | wc -l)
-ECG01=$(git ls-files -z ${1} data_structures/distribution_function | xargs -0 cat | wc -l)
-ECG02=$(git ls-files -z ${1} data_structures/fields | xargs -0 cat | wc -l)
-ECG03=$(git ls-files -z ${1} initialization_tools/function_initialization | xargs -0 cat | wc -l)
-ECG04=$(git ls-files -z ${1} interpolation/interpolators | xargs -0 cat | wc -l)
-ECG00=$(git ls-files -z ${1} interpolation/lagrange_interpolation | xargs -0 cat | wc -l)
-ECG00=$(git ls-files -z ${1} field_solvers/poisson_solvers_parallel | xargs -0 cat | wc -l)
-ECG00=$(git ls-files -z ${1} linear_solvers | xargs -0 cat | wc -l)
-ECG00=$(git ls-files -z ${1} parallelization/collective | xargs -0 cat | wc -l)
-ECG00=$(git ls-files -z ${1} low_level_utilities/assert | xargs -0 cat | wc -l)
-ECG00=$(git ls-files -z ${1} low_level_utilities/constants | xargs -0 cat | wc -l)
-ECG00=$(git ls-files -z ${1} low_level_utilities/memory | xargs -0 cat | wc -l)
-ECG00=$(git ls-files -z ${1} low_level_utilities/timer | xargs -0 cat | wc -l)
-ECG00=$(git ls-files -z ${1} low_level_utilities/utilities | xargs -0 cat | wc -l)
-ECG00=$(git ls-files -z ${1} low_level_utilities/working_precision | xargs -0 cat | wc -l)
-ECG00=$(git ls-files -z ${1} mesh/coordinate_transformations | xargs -0 cat | wc -l)
-ECG00=$(git ls-files -z ${1} mesh/mesh_calculus | xargs -0 cat | wc -l)
-ECG00=$(git ls-files -z ${1} parallelization/parallel_utilities | xargs -0 cat | wc -l)
-ECG00=$(git ls-files -z ${1} parallelization/point_to_point_communications | xargs -0 cat | wc -l)
-ECG00=$(git ls-files -z ${1} parallelization/remap | xargs -0 cat | wc -l)
-echo 'ECG'$ECG
+KK=$((KK1+KK2+KK3+KK4))
 
-YG=0
-YG+=$(git ls-files -z ${1} data_structures/vector_space | xargs -0 cat | wc -l)
-YG+=$(git ls-files -z ${1} time_integration/ode_integrators | xargs -0 cat | wc -l)
-YG+=$(git ls-files -z ${1} io/xdmf_io | xargs -0 cat | wc -l)
-YG+=$(git ls-files -z ${1} io/xdmf_io_parallel | xargs -0 cat | wc -l)
-YG+=$(git ls-files -z ${1} low_level_utilities/errors | xargs -0 cat | wc -l)
-YG+=$(git ls-files -z ${1} parallelization/parallel_array_utilities | xargs -0 cat | wc -l)
-YG+=$(git ls-files -z ${1} time_integration/ode_solvers | xargs -0 cat | wc -l)
-echo 'YG='$YG
+EC00=$(git ls-files data_structures/boundary_condition_descriptors | grep F90| xargs cat | wc -l)
+EC01=$(git ls-files data_structures/distribution_function | grep F90| xargs cat | wc -l)
+EC02=$(git ls-files data_structures/fields | grep F90| xargs cat | wc -l)
+EC03=$(git ls-files initialization_tools/function_initialization | grep F90| xargs cat | wc -l)
+EC04=$(git ls-files interpolation/interpolators | grep F90| xargs cat | wc -l)
+EC05=$(git ls-files interpolation/lagrange_interpolation | grep F90| xargs cat | wc -l)
+EC06=$(git ls-files field_solvers/poisson_solvers_parallel | grep F90| xargs cat | wc -l)
+EC07=$(git ls-files linear_solvers | grep F90| xargs cat | wc -l)
+EC08=$(git ls-files parallelization/collective | grep F90| xargs cat | wc -l)
+EC09=$(git ls-files low_level_utilities/assert | grep F90| xargs cat | wc -l)
+EC10=$(git ls-files low_level_utilities/constants | grep F90| xargs cat | wc -l)
+EC11=$(git ls-files low_level_utilities/memory | grep F90| xargs cat | wc -l)
+EC12=$(git ls-files low_level_utilities/timer | grep F90| xargs cat | wc -l)
+EC13=$(git ls-files low_level_utilities/utilities | grep F90| xargs cat | wc -l)
+EC14=$(git ls-files low_level_utilities/working_precision | grep F90| xargs cat | wc -l)
+EC15=$(git ls-files mesh/coordinate_transformations | grep F90| xargs cat | wc -l)
+EC16=$(git ls-files mesh/mesh_calculus | grep F90| xargs cat | wc -l)
+EC17=$(git ls-files parallelization/parallel_utilities | grep F90| xargs cat | wc -l)
+EC18=$(git ls-files parallelization/point_to_point_communications | grep F90| xargs cat | wc -l)
+EC19=$(git ls-files parallelization/remap | grep F90| xargs cat | wc -l)
 
-PH=$(git ls-files -z ${1} field_solvers/general_coordinate_elliptic_solvers | xargs -0 cat | wc -l)
-echo 'PH='$PH
+EC=$((EC00+EC01+EC02+EC03+EC04+EC05+EC06+EC07+EC08+EC09+EC10+EC11+EC12+EC13+EC14+EC15+EC16+EC17+EC18+EC19))
 
-CS=$(git ls-files -z ${1} field_solvers/gyroaverage | xargs -0 cat | wc -l)
-echo 'CS='$CS
+YG0=$(git ls-files data_structures/vector_space | grep F90| xargs cat | wc -l)
+YG1=$(git ls-files time_integration/ode_integrators | grep F90| xargs cat | wc -l)
+YG2=$(git ls-files io/xdmf_io | grep F90| xargs cat | wc -l)
+YG3=$(git ls-files io/xdmf_io_parallel | grep F90| xargs cat | wc -l)
+YG4=$(git ls-files low_level_utilities/errors | grep F90| xargs cat | wc -l)
+YG5=$(git ls-files parallelization/parallel_array_utilities | grep F90| xargs cat | wc -l)
+YG6=$(git ls-files time_integration/ode_solvers | grep F90| xargs cat | wc -l)
 
-PN=$(git ls-files -z ${1} field_solvers/maxwell_solvers | xargs -0 cat | wc -l)
-PN=$(git ls-files -z ${1} field_solvers/maxwell_solvers_parallel | xargs -0 cat | wc -l)
-PN=$(git ls-files -z ${1} io/file_io | xargs -0 cat | wc -l)
-PN=$(git ls-files -z ${1} io/file_io_parallel | xargs -0 cat | wc -l)
-PN=$(git ls-files -z ${1} particle_methods/pic_visu | xargs -0 cat | wc -l)
-echo 'PN='$PN
+YG=$((YG0+YG1+YG2+YG3+YG4+YG5+YG6))
 
-MM=$(git ls-files -z ${1} interpolation/hermite_interpolation | xargs -0 cat | wc -l)
-MM=$(git ls-files -z ${1} field_solvers/quasi_neutral_solvers_parallel | xargs -0 cat | wc -l)
-MM=$(git ls-files -z ${1} field_solvers/quasi_neutral_solvers | xargs -0 cat | wc -l)
-MM=$(git ls-files -z ${1} initialization_tools/namelist_initialization | xargs -0 cat | wc -l)
-MM=$(git ls-files -z ${1} interpolation/periodic_interpolation | xargs -0 cat | wc -l)
-MM=$(git ls-files -z ${1} parallelization/reduction | xargs -0 cat | wc -l)
-MM=$(git ls-files -z ${1} semi_lagrangian/advection | xargs -0 cat | wc -l)
-MM=$(git ls-files -z ${1} semi_lagrangian/fcisl | xargs -0 cat | wc -l)
-MM=$(git ls-files -z ${1} time_integration/characteristics | xargs -0 cat | wc -l)
-echo 'MM='$MM
+PH=$(git ls-files field_solvers/general_coordinate_elliptic_solvers | grep F90| xargs cat | wc -l)
 
-AR=$(git ls-files -z ${1} interfaces/sparse_matrix_manager | xargs -0 cat | wc -l)
-echo 'AR='$AR
+PH=$((PH))
 
-LM=$(git ls-files -z ${1} mesh/meshes | xargs -0 cat | wc -l)
-LM=$(git ls-files -z ${1} quadrature | xargs -0 cat | wc -l)
-echo 'LM='$LM
+CS=$(git ls-files field_solvers/gyroaverage | grep F90| xargs cat | wc -l)
 
-X=$(git ls-files -z ${1} field_solvers/poisson_solvers | xargs -0 cat | wc -l)
-PN+=PN/3
-AH+=X/3
-MM+=X/3
-exit
+CS=$((CS))
 
-echo 'KR'
-KR=$(git ls-files -z ${1} parallelization/decomposition | xargs -0 cat | wc -l)
+PN0=$(git ls-files field_solvers/maxwell_solvers | grep F90| xargs cat | wc -l)
+PN1=$(git ls-files field_solvers/maxwell_solvers_parallel | grep F90| xargs cat | wc -l)
+PN2=$(git ls-files io/file_io | grep F90| xargs cat | wc -l)
+PN3=$(git ls-files io/file_io_parallel | grep F90| xargs cat | wc -l)
+PN4=$(git ls-files particle_methods/pic_visu | grep F90| xargs cat | wc -l)
 
-echo 'JA'
-JA=$(git ls-files -z ${1} particle_methods/pic_1d | xargs -0 cat | wc -l)
-JA=$(git ls-files -z ${1} particle_methods/pif | xargs -0 cat | wc -l)
+PN=$((PN0+PN1+PN2+PN3+PN4))
 
-echo 'SH'
-SH=$(git ls-files -z ${1} particle_methods/pic_2d_standard | xargs -0 cat | wc -l)
-SH=$(git ls-files -z ${1} particle_methods/random_deviate_generators | xargs -0 cat | wc -l)
+MM0=$(git ls-files interpolation/hermite_interpolation | grep F90| xargs cat | wc -l)
+MM1=$(git ls-files field_solvers/quasi_neutral_solvers_parallel | grep F90| xargs cat | wc -l)
+MM2=$(git ls-files field_solvers/quasi_neutral_solvers | grep F90| xargs cat | wc -l)
+MM3=$(git ls-files initialization_tools/namelist_initialization | grep F90| xargs cat | wc -l)
+MM4=$(git ls-files interpolation/periodic_interpolation | grep F90| xargs cat | wc -l)
+MM5=$(git ls-files parallelization/reduction | grep F90| xargs cat | wc -l)
+MM6=$(git ls-files semi_lagrangian/advection | grep F90| xargs cat | wc -l)
+MM7=$(git ls-files semi_lagrangian/fcisl | grep F90| xargs cat | wc -l)
+MM8=$(git ls-files time_integration/characteristics | grep F90| xargs cat | wc -l)
 
-echo 'MCP+ALH' 
-MCP=$(git ls-files -z ${1} particle_methods/pic_remapped | xargs -0 cat | wc -l)
+MM=$((MM0+MM1+MM2+MM3+MM4+MM5+MM6+MM7+MM8))
 
-echo 'ECG+LM+MM+ES'
-ES=$(git ls-files -z ${1} splines | xargs -0 cat | wc -l)
+AR=$(git ls-files interfaces/sparse_matrix_manager | grep F90| xargs cat | wc -l)
+
+LM0=$(git ls-files mesh/meshes | grep F90| xargs cat | wc -l)
+LM1=$(git ls-files quadrature | grep F90| xargs cat | wc -l)
+LM=$((LM0+LM1))
+
+X=$(git ls-files field_solvers/poisson_solvers | grep F90| xargs cat | wc -l)
+PN=$((PN+X/3))
+AH=$((AH+X/3))
+MM=$((MM+X/3))
+
+KR=$(git ls-files parallelization/decomposition | grep F90| xargs cat | wc -l)
+
+JA0=$(git ls-files particle_methods/pic_1d | grep F90| xargs cat | wc -l)
+JA1=$(git ls-files particle_methods/pif | grep F90| xargs cat | wc -l)
+JA=$((JA0+JA1))
+
+SH0=$(git ls-files particle_methods/pic_2d_standard | grep F90| xargs cat | wc -l)
+SH1=$(git ls-files particle_methods/random_deviate_generators | grep F90| xargs cat | wc -l)
+SH=$((SH0+SH1))
+
+X=$(git ls-files particle_methods/pic_remapped | grep F90| xargs cat | wc -l)
+MC=$((X/2))
+AL=$((X/2))
+
+ES=$(git ls-files splines | grep F90| xargs cat | wc -l)
+
+SL=$((AB+AR+KK+EC+YG+CS+PN+MM+AR+LM+KR+H+JA+MC+ES+AB+AR))
+echo 'SL='$((SL))
+echo 'SL='$(git ls-files . | grep F90| xargs cat | wc -l)
+
+echo 'EC='$((EC*1000/SL))
+echo 'YG='$((YG*1000/SL))
+echo 'CS='$((CS*1000/SL))
+echo 'PN='$((PN*1000/SL))
+echo 'MM='$((MM*1000/SL))
+echo 'AR='$((AR*1000/SL))
+echo 'LM='$((LM*1000/SL))
+echo 'KR='$((KR*1000/SL))
+echo 'SH='$((SH*1000/SL))
+echo 'JA='$((JA*1000/SL))
+echo 'MC='$((MC*1000/SL))
+echo 'AL='$((AL*1000/SL))
+echo 'ES='$((ES*1000/SL))
+echo 'AB='$((AB*1000/SL))
+echo 'AR='$((AR*1000/SL))
+echo 'KK='$((KK*1000/SL))
