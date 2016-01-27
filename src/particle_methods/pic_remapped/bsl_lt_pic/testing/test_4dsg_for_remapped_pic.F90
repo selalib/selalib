9ca1f4b1d85bb14ff20c676235346bd6e33f4315
Antoine Le Hyaric <Antoine.Le_Hyaric@upmc.fr>
2016-01-27 09:28:39 +0100

    DebianTesting64 (I)

--- src/particle_methods/pic_remapped/bsl_lt_pic/testing/test_4dsg_for_remapped_pic.F90
+++ src/particle_methods/pic_remapped/bsl_lt_pic/testing/test_4dsg_for_remapped_pic.F90
@@ -1,10 +1,6 @@
 ! \file 
-! 
 ! \brief Testing the sparse interpolation algorithm from [[selalib:src/add_ons/sparse_grid]]
-! 
-! 
 ! \author MCP and ALH
-! 
 !   This code SeLaLib (for Semi-Lagrangian-Library) 
 !   is a parallel library for simulating the plasma turbulence 
 !   in a tokamak.
@@ -16,8 +12,8 @@
 !   circulated by CEA, CNRS and INRIA at the following URL
 !   "http://www.cecill.info". 
 ! 
-! [[shell:header\alh 'test_4dsg_for_remapped_pic.F90']] (cf [[file:~/alh/bin/headeralh]])
-! emacs-keywords authors="MCP and ALH" brief="Testing the sparse interpolation algorithm from [[selalib:src/add_ons/sparse_grid]]" default=0 dox f90 selalib start=02/11/15
+! [[elisp:(alh-set-keywords)]] ([[file:~/.emacs::alh-set-keywords]])
+! emacs-keywords authors="MCP and ALH" brief="Testing the sparse interpolation algorithm from [[selalib:src/add_ons/sparse_grid]]" dox f90 selalib start=02/11/15
 
 ! <<<<test_4dsg_for_remapped_pic>>>> The goal of this test is to reproduce the same function interpolations as in
 ! [[selalib:src/add_ons/sparse_grid/testing/test_sparse_grid_4d.F90::test_interpolation_4d]] (from directory
@@ -51,8 +47,7 @@ program test_4dsg_for_remapped_pic
   sll_real64,dimension(4) :: dx
 
   ! <<interp>> Interpolation object of type
-  ! [[selalib:src/add_ons/sparse_grid/sll_m_sparse_grid_4d.F90::sparse_grid_interpolator_4d]]
-  ! which extends
+  ! [[selalib:src/add_ons/sparse_grid/sll_m_sparse_grid_4d.F90::sparse_grid_interpolator_4d]] which extends
   ! [[selalib:src/add_ons/sparse_grid/sll_m_sparse_grid_interpolator.F90::type,%20public%20::%20sparse_grid_interpolator]]
   
   type(sparse_grid_interpolator_4d), target   :: interp
@@ -83,13 +78,13 @@ program test_4dsg_for_remapped_pic
   ! [[order]] This is a tunable parameter.
   order = 1;
   
-  ! Initialize sparse grid [[selalib:src/add_ons/sparse_grid/sll_m_sparse_grid_4d.F90::subroutine%20initialize_sg4d]]
+  ! Initialize sparse grid [[selalib:src/add_ons/sparse_grid/sll_m_sparse_grid_4d.F90::subroutine%20initialize_sg4d]] in
+  ! [[selalib:src/add_ons/sparse_grid/]]
   
   call interp%initialize(levelsini,order,order+1,0,eta_min,eta_max);
 
-  ! Initialize [[f]].
-  ! [[selalib:src/add_ons/sparse_grid/sll_m_sparse_grid_interpolator.F90::size_basis]]
-  ! is the number of grid points.
+  ! Initialize [[f]].  [[selalib:src/add_ons/sparse_grid/sll_m_sparse_grid_interpolator.F90::size_basis]] is the number of grid
+  ! points.
 
   SLL_ALLOCATE(f(interp%size_basis),ierr);
   do j=1,interp%size_basis
@@ -104,6 +99,7 @@ program test_4dsg_for_remapped_pic
   end do
 
   ! Evaluate a few random points
+  ! [[file:~/selalib/src/add_ons/sparse_grid/sll_m_sparse_grid_interpolator.F90::subroutine%20compute_hierarchical_surplus]]
 
   ferrmax = 0._f64
   call interp%compute_hierarchical_surplus(f)
@@ -112,6 +108,9 @@ program test_4dsg_for_remapped_pic
      dx(2) = randompoint(eta_min(2),eta_max(2));
      dx(3) = randompoint(eta_min(3),eta_max(3));
      dx(4) = randompoint(eta_min(4),eta_max(4));
+
+     ! [[file:~/selalib/src/add_ons/sparse_grid/sll_m_sparse_grid_4d.F90::function%20interpolate_value]]
+
      finterp = interp%interpolate_value(f,dx);
      fref = testfunction(dx(1),dx(2),dx(3),dx(4));
      ferr = abs(finterp-fref)
