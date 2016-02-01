36672600df996f804fcfd5dd971e4f64c4dc7f13
Katharina Kormann <katharina.kormann@tum.de>
2016-02-01 12:49:36 +0100

    [fft] Remove unnecessary pointer in sll_m_sim_bsl_gc_2d0v_curv.

--- simulations/parallel/bsl_gc_2d0v_curv/sll_m_sim_bsl_gc_2d0v_curv.F90
+++ simulations/parallel/bsl_gc_2d0v_curv/sll_m_sim_bsl_gc_2d0v_curv.F90
@@ -2327,7 +2327,7 @@ subroutine compute_field_from_phi_2d_fd_curvilinear(phi,mesh_2d,transformation,A
     sll_real64 :: delta_eta2
     sll_real64 :: eta1
     sll_int32 :: ierr 
-    type(sll_t_fft), pointer         :: pfwd
+    type(sll_t_fft)         :: pfwd
     
     Nc_eta1 = mesh_2d%num_cells1
     Nc_eta2 = mesh_2d%num_cells2
@@ -2341,7 +2341,7 @@ subroutine compute_field_from_phi_2d_fd_curvilinear(phi,mesh_2d,transformation,A
     
     SLL_ALLOCATE(int_r(Nc_eta2),ierr)
     SLL_ALLOCATE(data(Nc_eta1+1),ierr)
-    allocate(pfwd)
+    !allocate(pfwd)
     call sll_s_fft_init_r2r_1d(pfwd,Nc_eta2,int_r,int_r,sll_p_fft_forward,normalized = .TRUE.)
  
     w     = 0.0_f64
@@ -2405,7 +2405,7 @@ subroutine compute_field_from_phi_2d_fd_curvilinear(phi,mesh_2d,transformation,A
       time_mode(1:8)!,mode_slope
 
     call sll_s_fft_free(pfwd)
-    deallocate(pfwd)
+    !deallocate(pfwd)
     
   end subroutine time_history_diagnostic_polar
   
