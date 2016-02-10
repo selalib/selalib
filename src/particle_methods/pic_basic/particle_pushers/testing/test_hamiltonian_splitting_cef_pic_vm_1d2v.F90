a48e9032bca137a9644a344ccdf4957f2cf599d5
Katharina Kormann <katharina.kormann@tum.de>
2016-02-10 21:14:30 +0100

    [pic] New conventions applied for maxwell_1d.

--- src/particle_methods/pic_basic/particle_pushers/testing/test_hamiltonian_splitting_cef_pic_vm_1d2v.F90
+++ src/particle_methods/pic_basic/particle_pushers/testing/test_hamiltonian_splitting_cef_pic_vm_1d2v.F90
@@ -26,8 +26,7 @@ program test_hamiltonian_splitting_cef_pic_vm_1d2v
     sll_c_maxwell_1d_base
 
   use sll_m_maxwell_1d_fem, only: &
-    sll_t_maxwell_1d_fem, &
-    sll_f_new_maxwell_1d_fem
+    sll_t_maxwell_1d_fem
 
   use sll_m_particle_group_1d2v, only: &
     sll_s_new_particle_group_1d2v_ptr
@@ -126,8 +125,13 @@ program test_hamiltonian_splitting_cef_pic_vm_1d2v
        n_particles, degree_smoother, sll_p_galerkin) 
   
   ! Initialize Maxwell solver
-  maxwell_solver => sll_f_new_maxwell_1d_fem([eta_min, eta_max], num_cells, &
-       degree_smoother)
+    ! Initialize Maxwell solver
+  allocate( sll_t_maxwell_1d_fem :: maxwell_solver )
+  select type ( maxwell_solver )
+  type is ( sll_t_maxwell_1d_fem )
+     call maxwell_solver%init( [eta_min, eta_max], num_cells, &
+          degree_smoother)
+  end select
 
   SLL_ALLOCATE(efield(kernel_smoother_0%n_dofs,2),ierr)
   SLL_ALLOCATE(bfield(kernel_smoother_0%n_dofs),ierr)
@@ -250,7 +254,7 @@ program test_hamiltonian_splitting_cef_pic_vm_1d2v
   deallocate(kernel_smoother_0)
   call kernel_smoother_1%delete()
   deallocate(kernel_smoother_1)
-  call maxwell_solver%delete()
+  call maxwell_solver%free()
   deallocate(maxwell_solver)
   call propagator%delete()
   
