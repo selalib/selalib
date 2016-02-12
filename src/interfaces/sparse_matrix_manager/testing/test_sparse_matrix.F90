da17e2729484967d601bf3d1cb1b778c95e08ef6
Pierre Navaro <navaro@math.cnrs.fr>
2016-02-12 17:59:53 +0100

    [issue #6] Add umfpack as an option

--- src/interfaces/sparse_matrix_manager/testing/test_sparse_matrix.F90
+++ src/interfaces/sparse_matrix_manager/testing/test_sparse_matrix.F90
@@ -8,7 +8,10 @@ use sll_m_collective
 
 implicit none
 
-type(sll_t_csr_matrix),       pointer     :: mat
+type(sll_t_csr_matrix),     pointer     :: mat
+#ifdef UMFPACK
+type(sll_t_csr_matrix),     pointer     :: mat2
+#endif /* UMFPACK */
 sll_int32                               :: num_rows
 sll_int32                               :: num_elements
 sll_int32,  dimension(:,:), allocatable :: local_to_global_row
@@ -18,14 +21,18 @@ sll_int32                               :: num_local_dof_row
 sll_int32                               :: ierr
 sll_int32                               :: i
 sll_real64                              :: val
+
+#ifdef PASTIX
+
 sll_int32                               :: prank
 sll_int32                               :: psize
 
 call sll_s_boot_collective()
 prank = sll_f_get_collective_rank( sll_v_world_collective )
 psize = sll_f_get_collective_size( sll_v_world_collective )
-
 write(*,*) " #", prank, " of ", psize
+
+#endif /* PASTIX */
 
 num_rows = 2000
 num_elements = num_rows
@@ -43,14 +50,15 @@ enddo
 b = 1._f64
 
 print *,'#begin sll_f_new_csr_matrix'
-mat => sll_f_new_csr_matrix( &
-  num_rows, &
-  num_rows, &
-  num_elements, &
-  local_to_global_row, &
-  num_local_dof_row, &
-  local_to_global_row, &
-  num_local_dof_row)
+
+mat => sll_f_new_csr_matrix(  num_rows,            &
+                              num_rows,            &
+                              num_elements,        &
+                              local_to_global_row, &
+                              num_local_dof_row,   &
+                              local_to_global_row, &
+                              num_local_dof_row)
+
 print *,'#end sll_f_new_csr_matrix'
 
 !do identity matrix
@@ -71,6 +79,51 @@ else
   print *,'#system is not good resolved',maxval(abs(x))
 endif
 
+call sll_s_delete_csr_matrix(mat)
+
+#ifdef PASTIX
 call sll_s_halt_collective()
+#endif /* PASTIX */
+
+#ifdef UMFPACK
+
+print *,'#begin sll_f_new_csr_matrix with umfpack'
+
+mat2 => sll_f_new_csr_matrix( num_rows,            &
+                              num_rows,            &
+                              num_elements,        &
+                              local_to_global_row, &
+                              num_local_dof_row,   &
+                              local_to_global_row, &
+                              num_local_dof_row,   &
+                              sll_p_umfpack )
+
+print *,'#end sll_f_new_csr_matrix with umfpack'
+
+!do identity matrix
+print *,'#begin add_to_csr_matrix with umfpack'
+val = 1._f64
+do i=1,num_rows
+  call sll_s_add_to_csr_matrix(mat2, val, i, i)
+enddo
+print *,'#end add_to_csr_matrix with umfpack'
+
+print *,'#begin sll_s_solve_csr_matrix with umfpack'
+call sll_s_solve_csr_matrix(mat2, b, x)
+print *,'#end sll_s_solve_csr_matrix with umfpack'
+  
+if(maxval(abs(x-1.0_f64))<1.e-13)then
+  print *,'#PASSED'
+else
+  print *,'#system is not good resolved with umfpack',maxval(abs(x))
+endif
+
+call sll_s_delete_csr_matrix(mat2)
+
+#endif /* UMFPACK */
+
+#ifdef PASTIX
+call sll_s_halt_collective()
+#endif /* PASTIX */
 
 end program
