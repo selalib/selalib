5b4dc4f778c38f13476a20e1da239faa607244fc
Laura Mendoza <lasofivec@gmail.com>
2016-02-13 22:52:25 +0100

    [boxsplines] working on prefilters

--- src/splines/testing/test_box_splines_derivatives.F90
+++ src/splines/testing/test_box_splines_derivatives.F90
@@ -21,9 +21,12 @@ program test_box_splines_derivatives
        new_box_spline_2d, &
        boxspline_x1_derivative, &
        boxspline_x2_derivative, &
-       compute_box_spline
+       compute_box_spline, &
+       hex_interpolate_value
   use sll_m_hex_pre_filters, only : &
        pre_filter_pfir
+  use sll_m_constants, only : &
+       sll_pi
   implicit none
 
   ! Variable to check is successful. By default it is the case.
@@ -38,11 +41,17 @@ program test_box_splines_derivatives
   sll_int32    :: degree
   sll_int32    :: rule
   sll_int32    :: i
-  sll_real64   :: x1
-  sll_real64   :: x2
+  sll_real64   :: x1, x1p
+  sll_real64   :: x2, x2p
   sll_real64   :: somme
   sll_real64   :: val
+  sll_real64   :: error_current
+  sll_real64   :: error_linf
+  sll_real64   :: error_l2
+  sll_real64   :: appx_f
+  sll_real64   :: randnum
   sll_real64, dimension(:), allocatable :: filters
+  sll_real64, dimension(:), allocatable :: dist
   sll_real64, dimension(:), allocatable :: f
   sll_real64, dimension(:), allocatable :: dxf
   sll_real64, dimension(:), allocatable :: dyf
@@ -58,7 +67,7 @@ program test_box_splines_derivatives
 
   print *, "  Test will be made on the following mesh:"
   ! Mesh initialization
-  num_cells = 20
+  num_cells = 80
   mesh => new_hex_mesh_2d(num_cells, 0._f64, 0._f64, radius = 2._f64)
   call mesh%display()
 
@@ -73,30 +82,62 @@ program test_box_splines_derivatives
   SLL_ALLOCATE(f2(mesh%num_pts_tot),ierr)
   SLL_ALLOCATE(dxf2(mesh%num_pts_tot),ierr)
   SLL_ALLOCATE(dyf2(mesh%num_pts_tot),ierr)
+  ! Allocation for a distribution function
+  SLL_ALLOCATE(dist(mesh%num_pts_tot),ierr)
+
+  ! Initialization of distribution function
+  do i=1, mesh%num_pts_tot
+     x1 = mesh%global_to_x1(i)
+     x2 = mesh%global_to_x2(i)
+     dist(i) = cos(x1*2.0*sll_pi)
+  end do
 
   do degree=1,max_deg
+     ! Computing box-splines coefficients:
+     call spline%compute_coeff_box_spline_2d(dist, degree)
+     ! Reseting errors:
+     error_linf = 0._f64
+     error_l2   = 0._f64
      do i=1, mesh%num_pts_tot
         x1 = mesh%global_to_x1(i)
         x2 = mesh%global_to_x2(i)
+        if (i .lt. 3*(num_cells-4)*(num_cells-5)+1) then
+           call random_number(randnum)
+           x1p = x1 - randnum/num_cells
+           x2p = x2 - randnum/num_cells
+           ! Interpolation on mesh points computation:
+           appx_f = hex_interpolate_value(mesh, x1p, x2p, spline, degree)
+           ! Computing errors:
+           error_current = ABS(cos(x1p*2.0*sll_pi) - appx_f)
+           error_l2   = error_l2 + error_current**2
+           error_linf = max(error_linf, error_current)
+        end if
+        ! Computing values of box-splines and derivatives
+        ! Note: This is not needed for interpolation
         f(i) = compute_box_spline(spline, x1, x2, degree)
         dxf(i) = boxspline_x1_derivative(x1, x2, degree)
         dyf(i) = boxspline_x2_derivative(x1, x2, degree)
      end do
+     ! Storing degree two results:
      if (degree .eq. 2) then
         f2(:) = f(:)
         dxf2(:) = dxf(:)
         dyf2(:) = dyf(:)
      end if
+     ! Printing errors of interpolation and values of box-splines :
      print *, " * Degree =", degree
-     print *, "    --> sum boxspline      = ", sum(f)
-     print *, "    --> sum 1st derivative = ", sum(dxf)
-     print *, "    --> sum 2nd derivative = ", sum(dyf)
+     print *, "    --> Error in L_inf     = ", error_linf
+     print *, "    --> Error in L_2       = ", SQRT(error_l2)
+     ! print *, "    --> sum boxspline      = ", sum(f)
+     ! print *, "    --> sum 1st derivative = ", sum(dxf)
+     ! print *, "    --> sum 2nd derivative = ", sum(dyf)
      if (abs(sum(f)-1.0_f64).gt.criterion) then
         passed_test = .false.
         print *, " Test FAILED for box-spline of degree = ", degree
         STOP
      end if
   end do
+
 
   !Wrtting on docs:
   call write_field_hex_mesh_xmf(mesh,   f2, "boxspline2")
