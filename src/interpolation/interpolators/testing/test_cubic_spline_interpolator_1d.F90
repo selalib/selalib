51715b4852d89e27c48debc92623f0f77afd6127
Katharina Kormann <katharina.kormann@tum.de>
2016-03-04 12:45:16 +0100

    [cubic spline interpolation] Add second test for 20 points to test both algorithms.

--- src/interpolation/interpolators/testing/test_cubic_spline_interpolator_1d.F90
+++ src/interpolation/interpolators/testing/test_cubic_spline_interpolator_1d.F90
@@ -18,65 +18,84 @@ program cubic_spline_interpolator_1d
   implicit none
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-class(sll_c_interpolator_1d), pointer       :: interp
-type(sll_t_cubic_spline_interpolator_1d), target :: spline
+    sll_int32, parameter  :: n1 = 20
+    sll_int32, parameter  :: n2 = 64
+    sll_int32, parameter  :: m = 512
+    sll_real64, parameter :: tol1 = 1.d-4 ! tolerance for error (n1)
+    sll_real64, parameter :: tol2 = 1.d-6 ! tolerance for error (n2)
+    sll_real64            :: error1
+    sll_real64            :: error2
 
-sll_real64                            :: error
-sll_real64, allocatable, dimension(:) :: point
-sll_real64, allocatable, dimension(:) :: pdata  
-sll_real64, allocatable, dimension(:) :: fdata
-sll_real64, allocatable, dimension(:) :: coord
-sll_real64, allocatable, dimension(:) :: gdata
+    call test_for_given_n( n1, error1 )
+    call test_for_given_n( n2, error2 )
 
-sll_int32 :: ierr, i
-sll_int32, parameter :: n = 64
-sll_int32, parameter :: m = 512
-sll_real64, parameter :: tol = 1.d-3 ! tolerance for error
-
-sll_real64  :: x_min, x_max, delta
-
-SLL_ALLOCATE(coord(n), ierr)
-SLL_ALLOCATE(pdata(n), ierr)
-SLL_ALLOCATE(point(m), ierr)
-SLL_ALLOCATE(fdata(m), ierr)
-SLL_ALLOCATE(gdata(m), ierr)
-
-print*, 'Initialize data and point array'
-x_min = 0.0_f64
-x_max = 2.0_f64 * sll_p_pi
-delta = (x_max - x_min ) / real(n-1,f64) 
-do i=1,n
-   coord(i) = (i-1)*delta
-   pdata(i) = f(coord(i))
-!print*, i,coord(i), pdata(i)
-end do
-
-delta = (x_max - x_min ) / real(m-1,f64) 
-do i=1,m
-   point(i) = (i - 1) * delta
-   gdata(i) = f(point(i))
-end do
-
-print*, 'Cubic spline interpolation'
-call spline%initialize(n, x_min, x_max, sll_p_periodic )
-
-interp => spline
-call interp%compute_interpolants(pdata)
-
-do i = 1, m
-   fdata(i) = interp%interpolate_from_interpolant_value(point(i))
-end do
-
-error = maxval(abs(gdata-fdata))
-print*, 'error=', error
-if (error < tol) then
-   print*, 'Successful, exiting program.'
-   print*, 'PASSED'
-else
-   print*, 'FAILED'
-end if
+    if (error1 > tol1) then
+       print*, 'Failed for n=', n1
+       print*, 'FAILED.'
+    elseif (error2 > tol2) then
+       print*, 'Failed for n=', n2
+       print*, 'FAILED.'
+    else
+       print*, 'Successful, exiting program.'
+       print*, 'PASSED'
+    end if
 
 contains
+
+  subroutine test_for_given_n (n, error)
+    sll_int32,  intent(in)  :: n
+    sll_real64, intent(out) :: error
+
+    class(sll_c_interpolator_1d), pointer       :: interp
+    type(sll_t_cubic_spline_interpolator_1d), target :: spline
+
+    sll_real64, allocatable, dimension(:) :: point
+    sll_real64, allocatable, dimension(:) :: pdata  
+    sll_real64, allocatable, dimension(:) :: fdata
+    sll_real64, allocatable, dimension(:) :: coord
+    sll_real64, allocatable, dimension(:) :: gdata
+    
+    sll_int32 :: ierr, i
+    
+    sll_real64  :: x_min, x_max, delta
+    
+    SLL_ALLOCATE(coord(n), ierr)
+    SLL_ALLOCATE(pdata(n), ierr)
+    SLL_ALLOCATE(point(m), ierr)
+    SLL_ALLOCATE(fdata(m), ierr)
+    SLL_ALLOCATE(gdata(m), ierr)
+    
+    print*, 'Initialize data and point array'
+    x_min = 0.0_f64
+    x_max = 2.0_f64 * sll_p_pi
+    delta = (x_max - x_min ) / real(n-1,f64) 
+    do i=1,n
+       coord(i) = (i-1)*delta
+       pdata(i) = f(coord(i))
+       !print*, i,coord(i), pdata(i)
+    end do
+    
+    delta = (x_max - x_min ) / real(m-1,f64) 
+    do i=1,m
+       point(i) = (i - 1) * delta
+       gdata(i) = f(point(i))
+    end do
+    
+    print*, 'Cubic spline interpolation'
+    call spline%initialize(n, x_min, x_max, sll_p_periodic )
+    
+    interp => spline
+    call interp%compute_interpolants(pdata)
+    
+    do i = 1, m
+       fdata(i) = interp%interpolate_from_interpolant_value(point(i))
+    end do
+    
+    error = maxval(abs(gdata-fdata))
+    print*, 'error=', error
+    
+
+  end subroutine test_for_given_n
 
   function f(x)
 
