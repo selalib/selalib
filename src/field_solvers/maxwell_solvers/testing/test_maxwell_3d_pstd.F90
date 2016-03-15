76bd068c598792be4355196a59f13f958b4c0648
Pierre Navaro <navaro@math.cnrs.fr>
2016-03-15 10:26:23 +0100

    [test maxwell 3d] Add analytical solution
    - The test does not work.
    - Add periodic boundary conditions

--- src/field_solvers/maxwell_solvers/testing/test_maxwell_3d_pstd.F90
+++ src/field_solvers/maxwell_solvers/testing/test_maxwell_3d_pstd.F90
@@ -29,7 +29,6 @@ sll_int32                                 :: i, j, k
 sll_real64                                :: omega
 sll_real64                                :: time
 sll_int32                                 :: istep, nstep
-sll_real64                                :: err
 sll_real64                                :: dt
 sll_real64                                :: cfl = 0.5_f64
 sll_real64, dimension(:,:,:), allocatable :: x
@@ -46,19 +45,19 @@ sll_real64  :: pi = sll_p_pi
 
 call cpu_time(tstart)
 
-!w  = sqrt(6)*pi
+!w  =   sqrt(6)*pi
 !ex =   cos(pi*x)*sin(pi*y)*sin(-2*pi*z)*cos(w*t)
 !ey =   sin(pi*x)*cos(pi*y)*sin(-2*pi*z)*cos(w*t)
 !ez =   sin(pi*x)*sin(pi*y)*cos(-2*pi*z)*cos(w*t)
-!bx = - sin(pi*x)*cos(pi*y)*cos(-2*pi*z)*sin(w*t)*3*pi/w
-!by =   cos(pi*x)*sin(pi*y)*cos(-2*pi*z)*sin(w*t)*3*pi/w
-!bz = + 0
+!hx = - sin(pi*x)*cos(pi*y)*cos(-2*pi*z)*sin(w*t)*3*pi/w
+!hy =   cos(pi*x)*sin(pi*y)*cos(-2*pi*z)*sin(w*t)*3*pi/w
+!hz = + 0
 
 x_min = .0_f64; x_max = 1.0_f64
 y_min = .0_f64; y_max = 1.0_f64
 z_min = .0_f64; z_max = 1.0_f64
 
-nc_x = 32; nc_y = 32; nc_z = 32
+nc_x = 64; nc_y = 64; nc_z = 64
 
 delta_x = (x_max-x_min)/nc_x
 delta_y = (y_max-y_min)/nc_y
@@ -81,7 +80,7 @@ end do
 dt    = cfl/sqrt(1./delta_x**2+1./delta_y**2+1./delta_z**2)
 time  = 0._f64
 omega = sqrt(6.0_f64)*pi
-nstep = 2*pi / dt
+nstep = int(2*pi / dt,i32)
 
 print*, nstep*dt- 2*pi
 nstep = nstep +1
@@ -95,22 +94,28 @@ SLL_CLEAR_ALLOCATE(ex(1:nc_x+1,1:nc_y+1,1:nc_z+1), error)
 SLL_CLEAR_ALLOCATE(ey(1:nc_x+1,1:nc_y+1,1:nc_z+1), error)
 SLL_CLEAR_ALLOCATE(ez(1:nc_x+1,1:nc_y+1,1:nc_z+1), error)
 
-call sll_o_create(maxwell,                     &
+call sll_o_create(maxwell,            &
                   x_min, x_max, nc_x, &
                   y_min, y_max, nc_y, & 
                   z_min, z_max, nc_z  )
 
-ex = cos(pi*x)*sin(pi*y)*sin(-2.0_f64*pi*z)
-ey = sin(pi*x)*cos(pi*y)*sin(-2.0_f64*pi*z)
-ez = sin(pi*x)*sin(pi*y)*cos(-2.0_f64*pi*z)
+ex =   cos(pi*x)*sin(pi*y)*sin(-2.0_f64*pi*z)
+ey =   sin(pi*x)*cos(pi*y)*sin(-2.0_f64*pi*z)
+ez =   sin(pi*x)*sin(pi*y)*cos(-2.0_f64*pi*z)
+hx = - sin(pi*x)*cos(pi*y)*cos(-2.0_f64*pi*z)*sin(omega*(-0.5_f64*dt))*3.0_f64*pi/omega
+hy =   cos(pi*x)*sin(pi*y)*cos(-2.0_f64*pi*z)*sin(omega*(-0.5_f64*dt))*3.0_f64*pi/omega
+hz =   0.0_f64
+
 
 do istep = 1, nstep !*** Loop over time
 
   write(*,"(10x,' istep = ',I6)",advance="no") istep
   write(*,"(', time  = ',g12.3,' sec')",advance="no") time
-  write(*,"(', error = ',g15.5)") sum(abs(hx))/real(nc_x*nc_y*nc_z,f64)
+  write(*,"(', error = ',g15.5)") &
+  maxval(abs(hx+sin(pi*x)*cos(pi*y)*cos(-2*pi*z)*sin(omega*time)*3.0_f64*pi/omega)) 
 
-  call sll_o_solve(maxwell, hx, hy, hz, ex, ey, ez, dt)
+  call ampere(maxwell, ex, ey, ez, hx, hy, hz, dt) 
+  call faraday(maxwell, ex, ey, ez, hx, hy, hz, dt)   
 
   time = time + dt
 
