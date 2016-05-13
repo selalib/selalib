39417e2f5145cfb6432910cfe079a5122514ca64
Pierre Navaro <navaro@math.cnrs.fr>
2016-05-13 16:45:17 +0200

    [doxygen] Library tab
    - Fix missing description.
    - Remove non highlighted links
    - Change logo
    - Change project name and brief

--- src/particle_methods/pic_basic/particle_mesh_coupling/particle_mesh_coupling_doc.F90
+++ src/particle_methods/pic_basic/particle_mesh_coupling/particle_mesh_coupling_doc.F90
@@ -1,8 +1,8 @@
 !> @defgroup kernel_smoothers sll_particle_mesh_coupling
 !> @brief
-!> Library implementing kernel smoothers for accumulation and evaluation of charge densities for pic_basic.
+!> Kernel smoothers for accumulation and evaluation of charge densities.
+!> @author Katharina Kormann, IPP
 !> @details
 !> ...
 !>
-!> @author Katharina Kormann, IPP
 !> @todo Implement 3d splines and other basis functions, e.g. Fourier
