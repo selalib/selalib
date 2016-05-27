b98032907b1f05e38639c749341b49e274a2c156
Pierre Navaro <navaro@math.cnrs.fr>
2016-05-27 15:32:50 +0200

    [intel] Upgrade some build scripts

--- scripts/poincare-intel.sh
+++ scripts/poincare-intel.sh
@@ -1,13 +1,11 @@
-module load cmake
-module load intel-env
-module load mkl
-module load intelmpi
-module load fftw
-module load hdf5 petsc/3.5_intel_intelmpi
+module purge
+module load intel-env/15.0.0 
+module load openmpi/1.6.3_intel15.0.0 
+module load hdf5/1.8.16_intel_openmpi 
+module load fftw/3.3.3_intel
+module load cmake/3.4.3 
+module load mkl/11.2 
+module load python/epd
 export HDF5_ROOT=${HDF5_ROOT_DIR}
 export FFTW_ROOT=${FFTW_ROOT_DIR}/../
-cmake -DCMAKE_Fortran_COMPILER=ifort \
-      -DUSE_MKL=ON \
-      -DBUILD_JOREK=ON \
-      -DHDF5_PARALLEL_ENABLED=ON \
-      SELALIB_DIRECTORY/src
+cmake ~/selalib -DHDF5_PARALLEL_ENABLED=ON -DUSE_MKL=ON -DCMAKE_Fortran_COMPILER=ifort
