d522fc526e330619becfc5d0407c6742a62585f6
Pierre Navaro <navaro@math.cnrs.fr>
2016-05-30 11:22:58 +0200

    [poincare] Remove the fftw module to avoid conflict with MKL and fix the parallel build by changing the dependencies of fsl_vp_1d1v_tau simulation

--- scripts/poincare-intel.sh
+++ scripts/poincare-intel.sh
@@ -1,11 +1,10 @@
 module purge
 module load intel-env/15.0.0 
 module load openmpi/1.6.3_intel15.0.0 
-module load hdf5/1.8.16_intel_openmpi 
-module load fftw/3.3.3_intel
+module load hdf5/1.8.16_intel_openmpi
 module load cmake/3.4.3 
 module load mkl/11.2 
 module load python/epd
 export HDF5_ROOT=${HDF5_ROOT_DIR}
 export FFTW_ROOT=${FFTW_ROOT_DIR}/../
-cmake ~/selalib -DHDF5_PARALLEL_ENABLED=ON -DUSE_MKL=ON -DCMAKE_Fortran_COMPILER=ifort
+cmake ~/selalib -DHDF5_PARALLEL_ENABLED=ON -DUSE_MKL=ON -DCMAKE_Fortran_COMPILER=h5pfc
