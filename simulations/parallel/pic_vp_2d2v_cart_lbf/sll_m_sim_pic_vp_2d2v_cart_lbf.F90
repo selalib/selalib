ea974e12ab9b05d660cd6ea50c809f7ae361c83b
Martin Campos Pinto <campos@ann.jussieu.fr>
2016-07-06 10:36:14 +0200

    cleaned previous implementations of the lbf-resampled pic method

--- simulations/parallel/pic_vp_2d2v_cart_lbf/sll_m_sim_pic_vp_2d2v_cart_lbf.F90
+++ simulations/parallel/pic_vp_2d2v_cart_lbf/sll_m_sim_pic_vp_2d2v_cart_lbf.F90
@@ -1,11 +1,11 @@
 !> @ingroup particle_methods
 !> @author MCP ALH KK
-!> @brief Simulation of 2d2v Vlasov-Poisson with several PIC methods (including the resampled pic_lbfr),
+!> @brief Simulation of 2d2v Vlasov-Poisson with several PIC methods (including the lbf-resampled pic method),
 !>  periodic boundary conditions and Landau initial values along x1 only.
 
-! MCP: I am writing this simulation from Katharina's sll_m_sim_pic_vp_2d2v_cart simulation, to add the pic_lbfr particles
+! MCP: I am writing this simulation from Katharina's sll_m_sim_pic_vp_2d2v_cart simulation, to add the lbf particle group
 
-module sll_m_sim_pic_vp_2d2v_cart_lbfr
+module sll_m_sim_pic_vp_2d2v_cart_lbf
 
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #include "sll_errors.h"
@@ -46,9 +46,6 @@ module sll_m_sim_pic_vp_2d2v_cart_lbfr
     sll_s_new_particle_group_2d2v_ptr, &
     sll_t_particle_group_2d2v
 
-  use sll_m_pic_lbfr_4d_group, only: &    ! keeping this particle group for validation but should be discarded eventually
-    sll_s_new_pic_lbfr_4d_group_ptr
-
   use sll_m_particle_group_2d2v_lbf, only: &
     sll_s_new_particle_group_2d2v_lbf_ptr
 
@@ -88,19 +85,17 @@ module sll_m_sim_pic_vp_2d2v_cart_lbfr
   implicit none
 
   public :: &
-    sll_t_sim_pic_vp_2d2v_cart_lbfr, &
+    sll_t_sim_pic_vp_2d2v_cart_lbf, &
     sll_p_simple_particles,   &
-    sll_p_lbf_particles,  &
-    sll_p_lbf_particles_old
+    sll_p_lbf_particles
 
   private
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
   sll_int32, parameter :: sll_p_simple_particles=0
   sll_int32, parameter :: sll_p_lbf_particles=1
-  sll_int32, parameter :: sll_p_lbf_particles_old=2
 
-  type, extends(sll_c_simulation_base_class) :: sll_t_sim_pic_vp_2d2v_cart_lbfr
+  type, extends(sll_c_simulation_base_class) :: sll_t_sim_pic_vp_2d2v_cart_lbf
 
      ! Abstract particle group
      class(sll_c_particle_group_base), pointer :: particle_group
@@ -154,14 +149,14 @@ module sll_m_sim_pic_vp_2d2v_cart_lbfr
      procedure :: run => run_pic_2d2v
      procedure :: delete => delete_pic_2d2v
 
-  end type sll_t_sim_pic_vp_2d2v_cart_lbfr
+  end type sll_t_sim_pic_vp_2d2v_cart_lbf
 
   
 contains
 !------------------------------------------------------------------------------!
   ! Read in the simulation parameters from input file
   subroutine init_pic_2d2v (sim, filename)
-    class(sll_t_sim_pic_vp_2d2v_cart_lbfr), intent(inout) :: sim
+    class(sll_t_sim_pic_vp_2d2v_cart_lbf), intent(inout) :: sim
     character(len=*),                  intent(in)    :: filename
 
     sll_int32   :: n_time_steps
@@ -195,37 +190,13 @@ contains
     sll_real64  :: remapping_grid_vx_max
     sll_real64  :: remapping_grid_vy_min
     sll_real64  :: remapping_grid_vy_max
-    sll_int32,  dimension(4)  :: remapping_sparse_grid_max_levels
-    sll_real64, dimension(4)  :: remapping_grid_eta_min
-    sll_real64, dimension(4)  :: remapping_grid_eta_max
-
-    ! additional parameters for the old lbf particle group
-    sll_int32   :: remap_f_type
-    sll_int32   :: remapping_cart_grid_n_cells_x
-    sll_int32   :: remapping_cart_grid_n_cells_y
-    sll_int32   :: remapping_cart_grid_n_cells_vx
-    sll_int32   :: remapping_cart_grid_n_cells_vy
     sll_int32   :: remapping_sparse_grid_max_level_x
     sll_int32   :: remapping_sparse_grid_max_level_y
     sll_int32   :: remapping_sparse_grid_max_level_vx
     sll_int32   :: remapping_sparse_grid_max_level_vy
-    sll_int32   :: deposition_particles_pos_type
-    sll_int32   :: deposition_particles_move_type
-    sll_int32   :: n_deposition_particles
-    sll_int32   :: n_deposition_particles_per_cell_x   !< used with deposition particles on a struct grid
-    sll_int32   :: n_deposition_particles_per_cell_y   !< used with deposition particles on a struct grid
-    sll_int32   :: n_deposition_particles_vx           !< used with deposition particles on a struct grid
-    sll_int32   :: n_deposition_particles_vy           !< used with deposition particles on a struct grid
-    sll_int32   :: flow_markers_type
-    sll_int32   :: n_flow_markers_x
-    sll_int32   :: n_flow_markers_y
-    sll_int32   :: n_flow_markers_vx
-    sll_int32   :: n_flow_markers_vy
-    sll_int32   :: n_unstruct_markers_per_cell
-    sll_int32   :: flow_grid_n_cells_x
-    sll_int32   :: flow_grid_n_cells_y
-    sll_int32   :: flow_grid_n_cells_vx
-    sll_int32   :: flow_grid_n_cells_vy
+    sll_int32,  dimension(4)  :: remapping_sparse_grid_max_levels
+    sll_real64, dimension(4)  :: remapping_grid_eta_min
+    sll_real64, dimension(4)  :: remapping_grid_eta_max
 
     namelist /sim_params/         delta_t, n_time_steps, alpha, n_mode, thermal_v1, thermal_v2
     
@@ -235,63 +206,9 @@ contains
 
     namelist /simple_pic_params/  init_case_str, n_particles, with_control_variate
 
+    ! parameters for pic with lbf resamplings: we have 2 sets of parameters  -------------------------------------------------
 
-    ! OLD parameters for pic with lbf resamplings: we have 3 sets of parameters  -------------------------------------------------
-    !   - one for the discretization of the remapped f,
-    !   - one for the discretization of the deposited f
-    !   - one for the discretization of the flow
-
-    ! discretization of the remapped f
-    !   -> remapping period, type of interpolation structure and polynomial degree
-    !   -> size of the remapping grid / sparse grid
-    namelist /pic_lbf_remap_params_old/    &
-                                  remap_period,                    &
-                                  remap_f_type,                       &
-                                  remap_degree,                       &
-                                  remapping_grid_vx_min,              &   ! note: the x-y domain is given by the Poisson grid
-                                  remapping_grid_vx_max,              &
-                                  remapping_grid_vy_min,              &
-                                  remapping_grid_vy_max,              &
-                                  remapping_cart_grid_n_cells_x,      &   ! for splines
-                                  remapping_cart_grid_n_cells_y,      &   ! for splines
-                                  remapping_cart_grid_n_cells_vx,     &   ! for splines
-                                  remapping_cart_grid_n_cells_vy,     &   ! for splines
-                                  remapping_sparse_grid_max_level_x,  &   ! for the sparse grid
-                                  remapping_sparse_grid_max_level_y,  &   ! for the sparse grid
-                                  remapping_sparse_grid_max_level_vx, &   ! for the sparse grid
-                                  remapping_sparse_grid_max_level_vy      ! for the sparse grid
-
-    ! discretization of the deposited f
-    !   -> deposition_particles_pos_type:     0 = sampled (and resampled) on a structured grid,     1 = unstructured (eg random)
-    !   -> deposition_particles_move_type:    0 = new particles are created at each step,           1 = move until next remap step
-    namelist /pic_lbf_deposition_params_old/                             &
-                                  deposition_particles_pos_type,      &     ! structured grid (0) or unstructured (1)
-                                  deposition_particles_move_type,     &     ! fixed (0) or pushed (1)
-                                  n_deposition_particles,             &     ! for unstructured deposition particles
-                                  n_deposition_particles_per_cell_x,  &     ! for structured deposition particles
-                                  n_deposition_particles_per_cell_y,  &     ! for structured deposition particles
-                                  n_deposition_particles_vx,          &     ! for structured deposition particles
-                                  n_deposition_particles_vy                 ! for structured deposition particles
-
-    ! discretization of the flow:
-    !   -> number of markers to be pushed forward
-    !   -> size of the cells where the flow is linearized (the flow grid)
-    !      note: the bounds of the flow grid are the same as those of the remap grid
-    namelist /pic_lbf_markers_params_old/flow_markers_type,              &
-                                  n_flow_markers_x,                   &
-                                  n_flow_markers_y,                   &
-                                  n_flow_markers_vx,                  &
-                                  n_flow_markers_vy,                  &
-                                  n_unstruct_markers_per_cell,        &
-                                  flow_grid_n_cells_x,                &
-                                  flow_grid_n_cells_y,                &
-                                  flow_grid_n_cells_vx,               &
-                                  flow_grid_n_cells_vy
-
-
-    ! NEW parameters for pic with lbf resamplings: we have 2 sets of parameters  -------------------------------------------------
-
-    ! remapping (sparse grid) parameters
+    ! 1. remapping (sparse grid) parameters
     namelist /pic_lbf_remap_params/    &
                                   remap_period,                       &
                                   remap_degree,                       &
@@ -304,13 +221,12 @@ contains
                                   remapping_sparse_grid_max_level_vx, &
                                   remapping_sparse_grid_max_level_vy
 
-    ! particle parameters (initially located on a 4d cartesian grid) used to approximate the charge and the flow
+    ! 2. particle parameters (initially located on a 4d cartesian grid) used to approximate the charge and the flow
     namelist /pic_lbf_particles_params/                               &
                                   n_particles_x,                      &
                                   n_particles_y,                      &
                                   n_particles_vx,                     &
                                   n_particles_vy
-
 
     ! Read parameters from file
     open(newunit = input_file, file=trim(filename), IOStat=io_stat)
@@ -329,19 +245,13 @@ contains
        sim%particle_type = sll_p_simple_particles
        read(input_file, simple_pic_params)
 
-    case("SLL_LBF_PARTICLES_OLD")
-       sim%particle_type = sll_p_lbf_particles_old
-       read(input_file, pic_lbf_remap_params_old)
-       read(input_file, pic_lbf_deposition_params_old)
-       read(input_file, pic_lbf_markers_params_old)
-
     case("SLL_LBF_PARTICLES")
        sim%particle_type = sll_p_lbf_particles
        read(input_file, pic_lbf_remap_params)
        read(input_file, pic_lbf_particles_params)
 
     case default
-       SLL_ERROR('sll_m_sim_pic_vp_2d2v_cart_lbfr%init_pic_2d2v', 'particle_type' // particle_type_str // ' not implemented.')
+       SLL_ERROR('sll_m_sim_pic_vp_2d2v_cart_lbf%init_pic_2d2v', 'particle_type' // particle_type_str // ' not implemented.')
     end select
 
     close (input_file)
@@ -388,7 +298,6 @@ contains
       end select
 
     case( sll_p_lbf_particles )
-      print*, '111'
       sim%no_weights = 1   ! no control variate for the moment
       sim%init_case = sll_p_deterministic_sampling
       domain_is_x_periodic = .true.
@@ -406,7 +315,6 @@ contains
       remapping_sparse_grid_max_levels(3) = remapping_sparse_grid_max_level_vx
       remapping_sparse_grid_max_levels(4) = remapping_sparse_grid_max_level_vy
       sim%resample_period = remap_period
-      print*, '1112'
       call sll_s_new_particle_group_2d2v_lbf_ptr( &
           sim%particle_group, &
           species_charge,    &
@@ -422,62 +330,12 @@ contains
           n_particles_vx, &
           n_particles_vy &
       )
-      print*, '11 6'
       SLL_ASSERT( sim%world_size == 1 )
       sim%n_particles = sim%particle_group%n_particles
       sim%n_total_particles = sim%n_particles * sim%world_size
 
-    case( sll_p_lbf_particles_old )
-      sim%no_weights = 1   ! no control variate with pic_lbfr for the moment
-      sim%init_case = sll_p_deterministic_sampling
-      domain_is_x_periodic = .true.
-      domain_is_y_periodic = .true.
-      remapping_sparse_grid_max_levels(1) = remapping_sparse_grid_max_level_x
-      remapping_sparse_grid_max_levels(2) = remapping_sparse_grid_max_level_y
-      remapping_sparse_grid_max_levels(3) = remapping_sparse_grid_max_level_vx
-      remapping_sparse_grid_max_levels(4) = remapping_sparse_grid_max_level_vy
-      sim%resample_period = remap_period
-      call sll_s_new_pic_lbfr_4d_group_ptr &
-        (sim%particle_group,                   &
-        species_charge,                        &
-        species_mass,                          &
-        domain_is_x_periodic,                  &
-        domain_is_y_periodic,                  &
-        remap_f_type,                          &
-        remap_degree,                          &
-        remapping_grid_vx_min,                 &
-        remapping_grid_vx_max,                 &
-        remapping_grid_vy_min,                 &
-        remapping_grid_vy_max,                 &
-        remapping_cart_grid_n_cells_x,         &   ! for splines
-        remapping_cart_grid_n_cells_y,         &   ! for splines
-        remapping_cart_grid_n_cells_vx,        &   ! for splines
-        remapping_cart_grid_n_cells_vy,        &   ! for splines
-        remapping_sparse_grid_max_levels,      &   ! for the sparse grid
-        deposition_particles_pos_type,         &
-        deposition_particles_move_type,        &
-        n_deposition_particles,                &    ! todo: really needed ???
-        n_deposition_particles_per_cell_x,     &
-        n_deposition_particles_per_cell_y,     &
-        n_deposition_particles_vx,             &
-        n_deposition_particles_vy,             &
-        flow_markers_type,                     &
-        n_flow_markers_x,                      &
-        n_flow_markers_y,                      &
-        n_flow_markers_vx,                     &
-        n_flow_markers_vy,                     &
-        n_unstruct_markers_per_cell,           &
-        flow_grid_n_cells_x,                   &
-        flow_grid_n_cells_y,                   &
-        flow_grid_n_cells_vx,                  &
-        flow_grid_n_cells_vy,                  &
-        sim%mesh )
-
-      SLL_ASSERT( sim%world_size == 1 )
-      sim%n_particles = sim%particle_group%n_particles    ! with pic_lbfr this is the nb of flow markers and deposition particles
-      sim%n_total_particles = sim%n_particles * sim%world_size
     case default
-      SLL_ERROR('sll_m_sim_pic_vp_2d2v_cart_lbfr%init_pic_2d2v', 'this particle_type is not implemented.')
+      SLL_ERROR('sll_m_sim_pic_vp_2d2v_cart_lbf%init_pic_2d2v', 'this particle_type is not implemented.')
     end select
     
     ! Initialize control variate
@@ -518,7 +376,7 @@ contains
 !------------------------------------------------------------------------------!
 
   subroutine run_pic_2d2v (sim)
-    class(sll_t_sim_pic_vp_2d2v_cart_lbfr), intent(inout) :: sim
+    class(sll_t_sim_pic_vp_2d2v_cart_lbf), intent(inout) :: sim
 
     type(sll_t_initial_density_parameters)  :: initial_density_parameters  ! for the initial particle sampling
     type(sll_t_plotting_params_2d)          :: plotting_params_2d
@@ -570,7 +428,7 @@ contains
        print*, 'time step ', nt, '/', sim%n_time_steps, ' ... '
 
        ! particle resampling (conditional)
-       if( (sim%particle_type == sll_p_lbf_particles .or. sim%particle_type == sll_p_lbf_particles_old )  &
+       if( (sim%particle_type == sll_p_lbf_particles)  &
            .and. (modulo(nt-1, sim%resample_period) == 0 ) )then
 
          print *, "-- plotting f slice in gnuplot format..."
@@ -611,7 +469,7 @@ contains
 !------------------------------------------------------------------------------!
 
   subroutine delete_pic_2d2v (sim)
-    class(sll_t_sim_pic_vp_2d2v_cart_lbfr), intent(inout) :: sim
+    class(sll_t_sim_pic_vp_2d2v_cart_lbf), intent(inout) :: sim
     
     call sim%pic_poisson%free()
     deallocate(sim%pic_poisson)
@@ -647,4 +505,4 @@ contains
 
 
 
-end module sll_m_sim_pic_vp_2d2v_cart_lbfr
+end module sll_m_sim_pic_vp_2d2v_cart_lbf
