398b14c20ffc5a62a895fca1d9b644b2caa5d1ff
Yaman Güçlü <yaman.guclu@gmail.com>
2016-08-31 16:29:15 +0200

    [pyreldisp] Improve user scripts.

--- python/pyreldisp/BOT.py
+++ python/pyreldisp/BOT.py
@@ -1,18 +1,28 @@
 from __future__ import print_function
-from zealpy_BOT import zealpy_BOT
-from pylab import *
+
 import numpy as np
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+from zealpy_BOT  import zealpy_BOT
 
-rcParams['interactive']=True
+#===============================================================================
+# INPUT PARAMETERS
+#===============================================================================
 
-kmode = 0.5
+kmode_list = [0.1, 0.2, 0.3, 0.4, 0.5]
 
 xmin = -2.
 xmax = 2.
 ymin = 0.0001
 ymax = 1.
 
-for kmode in arange(0.5,0.0,-.1):
+#===============================================================================
+# EXECUTION (with terminal output)
+#===============================================================================
+
+zeros_dict = OrderedDict()
+
+for kmode in kmode_list:
     zp = zealpy_BOT(
         kmode  = kmode,
         a_bump = 0.4,
@@ -20,23 +30,34 @@ for kmode in arange(0.5,0.0,-.1):
         sigmab = 0.1,
         kbT    = 1.0,
         l0ld   = 18.0)
-    zp.get_zeros(xmin,xmax,ymin,ymax)
+    zp.get_zeros( xmin, xmax, ymin, ymax )
+    zeros_dict[kmode] = np.array( zp.zeros )
 
     print( 'mode:', kmode )
     print( 'zero with largest imaginary part:', \
-        zp.zeros[argmax(imag(zp.zeros))] )
+        zp.zeros[np.argmax(np.imag(zp.zeros))] )
 
-    plot(np.real(zp.zeros),np.imag(zp.zeros),'.',label='k='+str(kmode))
-    axis([-1.,1.,-1.,1.])
-    title('zeros of dispersion relation')
-    legend()
-#end for
+#===============================================================================
+# PLOTS
+#===============================================================================
 
-if ( (rcParams['backend']=='TkAgg') or
-     (rcParams['backend']=='WXAgg') ):
-    show()
+fig = plt.figure()
+ax = fig.add_subplot( 1,1,1 )
 
+for kmode, zeros in zeros_dict.items():
+    ax.plot( zeros.real, zeros.imag, '.', label='k={}'.format( kmode ) )
+
+ax.axis( [-1.,1.,-1.,1.] )
+ax.set_title( 'Zeros of dispersion relation' )
+ax.set_xlabel( r'Re($\omega$)', size=14 )
+ax.set_ylabel( r'Im($\omega$)', size=14, rotation='horizontal' )
+ax.grid()
+ax.legend()
+fig.show()
+
+msg = '\nPress ENTER to quit'
 try:
-    raw_input("Press enter")
+    raw_input( msg )
 except NameError:
-    input("Press enter")
+    input( msg )
+plt.close('all')
--- python/pyreldisp/landau.py
+++ python/pyreldisp/landau.py
@@ -1,27 +1,57 @@
 from __future__ import print_function
+
+import numpy as np
+import matplotlib.pyplot as plt
+from collections   import OrderedDict
 from zealpy_landau import zealpy_landau
-from pylab import *
 
-rcParams['interactive']=True
+#===============================================================================
+# INPUT PARAMETERS
+#===============================================================================
 
-for kmode in arange(0.5,0.0,-.1):
-    zp = zealpy_landau(kmode=kmode)
-    zp.get_zeros(-10,10,-2,10)
+kmode_list = [0.1, 0.2, 0.3, 0.4, 0.5]
+
+xmin = -10
+xmax =  10
+ymin = -2
+ymax =  10
+
+#===============================================================================
+# EXECUTION (with terminal output)
+#===============================================================================
+
+zeros_dict = OrderedDict()
+
+for kmode in kmode_list:
+    zp = zealpy_landau( kmode=kmode )
+    zp.get_zeros( xmin, xmax, ymin, ymax )
+    zeros_dict[kmode] = np.array( zp.zeros )
 
     print( 'mode:', kmode )
     print( 'zero with largest imaginary part:', \
-        zp.zeros[argmax(imag(zp.zeros))] )
+        zp.zeros[np.argmax(np.imag(zp.zeros))] )
 
-    plot(np.real(zp.zeros),np.imag(zp.zeros),'.',label='k='+str(kmode))
-    axis([-3,3,-2.2,.2])
-    title('zeros of dispersion relation')
-    legend()
+#===============================================================================
+# PLOTS
+#===============================================================================
 
-if ( (rcParams['backend']=='TkAgg') or
-     (rcParams['backend']=='WXAgg') ):
-    show()
+fig = plt.figure()
+ax = fig.add_subplot( 1,1,1 )
 
+for kmode, zeros in zeros_dict.items():
+    ax.plot( zeros.real, zeros.imag, '.', label='k={}'.format( kmode ) )
+
+ax.axis( [-3,3,-2.2,.2] )
+ax.set_title( 'Zeros of dispersion relation' )
+ax.set_xlabel( r'Re($\omega$)', size=14 )
+ax.set_ylabel( r'Im($\omega$)', size=14, rotation='horizontal' )
+ax.grid()
+ax.legend()
+fig.show()
+
+msg = '\nPress ENTER to quit'
 try:
-    raw_input("Press enter")
+    raw_input( msg )
 except NameError:
-    input("Press enter")
+    input( msg )
+plt.close('all')
