fe9d2ed14ef51f2fa1c671f370bba866d7eaf835
Edoardo Zoni <edoardo.zoni@ipp.mpg.de>
2016-08-31 14:47:53 +0200

    [pyreldisp-cleanup] Modify det and det2, cleanup source formatting, remove Zi param.

--- python/pyreldisp/fun_gyrokin_cleanup.f90
+++ python/pyreldisp/fun_gyrokin_cleanup.f90
@@ -41,16 +41,18 @@
 !-----------------------------------------------------------------------
 
 MODULE Function_Input_Module
-  USE Precision_Module
 
-  IMPLICIT NONE
+  use Precision_Module
+
+  implicit none
+
   integer, parameter :: max=61
   double precision :: ordre_grandeur = 1
-  complex(kind=dp), dimension(:,:), allocatable :: vector
+  complex (kind=dp), allocatable :: vector(:,:)
   !---------------------------------------------------------------------
   !**ACCESSIBILITY
   !
-  PUBLIC
+  public
 
   !---------------------------------------------------------------------
   !  If ICON = 3 or 4 (as specified in Zeal_Input_Module), then specify 
@@ -66,51 +68,48 @@ MODULE Function_Input_Module
   !      at most NEWTONF
   !  or if a maximum number of iterations is exceeded.
   !
-  REAL(KIND=DP) :: NEWTONZ = 5.0E-08_DP
-  REAL(KIND=DP) :: NEWTONF = 1.0E-14_DP
-  REAL(KIND=DP) :: PI = 3.1415926535897931_DP
+  real (kind=dp) :: NEWTONZ = 5.0E-08_DP
+  real (kind=dp) :: NEWTONF = 1.0E-14_DP
+  real (kind=dp) :: PI = 3.1415926535897931_DP
 
-  INTEGER       :: Zi
-  REAL(KIND=DP) :: kpar, ktheta
-  integer       :: NNr
-  REAL(KIND=DP) :: dr
-  REAL(KIND=DP) :: B0
+  real (kind=dp) :: kpar, ktheta
+  integer        :: NNr
+  real (kind=dp) :: dr
+  real (kind=dp) :: B0
 
-  REAL(KIND=DP), dimension(:), allocatable :: Ti
-  REAL(KIND=DP), dimension(:), allocatable :: dlogTi
-  REAL(KIND=DP), dimension(:), allocatable :: Te
-  REAL(KIND=DP), dimension(:), allocatable :: dlogn0
-  REAL(KIND=DP), dimension(:), allocatable :: ddlogn0
-  REAL(KIND=DP), dimension(:), allocatable :: rmesh
+  real (kind=dp), allocatable :: Ti(:)
+  real (kind=dp), allocatable :: dlogTi(:)
+  real (kind=dp), allocatable :: Te(:)
+  real (kind=dp), allocatable :: dlogn0(:)
+  real (kind=dp), allocatable :: ddlogn0(:)
+  real (kind=dp), allocatable :: rmesh(:)
 
-  REAL(KIND=DP), dimension(:), allocatable :: btheta
-  REAL(KIND=DP), dimension(:), allocatable :: bz
+  real (kind=dp), allocatable :: btheta(:)
+  real (kind=dp), allocatable :: bz(:)
 
   ! matrix of operator
-  COMPLEX (KIND=DP), dimension(:,:), allocatable :: A 
+  complex (kind=dp), allocatable :: A(:,:) 
   ! derivative of A (diagonal)
-  COMPLEX (KIND=DP), dimension(:), allocatable :: Ap   
+  complex (kind=dp), allocatable :: Ap(:)   
 
-CONTAINS
+contains
 
-  SUBROUTINE FDF(omega,F,DF)
+  subroutine FDF(omega,F,DF)
     !-------------------------------------------------------------
     !**PURPOSE
     !  Define the function F and its derivative DF.
     !-------------------------------------------------------------
-    COMPLEX(KIND=DP), INTENT(IN)   :: omega
-    COMPLEX(KIND=DP), INTENT(OUT)  :: F, DF
+    complex (kind=dp), intent(in   ) :: omega
+    complex (kind=dp), intent(  out) :: F, DF
+    complex (kind=dp)                :: res1, res2
 
-    ! local variables
+    call Matrice(omega)
+    call Dmatrice(omega)
+    call det2(res1, res2)
+    F  = res1/ordre_grandeur
+    DF = res2/ordre_grandeur
 
-    complex (kind=dp) :: res1, res2
-
-       call Matrice(omega)
-       call Dmatrice(omega)
-       call det2(res1, res2)
-       F = res1/ordre_grandeur
-       DF = res2/ordre_grandeur
-  END SUBROUTINE FDF
+  end subroutine FDF
 
 
   !-------------------------------------------------------------------- 
@@ -121,17 +120,18 @@ CONTAINS
     !       A(2,j) is the diagonal term in column j
     !       A(3,j) is the lower diagonal term in column j
     !-----------------------------------------------------
-    COMPLEX(KIND=DP), intent(in) :: omega
-    integer i, j
-    COMPLEX(KIND=DP) :: zeta, dzeta, rho
-    REAL(KIND=DP) :: kpar_modif
+    complex (kind=dp), intent(in) :: omega
+    complex (kind=dp)             :: zeta, dzeta, rho
+    real    (kind=dp)             :: kpar_modif
+    integer                       :: i,j
     
     do i = 1, NNr 
       kpar_modif = (kpar*bz(i)+(ktheta*btheta(i)/rmesh(i)))*sqrt(2.0_dp*Ti(i))
       rho = omega/(kpar_modif)
       call FriedConte(rho,zeta,dzeta)
       A(2,i) = 2._dp+ dr*dr*( (ktheta**2-0.25_dp)/rmesh(i)**2 &
-        + 1._dp/(Zi*Te(i)) + 0.5_dp*(0.5_dp*dlogn0(i)**2 &
+        !+ 1._dp/(Zi*Te(i)) + 0.5_dp*(0.5_dp*dlogn0(i)**2 &
+        + 1._dp/Te(i) + 0.5_dp*(0.5_dp*dlogn0(i)**2 &
         + dlogn0(i)/rmesh(i) + ddlogn0(i)) &
         + ( (1+rho*zeta)/Ti(i) &
         - ktheta/(rmesh(i)*B0*kpar_modif) &
@@ -148,12 +148,11 @@ CONTAINS
     ! Compute Ap the term to term derivative of matrice A
     ! Ap is a diagonal matrix
     !----------------------------------------
-    COMPLEX(KIND=DP), intent(in) :: omega
+    complex (kind=dp), intent(in) :: omega
 
     integer  i, j
-    COMPLEX(KIND=DP) :: zeta, dzeta, rho
-    REAL(KIND=DP) :: kpar_modif
-    !B0 =1._dp
+    complex (kind=dp) :: zeta, dzeta, rho
+    real (kind=dp) :: kpar_modif
     do i = 1, NNr  
       kpar_modif = (kpar*bz(i)+(ktheta*btheta(i)/rmesh(i)))*sqrt(2.0_dp*Ti(i))
       rho = omega/(kpar_modif)
@@ -169,58 +168,88 @@ CONTAINS
 
 
   !---------------------------------------------
-  subroutine det(res)
-    complex (kind=dp) ::res
-    complex (kind=dp) :: b, c,temp
-    integer :: i, j
+!  subroutine det(res)
+!    complex (kind=dp) ::res
+!    complex (kind=dp) :: b, c,temp
+!    integer :: i, j
+!
+!    b = 1
+!    c = A(2,1)
+!    do i = 2, NNr-1
+!       temp = c
+!       c = (A(2,i))*c - A(1,i)*A(3,i-1)*b
+!       b = temp
+!    end do
+!
+!    res = c
+!    return
+!  end subroutine det
 
-    b = 1
-    c = A(2,1)
-    do i = 2, NNr-1
-       temp = c
-       c = (A(2,i))*c - A(1,i)*A(3,i-1)*b
-       b = temp
-    end do
 
-    res = c
-    return
-  end subroutine det
+   !---------------------------------------------------------
+!  subroutine det2(res1, res2)
+!    ! Ap doit être la matrice des dérivées des termes de A
+!    ! Ap est diagonale dans notre cas
+!
+!    complex (kind=dp) :: res1, res2
+!    complex (kind=dp) :: b1, c1,temp1, b2, c2, temp2, c3, d1
+!    integer :: i, j
+!
+!    b1 = 1
+!    c1 = A(2,1)
+!    b2 = 0
+!    c2 = Ap(1)
+!    do i = 2, NNr-1
+!       temp1 = c1
+!       temp2 = c2
+!       c1 = A(2,i)*c1 - A(1,i)*A(3,i-1)*b1
+!       ! Ap est diagonale
+!       c2 = Ap(i)*temp1 + A(2,i)*c2 - A(1,i)*A(3,i-1)*b2
+!  
+!       d1 = b1
+!       b1 = temp1
+!       b2 = temp2
+!
+!    end do
+!    res2 = c2
+!    res1 = c1
+!    return
+!  end subroutine det2
 
 
-  !---------------------------------------------------------
+  ! Implement equations (36) and (37) of
+  ! D. Coulette, N. Besse / Journal of Computational Physics 248 (2013) 1-32
   subroutine det2(res1, res2)
-    ! Ap doit être la matrice des dérivées des termes de A
-    ! Ap est diagonale dans notre cas
 
     complex (kind=dp) :: res1, res2
-    complex (kind=dp) :: b1, c1,temp1, b2, c2, temp2, c3, d1
-    integer :: i, j
+    complex (kind=dp) :: D, Dp, D_tmp1, D_tmp2, Dp_tmp1, Dp_tmp2
+    integer :: i
 
-    b1 = 1
-    c1 = A(2,1)
-    b2 = 0
-    c2 = Ap(1)
-    do i = 2, NNr-1
-       temp1 = c1
-       temp2 = c2
-       c1 = A(2,i)*c1 - A(1,i)*A(3,i-1)*b1
-       ! Ap est diagonale
-       c2 = Ap(i)*temp1 + A(2,i)*c2 - A(1,i)*A(3,i-1)*b2
-  
-       d1 = b1
-       b1 = temp1
-       b2 = temp2
+    D_tmp1=A(2,1)
+    D_tmp2=1
+    Dp_tmp1=Ap(1)
+    Dp_tmp2=0
+    do i=2,NNr-1
+      D=A(2,i)*D_tmp1-D_tmp2
+      Dp=Ap(i)*D_tmp1+A(2,i)*Dp_tmp1-Dp_tmp2
 
-    end do
-    res2 = c2
-    res1 = c1
+      D_tmp2=D_tmp1
+      D_tmp1=D
+      Dp_tmp2=Dp_tmp1
+      Dp_tmp1=Dp
+    enddo
+
+    res1 = D
+    res2 = Dp
     return
   end subroutine det2
 
+
   !---------------------------------------------------------
   subroutine init
-    COMPLEX(KIND = DP) :: res,  omega
-    real(kind = dp), dimension(20) :: tab
+   !complex (kind=dp) :: res, omega
+    complex (kind=dp) :: res1, res2, omega
+    real (kind=dp) :: tab(20)
     integer :: i
 
     if (.not.allocated(A)) allocate(A(3,NNr))
@@ -229,8 +258,8 @@ CONTAINS
     do i = 1, 20
        omega = cmplx(-1+i*0.1,0.0)
        call matrice(omega)
-       call det(res)
-       tab(i) = real(res)
+       call det2(res1,res2)
+       tab(i) = real(res1)
     end do
     ordre_grandeur = maxval(tab)
   end subroutine init
@@ -238,24 +267,25 @@ CONTAINS
 
   !---------------------------------------------------
   subroutine kernel(omega)
-    complex(kind=dp), intent(in) :: omega
-    complex(kind=dp) :: res1,res2
-    complex(kind=dp), dimension(NNr,NNr) :: pt
-    complex(kind=dp), dimension(NNr,NNr) :: Q
-    complex(kind=dp), dimension(NNr,NNr) :: C
-    complex(kind=dp), dimension(NNr)     :: WORK
-    real(kind=dp)   , dimension(NNr)     :: D
-    real(kind=dp)   , dimension(NNr-1)   :: E
-    real(kind=dp)   , dimension(NNr)     :: RWORK
-    real(kind=dp)   , dimension(4*NNr)   :: RRWORK
-    real(kind=dp)   , dimension(NNr)     :: EE
-    integer :: i, infosub, j, ii
+    complex (kind=dp), intent(in) :: omega
+    complex (kind=dp)             :: res1,res2
+    complex (kind=dp)             :: pt    (NNr,NNr)
+    complex (kind=dp)             :: Q     (NNr,NNr)
+    complex (kind=dp)             :: C     (NNr,NNr)
+    complex (kind=dp)             :: work  (NNr    )
+    real    (kind=dp)             :: D     (NNr    )
+    real    (kind=dp)             :: E     (NNr-1  )
+    real    (kind=dp)             :: rwork (NNr    )
+    real    (kind=dp)             :: rrwork(4*NNr  )
+    real    (kind=dp)             :: EE    (NNr    )
+    integer                       :: i, infosub, j, ii
     
     call Matrice(omega)
 
-    call zgbbrd('B', NNr, NNr, 0, 1, 1, A, 3, D, E, Q, NNr, PT, NNr, C, NNr, WORK, RWORK,INFOSUB)
+    call zgbbrd('B',NNr,NNr,0,1,1,A,3,D,E,Q,NNr,PT,NNr,C,NNr,work,rwork,infosub)
     if (infosub .NE. 0) then
-       print*, 'zeal failed because there is an error in the compute of singular value decomposition'
+       print*, 'zeal failed because there is an error in the compute &
+               &of singular value decomposition'
        stop
     end if
 
@@ -263,9 +293,10 @@ CONTAINS
        EE(i) = E(i)
     end do
 
-    call zbdsqr('U', NNr, NNr, NNr, 0, D, EE , PT, NNr , Q, NNr, C, 1, rrwork, infosub)
+    call zbdsqr('U',NNr,NNr,NNr,0,D,EE,PT,NNr,Q,NNr,C,1,rrwork,infosub)
     if (infosub .NE. 0) then
-       print*, 'zeal failed because there is an error in the compute of singular value decomposition'
+       print*, 'zeal failed because there is an error in the compute &
+               &of singular value decomposition'
        stop
     end if
     ! Singular values are given in D in decreasing order, so
@@ -274,7 +305,10 @@ CONTAINS
     do while(D(NNr-ii).lt.1.e-10)
        ii=ii+1
     end do
-    print*, 'dim of kernel=',ii, 'omega= ',omega
+    print '(a,i0,/)', 'Dimension of kernel = ', ii
+    print '(a,f16.14,a,f16.14,a)', &
+          'omega = ', real(omega), '+', imag(omega), 'j'
+   !print*, 'omega =', omega
     if (ii.eq. 0) then
        print*, 'zealpy failed because A(', omega, ') has 0-dimensional kernel'
     else
