cccb5c99c826afc4ae5b3879ea4cfefab4762f51
Sever Hirstoaga <sever.hirstoaga@inria.fr>
2016-09-02 10:40:52 +0200

    test sampling

--- src/particle_methods/pic_opt2d/pic_opt2d_particle_initializers/testing/test_pic_initializers.F90
+++ src/particle_methods/pic_opt2d/pic_opt2d_particle_initializers/testing/test_pic_initializers.F90
@@ -32,9 +32,9 @@ program test_pic_initializers
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 #define THERM_SPEED 1._f64
-#define NUM_PARTICLES 100000_i32
+#define NUM_PARTICLES 50000_i32
 #define GUARD_SIZE 10000_i32
-#define PARTICLE_ARRAY_SIZE 150000_i32
+#define PARTICLE_ARRAY_SIZE 15000000_i32
 #define ALPHA 0.5_f64
 #define NC_X 128_i32
 #define XMIN 0._f64
@@ -48,8 +48,9 @@ program test_pic_initializers
   type(sll_t_particle_group_4d), pointer :: init_group_random
   type(sll_t_particle_group_4d), pointer :: init_group_hamm
   type(sll_t_particle_group_2d), pointer :: init_group_GC
-  type(sll_t_cartesian_mesh_2d),   pointer :: m2d
-
+  type(sll_t_cartesian_mesh_2d), pointer :: m2d
+  sll_real64 :: mean_ref(1:2), stddev_ref(1:2)
+    
   m2d =>  sll_f_new_cartesian_mesh_2d( NC_X, NC_Y, &
        XMIN, XMAX, YMIN, YMAX )
 
@@ -63,24 +64,63 @@ program test_pic_initializers
        PARTICLE_ARRAY_SIZE, &
        GUARD_SIZE, QoverM, m2d )
 
+  mean_ref   = [0._f64, 0._f64]! the 2d Gaussian for velocity
+  stddev_ref = [1._f64, 1._f64]!  
+
+  print*, 'the Random initialization' 
   call sll_s_initial_random_particles_4d(THERM_SPEED, &
         ALPHA, KX, m2d, NUM_PARTICLES, init_group_random )
+  call test_mean_stddev(init_group_random, 3._f64/sqrt(real(NUM_PARTICLES,f64)))
 
+  print*, 'the HAMMERSLEY initialization' 
   call sll_s_initial_hammersley_particles_4d(THERM_SPEED, &
         ALPHA, KX, m2d, NUM_PARTICLES, init_group_hamm )
+  call test_mean_stddev(init_group_hamm, 3._f64/sqrt(real(NUM_PARTICLES,f64)))
 
   init_group_GC => sll_f_new_particle_2d_group( &
        NUM_PARTICLES, &
        PARTICLE_ARRAY_SIZE, &
        GUARD_SIZE, QoverM, m2d )
+
   call sll_s_initial_particles_2d( ALPHA, &
        KX, m2d, NUM_PARTICLES, init_group_GC )
-  
+
   call sll_o_delete( init_group_random )
   call sll_o_delete( init_group_hamm )
+  call sll_o_delete( init_group_GC )
   call sll_o_delete( m2d )
+
   print*, "PASSED"
 
-!contains
-  
+contains
+
+  subroutine test_mean_stddev (part_group, tolerance)
+    type(sll_t_particle_group_4d), pointer, intent(in) :: part_group
+    sll_real64, intent(in)                             :: tolerance
+    sll_real64 :: mean(1:2), stddev(1:2)
+    sll_int32  :: i
+    sll_real64 :: out(1:2)
+
+    mean = 0._f64
+    do i = 1, NUM_PARTICLES
+       mean(1) = mean(1) + part_group%p_list(i)%vx
+       mean(2) = mean(2) + part_group%p_list(i)%vy
+    end do
+    mean = mean/real(NUM_PARTICLES,f64)
+
+    out = mean - mean_ref
+    if ( (abs(out(1)) > tolerance) .or. (abs(out(2)) > tolerance) ) then
+       print*, 'FAILED'
+    endif
+
+   stddev = 0._f64
+    do i = 1, NUM_PARTICLES
+       stddev(1) = stddev(1) + (part_group%p_list(i)%vx - mean(1))**2
+       stddev(2) = stddev(2) + (part_group%p_list(i)%vy - mean(2))**2
+    end do
+    stddev = stddev/real(NUM_PARTICLES-1,f64)
+    print*, 'Std deviation error=', stddev - stddev_ref
+
+  end subroutine test_mean_stddev
+
 end program test_pic_initializers
