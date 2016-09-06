4972afdbe4db68aecda45eae2c0e3c05136261c7
Edoardo Zoni <EZoni@lbl.gov>
2016-09-06 12:10:12 +0200

    [pyreldisp-cleanup] Move iota inside 'compute_b_anal'; more cleanup Python scripts.

--- python/pyreldisp/zealpy_gyrokin_cleanup_anal.py
+++ python/pyreldisp/zealpy_gyrokin_cleanup_anal.py
@@ -3,34 +3,34 @@ import numpy as np
 from zealpy_gyrokin_cleanup import zealpy_gyrokin_cleanup
 from pyreldisp_params import params, pyreldisp_exception
 
-#-------------------------------------------------------------
+#-------------------------------------------------------------------------------
 # Class 'zealpy_gyrokin_cleanup_anal'  
-#-------------------------------------------------------------
+#-------------------------------------------------------------------------------
 class zealpy_gyrokin_cleanup_anal(zealpy_gyrokin_cleanup):
     def __init__(self,**anal_arg):
         required_params={'Zi'      : int,
                          'NNr'     : int,
                          'rmin'    : float,
                          'Lr'      : float,
+                         'R0'      : float,
                          'invLTi'  : float,
                          'deltarTi': float,
                          'invLTe'  : float,
                          'deltarTe': float,
                          'invLn0'  : float,
                          'deltarn0': float,
-                         'iota'    : float,
-                         'B0'    : float
-                        }
+                         'B0'      : float }
+
         self.param_anal = params(required_params,anal_arg)
         Zi   = self.param_anal.get_value('Zi')
         NNr  = self.param_anal.get_value('NNr')
         rmin = self.param_anal.get_value('rmin')
         Lr   = self.param_anal.get_value('Lr')
-        iota   = self.param_anal.get_value('iota')
+        R0   = self.param_anal.get_value('R0')
         B0   = self.param_anal.get_value('B0')
         super(zealpy_gyrokin_cleanup_anal,self).__init__(Zi,NNr,rmin,Lr,B0)
 
-        #--> Compute the analytic profiles
+        # Compute the analytic profiles
         dr    = self.params.get_value('dr')
         rmesh = self.params.get_value('rmesh')
         anorm = rmesh[-1]-rmesh[0]
@@ -43,15 +43,11 @@ class zealpy_gyrokin_cleanup_anal(zealpy_gyrokin_cleanup):
         invLn0   = self.param_anal.get_value('invLn0')
         deltarn0 = self.param_anal.get_value('deltarn0')
 
-        [Ti,dlogTi]         = compute_Ti_dlogTi_anal(
-            rmesh,rpeak,invLTi,deltarTi)
-        [Te,dlogTe]         = compute_Te_dlogTe_anal(
-            rmesh,rpeak,invLTe,deltarTe)
+        [Ti,dlogTi] = compute_Ti_dlogTi_anal( rmesh, rpeak, invLTi, deltarTi )
+        [Te,dlogTe] = compute_Te_dlogTe_anal( rmesh, rpeak, invLTe, deltarTe )
         [n0,dlogn0,ddlogn0] = compute_n0_dlogn0_anal(
-            rmesh,rpeak,invLn0,deltarn0)
-        [btheta,bz] = compute_b_anal(
-            rmesh,iota)
-        self.eta = dlogTi/dlogn0
+                              rmesh, rpeak, invLn0, deltarn0)
+        [btheta,bz] = compute_b_anal( rmesh, R0 )
 
         Zi = self.param_anal.get_value('Zi')
         self.params.set_value('Zi',Zi)
@@ -66,68 +62,49 @@ class zealpy_gyrokin_cleanup_anal(zealpy_gyrokin_cleanup):
         B0 = self.param_anal.get_value('B0')
         self.params.set_value('B0',B0)
         if (not self.param_anal.check_validity()):
-            raise pyreldisp_exception("zealpy_gyrkin_cleanup_anal: Parameters not valid")
-#end class zealpy_gyrokin_cleanup_anal    
+           raise pyreldisp_exception("zealpy_gyrkin_cleanup_anal: Parameters not valid")
 
-
-#-------------------------------------------------
-# Compute ion temperature profile Ti and 
-#   dlogTi = d/dr(ln Ti(r))
-# with
-#   1/Ti(r) dTi(r)/dr = -(1/LTi)*cosh^-2(r-rpeak/deltar)   
-#     where (1/Ln) = kappa_Ti/R   
-#-------------------------------------------------
+#-------------------------------------------------------------------------------
+# Compute ion temperature profile Ti(r) and dlogTi(r)=d/dr(ln Ti(r))
+#-------------------------------------------------------------------------------
 def compute_Ti_dlogTi_anal(rmesh,rpeak,invLTi,deltarTi):
-    Ti     = np.exp(-invLTi*deltarTi * 
-                     np.tanh((rmesh-rpeak)/deltarTi))
+
+    Ti     = np.exp(-invLTi*deltarTi*np.tanh((rmesh-rpeak)/deltarTi))
     dlogTi = -invLTi/np.cosh((rmesh-rpeak)/deltarTi)**2
+
     return [Ti,dlogTi]
-#end def compute_Ti_dlogTi_anal
 
-
-#-------------------------------------------------
-# Compute electron temperature profile Te and 
-#   dlogTe = d/dr(ln Te(r))
-# with
-#   1/Te(r) dTe(r)/dr = -(1/LTe)*cosh^-2(r-rpeak/deltar)   
-#     where (1/Ln) = kappa_Te/R   
-#-------------------------------------------------
+#-------------------------------------------------------------------------------
+# Compute electron temperature profile Te(r) and dlogTe(r)=d/dr(ln Te(r))
+#-------------------------------------------------------------------------------
 def compute_Te_dlogTe_anal(rmesh,rpeak,invLTe,deltarTe):
-    Te     = np.exp(-invLTe*deltarTe * 
-                     np.tanh((rmesh-rpeak)/deltarTe))
+
+    Te     = np.exp(-invLTe*deltarTe*np.tanh((rmesh-rpeak)/deltarTe))
     dlogTe = -invLTe/np.cosh((rmesh-rpeak)/deltarTe)**2
+
     return [Te,dlogTe]
-#end def compute_Te_dlogTe_anal
 
-
-#-------------------------------------------------
-# Compute electron density profile n0,
-#   dlogn0  = d/dr(ln n0(r)) and 
-#   ddlogn0 = d/dr (dlogn0)
-# with
-#   1/n0(r) dn0(r)/dr = -(1/Ln0)*cosh^-2(r-rpeak/deltar)   
-#     where (1/Ln) = kappa_n0/R   
-#-------------------------------------------------
+#-------------------------------------------------------------------------------
+# Compute electron density profile n0(r), dlogn0(r)=d/dr(ln n0(r)) and
+# ddlogn0(r)=d/dr(dlogn0)
+#-------------------------------------------------------------------------------
 def compute_n0_dlogn0_anal(rmesh,rpeak,invLn0,deltarn0):
-    n0     = np.exp(-invLn0*deltarn0 * 
-                     np.tanh((rmesh-rpeak)/deltarn0))
-    dlogn0 = -invLn0/np.cosh((rmesh-rpeak)/deltarn0)**2
-    ddlogn0 = 2*invLn0/deltarn0*np.sinh((rmesh-rpeak)/deltarn0) / \
-        np.cosh((rmesh-rpeak)/deltarn0)**3
-    
-    return [n0,dlogn0,ddlogn0]
-#end def compute_n0_dlogn0_anal
 
-#-------------------------------------------------
-# Compute c,btheta,bz
-#   c  = iota*r  normally iota*r/R0 
-#-------------------------------------------------
-def compute_b_anal(rmesh,iota):
-  #c = iota*rmesh
-  #iota = 0.8/239.8
-  btheta = iota*rmesh/(1.0+(iota*rmesh)**2)**(0.5)
-  bz = 1.0/(1.0+(iota*rmesh)**2)**(0.5)   
-  #bz = 1.
-  
-  return [btheta,bz]
-#end def compute_b_anal
+    n0      = np.exp(-invLn0*deltarn0*np.tanh((rmesh-rpeak)/deltarn0))
+    dlogn0  = -invLn0/np.cosh((rmesh-rpeak)/deltarn0)**2
+    ddlogn0 = 2.0*invLn0/deltarn0*np.sinh((rmesh-rpeak)/deltarn0) \
+              /np.cosh((rmesh-rpeak)/deltarn0)**3
+
+    return [n0,dlogn0,ddlogn0]
+
+#-------------------------------------------------------------------------------
+# Compute xi(r), btheta(r) and bz(r)
+#-------------------------------------------------------------------------------
+def compute_b_anal(rmesh,R0):
+
+    iota   = 0.0*rmesh+0.8
+    xi     = iota*rmesh/R0
+    btheta = xi/np.sqrt(1.0+xi**2)
+    bz     = 1.0/np.sqrt(1.0+xi**2)   
+
+    return [btheta,bz]
