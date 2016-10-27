6a847c8ba8e4ac24109ff8043713e7e9cb0a9faf
Pierre Navaro <pierre.navaro@univ-rennes1.fr>
2016-10-27 12:39:27 +0200

    Improve the poisson solver for the Vlasov-HMF model
    I followed the coding guidelines.

--- src/field_solvers/poisson_solvers/sll_m_poisson_1d_hmf.F90
+++ src/field_solvers/poisson_solvers/sll_m_poisson_1d_hmf.F90
@@ -1,7 +1,5 @@
 !**************************************************************
 !  Copyright INRIA
-!  Authors : 
-!     CALVI project team
 !  
 !  This code SeLaLib (for Semi-Lagrangian-Library) 
 !  is a parallel library for simulating the plasma turbulence 
@@ -16,8 +14,14 @@
 !**************************************************************
 
 !> @ingroup poisson_solvers
-!> Module to sll_o_solve Poisson equation on one dimensional mesh using FFT
-!> transform.
+!> @author Pierre Navaro
+!> @brief
+!> Module to sll_o_solve Poisson equation for the HMF model 
+!> @details
+!> You could find more information in
+!> "On numerical Landau damping for splitting methods applied to the Vlasov-HMF model"
+!> by Erwan Faou, Romain Horsin and Frédéric Rousset.
+!> https://arxiv.org/abs/1510.06555
 module sll_m_poisson_1d_hmf
 
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -25,10 +29,8 @@ module sll_m_poisson_1d_hmf
 #include "sll_memory.h"
 #include "sll_working_precision.h"
 
-  use sll_m_constants, only: &
-    sll_p_pi, sll_p_i1
-  use sll_m_poisson_1d_base, only: &
-    sll_c_poisson_1d_base
+  use sll_m_constants, only: sll_p_pi, sll_p_i1, sll_p_i0
+  use sll_m_poisson_1d_base, only: sll_c_poisson_1d_base
   use sll_m_fft, only: sll_t_fft, &
     sll_s_fft_init_c2r_1d,        &
     sll_s_fft_init_r2c_1d,        &
@@ -38,17 +40,17 @@ module sll_m_poisson_1d_hmf
 
   implicit none
 
-  public :: &
-    sll_o_initialize, &
-    sll_o_new, &
-    sll_t_poisson_1d_hmf, &
-    sll_o_solve,               &
-    sll_f_new_poisson_1d_hmf
+  public ::                     &
+    sll_s_init_poisson_1d_hmf,  &
+    sll_t_poisson_1d_hmf,       &
+    sll_s_solve_poisson_1d_hmf, &
+    sll_s_free_poisson_1d_hmf
 
   private
 
-  !> Solver data structure
-  type :: sll_t_poisson_1d_hmf
+  !> Implementation of the poisson 1d solver for the Vlasov-HMF model
+  type, extends(sll_c_poisson_1d_base) :: sll_t_poisson_1d_hmf  
+  
      sll_int32                         :: nc_eta1   !< number of cells
      sll_real64                        :: eta1_min  !< left corner
      sll_real64                        :: eta1_max  !< right corner
@@ -56,64 +58,26 @@ module sll_m_poisson_1d_hmf
      sll_comp64             , pointer  :: rhok(:)   !< fft(rho)
      type(sll_t_fft)                   :: fw        !< forward fft plan
      type(sll_t_fft)                   :: bw        !< backward fft plan
-  end type sll_t_poisson_1d_hmf
-
-  type,extends(sll_c_poisson_1d_base) :: sll_c_poisson_1d_hmf  
-  
-    type(sll_t_poisson_1d_hmf), pointer :: poiss
   
   contains
-    procedure, pass(poisson) :: sll_o_initialize => &
-      initialize_poisson_1d_hmf_wrapper
-    procedure, pass(poisson) :: compute_phi_from_rho => &
-      compute_phi_from_rho_1d_hmf
-    procedure, pass(poisson) :: compute_E_from_rho => &
-      compute_E_from_rho_1d_hmf
+
+    procedure, pass(self)    :: init => sll_s_init_poisson_1d_hmf
+    procedure, pass(self)    :: free => sll_s_free_poisson_1d_hmf
+    procedure, pass(poisson) :: compute_phi_from_rho => compute_phi_from_rho_1d_hmf
+    procedure, pass(poisson) :: compute_e_from_rho => compute_E_from_rho_1d_hmf
       
-  end type sll_c_poisson_1d_hmf 
-
-  !> Create a sll_o_new poisson solver on 1d mesh
-  interface sll_o_new
-     module procedure new_poisson_1d_hmf
-  end interface
-
-  !> sll_o_initialize a sll_o_new poisson solver on 1d mesh
-  interface sll_o_initialize
-     module procedure initialize_poisson_1d_hmf
-  end interface
-
-  !> sll_o_solve the Poisson equation on 1d mesh and compute the potential
-  interface sll_o_solve
-     module procedure solve_poisson_1d_hmf 
-  end interface
+  end type sll_t_poisson_1d_hmf 
 
 contains
 
-  !> Create a sll_o_new solver
-  !> @return
-  function new_poisson_1d_hmf(eta1_min,eta1_max,nc_eta1,error) &
-     result(self)
-     type(sll_t_poisson_1d_hmf),pointer :: self     !< Solver data structure
-     sll_int32,intent(in)              :: nc_eta1  !< number of cells
-     sll_int32, intent(out)            :: error    !< error code
-     sll_real64, intent(in)            :: eta1_min !< left corner
-     sll_real64, intent(in)            :: eta1_max !< right corner
+  !> Initialize the poisson 1d hmf solver
+  subroutine sll_s_init_poisson_1d_hmf(self,eta1_min,eta1_max,nc_eta1,error)
 
-     SLL_ALLOCATE(self, error)
-     call initialize_poisson_1d_hmf(self,eta1_min,eta1_max,nc_eta1,error)
-
-  end function new_poisson_1d_hmf 
-  
-  !> sll_o_initialize the solver
-  subroutine initialize_poisson_1d_hmf(self,eta1_min,eta1_max,nc_eta1,error)
-
-    type(sll_t_poisson_1d_hmf),intent(out) :: self     !< Solver data structure
-    sll_int32,intent(in)                  :: nc_eta1  !< number of cells
-    sll_int32, intent(out)                :: error    !< error code
-    sll_real64, intent(in)                :: eta1_min !< left corner
-    sll_real64, intent(in)                :: eta1_max !< right corner
-
-    sll_int32 :: i
+    class(sll_t_poisson_1d_hmf), intent(out)  :: self     !< Solver structure
+    sll_int32,intent(in)                      :: nc_eta1  !< number of cells
+    sll_int32, intent(out)                    :: error    !< error code
+    sll_real64, intent(in)                    :: eta1_min !< left corner
+    sll_real64, intent(in)                    :: eta1_max !< right corner
 
     error = 0
     ! geometry
@@ -127,13 +91,14 @@ contains
     call sll_s_fft_init_r2c_1d(self%fw,nc_eta1,self%tmp,self%rhok)
     call sll_s_fft_init_c2r_1d(self%bw,nc_eta1,self%rhok,self%tmp)
 
-  end subroutine initialize_poisson_1d_hmf
+  end subroutine sll_s_init_poisson_1d_hmf
 
-  subroutine solve_poisson_1d_hmf(self, field, rhs)
+  !> Solve the 1d equation for the Vlasov-HMF model
+  subroutine sll_s_solve_poisson_1d_hmf(self, field, rhs)
 
-    type(sll_t_poisson_1d_hmf),intent(inout) :: self
-    sll_real64, dimension(:), intent(out)   :: field
-    sll_real64, dimension(:), intent(in)    :: rhs
+    class(sll_t_poisson_1d_hmf),intent(inout) :: self
+    sll_real64, dimension(:), intent(out)     :: field
+    sll_real64, dimension(:), intent(in)      :: rhs
 
     SLL_ASSERT(size(rhs)==self%nc_eta1+1)
 
@@ -141,82 +106,46 @@ contains
     call sll_s_fft_exec_r2c_1d(self%fw, self%tmp, self%rhok)
 
     self%rhok(2)  = self%rhok(2) * sll_p_pi * sll_p_i1 ! We keep only one mode
-    self%rhok(1)  = 0.0_f64
-    self%rhok(3:) = 0.0_f64
+    self%rhok(1)  = sll_p_i0
+    self%rhok(3:) = sll_p_i0
 
     call sll_s_fft_exec_c2r_1d(self%bw, self%rhok, self%tmp)
 
-    field(1:self%nc_eta1) = self%tmp / self%nc_eta1
+    field(1:self%nc_eta1) = self%tmp / real(self%nc_eta1,f64)
     ! complete last term by periodicity
     field(self%nc_eta1+1) = field(1)
 
-  end subroutine solve_poisson_1d_hmf
+  end subroutine sll_s_solve_poisson_1d_hmf
 
-  function sll_f_new_poisson_1d_hmf( &
-    eta1_min, &
-    eta1_max, &
-    nc_eta1) &
-    result(poisson)
-      
-    type(sll_c_poisson_1d_hmf),pointer :: poisson
-    sll_real64, intent(in) :: eta1_min
-    sll_real64, intent(in) :: eta1_max
-    sll_int32, intent(in) :: nc_eta1
-    sll_int32 :: ierr
-      
-    SLL_ALLOCATE(poisson,ierr)
-    call initialize_poisson_1d_hmf_wrapper( &
-      poisson, &
-      eta1_min, &
-      eta1_max, &
-      nc_eta1)    
-  end function sll_f_new_poisson_1d_hmf
-  
-  subroutine initialize_poisson_1d_hmf_wrapper( &
-    poisson, &
-    eta1_min, &
-    eta1_max, &
-    nc_eta1)
-    class(sll_c_poisson_1d_hmf) :: poisson
-    sll_real64, intent(in) :: eta1_min
-    sll_real64, intent(in) :: eta1_max
-    sll_int32, intent(in) :: nc_eta1
-    sll_int32 :: ierr
-
-    
-    poisson%poiss => sll_o_new( &
-      eta1_min, &
-      eta1_max, &
-      nc_eta1, &
-      ierr)
-    
-  end subroutine initialize_poisson_1d_hmf_wrapper
-  
-  ! solves -\Delta phi = rho in 2d
+  !> solves \f[ -\Delta \phi = \rho \f] in 2d
   subroutine compute_phi_from_rho_1d_hmf( poisson, phi, rho )
-    class(sll_c_poisson_1d_hmf), target :: poisson
+    class(sll_t_poisson_1d_hmf), target :: poisson
     sll_real64,dimension(:),intent(in) :: rho
     sll_real64,dimension(:),intent(out) :: phi
     
-    print *,'#compute_phi_from_rho_1d_hmf'
-    print *,'#not implemented yet'
-    phi = 0._f64
-    if(.not.(associated(poisson%poiss)))then
-      print *,'#poisson%poiss not associated'
-    endif
-    print *,maxval(rho)  
-    stop
+    stop ' compute phi from rho is not implemented '
     
   end subroutine compute_phi_from_rho_1d_hmf
 
-  ! solves E = -\nabla Phi with -\Delta phi = rho in 2d 
+  !> solves \f[ E = -\nabla \phi \f] with \f[ -\Delta \phi = \rho \f] in 2d 
   subroutine compute_E_from_rho_1d_hmf( poisson, E, rho )
-    class(sll_c_poisson_1d_hmf) :: poisson
+    class(sll_t_poisson_1d_hmf) :: poisson
     sll_real64,dimension(:),intent(in) :: rho
     sll_real64,dimension(:),intent(out) :: E
       
-    call sll_o_solve(poisson%poiss, E, rho)
+    call sll_s_solve_poisson_1d_hmf(poisson, E, rho)
            
   end subroutine compute_E_from_rho_1d_hmf
+
+  !> Free the poisson 1d hmf solver
+  subroutine sll_s_free_poisson_1d_hmf(self, error)
+
+    class(sll_t_poisson_1d_hmf) :: self
+    sll_int32, intent(inout)    :: error    !< error code
+
+    deallocate(self%rhok,stat=error)
+    deallocate(self%tmp,stat=error)
+
+  end subroutine sll_s_free_poisson_1d_hmf
   
 end module sll_m_poisson_1d_hmf
--- src/field_solvers/poisson_solvers/testing/test_poisson_1d_hmf.F90
+++ src/field_solvers/poisson_solvers/testing/test_poisson_1d_hmf.F90
@@ -3,17 +3,15 @@ program test_poisson_1d_hmf
 #include "sll_memory.h"
 #include "sll_working_precision.h"
 
-  use sll_m_poisson_1d_base, only: &
-    sll_c_poisson_1d_base
+  use sll_m_poisson_1d_base, only: sll_c_poisson_1d_base
 
-  use sll_m_constants, only: &
-    sll_p_pi
+  use sll_m_constants, only: sll_p_pi
 
   use sll_m_poisson_1d_hmf, only: &
-    sll_o_initialize, &
-    sll_t_poisson_1d_hmf, &
-    sll_f_new_poisson_1d_hmf, &
-    sll_o_solve
+    sll_t_poisson_1d_hmf,         &
+    sll_s_init_poisson_1d_hmf,    &
+    sll_s_solve_poisson_1d_hmf,   &
+    sll_s_free_poisson_1d_hmf
 
   implicit none
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -23,8 +21,10 @@ program test_poisson_1d_hmf
   sll_real64, dimension(:),   allocatable :: rho
   sll_real64, dimension(:,:), allocatable :: f
 
-  type(sll_t_poisson_1d_hmf)       :: poisson
-  class(sll_c_poisson_1d_base), pointer :: poisson_class
+  class(sll_c_poisson_1d_base), pointer  :: poisson
+
+  type(sll_t_poisson_1d_hmf),   pointer :: p_poisson_1d_hmf
+  type(sll_t_poisson_1d_hmf),   allocatable, target  :: t_poisson_1d_hmf
 
   sll_int32   :: nc_eta1, nc_eta2
   sll_real64  :: eta1_min, eta2_min
@@ -48,52 +48,56 @@ program test_poisson_1d_hmf
 
   eta1_min = -sll_p_pi
   eta1_max = +sll_p_pi
-  delta_eta1 = (eta1_max-eta1_min) / nc_eta1
+  delta_eta1 = (eta1_max-eta1_min) / real(nc_eta1, f64)
   eta2_min = -3.0_f64
   eta2_max = +3.0_f64
-  delta_eta2 = (eta2_max-eta2_min) / nc_eta2
+  delta_eta2 = (eta2_max-eta2_min) / real(nc_eta2, f64)
 
   do j = 1, nc_eta2+1
-    v = eta2_min+(j-1)*delta_eta2
+    v = eta2_min+real(j-1,f64)*delta_eta2
     do i = 1, nc_eta1+1
-       x = eta1_min+(i-1)*delta_eta1
+       x = eta1_min+real(i-1,f64)*delta_eta1
        f(i,j) = alpha * exp(-beta*(((v*v)*0.5_f64) - m * cos(x)))
     end do
   end do
 
   do i=1,nc_eta1+1
-    x = eta1_min+(i-1)*delta_eta1
+    x = eta1_min+real(i-1,f64)*delta_eta1
     rho(i)      =  sum(f(i,:)) * delta_eta2
     ex_exact(i) = -m * sin(x)
   end do
+  
+  allocate(sll_t_poisson_1d_hmf :: p_poisson_1d_hmf)
 
-  call sll_o_initialize(poisson, eta1_min, eta1_max, nc_eta1, error) 
+  call sll_s_init_poisson_1d_hmf(p_poisson_1d_hmf, eta1_min, eta1_max, nc_eta1, error) 
 
-  call sll_o_solve(poisson, ex, rho)
-
-  do i = 1, nc_eta1
-    x = eta1_min+(i-1)*delta_eta1
-    write(11,*) x, ex(i), ex_exact(i)
-  end do
+  call sll_s_solve_poisson_1d_hmf(p_poisson_1d_hmf, ex, rho)
 
   e=maxval(abs(ex-ex_exact))
-  if (e > 1.e-3) stop 'FAILED'
+  if (e > 0.001_f64) stop 'FAILED'
   print*, 'error=', e
 
-  poisson_class => sll_f_new_poisson_1d_hmf( eta1_min, &
-                                                  eta1_max, &
-                                                  nc_eta1   )
+  call sll_s_free_poisson_1d_hmf(p_poisson_1d_hmf, error)
+  deallocate(p_poisson_1d_hmf)
+
+
+  !Use the poisson_1d_hmf type as a target for the base class
+
+  allocate(t_poisson_1d_hmf)
+  call t_poisson_1d_hmf%init( eta1_min, eta1_max, nc_eta1, error)
+  poisson => t_poisson_1d_hmf
+
   do i=1,nc_eta1+1
-    x = eta1_min+(i-1)*delta_eta1
+    x = eta1_min+real(i-1,f64)*delta_eta1
     rho(i)      =  sum(f(i,:)) * delta_eta2
     ex_exact(i) = -m * sin(x)
   end do
 
-  call poisson_class%compute_e_from_rho( ex, rho )
+  call poisson%compute_e_from_rho( ex, rho )
 
   e=maxval(abs(ex-ex_exact))
 
-  if (e > 1.e-3) stop 'FAILED'
+  if (e > 0.001_f64) stop 'FAILED'
 
   print*, 'error=', e
   print*, '#PASSED'
