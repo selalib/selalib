815adfc2f5395af9127570b03add6fdaec4e9537
Pierre Navaro <navaro@math.cnrs.fr>
2016-10-27 14:46:03 +0200

    Remove the mass parameter in Vlasov-HMF initialization

--- src/simulation_tools/initial_profiles/sll_m_common_array_initializers.F90
+++ src/simulation_tools/initial_profiles/sll_m_common_array_initializers.F90
@@ -810,6 +810,14 @@ contains
   end function sll_f_beam_initializer_2d
 
 
+  !> @brief
+  !> Function to intialize distribution function for the Vlasov-HMF model
+  !> @details
+  !>
+  !> "On numerical Landau damping for splitting methods applied to the Vlasov-HMF model"
+  !> 
+  !> by Erwan Faou, Romain Horsin and Frédéric Rousset.
+  !> https://arxiv.org/abs/1510.06555
   function sll_f_hmf_initializer_2d( x, vx, params ) result(res)
 
     sll_real64 :: res
@@ -818,24 +826,22 @@ contains
 
     sll_real64, dimension(:), intent(in), optional :: params
     sll_real64 :: alpha, beta
-    sll_real64 :: mass, m, eps
+    sll_real64 :: m, eps
 
     if( .not. present(params) ) then
        print *, '#sll_f_hmf_initializer_2d, error: the params array must ', &
-            'be passed. params = [alpha, beta, mass, m, epsilon]'
+            'be passed. params = [alpha, beta, m, epsilon]'
        stop
     end if
     SLL_ASSERT(size(params)==5)
     alpha  = params(1)
     beta   = params(2)
-    mass   = params(3)
-    m      = params(4)
-    eps    = params(5)
+    m      = params(3)
+    eps    = params(4)
 
     res = alpha 
     res = res * exp(-beta*((vx*vx)*0.5_f64 - m * cos(x))) 
     res = res * (1.0_f64 + eps * cos(x))
-
 
   end function sll_f_hmf_initializer_2d
   
