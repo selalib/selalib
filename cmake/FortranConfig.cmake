e657e18fa538d9b5f2be468530e55faf170e8dad
Pierre Navaro <navaro@math.cnrs.fr>
2017-01-03 13:57:13 +0100

    [cmake] Update of fortran configuration to add PGI compiler.

--- cmake/FortranConfig.cmake
+++ cmake/FortranConfig.cmake
@@ -7,12 +7,23 @@ SET(CMAKE_Fortran_MODULE_DIRECTORY "${CMAKE_BINARY_DIR}/modules")
 
 GET_FILENAME_COMPONENT(Fortran_COMPILER_NAME "${CMAKE_Fortran_COMPILER}" NAME)
 MESSAGE(STATUS "CMAKE_Fortran_COMPILER_ID:${CMAKE_Fortran_COMPILER_ID}")
+SET(FULL_FORTRAN2003 FALSE)
 
 IF (CMAKE_Fortran_COMPILER_ID MATCHES Intel)
 
   EXEC_PROGRAM(${CMAKE_Fortran_COMPILER} ARGS "-v" OUTPUT_VARIABLE source_path)
   MESSAGE(STATUS "${source_path}")
   STRING(REGEX MATCH "1[0-9]\\.[0-9]\\.[0-9]" Fortran_COMPILER_VERSION ${source_path})
+  SET(CMAKE_Fortran_FLAGS_RELEASE "-nowarn -O3 -xHost -ip -fpic")
+  SET(CMAKE_Fortran_FLAGS_DEBUG   "-g -O0 -check all,noarg_temp_created -fpe0 -traceback -ftrapuv -fpic")
+  SET(CMAKE_SHARED_LIBRARY_LINK_Fortran_FLAGS "-shared-intel")
+
+  if(Fortran_COMPILER_VERSION VERSION_LESS "14.0.0")
+    message(STATUS "Insufficient ifort version for the F2003 standard")
+  else()
+    message(STATUS "Intel fortran version OK for the F2003 standard")
+    SET(FULL_FORTRAN2003 TRUE)
+  endif()
 
 ELSEIF (CMAKE_Fortran_COMPILER_ID MATCHES PGI)
 
@@ -20,17 +31,18 @@ ELSEIF (CMAKE_Fortran_COMPILER_ID MATCHES PGI)
   STRING(REGEX MATCH "1[0-9]\\.[0-9][0-9]\\-[0-9]" Fortran_COMPILER_VERSION ${source_path})
   SET(CMAKE_Fortran_FLAGS_DEBUG "-Mbounds -O0 -g")
   SET(CMAKE_Fortran_FLAGS_RELEASE "-acc -Minfo=accel -fast ")
+  SET(FULL_FORTRAN2003 TRUE)
 
-ELSE()
+ELSEIF (CMAKE_Fortran_COMPILER_ID MATCHES IBM)
+
+  SET(CMAKE_Fortran_FLAGS_DEBUG   "-qextname=flush -qxlf2003=polymorphic")
+  SET(CMAKE_Fortran_FLAGS_RELEASE "-qnosave -qextname=flush -qxlf2003=polymorphic")
+  SET(FULL_FORTRAN2003 TRUE)
+
+ELSEIF (CMAKE_Fortran_COMPILER_ID MATCHES GNU)
 
   EXEC_PROGRAM(${CMAKE_Fortran_COMPILER} ARGS "--version" OUTPUT_VARIABLE source_path)
   STRING(REGEX MATCH "[4-7]\\.[0-9]\\.[0-9]" Fortran_COMPILER_VERSION ${source_path})
-
-ENDIF()
-
-MESSAGE(STATUS "Fortran ${Fortran_COMPILER_NAME}-${Fortran_COMPILER_VERSION}")
-
-IF(Fortran_COMPILER_NAME MATCHES gfortran)
 
   ADD_DEFINITIONS(-DGFORTRAN)
   SET(CMAKE_Fortran_FLAGS_RELEASE "-w -ffree-line-length-none -fall-intrinsics -O3 -fPIC -march=native ")
@@ -49,22 +61,20 @@ IF(Fortran_COMPILER_NAME MATCHES gfortran)
     SET(CMAKE_Fortran_FLAGS_DEBUG "${CMAKE_Fortran_FLAGS_DEBUG} -Wno-unused-dummy-argument")
   ENDIF()
 
-ELSEIF(Fortran_COMPILER_NAME MATCHES ifort)
-
-  SET(CMAKE_Fortran_FLAGS_RELEASE "-nowarn -O3 -xHost -ip -fpic")
-  SET(CMAKE_Fortran_FLAGS_DEBUG   "-g -O0 -check all,noarg_temp_created -fpe0 -traceback -ftrapuv -fpic")
-  SET(CMAKE_SHARED_LIBRARY_LINK_Fortran_FLAGS "-shared-intel")
-
-ELSEIF(Fortran_COMPILER MATCHES "IBM")
-
-  SET(CMAKE_Fortran_FLAGS_DEBUG   "-qextname=flush -qxlf2003=polymorphic")
-  SET(CMAKE_Fortran_FLAGS_RELEASE "-qnosave -qextname=flush -qxlf2003=polymorphic")
+  if(Fortran_COMPILER_VERSION VERSION_LESS "4.8.0")
+    message(STATUS "Insufficient gfortran version for the Fortran 2003 Standard")
+  else()
+    message(STATUS "GNU fortran version OK for the Fortran 2003 standard")
+    SET(FULL_FORTRAN2003 TRUE)
+  endif()
 
 ELSE()
 
   MESSAGE(SEND_ERROR "NO KNOWN FORTRAN COMPILER FOUND")
 
 ENDIF()
+
+MESSAGE(STATUS "Fortran ${Fortran_COMPILER_NAME}-${Fortran_COMPILER_VERSION}")
 
 SET(ADDITIONAL_COMPILER_FLAGS "" CACHE STRING "The user can define additional compiler flags here")
 SET(CMAKE_Fortran_FLAGS_DEBUG   "${CMAKE_Fortran_FLAGS_DEBUG} ${ADDITIONAL_COMPILER_FLAGS}")
@@ -75,26 +85,6 @@ IF(OPENMP_ENABLED)
   SET(CMAKE_Fortran_FLAGS_DEBUG "${CMAKE_Fortran_FLAGS_DEBUG} ${OpenMP_Fortran_FLAGS}")
   SET(CMAKE_Fortran_FLAGS_RELEASE "${CMAKE_Fortran_FLAGS_RELEASE} ${OpenMP_Fortran_FLAGS}")
 ENDIF()
-
-SET(FULL_FORTRAN2003 FALSE)
-
-if(Fortran_COMPILER_NAME STREQUAL "gfortran")
-  if(Fortran_COMPILER_VERSION VERSION_LESS "4.8.0")
-    message(STATUS "Insufficient gfortran version for the Fortran 2003 Standard")
-  else()
-    message(STATUS "GNU fortran version OK for the Fortran 2003 standard")
-    SET(FULL_FORTRAN2003 TRUE)
-  endif()
-#elseif(Fortran_COMPILER_NAME STREQUAL "ifort")
-elseif(Fortran_COMPILER_NAME MATCHES ifort)
-
-  if(Fortran_COMPILER_VERSION VERSION_LESS "14.0.0")
-    message(STATUS "Insufficient ifort version for the F2003 standard")
-  else()
-    message(STATUS "Intel fortran version OK for the F2003 standard")
-    SET(FULL_FORTRAN2003 TRUE)
-  endif()
-endif()
 
 IF(FULL_FORTRAN2003)
   ADD_DEFINITIONS(-DFULL_FORTRAN2003)
