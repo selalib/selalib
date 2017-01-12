332632ac05810603ca0118e8ee4c3444bc759716
Edoardo Zoni <edoardo.zoni@ipp.mpg.de>
2017-01-12 18:02:25 +0100

    [issue #166] Add unit tests for polar parallel 2d Poisson solver.

---
+++ src/field_solvers/poisson_solvers_parallel/testing/m_test_case_poisson_2d_base.F90
@@ -0,0 +1,62 @@
+module m_test_case_poisson_2d_base
+!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#include "sll_working_precision.h"
+
+implicit none
+
+public :: &
+  c_test_case_poisson_2d_polar
+
+private
+!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+  type, abstract :: c_test_case_poisson_2d_polar
+
+    sll_real64 :: rmin
+    sll_real64 :: rmax
+    sll_int32  :: bc_rmin
+    sll_int32  :: bc_rmax
+
+  contains
+    ! 2D manufactured solution
+    procedure( i_func_2d_real ), deferred :: phi_ex
+    procedure( i_func_2d_real ), deferred :: phi_ex_diff1_r
+    procedure( i_func_2d_real ), deferred :: phi_ex_diff2_r
+    procedure( i_func_2d_real ), deferred :: phi_ex_diff2_th
+    ! 2D right-hand side to solver
+    procedure, non_overridable :: rhs => f_test_case__rhs
+
+  end type c_test_case_poisson_2d_polar
+
+  !-----------------------------------------------------------------------------
+  abstract interface
+
+    ! 2D polar profile, scalar real function
+    pure function i_func_2d_real( self, r, th ) result( val )
+      use sll_m_working_precision
+      import c_test_case_poisson_2d_polar
+      class( c_test_case_poisson_2d_polar ), intent(in) :: self
+      sll_real64, intent(in) :: r
+      sll_real64, intent(in) :: th
+      sll_real64 :: val
+    end function i_func_2d_real
+
+  end interface
+
+!<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+contains
+!<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+  pure function f_test_case__rhs( self, r, th ) result( rho )
+    class(c_test_case_poisson_2d_polar), intent(in) :: self
+    sll_real64, intent(in) :: r
+    sll_real64, intent(in) :: th
+    sll_real64 :: rho
+
+    rho = - self%phi_ex_diff2_r( r, th ) - self%phi_ex_diff1_r( r, th )/r &
+          - self%phi_ex_diff2_th( r, th )/r**2
+
+  end function f_test_case__rhs
+
+  
+end module m_test_case_poisson_2d_base
---
+++ src/field_solvers/poisson_solvers_parallel/testing/m_test_case_poisson_2d_neumann_mode0.F90
@@ -0,0 +1,94 @@
+module m_test_case_poisson_2d_neumann_mode0
+!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#include "sll_working_precision.h"
+
+  use m_test_case_poisson_2d_base, only: c_test_case_poisson_2d_polar
+
+  implicit none
+
+  public :: t_test_neumann_mode0_zero_error
+
+  private
+!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+  type, extends(c_test_case_poisson_2d_polar) :: t_test_neumann_mode0_zero_error
+
+    !---------------------------------------------------------------------------
+    ! \phi(r,th) = a*Q(r) + b*sin(k*(th-th0))*R(r)
+    !
+    ! Q(r) = (r-rmax)(r+rmax-2rmin)
+    ! R(r) = (r-rmax)(r-rmin)
+    !---------------------------------------------------------------------------
+    sll_real64, private :: a   = 0.1_f64
+    sll_real64, private :: b   = 1.0_f64
+    sll_real64, private :: th0 = 0.3_f64
+    sll_int32,  private :: k   = 3
+
+  contains
+    ! 2D manufactured solution
+    procedure :: phi_ex          => neumann_mode0_zero_error_phi_ex
+    procedure :: phi_ex_diff1_r  => neumann_mode0_zero_error_phi_ex_d1r
+    procedure :: phi_ex_diff2_r  => neumann_mode0_zero_error_phi_ex_d2r
+    procedure :: phi_ex_diff2_th => neumann_mode0_zero_error_phi_ex_d2th
+
+  end type t_test_neumann_mode0_zero_error
+
+!<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+contains
+!<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+  pure function neumann_mode0_zero_error_phi_ex( self, r, th ) result( val )
+    class(t_test_neumann_mode0_zero_error), intent(in) :: self
+    sll_real64                        , intent(in) :: r
+    sll_real64                        , intent(in) :: th
+    sll_real64 :: val
+
+    val = self%a*(r-self%rmax)*(r+self%rmax-2.0_f64*self%rmin) &
+          + self%b*(r-self%rmin)*(r-self%rmax)*sin( self%k*(th-self%th0) )
+
+  end function neumann_mode0_zero_error_phi_ex
+
+
+  pure function neumann_mode0_zero_error_phi_ex_d1r( self, r, th ) result( val )
+    class(t_test_neumann_mode0_zero_error), intent(in) :: self
+    sll_real64                        , intent(in) :: r
+    sll_real64                        , intent(in) :: th
+    sll_real64 :: val
+
+    associate( rmin => self%rmin, rmax => self%rmax, &
+               a => self%a, b => self%b, k => self%k, th0 => self%th0 )
+
+      val = a*2.0_f64*(r-rmin) + b*(2.0_f64*r-rmin-rmax)*sin( k*(th-th0) )
+
+    end associate
+
+  end function neumann_mode0_zero_error_phi_ex_d1r
+
+
+  pure function neumann_mode0_zero_error_phi_ex_d2r( self, r, th ) result( val )
+    class(t_test_neumann_mode0_zero_error), intent(in) :: self
+    sll_real64                        , intent(in) :: r
+    sll_real64                        , intent(in) :: th
+    sll_real64 :: val
+
+    val = self%a*2.0_f64 + self%b*2.0_f64*sin( self%k*(th-self%th0) )
+
+  end function neumann_mode0_zero_error_phi_ex_d2r
+
+
+  pure function neumann_mode0_zero_error_phi_ex_d2th( self, r, th ) result( val )
+    class(t_test_neumann_mode0_zero_error), intent(in) :: self
+    sll_real64                        , intent(in) :: r
+    sll_real64                        , intent(in) :: th
+    sll_real64 :: val
+
+    associate( rmin => self%rmin, rmax => self%rmax, &
+               k => self%k, th0 => self%th0 )
+
+      val = -self%b*(r-rmin)*(r-rmax)*k**2*sin( k*(th-th0) )
+
+    end associate
+
+  end function neumann_mode0_zero_error_phi_ex_d2th
+
+end module m_test_case_poisson_2d_neumann_mode0
