a1f0679bdee57ed4ca91967c6cfbb12847471ce5
Yaman Güçlü <yaman.guclu@gmail.com>
2017-01-13 18:10:42 +0100

    [issue #166] Remove forgotten lines.

--- simulations/parallel/bsl_gk_3d1v_polar_multi_mu/sll_m_sim_bsl_gk_3d1v_polar_multi_mu.F90
+++ simulations/parallel/bsl_gk_3d1v_polar_multi_mu/sll_m_sim_bsl_gk_3d1v_polar_multi_mu.F90
@@ -590,7 +590,6 @@ contains
     sim%deltarTe = deltarTe
     
     SLL_ALLOCATE(lambda(1:num_cells_x1+1),ierr) 
-    SLL_ALLOCATE(tmp_r(num_cells_x1+1,2),ierr)  
     sim%delta_f_method=delta_f_method
 
     
