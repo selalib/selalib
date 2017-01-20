8d84c274cc24275962a64027d53e0a2d26d38af2
Edoardo Zoni <edoardo.zoni@ipp.mpg.de>
2017-01-20 15:42:20 +0100

    [issue #166] Restructure test-case classes for parallel quasi-neutral 2D polar solver (plus small cleanup).

--- src/field_solvers/poisson_solvers_parallel/testing/test_poisson_2d_polar_par.F90
+++ src/field_solvers/poisson_solvers_parallel/testing/test_poisson_2d_polar_par.F90
@@ -68,7 +68,6 @@ program test_poisson_2d_polar_par
   nth = 32
   tol = 1.0e-11_f64
 
-  ! Run test case
   call run_test( comm, test_case_dirichlet_zero_error, nr, nth, error_norm )
 
   ! Write relative error norm (global) to standard output
@@ -92,7 +91,6 @@ program test_poisson_2d_polar_par
   nth = 32
   tol = 1.0e-4_f64
 
-  ! Run test case
   call run_test( comm, test_case_dirichlet, nr, nth, error_norm )
 
   ! Write relative error norm (global) to standard output
@@ -116,7 +114,6 @@ program test_poisson_2d_polar_par
   nth = 32
   tol = 1.0e-11_f64
 
-  ! Run test case
   call run_test( comm, test_case_neumann_mode0_zero_error, nr, nth, error_norm )
 
   ! Write relative error norm (global) to standard output
