71697eb91f0839ce04437740b94c288dda086489
Tamas Bela Feher <tamas.bela.feher@ipp.mpg.de>
2017-02-09 15:43:10 +0100

    sed script inlined, comments added

--- cmake/Forcheck.cmake
+++ cmake/Forcheck.cmake
@@ -1,3 +1,34 @@
+# CMake module to create targets to run Forcheck static source code analysis
+#
+# Usage:
+# Add the following line to the beginning of the main CMakeLists.txt file
+# include(PreprocessorTarget)
+# 
+# At the end of the same CMakeLists.txt call
+# add_forcheck_target()
+# 
+# After generating the makefiles one can use
+# make forcheck
+#
+# This will run a Forcheck analysis over all the source files, and the results
+# of the analysis will be saved forcheck/selalib.lst. Additionally 
+# forcheck/selalib.rep file will be also created.
+# 
+# If you call cmake with -DFORCHECK_SEPARATE_TARGETS=1, then for each target,
+# an additional forcheck target will be also created with the name
+# forcheck_TARGETNAME. For example to run Forcheck for sll_working_precision use
+#
+# make forcheck_sll_working_precision
+#
+# The results will be saved in forcheck/sll_working_precision.lst
+# Additionally forcheck/sll_working_precision.flb will be also genearted which
+# stores interface information about the library. It is used to facilitate testing
+# other libraries which depend on sll_working_precision. 
+# 
+# To run forcheck for all library individually with a single command use
+# make forcheck_separate
+
+
 FIND_PROGRAM(FORCHECK_EXECUTABLE
   NAMES forchk
   PATHS $ENV{FCKDIR}/bin /usr/bin /bin /usr/local/bin
@@ -35,40 +66,45 @@ IF(FORCHECK_FOUND)
 ENDIF(FORCHECK_FOUND)
 
 
-# adds a custom target for running the Forcheck analysis
+# Adds custom targets for running the Forcheck analysis
 # call this function at the end of the CMakeLists.txt
 function(add_forcheck_target)
   if(FORCHECK_FOUND)
     add_forcheck_target_allfiles() # process all sources at once
-    add_forcheck_target_separate() # separet forcheck target for each target
+    if(FORCHECK_SEPARATE_TARGETS)
+      add_forcheck_target_separate() # separet forcheck target for each library/executable
+    endif()
   endif()
 endfunction()
 
 # Adds a single target "forcheck" to process all the source files at once.
 function(add_forcheck_target_allfiles)
-  # include flags needed for preprocessing the input files:
+  # Include flags needed for preprocessing the input files:
   get_property(_includes GLOBAL PROPERTY CPP_INCLUDES)
-  # set up include flags for preprocessing
   set(_incflags)
   foreach(i ${_includes})
     set(_incflags ${_incflags} -I${i})
   endforeach()
 
+  # List of source files that we will process with Forcheck
   get_property(_fck_sources GLOBAL PROPERTY CPP_SOURCES)  
   list(REMOVE_DUPLICATES _fck_sources)
-  
-  # Create custom commands for preprocessing the Fortran files
+
+  # If the files preprocessed by PreprocessorTargets.cmake would be suitable
+  # for forcheck analysis, then we would do
+  # get_property(_fck_preproc_sources GLOBAL PROPERTY CPP_PREPROC_SOURCES)
+  #
+  # Unfortunately the preprocessing defined in PreprocessorTargets.cmake is
+  # NOT suitable for Forcheck analysis:
+  #  - they can contain arbitrary long lines because of macro expansion,
+  #  - Intel's proprocessor brakes up these lines, but create non-standard
+  #    conforming code.
+  # Therefore in the following loop we create custom commands to preprocess the 
+  # source files with gfortran and fix the problem with long lines using sed.
   foreach (_src ${_fck_sources})
-     # Here we generate the name of the output (preprocessed) file
-     get_filename_component(_e "${_src}" EXT)
-     get_filename_component(_d "${_src}" DIRECTORY)
-     get_filename_component(_n "${_src}" NAME_WE)
-     string(REGEX REPLACE "F" "f" _e "${_e}")
-     # get the path relative to the source dir
-     string(REGEX REPLACE "^${CMAKE_SOURCE_DIR}" "" _d ${_d})
-     set(_preproc_name "${CMAKE_BINARY_DIR}${_d}/${_n}_forchk${_e}")
-    
-     # get the compiler definitions for the file
+     get_preprocessed_filename(${_src} _preproc_name _forchk)
+
+     # Get the compiler definitions for the file
      get_source_file_property(_defs "${_src}" COMPILE_DEFINITIONS)
      set(_defflags)
      foreach(_d ${_defs})
@@ -78,27 +114,39 @@ function(add_forcheck_target_allfiles)
      # Create the preprocessor command
      # The preprocessed file is piped through a sed script, 
      # to break up the long lines that contain ';'.
+     # To avoid trouble, we delete comment lines that contain  ';'.
      add_custom_command(OUTPUT "${_preproc_name}"
-         COMMAND gfortran  ${_incflags} ${_defflags} -cpp -E -P ${_src} | sed -f ${FORCHECK_INPUT_DIR}/sedscript -f ${FORCHECK_INPUT_DIR}/sedscript2  > ${_preproc_name}
+         COMMAND gfortran  ${_incflags} ${_defflags} -cpp -E -P ${_src} | sed -e "/^[ \\t]*!.*;/d" -e "/^.\\{132\\}/s/;/\\n/g" > ${_preproc_name}
          DEPENDS "${_src}"
          COMMENT "Preprocessing ${_src}"
          VERBATIM
        ) 
      list(APPEND _fck_preproc_sources ${_preproc_name})
   endforeach()
-
-  # group all preprocessing commands into one target
-  get_property(_fck_preproc_sources GLOBAL PROPERTY CPP_PREPROC_SOURCES) #hack
-  #add_custom_target(forcheck_preproc DEPENDS ${_fck_preproc_sources})
-  #set_target_properties(forcheck_preproc PROPERTIES EXCLUDE_FROM_ALL TRUE)
   
+  # Group all preprocessing commands into one target
+  add_custom_target(forcheck_preproc DEPENDS ${_fck_preproc_sources})
+  set_target_properties(forcheck_preproc PROPERTIES EXCLUDE_FROM_ALL TRUE)
+  
+  # Forcheck will analyze already preprocessed files.
+  # These files will not contain #include proprocessor directives anymore,
+  # but they can still have fortran include lines.
+  # Therefore, we need to get the include flags and pass it to Forcheck
   get_forcheck_includes(_fck_incflags)
-  # the Forcheck target
+  
+  # The Forcheck target
   add_custom_target(forcheck
-      COMMAND forchk -batch -allc -rep selalib.rep -l selalib.lst ${_fck_incflags} ${_fck_preproc_sources}  ${FORCHECK_EXTERNAL_FLBS}
+      COMMAND forchk -batch -allc -rep selalib.rep -l selalib.lst ${_fck_incflags} ${_fck_preproc_sources}  ${FORCHECK_EXTERNAL_FLBS} || true
       WORKING_DIRECTORY  ${FORCHECK_OUTPUT_DIR}
       COMMENT "Running Forcheck static source code analysis"
-      DEPENDS all_preproc)
+      DEPENDS forcheck_preproc)
+  # "|| true" is used because of Forcheck's exit status:
+  # 0 no informative, warning, overflow or error messages presented
+  # 2 informative, but no warning, overflow or error messages presented
+  # 4 warning, but no overflow or error messages presented
+  # 6 table overflow, but no error messages presented
+  # 8 error messages presented
+  # 16 fatal error occurred
   set_target_properties(forcheck PROPERTIES EXCLUDE_FROM_ALL TRUE)
 endfunction()
 
@@ -109,33 +157,30 @@ endfunction()
 # individually: forcheck_separate
 function(add_forcheck_target_separate)
   get_forcheck_includes(_fck_incflags)
-  get_property(_library_targets GLOBAL PROPERTY LIBRARY_TARGETS)
+  
+  get_property(_target_list GLOBAL PROPERTY LIBRARY_TARGETS)
   set(_forcheck_targets)
-  foreach(_name ${_library_targets})
-    get_target_property(_directory ${_name} SOURCE_DIR)
-    #get_target_property(_location ${_name} LOCATION)
-    #get_filename_component(_directory ${_location} DIRECTORY)
-    #message(STATUS "${_directory}")
-    #set(_directory $<TARGET_FILE_DIR:${_name}>)
+  # todo: append the list of executables to target_list
+  foreach(_name ${_target_list})
+    get_target_property(_source_directory ${_name} SOURCE_DIR)
     get_target_property(_sources ${_name} SOURCES)
     if (_sources)
       # we create a list of preprocessed source file names 
       set(_current_library_sources)
       foreach (_src ${_sources})
-        # Here we generate the name of the preprocessed source file
-        get_filename_component(_e "${_src}" EXT)
-        get_filename_component(_n "${_src}" NAME_WE)
-        string(REGEX REPLACE "F" "f" _e "${_e}")
-        set(_preproc_src "${_directory}/${_n}${_e}")
+        get_preprocessed_filename(${_source_directory}/${_src} _preproc_src "_forchk")
         list(APPEND _current_library_sources ${_preproc_src})
       endforeach()
     
       # Create a list of library dependencies
       get_flb_dependencies(${_name} _flb_dependencies)
-      string(REPLACE ";" " " _flbs "${_flb_dependencies}")
-      # add forcheck command for the library
+      # The COMMAND option will need a space separated list while the
+      # DEPNDS option nedds a comma separated list
+      string(REPLACE ";" " " _flb_space "${_flb_dependencies}")
+      
+      # Add forcheck command for the library
       add_custom_command(OUTPUT ${FORCHECK_OUTPUT_DIR}/${_name}.flb
-        COMMAND forchk -allc -batch -rep ${_name}.rep -l ${_name}.lst ${_fck_incflags} ${_current_library_sources} -update ${_name}.flb ${_flbs} ${FORCHECK_EXTERNAL_FLBS} || true
+        COMMAND forchk -allc -batch -rep ${_name}.rep -l ${_name}.lst ${_fck_incflags} ${_current_library_sources} -update ${_name}.flb ${_flb_space} ${FORCHECK_EXTERNAL_FLBS} || true
         DEPENDS ${_current_library_sources} ${_flb_dependencies}
         BYPRODUCTS ${FORCHECK_OUTPUT_DIR}/${_name}.rep ${FORCHECK_OUTPUT_DIR}/${_name}.lst
         WORKING_DIRECTORY ${FORCHECK_OUTPUT_DIR}
@@ -149,7 +194,6 @@ function(add_forcheck_target_separate)
       # 6 table overflow, but no error messages presented
       # 8 error messages presented
       # 16 fatal error occurred
-    
       add_custom_target(forcheck_${_name} DEPENDS  ${FORCHECK_OUTPUT_DIR}/${_name}.flb)
       set_target_properties(forcheck_${_name} PROPERTIES EXCLUDE_FROM_ALL TRUE)
       list(APPEND _forcheck_targets ${FORCHECK_OUTPUT_DIR}/${_name}.flb)
@@ -158,6 +202,26 @@ function(add_forcheck_target_separate)
   
   add_custom_target(forcheck_separate DEPENDS ${_forcheck_targets})
   set_target_properties(forcheck PROPERTIES EXCLUDE_FROM_ALL TRUE)
+endfunction()
+
+# Generate a name for the preprocessed source file
+# _src_path is the the path of the source file with relative or absolute path
+# _output_name is the name of the variable to store the results
+# There is an optional suffix argument:
+# get_preprocessed_filename(${source} _preproc_name "suffix")
+function(get_preprocessed_filename _src_path _output_name)
+  get_source_file_property(_loc ${_src_path} LOCATION)  # I need the full path
+  get_filename_component(_e "${_loc}" EXT)
+  get_filename_component(_n "${_loc}" NAME_WE)
+  get_filename_component(_dir "${_loc}" DIRECTORY)
+  string(REGEX REPLACE "^${CMAKE_SOURCE_DIR}" "${CMAKE_BINARY_DIR}" _dir ${_dir})
+  string(REGEX REPLACE "F" "f" _e "${_e}")
+  if(${ARGC} GREATER 2)
+     # add optional suffix
+     set(${_output_name} "${_dir}/${_n}${ARGV2}${_e}" PARENT_SCOPE)
+  else()
+     set(${_output_name} "${_dir}/${_n}${_e}" PARENT_SCOPE)
+  endif()
 endfunction()
 
 # Returns a comma separated list of all the preprocessor flags
@@ -211,7 +275,7 @@ function(get_flb_dependencies _name _output_name)
     math(EXPR _idx "${_idx} + 1")
   endwhile()
   
-  #message(STATUS "${_name} depends on ${_dependencies}")
+  # message(STATUS "${_name} depends on ${_dependencies}")
   set(_flb_dependencies)
   foreach(_libname ${_dependencies})
     list(APPEND _flb_dependencies ${FORCHECK_OUTPUT_DIR}/${_libname}.flb)
