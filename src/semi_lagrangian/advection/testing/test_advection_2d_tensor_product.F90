0f8c85c9538cd3f58ccf974595f68bab60d0a786
Pierre Navaro <pierre.navaro@univ-rennes1.fr>
2017-02-10 14:30:21 +0100

    [pgi] advection_2d_tensor_product is fixed

--- src/semi_lagrangian/advection/testing/test_advection_2d_tensor_product.F90
+++ src/semi_lagrangian/advection/testing/test_advection_2d_tensor_product.F90
@@ -21,14 +21,14 @@ program test_advection_2d_tensor_product
 #include "sll_working_precision.h"
 
 use sll_m_advection_1d_base, only: sll_c_advector_1d
-use sll_m_advection_1d_bsl,  only: sll_f_new_advector_1d_bsl
+use sll_m_advection_1d_bsl,  only: sll_t_advector_1d_bsl
 use sll_m_advection_2d_base, only: sll_c_advector_2d
 use sll_m_advection_2d_tensor_product, only: sll_t_advector_2d_tensor_product
 use sll_m_boundary_condition_descriptors, only: sll_p_periodic
 use sll_m_characteristics_1d_base, only: sll_c_characteristics_1d_base
 use sll_m_characteristics_1d_explicit_euler, only: sll_f_new_explicit_euler_1d_charac
 use sll_m_characteristics_2d_base, only: sll_c_characteristics_2d_base
-use sll_m_characteristics_2d_explicit_euler, only: sll_f_new_explicit_euler_2d_charac
+use sll_m_characteristics_2d_explicit_euler, only: sll_t_charac_2d_explicit_euler
 use sll_m_cubic_spline_interpolator_1d, only: sll_f_new_cubic_spline_interpolator_1d
 use sll_m_cubic_spline_interpolator_2d, only: sll_t_cubic_spline_interpolator_2d
 use sll_m_interpolators_1d_base, only: sll_c_interpolator_1d
@@ -99,55 +99,40 @@ A2 = 1._f64
 
 error = 0._f64
 
-interp_x1 => sll_f_new_cubic_spline_interpolator_1d( &
-  num_cells_x1+1, &
-  x1_min, &
-  x1_max, &
-  sll_p_periodic)
+interp_x1 => sll_f_new_cubic_spline_interpolator_1d( num_cells_x1+1, &
+  x1_min, x1_max, sll_p_periodic)
+interp_x2 => sll_f_new_cubic_spline_interpolator_1d( num_cells_x2+1, &
+  x2_min, x2_max, sll_p_periodic)
 
-charac_x1 => sll_f_new_explicit_euler_1d_charac(&
-    num_cells_x1+1, &
-    sll_p_periodic)
+charac_x1 => sll_f_new_explicit_euler_1d_charac( num_cells_x1+1, sll_p_periodic)
+charac_x2 => sll_f_new_explicit_euler_1d_charac( num_cells_x2+1, sll_p_periodic)
 
-adv_x1 => sll_f_new_advector_1d_bsl(&
-  interp_x1, &
-  charac_x1, &
-  num_cells_x1+1, &
-  eta_coords = x1_mesh)
+allocate( sll_t_advector_1d_bsl :: adv_x1 )
+select type( a => adv_x1 )
+type is (sll_t_advector_1d_bsl)
+  call a%init( interp_x1, charac_x1, num_cells_x1+1, eta_coords = x1_mesh)
+end select
 
-interp_x2 => sll_f_new_cubic_spline_interpolator_1d( &
-  num_cells_x2+1, &
-  x2_min, &
-  x2_max, &
-  sll_p_periodic)
+allocate( sll_t_advector_1d_bsl :: adv_x2 )
+select type( a => adv_x2 )
+type is (sll_t_advector_1d_bsl)
+  call a%init( interp_x2, charac_x2, num_cells_x2+1, eta_coords = x2_mesh)
+end select
 
-charac_x2 => sll_f_new_explicit_euler_1d_charac(&
-    num_cells_x2+1, &
-    sll_p_periodic)
-
-adv_x2 => sll_f_new_advector_1d_bsl(&
-  interp_x2, &
-  charac_x2, &
-  num_cells_x2+1, &
-  eta_coords = x2_mesh)
-
-call interp_cs2d%init( &
-  num_cells_x1+1, &
-  num_cells_x2+1, &
-  x1_min, &
-  x1_max, &
-  x2_min, &
-  x2_max, &
-  sll_p_periodic, &
-  sll_p_periodic)
+call interp_cs2d%init( num_cells_x1+1, num_cells_x2+1, &
+  x1_min, x1_max, x2_min, x2_max, sll_p_periodic, sll_p_periodic)
 
 interp => interp_cs2d
 
-charac => sll_f_new_explicit_euler_2d_charac(&
-    num_cells_x1+1, &
-    num_cells_x2+1, &
-    sll_p_periodic, &
-    sll_p_periodic)
+allocate( sll_t_charac_2d_explicit_euler :: charac )
+
+select type ( c => charac )
+
+type is ( sll_t_charac_2d_explicit_euler )
+
+  call c%init( num_cells_x1+1, num_cells_x2+1, sll_p_periodic, sll_p_periodic)
+
+end select
 
 allocate( sll_t_advector_2d_tensor_product :: adv )
 
@@ -162,6 +147,6 @@ call adv%advect_2d(a1, a2, dt, input, output)
 error = maxval(abs(input-output))
 
 print *,'#error = ', error
-if(error < 1.e-15_f64) print *,'#PASSED' 
+if (error < 1.e-15_f64) print *,'#PASSED' 
 
 end program test_advection_2d_tensor_product
