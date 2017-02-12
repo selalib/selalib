6b724acf854de4014864dc0c668460ad300c58db
Pierre Navaro <navaro@math.cnrs.fr>
2017-02-12 14:10:14 +0100

    [pgi] bsl_gc_2d0v_cart
    If I replace sll_f_new functions for cubic splines interpolators 2d. It
    works for PGI but not for GNU anymore.

--- simulations/serial/bsl_gc_2d0v_cart/sim_bsl_gc_2d0v_cart.F90
+++ simulations/serial/bsl_gc_2d0v_cart/sim_bsl_gc_2d0v_cart.F90
@@ -4,20 +4,22 @@ program sim_bsl_gc_2d0v_cart
     sll_c_simulation_base_class
 
   use sll_m_sim_bsl_gc_2d0v_cart, only: &
+     initialize_guiding_center_2d_cartesian, &
+     sll_simulation_2d_guiding_center_cartesian, &
     sll_f_new_guiding_center_2d_cartesian
 
   implicit none
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  class(sll_c_simulation_base_class), pointer :: sim
+  type(sll_simulation_2d_guiding_center_cartesian) :: sim
   character(len=256) :: filename
   character(len=256) :: filename_local
 
   call get_command_argument(1, filename)
   if (len_trim(filename) == 0)then
-    sim => sll_f_new_guiding_center_2d_cartesian( )
+     call initialize_guiding_center_2d_cartesian(sim)
   else
     filename_local = trim(filename)
-    sim => sll_f_new_guiding_center_2d_cartesian( filename_local )
+     call initialize_guiding_center_2d_cartesian(sim, filename_local )
   endif
   call sim%run( )
   print *,'#PASSED'
