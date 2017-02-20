f559557eac859182ce4926372febc6589e65fce1
Pierre Navaro <navaro@math.cnrs.fr>
2017-02-20 14:54:49 +0100

    #110 Move sprng example to external

--- cmake/BuildSPRNG.cmake
+++ cmake/BuildSPRNG.cmake
@@ -1,17 +1,26 @@
 FIND_PACKAGE(MPI REQUIRED CXX)
-SET(SPRNG_CXX_COMPILER ${MPI_CXX_COMPILER} CACHE FILEPATH  "CXX compiler for SPRNG")
-SET(SPRNG_LIBS "-lmpi_cxx" CACHE STRING  "LIBS configure argument for SPRNG")
 IF(APPLE)
+
   MESSAGE(STATUS "Warning: Build SPRNG library on mac is tricky")
+  MESSAGE(STATUS "openmpi must be configured with cxx bindings")
+  SET(SPRNG_CONFIGURE_COMMAND ${CMAKE_BINARY_DIR}/sprng/configure --prefix=${CMAKE_BINARY_DIR}/sprng --with-mpi=yes )
+
+ELSE()
+
+  SET(SPRNG_CXX_COMPILER ${MPI_CXX_COMPILER} CACHE FILEPATH  "CXX compiler for SPRNG")
+  SET(SPRNG_LIBS "-lmpi_cxx" CACHE STRING  "LIBS configure argument for SPRNG")
+  SET(SPRNG_CONFIGURE_COMMAND ${CMAKE_BINARY_DIR}/sprng/configure --enable-silent-rules --prefix=${CMAKE_BINARY_DIR}/sprng --with-mpi=yes CXX=${SPRNG_CXX_COMPILER} LIBS=${SPRNG_LIBS} "CXXFLAGS=-w -g -O2")
+
 ENDIF(APPLE)
+
 INCLUDE(ExternalProject)
 SET(SPRNG_VERSION 5 CACHE STRING "SPRNG version number")
+
 EXTERNALPROJECT_ADD( sprng
    URL  http://www.sprng.org/Version5.0/sprng${SPRNG_VERSION}.tar.bz2
    SOURCE_DIR ${CMAKE_BINARY_DIR}/sprng
    BINARY_DIR ${CMAKE_BINARY_DIR}/sprng
-   #UPDATE_COMMAND autoreconf -if
-   CONFIGURE_COMMAND ${CMAKE_BINARY_DIR}/sprng/configure --enable-silent-rules --prefix=${CMAKE_BINARY_DIR}/sprng --with-mpi=yes CXX=${SPRNG_CXX_COMPILER} LIBS=${SPRNG_LIBS} "CXXFLAGS=-w -g -O2"
+   CONFIGURE_COMMAND ${SPRNG_CONFIGURE_COMMAND}
    BUILD_COMMAND ${MAKE}
    INSTALL_COMMAND ""
 )
