d2374306caaf90c6aea1997a59bed3f8be479d89
Pierre Navaro <pierre.navaro@univ-rennes1.fr>
2017-02-20 17:12:12 +0100

    Revert "[sprng] Fix sprng build on fedora"
    This reverts commit 20113c734d2400a75a5775f4e6f27ea34319b25b.

--- cmake/BuildSPRNG.cmake
+++ cmake/BuildSPRNG.cmake
@@ -4,14 +4,12 @@ IF(APPLE)
   MESSAGE(STATUS "SPRNG : Warning Build the library on mac is tricky")
   MESSAGE(STATUS "SPRNG : openmpi must be configured with cxx bindings")
   SET(SPRNG_PATCH_COMMAND patch -p0 < ${CMAKE_CURRENT_SOURCE_DIR}/sprng/sprng-mac.patch)
-  SET(SPRNG_UPDATE_COMMAND  "")
   SET(SPRNG_CONFIGURE_COMMAND ${CMAKE_BINARY_DIR}/sprng/configure --prefix=${CMAKE_BINARY_DIR}/sprng --with-mpi  LIBS=-lmpi_cxx )
 
 ELSE()
 
   SET(SPRNG_CXX_COMPILER ${MPI_CXX_COMPILER} CACHE FILEPATH  "CXX compiler for SPRNG")
   SET(SPRNG_PATCH_COMMAND patch -p0 < ${CMAKE_CURRENT_SOURCE_DIR}/sprng/sprng-mac.patch)
-  SET(SPRNG_UPDATE_COMMAND  ${CMAKE_BINARY_DIR}/sprng/configure; make )
   SET(SPRNG_CONFIGURE_COMMAND ${CMAKE_BINARY_DIR}/sprng/configure --prefix=${CMAKE_BINARY_DIR}/sprng --with-mpi )
 
 ENDIF(APPLE)
@@ -24,7 +22,6 @@ EXTERNALPROJECT_ADD( sprng
    SOURCE_DIR ${CMAKE_BINARY_DIR}/sprng
    BINARY_DIR ${CMAKE_BINARY_DIR}/sprng
    PATCH_COMMAND ${SPRNG_PATCH_COMMAND}
-   UPDATE_COMMAND ${SPRNG_UPDATE_COMMAND}
    CONFIGURE_COMMAND ${SPRNG_CONFIGURE_COMMAND}
    BUILD_COMMAND ${MAKE}
    INSTALL_COMMAND ""
