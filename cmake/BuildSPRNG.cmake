3a8e939f943690436c2cda54f50a224bf5315b93
Pierre Navaro <navaro@math.cnrs.fr>
2017-02-20 15:58:56 +0100

    [sprng] Fix apple build by adding a patch

--- cmake/BuildSPRNG.cmake
+++ cmake/BuildSPRNG.cmake
@@ -1,14 +1,16 @@
 FIND_PACKAGE(MPI REQUIRED CXX)
 IF(APPLE)
 
-  MESSAGE(STATUS "Warning: Build SPRNG library on mac is tricky")
-  MESSAGE(STATUS "openmpi must be configured with cxx bindings")
-  SET(SPRNG_CONFIGURE_COMMAND ${CMAKE_BINARY_DIR}/sprng/configure --prefix=${CMAKE_BINARY_DIR}/sprng --with-mpi=yes )
+  MESSAGE(STATUS "SPRNG : Warning Build the library on mac is tricky")
+  MESSAGE(STATUS "SPRNG : openmpi must be configured with cxx bindings")
+  SET(SPRNG_PATCH_COMMAND patch -p0 < ${CMAKE_CURRENT_SOURCE_DIR}/sprng/sprng-mac.patch)
+  SET(SPRNG_CONFIGURE_COMMAND ${CMAKE_BINARY_DIR}/sprng/configure --prefix=${CMAKE_BINARY_DIR}/sprng --with-mpi  LIBS=-lmpi_cxx )
 
 ELSE()
 
   SET(SPRNG_CXX_COMPILER ${MPI_CXX_COMPILER} CACHE FILEPATH  "CXX compiler for SPRNG")
   SET(SPRNG_LIBS "-lmpi_cxx" CACHE STRING  "LIBS configure argument for SPRNG")
+  SET(SPRNG_PATCH_COMMAND "")
   SET(SPRNG_CONFIGURE_COMMAND ${CMAKE_BINARY_DIR}/sprng/configure --enable-silent-rules --prefix=${CMAKE_BINARY_DIR}/sprng --with-mpi=yes CXX=${SPRNG_CXX_COMPILER} LIBS=${SPRNG_LIBS} "CXXFLAGS=-w -g -O2")
 
 ENDIF(APPLE)
@@ -20,6 +22,7 @@ EXTERNALPROJECT_ADD( sprng
    URL  http://www.sprng.org/Version5.0/sprng${SPRNG_VERSION}.tar.bz2
    SOURCE_DIR ${CMAKE_BINARY_DIR}/sprng
    BINARY_DIR ${CMAKE_BINARY_DIR}/sprng
+   PATCH_COMMAND ${SPRNG_PATCH_COMMAND}
    CONFIGURE_COMMAND ${SPRNG_CONFIGURE_COMMAND}
    BUILD_COMMAND ${MAKE}
    INSTALL_COMMAND ""
