7aa28d081ea06ab8b5f1cbd82583dfab96dbb2a0
Pierre Navaro <navaro@math.cnrs.fr>
2017-02-20 11:35:48 +0100

    [pgi] Add output to pic_opt2d tests to get more info when they fail

--- src/particle_methods/pic_opt2d/pic_opt2d_utilities/testing/test_charge_to_density.F90
+++ src/particle_methods/pic_opt2d/pic_opt2d_utilities/testing/test_charge_to_density.F90
@@ -20,7 +20,7 @@ type(sll_t_electric_field_accumulator_cs) :: e_accumulator_cs
 sll_int32, parameter :: num_cells1 = 10
 sll_int32, parameter :: num_cells2 = 10
 
-sll_real64 :: ref = 12100_f64
+sll_real64 :: ref = 12100_f64, s
 
 allocate(rho(num_cells1+1,num_cells2+1))
 allocate(e_1(num_cells1+1,num_cells2+1))
@@ -35,9 +35,12 @@ call sll_s_convert_charge_to_rho_2d_per_per( charge, rho )
 call sll_s_field_accumulator_2d_init( e_accumulator, mesh_2d ) 
 call sll_s_accumulate_field( e_1, e_2, e_accumulator)
 
-if ( abs(sum(rho) - ref) > 1d-7) stop 'FAILED'
-if ( abs(sum(e_1)) > 1d-7) stop 'FAILED'
-if ( abs(sum(e_2)) > 1d-7) stop 'FAILED'
+s = sum(rho)
+if ( abs(s - ref) > 1d-7) stop 'FAILED'
+s = sum(e_1)
+if ( abs(s) > 1d-7) stop 'FAILED'
+s = sum(e_2)
+if ( abs(s) > 1d-7) stop 'FAILED'
 
 call sll_s_charge_accumulator_2d_cs_init(charge_cs, mesh_2d )
 charge_cs%q_acc%q_ij = 1.0_f64
@@ -45,9 +48,12 @@ call sll_s_convert_charge_to_rho_2d_per_per_cs( charge_cs, rho )
 call sll_s_field_accumulator_cs_2d_init( e_accumulator_cs, mesh_2d ) 
 call sll_s_accumulate_field_cs( e_1, e_2, e_accumulator_cs)
 
-if ( abs(sum(rho) - ref) > 1d-7) stop 'FAILED'
-if ( abs(sum(e_1)) > 1d-7) stop 'FAILED'
-if ( abs(sum(e_2)) > 1d-7) stop 'FAILED'
+s = sum(rho)
+if ( abs(s - ref) > 1d-7) stop 'FAILED'
+s = sum(e_1)
+if ( abs(s) > 1d-7) stop 'FAILED'
+s = sum(e_2)
+if ( abs(s) > 1d-7) stop 'FAILED'
 
 print*, 'PASSED'
 end program test_charge_to_density
