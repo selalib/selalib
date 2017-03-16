selalib/selalib

commit f2d119f7c7e10550c47128635e3e8e87fd15808e

[forcheck] Make params argument non-optional for sll_i_scalar_initializer_Xd functions.

src/semi_lagrangian/fcisl/sll_m_fcisl_toroidal.F90
@@ -170,17 +170,14 @@ function toroidal_1(theta,phi,params) result(res)
   sll_real64 :: res
   sll_real64, intent(in) :: theta
   sll_real64, intent(in) :: phi
-  sll_real64, dimension(:), intent(in), optional :: params
+  sll_real64, dimension(:), intent(in) :: params
 
   sll_real64 :: psipr
   sll_real64 :: smallr
-  if (present(params)) then
-     smallr = params(1)
-     psipr = params(2)
-  else
-     smallr = 1.0_f64
-     psipr = 1.0_f64
-  end if
+
+  smallr = 1.0_f64
+  psipr = 1.0_f64
+
   res = -psipr/smallr  
   return !PN ADD TO PREVENT WARNING
   print*, phi, theta
@@ -190,22 +187,16 @@ function toroidal_2(theta,phi,params) result(res)
   sll_real64 :: res
   sll_real64, intent(in) :: theta
   sll_real64, intent(in) :: phi
-  sll_real64, dimension(:), intent(in), optional :: params
+  sll_real64, dimension(:), intent(in) :: params
 
   sll_real64 :: bigr
   sll_real64 :: F0
   sll_real64 :: R0
   sll_real64 :: smallr
 
-  if (present(params)) then
-     smallr = params(1)
-     R0 = params(2)
-     F0 = params(3)
-  else
-     smallr = 1.0_f64
-     R0 = 1.0_f64
-     F0 = 1.0_f64
-  end if
+  smallr = 1.0_f64
+  R0 = 1.0_f64
+  F0 = 1.0_f64
      
   bigr = R0+smallr*cos(theta)
   res=F0/bigr

src/parallelization/parallel_array_utilities/sll_m_parallel_array_initializer.F90
@@ -77,7 +77,7 @@ contains
     type(sll_t_cartesian_mesh_2d), pointer          :: mesh2d
     sll_real64, dimension(:,:), intent(out) :: array
     procedure(sll_i_scalar_initializer_2d)        :: func
-    sll_real64, dimension(:), optional          :: func_params
+    sll_real64, dimension(:), intent(in)          :: func_params
 
 
     sll_int32  :: i
@@ -154,7 +154,7 @@ contains
     sll_real64, dimension(:), intent(in) :: x2_array
     sll_real64, dimension(:,:), intent(out) :: array
     procedure(sll_i_scalar_initializer_2d)        :: func
-    sll_real64, dimension(:), optional          :: func_params
+    sll_real64, dimension(:), intent(in) :: func_params
 
 
     sll_int32  :: i
@@ -228,8 +228,8 @@ contains
     sll_real64, intent(in) :: delta3
     sll_real64, intent(in) :: delta4
     sll_real64, dimension(:,:,:,:), intent(out) :: array
-    procedure(sll_i_scalar_initializer_4d)        :: func
-    sll_real64, dimension(:), optional          :: func_params
+    procedure(sll_i_scalar_initializer_4d)      :: func
+    sll_real64, dimension(:), intent(in)        :: func_params
     sll_int32  :: i
     sll_int32  :: j
     sll_int32  :: k
@@ -313,7 +313,7 @@ contains
     type(sll_t_cartesian_mesh_1d), pointer          :: mesh1d_eta4
     sll_real64, dimension(:,:,:,:), intent(out) :: array
     procedure(sll_i_scalar_initializer_4d)        :: func
-    sll_real64, dimension(:), optional          :: func_params
+    sll_real64, dimension(:), intent(in)          :: func_params
     sll_real64  :: eta1_min
     sll_real64  :: eta2_min
     sll_real64  :: eta3_min
@@ -362,7 +362,7 @@ contains
     type(sll_t_cartesian_mesh_4d), pointer          :: mesh4d
     sll_real64, dimension(:,:,:,:), intent(out) :: array
     procedure(sll_i_scalar_initializer_4d)        :: func
-    sll_real64, dimension(:), optional          :: func_params
+    sll_real64, dimension(:), intent(in)          :: func_params
 
     sll_real64 :: delta1
     sll_real64 :: delta2
@@ -422,7 +422,7 @@ contains
     type(sll_t_cartesian_mesh_2d), pointer          :: mesh2d_eta3_eta4
     sll_real64, dimension(:,:,:,:), intent(out) :: array
     procedure(sll_i_scalar_initializer_4d)        :: func
-    sll_real64, dimension(:), optional          :: func_params
+    sll_real64, dimension(:), intent(in)          :: func_params
 
     class(sll_c_coordinate_transformation_2d_base), pointer, optional :: &
          transf_x1_x2
@@ -847,7 +847,7 @@ contains
     type(sll_t_cartesian_mesh_2d), pointer          :: mesh2d_eta3_eta4
     sll_real64, dimension(:,:,:,:), intent(out) :: array
     procedure(sll_i_scalar_initializer_4d)        :: func
-    sll_real64, dimension(:), optional          :: func_params
+    sll_real64, dimension(:), intent(in)          :: func_params
     sll_int32,optional   :: subcells1
     sll_int32,optional   :: subcells2
     sll_int32,optional   :: subcells3

commit 8af2198e665b0254b6bb32857ea24994708d3cb0

[forcheck] Read from parameter in fcisl_toroidal (bug introduced in Commit f2d119f7.

src/semi_lagrangian/fcisl/sll_m_fcisl_toroidal.F90
@@ -175,8 +175,8 @@ function toroidal_1(theta,phi,params) result(res)
   sll_real64 :: psipr
   sll_real64 :: smallr
 
-  smallr = 1.0_f64
-  psipr = 1.0_f64
+  smallr = params(1)! default: 1.0_f64
+  psipr = params(2) ! default: 1.0_f64
 
   res = -psipr/smallr  
   return !PN ADD TO PREVENT WARNING
@@ -194,9 +194,9 @@ function toroidal_2(theta,phi,params) result(res)
   sll_real64 :: R0
   sll_real64 :: smallr
 
-  smallr = 1.0_f64
-  R0 = 1.0_f64
-  F0 = 1.0_f64
+  smallr = params(1) ! default: 1.0_f64
+  R0 = params(2) ! default: 1.0_f64
+  F0 = params(3) ! default: 1.0_f64
      
   bigr = R0+smallr*cos(theta)
   res=F0/bigr