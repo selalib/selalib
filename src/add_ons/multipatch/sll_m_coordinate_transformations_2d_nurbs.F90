a9a80752e52067bab1934354f2eebc82c66d5695
Katharina Kormann <katharina.kormann@tum.de>
2017-03-16 15:22:27 +0100

    [forcheck] Add missing intent in write_to_file of coordinate_transformations_2d.

--- src/add_ons/multipatch/sll_m_coordinate_transformations_2d_nurbs.F90
+++ src/add_ons/multipatch/sll_m_coordinate_transformations_2d_nurbs.F90
@@ -815,7 +815,7 @@ contains
 
   subroutine write_to_file_2d_nurbs(transf,output_format)
     class(sll_t_coordinate_transformation_2d_nurbs), intent( inout ) :: transf
-    sll_int32, optional :: output_format 
+    sll_int32, intent( in ), optional :: output_format 
     class(sll_t_cartesian_mesh_2d), pointer :: lm
     sll_int32           :: local_format 
     sll_real64, dimension(:,:), pointer :: x1mesh
