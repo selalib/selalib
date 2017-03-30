de6a34747cd64bb96cf621c0f6e73f6d4880311a
Katharina Kormann <katharina.kormann@tum.de>
2017-03-30 11:44:49 +0200

    Update gitignore for eclipse.

---
+++ cmake/CompilerConfigString.cmake
@@ -0,0 +1,8 @@
+set(SLL_COMP "${Fortran_COMPILER_NAME} ${Fortran_COMPILER_VERSION}")
+IF(CMAKE_BUILD_TYPE MATCHES "Debug")
+   set(SLL_COMP "${SLL_COMP} (${CMAKE_Fortran_FLAGS_DEBUG})")
+ELSE()
+   set(SLL_COMP "${SLL_COMP} (${CMAKE_Fortran_FLAGS_RELEASE})")
+ENDIF()
+MESSAGE( STATUS "SLL_COMP:${SLL_COMP}" )
+add_definitions( -DSLL_COMP="${SLL_COMP}" )
