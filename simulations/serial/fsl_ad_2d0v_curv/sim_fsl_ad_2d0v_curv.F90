f81061fae195785136491264511083650b24db0a
Katharina Kormann <katharina.kormann@tum.de>
2017-04-02 17:09:49 +0200

    [update_interpolation] Remove pointer.

--- simulations/serial/fsl_ad_2d0v_curv/sim_fsl_ad_2d0v_curv.F90
+++ simulations/serial/fsl_ad_2d0v_curv/sim_fsl_ad_2d0v_curv.F90
@@ -20,7 +20,7 @@ program sim_fsl_ad_2d0v_curv
   implicit none
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-  type(sll_t_cubic_spline_2d), pointer :: spl_bsl,spl_bsl_nc,spl_fsl,spl_fsl_nc
+  type(sll_t_cubic_spline_2d) :: spl_bsl,spl_bsl_nc,spl_fsl,spl_fsl_nc
   sll_int32  :: N,Neta1,Neta2,mesh_case,test_case,step,nb_step,visu_step,field_case
   sll_int32  :: i,j,bc1_type,bc2_type,err,it
   sll_int32  :: i1,i2,i3
