b90470237487ca1a512efca0f8369f01dfd5c47e
Katharina Kormann <katharina.kormann@tum.de>
2017-04-02 17:02:55 +0200

    [update_interpolation] Rename sll_s_deposit_value_2d -> sll_s_cubic_spline_2d_deposit_value.

--- simulations/serial/fsl_ad_2d0v_polar/sim_fsl_ad_2d0v_polar.F90
+++ simulations/serial/fsl_ad_2d0v_polar/sim_fsl_ad_2d0v_polar.F90
@@ -12,7 +12,7 @@ program sim_fsl_ad_2d0v_polar
 
   use sll_m_cubic_splines, only: &
     sll_s_cubic_spline_2d_compute_interpolant, &
-    sll_s_deposit_value_2d, &
+    sll_s_cubic_spline_2d_deposit_value, &
     sll_f_cubic_spline_2d_eval, &
     sll_s_cubic_spline_2d_init, &
     sll_t_cubic_spline_2d
@@ -452,8 +452,8 @@ program sim_fsl_ad_2d0v_polar
       enddo
     enddo
     
-    call sll_s_deposit_value_2d(rfeet,thetafeet,spl_fsl,fh_fsl)
-    call sll_s_deposit_value_2d(rfeet,thetafeet,spl_fsl_nc,fh_fsl_nc)
+    call sll_s_cubic_spline_2d_deposit_value(rfeet,thetafeet,spl_fsl,fh_fsl)
+    call sll_s_cubic_spline_2d_deposit_value(rfeet,thetafeet,spl_fsl_nc,fh_fsl_nc)
     
     do i=1,Nr+1
       r=rmin+real(i-1,f64)*dr
