2d381a283e96a5c36c067219c470cfc121358ad5
Katharina Kormann <katharina.kormann@tum.de>
2017-06-30 10:12:18 +0200

    [thirdparty] Add VP4D_hybrid and DK4D_hybrid to thirdparty.

---
+++ thirdparty/DK4D_hybrid/src/vlasov_DK4D_hybrid.F90
@@ -0,0 +1,486 @@
+!===========================================================================
+!> Solving of the vlasov equation for
+!>  4D drift-kinetic hybrid simulation
+!>
+!> \date 2014-08-21
+!> \author V. Grandgirard
+!---------------------------------------------------------------------------
+module vlasov_DK4D_hybrid_module
+#include "sll_working_precision.h"
+#include "sll_memory.h"
+#include "sll_assert.h"
+
+  use elecpot_DK4D_hybrid_module
+  use fdistribu_DK4D_module
+  use field3d_DK4D_module
+  use magnetconf_DK4D_module
+  use mesh_DK4D_module
+!eaoter
+!VG!  use sll_m_arbitrary_degree_spline_interpolator_1d
+!VG!  use sll_m_arbitrary_degree_spline_interpolator_2d
+  use sll_m_cubic_spline_interpolator_1d
+  use sll_m_cubic_spline_interpolator_2d
+!baoter
+  use sll_m_cartesian_meshes
+  use sll_m_scalar_field_1d
+  use sll_m_scalar_field_2d
+
+
+  implicit none
+
+  !===========================================================================
+  !> 
+  !---------------------------------------------------------------------------
+  type, public :: vlasov_DK4D_hybrid_t
+
+    !> For computation of the interpolation of the distribution function
+!VG!    type(sll_t_arbitrary_degree_spline_interpolator_2d) :: interp2d_f_eta1eta2
+!VG!    type(sll_arbitrary_degree_spline_interpolator_1d) :: interp1d_f_eta3
+!VG!    type(sll_arbitrary_degree_spline_interpolator_1d) :: interp1d_f_vpar
+    type(sll_t_cubic_spline_interpolator_2d) :: interp2d_f_eta1eta2
+    type(sll_t_cubic_spline_interpolator_1d) :: interp1d_f_eta3
+    type(sll_t_cubic_spline_interpolator_1d) :: interp1d_f_vpar
+
+  end type vlasov_DK4D_hybrid_t
+  !---------------------------------------------------------------------------
+
+contains
+
+  !===========================================================================
+  !> Vlasov: Allocation 
+  !---------------------------------------------------------------------------
+  subroutine new_vlasov_DK4D_hybrid( vlasov, &
+      mesh4d, &
+      bound_cond, &
+      spline_degree )
+
+    type(vlasov_DK4D_hybrid_t)    , intent(inout) :: vlasov
+    type(mesh_DK4D_t)             , intent(in)    :: mesh4d
+    type(boundary_conditions_4d_t), intent(in)    :: bound_cond
+    type(spline_degree_4d_t)      , intent(in)    :: spline_degree
+
+    !-> Local variables
+    sll_int32 :: Neta3, Nvpar
+    !--> For cartesian meshes
+    type(sll_t_cartesian_mesh_2d), pointer :: logical_mesh2d_eta1eta2
+    type(sll_t_cartesian_mesh_1d), pointer :: logical_mesh1d_eta3
+    type(sll_t_cartesian_mesh_1d), pointer :: logical_mesh1d_vpar
+    !--> For electic field boundary conditions and spline degree
+    type(boundary_conditions_3d_t) :: elec_field_bc
+    type(spline_degree_3d_t)       :: elec_field_spline_degree
+
+    Neta3 = size(mesh4d%eta3_grid)
+    Nvpar = size(mesh4d%vpar_grid)
+
+    logical_mesh2d_eta1eta2 => mesh4d%eta1_eta2_mesh2d
+    logical_mesh1d_eta3     => mesh4d%eta3_mesh1d
+    logical_mesh1d_vpar     => mesh4d%vpar_mesh1d
+
+    !*** Allocation for the interpolations of the distribution function ***
+    !--> Initialization of the interpolators 
+    !\todo : Find a solution of be able to use the arbitrary spline
+    call vlasov%interp2d_f_eta1eta2%init( &
+      logical_mesh2d_eta1eta2%num_cells1+1, &
+      logical_mesh2d_eta1eta2%num_cells2+1, &
+      logical_mesh2d_eta1eta2%eta1_min, &
+      logical_mesh2d_eta1eta2%eta1_max, &
+      logical_mesh2d_eta1eta2%eta2_min, &
+      logical_mesh2d_eta1eta2%eta2_max, &
+       sll_p_hermite, &
+       sll_p_periodic )
+
+    call vlasov%interp1d_f_eta3%init( &
+       Neta3, &
+       logical_mesh1d_eta3%eta_min, &
+       logical_mesh1d_eta3%eta_max, &
+       sll_p_periodic )
+
+    call vlasov%interp1d_f_vpar%init( &
+       Nvpar, &
+       logical_mesh1d_vpar%eta_min, &
+       logical_mesh1d_vpar%eta_max, &
+       sll_p_hermite )
+
+!VG!    call vlasov%interp2d_f_eta1eta2%initialize( &
+!VG!      logical_mesh2d_eta1eta2%num_cells1+1, &
+!VG!      logical_mesh2d_eta1eta2%num_cells2+1, &
+!VG!      logical_mesh2d_eta1eta2%eta1_min, &
+!VG!      logical_mesh2d_eta1eta2%eta1_max, &
+!VG!      logical_mesh2d_eta1eta2%eta2_min, &
+!VG!      logical_mesh2d_eta1eta2%eta2_max, &
+!VG!      bound_cond%left_eta1, &
+!VG!      bound_cond%right_eta1, &
+!VG!      bound_cond%left_eta2, &
+!VG!      bound_cond%right_eta2, &
+!VG!      spline_degree%eta1, &
+!VG!      spline_degree%eta2 )
+!VG!
+!VG!    call vlasov%interp1d_f_eta3%initialize( &
+!VG!       Neta3, &
+!VG!       logical_mesh1d_eta3%eta_min, &
+!VG!       logical_mesh1d_eta3%eta_max, &
+!VG!       bound_cond%left_eta3, &
+!VG!       bound_cond%right_eta3, &
+!VG!       spline_degree%eta3 )
+!VG!
+!VG!    call vlasov%interp1d_f_vpar%initialize( &
+!VG!       Nvpar, &
+!VG!       logical_mesh1d_vpar%eta_min, &
+!VG!       logical_mesh1d_vpar%eta_max, &
+!VG!       bound_cond%left_vpar, &
+!VG!       bound_cond%right_vpar, &
+!VG!       spline_degree%vpar )
+
+  end subroutine new_vlasov_DK4D_hybrid
+  !---------------------------------------------------------------------------
+
+
+  !===========================================================================
+  !> Vlasov: Deallocation 
+  !---------------------------------------------------------------------------
+  subroutine delete_vlasov_DK4D_hybrid( vlasov )
+
+    type(vlasov_DK4D_hybrid_t), intent(inout) :: vlasov
+
+    call vlasov%interp2d_f_eta1eta2%delete( )
+    call sll_o_delete( vlasov%interp1d_f_eta3 )
+    call sll_o_delete( vlasov%interp1d_f_vpar )
+
+  end subroutine delete_vlasov_DK4D_hybrid
+  !---------------------------------------------------------------------------
+
+
+  !===========================================================================
+  !> Vlasov: Global iteration of the drift-kinetic 4D simulation
+  !---------------------------------------------------------------------------
+  subroutine iteration_vlasov_DK4D_hybrid( &
+      vlasov, &
+      fdistribu, &
+      mesh4d, &
+      magnetconf, &
+      elecpot, &
+      dt, &
+      advec_vpar_step1_coef, &
+      advec_eta3_step1_coef, &
+      advec_eta1eta2_step1_coef, &
+      advec_vpar_step2_coef, &
+      advec_eta3_step2_coef )
+
+    type(vlasov_DK4D_hybrid_t) , intent(inout) :: vlasov
+    type(fdistribu_DK4D_t)     , intent(inout) :: fdistribu
+    type(mesh_DK4D_t)          , intent(in)    :: mesh4d
+    type(magnetconf_DK4D_t)    , intent(in)    :: magnetconf
+    type(elecpot_DK4D_hybrid_t), intent(in)    :: elecpot
+    sll_real64                 , intent(in)    :: dt
+    sll_real64                 , intent(in)    :: advec_vpar_step1_coef
+    sll_real64                 , intent(in)    :: advec_eta3_step1_coef
+    sll_real64                 , intent(in)    :: advec_eta1eta2_step1_coef
+    sll_real64       , optional, intent(in)    :: advec_vpar_step2_coef
+    sll_real64       , optional, intent(in)    :: advec_eta3_step2_coef
+
+    !--> local variables
+    sll_int32 :: my_rank
+
+    my_rank = sll_f_get_collective_rank( sll_v_world_collective )
+
+    !*** Advection in vpar direction ***
+    if ( advec_vpar_step1_coef.ne.0._F64 ) then
+      if ( my_rank == 0) print*,' ==> first vpar advection with dt = ', &
+          advec_vpar_step1_coef
+      call advec1D_vpar_hybrid( &
+          vlasov, &
+          fdistribu, &
+          mesh4d, &
+          elecpot, &
+          advec_vpar_step1_coef*dt )
+    end if
+
+    !*** Advection in eta3 direction ***
+    if ( advec_eta3_step1_coef.ne.0._F64 ) then
+      if ( my_rank == 0) print*,' ==> first eta3 advection with dt = ', &
+          advec_eta3_step1_coef
+      call advec1D_eta3_hybrid( &
+          vlasov, &
+          fdistribu, &
+          mesh4d, &
+          advec_eta3_step1_coef*dt )
+    end if
+
+    !*** Advection in (eta1,eta2) directions ***
+    call sll_o_apply_remap_4d( &
+        fdistribu%seqx3x4_to_seqx1x2, &
+        fdistribu%val4d_seqx3x4,&
+        fdistribu%val4d_seqx1x2 )
+    if ( advec_eta1eta2_step1_coef.ne.0._F64 ) then
+      if ( my_rank == 0) print*,' ==> (eta1,eta2) advection with dt = ', &
+          advec_eta1eta2_step1_coef
+      call advec2D_eta1eta2_hybrid( vlasov , &
+          fdistribu, &
+          mesh4d, &
+          magnetconf, &
+          elecpot, &
+          advec_eta1eta2_step1_coef*dt )
+      call sll_o_apply_remap_4d( &
+          fdistribu%seqx1x2_to_seqx3x4, &
+          fdistribu%val4d_seqx1x2, &
+          fdistribu%val4d_seqx3x4 )
+    end if
+
+    !*** Advection in eta3 direction ***
+    if ( present(advec_eta3_step2_coef) ) then
+      if ( advec_eta3_step2_coef.ne.0._F64 ) then
+        if ( my_rank == 0) print*,' ==> second eta3 advection with dt = ', &
+            advec_eta3_step2_coef
+        call advec1D_eta3_hybrid( &
+            vlasov, &
+            fdistribu, &
+            mesh4d, &
+            advec_eta3_step1_coef*dt )
+      end if
+    end if
+
+    !*** Advection in vpar direction ***
+    if ( present(advec_vpar_step2_coef) ) then
+      if ( advec_vpar_step2_coef.ne.0._F64 ) then
+        if ( my_rank == 0) print*,' ==> second vpar advection with dt = ', &
+            advec_vpar_step2_coef
+        call advec1D_vpar_hybrid( &
+            vlasov, &
+            fdistribu, &
+            mesh4d, &
+            elecpot, &
+            advec_vpar_step2_coef*dt)
+      end if
+    end if
+
+    !*** Fill fdistribu%val4d_seqx1x2 (to be able to solve QN)
+    if ( present(advec_eta3_step2_coef) .or. &
+        present(advec_vpar_step2_coef) ) then
+      if ( (advec_vpar_step2_coef+advec_eta3_step2_coef).ne.0._F64 ) then
+        call sll_o_apply_remap_4d( &
+            fdistribu%seqx3x4_to_seqx1x2, &
+            fdistribu%val4d_seqx3x4,&
+            fdistribu%val4d_seqx1x2 )
+      end if
+    end if
+
+  end subroutine iteration_vlasov_DK4D_hybrid
+  !---------------------------------------------------------------------------
+
+
+  !===========================================================================
+  ! Vlasov: 1D advection in vpar direction
+  !---------------------------------------------------------------------------
+  subroutine advec1D_vpar_hybrid( vlasov , &
+      fdistribu, &
+      mesh4d, &
+      elecpot, &
+      deltat_advec )
+
+    type(vlasov_DK4D_hybrid_t) , intent(inout) :: vlasov
+    type(fdistribu_DK4D_t)     , intent(inout) :: fdistribu
+    type(mesh_DK4D_t)          , intent(in)    :: mesh4d
+    type(elecpot_DK4D_hybrid_t), intent(in)    :: elecpot
+    sll_real64                 , intent(in)    :: deltat_advec
+
+    sll_int32  :: ierr
+    sll_int32  :: iloc1, iloc2
+    sll_int32  :: ieta3, ivpar
+    sll_int32  :: loc4d_sz_x1, loc4d_sz_x2
+    sll_int32  :: loc4d_sz_x3, loc4d_sz_x4
+    sll_int32  :: Neta3, Nvpar
+    sll_real64 :: vpar_point, vpar_min, vpar_max, E_eta3, alpha4
+    sll_real64, dimension(:), pointer :: f1d_vpar_tmp
+
+    SLL_ASSERT( size(fdistribu%val4d_seqx3x4,1).eq.size(vlasov%elec_field_eta1_3d%val3d_seqx3,1) )
+    SLL_ASSERT( size(fdistribu%val4d_seqx3x4,2).eq.size(vlasov%elec_field_eta1_3d%val3d_seqx3,2) )
+    SLL_ASSERT( size(fdistribu%val4d_seqx3x4,3).eq.size(vlasov%elec_field_eta1_3d%val3d_seqx3,3) )
+
+    Neta3    = size( mesh4d%eta3_grid )
+    Nvpar    = size( mesh4d%vpar_grid )
+    vpar_min = minval( mesh4d%vpar_grid )
+    vpar_max = maxval( mesh4d%vpar_grid )
+
+    SLL_ALLOCATE( f1d_vpar_tmp(Nvpar), ierr )
+
+    call sll_o_compute_local_sizes( fdistribu%layout4d_seqx3x4, &
+      loc4d_sz_x1, &
+      loc4d_sz_x2, &
+      loc4d_sz_x3, &
+      loc4d_sz_x4 )    
+
+    !---> dvpar/dt = E_eta3 with E_eta3 = -dPhi/deta3
+    do iloc2 = 1,loc4d_sz_x2
+      do iloc1 = 1,loc4d_sz_x1
+        do ieta3 = 1,Neta3
+          E_eta3 = - elecpot%dPhi_deta3%val3d_seqx3(iloc1,iloc2,ieta3)
+          do ivpar = 1,Nvpar
+            f1d_vpar_tmp(ivpar) = &
+                fdistribu%val4d_seqx3x4(iloc1,iloc2,ieta3,ivpar)
+          end do
+          call vlasov%interp1d_f_vpar%compute_interpolants(f1d_vpar_tmp)   
+          do ivpar = 1,Nvpar
+            ! si change of coordinates in 3D
+            ! val_jac  = sim%transf_xy%jacobian(eta1,eta2,eta3) 
+            alpha4     = deltat_advec*E_eta3 
+            vpar_point = mesh4d%vpar_grid(ivpar) - alpha4
+            vpar_point = max(min(vpar_point,vpar_max),vpar_min)
+
+            fdistribu%val4d_seqx3x4(iloc1,iloc2,ieta3,ivpar) = &
+              vlasov%interp1d_f_vpar%interpolate_from_interpolant_value(vpar_point)
+          end do
+        end do
+      end do
+    end do
+
+    SLL_DEALLOCATE(f1d_vpar_tmp,ierr)
+
+  end subroutine advec1D_vpar_hybrid
+  !---------------------------------------------------------------------------
+
+
+  !===========================================================================
+  ! Vlasov: 1D advection in eta3 direction
+  !---------------------------------------------------------------------------
+  subroutine advec1D_eta3_hybrid( vlasov , &
+      fdistribu, &
+      mesh4d, &
+      deltat_advec )
+
+    type(vlasov_DK4D_hybrid_t), intent(inout) :: vlasov
+    type(fdistribu_DK4D_t)    , intent(inout) :: fdistribu
+    type(mesh_DK4D_t)         , intent(in)    :: mesh4d
+    sll_real64                , intent(in)    :: deltat_advec
+
+    sll_int32  :: ierr
+    sll_int32  :: iloc1, iloc2
+    sll_int32  :: ieta3, ivpar
+    sll_int32  :: loc4d_sz_x1, loc4d_sz_x2
+    sll_int32  :: loc4d_sz_x3, loc4d_sz_x4
+    sll_int32  :: Neta3, Nvpar
+    sll_real64 :: eta3_point, alpha3, vpar_point
+    sll_real64, dimension(:), pointer :: f1d_eta3_tmp
+
+    Neta3 = size(mesh4d%eta3_grid)
+    Nvpar = size(mesh4d%vpar_grid)
+
+    call sll_o_compute_local_sizes( fdistribu%layout4d_seqx3x4, &
+        loc4d_sz_x1, &
+        loc4d_sz_x2, &
+        loc4d_sz_x3, &
+        loc4d_sz_x4 )    
+
+    !---> dphi/dt = vpar
+    SLL_ALLOCATE(f1d_eta3_tmp(Neta3),ierr)
+    do iloc2 = 1,loc4d_sz_x2
+      do iloc1 = 1,loc4d_sz_x1
+        do ivpar = 1,Nvpar
+          vpar_point = mesh4d%vpar_grid(ivpar)
+          do ieta3 = 1,Neta3
+            f1d_eta3_tmp(ieta3) = fdistribu%val4d_seqx3x4(iloc1,iloc2,ieta3,ivpar)
+          end do
+          call vlasov%interp1d_f_eta3%compute_interpolants(f1d_eta3_tmp)  
+
+          do ieta3 = 1,Neta3
+            alpha3     = deltat_advec*vpar_point
+            eta3_point = mesh4d%eta3_grid(ieta3) - alpha3
+
+            fdistribu%val4d_seqx3x4(iloc1,iloc2,ieta3,ivpar) = &
+                vlasov%interp1d_f_eta3%interpolate_from_interpolant_value(eta3_point)
+          end do
+        end do
+      end do
+    end do
+
+    SLL_DEALLOCATE(f1d_eta3_tmp,ierr)
+
+  end subroutine advec1D_eta3_hybrid
+  !---------------------------------------------------------------------------
+
+
+  !===========================================================================
+  ! Vlasov: 2D advection in (eta1,eta2) directions
+  !---------------------------------------------------------------------------
+  subroutine advec2D_eta1eta2_hybrid( &
+      vlasov, &
+      fdistribu, &
+      mesh4d, &
+      magnetconf, &
+      elecpot, &
+      deltat_advec )
+
+    type(vlasov_DK4D_hybrid_t) , intent(inout) :: vlasov
+    type(fdistribu_DK4D_t)     , intent(inout) :: fdistribu
+    type(mesh_DK4D_t)          , intent(in)    :: mesh4d
+    type(magnetconf_DK4D_t)    , intent(in)    :: magnetconf
+    type(elecpot_DK4D_hybrid_t), intent(in)    :: elecpot
+    sll_real64                 , intent(in)    :: deltat_advec
+
+    sll_int32  :: ierr
+    sll_int32  :: iloc3, iloc4
+    sll_int32  :: ieta1, ieta2
+    sll_int32  :: Neta1, Neta2
+    sll_int32  :: loc4d_sz_x1, loc4d_sz_x2
+    sll_int32  :: loc4d_sz_x3, loc4d_sz_x4
+    sll_real64 :: eta1_point, eta2_point 
+    sll_real64 :: alpha1, alpha2, E_eta1, E_eta2
+    sll_real64 :: val_jac,val_B
+    sll_real64, dimension(:,:), pointer :: f2d_eta1eta2_tmp
+
+    Neta1 = size( mesh4d%eta1_grid )
+    Neta2 = size( mesh4d%eta2_grid )
+
+    call sll_o_compute_local_sizes( fdistribu%layout4d_seqx1x2, &
+        loc4d_sz_x1, &
+        loc4d_sz_x2, &
+        loc4d_sz_x3, &
+        loc4d_sz_x4 )    
+
+    !---> ( deta1/dt )                              ( 0 -1 ) (  d phi/deta1 ) 
+    !     (          )=  1 / (B* jac(eta_1,eta2) )  (      ) (              )
+    !---> ( deta2/dt )                              ( 1  0 ) (  d phi/deta2 )
+    SLL_ALLOCATE( f2d_eta1eta2_tmp(Neta1,Neta2), ierr )
+
+    do iloc4 = 1,loc4d_sz_x4
+      do iloc3 = 1,loc4d_sz_x3
+        do ieta2 = 1,Neta2
+          do ieta1 = 1,Neta1
+            f2d_eta1eta2_tmp(ieta1,ieta2) = &
+                fdistribu%val4d_seqx1x2(ieta1,ieta2,iloc3,iloc4)            
+          end do
+        end do
+
+        call vlasov%interp2d_f_eta1eta2%compute_interpolants( f2d_eta1eta2_tmp )
+
+        do ieta2 = 1,Neta2
+          do ieta1 = 1,Neta1
+            E_eta1  = - elecpot%dPhi_deta1%val3d_seqx1x2(ieta1,ieta2,iloc3)
+            E_eta2  = - elecpot%dPhi_deta2%val3d_seqx1x2(ieta1,ieta2,iloc3)
+            val_jac = mesh4d%transf_eta1eta2_xy%jacobian(eta1_point,eta2_point)
+            val_B   = magnetconf%B_xy(ieta1,ieta2)
+            alpha1  =  deltat_advec*E_eta2 / (val_jac*val_B)
+            alpha2  =  -deltat_advec*E_eta1 / (val_jac*val_B)
+            eta1_point = mesh4d%eta1_grid(ieta1) - alpha1
+            eta2_point = mesh4d%eta2_grid(ieta2) - alpha2
+
+            if (eta1_point <= mesh4d%eta1_eta2_mesh2d%eta1_min) then 
+              eta1_point = mesh4d%eta1_eta2_mesh2d%eta1_min
+            else if ( eta1_point>= mesh4d%eta1_eta2_mesh2d%eta1_max) then 
+              eta1_point = mesh4d%eta1_eta2_mesh2d%eta1_max
+            end if
+
+            fdistribu%val4d_seqx1x2(ieta1,ieta2,iloc3,iloc4) = &
+                vlasov%interp2d_f_eta1eta2%interpolate_from_interpolant_value( eta1_point,eta2_point )
+
+          end do
+        end do
+      end do
+    end do
+
+    SLL_DEALLOCATE(f2d_eta1eta2_tmp,ierr)
+
+  end subroutine advec2D_eta1eta2_hybrid
+  !---------------------------------------------------------------------------
+
+end module vlasov_DK4D_hybrid_module
+!---------------------------------------------------------------------------
---
+++ thirdparty/VP4D_hybrid/Python_scripts/VP4Dhyb_resu_class.py
@@ -0,0 +1,285 @@
+"""
+#  file : VP4Dresu_class.py
+#  date : 2014-01-24
+
+   Reading of all the result files
+   and saving of the time evolution 
+   of all VP4D outputs
+"""
+
+import glob
+import os
+import numpy as np
+import string 
+
+import VP4Dhyb_GYSutils
+GYSut   = VP4Dhyb_GYSutils.impall('VP4Dhyb_GYSutils')
+
+
+#---------------------------------------
+# GYSspecies class definition
+#---------------------------------------
+class VP4Dresu():    
+
+    #---------------------------------------
+    # VP4Dresu initialisation
+    #---------------------------------------
+    def __init__(self,VP4Ddir=''):
+
+        #--> Initialization of the GYSELA result directory
+        if (VP4Ddir==''):
+            self.Directory = Ask_directory()
+            if (status==-1):
+                print '\033[0;31m ==> So, stop the script !!!!! \033[1;m '
+                return
+        else:
+            self.Directory =  VP4Ddir
+        print " "
+        print " ********************************************************************"
+        print " " + str(self.Directory)
+        print " ********************************************************************"
+        self.Prefix = self.Directory+'/'
+        self.ibeg   = -1
+        self.iend   = -1
+
+    #end def ___init__
+
+
+    #--------------------------------------------------
+    # Reading of the file 'mesh.h5'
+    #--------------------------------------------------
+    def read_mesh(self):
+
+        """ Reading of the file 'mesh.h5'"""
+
+        #--> Load the file
+        mesh_filename = self.Prefix+'/mesh.h5'
+        print ' ---> Reading of the file : ' + mesh_filename
+        H = GYSut.loadHDF5(mesh_filename)
+        for iname in H.keys:
+            exec "self.%s = H.%s" % (iname,iname)
+        #end for
+
+    #end def read_mesh
+
+
+    #--------------------------------------------------
+    # Reading of the file 'equilibrium.h5'
+    #--------------------------------------------------
+    def read_equilibrium(self):
+
+        """ Reading of the file 'equilibrium.h5'"""
+
+        #--> Load the file
+        equilibrium_filename = self.Prefix+'/equilibrium.h5'
+        print ' ---> Reading of the file : ' + equilibrium_filename 
+        H = GYSut.loadHDF5(equilibrium_filename)
+        for iname in H.keys:
+            exec "self.%s = H.%s" % (iname,iname)
+        #end for
+
+    #end def read_equilibrium
+
+
+    #--------------------------------------------------
+    # Reading of the files 'diags_d<num_diag>.h5'
+    #--------------------------------------------------
+    def read_diags_files(self):
+
+        """ Reading of the files 'diags_d<num_diag>.h5'"""
+
+        #--> Find all the result files
+        [fname_list_diags,nb_files,ibeg,iend] = \
+            self.find_existing_results('diags_d*.h5')
+        self.fname_list_diags = fname_list_diags
+        self.nb_files = nb_files
+        self.ibeg     = ibeg
+        self.iend     = iend
+        self.read_filename_list(fname_list_diags,nb_files,ibeg,iend)
+
+    #end def read_diags_files
+
+
+    #--------------------------------------------------
+    # Reading of the files 'fdistribu_d<num_diag>.h5'
+    #--------------------------------------------------
+    def read_fdistribu_files(self):
+
+        """ Reading of the files 'fdistribu_d<num_diag>.h5'"""
+
+        #--> Find all the result files
+        [fname_list_fdistribu,nb_files,ibeg,iend] = \
+            self.find_existing_results('fdistribu_d*.h5', 
+                                       ifirst_resufile=self.ibeg, 
+                                       ilast_resufile=self.iend)
+
+        self.fname_list_fdistribu = fname_list_fdistribu
+        self.nb_files = nb_files
+        self.ibeg     = ibeg
+        self.iend     = iend
+        self.read_filename_list(fname_list_fdistribu,nb_files,ibeg,iend)
+
+    #end def read_fdistribu_files
+
+
+    #--------------------------------------------------
+    # Reading of the files 'Phi_d<num_diag>.h5'
+    #--------------------------------------------------
+    def read_Phi_files(self):
+
+        """ Reading of the files 'Phi_d<num_diag>.h5'"""
+
+        #--> Find all the result files
+        [fname_list_Phi,nb_files,ibeg,iend] = \
+            self.find_existing_results('Phi_d*.h5', 
+                                       ifirst_resufile=self.ibeg, 
+                                       ilast_resufile=self.iend)
+
+        self.fname_list_Phi = fname_list_Phi
+        self.nb_files = nb_files
+        self.ibeg     = ibeg
+        self.iend     = iend
+        self.read_filename_list(fname_list_Phi,nb_files,ibeg,iend)
+
+    #end def read_Phi_files
+
+
+    #---------------------------------------
+    # Result reading
+    #---------------------------------------
+    def find_existing_results(self, 
+                              file_name_list,
+                              ifirst_resufile=-1, 
+                              ilast_resufile=-1):
+
+        """ Result reading"""
+
+        #--> Read all the existing results
+        #VG#l_resufiles = glob.glob(self.Directory+'/diags*.h5')
+        l_resufiles = glob.glob(self.Directory+'/'+file_name_list)
+        l_resufiles = sorted(l_resufiles)
+
+        size_extent   = 3  # for '.h5'
+        size_num      = 5  # size of the integer for diagnostic 
+        l_diagnum     = map(lambda x : x[-(size_extent+size_num):-size_extent],l_resufiles)
+        i_diagnum_min = int(min(l_diagnum)) 
+        i_diagnum_max = int(max(l_diagnum)) 
+        nb_diag       = i_diagnum_max-i_diagnum_min+1
+
+        if ((ifirst_resufile==-1) or (ilast_resufile==-1)):
+            [indx_first_ask,indx_last_ask] = \
+                Ask_resultFiles(l_diagnum)
+            ibeg  = indx_first_ask
+            iend  = indx_last_ask
+        else:
+            ibeg = ifirst_resufile
+            iend = ilast_resufile 
+        #end if
+        nb_files = iend-ibeg+1
+
+        return  [l_resufiles,nb_files,ibeg,iend]
+
+    #end def find_existing_results
+
+
+    #--------------------------------------------------
+    # Create and fill all the arrays for the time 
+    #  evolution of each quantities contained in the
+    #  list of file names
+    #--------------------------------------------------
+    def read_filename_list(self,fname_list,nfile,ibeg,iend):
+
+        """ """
+
+        exclude_vars = ['xgrid_2d','ygrid_2d']
+
+        #--> Load the first file
+        H = GYSut.loadHDF5(fname_list[ibeg])
+        for iname in H.keys:
+            if (iname not in exclude_vars):
+                tupple_tmp = (nfile,)
+                exec "sh = np.shape(H.%s)" % (iname)
+                tupple_tmp = tupple_tmp + sh
+                exec "self.%s = np.zeros(tupple_tmp)" % (iname)
+            #end if
+        #end for
+        
+        #--> Read all files
+        ifile = 0
+        for filename in fname_list[ibeg:iend+1]:
+            filename_str = string.replace(filename,self.Prefix,'')
+            print ' ---> Reading of the file : ' + self.Prefix + filename_str
+            H = GYSut.loadHDF5(filename)
+            for iname in H.keys:
+                if (iname not in exclude_vars):
+                    exec "self.%s[ifile,] = H.%s" % (iname,iname)
+                #end if
+            #end for
+            ifile = ifile + 1
+        #end for
+        
+    #end def read_filename_list
+
+#end class VP4Dresu
+
+
+#---------------------------------------------------------
+# Ask which directory contains VP4D results to be 
+#  analysed
+#---------------------------------------------------------
+def Ask_directory():
+
+    """ Ask which directory contains VP4D results
+    to be analysed"""
+
+    directory = ''
+    str_ask   = ' Directory for files reading'+ \
+                '  [default = current] ? '
+    directory = raw_input(str_ask);
+    if (directory==''):
+        directory = str(os.getcwd())
+    nb_VP4D = np.size(glob.glob(directory+'/diags_*.h5'))
+    if ( (nb_VP4D==0) ):
+        print '\033[0;31m',\
+            ' ==> There are NO VP4D results HDF5 files in :',\
+            '\033[1;m',str(directory)
+        status = -1
+        return status
+    return directory
+
+#end def Ask_directory
+
+
+#---------------------------------------------------------------------
+# Ask which files will be treated in the GYSELA diagnostics
+#  and create the list of file names required
+#---------------------------------------------------------------------
+def Ask_resultFiles(diagnum_list):
+
+    """ Ask which files will be treated in the GYSELA diagnostics
+    and create the list of file names required"""
+
+    str_first_file = str(diagnum_list[0])
+    str_last_file  = str(diagnum_list[-1])
+
+    print '';
+    print ' *** VP4D hybrid diagnostics : choice of files *** '
+    str_first_ask = raw_input(' First file ? (between ' + \
+                str_first_file + ' (default) and ' + \
+                str_last_file + ' )')
+    if (str_first_ask ==''): str_first_ask  = str_first_file
+    str_last_ask  = raw_input(' Last file ?  (between ' + \
+                 str_first_ask + ' and ' + str_last_file + \
+                 ' (default) )')
+    if (str_last_ask ==''): str_last_ask = str_last_file
+
+    l_tmp          = filter(lambda x: int(x) >= int(str_first_ask), diagnum_list)
+    indx_first_ask = diagnum_list.index(l_tmp[0])
+    l_tmp          = filter(lambda x: int(x) <= int(str_last_ask), diagnum_list)
+    indx_last_ask  = diagnum_list.index(l_tmp[-1])
+    return [indx_first_ask,indx_last_ask]
+
+#end def Ask_resultFiles
+
+
+
