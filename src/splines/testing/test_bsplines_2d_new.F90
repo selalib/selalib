64bb27f5cd0de3362530cfabd7478bcbbc31cbe6
Yaman Güçlü <yaman.guclu@gmail.com>
2017-07-03 19:55:16 +0200

    [bsplines] Add test: spline convergence to cos-cos profile.

--- src/splines/testing/test_bsplines_2d_new.F90
+++ src/splines/testing/test_bsplines_2d_new.F90
@@ -4,6 +4,9 @@ program test_bsplines_2d_new
   use sll_m_working_precision, only: &
     f64
 
+  use sll_m_constants, only: &
+    sll_p_twopi
+
   use m_analytical_profiles_2d, only: &
     t_profile_2d_info, &
     t_analytical_profile_2d_cos_cos, &
@@ -29,6 +32,7 @@ program test_bsplines_2d_new
   type(t_analytical_profile_2d_cos_cos) :: profile_2d_cos_cos
   type(t_analytical_profile_2d_poly)    :: profile_2d_poly
   integer, allocatable                  :: bc_kinds(:)
+  integer, allocatable                  :: nx_list(:)
 
   integer  :: deg1
   integer  :: deg2
@@ -41,6 +45,9 @@ program test_bsplines_2d_new
   real(wp) :: tol
   logical  :: passed
   logical  :: success
+  real(wp) :: dx1
+  real(wp) :: dx2
+  integer  :: j
 
   ! ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
   ! TEST 1: Evaluate spline at interpolation points (error should be zero)
@@ -53,7 +60,7 @@ program test_bsplines_2d_new
 
   ! Choose boundary conditions to be tested
   allocate( bc_kinds(3) )
-  bc_kinds(1:3) = [sll_p_periodic, sll_p_hermite, sll_p_greville]
+  bc_kinds(:) = [sll_p_periodic, sll_p_hermite, sll_p_greville]
 
   ! Initialize 'PASSED/FAILED' condition
   passed = .true.
@@ -128,7 +135,7 @@ program test_bsplines_2d_new
 
   ! Choose boundary conditions to be tested
   allocate( bc_kinds(2) )
-  bc_kinds(1:2) = [sll_p_hermite, sll_p_greville]
+  bc_kinds(:) = [sll_p_hermite, sll_p_greville]
 
   ! Choose number of knots in tensor grid
   nx1 = 23
@@ -149,6 +156,7 @@ program test_bsplines_2d_new
         do i2 = 1, size( bc_kinds )
           bc2 = bc_kinds(i2)
 
+          ! Initialize test facility
           call test_facility % init( &
             profile_2d = profile_2d_poly, &
             nx1        = nx1 , &
@@ -158,11 +166,14 @@ program test_bsplines_2d_new
             bc1        = bc1 , &
             bc2        = bc2 )
 
+          ! Run tests
           call test_facility % evaluate_func_and_grad_on_uniform_grid( &
             grid_dim, tol, success )
 
+          ! Free memory
           call test_facility % free()
 
+          ! Update 'PASSED/FAILED' condition
           passed = (passed .and. success)
 
         end do  ! bc2
@@ -172,6 +183,91 @@ program test_bsplines_2d_new
 
   ! Deallocate local arrays
   deallocate( bc_kinds )
+
+  ! ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+  ! TEST 3: convergence analysis on cos*cos profile (with error bound)
+  ! ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+  ! TODO: print test explanation to terminal
+  write(*,*)
+  write(*,*)
+
+  ! Choose boundary conditions to be tested
+  allocate( bc_kinds(3) )
+  bc_kinds(:) = [sll_p_periodic, sll_p_hermite, sll_p_greville]
+
+  allocate( nx_list(4) )
+  nx_list(:) = [13, 26, 53, 78]
+
+  ! Print report header
+  call test_facility % print_header()
+
+  ! Initialize profile
+  call profile_2d_cos_cos % init()
+
+  ! Extract information about 2D analytical profile
+  call profile_2d_cos_cos % get_info( pinfo )
+
+  ! Choose dimension of uniform grid of evaluation points
+  grid_dim = [20,20]
+
+  do deg1 = 2, 9
+
+    ! Use same spline degree along both directions
+    deg2 = deg1
+
+    do i1 = 1, size( bc_kinds )
+      bc1 = bc_kinds(i1)
+
+      do i2 = 1, size( bc_kinds )
+        bc2 = bc_kinds(i2)
+
+        do j = 1, size( nx_list )
+          nx1 = nx_list(j)
+
+          ! Use same number of knots along both directions
+          nx2 = nx1
+
+          ! Initialize test facility
+          call test_facility % init( &
+            profile_2d = profile_2d_cos_cos, &
+            nx1        = nx1 , &
+            nx2        = nx2 , &
+            deg1       = deg1, &
+            deg2       = deg2, &
+            bc1        = bc1 , &
+            bc2        = bc2 )
+
+          ! Determine error tolerance
+          dx1 = (pinfo%x1_max - pinfo%x1_min) / nx1
+          dx2 = (pinfo%x2_max - pinfo%x2_min) / nx2
+          tol = error_bound_cos_cos( dx1, dx2, deg1, deg2 )
+          tol = max( 1e-14_wp, tol )
+
+          ! Run tests
+          ! TODO: print tolerance
+          ! TODO: evaluate all grid points at once
+          ! TODO: print numerical order of accuracy
+          call test_facility % evaluate_func_and_grad_on_uniform_grid( &
+            grid_dim, tol, success )
+
+          ! Free memory
+          call test_facility % free()
+
+          ! Update 'PASSED/FAILED' condition
+          passed = (passed .and. success)
+
+        end do
+        write(*,*)
+
+      end do
+    end do
+  end do
+
+  ! Deallocate local arrays
+  deallocate( bc_kinds )
+  deallocate( nx_list  )
+
   ! ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
   ! CTest key
@@ -180,5 +276,57 @@ program test_bsplines_2d_new
   else
     write(*,*) "FAILED"
   end if
+
+!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+contains
+!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+  pure function factorial( n ) result( f )
+    integer, intent(in) :: n
+    real(wp) :: f
+
+    f = gamma( real( n+1, wp ) )
+
+  end function factorial
+
+  !-----------------------------------------------------------------------------
+  pure function taylor_error_term_cos_cos( k1, k2 ) result( e )
+    integer, intent(in) :: k1
+    integer, intent(in) :: k2
+    real(wp) :: e
+
+    e = sll_p_twopi**(k1+k2) * dx1**k1 * dx2**k2 &
+                / (factorial( k1 ) * factorial( k2 ))
+
+  end function taylor_error_term_cos_cos
+
+  !-----------------------------------------------------------------------------
+  pure function error_bound_cos_cos( dx1, dx2, deg1, deg2 ) result( max_error )
+    real(wp), intent(in   ) :: dx1
+    real(wp), intent(in   ) :: dx2
+    integer , intent(in   ) :: deg1
+    integer , intent(in   ) :: deg2
+    real(wp) :: max_error
+
+!    integer :: k1, k2
+!
+!    max_error = 0.0_wp
+!
+!    k2 = deg2+1
+!    do k1 = 0, deg1
+!      max_error = max_error + taylor_error_term_cos_cos( k1, k2 )
+!    end do
+!
+!    k1 = deg1+1
+!    do k2 = 0, deg2
+!      max_error = max_error + taylor_error_term_cos_cos( k1, k2 )
+!    end do
+!
+!    max_error = max_error + taylor_error_term_cos_cos( deg1+1, deg2+1 )
+
+    max_error = taylor_error_term_cos_cos( deg1+1, 0 ) &
+              + taylor_error_term_cos_cos( 0, deg2+1 )
+
+  end function error_bound_cos_cos
 
 end program test_bsplines_2d_new
