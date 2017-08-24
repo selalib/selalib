e9255d049a92452f00cd3061bc92413836e7d551
Yaman Güçlü <yaman.guclu@gmail.com>
2017-08-24 12:23:45 +0200

    [bspline] 2D non-uniform spline: handle all combinations of periodic/Hermite/Greville BCs at xmin/xmax along x1 and x2; update tests.

--- src/splines/sll_m_spline_2d_non_uniform.F90
+++ src/splines/sll_m_spline_2d_non_uniform.F90
@@ -10,6 +10,7 @@ module sll_m_spline_2d_non_uniform
 
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #include "sll_assert.h"
+#include "sll_errors.h"
 #include "sll_memory.h"
 #include "sll_working_precision.h"
 
@@ -35,6 +36,7 @@ use sll_m_spline_1d_non_uniform, only: &
 implicit none
 
 public :: &
+  sll_t_spline_2d_boundary_data,                   &
   sll_t_spline_2d_non_uniform,                     &
   sll_s_spline_2d_non_uniform_init,                &
   sll_s_spline_2d_non_uniform_free,                &
@@ -55,12 +57,44 @@ private
 !> treated as an opaque type. No access to its internals is directly allowed.
 type :: sll_t_spline_2d_non_uniform
 
+  integer ::     deg(2)
+  integer ::       n(2)
+  integer :: bc_xmin(2)
+  integer :: bc_xmax(2)
+
   type(sll_t_spline_1d_non_uniform) :: bs1
   type(sll_t_spline_1d_non_uniform) :: bs2
   sll_real64, allocatable           :: bcoef(:,:)
-  sll_real64, allocatable           :: bwork(:,:)
+
+  sll_real64, allocatable, private  :: bwork(:,:)
+  integer                , private  :: nbc_xmin(2)
+  integer                , private  :: nbc_xmax(2)
 
 end type sll_t_spline_2d_non_uniform
+
+
+!> Container for boundary condition data
+!>
+!>  x2_max  ____________
+!>         |            |
+!>         | c        d |
+!>         |            |
+!>         |            |
+!>         |            |
+!>         | a        b |
+!>  x2_min |____________|
+!>       x1_min       x1_max
+!>
+type :: sll_t_spline_2d_boundary_data
+  sll_real64, allocatable :: derivs_x1_min (:,:)
+  sll_real64, allocatable :: derivs_x1_max (:,:)
+  sll_real64, allocatable :: derivs_x2_min (:,:)
+  sll_real64, allocatable :: derivs_x2_max (:,:)
+  sll_real64, allocatable :: mixed_derivs_a(:,:)
+  sll_real64, allocatable :: mixed_derivs_b(:,:)
+  sll_real64, allocatable :: mixed_derivs_c(:,:)
+  sll_real64, allocatable :: mixed_derivs_d(:,:)
+end type sll_t_spline_2d_boundary_data
 
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 contains
@@ -84,29 +118,32 @@ contains
     sll_int32                        , intent(in   ) :: bc_xmin(2)
     sll_int32                        , intent(in   ) :: bc_xmax(2)
 
-    sll_int32  :: n1, g1
-    sll_int32  :: n2, g2
-
-    ! NOTE: in the future different boundary conditions at xmin and xmax
-    !       should be considered. For now we only check that bc_xmin==bc_xmax
-    SLL_ASSERT( bc_xmin(1) == bc_xmax(1) )
-    SLL_ASSERT( bc_xmin(2) == bc_xmax(2) )
-
     call self%bs1%init( degree(1), breaks1, bc_xmin(1), bc_xmax(1) )
     call self%bs2%init( degree(2), breaks2, bc_xmin(2), bc_xmax(2) )
 
-    n1 = self%bs1%n
-    n2 = self%bs2%n
-    allocate( self%bwork(1:n2,1:n1) )
-    self%bwork = 0.0_f64
+    associate( n1 => self%bs1%n, &
+               n2 => self%bs2%n, &
+               g1 => merge( 1+degree(1)/2, 0, bc_xmin(1) == sll_p_periodic ), &
+               g2 => merge( 1+degree(2)/2, 0, bc_xmin(2) == sll_p_periodic ) )
 
-    ! Allocate array of spline coefficients
-    ! in case of periodic BCs, a larger array of coefficients is used in order
-    ! to avoid a loop with calls to the "mod( , )" function at evaluation.
-    g1 = merge( 1+self%bs1%deg/2, 0, self%bs1%bc_xmin == sll_p_periodic )
-    g2 = merge( 1+self%bs2%deg/2, 0, self%bs2%bc_xmin == sll_p_periodic )
-    allocate( self%bcoef(1-g1:n1+g1,1-g2:n2+g2) )
-    self%bcoef = 0.0_f64
+      ! Save data into type
+      self%deg     = degree
+      self%n       = [n1,n2]
+      self%bc_xmin = bc_xmin
+      self%bc_xmax = bc_xmax
+
+      ! Allocate array of spline coefficients, and work array for interpolation.
+      ! in case of periodic BCs, a larger array of coefficients is used in order
+      ! to avoid a loop with calls to the "mod( , )" function at evaluation.
+      allocate( self%bcoef(1-g1:n1+g1,1-g2:n2+g2) );  self%bcoef = 0.0_f64
+      allocate( self%bwork(1-g2:n2+g2,1-g1:n1+g1) );  self%bwork = 0.0_f64
+
+      ! Calculate number of additional boundary data on each side of domain
+      ! (i.e. derivatives for Hermite BC)
+      self%nbc_xmin = merge( degree/2, 0, bc_xmin == sll_p_hermite )
+      self%nbc_xmax = merge( degree/2, 0, bc_xmax == sll_p_hermite )
+
+    end associate
 
   end subroutine sll_s_spline_2d_non_uniform_init
 
@@ -114,164 +151,161 @@ contains
   subroutine sll_s_spline_2d_non_uniform_compute_interpolant( &
     self, &
     gtau, &
-    derivs_x1_min, &
-    derivs_x1_max, &
-    derivs_x2_min, &
-    derivs_x2_max, &
-    derivs_corners )
+    boundary_data )
 
-    type(sll_t_spline_2d_non_uniform), intent(inout)           :: self
-    sll_real64                       , intent(in   )           :: gtau(:,:)
-    sll_real64                       , intent(in   ), optional :: derivs_x1_min (:,:)
-    sll_real64                       , intent(in   ), optional :: derivs_x1_max (:,:)
-    sll_real64                       , intent(in   ), optional :: derivs_x2_min (:,:)
-    sll_real64                       , intent(in   ), optional :: derivs_x2_max (:,:)
-    sll_real64                       , intent(in   ), optional :: derivs_corners(:,:,:)
+    type(sll_t_spline_2d_non_uniform)  , intent(inout)           :: self
+    sll_real64                         , intent(in   )           :: gtau(:,:)
+    type(sll_t_spline_2d_boundary_data), intent(in   ), optional :: boundary_data
 
-    sll_int32 :: i1, n1, ncond1
-    sll_int32 :: i2, n2, ncond2, j2
-    integer   :: bc_type1, bc_type2
+    character(len=*), parameter :: &
+      this_sub_name = "sll_s_spline_2d_non_uniform_compute_interpolant"
 
-    ! TODO: check size of gtau == tau
-    ! TODO: check size of all input arrays
+    sll_int32 :: i1, i2
 
-    ! Number of degrees of freedom
-    n1 = self%bs1%n
-    n2 = self%bs2%n
-
-    ! Type of boundary conditions (TODO: deal with mixed BCs)
-    bc_type1 = self%bs1%bc_xmin
-    bc_type2 = self%bs2%bc_xmin
-
-    ! Compute number of additional boundary values needed
-    ncond1 = 0
-    ncond2 = 0
-    if (bc_type1 == sll_p_hermite)  ncond1 = self%bs1%deg/2
-    if (bc_type2 == sll_p_hermite)  ncond2 = self%bs2%deg/2
-
-    !--------------------------------------------
-    ! Compute spline coefficients in x1 direction
-    !--------------------------------------------
-
-    ! Boundary points in x2 (only for Hermite):
-    ! Additional x1-interpolation of x2-derivatives on boundaries is needed!
-    if (bc_type2 == sll_p_hermite) then
-
-      ! Hermite-Hermite case:
-      if (bc_type1 == sll_p_hermite) then
-
-        ! boundary conditions at x2_min
-        if (present(derivs_x2_min)) then
-          do j2 = 1, ncond2
-            if( present(derivs_corners)) then
-              call self%bs1%compute_interpolant( &
-                derivs_x2_min (j2,:)  , &
-                derivs_corners(:,j2,1), &
-                derivs_corners(:,j2,2))
-            else
-              call self%bs1%compute_interpolant( derivs_x2_min(j2,:) )
-            end if
-            self%bwork(j2,:) = self%bs1%bcoef(1:n1)
-          end do
-        else  ! set needed boundary values to 0
-          self%bwork(1:ncond2,:) = 0.0_f64
-        end if
-
-        ! boundary conditions at x2_max
-        if (present(derivs_x2_max)) then
-          do j2 = 1, ncond2
-            if (present(derivs_corners)) then
-              call self%bs1%compute_interpolant( &
-                derivs_x2_max (j2,:)  , &
-                derivs_corners(:,j2,3), &
-                derivs_corners(:,j2,4))
-            else
-              call self%bs1%compute_interpolant( derivs_x2_max(j2,:) )
-            end if
-            self%bwork(n2-ncond2+j2,:) = self%bs1%bcoef(1:n1)
-          end do
-        else ! set needed boundary values to 0
-          self%bwork(n2-ncond2+1:n2,:) = 0.0_f64
-        end if
-
-        ! Other-Hermite case:
-      else
-
-        ! boundary conditions at x2_min
-        if (present(derivs_x2_min)) then
-          do j2 = 1, ncond2
-            call self%bs1%compute_interpolant( derivs_x2_min(j2,:) )
-            self%bwork(j2,:) = self%bs1%bcoef(1:n1)
-          end do
-        else  ! set needed boundary values to 0
-          self%bwork(1:ncond2,:) = 0.0_f64
-        end if
-
-        ! boundary conditions at x2_max
-        if (present(derivs_x2_max)) then
-          do j2 = 1, ncond2
-            call self%bs1%compute_interpolant( derivs_x2_max(j2,:) )
-            self%bwork(n2-ncond2+j2,:) = self%bs1%bcoef(1:n1)
-          end do
-        else ! set needed boundary values to 0
-          self%bwork(n2-ncond2+1:n2,:) = 0.0_f64
-        end if
-
+    ! User must provide derivatives at boundary in case of Hermite BC
+    if (.not. present(boundary_data)) then
+      if (self%bc_xmin(1) == sll_p_hermite) then
+        SLL_ERROR( this_sub_name, "Hermite BC at x1_min requires derivatives" )
+      else if (self%bc_xmax(1) == sll_p_hermite) then
+        SLL_ERROR( this_sub_name, "Hermite BC at x1_max requires derivatives" )
+      else if (self%bc_xmin(2) == sll_p_hermite) then
+        SLL_ERROR( this_sub_name, "Hermite BC at x2_min requires derivatives" )
+      else if (self%bc_xmax(2) == sll_p_hermite) then
+        SLL_ERROR( this_sub_name, "Hermite BC at x2_max requires derivatives" )
       end if
     end if
 
-    ! Interior points
-    do i2 = 1, n2-2*ncond2
-      if (present(derivs_x1_min) .and. present(derivs_x1_max)) then
-        call self%bs1%compute_interpolant( &
-          gtau         (:,i2), &
-          derivs_x1_min(:,i2), &
-          derivs_x1_max(:,i2) )
-      else
-        call self%bs1%compute_interpolant( gtau(:,i2) )
-      end if
-      self%bwork(i2+ncond2,:) = self%bs1%bcoef(1:n1)
+    associate( w  => self%bcoef       , &
+               wt => self%bwork       , &
+               n1 => self%n(1)        , &
+               a1 => self%nbc_xmin(1) , &
+               b1 => self%nbc_xmax(1) , &
+               n2 => self%n(2)        , &
+               a2 => self%nbc_xmin(2) , &
+               b2 => self%nbc_xmax(2) )
+
+    SLL_ASSERT( all( shape(gtau) == [n1-a1-b1,n2-a2-b2] ) )
+
+    ! DEBUG mode, Hermite boundary conditions:
+    ! Verify that required arrays are passed and allocated with correct shape
+    if (a1 > 0) then
+      SLL_ASSERT(   present( boundary_data ) )
+      SLL_ASSERT( allocated( boundary_data % derivs_x1_min )  )
+      SLL_ASSERT(      size( boundary_data % derivs_x1_min, 1 ) == a1       )
+      SLL_ASSERT(      size( boundary_data % derivs_x1_min, 2 ) == n2-a2-b2 )
+    end if
+
+    if (b1 > 0) then
+      SLL_ASSERT(   present( boundary_data ) )
+      SLL_ASSERT( allocated( boundary_data % derivs_x1_max )  )
+      SLL_ASSERT(      size( boundary_data % derivs_x1_max, 1 ) == b1       )
+      SLL_ASSERT(      size( boundary_data % derivs_x1_max, 2 ) == n2-a2-b2 )
+    end if
+
+    if (a2 > 0) then
+      SLL_ASSERT(   present( boundary_data ) )
+      SLL_ASSERT( allocated( boundary_data % derivs_x2_min )  )
+      SLL_ASSERT(      size( boundary_data % derivs_x2_min, 1 ) == a2       )
+      SLL_ASSERT(      size( boundary_data % derivs_x2_min, 2 ) == n1-a1-b1 )
+    end if
+
+    if (b2 > 0) then
+      SLL_ASSERT(   present( boundary_data ) )
+      SLL_ASSERT( allocated( boundary_data % derivs_x2_max )  )
+      SLL_ASSERT(      size( boundary_data % derivs_x2_max, 1 ) == b2       )
+      SLL_ASSERT(      size( boundary_data % derivs_x2_max, 2 ) == n1-a1-b1 )
+    end if
+
+    if (a1 > 0 .and. a2 > 0) then
+      SLL_ASSERT( allocated( boundary_data % mixed_derivs_a )  )
+      SLL_ASSERT(      size( boundary_data % mixed_derivs_a, 1 ) == a1 )
+      SLL_ASSERT(      size( boundary_data % mixed_derivs_a, 2 ) == a2 )
+    end if
+
+    if (b1 > 0 .and. a2 > 0) then
+      SLL_ASSERT( allocated( boundary_data % mixed_derivs_b )  )
+      SLL_ASSERT(      size( boundary_data % mixed_derivs_b, 1 ) == b1 )
+      SLL_ASSERT(      size( boundary_data % mixed_derivs_b, 2 ) == a2 )
+    end if
+
+    if (a1 > 0 .and. b2 > 0) then
+      SLL_ASSERT( allocated( boundary_data % mixed_derivs_c )  )
+      SLL_ASSERT(      size( boundary_data % mixed_derivs_c, 1 ) == a1 )
+      SLL_ASSERT(      size( boundary_data % mixed_derivs_c, 2 ) == b2 )
+    end if
+
+    if (b1 > 0 .and. b2 > 0) then
+      SLL_ASSERT( allocated( boundary_data % mixed_derivs_d )  )
+      SLL_ASSERT(      size( boundary_data % mixed_derivs_d, 1 ) == b1 )
+      SLL_ASSERT(      size( boundary_data % mixed_derivs_d, 2 ) == b2 )
+    end if
+
+    ! Copy interpolation data onto w array
+    w(1+a1:n1-b1,1+a2:n2-b2) = gtau(:,:)
+
+    ! Hermite BCs: store boundary data in appropriate chunk of w array
+    if (present( boundary_data )) then
+
+      if (a1 > 0) w(      1:a1,1+a2:n2-b2) = boundary_data % derivs_x1_min(1:a1,:)
+      if (b1 > 0) w(n1-b1+1:n1,1+a2:n2-b2) = boundary_data % derivs_x1_max(1:b1,:)
+
+      if (a2 > 0) w(1+a1:n1-b1,      1:a2) = transpose( boundary_data % derivs_x2_min(1:a2,:) )
+      if (b2 > 0) w(1+a1:n1-b1,n2-b2+1:n2) = transpose( boundary_data % derivs_x2_max(1:b2,:) )
+
+      if (a1 > 0 .and. a2 > 0) w(      1:a1,      1:a2) = boundary_data % mixed_derivs_a(:,:)
+      if (b1 > 0 .and. a2 > 0) w(n1-b1+1:n1,      1:a2) = boundary_data % mixed_derivs_b(:,:)
+      if (a1 > 0 .and. b2 > 0) w(      1:a1,n2-b2+1:n2) = boundary_data % mixed_derivs_c(:,:)
+      if (b1 > 0 .and. b2 > 0) w(n1-b1+1:n1,n2-b2+1:n2) = boundary_data % mixed_derivs_d(:,:)
+
+    end if
+
+    ! Cycle over x2 position (or order of x2-derivative at boundary)
+    ! and interpolate f along x1 direction. Store coefficients in bwork
+    do i2 = 1, n2
+
+      call self%bs1%compute_interpolant( &
+        gtau        = w(   1+a1:n1-b1,i2) , &
+        derivs_xmin = w(      1:a1   ,i2) , &
+        derivs_xmax = w(n1-b1+1:n1   ,i2) )
+
+      w(1:n1,i2) = self%bs1%bcoef(1:n1)
+
     end do
 
-    !--------------------------------------------
-    ! Compute spline coefficients in x2 direction
-    !--------------------------------------------
-    if (bc_type2 == sll_p_hermite) then
+    wt = transpose( w )
 
-      if (present(derivs_x2_min) .and. present(derivs_x2_max)) then
-        do i1 = 1, n1
-          call self%bs2%compute_interpolant( &
-               self%bwork(ncond2+1:n2-ncond2,i1), &
-               self%bwork(1:ncond2,i1), &
-               self%bwork(n2-ncond2+1:n2,i1) )
-          self%bcoef(i1,:) = self%bs2%bcoef(:)
-        end do
-      else
-        do i1 = 1, n1
-          call self%bs2%compute_interpolant( self%bwork(ncond2+1:n2-ncond2,i1) )
-          self%bcoef(i1,:) = self%bs2%bcoef(:)
-        end do
-      end if
+    ! Cycle over x1 position (or order of x1-derivative at boundary)
+    ! and interpolate w along x2 direction. Store coefficients in bcoef
+    do i1 = 1, n1
 
-    else
+      call self%bs2%compute_interpolant( &
+        gtau        = wt(   1+a2:n2-b2,i1) , &
+        derivs_xmin = wt(      1:a2   ,i1) , &
+        derivs_xmax = wt(n2-b2+1:n2   ,i1) )
 
-      do i1 = 1, n1
-        call self%bs2%compute_interpolant( self%bwork(:,i1) )
-        self%bcoef(i1,:) = self%bs2%bcoef(:)
-      end do
+      wt(1:n2,i1) = self%bs2%bcoef(1:n2)
 
-    end if
+    end do
 
-    !--------------------------------------------------------------
-    ! Periodic only: "wrap around" coefficients onto extended array
-    ! NOTE: only x1 direction is necessary, x2 already taken care of
-    !--------------------------------------------------------------
-    if (self%bs1%bc_xmin == sll_p_periodic) then
-      associate( g1 => 1+self%bs1%deg/2 )
-        self%bcoef(1-g1:0    ,:) = self%bcoef(n1-g1+1:n1,:)
-        self%bcoef(n1+1:n1+g1,:) = self%bcoef(      1:g1,:)
+    ! x1-periodic only: "wrap around" coefficients onto extended array
+    if (self%bc_xmin(1) == sll_p_periodic) then
+      associate( g1 => 1+self%deg(1)/2 )
+        wt(:,1-g1:0    ) = wt(:,n1-g1+1:n1)
+        wt(:,n1+1:n1+g1) = wt(:,      1:g1)
       end associate
     end if
+
+    w = transpose( wt )
+
+    ! x2-periodic only: "wrap around" coefficients onto extended array
+    if (self%bc_xmin(2) == sll_p_periodic) then
+      associate( g2 => 1+self%deg(2)/2 )
+        w(:,1-g2:0    ) = w(:,n2-g2+1:n2)
+        w(:,n2+1:n2+g2) = w(:,      1:g2)
+      end associate
+    end if
+
+    end associate
 
   end subroutine sll_s_spline_2d_non_uniform_compute_interpolant
 
