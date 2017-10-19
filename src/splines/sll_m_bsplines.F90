0dd3c6f318f05b34ed67266fac56e4d5ce55d6ab
Yaman Güçlü <yaman.guclu@gmail.com>
2017-10-19 20:44:45 +0200

    [polar-spline] Use radial B-splines on domain [0,R] instead of standard B-splines on extended domain [-R,R].

--- src/splines/sll_m_bsplines.F90
+++ src/splines/sll_m_bsplines.F90
@@ -16,7 +16,9 @@ module sll_m_bsplines
 
   public :: &
     sll_c_bsplines, &
-    sll_s_bsplines_new
+    sll_s_bsplines_new, &
+    sll_s_bsplines_new_2d_polar, &
+    sll_s_bsplines_new_mirror_copy
 
   private
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -88,6 +90,119 @@ contains
       call bsplines % init( degree, periodic, breaks )
     end select
 
+    ! Set 'radial' flag to false
+    bsplines % radial = .false.
+
   end subroutine sll_s_bsplines_new
+
+  !-----------------------------------------------------------------------------
+  !> @brief      Allocate and initialize two B-splines for use on polar grid
+  !-----------------------------------------------------------------------------
+  subroutine sll_s_bsplines_new_2d_polar( &
+      bsplines_radial , &
+      bsplines_angular, &
+      degree          , &
+      ncells          , &
+      max_radius      , &
+      theta_lims      , &
+      breaks_radial   , &
+      breaks_angular  )
+
+    class(sll_c_bsplines), allocatable, intent(inout) :: bsplines_radial
+    class(sll_c_bsplines), allocatable, intent(inout) :: bsplines_angular
+    integer                           , intent(in   ) :: degree(2)
+    integer                           , intent(in   ) :: ncells(2)
+    real(wp)                          , intent(in   ) :: max_radius
+    real(wp)                          , intent(in   ) :: theta_lims(2)
+    real(wp),                 optional, intent(in   ) :: breaks_radial (:)
+    real(wp),                 optional, intent(in   ) :: breaks_angular(:)
+
+    character(len=*), parameter :: this_sub_name = "sll_s_bsplines_new_2d_polar"
+
+    logical  :: uniform(2)
+    real(wp) :: dr
+
+    ! Sanity checks
+    SLL_ASSERT( .not. allocated( bsplines_radial  ) )
+    SLL_ASSERT( .not. allocated( bsplines_angular ) )
+    SLL_ASSERT( degree(1) > 0 )
+    SLL_ASSERT( degree(2) > 0 )
+    SLL_ASSERT( ncells(1) > 0 )
+    SLL_ASSERT( ncells(2) > 0 )
+    SLL_ASSERT( theta_lims(1) < theta_lims(2) )
+
+    ! Specific checks for polar geometry
+    SLL_ASSERT( max_radius > 0.0_wp )
+    SLL_ASSERT( modulo( ncells(2), 2 ) == 0 )
+
+    ! We do not accept non-uniform splines yet
+    if (present( breaks_radial )) then
+      uniform(1) = .false.
+      SLL_ERROR( this_sub_name, "non-uniform option not yet implemented." )
+    else
+      uniform(1) = .true.
+    end if
+
+    if (present( breaks_angular )) then
+      uniform(2) = .false.
+      SLL_ERROR( this_sub_name, "non-uniform option not yet implemented." )
+    else
+      uniform(2) = .true.
+    end if
+
+    dr = max_radius / (real(ncells(1),wp)-0.5_wp)
+
+    call sll_s_bsplines_new( &
+      bsplines = bsplines_radial, &
+      degree   = degree(1) , &
+      periodic = .false.   , &
+      xmin     = -0.5_wp*dr, &
+      xmax     = max_radius, &
+      ncells   = ncells(1) , &
+      breaks   = breaks_radial )
+
+    call sll_s_bsplines_new( &
+      bsplines = bsplines_angular, &
+      degree   = degree(2)    , &
+      periodic = .true.       , &
+      xmin     = theta_lims(1), &
+      xmax     = theta_lims(2), &
+      ncells   = ncells(2)    , &
+      breaks   = breaks_angular )
+
+    ! Set 'radial' flag to true
+    bsplines_radial % radial = .true.
+
+  end subroutine sll_s_bsplines_new_2d_polar
+
+  !-----------------------------------------------------------------------------
+  !> @brief  Create B-splines by mirroring radial basis onto extended domain [-R,R]
+  !-----------------------------------------------------------------------------
+  subroutine sll_s_bsplines_new_mirror_copy( radial_basis, extended_basis )
+    class(sll_c_bsplines),              intent(in   ) :: radial_basis
+    class(sll_c_bsplines), allocatable, intent(inout) :: extended_basis
+
+    character(len=*), parameter :: this_sub_name = "sll_s_bsplines_new_mirror_copy"
+
+    SLL_ASSERT( radial_basis % radial )
+    SLL_ASSERT( .not. allocated( extended_basis ) )
+
+    select type( radial_basis )
+
+    type is( sll_t_bsplines_uniform )
+      call sll_s_bsplines_new( &
+        bsplines =  extended_basis, &
+        degree   =  radial_basis % degree, &
+        periodic = .false., &
+        xmin     = -radial_basis % xmax, &
+        xmax     =  radial_basis % xmax, &
+        ncells   =  radial_basis % ncells*2 - 1 )
+
+    class default
+      SLL_ERROR( this_sub_name, "non-uniform option not yet implemented." )
+
+    end select
+
+  end subroutine sll_s_bsplines_new_mirror_copy
 
 end module sll_m_bsplines
