06650b1003429eace2c9f7a4c173ef001fdc92b0
Edoardo Zoni <EZoni@lbl.gov>
2018-01-22 17:56:07 +0100

    [polar-spline] Add method to compute Jacobian determinant (analytical and discrete types) and cleanup:
    - unique method to store data on HDF5 files (mesh, Jacobian determinant, control points, ...);
    - set default Target and Czarny mappings to produce standard circular mapping;
    - add some output to Python script;
    - CMakeLists.txt: remove sll_xdmf_io from TARGET_LINK_LIBRARIES (added last commit but unnecessary).

--- src/splines/sll_m_polar_mapping_analytical.F90
+++ src/splines/sll_m_polar_mapping_analytical.F90
@@ -1,7 +1,17 @@
 module sll_m_polar_mapping_analytical
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+  use sll_m_working_precision, only: f64
+
+  use sll_m_constants, only: sll_p_twopi
+
   use sll_m_polar_mapping_base, only: sll_c_polar_mapping
+
+  use sll_m_hdf5_io_serial, only: &
+    sll_t_hdf5_ser_handle     , &
+    sll_s_hdf5_ser_file_create, &
+    sll_s_hdf5_ser_file_close , &
+    sll_o_hdf5_ser_write_array
 
   implicit none
 
@@ -10,9 +20,69 @@ module sll_m_polar_mapping_analytical
   private
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+  !> Working precision
+  integer, parameter :: wp = f64
+
   !> Abstract type, analytical polar mapping
   !  (may contain common components/methods)
   type, extends(sll_c_polar_mapping), abstract :: sll_c_polar_mapping_analytical
+
+  contains
+
+    procedure :: store_data => s_polar_mapping_analytical__store_data
+
   end type sll_c_polar_mapping_analytical
+
+!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+contains
+!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+  subroutine s_polar_mapping_analytical__store_data( self, n1, n2, file_name )
+    class(sll_c_polar_mapping_analytical), intent(in) :: self
+    integer                              , intent(in) :: n1
+    integer                              , intent(in) :: n2
+    character(len=*)                     , intent(in) :: file_name
+
+    integer  :: i1, i2
+    real(wp) :: eta(2), x(2)
+
+    real(wp), allocatable :: x1(:,:), x2(:,:), jacobian(:,:)
+
+    ! For hdf5 I/O
+    type(sll_t_hdf5_ser_handle) :: file_id
+    integer                     :: error
+
+    ! Allocate physical mesh
+    allocate( x1( n1, n2+1 ) ) ! repeated point along eta2
+    allocate( x2( n1, n2+1 ) ) ! repeated point along eta2
+
+    ! Allocate Jacobian determinant
+    allocate( jacobian( n1, n2+1 ) ) ! repeated point along eta2
+
+    ! Create HDF5 file
+    call sll_s_hdf5_ser_file_create( trim( file_name )//'.h5', file_id, error )
+
+    ! Compute physical mesh and Jacobian determinant
+    do i2 = 1, n2+1 ! repeated point along eta2
+      do i1 = 1, n1
+        eta(1) = real( i1-1, wp ) / real( n1-1, wp )
+        eta(2) = real( i2-1, wp ) * sll_p_twopi / real( n2, wp )
+        x  (:) = self % eval( eta )
+        x1(i1,i2) = x(1)
+        x2(i1,i2) = x(2)
+        jacobian(i1,i2) = self % jacobian( eta )
+      end do
+    end do
+
+    ! Store physical mesh
+    call sll_o_hdf5_ser_write_array( file_id, x1, "/x1", error )
+    call sll_o_hdf5_ser_write_array( file_id, x2, "/x2", error )
+
+    ! Store Jacobian determinant
+    call sll_o_hdf5_ser_write_array( file_id, jacobian, "/jacobian", error )
+
+    call sll_s_hdf5_ser_file_close ( file_id, error )
+
+  end subroutine s_polar_mapping_analytical__store_data
 
 end module sll_m_polar_mapping_analytical
--- src/splines/sll_m_polar_mapping_base.F90
+++ src/splines/sll_m_polar_mapping_base.F90
@@ -3,14 +3,6 @@ module sll_m_polar_mapping_base
 
   use sll_m_working_precision, only: f64
 
-  use sll_m_constants, only: sll_p_twopi
-
-  use sll_m_hdf5_io_serial, only: &
-    sll_t_hdf5_ser_handle     , &
-    sll_s_hdf5_ser_file_create, &
-    sll_s_hdf5_ser_file_close , &
-    sll_o_hdf5_ser_write_array
-
   implicit none
 
   public :: sll_c_polar_mapping
@@ -27,10 +19,9 @@ module sll_m_polar_mapping_base
   contains
 
     ! Deferred procedures
-    procedure(i_fun_eval), deferred :: eval
-
-    ! Non-deferred procedures
-    procedure :: write_mesh => s_polar_mapping__write_mesh
+    procedure(i_fun_eval)      , deferred :: eval
+    procedure(i_fun_jacobian)  , deferred :: jacobian ! Jacobian determinant
+    procedure(i_sub_store_data), deferred :: store_data
 
   end type sll_c_polar_mapping
 
@@ -44,48 +35,21 @@ module sll_m_polar_mapping_base
       real(wp) :: x(2)
     end function i_fun_eval
 
+    function i_fun_jacobian( self, eta ) result( jacobian )
+      import sll_c_polar_mapping, wp
+      class(sll_c_polar_mapping), intent(in) :: self
+      real(wp)                  , intent(in) :: eta(2)
+      real(wp) :: jacobian
+    end function i_fun_jacobian
+
+    subroutine i_sub_store_data( self, n1, n2, file_name )
+      import sll_c_polar_mapping
+      class(sll_c_polar_mapping), intent(in) :: self
+      integer                   , intent(in) :: n1
+      integer                   , intent(in) :: n2
+      character(len=*)          , intent(in) :: file_name
+    end subroutine i_sub_store_data
+
   end interface
-
-!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-contains
-!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-  subroutine s_polar_mapping__write_mesh( self, n1, n2, file_name )
-    class(sll_c_polar_mapping), intent(inout) :: self
-    integer                   , intent(in   ) :: n1
-    integer                   , intent(in   ) :: n2
-    character(len=*)          , intent(in   ) :: file_name
-
-    integer  :: i1, i2
-    real(wp) :: eta(2), x(2)
-
-    real(wp), allocatable :: x1(:,:)
-    real(wp), allocatable :: x2(:,:)
-
-    ! For hdf5 I/O
-    type(sll_t_hdf5_ser_handle) :: file_id
-    integer                     :: error
-
-    ! Allocate physical mesh
-    allocate( x1( n1, n2+1 ) ) ! repeated point along eta2
-    allocate( x2( n1, n2+1 ) ) ! repeated point along eta2
-
-    ! Initialize physical mesh (assuming domain [0,1]x[0,2\pi] )
-    do i2 = 1, n2+1 ! repeated point along eta2
-      do i1 = 1, n1
-        eta(1) = real( i1-1, wp ) / real( n1-1, wp )
-        eta(2) = real( i2-1, wp ) * sll_p_twopi / real( n2, wp )
-        x  (:) = self % eval( eta )
-        x1(i1,i2) = x(1)
-        x2(i1,i2) = x(2)
-      end do
-    end do
-
-    call sll_s_hdf5_ser_file_create( trim( file_name )//".h5", file_id, error )
-    call sll_o_hdf5_ser_write_array( file_id, x1, "/x1", error )
-    call sll_o_hdf5_ser_write_array( file_id, x2, "/x2", error )
-    call sll_s_hdf5_ser_file_close ( file_id, error ) 
-
-  end subroutine s_polar_mapping__write_mesh
 
 end module sll_m_polar_mapping_base
