6f115f20d536c158d3fb68a3637dbcf60ad75132
Edoardo Zoni <edoardo.zoni@ipp.mpg.de>
2018-03-22 18:43:41 +0100

    [fem-ssm-Poisson] Fix bug in assembler (wrong global indices to access matrices).

--- src/field_solvers/poisson_solvers/sll_m_poisson_2d_fem_ssm_assembler.F90
+++ src/field_solvers/poisson_solvers/sll_m_poisson_2d_fem_ssm_assembler.F90
@@ -70,43 +70,58 @@ contains
 
     integer  :: i, j, i1, i2, j1, j2, p1, p2
     real(wp) :: Aij, Mij
+    integer  :: il1, il2, jl1, jl2
 
     ! Extract degree of B-splines
     p1 = size( data_1d_eta1, 3 ) - 1
     p2 = size( data_1d_eta2, 3 ) - 1
 
-    ! Cycle over basis functions in 2D test space
-    do i1 = 1, p1+1
-      do i2 = 1, p2+1
+    associate( n1 => self % n1, n2 => self % n2 )
 
-        ! First matrix global index
-        i = i1 * self % n2 + i2
+      ! Cycle over basis functions in 2D trial space (same as test space)
+      do jl2 = 1, p2+1
 
-        ! Cycle over basis functions in 2D trial space (same as test space)
-        do j1 = 1, p1+1
-          do j2 = 1, p2+1
+        j2 = modulo( k2 + jl2 - 2, n2 )
 
-            ! Second matrix global index
-            j = j1 * self % n2 + j2
+        do jl1 = 1, p1+1
 
-            call self % weak_form % element( &
-              test_values_and_derivs_eta1  = data_1d_eta1(:,:,i1,k1)  , &
-              test_values_and_derivs_eta2  = data_1d_eta2(:,:,i2,k2)  , &
-              trial_values_and_derivs_eta1 = data_1d_eta1(:,:,j1,k1)  , &
-              trial_values_and_derivs_eta2 = data_1d_eta2(:,:,j2,k2)  , &
-              int_volume                   = int_volume(:,:,k1,k2)    , &
-              inv_metric                   = inv_metric(:,:,k1,k2,:,:), &
-              Aij = Aij, &
-              Mij = Mij )
+          j1 = k1 + jl1 - 2
 
-            A(i,j) = A(i,j) + Aij
-            M(i,j) = M(i,j) + Mij
+          ! Second matrix global index
+          j = j1 * n2 + j2 + 1
 
-          end do
-        end do ! End cycle over basis functions in 2D trial space
+          ! Cycle over basis functions in 2D test space
+          do il2 = 1, p2+1
 
-      end do
-    end do ! End cycle over basis functions in 2D test space
+            i2 = modulo( k2 + il2 - 2, n2 )
+
+            do il1 = 1, p1+1
+
+              i1 = k1 + il1 - 2
+
+              ! First matrix global index
+              i = i1 * n2 + i2 + 1
+
+              call self % weak_form % element( &
+                test_values_and_derivs_eta1  = data_1d_eta1(:,:,il1,k1)  , &
+                test_values_and_derivs_eta2  = data_1d_eta2(:,:,il2,k2)  , &
+                trial_values_and_derivs_eta1 = data_1d_eta1(:,:,jl1,k1)  , &
+                trial_values_and_derivs_eta2 = data_1d_eta2(:,:,jl2,k2)  , &
+                int_volume                   = int_volume(:,:,k1,k2)    , &
+                inv_metric                   = inv_metric(:,:,k1,k2,:,:), &
+                Aij = Aij, &
+                Mij = Mij )
+
+              A(i,j) = A(i,j) + Aij
+              M(i,j) = M(i,j) + Mij
+
+            end do
+          end do ! End cycle over basis functions in 2D test space
+
+        end do
+      end do ! End cycle over basis functions in 2D trial space
+
+    end associate
 
   end subroutine s_poisson_2d_fem_ssm_assembler__add_element
 
