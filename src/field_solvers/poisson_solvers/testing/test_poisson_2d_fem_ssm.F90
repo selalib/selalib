68aea9a7a9d78fcbff68f804ec1a4df91f929981
Edoardo Zoni <EZoni@lbl.gov>
2018-03-28 10:58:10 +0200

    [fem-ssm-Poisson] Update indices to access B-splines coefficients after change of numbering.

--- src/field_solvers/poisson_solvers/testing/test_poisson_2d_fem_ssm.F90
+++ src/field_solvers/poisson_solvers/testing/test_poisson_2d_fem_ssm.F90
@@ -303,8 +303,7 @@ program test_poisson_2d_fem_ssm
 
     ! Indices to access control points
     j = (i-1) / n2 + 1
-    ! TODO: remove shift of p/2 after numeration of periodic B-splines is fixed
-    k = modulo( (i-1)-p2/2, n2 ) + 1
+    k = modulo( (i-1), n2 ) + 1
 
     cx = mapping_iga % spline_2d_x1 % bcoef(j,k)
     cy = mapping_iga % spline_2d_x2 % bcoef(j,k)
