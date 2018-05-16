4c235a856448c3d35cd26acad26008a79f911e46
Pierre Navaro <navaro@math.cnrs.fr>
2018-05-16 17:32:41 +0200

    Improve the gaussian deviate module

--- src/particle_methods/random_deviate_generators/sll_m_gaussian.F90
+++ src/particle_methods/random_deviate_generators/sll_m_gaussian.F90
@@ -27,7 +27,7 @@ contains
   function sll_f_gaussian_deviate()
     sll_real64 :: sll_f_gaussian_deviate
 
-    sll_real64 :: rsq, v1, v2
+    sll_real64 :: rsq, v(2)
     sll_real64, save :: g
     logical, save :: g_stored=.false.
     
@@ -36,16 +36,15 @@ contains
        g_stored=.false.
     else
        do
-          call random_number(v1)
-          call random_number(v2)
-          v1=2._f64*v1 - 1._f64
-          v2=2._f64*v2 - 1._f64
-          rsq = v1**2+v2**2
-          if (rsq>0._f64.and.rsq<1._f64) exit
+          call random_number(v)
+          v(1)=2._f64*v(1) - 1._f64
+          v(2)=2._f64*v(2) - 1._f64
+          rsq = v(1)*v(1) + v(2)*v(2)
+          if (rsq < 1._f64) exit
        enddo
        rsq = sqrt(-2._f64*log(rsq)/rsq)
-       sll_f_gaussian_deviate=v1*rsq
-       g=v2*rsq
+       sll_f_gaussian_deviate=v(1)*rsq
+       g=v(2)*rsq
        g_stored=.true.
     endif
   end function sll_f_gaussian_deviate
