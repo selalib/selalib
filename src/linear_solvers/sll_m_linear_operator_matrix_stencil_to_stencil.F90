49d7c9234e0291c646a19ee33ac305cdaba495fc
Yaman Güçlü <yaman.guclu@gmail.com>
2018-09-04 15:17:03 +0200

    [linear-operator] Optimize dot product loops (stencil-to-stencil version).

--- src/linear_solvers/sll_m_linear_operator_matrix_stencil_to_stencil.F90
+++ src/linear_solvers/sll_m_linear_operator_matrix_stencil_to_stencil.F90
@@ -79,6 +79,7 @@ contains
     class(sll_c_vector_space)                             , intent(inout) :: y ! stencil
 
     integer :: i1, i2, j1, j2, k1, k2
+    real(wp) :: temp
 
     character(len=*), parameter :: this_sub_name = "sll_t_linear_operator_matrix_stencil_to_stencil % dot"
     character(len=64) :: err_msg
@@ -109,15 +110,32 @@ contains
           SLL_ASSERT( self % s2 ==  ubound( y % array, 2 ) + lbound( y % array, 2 ) - 1 )
 
           y % array = 0.0_wp
+
           do i2 = 1, s2
             do i1 = 1, s1
+
+              temp = 0.0_wp
+
               do k2 = -p2, p2
                 do k1 = -p1, p1
-                  j1 = modulo( i1 - 1 + k1, s1 ) + 1
-                  j2 = modulo( i2 - 1 + k2, s2 ) + 1
-                  y % array(i1,i2) = y % array(i1,i2) + self % A(k1,k2,i1,i2) * x % array(j1,j2)
+
+                  j1 = i1+k1
+                  j2 = i2+k2
+
+                  ! Hand-made modulo operation
+                  if (j2 < 1) then
+                    j2 = j2+s2
+                  else if (j2 > s2) then
+                    j2 = j2-s2
+                  end if
+
+                  temp = temp + self % A(k1,k2,i1,i2) * x % array(j1,j2)
+
                 end do
               end do
+
+              y % array(i1,i2) = y % array(i1,i2) + temp
+
             end do
           end do
 
@@ -150,6 +168,7 @@ contains
     class(sll_c_vector_space)                             , intent(inout) :: y ! stencil
 
     integer :: i1, i2, j1, j2, k1, k2
+    real(wp) :: temp
 
     character(len=*), parameter :: this_sub_name = "sll_t_linear_operator_matrix_stencil_to_stencil % dot"
     character(len=64) :: err_msg
@@ -181,13 +200,29 @@ contains
 
           do i2 = 1, s2
             do i1 = 1, s1
+
+              temp = 0.0_wp
+
               do k2 = -p2, p2
                 do k1 = -p1, p1
-                  j1 = modulo( i1 - 1 + k1, s1 ) + 1
-                  j2 = modulo( i2 - 1 + k2, s2 ) + 1
-                  y % array(i1,i2) = y % array(i1,i2) + self % A(k1,k2,i1,i2) * x % array(j1,j2)
+
+                  j1 = i1+k1
+                  j2 = i2+k2
+
+                  ! Hand-made modulo operation
+                  if (j2 < 1) then
+                    j2 = j2+s2
+                  else if (j2 > s2) then
+                    j2 = j2-s2
+                  end if
+
+                  temp = temp + self % A(k1,k2,i1,i2) * x % array(j1,j2)
+
                 end do
               end do
+
+              y % array(i1,i2) = y % array(i1,i2) + temp
+
             end do
           end do
 
