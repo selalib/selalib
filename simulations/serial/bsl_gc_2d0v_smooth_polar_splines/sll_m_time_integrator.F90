9c08623e595b0baf6dc5eddbd1c316de1a1f27e0
Edoardo Zoni <EZoni@lbl.gov>
2018-09-27 17:48:05 +0200

    [guiding-center-cleanup] Add separate predictor and corrector methods to time integrator module.

--- simulations/serial/bsl_gc_2d0v_smooth_polar_splines/sll_m_time_integrator.F90
+++ simulations/serial/bsl_gc_2d0v_smooth_polar_splines/sll_m_time_integrator.F90
@@ -32,11 +32,13 @@ module sll_m_time_integrator
     type(sll_t_spline_interpolator_2d)        , pointer :: spline_interp_2d => null()
 
     type(sll_t_simulation_state), pointer :: sim_state
-    type(sll_t_simulation_state)          :: sim_state_aux
+    type(sll_t_simulation_state)          :: sim_state_copy
 
   contains
 
     procedure :: init            => s_time_integrator__init
+    procedure :: predictor       => s_time_integrator__predictor
+    procedure :: corrector       => s_time_integrator__corrector
     procedure :: advance_in_time => s_time_integrator__advance_in_time
     procedure :: free            => s_time_integrator__free
 
@@ -61,9 +63,69 @@ contains
     self % spline_interp_2d => spline_interp_2d
 
     self % sim_state => sim_state
-    call self % sim_state % copy( self % sim_state_aux )
+    call self % sim_state % copy( self % sim_state_copy )
 
   end subroutine s_time_integrator__init
+
+  !-----------------------------------------------------------------------------
+  subroutine s_time_integrator__predictor( self, success )
+    class(sll_t_time_integrator), intent(inout) :: self
+    logical                     , intent(  out) :: success
+
+    integer :: ic
+
+    associate( dt                 => self % dt                               , &
+               nc                 => size( self % sim_state % point_charges ), &
+               point_charges      => self % sim_state % point_charges        , &
+               point_charges_copy => self % sim_state_copy % point_charges   , &
+               rho_copy           => self % sim_state_copy % rho )
+
+      ! Evolve density
+      call self % advector % advect_distribution( -0.5_wp*dt, success, rho_copy )
+      if ( .not. success ) return
+
+      ! Evolve point charges
+      if ( nc /= 0 ) then
+        do ic = 1, nc
+          point_charges_copy(ic) % location(:) = point_charges(ic) % location(:)
+        end do
+        call self % advector % advect_point_charges( 0.5_wp*dt, success, point_charges )
+        if ( .not. success ) return
+      end if
+
+    end associate
+
+  end subroutine s_time_integrator__predictor
+
+  !-----------------------------------------------------------------------------
+  subroutine s_time_integrator__corrector( self, success )
+    class(sll_t_time_integrator), intent(inout) :: self
+    logical                     , intent(  out) :: success
+
+    integer :: ic
+
+    associate( dt                 => self % dt                               , &
+               nc                 => size( self % sim_state % point_charges ), &
+               point_charges      => self % sim_state % point_charges        , &
+               point_charges_copy => self % sim_state_copy % point_charges   , &
+               rho_copy           => self % sim_state_copy % rho )
+
+      ! Evolve density
+      call self % advector % advect_distribution( -dt, success, rho_copy )
+      if ( .not. success ) return
+
+      ! Evolve point charges
+      if ( nc /= 0 ) then
+        do ic = 1, nc
+          point_charges(ic) % location(:) = point_charges_copy(ic) % location(:)
+        end do
+        call self % advector % advect_point_charges( dt, success, point_charges )
+        if ( .not. success ) return
+      end if
+
+    end associate
+
+  end subroutine s_time_integrator__corrector
 
   !-----------------------------------------------------------------------------
   subroutine s_time_integrator__advance_in_time( self, success )
@@ -72,30 +134,19 @@ contains
 
     integer :: ic
 
-    associate( dt                => self % dt                               , &
-               nc                => size( self % sim_state % point_charges ), &
-               ntau2             => size( self % sim_state % rho, 2 ) - 1   , &
-               spline_interp_2d  => self % spline_interp_2d                 , &
-               spline_2d_rho     => self % sim_state % spline_2d_rho        , &
-               spline_2d_phi     => self % sim_state % spline_2d_phi        , &
-               point_charges     => self % sim_state % point_charges        , &
-               point_charges_aux => self % sim_state_aux % point_charges    , &
-               rho_aux           => self % sim_state_aux % rho )
+    associate( nc                 => size( self % sim_state % point_charges ), &
+               ntau2              => size( self % sim_state % rho, 2 ) - 1   , &
+               spline_interp_2d   => self % spline_interp_2d                 , &
+               spline_2d_rho      => self % sim_state % spline_2d_rho        , &
+               spline_2d_phi      => self % sim_state % spline_2d_phi        , &
+               point_charges      => self % sim_state % point_charges        , &
+               rho                => self % sim_state % rho                  , &
+               point_charges_copy => self % sim_state_copy % point_charges   , &
+               rho_copy           => self % sim_state_copy % rho )
 
-      ! Predictor: evolve density
-      call self % advector % advect_distribution( -0.5_wp*dt, success, rho_aux )
-      if ( .not. success ) return
+      call self % predictor( success )
 
-      ! Predictor: evolve point charges
-      if ( nc /= 0 ) then
-        do ic = 1, nc
-          point_charges_aux(ic) % location(:) = point_charges(ic) % location(:)
-        end do
-        call self % advector % advect_point_charges( 0.5_wp*dt, success, point_charges )
-        if ( .not. success ) return
-      end if
-
-      call spline_interp_2d % compute_interpolant( spline_2d_rho, rho_aux(:,1:ntau2) )
+      call spline_interp_2d % compute_interpolant( spline_2d_rho, rho_copy(:,1:ntau2) )
 
       ! Solve Poisson equation
       call self % poisson_solver % reset_charge()
@@ -112,20 +163,9 @@ contains
 
       call spline_interp_2d % compute_interpolant( spline_2d_rho, self % sim_state % rho(:,1:ntau2) )
 
-      ! Corrector: evolve density
-      call self % advector % advect_distribution( -dt, success, rho_aux )
-      if ( .not. success ) return
+      call self % corrector( success )
 
-      ! Corrector: evolve point charges
-      if ( nc /= 0 ) then
-        do ic = 1, nc
-          point_charges(ic) % location(:) = point_charges_aux(ic) % location(:)
-        end do
-        call self % advector % advect_point_charges( dt, success, point_charges )
-        if ( .not. success ) return
-      end if
-
-      call spline_interp_2d % compute_interpolant( spline_2d_rho, rho_aux(:,1:ntau2) )
+      call spline_interp_2d % compute_interpolant( spline_2d_rho, rho_copy(:,1:ntau2) )
 
       ! Solve Poisson equation
       call self % poisson_solver % reset_charge()
@@ -140,7 +180,7 @@ contains
       end if
       call self % poisson_solver % solve ( spline_2d_phi )
 
-      self % sim_state % rho = rho_aux
+      self % sim_state % rho = rho_copy
 
     end associate
 
