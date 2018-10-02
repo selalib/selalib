7bb4368bb09a8b4e9ac441b541c99d4106f6bc8d
Edoardo Zoni <EZoni@lbl.gov>
2018-10-02 18:22:11 +0200

    [guiding-center-cleanup] Add explicit 2nd order time integrator and restructure related classes/types (further cleanup needed).

--- simulations/serial/bsl_gc_2d0v_smooth_polar_splines/sll_m_time_integrator_implicit.F90
+++ simulations/serial/bsl_gc_2d0v_smooth_polar_splines/sll_m_time_integrator_implicit.F90
@@ -1,4 +1,4 @@
-module sll_m_time_integrator
+module sll_m_time_integrator_implicit
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #include "sll_assert.h"
 
@@ -10,13 +10,15 @@ module sll_m_time_integrator
 
   use sll_m_polar_mapping_iga, only: sll_t_polar_mapping_iga
 
+  use sll_m_point_charge, only: sll_t_point_charge
+
   use sll_m_poisson_2d_fem_sps_stencil_new, only: sll_t_poisson_2d_fem_sps_stencil_new
 
   use sll_m_time_integrator_base, only: sll_c_time_integrator
 
   implicit none
 
-  public :: sll_t_time_integrator
+  public :: sll_t_time_integrator_implicit
 
   private
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -24,30 +26,32 @@ module sll_m_time_integrator
   ! Working precision
   integer, parameter :: wp = f64
 
-  type, extends(sll_c_time_integrator) :: sll_t_time_integrator
+  type, extends(sll_c_time_integrator) :: sll_t_time_integrator_implicit
 
     real(wp) :: abs_tol
     real(wp) :: rel_tol
     integer  :: maxiter
 
-    type(sll_t_simulation_state) :: sim_state_copy
+    type(sll_t_simulation_state), pointer :: sim_state_copy
 
   contains
 
-    procedure :: init                 => s_time_integrator__init
-    procedure :: advect_single_coords => s_time_integrator__advect_single_coords
-    procedure :: predictor            => s_time_integrator__predictor
-    procedure :: corrector            => s_time_integrator__corrector
-    procedure :: advance_in_time      => s_time_integrator__advance_in_time
-    procedure :: free                 => s_time_integrator__free
+    procedure :: init                 => s_time_integrator_implicit__init
+    procedure :: advect_single_coords => s_time_integrator_implicit__advect_single_coords
+    procedure :: advect_distribution  => s_time_integrator_implicit__advect_distribution
+    procedure :: advect_point_charges => s_time_integrator_implicit__advect_point_charges
+    procedure :: predictor            => s_time_integrator_implicit__predictor
+    procedure :: corrector            => s_time_integrator_implicit__corrector
+    procedure :: advance_in_time      => s_time_integrator_implicit__advance_in_time
+    procedure :: free                 => s_time_integrator_implicit__free
 
-  end type sll_t_time_integrator
+  end type sll_t_time_integrator_implicit
 
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 contains
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-  subroutine s_time_integrator__init( &
+  subroutine s_time_integrator_implicit__init( &
     self            , &
     dt              , &
     tau_eta1        , &
@@ -55,17 +59,19 @@ contains
     mapping_discrete, &
     spline_interp_2d, &
     sim_state       , &
+    sim_state_copy  , &
     poisson_solver  , &
     abs_tol         , &
     rel_tol         , &
     maxiter )
-    class(sll_t_time_integrator)                      , intent(inout) :: self
+    class(sll_t_time_integrator_implicit)             , intent(inout) :: self
     real(wp)                                          , intent(in   ) :: dt
     real(wp)                                  , target, intent(in   ) :: tau_eta1(:)
     real(wp)                                  , target, intent(in   ) :: tau_eta2(:)
     type(sll_t_polar_mapping_iga)                     , intent(in   ) :: mapping_discrete
     type(sll_t_spline_interpolator_2d)        , target, intent(in   ) :: spline_interp_2d
     type(sll_t_simulation_state)              , target, intent(in   ) :: sim_state
+    type(sll_t_simulation_state)              , target, intent(in   ) :: sim_state_copy
     type(sll_t_poisson_2d_fem_sps_stencil_new), target, intent(in   ) :: poisson_solver
     real(wp)                                          , intent(in   ) :: abs_tol
     real(wp)                                          , intent(in   ) :: rel_tol
@@ -81,26 +87,26 @@ contains
     self % poisson_solver   => poisson_solver
     self % spline_interp_2d => spline_interp_2d
 
-    self % sim_state => sim_state
-    call self % sim_state % copy( self % sim_state_copy )
+    self % sim_state      => sim_state
+    self % sim_state_copy => sim_state_copy
 
     self % abs_tol = abs_tol
     self % rel_tol = rel_tol
     self % maxiter = maxiter
 
-  end subroutine s_time_integrator__init
+  end subroutine s_time_integrator_implicit__init
 
   !-----------------------------------------------------------------------------
-  recursive subroutine s_time_integrator__advect_single_coords( self, h, success, eta )
-    class(sll_t_time_integrator), intent(in   ) :: self
-    real(wp)                    , intent(in   ) :: h
-    logical                     , intent(  out) :: success
-    real(wp)                    , intent(inout) :: eta(2)
+  recursive subroutine s_time_integrator_implicit__advect_single_coords( self, h, success, eta )
+    class(sll_t_time_integrator_implicit), intent(in   ) :: self
+    real(wp)                             , intent(in   ) :: h
+    logical                              , intent(  out) :: success
+    real(wp)                             , intent(inout) :: eta(2)
 
     integer  :: j
     real(wp) :: tol_sqr, eta0(2), x0(2), x(2), dx(2), temp(2), k2(2), a0(2)
 
-    character(len=*), parameter :: this_sub_name = "sll_t_time_integrator % advect_single_coords"
+    character(len=*), parameter :: this_sub_name = "sll_t_time_integrator_implicit % advect_single_coords"
 
     success = .true.
 
@@ -111,7 +117,7 @@ contains
     tol_sqr = ( self % abs_tol + self % rel_tol * norm2( x0 ) )**2
 
     ! Pseudo-Cartesian components of advection field
-    a0 = self % advection_field( eta )
+    a0 = self % advection_field( self % sim_state, eta )
 
     ! First iteration
     dx  = h * a0
@@ -128,7 +134,7 @@ contains
         do j = 2, self % maxiter
 
           ! k2 = f(t,x_{i-1})
-          k2 = self % advection_field( eta )
+          k2 = self % advection_field( self % sim_state, eta )
 
           dx_old = dx
           dx     = h_half*(k1+k2)
@@ -158,12 +164,74 @@ contains
 
     end if
 
-  end subroutine s_time_integrator__advect_single_coords
+  end subroutine s_time_integrator_implicit__advect_single_coords
 
   !-----------------------------------------------------------------------------
-  subroutine s_time_integrator__predictor( self, success )
-    class(sll_t_time_integrator), intent(inout) :: self
-    logical                     , intent(  out) :: success
+  subroutine s_time_integrator_implicit__advect_distribution( self, h, success, rho_new )
+    class(sll_t_time_integrator_implicit), intent(inout) :: self
+    real(wp)                             , intent(in   ) :: h
+    logical                              , intent(  out) :: success
+    real(wp)                             , intent(inout) :: rho_new(:,:)
+
+    integer  :: i1, i2, ntau1, ntau2
+    real(wp) :: eta(2)
+
+    ntau1 = size( self % tau_eta1 )
+    ntau2 = size( self % tau_eta2 )
+
+    success = .true.
+
+    !$OMP PARALLEL DO PRIVATE(eta,success)
+    do i2 = 1, ntau2
+      do i1 = 1, ntau1
+
+        eta(1) = self % tau_eta1(i1)
+        eta(2) = self % tau_eta2(i2)
+
+        call self % advect_single_coords( h, success, eta )
+
+        ! Check if integrator converged
+        if ( success ) then
+          rho_new(i1,i2) = self % sim_state % spline_2d_rho % eval( eta(1), eta(2) )
+        else
+          exit
+        end if
+
+      end do
+    end do
+    !$OMP END PARALLEL DO
+
+    ! Apply periodicity along theta
+    if ( success ) rho_new(:,ntau2+1) = rho_new(:,1)
+
+  end subroutine s_time_integrator_implicit__advect_distribution
+
+  !-----------------------------------------------------------------------------
+  subroutine s_time_integrator_implicit__advect_point_charges( self, h, success, point_charges )
+    class(sll_t_time_integrator_implicit), intent(inout) :: self
+    real(wp)                             , intent(in   ) :: h
+    logical                              , intent(  out) :: success
+    type(sll_t_point_charge)             , intent(inout) :: point_charges(:)
+
+    integer :: ic
+
+    SLL_ASSERT( size( point_charges ) > 0 )
+
+    associate( nc => self % sim_state % nc )
+
+      do ic = 1, nc
+        call self % advect_single_coords( h, success, point_charges(ic) % location )
+        if ( .not. success ) exit
+      end do
+
+    end associate
+
+  end subroutine s_time_integrator_implicit__advect_point_charges
+
+  !-----------------------------------------------------------------------------
+  subroutine s_time_integrator_implicit__predictor( self, success )
+    class(sll_t_time_integrator_implicit), intent(inout) :: self
+    logical                              , intent(  out) :: success
 
     integer :: ic
 
@@ -179,51 +247,47 @@ contains
 
       ! Evolve point charges
       if ( self % sim_state % point_charges_present ) then
+
         do ic = 1, nc
-          point_charges_copy(ic) % location(:) = point_charges(ic) % location(:)
+          point_charges_copy(ic) % location = point_charges(ic) % location
         end do
-        call self % advect_point_charges( 0.5_wp*dt, success, point_charges )
+
+        call self % advect_point_charges( 0.5_wp*dt, success, point_charges_copy )
         if ( .not. success ) return
+
       end if
 
     end associate
 
-  end subroutine s_time_integrator__predictor
+  end subroutine s_time_integrator_implicit__predictor
 
   !-----------------------------------------------------------------------------
-  subroutine s_time_integrator__corrector( self, success )
-    class(sll_t_time_integrator), intent(inout) :: self
-    logical                     , intent(  out) :: success
+  subroutine s_time_integrator_implicit__corrector( self, success )
+    class(sll_t_time_integrator_implicit), intent(inout) :: self
+    logical                              , intent(  out) :: success
 
-    integer :: ic
-
-    associate( dt                 => self % dt                            , &
-               nc                 => self % sim_state % nc                , &
-               point_charges      => self % sim_state % point_charges     , &
-               point_charges_copy => self % sim_state_copy % point_charges, &
-               rho_copy           => self % sim_state_copy % rho )
+    associate( dt            => self % dt                       , &
+               point_charges => self % sim_state % point_charges, &
+               rho           => self % sim_state % rho )
 
       ! Evolve density
-      call self % advect_distribution( -dt, success, rho_copy )
+      call self % advect_distribution( -dt, success, rho )
       if ( .not. success ) return
 
       ! Evolve point charges
       if ( self % sim_state % point_charges_present ) then
-        do ic = 1, nc
-          point_charges(ic) % location(:) = point_charges_copy(ic) % location(:)
-        end do
         call self % advect_point_charges( dt, success, point_charges )
         if ( .not. success ) return
       end if
 
     end associate
 
-  end subroutine s_time_integrator__corrector
+  end subroutine s_time_integrator_implicit__corrector
 
   !-----------------------------------------------------------------------------
-  subroutine s_time_integrator__advance_in_time( self, success )
-    class(sll_t_time_integrator), intent(inout) :: self
-    logical                     , intent(  out) :: success
+  subroutine s_time_integrator_implicit__advance_in_time( self, success )
+    class(sll_t_time_integrator_implicit), intent(inout) :: self
+    logical                              , intent(  out) :: success
 
     integer :: ic
 
@@ -232,9 +296,10 @@ contains
                spline_interp_2d   => self % spline_interp_2d              , &
                spline_2d_rho      => self % sim_state % spline_2d_rho     , &
                spline_2d_phi      => self % sim_state % spline_2d_phi     , &
-               point_charges      => self % sim_state % point_charges     , &
                rho                => self % sim_state % rho               , &
-               rho_copy           => self % sim_state_copy % rho )
+               rho_copy           => self % sim_state_copy % rho          , &
+               point_charges      => self % sim_state % point_charges     , &
+               point_charges_copy => self % sim_state_copy % point_charges )
 
       call self % predictor( success )
       if ( .not. success ) return
@@ -246,10 +311,9 @@ contains
       call self % poisson_solver % accumulate_charge ( spline_2d_rho )
       if ( self % sim_state % point_charges_present ) then
         do ic = 1, nc
-          associate( intensity => point_charges(ic)%intensity, &
-                     location  => point_charges(ic)%location )
-            call self % poisson_solver % accumulate_charge( intensity, location )
-          end associate
+          call self % poisson_solver % accumulate_charge( &
+            point_charges_copy(ic) % intensity, &
+            point_charges_copy(ic) % location )
         end do
       end if
       call self % poisson_solver % solve ( spline_2d_phi )
@@ -259,30 +323,27 @@ contains
       call self % corrector( success )
       if ( .not. success ) return
 
-      call spline_interp_2d % compute_interpolant( spline_2d_rho, rho_copy(:,1:ntau2) )
+      call spline_interp_2d % compute_interpolant( spline_2d_rho, rho(:,1:ntau2) )
 
       ! Solve Poisson equation
       call self % poisson_solver % reset_charge()
       call self % poisson_solver % accumulate_charge ( spline_2d_rho )
       if ( self % sim_state % point_charges_present ) then
         do ic = 1, nc
-          associate( intensity => point_charges(ic)%intensity, &
-                     location  => point_charges(ic)%location )
-            call self % poisson_solver % accumulate_charge( intensity, location )
-          end associate
+          call self % poisson_solver % accumulate_charge( &
+            point_charges(ic) % intensity, &
+            point_charges(ic) % location )
         end do
       end if
       call self % poisson_solver % solve ( spline_2d_phi )
 
-      self % sim_state % rho = rho_copy
-
     end associate
 
-  end subroutine s_time_integrator__advance_in_time
+  end subroutine s_time_integrator_implicit__advance_in_time
 
   !-----------------------------------------------------------------------------
-  subroutine s_time_integrator__free( self )
-    class(sll_t_time_integrator), intent(inout) :: self
+  subroutine s_time_integrator_implicit__free( self )
+    class(sll_t_time_integrator_implicit), intent(inout) :: self
 
     nullify( self % tau_eta1 )
     nullify( self % tau_eta2 )
@@ -292,6 +353,6 @@ contains
 
     call self % sim_state_copy % free()
 
-  end subroutine s_time_integrator__free
+  end subroutine s_time_integrator_implicit__free
 
-end module sll_m_time_integrator
+end module sll_m_time_integrator_implicit
