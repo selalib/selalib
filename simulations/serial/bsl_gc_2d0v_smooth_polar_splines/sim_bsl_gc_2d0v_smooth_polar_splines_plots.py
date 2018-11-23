229a7d0106e691e759287974a6d69548ad8af600
Edoardo Zoni <EZoni@lbl.gov>
2018-11-23 10:39:42 +0100

    [guiding-center] Improve Python scripts for plots.

--- simulations/serial/bsl_gc_2d0v_smooth_polar_splines/sim_bsl_gc_2d0v_smooth_polar_splines_plots.py
+++ simulations/serial/bsl_gc_2d0v_smooth_polar_splines/sim_bsl_gc_2d0v_smooth_polar_splines_plots.py
@@ -7,7 +7,7 @@ from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
 nr = n1 // 8
 
 # color map for plots
-cmap = 'Greys'
+cmap = 'jet'
 
 # angular velocity of background
 omega = 0.3332
@@ -33,13 +33,14 @@ def return_index( array, minval, maxval ):
 #-------------------------------------------------------------------------------
 
 # equilibrium density
-def plot_equilibrium_density():
+def plot_equilibrium_density( save=False ):
     fg = plt.figure(figsize=[9.0,9.0])
     ax = fg.add_subplot(111)
-    cax = make_axes_locatable(ax).append_axes( 'right', size='8%', pad='5%' )
+    #cax = make_axes_locatable(ax).append_axes( 'right', size='8%', pad='5%' )
     clevels = np.linspace( min_rho_eq, max_rho_eq, 101 )
     im = ax.contourf( x1, x2, rho_eq, clevels, cmap=cmap )
-    fg.colorbar( im, cax=cax )
+    for c in im.collections:
+        c.set_edgecolor('face')
     ax.set_title( r'Equilibrium density $\rho_{eq}$' )
     ax.plot( x1[:,::nr], x2[:,::nr], color='lightgrey', lw=0.5 )
     ax.plot( x1[:,n1-1], x2[:,n1-1], color='lightgrey', lw=0.5 )
@@ -47,11 +48,18 @@ def plot_equilibrium_density():
     ax.set_xlabel( r'$x$' )
     ax.set_ylabel( r'$y$', rotation=0 )
     ax.set_aspect( 'equal' )
+    cb = fg.colorbar( im, ax=ax )
+    cb.formatter.set_powerlimits((0,0))
+    cb.formatter.useMathText = True
+    cb.update_ticks()
     fg.tight_layout()
     fg.show()
+    if ( save ):
+       fg_name = 'figure.pdf'
+       fg.savefig( fg_name, dpi=300 )
 
 # point charge trajectory
-def plot_point_charge_trajectory( frame, tmax ):
+def plot_point_charge_trajectory( frame, tmax, save=False ):
     fg = plt.figure(figsize=[9.0,9.0])
     ax = fg.add_subplot(111)
     if ( frame == 'inertial' ):
@@ -73,9 +81,12 @@ def plot_point_charge_trajectory( frame, tmax ):
     ax.set_aspect( 'equal' )
     fg.tight_layout()
     fg.show()
+    if ( save ):
+       fg_name = 'figure.pdf'
+       fg.savefig( fg_name, dpi=300 )
 
 # time evolution of radial position (rotating frame)
-def plot_point_charge_coordinates( tmax ):
+def plot_point_charge_coordinates( tmax, save=False ):
     fg = plt.figure(figsize=[9.0,9.0])
     ax1 = fg.add_subplot(121)
     ax2 = fg.add_subplot(122)
@@ -95,15 +106,20 @@ def plot_point_charge_coordinates( tmax ):
     ax2.set_title( 'Angular position of point charge (rotating frame)' )
     fg.tight_layout()
     fg.show()
+    if ( save ):
+       fg_name = 'figure.pdf'
+       fg.savefig( fg_name, dpi=300 )
 
 # time shot of density and point charge
-def plot_point_charge_time_shot( frame, t ):
+def plot_point_charge_time_shot( frame, t, save=False ):
     fg = plt.figure(figsize=[9.0,9.0])
     ax = fg.add_subplot(111)
-    cax = make_axes_locatable(ax).append_axes( 'right', size='8%', pad='5%' )
+    #cax = make_axes_locatable(ax).append_axes( 'right', size='8%', pad='5%' )
     clevels = np.linspace( min_rho, max_rho, 101 )
     if ( frame == 'inertial' ):
         im = ax.contourf( x1, x2, rho[t], clevels, cmap=cmap )
+        for c in im.collections:
+            c.set_edgecolor('face')
         if ( nc > 0 ):
             for ic in range(nc):
                 xc = point_charges[t][ic,0]
@@ -113,6 +129,8 @@ def plot_point_charge_time_shot( frame, t ):
     if ( frame == 'rotating' ):
         x1r, x2r = rotate( x1, x2, -omega*(t*dt) )
         im = ax.contourf( x1r, x2r, rho[t], clevels, cmap=cmap )
+        for c in im.collections:
+            c.set_edgecolor('face')
         if ( nc > 0 ):
             for ic in range(nc):
                 xc = point_charges[t][ic,0]
@@ -126,60 +144,80 @@ def plot_point_charge_time_shot( frame, t ):
     ax.set_xlabel( r'$x$' )
     ax.set_ylabel( r'$y$', rotation=0 )
     ax.set_aspect( 'equal' )
-    fg.colorbar( im, cax=cax )
+    cb = fg.colorbar( im, ax=ax )
+    cb.formatter.set_powerlimits((0,0))
+    cb.formatter.useMathText = True
+    cb.update_ticks()
     fg.show()
+    if ( save ):
+       fg_name = 'figure.pdf'
+       fg.savefig( fg_name, dpi=300 )
 
 # animated plots
 def plot_time_evolution( arg, tmax ):
 
     fg = plt.figure(figsize=[9.0,9.0])
     ax = fg.add_subplot(111)
-    cax = make_axes_locatable(ax).append_axes( 'right', size='8%', pad='5%' )
+    #cax = make_axes_locatable(ax).append_axes( 'right', size='8%', pad='5%' )
 
     for t in (ti for ti in tt if ti <= tmax):
         ax.clear()
-        cax.clear()
+        #cax.clear()
         if ( arg == 'rho' ):
             clevels = np.linspace( min_rho, max_rho, 101 )
             im = ax.contourf( x1, x2, rho[t], clevels, cmap=cmap )
+            for c in im.collections:
+                c.set_edgecolor('face')
             if ( nc > 0 ):
                 for ic in range(nc):
                     xc = point_charges[t][ic,0]
                     yc = point_charges[t][ic,1]
-                    ax.plot( xc, yc, marker='o', color='w', markersize=5. )
+                    ax.plot( xc, yc, marker='o', color='k', markersize=5. )
             ax.set_title( r'Density $\rho$ at $T = %g$ (inertial frame)' %(t*dt*tconv) )
         if ( arg == 'rho_rot' ):
             clevels = np.linspace( min_rho, max_rho, 101 )
             x1r, x2r = rotate( x1, x2, -omega*(t*dt) )
             im = ax.contourf( x1r, x2r, rho[t], clevels, cmap=cmap )
+            for c in im.collections:
+                c.set_edgecolor('face')
             if ( nc > 0 ):
                 for ic in range(nc):
                     xc = point_charges[t][ic,0]
                     yc = point_charges[t][ic,1]
                     xr, yr = rotate( xc, yc, -omega*(t*dt) )
-                    ax.plot( xr, yr, marker='o', color='w', markersize=5. )
+                    ax.plot( xr, yr, marker='o', color='k', markersize=5. )
             ax.set_xlim([-1.,1.])
             ax.set_ylim([-1.,1.])
             ax.set_title( r'Density $\rho$ at $T = %g$ (rotating frame)' %(t*dt*tconv) )
         if ( arg == 'delta_rho' ):
             clevels = np.linspace( min_delta_rho, max_delta_rho, 101 )
             im = ax.contourf( x1, x2, rho[t]-rho_eq, clevels, cmap=cmap )
+            for c in im.collections:
+                c.set_edgecolor('face')
             ax.set_title( r'Density $\rho$ at $T = %g$' %(t*dt*tconv) )
         if ( arg == 'phi' ):
             clevels = np.linspace( min_phi, max_phi, 101 )
             im = ax.contourf( x1, x2, phi[t], clevels, cmap=cmap )
+            for c in im.collections:
+                c.set_edgecolor('face')
             ax.set_title( r'Potential $\phi$ at $T = %g$' %(t*dt*tconv) )
         if ( arg == 'Ex' ):
             clevels = np.linspace( min_Ex, max_Ex, 101 )
             im = ax.contourf( x1, x2, Ex[t], clevels, cmap=cmap )
+            for c in im.collections:
+                c.set_edgecolor('face')
             ax.set_title( r'Electric field component $E_x$ at $T = %g$' %(t*dt*tconv) )
         if ( arg == 'Ey' ):
             clevels = np.linspace( min_Ey, max_Ey, 101 )
             im = ax.contourf( x1, x2, Ey[t], clevels, cmap=cmap )
+            for c in im.collections:
+                c.set_edgecolor('face')
             ax.set_title( r'Electric field component $E_y$ at $T = %g$' %(t*dt*tconv) )
         if ( arg == 'Em' ):
             clevels = np.linspace( min_Em, max_Em, 101 )
             im = ax.contourf( x1, x2, Em[t], clevels, cmap=cmap )
+            for c in im.collections:
+                c.set_edgecolor('face')
             ax.set_title( r'Electric field magnitude $|E|$ at $T = %g$' %(t*dt*tconv) )
         ax.plot( x1[:,::nr], x2[:,::nr], color='lightgrey', lw=0.5 )
         ax.plot( x1[:,n1-1], x2[:,n1-1], color='lightgrey', lw=0.5 )
@@ -187,7 +225,10 @@ def plot_time_evolution( arg, tmax ):
         ax.set_xlabel( r'$x$' )
         ax.set_ylabel( r'$y$', rotation=0 )
         ax.set_aspect( 'equal' )
-        fg.colorbar( im, cax=cax )
+        cb = fg.colorbar( im, ax=ax )
+        cb.formatter.set_powerlimits((0,0))
+        cb.formatter.useMathText = True
+        cb.update_ticks()
         fg.canvas.draw()
         plt.pause(0.1)
 
@@ -198,53 +239,66 @@ def plot_time_evolution( arg, tmax ):
 #ya = ya[mask]
 
 # time evolution of scalar diagnostics
-def plot_scalar_data( arg ):
+def plot_scalar_data( arg, save=False ):
 
-    fg = plt.figure( figsize=[9.5,6.0] )
+    fg = plt.figure()
     ax = fg.add_subplot(111)
-    fs = 16
+    fs = 12
     if ( arg == 'mass' ):
-        ax.plot( xt, y1, '-r' , lw=1.5, label='Mass conservation' )
-        ax.set_ylabel( r'$\int dx\,dy\,\rho$', fontsize=fs )
+        # optimal size [6.4,2.4]
+        ax.plot( xt, (y1[0]-y1[:])/y1[0], '-r' , lw=1.5 )
+        ax.set_ylabel( r'$\mathcal{E}_\mathcal{M}$', rotation=0, fontsize=fs )
     if ( arg == 'energy' ):
-        ax.plot( xt, y2, '-r' , lw=1.5, label='Energy conservation' )
-        ax.set_ylabel( r'$\int dx\,dy\,|\nabla\phi|^2$', fontsize=fs )
+        # optimal size [6.4,2.4]
+        ax.plot( xt, (y2[0]-y2[:])/y2[0], '-r' , lw=1.5 )
+        ax.set_ylabel( r'$\mathcal{E}_\mathcal{W}$', rotation=0, fontsize=fs )
     if ( arg == 'l2_norm' ):
+        # optimal size [9.5,6.0]
         ax.plot( xt, y3, '-r' , lw=1.5, label=r'Numerical instability (mode $m=%d$)' %(l) )
-        ax.plot( xt, ya, '--k', lw=1.5, label=r'Analytical growth rate: $\gamma\approx %g$' %(gamma) )
+        ax.plot( xt, ya, '--k', lw=1.5, label=r'Analytical growth rate: Im$(\omega)\approx %1.2g$' %(gamma) )
         ax.set_ylabel( r'$||\phi-\phi_0||_{L^2}$', fontsize=fs )
         ax.set_yscale( 'log' )
         ymin = ax.get_ylim()[0]
         ymax = 2.*max(y3)
         ax.set_ylim( [ymin,ymax] )
+        ax.legend( loc='best', fontsize=fs )
     ax.set_xlabel( r'$t$', fontsize=fs )
-    ax.legend( loc='best', fontsize=fs )
+    ax.ticklabel_format( axis='y', style='sci', scilimits=(0,0), useMathText=True )
     ax.tick_params( labelsize=fs )
     ax.grid()
     fg.tight_layout()
     fg.show()
+    if ( save ):
+       fg_name = 'figure.pdf'
+       fg.savefig( fg_name, dpi=300 )
 
 # vorticity, shear, expansion
 def plot_advection_field( arg, tmax ):
 
     fg = plt.figure(figsize=[9.0,9.0])
     ax = fg.add_subplot(111)
-    cax = make_axes_locatable(ax).append_axes( 'right', size='8%', pad='5%' )
+    #cax = make_axes_locatable(ax).append_axes( 'right', size='8%', pad='5%' )
 
     for t in (ti for ti in tt if ti <= tmax):
         ax.clear()
-        cax.clear()
+        #cax.clear()
         if ( arg == 'vorticity' ):
             clevels = np.linspace( min_vorticity, max_vorticity, 101 )
             im = ax.contourf( x_meshgrid, y_meshgrid, vorticity[t], clevels, cmap=cmap )
+            for c in im.collections:
+                c.set_edgecolor('face')
             ax.set_title( r'Vorticity of advection field at $T = %g$' %(t*dt*tconv) )
         if ( arg == 'shear' ):
             clevels = np.linspace( min_shearrate, max_shearrate, 101 )
             im = ax.contourf( x_meshgrid, y_meshgrid, shearrate[t], clevels, cmap=cmap )
+            for c in im.collections:
+                c.set_edgecolor('face')
             ax.set_title( r'Shear rate of advection field at $T = %g$' %(t*dt*tconv) )
         if ( arg == 'expansion' ):
             clevels = np.linspace( min_expansion, max_expansion, 101 )
             im = ax.contourf( x_meshgrid, y_meshgrid, expansion[t], clevels, cmap=cmap )
+            for c in im.collections:
+                c.set_edgecolor('face')
             ax.set_title( r'Expansion rate of advection field at $T = %g$' %(t*dt*tconv) )
         nx = 16
         ax.plot( x_meshgrid[:,::nx], y_meshgrid[:,::nx], color='lightgrey', lw=0.5 )
@@ -257,7 +311,10 @@ def plot_advection_field( arg, tmax ):
         ax.set_xlabel( r'$x$' )
         ax.set_ylabel( r'$y$', rotation=0 )
         ax.set_aspect( 'equal' )
-        fg.colorbar( im, cax=cax )
+        cb = fg.colorbar( im, ax=ax )
+        cb.formatter.set_powerlimits((0,0))
+        cb.formatter.useMathText = True
+        cb.update_ticks()
         fg.canvas.draw()
         plt.pause(0.1)
 
@@ -268,7 +325,7 @@ index_x2 = return_index( x2_cart, -0.1, 0.1 )
 
 index = tuple( ( index_x2, index_x1 ) )
 
-def plot_stream_lines_global( frame ):
+def plot_stream_lines_global( frame, save=False ):
     t = 0
     fg = plt.figure(figsize=[9.0,9.0])
     ax = fg.add_subplot(111)
@@ -295,8 +352,11 @@ def plot_stream_lines_global( frame ):
     ax.set_ylabel( r'$y$', rotation=0 )
     ax.set_aspect( 'equal' )
     fg.show()
+    if ( save ):
+       fg_name = 'figure.pdf'
+       fg.savefig( fg_name, dpi=300 )
 
-def plot_stream_lines_local( frame ):
+def plot_stream_lines_local( frame, save=False ):
     t = 0
     fg = plt.figure(figsize=[9.0,9.0])
     ax = fg.add_subplot(111)
@@ -320,29 +380,6 @@ def plot_stream_lines_local( frame ):
     ax.set_xlabel( r'$x$' )
     ax.set_ylabel( r'$y$', rotation=0 )
     fg.show()
-
-## save figures
-#t  = 0
-#fg = plt.figure()
-#ax = fg.add_subplot(111)
-#cax = make_axes_locatable(ax).append_axes( 'right', size='8%', pad='5%' )
-#
-#clevels = np.linspace( min_rho, max_rho, 101 )
-## contour plot
-#im = ax.contourf( x1, x2, rho[t], clevels, cmap=cmap )
-#fg.colorbar( im, cax=cax )
-## grid
-#ax.plot( x1[:,::nr], x2[:,::nr], color='lightgrey', lw=0.5 )
-#ax.plot( x1[:,n1-1], x2[:,n1-1], color='lightgrey', lw=0.5 )
-#ax.plot( x1.transpose()[:,::nr], x2.transpose()[:,::nr], color='lightgrey', lw=0.5 )
-## style
-#fs = 10
-#ax.set_xlabel( r'$x$', fontsize=fs )
-#ax.set_ylabel( r'$y$', fontsize=fs, rotation=0 )
-#ax.set_title( r'Density $\rho$ at $T = %g$' %(t*dt*tconv) )
-#ax.set_aspect( 'equal' )
-#fg.tight_layout()
-#fg.show()
-#fg_name = './rho_t=%g.pdf' %(i*dt)
-#fg.savefig( fg_name, dpi=300 )
-
+    if ( save ):
+       fg_name = 'figure.pdf'
+       fg.savefig( fg_name, dpi=300 )
