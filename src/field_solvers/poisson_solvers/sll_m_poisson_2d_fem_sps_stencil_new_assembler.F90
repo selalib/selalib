2b52311f9977a4f718cb24a9bcff301617483294
Edoardo Zoni <edoardo.zoni@ipp.mpg.de>
2019-04-02 14:09:53 +0200

    Move projector and abstract class for weak form (common to Poisson solver and future quasi-neutrality solver) to existing common folder of general elliptic solvers.

--- src/field_solvers/poisson_solvers/sll_m_poisson_2d_fem_sps_stencil_new_assembler.F90
+++ src/field_solvers/poisson_solvers/sll_m_poisson_2d_fem_sps_stencil_new_assembler.F90
@@ -3,7 +3,7 @@ module sll_m_poisson_2d_fem_sps_stencil_new_assembler
 
   use sll_m_working_precision, only: f64
 
-  use sll_m_elliptic_2d_fem_sps_weak_form, only: sll_c_elliptic_2d_fem_sps_weak_form
+  use sll_m_poisson_2d_fem_sps_weak_form, only: sll_t_poisson_2d_fem_sps_weak_form
 
   implicit none
 
@@ -22,7 +22,7 @@ module sll_m_poisson_2d_fem_sps_stencil_new_assembler
     integer :: p1
     integer :: p2
 
-    class(sll_c_elliptic_2d_fem_sps_weak_form), pointer :: weak_form
+    class(sll_t_poisson_2d_fem_sps_weak_form), pointer :: weak_form
 
   contains
 
@@ -38,12 +38,12 @@ contains
 
   ! Initializer
   subroutine s_poisson_2d_fem_sps_stencil_new_assembler__init( self, n1, n2, p1, p2, weak_form )
-    class(sll_t_poisson_2d_fem_sps_stencil_new_assembler) , intent(inout) :: self
-    integer                                               , intent(in   ) :: n1
-    integer                                               , intent(in   ) :: n2
-    integer                                               , intent(in   ) :: p1
-    integer                                               , intent(in   ) :: p2
-    class(sll_c_elliptic_2d_fem_sps_weak_form),     target, intent(in   ) :: weak_form
+    class(sll_t_poisson_2d_fem_sps_stencil_new_assembler), intent(inout) :: self
+    integer                                              , intent(in   ) :: n1
+    integer                                              , intent(in   ) :: n2
+    integer                                              , intent(in   ) :: p1
+    integer                                              , intent(in   ) :: p2
+    class(sll_t_poisson_2d_fem_sps_weak_form),     target, intent(in   ) :: weak_form
 
     self % n1 = n1
     self % n2 = n2
