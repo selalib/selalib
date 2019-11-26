a76b92cb47e0cb455a5b704a3d3e7da54014fad1
Klaus Reuter <khr@rzg.mpg.de>
2019-11-26 14:27:55 +0100

    updates to cmake files and build scripts picked from bsl_6d

--- cmake/CompilerConfigString.cmake
+++ cmake/CompilerConfigString.cmake
@@ -1,10 +1,8 @@
-# Macro to write out compiler and compiler flags in simulations
-# author: Klaus Reuther
 set(SLL_COMP "${Fortran_COMPILER_NAME} ${Fortran_COMPILER_VERSION}")
-IF(CMAKE_BUILD_TYPE MATCHES Debug)
-   set(SLL_COMP "${SLL_COMP} (${CMAKE_Fortran_FLAGS_DEBUG} )")
+IF(CMAKE_BUILD_TYPE MATCHES "Debug")
+   set(SLL_COMP "${SLL_COMP} (${CMAKE_Fortran_FLAGS_DEBUG})")
 ELSE()
-   set(SLL_COMP "${SLL_COMP} (${CMAKE_Fortran_FLAGS_RELEASE} )")
+   set(SLL_COMP "${SLL_COMP} (${CMAKE_Fortran_FLAGS_RELEASE})")
 ENDIF()
 MESSAGE( STATUS "SLL_COMP:${SLL_COMP}" )
 add_definitions( -DSLL_COMP="${SLL_COMP}" )
