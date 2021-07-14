5b563cd0d848dba3e02ae52d0bd3ca4793a1b058
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2021-07-14 17:01:41 +0200

    Update FortranConfig.cmake
    Add the flag -fallow-argument-mismatch when gfortran version is greater or equal to 10

--- cmake/FortranConfig.cmake
+++ cmake/FortranConfig.cmake
@@ -34,12 +34,17 @@ elseif(CMAKE_Fortran_COMPILER_ID MATCHES GNU)
     ${CMAKE_Fortran_COMPILER} ARGS
     "--version"
     OUTPUT_VARIABLE source_path)
-  string(REGEX MATCH "[4-9]\\.[0-9]\\.[0-9]" Fortran_COMPILER_VERSION
+  string(REGEX MATCH "([0-9]+)\\.[0-9]+\\.[0-9]+" Fortran_COMPILER_VERSION
                ${source_path})
+
+  string(REPLACE "." ";" VERSION_LIST ${Fortran_COMPILER_VERSION})
+  list(GET VERSION_LIST 0 Fortran_VERSION_MAJOR)
+  list(GET VERSION_LIST 1 Fortran_VERSION_MINOR)
+  list(GET VERSION_LIST 2 Fortran_VERSION_PATCH)
 
   add_definitions(-DGFORTRAN)
   set(CMAKE_Fortran_FLAGS_RELEASE
-      "-std=f2008 -ffree-line-length-none -fstack-arrays -O3 -fPIC  -w")
+      "-std=f2008 -ffree-line-length-none -fstack-arrays -O3 -fPIC  -w -fallow-argument-mismatch")
   if(NOT APPLE)
     set(CMAKE_Fortran_FLAGS_RELEASE
         "${CMAKE_Fortran_FLAGS_RELEASE} -march=native")
@@ -67,13 +72,20 @@ elseif(CMAKE_Fortran_COMPILER_ID MATCHES GNU)
         "${CMAKE_Fortran_FLAGS_DEBUG} -Wno-unused-dummy-argument")
   endif()
 
+  if(Fortran_VERSION_MAJOR GREATER_EQUAL 10)
+    set(CMAKE_Fortran_FLAGS_DEBUG
+        "${CMAKE_Fortran_FLAGS_DEBUG} -fallow-argument-mismatch")
+    set(CMAKE_Fortran_FLAGS_RELEASE
+        "${CMAKE_Fortran_FLAGS_RELEASE} -fallow-argument-mismatch")
+  endif()
+
 else()
 
   message(SEND_ERROR "NO KNOWN FORTRAN COMPILER FOUND")
 
 endif()
 
-message(STATUS "Fortran ${Fortran_COMPILER_NAME}-${Fortran_COMPILER_VERSION}")
+message(STATUS "Fortran ${Fortran_COMPILER_NAME} version ${Fortran_COMPILER_VERSION}")
 
 # --- enable fully user-defineable compiler flags
 if(FORCE_Fortran_FLAGS_RELEASE)
