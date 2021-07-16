06964c928ddf51abef923e9cc5da46dbf711d76f
Pierre Navaro <navaro@math.cnrs.fr>
2021-07-16 12:10:48 +0200

    Update test_splines_pp_2d.F90

--- src/splines/splines_basic/testing/test_splines_pp_2d.F90
+++ src/splines/splines_basic/testing/test_splines_pp_2d.F90
@@ -56,22 +56,27 @@ program test_splines_pp_2d_boundary
    print *, 'periodic'
    boundary = [0, 0]
    call spline_test(spline_pp, degree, n_cells, boundary, xnorm, fail)
+   if (fail) stop 'FAILED'
    print *, 'clamped, first interval, second last interval'
    boundary = [1, 1]
    xnorm = [0.01_f64, 0.97_f64]
    call spline_test(spline_pp, degree, n_cells, boundary, xnorm, fail)
+   if (fail) stop 'FAILED'
    print *, 'clamped, last interval, second interval'
    xnorm = [0.99_f64, 0.03_f64]
    boundary = [1, 1]
    call spline_test(spline_pp, degree, n_cells, boundary, xnorm, fail)
+   if (fail) stop 'FAILED'
    print *, 'clamped, periodic'
    boundary = [1, 0]
    xnorm = [0.035_f64, 0.567_f64]
    call spline_test(spline_pp, degree, n_cells, boundary, xnorm, fail)
+   if (fail) stop 'FAILED'
    print *, 'clamped, periodic'
    boundary = [1, 0]
    xnorm = [0.567_f64, 0.035_f64]
    call spline_test(spline_pp, degree, n_cells, boundary, xnorm, fail)
+   if (fail) stop 'FAILED'
    print *, 'clamped-clampeddiri, periodic'
    degree = [3, 3]
    boundary = [2, 0]
@@ -81,41 +86,49 @@ program test_splines_pp_2d_boundary
    boundary = [3, 0]
    xnorm = [0.085_f64, 0.567_f64]
    call spline_test(spline_pp, degree, n_cells, boundary, xnorm, fail)
+   if (fail) stop 'FAILED'
    print *, 'clampeddiri, periodic, last interval'
    degree = [3, 3]
    boundary = [3, 0]
    xnorm = [0.985_f64, 0.567_f64]
    call spline_test(spline_pp, degree, n_cells, boundary, xnorm, fail)
+   if (fail) stop 'FAILED'
    print *, 'clampeddiri-clamped, periodic'
    degree = [3, 3]
    boundary = [4, 0]
    xnorm = [0.085_f64, 0.567_f64]
    call spline_test(spline_pp, degree, n_cells, boundary, xnorm, fail)
+   if (fail) stop 'FAILED'
    print *, 'periodic, clamped-clampeddiri'
    degree = [3, 3]
    boundary = [0, 2]
    xnorm = [0.567_f64, 0.995_f64]
    call spline_test(spline_pp, degree, n_cells, boundary, xnorm, fail)
+   if (fail) stop 'FAILED'
    print *, 'periodic, clampeddiri, last interval'
    degree = [3, 3]
    boundary = [0, 3]
    xnorm = [0.567_f64, 0.99_f64]!0.975_f64]
    call spline_test(spline_pp, degree, n_cells, boundary, xnorm, fail)
+   if (fail) stop 'FAILED'
    print *, 'periodic, clampeddiri, first interval'
    degree = [3, 3]
    boundary = [0, 3]
    xnorm = [0.567_f64, 0.01_f64]
    call spline_test(spline_pp, degree, n_cells, boundary, xnorm, fail)
+   if (fail) stop 'FAILED'
    print *, 'periodic, clampeddiri-clamped, last interval'
    degree = [3, 3]
    boundary = [0, 4]
    xnorm = [0.567_f64, 0.995_f64]
    call spline_test(spline_pp, degree, n_cells, boundary, xnorm, fail)
+   if (fail) stop 'FAILED'
    print *, 'periodic, clampeddiri-clamped, first interval'
    degree = [3, 3]
    boundary = [0, 4]
    xnorm = [0.567_f64, 0.01_f64]
    call spline_test(spline_pp, degree, n_cells, boundary, xnorm, fail)
+   if (fail) stop 'FAILED'
 
    print *, 'Order 2'
    degree = [2, 2]
@@ -124,12 +137,9 @@ program test_splines_pp_2d_boundary
    xnorm = [0.767_f64, 0.99_f64]
    call spline_test(spline_pp, degree, n_cells, boundary, xnorm, fail)
 
-   if (fail .eqv. .false.) then
-      write (*, *) 'PASSED'
-   else
-      write (*, *) 'FAILED'
-      stop
-   end if
+   if (fail) stop 'FAILED'
+
+   write (*, *) 'PASSED'
 
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 contains
@@ -182,6 +192,7 @@ contains
          breaks2(i + 1) = breaks2(i) + delta_x(2)
       end do
 
+      call random_seed(put=seed)
       call random_number(b_coeffs)
 
       call sll_s_spline_pp_init_2d(spline_pp, degree, n_cells, boundary)
@@ -333,6 +344,7 @@ contains
       end if
 
       !test horner for arbitrary polynomials
+      call random_seed(put=seed)
       call random_number(xp)
       res = sll_f_spline_pp_horner_2d(degree, pp_coeffs, xp, [1, 1], [1, 1])
       res2 = 0._f64
