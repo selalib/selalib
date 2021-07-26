c2e709b06f0be1cf26d6b19c59d78b59e3739496
benedikt <benedikt.perse@ipp.mpg.de>
2021-07-26 11:22:41 +0200

    ctest pmc clamped

--- src/particle_methods/pic_basic/particle_mesh_coupling/testing/test_particle_mesh_coupling_spline_cl_1d.F90
+++ src/particle_methods/pic_basic/particle_mesh_coupling/testing/test_particle_mesh_coupling_spline_cl_1d.F90
@@ -3,6 +3,9 @@ program test_particle_mesh_coupling_spline_cl_1d
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #include "sll_working_precision.h"
 
+  use sll_m_constants, only: &
+       sll_p_twopi
+  
   use sll_m_particle_mesh_coupling_base_1d, only: &
     sll_p_collocation, &
     sll_c_particle_mesh_coupling_1d
@@ -13,23 +16,29 @@ program test_particle_mesh_coupling_spline_cl_1d
      sll_s_new_particle_mesh_coupling_spline_cl_1d_ptr
     
   use sll_m_particle_group_1d2v, only: &
-    sll_t_particle_group_1d2v
-
+       sll_t_particle_group_1d2v
+  
   use sll_m_splines_pp
+  
+  use sll_m_maxwell_clamped_1d_fem_sm, only: &
+       sll_t_maxwell_clamped_1d_fem_sm
 
   implicit none
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-  !class(sll_c_particle_mesh_coupling), allocatable :: ksa
+
+  sll_int32, parameter :: n_cells = 10; ! Number of cells
+  sll_int32, parameter :: n_particles = 4 ! Number of particles
+  sll_int32, parameter :: spline_degree = 3 ! Spline degree
+  
   class(sll_c_particle_mesh_coupling_1d), pointer     :: ksp
   type(sll_t_particle_mesh_coupling_spline_cl_1d) :: kernel
+  type(sll_t_maxwell_clamped_1d_fem_sm) :: maxwell
   ! Abstract particle group
   type(sll_t_particle_group_1d2v) :: particle_group
 
   ! Parameters for the test
-  sll_int32 :: n_cells, boundary
-  sll_int32 :: n_particles
-  sll_int32 :: spline_degree
+  sll_int32 :: boundary
   sll_real64 :: domain(2)
   sll_real64 :: dx
   sll_real64 :: x_vec(4)
@@ -37,45 +46,29 @@ program test_particle_mesh_coupling_spline_cl_1d
 
   ! helper variables
   sll_int32  :: i_part 
-  sll_real64 :: xi(3), wi(1), vi(3), vi1(3), x_new(1)
+  sll_real64 :: xi(3), wi(1), vi(3), x_new(1), vh
   logical :: passed
   sll_real64 :: error
-  sll_real64 :: error1
   sll_real64 :: error2
-  sll_real64 :: error3
-  sll_real64 :: error4
+  sll_real64 :: eval, eval_ref
   
   ! Sparse structure for shape factors (for reference)
-  !sll_int32 :: index_grid(1,4)    !< First dof index with contribution from th
   sll_real64 :: values_grid(4,1,4) !< Values of the space factors in each dimesion.
 
   ! Rho dofs
-  sll_real64 :: rho_dofs(13)
-  sll_real64 :: rho_dofs1(13)
-  sll_real64 :: rho_dofs_ref(13)
+  sll_real64 :: rho_dofs(n_cells+spline_degree)
+  sll_real64 :: rho_dofs_ref(n_cells+spline_degree)
   ! J dofs
-  sll_real64 :: j_dofs(12)
-  sll_real64 :: j_dofs2(12)
-  sll_real64 :: j_dofs_ref(12)
-  ! B dofs
-  sll_real64 :: b_dofs(12)
-
-  ! For evaluation check
-  sll_real64 :: particle_values(4)
-  sll_real64 :: particle_values1(4)
-  sll_real64 :: particle_values_ref(4)
-
+  sll_real64 :: j_dofs(n_cells+spline_degree)
+  sll_real64 :: j_dofs_ref(n_cells+spline_degree)
   ! 
   passed = .TRUE.
 
   ! This tests the kernel smoother for a fixed particle and grid and spline degree 3.
   ! Test parameters
-  n_cells = 10; ! Number of cells
-  n_particles = 4 ! Number of particles
-  spline_degree = 3 ! Spline degree
   domain = [0.0_f64, 2.0_f64] ! x_min, x_max
   x_vec = [0.1_f64, 0.65_f64, 0.7_f64, 1.5_f64] ! Particle positions
-  v_vec(:,1) = [1.5_f64, -3.0_f64, 0.0_f64, 6.0_f64]
+  v_vec(:,1) = [1.5_f64, -3.0_f64, 0.2_f64, 6.0_f64]
   v_vec(:,2) = [0.0_f64, 0.5_f64, 0.0_f64, 0.0_f64]
   dx = (domain(2)-domain(1))/real(n_cells,f64)
 
@@ -94,12 +87,12 @@ program test_particle_mesh_coupling_spline_cl_1d
      call particle_group%set_v(i_part, xi)
   end do
   
-  values_grid(:,1,1) = [ 2.0833333333333332E-002_f64,  0.47916666666666663_f64,    &
-       0.47916666666666663_f64,        2.0833333333333332E-002_f64]
+  values_grid(:,1,1) = [ 2.0833333333333E-002_f64,  0.4791666666666_f64,    &
+       0.47916666666666_f64,        2.0833333333333E-002_f64]
   values_grid(:,1,3) = values_grid(:,1,1)   
   values_grid(:,1,4) = values_grid(:,1,1) 
-  values_grid(:,1,2) = [7.0312500000000000E-002_f64,  0.61197916666666663_f64, &
-       0.31510416666666663_f64,        2.6041666666666665E-003_f64 ]
+  values_grid(:,1,2) = [7.03125E-002_f64,  0.61197916666666_f64, &
+       0.315104166666666_f64,        2.60416666666666E-003_f64 ]
 
   if( spline_degree == 2 ) then
      boundary = sll_p_boundary_clamped_square
@@ -108,6 +101,9 @@ program test_particle_mesh_coupling_spline_cl_1d
   else
      boundary = sll_p_boundary_clamped
   end if
+
+  ! Initialize Maxwell solver
+  call maxwell%init( domain, n_cells, spline_degree, boundary )
 
   ! Initialize the kernel
   call kernel%init &
@@ -119,81 +115,65 @@ program test_particle_mesh_coupling_spline_cl_1d
 
   ! Accumulate rho
   rho_dofs = 0.0_f64
-  rho_dofs1 = 0.0_f64
   do i_part = 1,n_particles
      xi = particle_group%get_x(i_part)
      wi = particle_group%get_charge(i_part)
      call kernel%add_charge(xi(1), wi(1), rho_dofs)
   end do
-  !rho_dofs = rho_dofs
-  rho_dofs_ref = 0.0_f64
-  rho_dofs_ref(8:10) = values_grid(1:3,1,1)
-  rho_dofs_ref(1) = values_grid(4,1,1)
-  rho_dofs_ref(1:4) = rho_dofs_ref(1:4) + values_grid(:,1,2) + values_grid(:,1,3)
-  rho_dofs_ref(5:8) = rho_dofs_ref(5:8) + values_grid(:,1,4)
-  rho_dofs_ref = rho_dofs_ref/real(n_particles, f64) * real(n_cells,f64)/domain(2)
+  
+  rho_dofs_ref = [   0.0_f64,       0.7421875_f64, &
+       0.3255208333333_f64,       0.1399739583333_f64,&
+       1.3639322916666_f64,       0.99283854166666_f64,&
+       2.9296874999999E-002_f64,  2.60416666666666E-002_f64, &
+       0.5989583333333_f64,       0.59895833333333_f64, &
+       2.6041666666666E-002_f64,  0.0_f64,   0.0_f64 ];
   error = maxval(abs(rho_dofs-rho_dofs_ref))
 
   
-  if (error > 1.e-14) then
+  if (error > 1.d-13) then
      passed = .FALSE.
-     print*, 'Error in procedure add_charge .'
+     print*, 'Error in procedure add_charge:', error
   end if
 
   ! Test j accumulations
   j_dofs = 0.0_f64
-  j_dofs2 = 0.0_f64
-  b_dofs = 0.0_f64
   do i_part=1, n_particles 
      xi = particle_group%get_x(i_part)
      wi = particle_group%get_charge(i_part)
      vi = particle_group%get_v(i_part)
 
      x_new = xi(1) + vi(1)/20.0_f64
+     vh = vi(2)/vi(1) 
         
-     call kernel%add_current_update_v( xi(1), x_new, wi(1), 1.0_f64, b_dofs, vi, j_dofs )
-    
-     call kernel%add_current( xi(1), x_new, wi(1), j_dofs2 )
+     call kernel%add_current( xi(1), x_new, vh*wi(1), j_dofs )
   end do
-  j_dofs_ref = [ 2.4617513020833336D-002,   4.0690104166666692D-005, 0.0_f64, &
-       0.0_f64, 0.0_f64, 0.0_f64,  0.0_f64,  6.5104166666666674D-004, &
-       4.6834309895833329D-002,    0.11535644531249999_f64, &
-       0.11535644531249999_f64,    0.11535644531249999_f64];
-  j_dofs_ref = j_dofs_ref + [ -0.16219075520833331_f64, -0.16219075520833331_f64, &
-       -2.52685546875D-002,  -4.0690104166666692D-005 , &
-       0.0_f64, 0.0_f64, 0.0_f64, 0.0_f64,  &
-       -4.0690104166666692D-005,  -2.52685546875D-002, &
-       -2.52685546875D-002, -2.52685546875D-002];
-  j_dofs_ref = j_dofs_ref + [ 6.5104166666666696D-004,  0.0_f64, 0.0_f64,  &
-       0.0_f64, 6.5104166666666674D-004, 5.0130208333333329D-002,  &
-       0.19986979166666666_f64, 0.24869791666666663_f64,  &
-       0.19986979166666666_f64, 5.0130208333333329D-002, &
-       5.0130208333333329D-002, 5.0130208333333329D-002];
- 
-  error = maxval(abs(j_dofs-j_dofs_ref))
-  error2= maxval(abs(j_dofs2-j_dofs_ref*dx))  
   
-  if (error > 1.e-14) then
+  j_dofs_ref = [    0.0_f64,   0.0_f64,  -1.44675925925926E-004_f64, &
+       -1.04076244212962E-002_f64,  -2.5634765625E-002_f64, &
+       -5.47055844907407E-003_f64,  -9.04224537037E-006_f64,   &
+       0.0_f64, 0.0_f64, 0.0_f64, 0.0_f64, 0.0_f64, 0.0_f64 ];
+  
+
+  error= maxval(abs(j_dofs-j_dofs_ref))  
+  if (error > 1.d-14) then
      passed = .FALSE.
-     print*, 'Error in procedure add_current_update_v.'
-  elseif (error2 > 1.e-14) then
-     passed = .FALSE.
-     print*, 'Error in procedure add_current.'
+     print*, 'Error in procedure add_current:', error2
   end if
    
-  ! Test function evaluation
-  do i_part = 1, n_particles
-     xi = particle_group%get_x(i_part)
-     call kernel%evaluate(xi(1), rho_dofs, particle_values(i_part))
-  end do
-  particle_values_ref = [1.1560058593749998_f64,       2.3149278428819446_f64, &
-       2.2656250000000000_f64,        1.1512586805555554_f64]/domain(2);
-  error = maxval(abs(particle_values-particle_values_ref))
-  error1 = maxval(abs(particle_values1-particle_values_ref))
-  if (error > 1.e-14) then
-     passed = .FALSE.
-     print*, 'Error in procedure evaluate_field_single.'
+  ! Test evaluate
+  xi = 1.756_f64
+  call maxwell%L2projection( test_func, spline_degree, rho_dofs  )
+  call kernel%evaluate( xi, rho_dofs, eval )
+
+  eval_ref = test_func( xi(1) )
+
+  error = eval_ref -eval;
+  if (error > 1.1d-3) then
+     print*, 'Error in procedure evaluate', error
+     passed = .false.
   end if
+
+  
   if (passed .EQV. .TRUE.) then
      print*, 'PASSED'
   else
@@ -203,6 +183,15 @@ program test_particle_mesh_coupling_spline_cl_1d
 
   call kernel%free()
   call particle_group%free()
+
+contains
+  
+  function test_func(x)
+    sll_real64             ::test_func
+    sll_real64, intent(in) :: x
+
+    test_func = sin(sll_p_twopi*x/domain(2)) 
+  end function test_func
 
 
 end program test_particle_mesh_coupling_spline_cl_1d
